4989661f8f2c461d4d7f1026269ffa1c4c9e025e
Nicklas Utgaard <nicklas.utgaard@nav.no>
2017-02-23 15:09:44 +0100

    La til global decorator slik at vi får litt padding i hovedvisningen

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,11 @@
-import { configure } from '@kadira/storybook';
+import React from 'react';
+import { configure, addDecorator } from '@kadira/storybook';
 
 const req = require.context('./../packages/node_modules', true, /stories\.js$/);
+
+addDecorator((story) => (
+    <div style={{padding: '1rem'}}>{story()}</div>
+));
 
 function loadStories() {
     req.keys().forEach((filename) => req(filename));
