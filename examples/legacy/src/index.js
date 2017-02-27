149314731730e597eb6434d245407d37c19e69c9
Nicklas Utgaard <nicklas.utgaard@nav.no>
2017-02-27 14:30:30 +0100

    Tar ibruk typografi og fikset less avhengighet

--- examples/legacy/src/index.js
+++ examples/legacy/src/index.js
@@ -1,17 +1,19 @@
 // Skrevet i ES5 så man slipper babel-avhengighet for eksempelprosjektet
 const React = require('react');
 const ReactDOM = require('react-dom');
+const Typografi = require('nav-frontend-typografi');
 const Etiketter = require('nav-frontend-etiketter');
 const Paneler = require('nav-frontend-paneler');
 
 const createElement = React.createElement;
+const Sidetittel = Typografi.Sidetittel;
 const Panel = Paneler.Panel;
 const AdvarselEtikett = Etiketter.EtikettAdvarsel;
 
 function Application() {
     return createElement('div', {},
         createElement(Panel, {},
-            createElement('h1', {}, 'Test')
+            createElement(Sidetittel, {}, 'Test')
         ),
         createElement(Panel, {},
             createElement(AdvarselEtikett, {}, 'Oops')
