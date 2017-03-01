9339282602967c4f7e5152de867b163f053a0be5
Nicklas Utgaard <nicklas.utgaard@nav.no>
2017-03-01 10:19:46 +0100

    Custom webpack setup.
    Endrer på UglifyJS setup slik at funksjons-navn blir værende for react-views

---
+++ .storybook/webpack.config.custom.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    module: {
+        loaders: [
+            {
+                test: /\.less$/,
+                loaders: ['style', 'css', 'less?{"globalVars":{"nodeModulesPath":"\'./../..\'", "coreModulePath":"\'./../../../../node_modules\'"}}'],
+                include: path.resolve(__dirname, '../')
+            },
+            {
+                test: /\.md$/,
+                loaders: ['raw']
+            },
+            {
+                test: /\.json$/,
+                loaders: ['json']
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css']
+            }
+        ]
+    }
+};
