8332dc4a18caa90c6e2eb2c4de993684cbdb4f76
Nicklas Utgaard <nicklas.utgaard@nav.no>
2017-03-01 16:28:35 +0100

    La til webpack example, med og uten style-loader

---
+++ examples/webpack/webpack.config.style.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'out'),
+        filename: 'bundle.styles.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.less$/, loader: ['style-loader', 'css-loader', 'less-loader?{"globalVars":{"nodeModulesPath":"\'./../../\'", "coreModulePath":"\'./../../\'"}}'] }
+        ]
+    }
+};
