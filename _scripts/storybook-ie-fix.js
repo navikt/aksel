5dfbb1f9ebcc79ad4a82982d7595823e22cf9754
Nicklas Utgaard <nicklas.utgaard@nav.no>
2017-03-09 12:20:31 +0100

    IE emulerer IE7 om man ikke sier noe annet.
    For å si noe annet legger vi til X-UA-Compatible header etter bygging av storybook. PR pending

---
+++ _scripts/storybook-ie-fix.js
@@ -0,0 +1,20 @@
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+function readFile(src) {
+    return Promise.resolve(fs.readFileSync(src, 'utf-8'));
+}
+function fix(content) {
+    const $ = cheerio.load(content);
+
+    $('head').append('    <meta http-equiv="X-UA-Compatible" content="IE=edge">\n');
+    console.log(`html`, $.html());
+    return $.html();
+}
+function writeToFile(src) {
+    return (content) => Promise.resolve(fs.writeFileSync(src, content, 'utf-8'));
+}
+
+readFile('./../dist/index.html')
+    .then(fix)
+    .then(writeToFile('./../dist/new.html'));
