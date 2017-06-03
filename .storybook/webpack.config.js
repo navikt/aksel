924e176ce5e9579d9bc884fe867060f24baaba13
Nicklas Utgaard <nicklas.utgaard@bekk.no>
2017-06-03 16:50:46 +0200

    Revert "Custom webpack setup."
    This reverts commit 9339282602967c4f7e5152de867b163f053a0be5.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,45 +1,25 @@
 const path = require('path');
-const webpack = require('webpack');
-const customConfig = require('./webpack.config.custom.js');
-const extend = require('extend');
 
-module.exports = (config, configType) => {
-    const mergedConfig = merge(config, customConfig);
-    mergedConfig.entry.manager.unshift('babel-polyfill');
-    mergedConfig.entry.preview.unshift('babel-polyfill');
-
-    if (configType !== 'PRODUCTION') {
-        return mergedConfig;
-    } else {
-        mergedConfig.plugins.pop();
-        mergedConfig.plugins.push(new webpack.optimize.UglifyJsPlugin({
-            sourceMap: false,
-            compress: {
-                screw_ie8: true,
-                warnings: false,
-                keep_fnames: true
+module.exports = {
+    module: {
+        loaders: [
+            {
+                test: /\.less$/,
+                loaders: ['style', 'css', 'less?{"globalVars":{"nodeModulesPath":"\'./../..\'", "coreModulePath":"\'./../../../../node_modules\'"}}'],
+                include: path.resolve(__dirname, '../')
             },
-            mangle: { keep_fnames: true },
-            output: {
-                comments: false,
-                screw_ie8: true
+            {
+                test: /\.md$/,
+                loaders: ['raw']
+            },
+            {
+                test: /\.json$/,
+                loaders: ['json']
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css']
             }
-        }));
-        return mergedConfig;
+        ]
     }
 };
-
-function merge(config, custom) {
-    const plugins = config.plugins.concat(custom.plugins || []);
-    const loaders = config.module.loaders.concat(custom.module.loaders || []);
-    return extend({}, config, custom, {
-        devtool: custom.devtool || config.devtool,
-        plugins: plugins,
-        module: extend({}, config.module, custom.module, {
-            loaders: loaders
-        }),
-        resolve: extend({}, config.resolve, custom.resolve, {
-            alias: extend({}, config.alias, (custom.resolve && custom.resolve.alias))
-        })
-    });
-}
