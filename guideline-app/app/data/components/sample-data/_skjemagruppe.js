409d292a6e29341c487979297f9b0af2e365c904
Erlend Vige <erlend.vige@gmail.com>
2017-07-26 12:39:49 +0200

    remove old Skjemagruppe example function

--- guideline-app/app/data/components/sample-data/_skjemagruppe.js
+++ guideline-app/app/data/components/sample-data/_skjemagruppe.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import { SkjemaGruppe, Checkbox } from './../../../../../packages/node_modules/nav-frontend-skjema';
 
-const SkjemaGruppeComp = (props) => {
-    const errorMessage = { feilmelding:'Feil! Velg minst et valg' };
-
-    return (
-        <SkjemaGruppe title="Hvor vil du sitte?" feil={ props.hasError ? errorMessage : null }>
-            <Checkbox label="Bakerst" value="bakerst" id="bakerst-checkbox" name="bakerst" />
-            <Checkbox label="Fremst" value="fremst" id="fremst-checkbox" name="fremst" />
-            <Checkbox label="Midten" value="midten" id="midten-checkbox" name="midten" />
-        </SkjemaGruppe>
-    )
-};
-
 const options = [
     { label: 'Bakerst', value: 'bakerst', id: 'bakerst-checkbox', name: 'bakerst' },
     { label: 'Fremst', value: 'fremst', id: 'fremst-checkbox', name: 'fremst' },
