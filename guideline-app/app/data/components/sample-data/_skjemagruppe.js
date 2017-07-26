bf0d927efbba78966969e94c8db4c4603376cf6a
Erlend Vige <erlend.vige@gmail.com>
2017-07-26 12:39:20 +0200

    Fixed react code example for SkjemaGruppe component

--- guideline-app/app/data/components/sample-data/_skjemagruppe.js
+++ guideline-app/app/data/components/sample-data/_skjemagruppe.js
@@ -13,17 +13,39 @@ const SkjemaGruppeComp = (props) => {
     )
 };
 
+const options = [
+    { label: 'Bakerst', value: 'bakerst', id: 'bakerst-checkbox', name: 'bakerst' },
+    { label: 'Fremst', value: 'fremst', id: 'fremst-checkbox', name: 'fremst' },
+    { label: 'Midten', value: 'midten', id: 'midten-checkbox', name: 'midten' }
+];
+
+const checkboxChildren = (
+    options.map((checkboxChildProps) => ({
+        component: Checkbox,
+        attrs: checkboxChildProps
+    }))
+);
+
 const skjemagruppe = {
     children: '',
     types: [
         {
-            component: () => <SkjemaGruppeComp />,
+            component: SkjemaGruppe,
+            children: checkboxChildren,
             label: 'Vanlig',
+            attrs: {
+                title: 'Hvor vil du sitte?'
+            },
             _default: true
         },
         {
-            component: () => <SkjemaGruppeComp hasError="true" />,
+            component: SkjemaGruppe,
+            children: checkboxChildren,
             label: 'Med feilmelding',
+            attrs: {
+                title: 'Hvor vil du sitte?',
+                feil: { feilmelding: 'Feil! Velg minst et valg' }
+            },
             _default: true
         }
     ]
