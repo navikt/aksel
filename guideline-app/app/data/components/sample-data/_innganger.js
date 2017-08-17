6b8cbc5fe65c3e01c8f2c711893709cf9297cb75
Erlend Vige <erlend.vige@gmail.com>
2017-08-17 14:53:00 +0200

    Fix sample-data for _innganger.js

--- guideline-app/app/data/components/sample-data/_innganger.js
+++ guideline-app/app/data/components/sample-data/_innganger.js
@@ -6,55 +6,34 @@ import {
     StorVertikalInngang
 } from 'NavFrontendModules/nav-frontend-inngang';
 
-const innganger = {
-    types: [
-        {
-            component: AppInngang,
-            attrs: {
-                tittel: 'Gå til en applikasjon'
-            },
-            label: 'Appinngang',
-            _default: true
-        },
-        {
-            component: DobbelIngressInngang,
-            attrs: {
-                tittel: 'Her er en tittel',
-                ingress1: 'Her er ingress 1',
-                ingress2: 'Her er ingress 2',
-                ikon: 'info-sirkel'
-            },
-            label: 'Dobbel-ingress inngang'
-        },
-        {
-            component: EnkelIngressInngang,
-            attrs: {
-                tittel: 'Her er en tittel',
-                ingress: 'Her er en ingress',
-                ikon: 'info-sirkel'
-            },
-            label: 'Enkel-ingress inngang'
-        },
-        {
-            component: NormalVertikalInngang,
-            attrs: {
-                tittel: 'Her er en tittel',
-                tekst: 'Og her har du en tekst',
-                lesMerHref: '/#/components/innganger/lenkeeksempel/'
-            },
-            label: 'Normal vertikal inngang'
-        },
-        {
-            component: StorVertikalInngang,
-            attrs: {
-                tittel: 'Her er en tittel',
-                ingress: 'Og her har du en ingress',
-                ikon: 'info-sirkel'
-            },
-            label: 'Stor vertikal inngang'
-        }
+import {
+    createSampleData,
+    newType
+} from './../sampleDataHelper';
 
-    ]
-};
+const types = [
+    newType(AppInngang, 'Appinngang', null, { tittel: 'Gå til en applikasjon' }),
+    newType(DobbelIngressInngang, 'Dobbel-ingress inngang', null, {
+        tittel: 'Her er en tittel',
+        ingress1: 'Her er ingress 1',
+        ingress2: 'Her er ingress 2',
+        ikon: 'info-sirkel'
+    }),
+    newType(EnkelIngressInngang, 'Enkel-ingress inngang', null, {
+        tittel: 'Her er en tittel',
+        ingress: 'Her er en ingress',
+        ikon: 'info-sirkel'
+    }),
+    newType(NormalVertikalInngang, 'Normal vertikal inngang', null, {
+        tittel: 'Her er en tittel',
+        tekst: 'Og her har du en tekst',
+        lesMerHref: '/#/components/innganger/lenkeeksempel/'
+    }),
+    newType(StorVertikalInngang, 'Stor vertikal inngang', null, {
+        tittel: 'Her er en tittel',
+        ingress: 'Og her har du en ingress',
+        ikon: 'info-sirkel'
+    })
+];
 
-export default innganger;
+export default createSampleData(types);
