14831e9ae023a564092870f0dc7c23b456735103
Erlend Vige <erlend.vige@gmail.com>
2017-08-22 12:45:38 +0200

    Fix some styling issues with inngang-style modules

--- guideline-app/app/data/components/sample-data/_innganger.js
+++ guideline-app/app/data/components/sample-data/_innganger.js
@@ -12,24 +12,24 @@ import {
 } from './../sampleDataHelper';
 
 const types = [
-    newType(AppInngang, 'Appinngang', null, { tittel: 'Gå til en applikasjon' }),
-    newType(DobbelIngressInngang, 'Dobbel-ingress inngang', null, {
+    newType(AppInngang, 'Inngang 1', null, { tittel: 'Gå til en applikasjon' }),
+    newType(DobbelIngressInngang, 'Inngang 2', null, {
         tittel: 'Her er en tittel',
         ingress1: 'Her er ingress 1',
         ingress2: 'Her er ingress 2',
         ikon: 'info-sirkel'
     }),
-    newType(EnkelIngressInngang, 'Enkel-ingress inngang', null, {
+    newType(EnkelIngressInngang, 'Inngang 3', null, {
         tittel: 'Her er en tittel',
         ingress: 'Her er en ingress',
         ikon: 'info-sirkel'
     }),
-    newType(NormalVertikalInngang, 'Normal vertikal inngang', null, {
+    newType(NormalVertikalInngang, 'Inngang 4', null, {
         tittel: 'Her er en tittel',
         tekst: 'Og her har du en tekst',
         lesMerHref: '/#/components/innganger/lenkeeksempel/'
     }),
-    newType(StorVertikalInngang, 'Stor vertikal inngang', null, {
+    newType(StorVertikalInngang, 'Inngang 5', null, {
         tittel: 'Her er en tittel',
         ingress: 'Og her har du en ingress',
         ikon: 'info-sirkel'
