35d59c2061c9a8739def823958351398a767df24
Erlend Vige <erlend.vige@gmail.com>
2017-08-22 14:09:41 +0200

    Fixed tabbing not working for the different types of Inngang components

--- guideline-app/app/data/components/sample-data/_innganger.js
+++ guideline-app/app/data/components/sample-data/_innganger.js
@@ -11,37 +11,48 @@ import {
     newType
 } from './../sampleDataHelper';
 
+const href = { href: '/#/components/innganger' };
+
 const types = [
-    newType(AppInngang, 'Inngang 1', null, { tittel: 'Gå til en applikasjon' }),
+    newType(AppInngang, 'Inngang 1', null, {
+        tittel: 'Gå til en applikasjon',
+        ... href
+    }),
     newType(DobbelIngressInngang, 'Inngang 2', null, {
         tittel: 'Her er en tittel',
         ingress1: 'Her er ingress 1',
         ingress2: 'Her er ingress 2',
-        ikon: 'info-sirkel'
+        ikon: 'info-sirkel',
+        ... href
     }),
     newType(EnkelIngressInngang, 'Inngang 3', null, {
         tittel: 'Her er en tittel',
         ingress: 'Her er en ingress',
-        ikon: 'info-sirkel'
+        ikon: 'info-sirkel',
+        ... href
     }),
     newType(NormalVertikalInngang, 'Inngang 4', null, {
         tittel: 'Her er en tittel',
         tekst: 'Og her har du en tekst',
-        lesMerHref: '/#/components/innganger/lenkeeksempel/'
+        lesMerHref: '/#/components/innganger/lenkeeksempel/',
+        ... href
     }),
     newType(NormalVertikalInngang, 'Inngang 5', null, {
         tittel: 'Her er en tittel',
-        tekst: 'Og her har du en tekst'
+        tekst: 'Og her har du en tekst',
+        ... href
     }),
     newType(NormalVertikalInngang, 'Inngang 6', null, {
         tittel: 'Her er en tittel',
         tekst: 'Og her har du en tekst',
-        ikon: 'info-sirkel'
+        ikon: 'info-sirkel',
+        ... href
     }),
     newType(StorVertikalInngang, 'Inngang 7', null, {
         tittel: 'Her er en tittel',
         ingress: 'Og her har du en ingress',
-        ikon: 'info-sirkel'
+        ikon: 'info-sirkel',
+        ... href
     })
 ];
 
