7ac7e42722cdd79c77cf5da94b434b63c65bfd5e
Erlend Vige <erlend.vige@gmail.com>
2017-11-09 16:26:31 +0100

    Implemented new front pages for Component and Styling main pages

--- guideline-app/app/ui/containers/style/StyleMainPage.js
+++ guideline-app/app/ui/containers/style/StyleMainPage.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PT from 'prop-types';
-
+import { Sidetittel, Ingress } from './../../../../../packages/node_modules/nav-frontend-typografi';
+import stylingImg from './../../../assets/images/styling/styling.png';
 import SubRoutesWrapper from '../../../utils/routing/subroutesWrapper.component';
+import './styles.less';
 
-function StyleMainPage(props) {
-    return (
-        <div>
-            <SubRoutesWrapper routes={props.routes} />
-        </div>
-    );
+export default class StyleMainPage extends Component {
+
+    static renderStyleMainContent() {
+        return (
+            <div className="styleMainPage">
+                <div className="styleMainPage__content">
+                    <img src={stylingImg} alt="" />
+                    <div className="styleMainPage__content__text">
+                        <Sidetittel>Styling</Sidetittel>
+                        <Ingress>
+                            Under styling kan man finne hvordan man bruke farger, ikoner, typografi og tekst til å skape
+                            tydelig kommunikasjon og bidra en konsisten NAV identitet som oppstår enhetlig, pålitelig og
+                            profesjonell.
+                        </Ingress>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    renderStyleSubrouteContent() {
+        return (
+            <div className="styleMainPage">
+                <SubRoutesWrapper routes={this.props.routes} />
+            </div>
+        );
+    }
+
+
+    render() {
+        if (window.location.hash !== '#/styling') {
+            return this.renderStyleSubrouteContent();
+        }
+        return StyleMainPage.renderStyleMainContent();
+    }
 }
 
 StyleMainPage.propTypes = {
     routes: PT.arrayOf(PT.shape).isRequired
 };
-
-export default StyleMainPage;
