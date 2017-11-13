1c921a376f759c96254efb8afa904ba40511f76b
Erlend Vige <erlend.vige@gmail.com>
2017-11-13 16:54:14 +0100

    Remove storybook, add description to development/app/components/App.js

--- development/app/components/App.js
+++ development/app/components/App.js
@@ -1,5 +1,23 @@
 import React, { Component } from 'react';
 import { AlertStripeSuksess } from './../../../packages/node_modules/nav-frontend-alertstriper';
+import { Hovedknapp, Fareknapp } from './../../../packages/node_modules/nav-frontend-knapper';
+import Chevron from './../../../packages/node_modules/nav-frontend-chevron';
+
+/*
+ * Her er komponenten som benyttes til utvikling av eksisterende og nye moduler til nav-frontend.
+ * Appen blir kjørt opp fra npm start-scriptet i package.json på rot, og tar utgangspunkt i
+ * webpack-configen som ligger under /development/conf/webpack.config.js.
+ *
+ * Når du endrer på filene under /src i den aktuelle nav-frontend modulen du sitter og utvikler på, vil dette bli
+ * kompilert og reloades av seg selv i browseren, gitt at komponenten blir brukt her inne (hvis ikke
+ * vil Webpack la være å gjøre dette pga. sin tree-shaking).
+ *
+ * Ingenting i denne fila eller i /components-katalogen vil bli versjonskontrollert,
+ * så endringer du gjør her vil ikke bli med i dine commits.
+ *
+ * Enjoy!
+ */
+
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class App extends Component {
@@ -7,6 +25,9 @@ export default class App extends Component {
         return (
             <div>
                 <AlertStripeSuksess>Heisann Hoppsann!</AlertStripeSuksess>
+                <Hovedknapp>Hovedknapp</Hovedknapp>
+                <Fareknapp>Fareknapp</Fareknapp>
+                <Chevron />
             </div>
         );
     }
