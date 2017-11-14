f8f188dbffcf5121b22a0a45f36908cb7a48154b
Erlend Vige <erlend.vige@gmail.com>
2017-11-14 09:37:25 +0100

    Update development environment description

--- development/app/components/App.js
+++ development/app/components/App.js
@@ -12,8 +12,9 @@ import Chevron from './../../../packages/node_modules/nav-frontend-chevron';
  * kompilert og reloades av seg selv i browseren, gitt at komponenten blir brukt her inne (hvis ikke
  * vil Webpack la være å gjøre dette pga. sin tree-shaking).
  *
- * Ingenting i denne fila eller i /components-katalogen vil bli versjonskontrollert,
- * så endringer du gjør her vil ikke bli med i dine commits.
+ * Det er i utgangspunktet ikke ønskelig å sjekke inn endringer som gjøres her til repository, ettersom det er tenkt
+ * som et rent utviklingsmiljø og ikke trenger å versjonskontrolleres. Om det er nødvendig å endre på ting her,
+ * forklar hvorfor i en PR, eller ta det opp i frontend-forum.
  *
  * Enjoy!
  */
