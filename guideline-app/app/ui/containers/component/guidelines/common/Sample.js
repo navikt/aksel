0e6bac3de0e6dd21f969b594c807cfb65ef24e8a
Erlend Vige <erlend.vige@gmail.com>
2018-01-05 13:35:41 +0100

    Remove unused logic for single-choice activeModifiers

--- guideline-app/app/ui/containers/component/guidelines/common/Sample.js
+++ guideline-app/app/ui/containers/component/guidelines/common/Sample.js
@@ -17,40 +17,6 @@ class Sample extends Component {
         }
     }
 
-    componentDidUpdate(previousProps) {
-        const componentData = this.props.componentData;
-        const activeType = this.props.activeType;
-
-        if (componentData.modifiers && componentData.modifiers.length > 0 && this.props.activeModifier) {
-            const typeMatch = this.getTypeMatchingCurrentActiveModifier();
-            if (typeMatch && typeMatch.component !== activeType.component) {
-                this.changeActiveType({
-                    type: typeMatch,
-                    resetModifiers: previousProps.componentName !== this.props.componentName
-                });
-            }
-        }
-    }
-
-    getTypeMatchingCurrentActiveModifier() {
-        const types = this.props.componentData.types;
-        const activeModifier = this.props.activeModifier;
-        const activeComponent = this.props.activeType.component;
-        let match = null;
-
-        types.forEach((type) => {
-            if (type.modifiers && this.componentExistsIncomponentDataSubTree(activeComponent, type)) {
-                type.modifiers.forEach((modifier) => {
-                    if (modifier.value === activeModifier.value) {
-                        match = modifier;
-                    }
-                });
-            }
-        });
-
-        return match;
-    }
-
     getDefaultType() {
         // eslint-disable-next-line no-underscore-dangle,react/prop-types
         return this.props.componentData.types.filter((el) => el._default)[0];
@@ -127,27 +93,23 @@ class Sample extends Component {
 // eslint-disable-next-line no-class-assign
 Sample = connect((state) => ({
     activeType: state.sample.activeType,
-    activeModifier: state.sample.activeModifier,
     activeMultipleChoiceModifiers: state.sample.activeMultipleChoiceModifiers,
     activeRef: state.sample.activeRef
 }))(Sample);
 
 Sample.propTypes = {
     activeType: PT.shape({ component: PT.element, children: PT.element }),
-    activeModifier: PT.string,
     activeMultipleChoiceModifiers: PT.arrayOf(PT.string),
     activeRef: PT.node,
     componentData: PT.shape({
         types: PT.arrayOf(PT.shape({})),
         modifiers: PT.arrayOf(PT.shape({})),
         multipleChoiceModifiers: PT.arrayOf(PT.shape({}))
-    }).isRequired,
-    componentName: PT.string
+    }).isRequired
 };
 
 Sample.defaultProps = {
     activeType: { component: null },
-    activeModifier: null,
     activeMultipleChoiceModifiers: [],
     activeRef: null,
     componentName: undefined
