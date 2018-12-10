8ef65aff74005408ec873560fa7747243fa4f63a
Lillebo <eirik@archivedworks.com>
2018-12-10 13:00:38 +0100

    Templates message

--- guideline-app/app/ui/containers/templates/TemplatesMainPage.js
+++ guideline-app/app/ui/containers/templates/TemplatesMainPage.js
@@ -10,13 +10,12 @@ const TemplatesMainPage = () => (
             <Panel border>
                 <h2>Maler kommer</h2>
                 <p>
-                    Vi jobber med å lage flere større eksempler på side-typer og maler som skal vise hvordan
-                    man kan bruke og kombinere komponenter i en større sammenheng. I dette arbeidet vil vi 
-                    bl.a. hente eksempler fra allerede eksisterende/pågående prosjekter i NAV.
+                    Vi jobber med å lage flere større eksempler som skal vise hvordan man kan bruke og kombinere 
+                    komponenter i en større sammenheng. Samtidig pågår det et arbeid med å lage maler for mer 
+                    kjente side-typer.
                 </p>
                 <p>
-                    <strong>Merk at disse malene kun er ment som forslag og eksempler på bruk, og skal ikke være
-                    strenge regler for hvordan alle sidene på NAV må se ut.</strong>
+                    Mer informasjon om dette kommer.
                 </p>
             </Panel>
         </section>
