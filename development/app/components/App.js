6d24badad068f4a4019060fa8d0b5632c995f6f0
Lillebo <eirik@archivedworks.com>
2019-04-30 09:54:09 +0200

    dev app

--- development/app/components/App.js
+++ development/app/components/App.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
-import { AlertStripeSuksess } from 'NavFrontendModules/nav-frontend-alertstriper';
+import { 
+    AlertStripeSuksess,
+    AlertStripeInfo,
+    AlertStripeAdvarsel,
+    AlertStripeFeil,
+    AlertStripeSuksessInline,
+    AlertStripeInfoInline,
+    AlertStripeAdvarselInline,
+    AlertStripeFeilInline
+} from 'NavFrontendModules/nav-frontend-alertstriper';
 import { Hovedknapp, Fareknapp } from 'NavFrontendModules/nav-frontend-knapper';
 import Chevron from 'NavFrontendModules/nav-frontend-chevron';
 
@@ -23,10 +32,29 @@ export default class App extends Component {
     render() {
         return (
             <div>
+                <AlertStripeInfo>
+                     Her er komponenten som benyttes til utvikling av eksisterende og nye moduler til nav-frontend. Appen blir kjørt opp fra npm start-scriptet i package.json på rot, og tar utgangspunkt i webpack-configen som ligger under /development/conf/webpack.config.js.
+                </AlertStripeInfo>
+                <br/>
                 <AlertStripeSuksess>Heisann Hoppsann!</AlertStripeSuksess>
-                <Hovedknapp>Hovedknapp</Hovedknapp>
-                <Fareknapp>Fareknapp</Fareknapp>
-                <Chevron />
+                <br/>
+                <AlertStripeAdvarsel>Heisann Hoppsann!</AlertStripeAdvarsel>
+                <br/>
+                <AlertStripeFeil>Heisann Hoppsann!</AlertStripeFeil>
+                
+
+                <br/><br/><br/>
+
+
+                <AlertStripeInfoInline>
+                    Her er komponenten som benyttes til utvikling av eksisterende og nye moduler til nav-frontend. Appen blir kjørt opp fra npm start-scriptet i package.json på rot, og tar utgangspunkt i webpack-configen som ligger under /development/conf/webpack.config.js.
+                </AlertStripeInfoInline>
+                <br/><br/>
+                <AlertStripeSuksessInline>Heisann Hoppsann!</AlertStripeSuksessInline>
+                <br/><br/>
+                <AlertStripeAdvarselInline>Heisann Hoppsann!</AlertStripeAdvarselInline>
+                <br/><br/>
+                <AlertStripeFeilInline>Heisann Hoppsann!</AlertStripeFeilInline>
             </div>
         );
     }
