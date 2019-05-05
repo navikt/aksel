8f9482222038730f30fb93d2b71ae07eab367bc8
Lillebo <eirik@archivedworks.com>
2019-05-05 14:38:47 +0200

    dev app

--- development/app/components/App.js
+++ development/app/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Knapp from 'NavFrontendModules/nav-frontend-knapper';
 import Popover from 'NavFrontendModules/nav-frontend-popover';
 import Lenke from 'NavFrontendModules/nav-frontend-lenker';
 
@@ -27,10 +28,10 @@ export default class App extends React.Component {
                 <p style={{maxWidth: 700, lineHeight: '1.5rem'}}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sollicitudin molestie tristique. Vestibulum 
                     at mi lobortis, pretium felis eu, tincidunt velit. Aliquam ac dui id nisl suscipit euismod non nec felis. 
-                    Donec suscipit mi ut metus interdum <Lenke onMouseLeave={() => this.setState({anker1: undefined})} onMouseEnter={(e) => this.togglePopover('anker1', e.currentTarget)}>tincidunt eget eu orci</Lenke>. Aliquam vitae risus mollis, ullamcorper arcu 
-                    vitae, semper magna. Praesent tincidunt maximus nulla, ut vehicula velit consequat a. Quisque pulvinar 
-                    sem nibh, in aliquam enim gravida a.
+                    Donec suscipit mi ut metus interdum <Lenke onMouseLeave={() => this.setState({anker1: undefined})} onMouseEnter={(e) => this.togglePopover('anker1', e.currentTarget)}>tincidunt</Lenke> eget eu orci. Aliquam vitae risus mollis, ullamcorper arcu 
+                    vitae, semper magna. Praesent tincidunt maximus nulla, ut vehicula velit consequat a. Quisque <Lenke onClick={(e) => this.togglePopover('anker1', e.currentTarget)}>pulvinar</Lenke> sem nibh, in aliquam enim gravida a.
                 </p>
+                <Knapp onClick={(e) => this.togglePopover('anker1', e.currentTarget)}>Yo</Knapp>
                 <Popover
                     ankerEl={this.state.anker1}
                     apen={this.state.anker1 !== undefined}
