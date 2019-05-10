31223ed9c26197dc0ccc41a8cb93a5f3c0f92a32
Lillebo <eirik@archivedworks.com>
2019-05-10 13:00:58 +0200

    Reset dev app

--- development/app/components/App.js
+++ development/app/components/App.js
@@ -1,56 +1,32 @@
-import React from 'react';
-
-import Knapp from 'NavFrontendModules/nav-frontend-knapper';
-import Popover from 'NavFrontendModules/nav-frontend-popover';
-import Lenke from 'NavFrontendModules/nav-frontend-lenker';
-import Hjelpetekst from 'NavFrontendModules/nav-frontend-hjelpetekst';
-
-import { Systemtittel, Undertittel } from 'NavFrontendModules/nav-frontend-typografi';
+import React, { Component } from 'react';
+import { AlertStripeSuksess } from 'NavFrontendModules/nav-frontend-alertstriper';
+import { Hovedknapp, Fareknapp } from 'NavFrontendModules/nav-frontend-knapper';
+import Chevron from 'NavFrontendModules/nav-frontend-chevron';
 
 import './styles.less';
 
-export default class App extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            anker1: undefined
-        };
-    }
+/*
+ * Her er komponenten som benyttes til utvikling av eksisterende og nye moduler til nav-frontend.
+ * Appen blir kjørt opp fra npm start-scriptet i package.json på rot, og tar utgangspunkt i
+ * webpack-configen som ligger under /development/conf/webpack.config.js.
+ *
+ * Det er i utgangspunktet ikke ønskelig å sjekke inn endringer som gjøres her til repository, ettersom det er tenkt
+ * som et rent utviklingsmiljø og ikke trenger å versjonskontrolleres. Om det er nødvendig å endre på ting her,
+ * forklar hvorfor i en PR.
+ *
+ * Enjoy!
+ */
 
-    togglePopover = (popover, anker) => {
-        const newState = Object.assign({}, this.state);
-        newState[popover] = (newState[popover]) ? undefined : anker;
-        this.setState(newState);
-    }
 
-    render(){
+// eslint-disable-next-line react/prefer-stateless-function
+export default class App extends Component {
+    render() {
         return (
-            <div className="centered-example">
-                <Knapp>HM</Knapp>
-                <p style={{maxWidth: 700, lineHeight: '1.5rem'}}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sollicitudin molestie tristique. Vestibulum 
-                    at mi lobortis, pretium felis eu, tincidunt velit. Aliquam ac dui id nisl suscipit euismod non nec felis. 
-                    Donec suscipit mi ut metus interdum <Lenke onMouseLeave={() => this.setState({anker1: undefined})} onMouseEnter={(e) => this.togglePopover('anker1', e.currentTarget)}>tincidunt</Lenke> eget eu orci. Aliquam vitae risus mollis, ullamcorper arcu 
-                    vitae, semper magna. Praesent tincidunt maximus nulla, ut vehicula velit consequat a. Quisque <Lenke onClick={(e) => this.togglePopover('anker1', e.currentTarget)}>pulvinar</Lenke> sem nibh, in aliquam enim gravida a.
-                </p>
-                <Hjelpetekst>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </Hjelpetekst>
-                <br/>
-                <br/>
-                <br/>
-                <div align="center">
-                    <Knapp onClick={(e) => this.togglePopover('anker1', e.currentTarget)}>Yo</Knapp>
-                </div>
-                <Popover
-                    ankerEl={this.state.anker1}
-                    onClose={() => this.setState({anker1: undefined})}
-                    orientering="over-høyre"
-                >
-                    <p align="center">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    </p>
-                </Popover>
+            <div>
+                <AlertStripeSuksess>Heisann Hoppsann!</AlertStripeSuksess>
+                <Hovedknapp>Hovedknapp</Hovedknapp>
+                <Fareknapp>Fareknapp</Fareknapp>
+                <Chevron />
             </div>
         );
     }
