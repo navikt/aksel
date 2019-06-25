9ea66cc2f6a8b32a3bb02f2b8290613aef9be605
Eirik Lillebo <eirik@archivedworks.com>
2019-06-25 15:54:20 +0200

    Temp bilde path for forhåndsvisning

--- guideline-app/app/ui/app.js
+++ guideline-app/app/ui/app.js
@@ -5,9 +5,11 @@ import { Provider } from 'react-redux';
 import store from './../redux';
 import App from './containers/app/App';
 
+const basename = window.location.href.indexOf('localhost') !== -1 ? undefined : "/dev/nav/nye-docs";
+
 render(
     <Provider store={store}>
-        <Router>
+        <Router basename={basename}>
             <App />
         </Router>
     </Provider>,
