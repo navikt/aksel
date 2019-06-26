8827defbbe4f26586d88e9b2990c4e5db4ccdb4b
Eirik Lillebo <eirik@archivedworks.com>
2019-06-26 15:13:20 +0200

    Cleanup

--- guideline-app/app/ui/app.js
+++ guideline-app/app/ui/app.js
@@ -5,11 +5,9 @@ import { Provider } from 'react-redux';
 import store from './../redux';
 import App from './containers/app/App';
 
-const basename = window.location.href.indexOf('localhost') !== -1 ? undefined : '/dev/nav/nye-docs';
-
 render(
     <Provider store={store}>
-        <Router basename={basename}>
+        <Router>
             <App />
         </Router>
     </Provider>,
