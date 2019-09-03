efd0fde360c71a54703b2f60e180c4ed006a9906
Eirik Lillebo <eirik@archivedworks.com>
2019-09-03 13:40:17 +0200

    Lagt til sider om avstand, typografi

---
+++ guideline-app/app/ui/containers/resources/layout/LayoutPage.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { Innholdstittel, Ingress } from 'NavFrontendModules/nav-frontend-typografi';
+
+import Grid from './tabs/Grid.mdx';
+import Spacing from './tabs/Spacing.mdx';
+
+import MdxContent from './../../../components/mdx-content/MdxContent';
+import TabbedContainer from './../../tabbed-container/TabbedContainer';
+
+// import './styles.less';
+
+class LayoutPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.tabs = [
+            {
+                label: 'Grid',
+                content: () => (<MdxContent>{Grid}</MdxContent>)
+            },
+            {
+                id: 'spacing',
+                label: 'Avstand',
+                content: () => (<MdxContent>{Spacing}</MdxContent>)
+            }
+        ];
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Innholdstittel>Layout</Innholdstittel>
+                <TabbedContainer tabs={this.tabs} {...this.props} />
+            </React.Fragment>
+        );
+    }
+}
+
+export default LayoutPage;
