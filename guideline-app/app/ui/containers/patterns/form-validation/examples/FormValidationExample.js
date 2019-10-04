f65b2bac64280cd8abd81d8f0030df89f7e621c8
Eirik Lillebo <eirik@archivedworks.com>
2019-10-05 00:47:48 +0200

    Init patterns

---
+++ guideline-app/app/ui/containers/patterns/form-validation/examples/FormValidationExample.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class FormValidationExample extends React.Component {
+    render(){
+        return (
+            <div>
+                Hello
+            </div>
+        );
+    }
+}
+
+export default FormValidationExample;
--- guideline-app/app/utils/routing/routes.config.js
+++ guideline-app/app/utils/routing/routes.config.js
@@ -18,6 +18,7 @@ import AccessibilityAltTextPage from '../../ui/containers/accessibility/alt-text
 import AccessibilityToolsPage from '../../ui/containers/accessibility/tools/AccessibilityToolsPage';
 import NotFoundPage from '../../ui/containers/404/NotFoundPage';
 import CommunityMainPage from '../../ui/containers/community/CommunityMainPage';
+import FormValidationPage from '../../ui/containers/patterns/form-validation/FormValidationPage'
 
 const componentRoutes = resolveComponentRoutes('components');
 
@@ -33,6 +34,18 @@ const routeConfig = [
         component: ComponentsMainPage,
         title: 'Komponenter',
         routes: componentRoutes
+    },
+    {
+        path: '/patterns',
+        component: GenericSectionStart,
+        title: 'Mønster',
+        routes: [
+            {
+                path: '/patterns/form-validation',
+                component: FormValidationPage,
+                title: 'Skjemavalidering'
+            }
+        ]
     },
     {
         path: '/resources',
@@ -107,11 +120,6 @@ const routeConfig = [
                 title: 'Verktøy for UU-testing'
             }
         ]
-    },
-    {
-        path: '/community',
-        component: CommunityMainPage,
-        title: 'Diskusjon'
     },
     {
         component: NotFoundPage,
