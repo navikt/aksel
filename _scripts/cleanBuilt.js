6949efcbaa697495e6794de595c18a031765f2ea
Nicklas Utgaard <nicklas.utgaard@nav.no>
2020-02-05 15:42:58 +0100

    sletter unna enda flere autogenererte filer
    Sletter nÃ¥*.d.ts og lib/ siden disse har en tendens
    til Ã¥ henge igjen ved endringer og skape trÃ¸bbel

--- _scripts/cleanBuilt.js
+++ _scripts/cleanBuilt.js
@@ -19,9 +19,32 @@ function deleteFolder(folder) {
     }
 }
 
-glob('./packages/node_modules/**/lib', { dot: true }, (err, folders) => {
-    folders.forEach((folder) => {
-        console.log(`Deleting ${folder}`);
-        deleteFolder(folder);
+function getGlobFiles(globPattern, options) {
+    return new Promise((resolve, reject) => {
+        glob(globPattern, options, (err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        });
     });
-});
+}
+
+Promise.all([
+    getGlobFiles('./packages/node_modules/**/lib', { dot: true }),
+    getGlobFiles('./packages/node_modules/**/src/*.d.ts', { dit: true })
+])
+    .then(([ folders, files ]) => {
+        files.forEach((file) => {
+            console.log(`Deleting ${file}`);
+            fs.unlinkSync(file);
+        });
+
+        folders.forEach((folder) => {
+            console.log(`Deleting ${folder}`);
+            deleteFolder(folder);
+        });
+
+        console.log(`Deleted ${folders.length} folders, and ${files.length} .d.ts-files`);
+    });
