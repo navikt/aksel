db332fbef9f5e144a927e16d402a4d547572346a
KenAJoh <ken.aleksander.johansen@nav.no>
2020-07-09 15:46:12 +0200

    Oppdatert Webpack bootstrap
    - Fjernet parcel og browserify bootstrap

--- examples/webpack/src/index.js
+++ examples/webpack/src/index.js
@@ -1,27 +1,5 @@
-// Skrevet i ES5 så man slipper babel-avhengighet for eksempelprosjektet
-const React = require('react');
-const ReactDOM = require('react-dom');
-const Typografi = require('nav-frontend-typografi');
-const Etiketter = require('nav-frontend-etiketter');
-const Paneler = require('nav-frontend-paneler');
-require('./index.less');
-const HakeIkon = require('file-loader!nav-frontend-ikoner-assets/assets/stegindikator__hake.svg');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-const createElement = React.createElement;
-const Sidetittel = Typografi.Sidetittel;
-const Panel = Paneler.Panel;
-const AdvarselEtikett = Etiketter.EtikettAdvarsel;
-
-function Application() {
-    return createElement('div', {},
-        createElement(Panel, {},
-            createElement(Sidetittel, {}, 'Test')
-        ),
-        createElement(Panel, {},
-            createElement(AdvarselEtikett, {}, 'Oops')
-        ),
-        createElement('img', { src: HakeIkon, alt: ''})
-    );
-}
-
-ReactDOM.render(createElement(Application), document.getElementById('application'));
+ReactDOM.render(<App />,document.getElementById('app'));
