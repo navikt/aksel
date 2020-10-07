58aedbbca39d505575acc63cf79ed649e1649a0f
KenAJoh <ken.aleksander.johansen@nav.no>
2020-10-07 16:18:56 +0200

    Routing with Graphql

--- portal/src/components/header/header.tsx
+++ portal/src/components/header/header.tsx
@@ -6,18 +6,23 @@ import { Sidetittel } from "nav-frontend-typografi";
 import "./header.less";
 import { NAVLogo } from "../assets/svg";
 
-const Header = ({ siteTitle, className = "" }) => (
-  <header className={cl("header", className)}>
-    <a href="#hovedinnhold" id="skip-link">
-      Jump to content
-    </a>
-    <div className="header__content">
-      <Link className="header__logo" to="/">
-        <NAVLogo />
-        <Sidetittel className="header__title">{siteTitle}</Sidetittel>
-      </Link>
-    </div>
-  </header>
-);
+import { routingPaths } from "../util/routing";
+
+const Header = ({ siteTitle, className = "" }) => {
+  console.log(JSON.stringify(routingPaths().menuPaths, null, 4));
+  return (
+    <header className={cl("header", className)}>
+      <a href="#hovedinnhold" id="skip-link">
+        Jump to content
+      </a>
+      <div className="header__content">
+        <Link className="header__logo" to="/">
+          <NAVLogo />
+          <Sidetittel className="header__title">{siteTitle}</Sidetittel>
+        </Link>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
