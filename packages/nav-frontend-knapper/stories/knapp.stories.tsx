150bf5b429dfbf00949146375018a88050f18fc2
Andreas Nordahl <andnorda@gmail.com>
2020-10-09 10:46:01 +0200

    moved button stories to separate dir

--- packages/nav-frontend-knapper/stories/knapp.stories.tsx
+++ packages/nav-frontend-knapper/stories/knapp.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Fareknapp, Flatknapp, Hovedknapp, Knapp } from "./index";
+import { Fareknapp, Flatknapp, Hovedknapp, Knapp } from "../src/index";
 
 const Cog = () => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
