0f17cf6e4c63cabc1e6dd0bc7505827bc07d6845
KenAJoh <ken.aleksander.johansen@nav.no>
2020-10-12 15:05:09 +0200

    Fixed correct default tab when changing/loading page

--- portal/src/components/layout/TabbedContainer.js
+++ portal/src/components/layout/TabbedContainer.js
@@ -2,16 +2,17 @@ import React from "react";
 import Tabs from "nav-frontend-tabs";
 import { navigate } from "gatsby";
 
-const TabbedContainer = ({ tabs }) => (
+const TabbedContainer = ({ tabs, defaultActive }) => (
   <React.Fragment>
     <div className="tabsContainer tabsContainer--fullWidth">
       <div className="tabsContainer__inner">
         <Tabs
-          defaultAktiv={0}
+          defaultAktiv={defaultActive === undefined ? 0 : defaultActive}
           onChange={(e, i) => navigate(tabs[i].path, { replace: true })}
-          tabs={tabs.map((tab) => ({
+          tabs={tabs.map((tab, i) => ({
             label: tab.label,
             key: tab.label,
+            aktiv: defaultActive === i,
           }))}
         />
       </div>
