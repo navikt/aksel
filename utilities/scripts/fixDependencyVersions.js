54fde438ede5a6b01da2a0ea8efcb55d36b54f94
KenAJoh <ken.aleksander.johansen@nav.no>
2020-10-17 19:15:08 +0200

    Updated peerDep to match devDep for each package

--- utilities/scripts/fixDependencyVersions.js
+++ utilities/scripts/fixDependencyVersions.js
@@ -2,7 +2,7 @@ const glob = require("glob");
 const fs = require("fs");
 const extend = require("extend");
 const utils = require("./utils");
-const globalPackage = require("./../package.json");
+const globalPackage = require("../../package.json");
 
 const globalDeps = extend(
   {},
@@ -12,10 +12,11 @@ const globalDeps = extend(
 
 function getModules() {
   return new Promise((resolve, reject) => {
-    glob("../../packages/**/package.json", {}, (err, files) => {
+    glob("./packages/**/package.json", {}, (err, files) => {
       if (err) {
         reject(err);
       } else {
+        console.log(files);
         resolve(files);
       }
     });
