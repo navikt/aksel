b71690619786d95fbad13b486cedd887609d35b5
KenAJoh <ken.aleksander.johansen@nav.no>
2020-10-21 10:34:05 +0200

    ColorPalette now uses less variables for color lookup

--- website/src/components/colors/ColorPalette.tsx
+++ website/src/components/colors/ColorPalette.tsx
@@ -2,41 +2,45 @@ import React from "react";
 import Color from "color";
 import palette from "../assets/data/palette";
 import ColorSample from "./ColorSample";
+import vars from "nav-frontend-core";
 import "./styles.less";
 
 interface PaletteProps {
   onClick: ({ name: string, color: Color }) => void;
 }
 
-const white = Color("#FFFFFF");
-const black = Color("#3E3832");
-
-const variations = (baseColor, modifyer) => {
-  const colors = [];
-  for (let i = 80; i > 0; i -= 20) {
-    colors.push(baseColor.mix(modifyer, i / 100));
-  }
-  return colors;
-};
-
-const colorName = (base, i) => {
+const colorName = (base: string, i: number) => {
   if (i === 4) return base;
   return i < 4 ? `${base}Darken${80 - i * 20}` : `${base}Lighten${i * 20 - 80}`;
+};
+
+const name = (name: string, i: number) => {
+  switch (true) {
+    case i > 0:
+      return `${name}Darken${i}`;
+    case i < 0:
+      return `${name}Lighten${-1 * i}`;
+    case i === 0:
+      return `${name}`;
+    default:
+      return "";
+  }
+};
+const colorTest = (base: string) => {
+  const colors = [];
+  for (let i = 80; i >= -80; i -= 20) {
+    colors.push(Color(vars[name(base, i).replace("@", "")]));
+  }
+  return colors;
 };
 
 const ColorPalette = ({ onClick }: PaletteProps) => {
   return (
     <div className="color__palette">
       {Object.keys(palette).map((colorVar) => {
-        const color = Color(palette[colorVar]);
-        const lightVersions = variations(color, white);
-        lightVersions.push(color);
-        const darkVersions = variations(color, black);
-        const group = lightVersions.concat(darkVersions.reverse()).reverse();
-
         return (
           <div className="color__palette--group" key={colorVar}>
-            {group.map((col, ii) => {
+            {colorTest(colorVar).map((col, ii) => {
               const name = colorName(colorVar, ii);
               return (
                 <ColorSample
