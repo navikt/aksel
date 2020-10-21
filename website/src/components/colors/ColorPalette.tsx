2cdfeb43300212af25c280b45beb0520048c3597
Andreas Nordahl <andnorda@gmail.com>
2020-10-21 15:41:46 +0200

    refactor color palette

--- website/src/components/colors/ColorPalette.tsx
+++ website/src/components/colors/ColorPalette.tsx
@@ -1,63 +1,54 @@
 import React from "react";
 import Color from "color";
-import palette from "../assets/data/palette";
 import ColorSample from "./ColorSample";
 import vars from "nav-frontend-core";
-import "./styles.less";
+import "./color-palette.less";
 
 interface PaletteProps {
   onClick: ({ name: string, color: Color }) => void;
 }
 
-const colorName = (base: string, i: number) => {
-  if (i === 4) return base;
-  return i < 4 ? `${base}Darken${80 - i * 20}` : `${base}Lighten${i * 20 - 80}`;
-};
-
-const name = (name: string, i: number) => {
-  switch (true) {
-    case i > 0:
-      return `${name}Darken${i}`;
-    case i < 0:
-      return `${name}Lighten${-1 * i}`;
-    case i === 0:
-      return `${name}`;
-    default:
-      return "";
-  }
-};
-const colorSearch = (base: string) => {
-  const colors: { color: Color; name: string }[] = [];
-  for (let i = 80; i >= -80; i -= 20) {
-    colors.push({
-      color: Color(vars[name(base, i).replace("@", "")]),
-      name: name(base, i),
-    });
-  }
-  return colors;
-};
-
-const ColorPalette = ({ onClick }: PaletteProps) => {
-  return (
-    <div className="color__palette">
-      {Object.keys(palette).map((colorVar) => {
-        return (
-          <div className="color__palette--group" key={colorVar}>
-            {colorSearch(colorVar).map((col, ii) => {
-              return (
-                <ColorSample
-                  key={col.name}
-                  name={col.name}
-                  color={col.color.hex()}
-                  onClick={() => onClick({ name: col.name, color: col.color })}
-                />
-              );
-            })}
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+const ColorPalette = ({ onClick }: PaletteProps) => (
+  <div className="color__palette">
+    {[
+      "redError",
+      "navOransje",
+      "orangeFocus",
+      "navLimeGronn",
+      "navGronn",
+      "navLilla",
+      "navDypBla",
+      "navBla",
+      "navLysBla",
+    ].map((baseColor) => (
+      <div key={baseColor}>
+        {[
+          "Darken80",
+          "Darken60",
+          "Darken40",
+          "Darken20",
+          "",
+          "Lighten20",
+          "Lighten40",
+          "Lighten60",
+          "Lighten80",
+        ]
+          .map((suffix) => `${baseColor}${suffix}`)
+          .map((color) => ({
+            name: `@${color}`,
+            color: Color(vars[color]),
+          }))
+          .map(({ name, color }) => (
+            <ColorSample
+              key={name}
+              name={name}
+              color={color.hex()}
+              onClick={() => onClick({ name, color })}
+            />
+          ))}
+      </div>
+    ))}
+  </div>
+);
 
 export default ColorPalette;
