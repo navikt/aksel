88275dfb97942186af6173b6be9a6e99b399595c
Ken <26967723+KenAJoh@users.noreply.github.com>
2020-10-22 14:06:11 +0200

    Gatsby/navpage refactor (#834)
    * Fixed grid styling, some mdx issues
    
    * fixed linting

--- website/src/components/nav-page/NavPage.tsx
+++ website/src/components/nav-page/NavPage.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Link } from "gatsby";
+import { guid } from "nav-frontend-js-utils";
 import { LenkepanelBase } from "nav-frontend-lenkepanel";
 import { Undertittel } from "nav-frontend-typografi";
 import { usePageMenu } from "../../useSiteStructure";
+import "./styles.less";
 
 const NavPage = ({ location }) => {
   const menu = usePageMenu(location);
 
   return (
     <>
-      <nav className="catalog" aria-labelledby="left-navigation-title">
+      <nav className="navPage" aria-labelledby="left-navigation-title">
         {menu.map(({ link, title }, index) => (
           <LenkepanelBase
-            key={index}
+            key={guid()}
             linkCreator={(props) => (
               <Link className="lenkepanel lenkepanel--border" to={link}>
                 {props.children}
