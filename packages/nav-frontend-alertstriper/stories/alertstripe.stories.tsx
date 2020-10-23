6d31bf99f0d41caaa7c09bb2a757bf679e9084f5
KenAJoh <ken.aleksander.johansen@nav.no>
2020-10-23 14:25:54 +0200

    Another attempte at chromatic fix

--- packages/nav-frontend-alertstriper/stories/alertstripe.stories.tsx
+++ packages/nav-frontend-alertstriper/stories/alertstripe.stories.tsx
@@ -8,42 +8,44 @@ import Alertstripe, {
 import { Meta } from "@storybook/react/types-6-0";
 
 export default {
-  title: "Alertstripe",
+  title: "Alertstripe/Types",
   component: Alertstripe,
 } as Meta;
 
 export const Info = () => (
-  <>
+  <div className="chromatic-ignore">
     <AlertStripeInfo>{`<AlertStripeInfo>`}</AlertStripeInfo>
     <br />
     <Alertstripe type="info">{`<Alertstripe type="info">`}</Alertstripe>
-  </>
+  </div>
 );
 export const Suksess = () => (
-  <>
+  <div className="chromatic-ignore">
     <AlertStripeSuksess>{`<AlertStripeSuksess>`}</AlertStripeSuksess>
     <br />
     <Alertstripe type="suksess">{`<Alertstripe type="suksess">`}</Alertstripe>
-  </>
+  </div>
 );
 export const Advarsel = () => (
-  <>
+  <div className="chromatic-ignore">
     <AlertStripeAdvarsel>{`<AlertStripeAdvarsel>`}</AlertStripeAdvarsel>
     <br />
     <Alertstripe type="advarsel">
       {` <Alertstripe type="advarsel">`}
     </Alertstripe>
-  </>
+  </div>
 );
 export const Feil = () => (
-  <>
+  <div className="chromatic-ignore">
     <AlertStripeFeil>{`<AlertStripeFeil>`}</AlertStripeFeil>
     <br />
     <Alertstripe type="feil">{`<Alertstripe type="feil">`}</Alertstripe>
-  </>
+  </div>
 );
 export const Inline = () => (
-  <Alertstripe type="info" form="inline">
-    {`<Alertstripe type="info" form="inline">`}
-  </Alertstripe>
+  <div className="chromatic-ignore">
+    <Alertstripe type="info" form="inline">
+      {`<Alertstripe type="info" form="inline">`}
+    </Alertstripe>
+  </div>
 );
