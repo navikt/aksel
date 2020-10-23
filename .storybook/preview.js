01a417ed3eef978ea889413a71b3363d0234ab19
KenAJoh <ken.aleksander.johansen@nav.no>
2020-10-23 13:48:31 +0200

    Completed storybook setup for alertstriper

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,4 +2,14 @@ import "nav-frontend-core";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  options: {
+    storySort: (a, b) => {
+      console.log(b);
+      if (a[0].indexOf("all") !== -1) return -1;
+      if (b[0].indexOf("all") !== -1) return 1;
+      return a[0] === b[0]
+        ? 0
+        : a[0].localeCompare(b[0], undefined, { numeric: true });
+    },
+  },
 };
--- packages/nav-frontend-alertstriper/stories/snapshot.stories.tsx
+++ packages/nav-frontend-alertstriper/stories/snapshot.stories.tsx
@@ -11,42 +11,49 @@ export default {
   title: "Alertstripe",
   component: Alertstripe,
   parameters: {
-    chromatic: { disable: true, viewports: [320, 768, 1280] },
+    chromatic: { disable: false, viewports: [320, 768, 1280] },
   },
 } as Meta;
 
-export const Info = () => (
+export const All = () => (
   <>
     <AlertStripeInfo>{`<AlertStripeInfo>`}</AlertStripeInfo>
     <br />
     <Alertstripe type="info">{`<Alertstripe type="info">`}</Alertstripe>
-  </>
-);
-export const Suksess = () => (
-  <>
+    <br />
     <AlertStripeSuksess>{`<AlertStripeSuksess>`}</AlertStripeSuksess>
     <br />
     <Alertstripe type="suksess">{`<Alertstripe type="suksess">`}</Alertstripe>
-  </>
-);
-export const Advarsel = () => (
-  <>
+    <br />
     <AlertStripeAdvarsel>{`<AlertStripeAdvarsel>`}</AlertStripeAdvarsel>
     <br />
     <Alertstripe type="advarsel">
       {` <Alertstripe type="advarsel">`}
     </Alertstripe>
-  </>
-);
-export const Feil = () => (
-  <>
+    <br />
     <AlertStripeFeil>{`<AlertStripeFeil>`}</AlertStripeFeil>
     <br />
     <Alertstripe type="feil">{`<Alertstripe type="feil">`}</Alertstripe>
+    <br />
+    <Alertstripe type="info" form="inline">
+      {`<Alertstripe type="info" form="inline">`}
+    </Alertstripe>
+    <br />
+    <Alertstripe type="suksess" form="inline">
+      {`<Alertstripe type="suksess" form="inline">`}
+    </Alertstripe>
+    <br />
+    <Alertstripe type="advarsel" form="inline">
+      {`<Alertstripe type="advarsel" form="inline">`}
+    </Alertstripe>
+    <br />
+    <Alertstripe type="feil" form="inline">
+      {`<Alertstripe type="feil" form="inline">`}
+    </Alertstripe>
+    <br />
+    <Alertstripe
+      type="info"
+      size="3em"
+    >{`<Alertstripe type="info">`}</Alertstripe>
   </>
-);
-export const Inline = () => (
-  <Alertstripe type="info" form="inline">
-    {`<Alertstripe type="info" form="inline">`}
-  </Alertstripe>
 );
