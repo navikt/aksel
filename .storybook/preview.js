d5146374bac217237f5fa364f6648ff576c8560c
Ken <26967723+KenAJoh@users.noreply.github.com>
2020-10-23 15:18:52 +0200

    Update preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,7 +5,6 @@ export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   options: {
     storySort: (a, b) => {
-      console.log(b);
       if (a[0].indexOf("all") !== -1) return -1;
       if (b[0].indexOf("all") !== -1) return 1;
       return a[0] === b[0]
