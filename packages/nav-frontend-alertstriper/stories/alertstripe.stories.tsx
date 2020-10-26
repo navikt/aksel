f2350575c5a56e9fafdde1dccdf9601041eca9d5
KenAJoh <ken.aleksander.johansen@nav.no>
2020-10-26 11:20:56 +0100

    removedchroamtic-ignore classname

--- packages/nav-frontend-alertstriper/stories/alertstripe.stories.tsx
+++ packages/nav-frontend-alertstriper/stories/alertstripe.stories.tsx
@@ -16,7 +16,7 @@ export default {
 } as Meta;
 
 const Template = ({ children, type, ...args }) => (
-  <Alertstripe type={type} {...args} className="chromatic-ignore">
+  <Alertstripe type={type} {...args}>
     {children}
   </Alertstripe>
 );
@@ -28,39 +28,39 @@ Example.args = {
 };
 
 export const Info = () => (
-  <div className="chromatic-ignore">
+  <>
     <AlertStripeInfo>{`<AlertStripeInfo>`}</AlertStripeInfo>
     <br />
     <Alertstripe type="info">{`<Alertstripe type="info">`}</Alertstripe>
-  </div>
+  </>
 );
 export const Suksess = () => (
-  <div className="chromatic-ignore">
+  <>
     <AlertStripeSuksess>{`<AlertStripeSuksess>`}</AlertStripeSuksess>
     <br />
     <Alertstripe type="suksess">{`<Alertstripe type="suksess">`}</Alertstripe>
-  </div>
+  </>
 );
 export const Advarsel = () => (
-  <div className="chromatic-ignore">
+  <>
     <AlertStripeAdvarsel>{`<AlertStripeAdvarsel>`}</AlertStripeAdvarsel>
     <br />
     <Alertstripe type="advarsel">
       {` <Alertstripe type="advarsel">`}
     </Alertstripe>
-  </div>
+  </>
 );
 export const Feil = () => (
-  <div className="chromatic-ignore">
+  <>
     <AlertStripeFeil>{`<AlertStripeFeil>`}</AlertStripeFeil>
     <br />
     <Alertstripe type="feil">{`<Alertstripe type="feil">`}</Alertstripe>
-  </div>
+  </>
 );
 export const Inline = () => (
-  <div className="chromatic-ignore">
+  <>
     <Alertstripe type="info" form="inline">
       {`<Alertstripe type="info" form="inline">`}
     </Alertstripe>
-  </div>
+  </>
 );
