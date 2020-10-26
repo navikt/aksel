bb4200cd3234aadab11b42b695c3d42f135bcc5e
KenAJoh <ken.aleksander.johansen@nav.no>
2020-10-26 13:30:03 +0100

    added chevron stories

---
+++ packages/nav-frontend-chevron/stories/chromatic.stories.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import NavFrontendChevron, {
+  HoyreChevron,
+  VenstreChevron,
+  OppChevron,
+  NedChevron,
+} from "../src/chevron";
+import { Meta } from "@storybook/react/types-6-0";
+
+export default {
+  title: "Chevron/All",
+  component: NavFrontendChevron,
+  parameters: {
+    chromatic: { disable: true },
+  },
+} as Meta;
+
+export const All = () => (
+  <>
+    <HoyreChevron />
+    {"  "}
+    <VenstreChevron />
+    {"  "}
+    <OppChevron />
+    {"  "}
+    <NedChevron />
+
+    <br />
+    <br />
+
+    <HoyreChevron stor />
+    {"  "}
+    <VenstreChevron stor />
+    {"  "}
+    <OppChevron stor />
+    {"  "}
+    <NedChevron stor />
+  </>
+);
