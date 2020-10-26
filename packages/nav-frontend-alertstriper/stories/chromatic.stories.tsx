00e46d92af2cd764413297d28240144cf3c27b94
KenAJoh <ken.aleksander.johansen@nav.no>
2020-10-26 11:06:02 +0100

    Set up structure for chromatic to detect disabled stories

--- packages/nav-frontend-alertstriper/stories/chromatic.stories.tsx
+++ packages/nav-frontend-alertstriper/stories/chromatic.stories.tsx
@@ -8,59 +8,49 @@ import Alertstripe, {
 import { Meta } from "@storybook/react/types-6-0";
 
 export default {
-  title: "Alertstripe",
+  title: "Alertstripe/All",
   component: Alertstripe,
-  parameters: {
-    chromatic: { disable: true },
-  },
 } as Meta;
 
-const Template = ({ children, type, ...args }) => (
-  <Alertstripe type={type} {...args} className="chromatic-ignore">
-    {children}
-  </Alertstripe>
-);
-
-export const Example = Template.bind({});
-Example.args = {
-  type: "info",
-  children: "Excepteur esse id incididunt magna nostrud.",
-};
-
-export const Info = () => (
-  <div className="chromatic-ignore">
+export const All = () => (
+  <>
     <AlertStripeInfo>{`<AlertStripeInfo>`}</AlertStripeInfo>
     <br />
     <Alertstripe type="info">{`<Alertstripe type="info">`}</Alertstripe>
-  </div>
-);
-export const Suksess = () => (
-  <div className="chromatic-ignore">
+    <br />
     <AlertStripeSuksess>{`<AlertStripeSuksess>`}</AlertStripeSuksess>
     <br />
     <Alertstripe type="suksess">{`<Alertstripe type="suksess">`}</Alertstripe>
-  </div>
-);
-export const Advarsel = () => (
-  <div className="chromatic-ignore">
+    <br />
     <AlertStripeAdvarsel>{`<AlertStripeAdvarsel>`}</AlertStripeAdvarsel>
     <br />
     <Alertstripe type="advarsel">
       {` <Alertstripe type="advarsel">`}
     </Alertstripe>
-  </div>
-);
-export const Feil = () => (
-  <div className="chromatic-ignore">
+    <br />
     <AlertStripeFeil>{`<AlertStripeFeil>`}</AlertStripeFeil>
     <br />
     <Alertstripe type="feil">{`<Alertstripe type="feil">`}</Alertstripe>
-  </div>
-);
-export const Inline = () => (
-  <div className="chromatic-ignore">
+    <br />
     <Alertstripe type="info" form="inline">
       {`<Alertstripe type="info" form="inline">`}
     </Alertstripe>
-  </div>
+    <br />
+    <Alertstripe type="suksess" form="inline">
+      {`<Alertstripe type="suksess" form="inline">`}
+    </Alertstripe>
+    <br />
+    <Alertstripe type="advarsel" form="inline">
+      {`<Alertstripe type="advarsel" form="inline">`}
+    </Alertstripe>
+    <br />
+    <Alertstripe type="feil" form="inline">
+      {`<Alertstripe type="feil" form="inline">`}
+    </Alertstripe>
+    <br />
+    <Alertstripe
+      type="info"
+      size="3em"
+    >{`<Alertstripe type="info" size="3em">`}</Alertstripe>
+  </>
 );
