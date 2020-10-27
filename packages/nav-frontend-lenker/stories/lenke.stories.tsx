0e5684029bd2a33a3dbdbe423d46526abaf53bf9
KenAJoh <ken.aleksander.johansen@nav.no>
2020-10-27 10:14:00 +0100

    la til fokusert versjon av lenke for chromatic

--- packages/nav-frontend-lenker/stories/lenke.stories.tsx
+++ packages/nav-frontend-lenker/stories/lenke.stories.tsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import * as ReactDOM from "react-dom";
 import Lenke from "../src/lenke";
 import { Meta } from "@storybook/react/types-6-0";
 
 export default {
   title: "Lenke",
   component: Lenke,
+  parameters: {
+    chroamtic: { disabled: true },
+  },
 } as Meta;
 
-const Template = ({ children, ...args }) => (
-  <Lenke href="#" {...args}>
-    {children}
-  </Lenke>
-);
+const Template = ({ children, ...args }) => {
+  return (
+    <Lenke href="#" {...args}>
+      {children}
+    </Lenke>
+  );
+};
 
 export const Example = Template.bind({});
 Example.args = {
