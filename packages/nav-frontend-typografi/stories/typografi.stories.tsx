388288840890c9125c79e3067ba7cdf9a27d5a82
KenAJoh <ken.aleksander.johansen@nav.no>
2020-10-27 14:58:28 +0100

    typografi stories

---
+++ packages/nav-frontend-typografi/stories/typografi.stories.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  Sidetittel,
+  Innholdstittel,
+  Systemtittel,
+  Undertittel,
+  Ingress,
+  Undertekst,
+  Element,
+  Feilmelding,
+  Normaltekst,
+  EtikettLiten,
+  UndertekstBold,
+} from "../src/index";
+import { Meta } from "@storybook/react/types-6-0";
+
+export default {
+  title: "Typografi",
+  component: Sidetittel,
+} as Meta;
+
+export const All = () => {
+  return (
+    <div style={{ display: "grid", gridAutoRows: "4rem", rowGap: "0rem" }}>
+      <Sidetittel>Sidetittel</Sidetittel>
+      <Innholdstittel>Innholdstittel</Innholdstittel>
+      <Systemtittel>Systemtittel</Systemtittel>
+      <Undertittel>Undertittel</Undertittel>
+      <Ingress>Ingress</Ingress>
+      <Element>Element</Element>
+      <Feilmelding>Feilmelding</Feilmelding>
+      <Normaltekst>Normaltekst</Normaltekst>
+      <EtikettLiten>EtikettLiten</EtikettLiten>
+      <Undertekst>Undertekst</Undertekst>
+      <UndertekstBold>UndertekstBold</UndertekstBold>
+    </div>
+  );
+};
