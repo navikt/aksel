fa9ae113360913bbd9c9e09224a529f6949be163
KenAJoh <ken.aleksander.johansen@nav.no>
2020-10-27 10:48:21 +0100

    Paneler stories

---
+++ packages/nav-frontend-paneler/stories/chromatic.stories.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Panel from "../src/index";
+import { Meta } from "@storybook/react/types-6-0";
+
+export default {
+  title: "Panel",
+  component: Panel,
+} as Meta;
+
+export const All = () => {
+  return (
+    <>
+      <Panel border>
+        Deserunt ad ullamco qui cupidatat eu fugiat incididunt ex ea culpa sit
+        consequat. Amet incididunt commodo incididunt voluptate do esse do
+        fugiat duis. Sit voluptate reprehenderit voluptate amet excepteur
+        incididunt exercitation sit pariatur amet exercitation duis ullamco.
+        Tempor ipsum mollit cupidatat anim reprehenderit cupidatat velit. Do qui
+        adipisicing consequat cillum qui et labore exercitation qui non. Elit
+        voluptate et irure cupidatat ex velit mollit est cillum. Excepteur dolor
+        sint deserunt in nulla fugiat cillum irure consectetur.
+      </Panel>
+      <br />
+      <br />
+      <Panel>
+        Deserunt ad ullamco qui cupidatat eu fugiat incididunt ex ea culpa sit
+        consequat. Amet incididunt commodo incididunt voluptate do esse do
+        fugiat duis. Sit voluptate reprehenderit voluptate amet excepteur
+        incididunt exercitation sit pariatur amet exercitation duis ullamco.
+        Tempor ipsum mollit cupidatat anim reprehenderit cupidatat velit. Do qui
+        adipisicing consequat cillum qui et labore exercitation qui non. Elit
+        voluptate et irure cupidatat ex velit mollit est cillum. Excepteur dolor
+        sint deserunt in nulla fugiat cillum irure consectetur.
+      </Panel>
+    </>
+  );
+};
