0330414a0dde5fa3c0cba671cb5f54fdbf2587eb
Andreas Nordahl <andnorda@gmail.com>
2020-11-02 12:38:47 +0100

    endringer i tekster foreslått av Berit Norum

--- packages/nav-frontend-skjema/md/bekreftcheckbokspanel/Feilmelding.js
+++ packages/nav-frontend-skjema/md/bekreftcheckbokspanel/Feilmelding.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
+import { BekreftCheckboksPanel } from "../../lib";
 
-import { BekreftCheckboksPanel } from "..";
-
-const BekreftCheckboksPanelExample = ({ feil }) => {
+const BekreftCheckboksPanelExample = () => {
   const [isChecked, setIsChecked] = useState(false);
 
   return (
     <BekreftCheckboksPanel
-      label="Jeg bekrefter at opplysningene jeg har gitt, er riktige og fullstendige."
+      label="Jeg bekrefter at opplysningene jeg har gitt, er riktige og fullstendige"
       checked={isChecked}
       onChange={(e) => setIsChecked(e.target.checked)}
-      feil={feil && "Du må bekrefte dette før du kan sende inn søknaden."}
+      feil="Du må bekrefte dette før du kan sende inn søknaden."
     >
       Jeg vet at jeg kan miste retten til sykepenger hvis opplysningene jeg gir,
       ikke er riktige eller fullstendige.
