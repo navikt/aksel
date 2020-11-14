cd519c1b9ec9c49a03395c27b8b10050b4a3d9d6
Andreas Nordahl <andnorda@gmail.com>
2020-11-14 17:47:05 +0100

    refactor story

--- @nav-frontend/react-button/stories/button.stories.tsx
+++ @nav-frontend/react-button/stories/button.stories.tsx
@@ -6,55 +6,59 @@ export default {
   component: Button,
 };
 
+const Section = ({ children }) => (
+  <div
+    style={{
+      display: "grid",
+      gap: 16,
+      gridAutoFlow: "column",
+      justifyContent: "start",
+      padding: 24,
+      background: "lightgray",
+    }}
+  >
+    {children}
+  </div>
+);
+
+const variants: Array<"primary" | "secondary" | "action" | "danger"> = [
+  "primary",
+  "secondary",
+  "action",
+  "danger",
+];
+
 export const All = () => {
   return (
     <>
-      <div
-        style={{
-          padding: 24,
-          background: "lightgray",
-        }}
-      >
-        {["primary", "secondary", "action", "danger"].map(
-          (variant: "primary" | "secondary" | "action" | "danger") => (
-            <Button key={variant} variant={variant}>
-              {variant}
-            </Button>
-          )
-        )}
-      </div>
-      <div
-        style={{
-          padding: 24,
-          background: "lightgray",
-        }}
-      >
-        {["primary", "secondary", "action", "danger"].map(
-          (variant: "primary" | "secondary" | "action" | "danger") => (
-            <Button key={variant} variant={variant} disabled>
-              {variant}
-            </Button>
-          )
-        )}
-      </div>
-      <div
-        style={{
-          padding: 24,
-          background: "lightgray",
-        }}
-      >
-        {["primary", "secondary", "action", "danger"].map(
-          (variant: "primary" | "secondary" | "action" | "danger") => (
-            <a
-              key={variant}
-              className={`navds-button navds-button--${variant}`}
-              href="the-link"
-            >
-              {variant}
-            </a>
-          )
-        )}
-      </div>
+      <h1>Button</h1>
+      <Section>
+        {variants.map((variant) => (
+          <Button key={variant} variant={variant}>
+            {variant}
+          </Button>
+        ))}
+      </Section>
+      <h2>disabled</h2>
+      <Section>
+        {variants.map((variant) => (
+          <Button key={variant} variant={variant} disabled>
+            {variant}
+          </Button>
+        ))}
+      </Section>
+      <h2>As link</h2>
+      <Section>
+        {variants.map((variant) => (
+          <a
+            key={variant}
+            className={`navds-button navds-button--${variant}`}
+            href="the-link"
+          >
+            {variant}
+          </a>
+        ))}
+      </Section>
     </>
   );
 };
