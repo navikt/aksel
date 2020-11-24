09eefe5afe7d71f1abcccc0f07f1881e0da82264
KenAJoh <ken.aleksander.johansen@nav.no>
2020-11-24 13:14:47 +0100

    start accordion css

--- @nav-frontend/react-accordion/src/Accordion.tsx
+++ @nav-frontend/react-accordion/src/Accordion.tsx
@@ -1,50 +1,98 @@
-import React, { forwardRef } from "react";
-import cl from "classnames";
+import * as React from "react";
+import cls from "classnames";
 import {
-  ErrorFilled,
-  WarningFilled,
-  InformationFilled,
-  SuccessFilled,
-} from "@nav-frontend/icons";
-import "@nav-frontend/alert-styles";
+  UnmountClosed,
+  Collapse,
+  CollapseProps,
+  CollapseCallbackArgs,
+} from "react-collapse";
+import "@nav-frontend/accordion-styles";
+import { guid, keyCodes } from "nav-frontend-js-utils";
+import { forwardRef, useEffect, useRef, useState } from "react";
 
-interface AlertProps {
-  variant: "error" | "warning" | "info" | "success";
+interface AccordionProps {
+  open?: boolean;
+  onClick?: (event: React.SyntheticEvent<HTMLButtonElement>) => void;
   children: React.ReactNode;
   className?: string;
-  size?: "medium" | "small";
+  renderContentWhenClosed?: boolean;
+  collapseProps?: Partial<CollapseProps>;
+  buttonId?: string;
+  border?: boolean;
 }
 
-const Icon = ({ variant }) => {
-  switch (variant) {
-    case "error":
-      return <ErrorFilled />;
-    case "warning":
-      return <WarningFilled />;
-    case "info":
-      return <InformationFilled />;
-    case "success":
-      return <SuccessFilled />;
-    default:
-      return null;
-  }
-};
+const Accordion = forwardRef<HTMLDivElement, AccordionProps>(
+  (
+    {
+      children,
+      open = false,
+      collapseProps,
+      className,
+      renderContentWhenClosed = false,
+      onClick,
+      buttonId = guid(),
+      border = true,
+      ...rest
+    },
+    ref
+  ) => {
+    const contentId = useRef(guid());
+    const [internalOpen, setOpenInternalOpen] = useState<boolean>(open);
 
-const Alert = forwardRef<HTMLDivElement, AlertProps>(
-  ({ variant, children, className, size = "medium" }, ref) => (
-    <div
-      ref={ref}
-      className={cl(
-        className,
-        "navds-alert",
-        `navds-alert--${variant}`,
-        `navds-alert--${size}`
-      )}
-    >
-      <Icon variant={variant} />
-      <span>{children}</span>
-    </div>
-  )
+    useEffect(() => {
+      setOpenInternalOpen(open);
+    }, [open]);
+
+    const CollapseComponent = renderContentWhenClosed
+      ? Collapse
+      : UnmountClosed;
+
+    const handleClick = (e) => {
+      e.preventDefault();
+      if (onClick) {
+        onClick(e);
+      }
+    };
+    const onRest = (args: CollapseCallbackArgs) => {
+      if (collapseProps && collapseProps.onRest) {
+        collapseProps.onRest(args);
+      }
+    };
+
+    return (
+      <div
+        ref={ref}
+        className={cls("navds-accordion", className, {
+          "navds-accordion--border": border,
+          "navds-accordion--open": internalOpen,
+          "navds-accordion--closed": !internalOpen,
+        })}
+      >
+        <button
+          id={buttonId}
+          className="navds-accordion__button"
+          aria-expanded={open}
+          aria-controls={contentId.current}
+          onClick={(e) => handleClick(e)}
+          {...rest}
+        >
+          <div className="navds-accordion__flex-wrapper">
+            <span className="navds-accordion__title">Tittel</span>
+            <span className="navds-accordion__chevron" />
+          </div>
+        </button>
+        <div id={contentId.current} role="region" aria-labelledby={buttonId}>
+          <CollapseComponent
+            {...collapseProps}
+            isOpened={internalOpen}
+            onRest={(args: CollapseCallbackArgs) => onRest(args)}
+          >
+            <div className="navds-accordion__content">{children}</div>
+          </CollapseComponent>
+        </div>
+      </div>
+    );
+  }
 );
 
-export default Alert;
+export default Accordion;
--- @nav-frontend/react-accordion/stories/accordion.stories.tsx
+++ @nav-frontend/react-accordion/stories/accordion.stories.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "../src/index";
 
 export default {
@@ -6,30 +6,50 @@ export default {
   component: Accordion,
 };
 
-const variants: Array<"error" | "warning" | "info" | "success"> = [
-  "error",
-  "warning",
-  "info",
-  "success",
-];
-
 export const All = () => {
+  const [open, setOpen] = useState(false);
+
+  const onRest = (e) => {
+    console.log("storyonrest");
+  };
   return (
     <>
-      <h1>Alert</h1>
-      {variants.map((variant) => (
-        <Alert key={variant} variant={variant}>
-          {Array(8).fill(`${variant} content`).join(" ")}
-        </Alert>
-      ))}
-      <h2>Small</h2>
-      <div>
-        {variants.map((variant) => (
-          <Alert key={variant} variant={variant} size="small">
-            {Array(8).fill(`${variant} content`).join(" ")}
-          </Alert>
-        ))}
-      </div>
+      <h1>Accordion</h1>
+      <Accordion
+        collapseProps={{ onRest }}
+        open={open}
+        onClick={(e) => setOpen(!open)}
+      >
+        Eu magna fugiat officia sit ullamco reprehenderit reprehenderit officia.
+        Culpa fugiat irure deserunt irure enim adipisicing qui id. Pariatur
+        commodo dolore consequat nulla cupidatat commodo nisi sunt. Ad proident
+        occaecat cillum eu Lorem proident sunt anim officia voluptate. Veniam
+        ullamco magna elit amet mollit exercitation magna quis ad occaecat in
+        elit. Labore ad id pariatur consequat consectetur ullamco culpa dolor
+        dolor. Sunt irure eiusmod velit duis excepteur veniam consectetur eu
+        occaecat reprehenderit qui anim dolore. Ea voluptate dolore proident
+        esse aliqua amet magna pariatur incididunt ea excepteur. Aliquip magna
+        ut ipsum veniam eu laborum id commodo.
+      </Accordion>
+
+      <h1>Accordion no Border</h1>
+      <Accordion
+        collapseProps={{ onRest }}
+        open={open}
+        onClick={(e) => setOpen(!open)}
+        border={false}
+      >
+        Eu magna fugiat officia sit ullamco reprehenderit reprehenderit officia.
+        Culpa fugiat irure deserunt irure enim adipisicing qui id. Pariatur
+        commodo dolore consequat nulla cupidatat commodo nisi sunt. Ad proident
+        occaecat cillum eu Lorem proident sunt anim officia voluptate. Veniam
+        ullamco magna elit amet mollit exercitation magna quis ad occaecat in
+        elit. Labore ad id pariatur consequat consectetur ullamco culpa dolor
+        dolor. Sunt irure eiusmod velit duis excepteur veniam consectetur eu
+        occaecat reprehenderit qui anim dolore. Ea voluptate dolore proident
+        esse aliqua amet magna pariatur incididunt ea excepteur. Aliquip magna
+        ut ipsum veniam eu laborum id commodo.
+      </Accordion>
     </>
   );
 };
