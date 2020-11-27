b976bce15792cbdfe1cb2d5c710cd91854d1eff0
KenAJoh <ken.aleksander.johansen@nav.no>
2020-11-27 09:56:14 +0100

    oppdatert tabell eksempel

--- packages/nav-frontend-tabell-style/stories/tabell.stories.tsx
+++ packages/nav-frontend-tabell-style/stories/tabell.stories.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Meta } from "@storybook/react/types-6-0";
 
 import Lenke from "nav-frontend-lenker";
@@ -9,32 +9,37 @@ export default {
 } as Meta;
 
 export const All = () => {
+  const [sort, setSort] = useState("asc");
+  const handleClick = (e) => {
+    e.preventDefault();
+    sort === "asc" ? setSort("desc") : setSort("asc");
+  };
   return (
     <div style={{ display: "grid", gridAutoRows: "8rem", rowGap: "8rem" }}>
       <table className="tabell">
         <thead>
           <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Fornavn</th>
-            <th scope="col">Etternavn</th>
-            <th scope="col">Rolle</th>
+            <th>ID</th>
+            <th>Fornavn</th>
+            <th>Etternavn</th>
+            <th>Rolle</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <th scope="row">1</th>
+            <th>1</th>
             <td>Jean-Luc</td>
             <td>Picard</td>
             <td>Kaptein</td>
           </tr>
           <tr>
-            <th scope="row">2</th>
+            <th>2</th>
             <td>William</td>
             <td>Riker</td>
             <td>Kommandør</td>
           </tr>
           <tr>
-            <th scope="row">3</th>
+            <th>3</th>
             <td>Geordi</td>
             <td>La Forge</td>
             <td>Sjefsingeniør</td>
@@ -108,20 +113,36 @@ export const All = () => {
         <thead>
           <tr>
             <th role="columnheader" aria-sort="none">
-              <Lenke href="#">ID</Lenke>
+              <Lenke href="#" role="button" onClick={(e) => e.preventDefault()}>
+                ID
+                <span />
+              </Lenke>
             </th>
             <th
               role="columnheader"
               aria-sort="descending"
               className="tabell__th--sortert-desc"
             >
-              <Lenke href="#">Fornavn</Lenke>
+              <Lenke href="#" role="button" onClick={(e) => e.preventDefault()}>
+                Fornavn
+                <span />
+              </Lenke>
+            </th>
+            <th
+              role="columnheader"
+              aria-sort="none"
+              className={`tabell__th--sortert-${sort}`}
+            >
+              <Lenke href="#" role="button" onClick={(e) => handleClick(e)}>
+                Etternavn
+                <span />
+              </Lenke>
             </th>
             <th role="columnheader" aria-sort="none">
-              <Lenke href="#">Etternavn</Lenke>
-            </th>
-            <th role="columnheader" aria-sort="none">
-              <Lenke href="#">Rolle</Lenke>
+              <Lenke href="#" role="button" onClick={(e) => e.preventDefault()}>
+                Rolle
+                <span />
+              </Lenke>
             </th>
           </tr>
         </thead>
