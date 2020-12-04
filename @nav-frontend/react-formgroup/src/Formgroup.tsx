dbcc072d047641fb8b2a401e3179dd9e714134c5
KenAJoh <ken.aleksander.johansen@nav.no>
2020-12-04 13:50:37 +0100

    implementert skjemagruppe i vnext

--- @nav-frontend/react-formgroup/src/Formgroup.tsx
+++ @nav-frontend/react-formgroup/src/Formgroup.tsx
@@ -1,24 +1,47 @@
 import React, { forwardRef } from "react";
 import cl from "classnames";
+import { guid } from "nav-frontend-js-utils";
 import "@nav-frontend/forms-styles";
 
 export interface FormgroupProps {
+  children: React.ReactNode;
   /**
    * User defined classname
    */
   className?: string;
-  children: React.ReactNode;
   title?: string;
   description?: string;
+  error?: React.ReactNode;
+  errorId?: string;
 }
 
 const Formgroup = forwardRef<HTMLFieldSetElement, FormgroupProps>(
-  ({ children, className, title, description }, ref) => {
+  (
+    { children, className, title, description, error, errorId = guid() },
+    ref
+  ) => {
+    const descriptionId = description ? guid() : undefined;
+
     return (
-      <fieldset ref={ref} className={cl("navds-formgroup", className)}>
-        {title && <legend>{title}</legend>}
-        {description && <div>{description}</div>}
+      <fieldset
+        ref={ref}
+        className={cl("navds-forms__formgroup", className)}
+        aria-invalid={!!error}
+        aria-errormessage={error ? errorId : undefined}
+        aria-describedby={descriptionId}
+      >
+        {title && <legend className="navds-forms__legend">{title}</legend>}
+        {description && (
+          <div className="navds-forms__description" id={descriptionId}>
+            {description}
+          </div>
+        )}
         {children}
+        {error && (
+          <div aria-live="polite" className="navds-forms__errormsg">
+            {error}
+          </div>
+        )}
       </fieldset>
     );
   }
---
+++ @nav-frontend/react-formgroup/stories/formgroup.stories.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Formgroup from "../src/index";
+
+export default {
+  title: "@nav-frontend/react-formgroup",
+  component: Formgroup,
+};
+
+export const All = () => {
+  return (
+    <>
+      <h1>Default</h1>
+      <Formgroup>
+        <input type="radio" id="male" name="gender" value="male" />
+        <label htmlFor="male">Male</label>
+        <br />
+        <input type="radio" id="female" name="gender" value="female" />
+        <label htmlFor="female">Female</label>
+        <br />
+        <input type="radio" id="other" name="gender" value="other" />
+        <label htmlFor="other">Other</label>
+      </Formgroup>
+
+      <h1>Title</h1>
+      <Formgroup title="Dette er en tittel">
+        <input type="radio" id="male" name="gender" value="male" />
+        <label htmlFor="male">Male</label>
+        <br />
+        <input type="radio" id="female" name="gender" value="female" />
+        <label htmlFor="female">Female</label>
+        <br />
+        <input type="radio" id="other" name="gender" value="other" />
+        <label htmlFor="other">Other</label>
+      </Formgroup>
+
+      <h1>Title + description</h1>
+      <Formgroup
+        title="Dette er en tittel"
+        description="Dette er en description"
+      >
+        <input type="radio" id="male" name="gender" value="male" />
+        <label htmlFor="male">Male</label>
+        <br />
+        <input type="radio" id="female" name="gender" value="female" />
+        <label htmlFor="female">Female</label>
+        <br />
+        <input type="radio" id="other" name="gender" value="other" />
+        <label htmlFor="other">Other</label>
+      </Formgroup>
+    </>
+  );
+};
