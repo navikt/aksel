f39eee6116bc3948cd9f4e784f05986f164f29f1
KenAJoh <ken.aleksander.johansen@nav.no>
2020-12-06 20:51:28 +0100

    added props to modal

--- @nav-frontend/react-modal/src/Modal.tsx
+++ @nav-frontend/react-modal/src/Modal.tsx
@@ -1,12 +1,33 @@
 import React, { forwardRef } from "react";
 import cl from "classnames";
+import ReactModal, { Props } from "react-modal";
 import "@nav-frontend/modal-styles";
 
 export interface ModalProps {
   /**
-   * User defined classname
+   * Modal open/visible state
+   */
+  open: boolean;
+  /**
+   * Content of modal
+   */
+  children: React.ReactNode;
+  /**
+   * Adds a button in the top right corner
+   */
+  closeButton?: boolean;
+  /**
+   * If modal should close on overlay click
+   */
+  shouldCloseOnOverlayClick?: boolean;
+  /**
+   * User defined classname for wrapper element
    */
   className?: string;
+  /**
+   * User defined classname for modal content
+   */
+  contentClassName?: string;
 }
 
 const Modal = forwardRef<HTMLDivElement, ModalProps>(
