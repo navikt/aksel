1a05522397795b4830e7683c4fe1bcbed7b38bf3
KenAJoh <ken.aleksander.johansen@nav.no>
2020-12-09 14:53:10 +0100

    fikset ikon stilen

--- @nav-frontend/react-link/stories/link.stories.tsx
+++ @nav-frontend/react-link/stories/link.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "../src/index";
-
+import { Add, Add1 } from "@nav-frontend/icons";
 export default {
   title: "@nav-frontend/react-link",
   component: Link,
@@ -11,6 +11,16 @@ export const All = () => {
     <>
       <h1>Link</h1>
       <Link href="#">Dette er en tekstlenke</Link>
+      <h1>Icon after</h1>
+      <Link href="#">
+        <span>Dette er en tekstlenke</span>
+        <Add />
+      </Link>
+      <h1>Icon before</h1>
+      <Link href="#">
+        <Add1 />
+        <span>Dette er en tekstlenke </span>
+      </Link>
     </>
   );
 };
