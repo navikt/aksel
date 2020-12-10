26dc4c16cb175a85acb22494ba837b937ad8e0af
Ken <26967723+KenAJoh@users.noreply.github.com>
2020-12-10 17:21:38 +0100

    Vnext typo endringer og litt småfiks + qol (#905)
    * Flyttet nye komponenter i sorterte dirs
    
    * oppdatert accordion
    
    * justerte alert-styles
    
    * oppdatert button-styles
    
    * oppdatert variabler i accordion og modal
    
    * oppdatert tag-styles
    
    * oppdatert typo-styles
    
    * oppdatert typo-styles
    
    * Vnext react pakke (draft) (#907)
    
    * satt opp react pakke
    
    * react pakke cleanup
    
    * ferdigsatt react-pakke

--- @nav-frontend/react-typography/src/index.ts
+++
@@ -1,6 +0,0 @@
-import "@nav-frontend/typography-styles";
-
-export { default as Heading } from "./Heading";
-export { default as Text } from "./Text";
-export { default as Paragraph } from "./Paragraph";
-export { default as Lead } from "./Lead";
---
+++ @nav-frontend/react/react-button/src/index.ts
@@ -0,0 +1,3 @@
+import Button from "./Button";
+export default Button;
+export { ButtonProps } from "./Button";
