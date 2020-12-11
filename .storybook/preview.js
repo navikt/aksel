7e774a9be49bc429fc5ed359724806cdbadbc541
Ken <26967723+KenAJoh@users.noreply.github.com>
2020-12-11 12:10:07 +0100

    fikset pakkenavn (#911)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,5 @@
 import "nav-frontend-core";
-import "@nav-frontend/css/baseline/index.css";
+import "@navikt/ds-css/baseline/index.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
