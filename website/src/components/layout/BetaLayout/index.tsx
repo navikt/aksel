3ff3dc5699c4b88d3e35b943c4ec4691dbff6053
Andreas Nordahl <andnorda@gmail.com>
2020-12-11 15:25:08 +0100

    add simple typo doc of proposed structure

--- website/src/components/layout/BetaLayout/index.tsx
+++ website/src/components/layout/BetaLayout/index.tsx
@@ -3,6 +3,9 @@ import { MDXProvider } from "@mdx-js/react";
 import { Link } from "gatsby";
 import { NAVLogo } from "../../assets/images/svg";
 import { useBetaMenu } from "../../../useSiteStructure";
+import Example from "../../example/Example";
+import Codeblock, { InlineCode } from "../../code/Code";
+import { Heading, Paragraph } from "@navikt/ds-react";
 import "./layout.css";
 
 const SubMenu = (props) => (
@@ -65,7 +68,21 @@ const BetaLayout = (props) => (
   <div className="page-wrapper">
     <Sidebar />
     <div className="content">
-      <MDXProvider>{props.children}</MDXProvider>
+      <MDXProvider
+        components={{
+          code: (props) => <Codeblock {...props} />,
+          inlineCode: (props) => <InlineCode {...props} />,
+          Example,
+          h1: (props) => <Heading {...props} level={1} size="xxl" />,
+          h2: (props) => <Heading {...props} level={2} size="xl" />,
+          h3: (props) => <Heading {...props} level={3} size="large" />,
+          h4: (props) => <Heading {...props} level={4} size="medium" />,
+          h5: (props) => <Heading {...props} level={5} size="small" />,
+          p: (props) => <Paragraph {...props} />,
+        }}
+      >
+        {props.children}
+      </MDXProvider>
     </div>
   </div>
 );
