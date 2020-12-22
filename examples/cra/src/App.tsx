647e3bd240bebcef6639cffa6f266bc4271ca42e
KenAJoh <ken.aleksander.johansen@nav.no>
2020-12-22 13:16:27 +0100

    oppdatert CRA

--- examples/cra/src/App.tsx
+++ examples/cra/src/App.tsx
@@ -1,27 +1,14 @@
 import React from "react";
-
-import { Sidetittel, Normaltekst } from "nav-frontend-typografi";
-import AlertStripe from "nav-frontend-alertstriper";
-import Panel from "nav-frontend-paneler";
-import Lenke from "nav-frontend-lenker";
-
 import "./App.less";
-import logo from "./assets/logo.svg";
 
-const App = (): JSX.Element => (
-  <main className="app">
-    <img src={logo} alt="Nav logo" />
-    <Sidetittel>CRA bootstrap</Sidetittel>
-    <Panel className="panel" border>
-      <Normaltekst>
-        Create react app oppsett med støtte for .less, samt hot-reloading
-      </Normaltekst>
-      <AlertStripe className="alerts" type="info">
-        For å ta i bruk flere komponenter, gå til{" "}
-        <Lenke href="https://design.nav.no/components">Design.nav.no</Lenke>
-      </AlertStripe>
-    </Panel>
-  </main>
-);
+const App = (): JSX.Element => {
+  return (
+    <div className="app">
+      <header className="app__header"></header>
+      <main className="app__main"></main>
+      <footer className="app__footer"></footer>
+    </div>
+  );
+};
 
 export default App;
