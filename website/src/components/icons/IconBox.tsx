9de7b7782f755b95b1a219981b3ffbc9636625ca
KenAJoh <ken.aleksander.johansen@nav.no>
2021-01-04 16:05:06 +0100

    implementerte icon modal popup

--- website/src/components/icons/IconBox.tsx
+++ website/src/components/icons/IconBox.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import * as Icons from "@navikt/ds-icons";
 import "./styles.less";
+import "@navikt/ds-css/button/index.css";
 import { Undertekst } from "nav-frontend-typografi";
 
-const IconBox = ({ name, ...props }) => {
+const IconBox = ({ name, onClick, ...props }) => {
   const Icon = Icons[name];
   return (
-    <div className="iconbox">
-      <Icon />
-      <Undertekst>{name}</Undertekst>
+    <div
+      role="button"
+      tabIndex={0}
+      aria-label={`open ${name}-icon modal`}
+      className="iconbox"
+      onClick={() => onClick(name)}
+    >
+      <Icon className="iconbox__icon" />
+      <Undertekst className="iconbox__name">{name}</Undertekst>
     </div>
   );
 };
--- website/src/components/icons/IconPage.tsx
+++ website/src/components/icons/IconPage.tsx
@@ -1,19 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import * as cls from "classnames";
 import * as Icons from "@navikt/ds-icons";
 import { Input } from "nav-frontend-skjema";
+import Modal from "nav-frontend-modal";
 import "./styles.less";
 import IconBox from "./IconBox";
+import Code from "../code/Code";
+import { Systemtittel } from "nav-frontend-typografi";
 
 const IconPage = () => {
+  useEffect(() => {
+    Modal.setAppElement(".mainWrapper");
+  }, []);
+
   const [filter, setFilter] = useState("");
+  const [openModal, setOpenModal] = useState(false);
+  const [modalIcon, setModalIcon] = useState("");
 
   const filteredIcons = Object.keys(Icons).filter(
     (name) => name.toLowerCase().indexOf(filter.toLowerCase()) !== -1
   );
 
+  const handleModal = (name) => {
+    setOpenModal(true);
+    setModalIcon(name);
+  };
+  const handleModalClose = () => {
+    setOpenModal(false);
+    setModalIcon("");
+  };
+
+  const Icon = modalIcon && Icons[modalIcon];
   return (
     <div className="iconpage">
       <Input
+        className="iconpage__input"
         bredde="XL"
         label="Filter"
         description={filteredIcons.length + " ikoner matcher søket"}
@@ -21,9 +42,31 @@ const IconPage = () => {
       />
       <div className="iconpage__icons">
         {filteredIcons.map((name) => (
-          <IconBox key={name} name={name} />
+          <IconBox
+            key={name}
+            name={name}
+            onClick={(name) => handleModal(name)}
+          />
         ))}
       </div>
+      {modalIcon !== "" && (
+        <Modal
+          className="iconpage__modal"
+          isOpen={openModal}
+          closeButton={true}
+          onRequestClose={() => handleModalClose()}
+          contentLabel="Modal for icon-visning"
+        >
+          <div>
+            <Systemtittel>{modalIcon}</Systemtittel>
+            <Code className="language-jsx">{`import { ${modalIcon} } from '@navikt/ds-icons'`}</Code>
+            <div className="iconpage__modalIcons">
+              <Icon className="iconpage__modalIcons--light" />
+              <Icon className="iconpage__modalIcons--dark" />
+            </div>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
