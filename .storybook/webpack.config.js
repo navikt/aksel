c58729342e42a30f6e40ccc0d80f3755c21d8ba1
Ken <ken.aleksander@gmail.com>
2021-01-22 15:54:02 +0100

    fikset storybook webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,10 +4,7 @@ module.exports = (baseConfig) => {
   baseConfig.config.module.rules.push({
     test: /\.less$/,
     loaders: ["style-loader", "css-loader", "less-loader"],
-    include: [
-      path.resolve(__dirname, "../packages/"),
-      path.resolve(__dirname, "../@navikt/ds-react/internal-header"),
-    ],
+    include: path.resolve(__dirname, "../packages/"),
   });
 
   return baseConfig.config;
