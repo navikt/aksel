de16349e0311e7da70f6d17b5f3e935404c47507
Ken <ken.aleksander@gmail.com>
2021-01-25 10:03:10 +0100

    patching av småfeil

--- website/src/components/layout/layout.tsx
+++ website/src/components/layout/layout.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import BetaLayout from "./BetaLayout";
 import OldLayout from "./OldLayout";
 
-const Layout = (props) =>
-  props.path.startsWith("/beta/") ? (
+const Layout = (props) => {
+  return props.location.pathname.startsWith("/beta/") ? (
     <BetaLayout {...props} />
   ) : (
     <OldLayout {...props} />
   );
+};
 
 export default Layout;
