2d95b247b9ec3fd04949e3def1a6059a1e593480
Ken <26967723+KenAJoh@users.noreply.github.com>
2021-01-25 12:32:50 +0100

    Header component (basert på sykepenger) (#948)
    * laget header component
    
    * fjernet icons
    
    * Endringer header api
    
    * fjernet height title.css
    
    * Link component internalHeader
    
    * fjernet link component
    
    * merge master
    
    * fjernet ubrukt token
    
    * fikset storybook webpack config

---
+++ @navikt/ds-react/internal-header/src/InternalHeaderUser.tsx
@@ -0,0 +1,22 @@
+import React, { forwardRef, HTMLAttributes } from "react";
+import cl from "classnames";
+import "@navikt/ds-css/internal-header/index.css";
+import "@navikt/ds-css/typography/index.css";
+
+export interface InternalHeaderUserProps
+  extends HTMLAttributes<HTMLDivElement> {
+  className?: string;
+  name: string;
+  ident: string;
+}
+
+const InternalHeaderUser = forwardRef<HTMLDivElement, InternalHeaderUserProps>(
+  ({ className, name, ident, ...rest }, ref) => (
+    <div ref={ref} className={cl("navds-header__user", className)} {...rest}>
+      <span className="navds-header__name">{name}</span>
+      <span className="navds-header__ident">{ident}</span>
+    </div>
+  )
+);
+
+export default InternalHeaderUser;
