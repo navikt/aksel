6c0b8b6ee73c138894b3c7aceb01cb6caee1f3f5
Ken <ken.aleksander@gmail.com>
2021-02-01 16:37:18 +0100

    converterer svg til png format

---
+++ @navikt/ds-icons/figma-api/pngConverter.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const sharp = require("sharp");
+const rimraf = require("rimraf");
+
+const convertToPng = () => {
+  const inputDir = "./svg/";
+  const outDir = "./png/";
+  const sizes = [16, 24, 128, 256];
+
+  if (fs.existsSync(outDir)) {
+    rimraf.sync(outDir);
+  }
+  fs.mkdirSync(outDir);
+  sizes.forEach((size) => fs.mkdirSync(outDir + size.toString()));
+
+  fs.readdirSync(inputDir).forEach((file) => {
+    sizes.forEach((size) => {
+      const density = (72 * size) / 16;
+      sharp(inputDir + file, { density })
+        .resize(size)
+        .png()
+        .toFile(outDir + size.toString() + "/" + file.replace("svg", "png"))
+        .catch((e) => console.error(e));
+    });
+  });
+};
+
+try {
+  convertToPng();
+} catch (e) {
+  console.error(e);
+}
