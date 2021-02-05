bfe71f4edcdf5404422c45e6f69cb835930effab
Ken <ken.aleksander@gmail.com>
2021-02-05 15:08:01 +0100

    css accordionCode

---
+++ website/src/components/code/AccordionCode.tsx
@@ -0,0 +1,61 @@
+import { Expand } from "@navikt/ds-icons";
+import cl from "classnames";
+import Prism from "prismjs";
+import React, { useState } from "react";
+import { Collapse } from "react-collapse";
+import "@navikt/ds-css/accordion/index.css";
+import "./theme.css";
+import "./index.css";
+
+export const AccordionCode = ({ children, type, ...props }) => {
+  const [open, setOpen] = useState(false);
+
+  const highlighted =
+    type === "html"
+      ? Prism.highlight(children, Prism.languages.jsx, "html")
+      : type === "css"
+      ? Prism.highlight(children, Prism.languages.css, "css")
+      : Prism.highlight(children, Prism.languages.jsx, "jsx");
+
+  return (
+    <>
+      <button
+        aria-expanded={open}
+        onClick={() => setOpen(!open)}
+        className="code__dropdown"
+      >
+        <Expand
+          className={cl(
+            "navds-accordion__chevron",
+            `navds-accordion__chevron--${open ? "up" : "down"}`,
+            "code__dropdownChevron"
+          )}
+          focusable="false"
+        />
+        <span className="code__dropdownHeading">{type}</span>
+      </button>
+
+      <Collapse isOpened={open} theme={{ collapse: "code__dropdownCode" }}>
+        <div
+          className={cl("code__dropdownCode--wrapper", {
+            "code__dropdownCode--open": open,
+          })}
+        >
+          <figure
+            /* className="code-example" */
+            role="figure"
+            aria-label="Kode-eksempel"
+          >
+            <pre className="code__dropdownCode--pre">
+              <code
+                className="code__dropdownCode--code"
+                {...props}
+                dangerouslySetInnerHTML={{ __html: highlighted }}
+              ></code>
+            </pre>
+          </figure>
+        </div>
+      </Collapse>
+    </>
+  );
+};
--- website/src/components/code/Code.tsx
+++ website/src/components/code/Code.tsx
@@ -1,16 +1,12 @@
-import React, { useState } from "react";
-import cl from "classnames";
+import "@navikt/ds-css/accordion/index.css";
 import classnames from "classnames";
-import Prism from "prismjs";
 import copy from "copy-to-clipboard";
 import Popover, { PopoverOrientering } from "nav-frontend-popover";
 import { Normaltekst } from "nav-frontend-typografi";
+import Prism from "prismjs";
+import React, { useState } from "react";
 import { CopyIcon } from "../assets/images/svg";
-import { Expand } from "@navikt/ds-icons";
-import { Collapse } from "react-collapse";
-import "@navikt/ds-css/accordion/index.css";
-/* import { Accordion } from "@navikt/ds-react"; */
-
+import { AccordionCode } from "./AccordionCode";
 import "./styles.less";
 import "./theme.css";
 
@@ -98,59 +94,6 @@ export const Bash = ({ children, className, onClick, ...props }: CodeProps) => {
   );
 };
 
-const DropDownCode = ({ children, type, ...props }) => {
-  const [open, setOpen] = useState(false);
-
-  const highlighted =
-    type === "html"
-      ? Prism.highlight(children, Prism.languages.jsx, "html")
-      : type === "css"
-      ? Prism.highlight(children, Prism.languages.css, "css")
-      : Prism.highlight(children, Prism.languages.jsx, "jsx");
-
-  return (
-    <>
-      <button
-        aria-expanded={open}
-        onClick={() => setOpen(!open)}
-        className="code__dropdown"
-      >
-        <Expand
-          className={cl(
-            "navds-accordion__chevron",
-            `navds-accordion__chevron--${open ? "up" : "down"}`,
-            "code__dropdownChevron"
-          )}
-          focusable="false"
-        />
-        <span className="code__dropdownHeading">{type}</span>
-      </button>
-
-      <Collapse isOpened={open} theme={{ collapse: "code__dropdownCode" }}>
-        <div
-          className={cl("code__dropdownCode--wrapper", {
-            "code__dropdownCode--open": open,
-          })}
-        >
-          <figure
-            /* className="code-example" */
-            role="figure"
-            aria-label="Kode-eksempel"
-          >
-            <pre className="code__dropdownCode--pre">
-              <code
-                className="code__dropdownCode--code"
-                {...props}
-                dangerouslySetInnerHTML={{ __html: highlighted }}
-              ></code>
-            </pre>
-          </figure>
-        </div>
-      </Collapse>
-    </>
-  );
-};
-
 const Code = ({
   children,
   className,
@@ -186,7 +129,7 @@ const Code = ({
   };
 
   if (props.type && ["html", "react"].includes(props.type.toLowerCase())) {
-    return <DropDownCode type={props.type} children={children} />;
+    return <AccordionCode type={props.type} children={children} />;
   }
 
   return (
