4c43c1614ccc01f013b178fcbfec9f9a02cc519a
Morten Jansrud <morten.jansrud@nav.no>
2021-02-09 13:23:34 +0100

    Fiks linting på layout

--- @navikt/ds-react/layout/stories/grid.stories.tsx
+++ @navikt/ds-react/layout/stories/grid.stories.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Layout } from "../src/index";
+import Lenke from "nav-frontend-lenker";
 
 export default {
-  title: "@navikt/grid",
+  title: "@navikt/layout",
   component: { Layout },
 };
 
 export const All = () => {
-  const Navigation = () => <div>Navigation</div>;
+  const Navigation = () => (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <Lenke href={"#"}>Test</Lenke>
+      <Lenke href={"#"}>Test</Lenke>
+      <Lenke href={"#"}>Test</Lenke>
+      <Lenke href={"#"}>Test</Lenke>
+    </div>
+  );
   const Content = () => <div>Main content</div>;
   const Context = () => <div>Test</div>;
   return (
