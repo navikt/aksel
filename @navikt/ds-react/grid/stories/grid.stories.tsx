6d7f51da277bf2b87db7657b3ddb7ce91dac5884
Morten Jansrud <morten.jansrud@nav.no>
2021-02-10 10:23:50 +0100

    Legg på hvit bakgrunn og border radius

--- @navikt/ds-react/grid/stories/grid.stories.tsx
+++ @navikt/ds-react/grid/stories/grid.stories.tsx
@@ -9,13 +9,13 @@ export default {
 export const All = () => (
   <Container style={styles.container}>
     <Grid>
-      <Cell style={styles.cell} sm={4} md={8} lg={3}>
+      <Cell sm={4} md={8} lg={3}>
         Kolonne
       </Cell>
-      <Cell style={styles.cell} sm={4} md={4} lg={6}>
+      <Cell sm={4} md={4} lg={6}>
         Kolonne
       </Cell>
-      <Cell style={styles.cell} sm={4} md={4} lg={3}>
+      <Cell sm={4} md={4} lg={3}>
         Kolonne
       </Cell>
     </Grid>
@@ -23,6 +23,5 @@ export const All = () => (
 );
 
 const styles = {
-  container: { background: "rgba(0, 0, 0, 0.2)" },
-  cell: { background: "rgba(0, 0, 0, 0.2)" },
+  container: { background: "#f1f1f1" },
 };
