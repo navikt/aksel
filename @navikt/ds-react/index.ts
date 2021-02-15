4c36a5b59a8a9ba290f026b71240c85eb4006d1f
Morten Jansrud <morten.jansrud@nav.no>
2021-02-15 14:02:06 +0100

    Rydd opp i ds-react index

--- @navikt/ds-react/index.ts
+++ @navikt/ds-react/index.ts
@@ -5,6 +5,9 @@ export { default as Link, LinkProps } from "./link/src/index";
 export { default as Modal, ModalProps } from "./modal/src/index";
 export { default as Popover, PopoverProps } from "./popover/src/index";
 export { default as Tag, TagProps } from "./tag/src/index";
+export { Grid, Cell, GridProps, CellProps } from "./grid/src/index";
+export { Layout, LayoutProps, SectionProps } from "./layout/src/index";
+
 export {
   InternalHeader,
   InternalHeaderProps,
@@ -13,6 +16,7 @@ export {
   InternalHeaderUser,
   InternalHeaderUserProps,
 } from "./internal-header/src/index";
+
 export {
   Heading,
   Text,
@@ -22,6 +26,7 @@ export {
   TextProps,
 } from "./typography/src/index";
 
-export { Layout } from "./layout/src/index";
-export { Grid, Cell } from "./grid/src/index";
-export { default as ContentContainer } from "./content-container/src/index";
+export {
+  ContentContainer,
+  ContentContainerProps,
+} from "./content-container/src/index";
