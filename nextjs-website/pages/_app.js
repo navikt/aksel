b8b82e4c6daac9e143a23be0e7b7a90995c2d662
Ken <ken.aleksander@gmail.com>
2021-03-05 23:08:05 +0100

    flyttet useRoute

--- nextjs-website/pages/_app.js
+++ nextjs-website/pages/_app.js
@@ -1,7 +1,21 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
 import "@navikt/ds-css/baseline/index.css";
+import Sidebar from "../components/sidebar/Sidebar";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+const Website = ({ Component, pageProps }) => {
+  const route = useRouter();
+  console.log(route);
+  return (
+    <>
+      <Head>
+        <title>NAV Designsystem</title>
+        <link rel="icon" href="/favicon-32x32.png" />
+      </Head>
+      <Sidebar route={route} />
+      <Component {...pageProps} />
+    </>
+  );
+};
 
-export default MyApp;
+export default Website;
--- nextjs-website/pages/index.tsx
+++ nextjs-website/pages/index.tsx
@@ -1,13 +1,6 @@
-import Head from "next/head";
-
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>NAV Designsystem</title>
-        <link rel="icon" href="/favicon-32x32.png" />
-      </Head>
-
       <main>Website</main>
     </div>
   );
