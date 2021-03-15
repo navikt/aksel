687bab39ff5f6d2fb5ee864524801e103379a03c
Ken <ken.aleksander@gmail.com>
2021-03-15 13:00:10 +0100

    button comments + props

--- @navikt/ds-react/button/Button.tsx
+++ @navikt/ds-react/button/Button.tsx
@@ -1,8 +1,20 @@
+import PropTypes from "prop-types";
 import React, { forwardRef } from "react";
 import cl from "classnames";
 
 export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  /**
+   * @ignore
+   */
+  className?: string;
+  /**
+   * Decides component design and interactions
+   * @default "primary"
+   */
   variant?: "primary" | "secondary" | "action" | "danger";
+  /**
+   * Reduces internal padding for component
+   */
   size?: "medium" | "small";
 }
 
@@ -20,5 +32,26 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
     />
   )
 );
+
+Button.propTypes = {
+  /**
+   * @ignore
+   */
+  className: PropTypes.string,
+  /**
+   * Reduces internal padding for component
+   */
+  size: PropTypes.oneOf<"medium" | "small">(["medium", "small"]),
+  /**
+   * Decides component design and interactions
+   * @default "primary"
+   */
+  variant: PropTypes.oneOf<"primary" | "secondary" | "action" | "danger">([
+    "primary",
+    "secondary",
+    "action",
+    "danger",
+  ]),
+};
 
 export default Button;
