57b10907e95ee244663fdd6dff56050dab2e32b8
Ken <ken.aleksander@gmail.com>
2021-04-08 12:24:56 +0200

    fikset warning bugs TMP

--- docs/components/MDXComponents.tsx
+++ docs/components/MDXComponents.tsx
@@ -4,15 +4,15 @@ import Import from "./code/Import";
 import Preview from "./code-preview/Preview";
 import Npm from "./npm/Npm";
 import TableOfContents from "./table-of-contents/TableOfContents";
-import KnappBase, { Knapp } from "nav-frontend-knapper";
+import { Knapp } from "nav-frontend-knapper";
 import { Settings } from "@navikt/ds-icons";
-import renderToString from "next-mdx-remote/render-to-string";
 
 const components = {
   h1: (props) => <Heading size="xxl" level={1} {...props} />,
   h2: (props) => <Heading size="large" level={2} {...props} />,
   h3: (props) => <Heading size="small" level={3} {...props} />,
   p: (props) => <Paragraph size="medium" {...props} />,
+  a: (props) => <Link {...props} />,
   Bash,
   Preview,
   Import,
@@ -25,12 +25,3 @@ const components = {
 };
 
 export default components;
-
-/* export const mdxSource = (content) => {
-  return renderToString(content, {
-    components: { components },
-    mdxOptions: {
-      remarkPlugins: [require("remark-slug")],
-    },
-  });
-}; */
--- docs/pages/[[...path]].js
+++ docs/pages/[[...path]].js
@@ -4,39 +4,15 @@ import matter from "gray-matter";
 import hydrate from "next-mdx-remote/hydrate";
 import tableOfContents from "../src/table-of-contents";
 import mainMenu from "../src/main-menu";
-import { Heading, Paragraph, Alert, Link } from "@navikt/ds-react";
-import Bash from "../components/code/Bash";
-import Import from "../components/code/Import";
-import Preview from "../components/code-preview/Preview";
-import Npm from "../components/npm/Npm";
-import TableOfContents from "../components/table-of-contents/TableOfContents";
-import KnappBase, { Knapp } from "nav-frontend-knapper";
-import { Settings } from "@navikt/ds-icons";
-/* import components, { mdxSource } from "../components/MDXComponents"; */
+import components from "../components/MDXComponents";
 import renderToString from "next-mdx-remote/render-to-string";
-
-const components = {
-  h1: (props) => <Heading size="xxl" level={1} {...props} />,
-  h2: (props) => <Heading size="large" level={2} {...props} />,
-  h3: (props) => <Heading size="small" level={3} {...props} />,
-  p: (props) => <Paragraph size="medium" {...props} />,
-  Bash: Bash,
-  Preview: Preview,
-  Import: Import,
-  Npm,
-  TableOfContents,
-  Alert,
-  Link,
-  Knapp: Knapp,
-  Settings: Settings,
-};
 
 const Page = ({ mdxSource }) => hydrate(mdxSource, { components });
 
 export default Page;
 
 export async function getStaticProps({ params: { path } }) {
-  const { content, data } = matter(
+  const { content } = matter(
     fs.readFileSync(`data/${path?.join("/") || "index"}.mdx`, "utf8")
   );
 
