95fc9beedf305aff55924ef9d19a6b1a10619c64
Ken <ken.aleksander@gmail.com>
2021-04-27 14:11:35 +0200

    fjernet strangler

--- @navikt/ds-react/src/popover/index.ts
+++ @navikt/ds-react/src/popover/index.ts
@@ -1,2 +1,1 @@
 export { default as Popover } from "./Popover";
-export { default as PopoverBase } from "./PopoverBase";
--- @navikt/ds-react/src/popover/stories/popover.stories.tsx
+++ @navikt/ds-react/src/popover/stories/popover.stories.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Popover, PopoverBase } from "../index";
+import { Popover } from "../index";
 import { Meta } from "@storybook/react/types-6-0";
 import { placements } from "@popperjs/core";
 export default {
@@ -53,20 +53,6 @@ export const All = () => {
           <Template placement={placement} />
         </div>
       ))}
-    </div>
-  );
-};
-
-export const PopoverBaseExample = () => {
-  const [open, setOpen] = useState(true);
-  return (
-    <div>
-      <button onClick={() => setOpen(!open)}>Open</button>
-      <div style={{ width: 50, height: 50, background: "blue" }}></div>
-      <PopoverBase onClose={() => {}} open={open} position={{ y: 50, x: 50 }}>
-        <div style={{ margin: "1rem" }}>Contents</div>
-      </PopoverBase>
-      <div style={{ width: 300, height: 200, background: "lightblue" }}></div>
     </div>
   );
 };
