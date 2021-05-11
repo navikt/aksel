fef7c00adb17d0b2b49d4cf28e7bbaafd3355a4b
Ken <ken.aleksander@gmail.com>
2021-05-11 12:59:30 +0200

    oppdatert typo komponent

--- @navikt/ds-react/src/typography/index.ts
+++ @navikt/ds-react/src/typography/index.ts
@@ -12,7 +12,7 @@ export * from "./old/Lead";
  */
 export { default as Title } from "./Title";
 export { default as Ingress } from "./Ingress";
-export { default as Body } from "./Body";
-export { default as Component } from "./Component";
+export { default as BodyLong } from "./BodyLong";
+export { default as BodyShort } from "./BodyShort";
 export { default as Detail } from "./Detail";
 export { default as Label } from "./Label";
