983c2c1def81d9687eb3335e9223e45f22bfa31b
Andreas Nordahl <andnorda@gmail.com>
2021-05-18 09:19:39 +0200

    Bakgrunnsfarger på etiketter stemmer nå overens med Figma (#1094)

--- packages/nav-frontend-etiketter/stories/etiketter.stories.tsx
+++ packages/nav-frontend-etiketter/stories/etiketter.stories.tsx
@@ -13,41 +13,19 @@ export default {
 } as Meta;
 
 export const All = () => (
-  <div
-    style={{
-      display: "grid",
-      gridAutoRows: "auto",
-      rowGap: "2rem",
-      gridAutoColumns: "fit-content",
-      maxWidth: "50vw",
-    }}
-  >
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-evenly",
-      }}
-    >
-      <EtikettInfo> Info</EtikettInfo>
-
+  <div>
+    <h1>Etikett</h1>
+    <div style={{ display: "flex", gap: 8 }}>
+      <EtikettInfo>Info</EtikettInfo>
       <EtikettSuksess>Suksess</EtikettSuksess>
-
       <EtikettFokus>Fokus</EtikettFokus>
-
       <EtikettAdvarsel>Advarsel</EtikettAdvarsel>
     </div>
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-evenly",
-      }}
-    >
-      <EtikettInfo mini> Info</EtikettInfo>
-
+    <h2>mini</h2>
+    <div style={{ display: "flex", gap: 8 }}>
+      <EtikettInfo mini>Info</EtikettInfo>
       <EtikettSuksess mini>Suksess</EtikettSuksess>
-
       <EtikettFokus mini>Fokus</EtikettFokus>
-
       <EtikettAdvarsel mini>Advarsel</EtikettAdvarsel>
     </div>
   </div>
