f76110b1ac875ebcb800ae1e800e658d2ef32d3f
Ken <ken.aleksander@gmail.com>
2021-05-31 10:08:31 +0200

    organized index.ts imports

--- @navikt/ds-react/src/index.ts
+++ @navikt/ds-react/src/index.ts
@@ -15,8 +15,9 @@ export {
   ContentContainer,
   ContentContainerProps,
 } from "./content-container/index";
-export { Cell, CellProps, Grid, GridProps } from "./grid/index";
 export { CopyToClipboard } from "./copy-to-clipboard/index";
+export { Cell, CellProps, Grid, GridProps } from "./grid/index";
+export { Guide, GuideProps } from "./guide/index";
 export {
   InternalHeader,
   InternalHeaderProps,
@@ -32,10 +33,9 @@ export {
 } from "./layouts/index";
 export { Link, LinkProps } from "./link/index";
 export { Modal, ModalProps } from "./modal/index";
+export { Panel, PanelProps } from "./panel/index";
 export { Popover, PopoverProps } from "./popover/index";
 export { Tag, TagProps } from "./tag/index";
-export { Panel, PanelProps } from "./panel/index";
-export { Guide, GuideProps } from "./guide/index";
 export {
   Heading,
   HeadingProps,
