9a2fe89cfda9380f5328f9595c26e45176cdbc20
Ken <ken.aleksander@gmail.com>
2021-07-12 13:26:25 +0200

    scaffolding input

---
+++ @navikt/ds-react/src/template/stories/input.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Input } from "../index";
+import { Meta } from "@storybook/react/types-6-0";
+export default {
+  title: "ds-react/form/input",
+  component: Input,
+} as Meta;
+
+export const All = () => {
+  return <div></div>;
+};
