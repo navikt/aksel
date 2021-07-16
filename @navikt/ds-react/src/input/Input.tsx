77b16e06d45c59bd2cba996ae1e0bdd28eee4dc9
Ken <ken.aleksander@gmail.com>
2021-07-16 22:50:23 +0200

    implementerte formHook

--- @navikt/ds-react/src/input/Input.tsx
+++ @navikt/ds-react/src/input/Input.tsx
@@ -1,12 +1,6 @@
-import React, {
-  forwardRef,
-  InputHTMLAttributes,
-  useContext,
-  useRef,
-} from "react";
+import React, { forwardRef, InputHTMLAttributes } from "react";
 import cl from "classnames";
-import { v4 as uuidv4 } from "uuid";
-import { FieldsetContext } from "../index";
+import { useFormHandler } from "../util";
 
 export interface InputProps
   extends Omit<InputHTMLAttributes<HTMLInputElement>, "size"> {
@@ -28,31 +22,28 @@ export interface InputProps
 }
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  (
-    { className, size, id, label, description, error, errorId, ...rest },
-    ref
-  ) => {
-    const internalId = useRef(uuidv4());
-    const internalErrorId = useRef(uuidv4());
-
-    const context = useContext(FieldsetContext);
-
-    const errorMsg = context.error ?? error;
-    const errorUuid = context.errorId ?? errorId ?? internalErrorId.current;
-
-    const selectedSize = size ? size : context.size ?? "m";
+  ({ className, label, description, ...rest }, ref) => {
+    const {
+      isInvalid,
+      errorMsg,
+      errorId,
+      id,
+      renderErrorMsg,
+      size,
+      restProps,
+    } = useFormHandler(rest);
 
     return (
       <div
         className={cl("navds-form__element", {
-          "navds-input--error": !!errorMsg,
+          "navds-input--error": isInvalid,
         })}
       >
         {label && (
-          <label htmlFor={id ?? internalId.current}>
+          <label htmlFor={id}>
             <div
               className={cl("navds-form__label", "navds-label", {
-                "navds-label--s": selectedSize === "s",
+                "navds-label--s": size === "s",
               })}
             >
               {label}
@@ -60,7 +51,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
             {description && (
               <div
                 className={cl("navds-form__description", "navds-body-short", {
-                  "navds-body--s": selectedSize === "s",
+                  "navds-body--s": size === "s",
                 })}
               >
                 {description}
@@ -69,31 +60,29 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
           </label>
         )}
         <input
-          id={id ?? internalId.current}
+          id={id}
           ref={ref}
           type="text"
           className={cl(
             "navds-input",
             className,
-            `navds-input--${selectedSize}`,
+            `navds-input--${size}`,
             "navds-body-short",
-            { "navds-body--s": selectedSize === "s" }
+            { "navds-body--s": size === "s" }
           )}
-          aria-invalid={rest.disabled ? undefined : !!errorMsg}
-          aria-describedby={rest.disabled ? undefined : !!errorMsg && errorUuid}
-          {...rest}
+          aria-invalid={isInvalid}
+          aria-describedby={isInvalid ? errorId : undefined}
+          {...restProps}
         />
         <div
           className={cl("navds-label", "navds-form--error", {
-            "navds-label--s": selectedSize === "s",
+            "navds-label--s": size === "s",
           })}
-          id={errorUuid}
+          id={errorId}
           aria-relevant="additions removals"
           aria-live="polite"
         >
-          {!context.error && errorMsg && !rest.disabled && (
-            <div>{errorMsg}</div>
-          )}
+          {renderErrorMsg && <div>{errorMsg}</div>}
         </div>
       </div>
     );
--- @navikt/ds-react/src/util/Hooks.tsx
+++ @navikt/ds-react/src/util/Hooks.tsx
@@ -2,12 +2,23 @@ import { useContext, useEffect, useState } from "react";
 import { FieldsetContext } from "../index";
 import { v4 as uuidv4 } from "uuid";
 
-export function useFormHandler({ error, errorId: oldErrorId, id: oldId }) {
+export function useFormHandler(props) {
+  const {
+    error,
+    errorId: oldErrorId,
+    id: oldId,
+    disabled,
+    size: oldSize,
+    ...rest
+  } = props;
+
   const [intId, setIntId] = useState("");
   const [intErrorId, setIntErrorId] = useState("");
   const [errorMsg, setErrorMsg] = useState<string>();
   const [errorId, setErrorId] = useState<string>();
   const [id, setId] = useState("");
+  const [renderErrorMsg, setRenderErrorMsg] = useState(false);
+  const [size, setSize] = useState("m");
 
   const context = useContext(FieldsetContext);
 
@@ -28,5 +39,21 @@ export function useFormHandler({ error, errorId: oldErrorId, id: oldId }) {
     setId(oldId ?? intId);
   }, [oldId, intId]);
 
-  return { isInvalid: !!errorMsg, errorMsg, errorId, id };
+  useEffect(() => {
+    setRenderErrorMsg(!context.error && !!errorMsg && !disabled);
+  }, [context.error, disabled, errorMsg]);
+
+  useEffect(() => {
+    setSize(oldSize ? oldSize : context.size ?? "m");
+  }, [context.size, oldSize]);
+
+  return {
+    isInvalid: !!errorMsg && !disabled,
+    errorMsg,
+    errorId,
+    id,
+    renderErrorMsg,
+    size,
+    restProps: rest,
+  };
 }
