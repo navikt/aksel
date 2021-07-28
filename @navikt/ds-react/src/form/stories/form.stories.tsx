8507d946aacdb3caed2a9b274bab5615196af06d
Andreas Nordahl <andnorda@gmail.com>
2021-07-28 13:29:19 +0200

    input size problem story

--- @navikt/ds-react/src/form/stories/form.stories.tsx
+++ @navikt/ds-react/src/form/stories/form.stories.tsx
@@ -1,25 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
 import { Meta } from "@storybook/react/types-6-0";
-import { TextInput } from "../..";
+import { TextInput, Select } from "../..";
 export default {
   title: "ds-react/form",
 } as Meta;
 
 export const All = () => {
-  const [text, setText] = useState("");
-
   return (
     <form>
-      <TextInput label="Text input" />
-      <TextInput label="A disabled text input" disabled />
-      <TextInput
-        label="Another text input"
-        description="Text input description"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        error="This text input has an error"
-      />
-      <button type="submit">Submit</button>
+      <input type="text" size={3} />
+      <TextInput label="Text input" size="s" />
+
+      <select size={2}>
+        <option>one</option>
+        <option>two</option>
+        <option>three</option>
+      </select>
+      <Select size="s">
+        <option>one</option>
+        <option>two</option>
+        <option>three</option>
+      </Select>
     </form>
   );
 };
+
+/*
+const [text, setText] = useState("");
+
+<TextInput label="A disabled text input" disabled />
+<TextInput
+label="Another text input"
+description="Text input description"
+value={text}
+onChange={(e) => setText(e.target.value)}
+error="This text input has an error"
+/>
+<button type="submit">Submit</button>
+*/
