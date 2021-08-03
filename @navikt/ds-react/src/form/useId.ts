eeab81eff8e34b2c16f54fe6eb7e0fcc558d1343
Ken <ken.aleksander@gmail.com>
2021-08-03 09:01:06 +0200

    bruker nå short-uuid

--- @navikt/ds-react/src/form/useId.ts
+++ @navikt/ds-react/src/form/useId.ts
@@ -1,6 +1,6 @@
 import { useRef } from "react";
-import { v4 as uuidv4 } from "uuid";
 import ShortUuid from "short-uuid";
+/* import { v4 as uuidv4 } from "uuid"; */
 
 const useId = (id?: string): string => {
   const localId = useRef(ShortUuid.generate()).current;
