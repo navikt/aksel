6321bafcb4d7122fcc94ffebfa3b14879673a624
Ken <ken.aleksander@gmail.com>
2021-08-09 13:23:09 +0200

    oppdaterte useId for SSR

--- @navikt/ds-react/src/form/useId.ts
+++ @navikt/ds-react/src/form/useId.ts
@@ -1,11 +1,18 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import ShortUuid from "short-uuid";
 /* import { v4 as uuidv4 } from "uuid"; */
 
 const useId = (props?: { id?: string; prefix?: string }): string => {
   const { id = null, prefix = "" } = props ? props : {};
 
-  const localId = useRef(`${prefix}-${ShortUuid.generate()}`).current;
+  // const localId = useRef(`${prefix}-${ShortUuid.generate()}`).current;
+
+  const [localId, setlocalId] = useState<string>("");
+
+  useEffect(() => {
+    setlocalId(`${prefix}-${ShortUuid.generate()}`);
+  }, [prefix]);
+
   return id ?? localId;
 };
 
