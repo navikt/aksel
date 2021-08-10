a270317c180230c3b1a72d992d23166077189ae8
Ken <ken.aleksander@gmail.com>
2021-08-10 14:07:09 +0200

    fikset imports/exports i index.ts

--- @navikt/ds-react/src/index.ts
+++ @navikt/ds-react/src/index.ts
@@ -1,72 +1,23 @@
-export { Accordion, AccordionProps } from "./accordion";
-export {
-  AccordionAnchorMenu,
-  AccordionAnchorMenuCollapsable,
-  AccordionAnchorMenuItem,
-} from "./accordion-anchor-menu";
-export {
-  AccordionMenu,
-  AccordionMenuCollapsable,
-  AccordionMenuItem,
-} from "./accordion-menu";
-export { Alert, AlertProps } from "./alert";
-export { Button, ButtonProps } from "./button";
-export { MicroCard, MicroCardProps } from "./card";
-export { ContentContainer, ContentContainerProps } from "./content-container";
-export { CopyToClipboard } from "./copy-to-clipboard";
-export {
-  Checkbox,
-  CheckboxProps,
-  CheckboxGroup,
-  CheckboxGroupProps,
-  Fieldset,
-  FieldsetContext,
-  FieldsetProps,
-  Radio,
-  RadioProps,
-  RadioGroup,
-  RadioGroupProps,
-  Select,
-  SelectProps,
-  Textarea,
-  TextareaProps,
-  TextField,
-  TextFieldProps,
-} from "./form";
-export { Cell, CellProps, Grid, GridProps } from "./grid";
-export { Header, HeaderProps } from "./header";
-export {
-  InternalHeader,
-  InternalHeaderProps,
-  InternalHeaderTitle,
-  InternalHeaderTitleProps,
-  InternalHeaderUser,
-  InternalHeaderUserProps,
-} from "./internal-header";
-export {
-  ProductPageLayout,
-  ProductPagePanel,
-  ProductPageSection,
-} from "./layouts";
-export { Link, LinkProps } from "./link";
-export { LinkPanel, LinkPanelProps, LinkPanelTitle } from "./link-panel";
-export { Loader, LoaderProps } from "./loader";
-export { Modal, ModalProps } from "./modal";
-export { Panel, PanelProps } from "./panel";
-export { Popover, PopoverProps } from "./popover";
-export {
-  Bubble,
-  BubbleProps,
-  SpeechBubble,
-  SpeechBubbleProps,
-} from "./speech-bubble";
-export { Tag, TagProps } from "./tag";
-export {
-  BodyLong,
-  BodyShort,
-  Detail,
-  Ingress,
-  Label,
-  Title,
-} from "./typography";
+export * from "./accordion";
+export * from "./accordion-anchor-menu";
+export * from "./accordion-menu";
+export * from "./alert";
+export * from "./button";
+export * from "./card";
+export * from "./content-container";
+export * from "./copy-to-clipboard";
+export * from "./form";
+export * from "./grid";
+export * from "./header";
+export * from "./internal-header";
+export * from "./layouts";
+export * from "./link";
+export * from "./link-panel";
+export * from "./loader";
+export * from "./modal";
+export * from "./panel";
+export * from "./popover";
+export * from "./speech-bubble";
+export * from "./tag";
+export * from "./typography";
 export * from "./util";
