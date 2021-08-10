fce978c0f995b1a4ba83b237ef01c514c1fd174e
Ken <ken.aleksander@gmail.com>
2021-08-10 13:48:49 +0200

    oppdatert omit

--- @navikt/ds-react/src/util/utils.ts
+++ @navikt/ds-react/src/util/utils.ts
@@ -1,13 +1,10 @@
-export function addPropToObject(
-  obj: object,
-  [key, value]: [string | number, any]
-): object {
-  obj[key] = value;
-  return obj;
-}
-
-export function omit(obj: object, props: string[]) {
-  return Object.entries(obj)
+export const omit = (obj: object, props: string[]) =>
+  Object.entries(obj)
     .filter(([key]) => !props.includes(key))
-    .reduce(addPropToObject, {});
-}
+    .reduce(
+      (obj, [key, value]) => ({
+        ...obj,
+        [key]: value,
+      }),
+      {}
+    );
