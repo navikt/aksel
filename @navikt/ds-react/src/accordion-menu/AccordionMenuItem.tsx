f870ba482e9872208ac494027924cc00dedfcc57
Andreas Nordahl <andnorda@gmail.com>
2021-08-11 11:43:27 +0200

    accordion menu item without overridable component

--- @navikt/ds-react/src/accordion-menu/AccordionMenuItem.tsx
+++ @navikt/ds-react/src/accordion-menu/AccordionMenuItem.tsx
@@ -2,38 +2,32 @@ import React, { forwardRef } from "react";
 import { OverridableComponent } from "../util";
 import cl from "classnames";
 
-export type AccordionMenuItemType = OverridableComponent<AccordionMenuItemProps>;
-
-export interface AccordionMenuItemProps {
-  props: {
-    children: React.ReactNode;
-    active?: boolean;
-  } & React.HTMLAttributes<HTMLAnchorElement>;
-  defaultComponent: "a";
+export interface AccordionMenuItemProps
+  extends React.HTMLAttributes<HTMLAnchorElement> {
+  children: React.ReactNode;
+  active?: boolean;
 }
 
-const AccordionMenuItem: AccordionMenuItemType = forwardRef(
-  (
-    {
-      children,
-      component: Component = "a",
-      active = false,
-      className,
-      ...rest
-    },
-    ref
-  ) => {
-    return (
-      <Component
-        ref={ref}
-        className={cl("navds-accordion-menu-item", className, {
-          "navds-accordion-menu-item--active": active,
-        })}
-        {...rest}
-      >
-        {children}
-      </Component>
-    );
+const AccordionMenuItem = forwardRef<HTMLAnchorElement, AccordionMenuItemProps>(
+  ({ children, active = false, className, ...rest }, ref) => {
+    let child =
+      typeof children === "string" ? (
+        <a>{children}</a>
+      ) : (
+        React.Children.only(children)
+      );
+
+    const props = {
+      ...rest,
+      ref,
+      className: cl("navds-accordion-menu-item", className, {
+        "navds-accordion-menu-item--active": active,
+      }),
+    };
+
+    return React.isValidElement(child)
+      ? React.cloneElement(child, props)
+      : null;
   }
 );
 
--- @navikt/ds-react/src/accordion-menu/stories/accordion-menu.stories.tsx
+++ @navikt/ds-react/src/accordion-menu/stories/accordion-menu.stories.tsx
@@ -66,80 +66,62 @@ export const All = () => (
 export const ReactRouter = () => (
   <AccordionMenu>
     <Collapsable title="Nivå 1 – 3 rems høy">
-      <Item
-        component={NavLink}
-        activeClassName="navds-accordion-menu-item--active"
-        to="/1"
-      >
-        Nivå 2 innrykk 2 rems
+      <Item>
+        <NavLink to="/1" activeClassName="navds-accordion-menu-item--active">
+          Nivå 2 innrykk 2 rems
+        </NavLink>
       </Item>
-      <Item
-        component={NavLink}
-        activeClassName="navds-accordion-menu-item--active"
-        to="/2"
-      >
-        Subtitle 3
+      <Item>
+        <NavLink to="/2" activeClassName="navds-accordion-menu-item--active">
+          Subtitle 3
+        </NavLink>
       </Item>
     </Collapsable>
     <Collapsable title="Skjema og søknad">
       <Collapsable title="Satser">
-        <Item
-          component={NavLink}
-          activeClassName="navds-accordion-menu-item--active"
-          to="/3"
-        >
-          Nivå 3 – 3 rems innrykk Har du rett til dagpenger når du mottar annen
-          økonomisk støtte fra NAV?
+        <Item>
+          <NavLink to="/3" activeClassName="navds-accordion-menu-item--active">
+            Nivå 3 – 3 rems innrykk Har du rett til dagpenger når du mottar
+            annen økonomisk støtte fra NAV?
+          </NavLink>
         </Item>
       </Collapsable>
-      <Item
-        component={NavLink}
-        activeClassName="navds-accordion-menu-item--active"
-        to="/4"
-      >
-        Subtitle 2
+      <Item>
+        <NavLink to="/4" activeClassName="navds-accordion-menu-item--active">
+          Subtitle 2
+        </NavLink>
       </Item>
-      <Item
-        component={NavLink}
-        activeClassName="navds-accordion-menu-item--active"
-        to="/5"
-      >
-        Subtitle 3
+      <Item>
+        <NavLink to="/5" activeClassName="navds-accordion-menu-item--active">
+          Subtitle 3
+        </NavLink>
       </Item>
     </Collapsable>
     <Collapsable title="Sakbehandlingstider">
       <Collapsable title="Subtitle 4 Lang tittel på andre nivå lorem ipsum">
-        <Item
-          component={NavLink}
-          activeClassName="navds-accordion-menu-item--active"
-          to="/6"
-        >
-          Subtitle 5
+        <Item>
+          <NavLink to="/6" activeClassName="navds-accordion-menu-item--active">
+            Subtitle 5
+          </NavLink>
         </Item>
       </Collapsable>
     </Collapsable>
-    <Item
-      component={NavLink}
-      activeClassName="navds-accordion-menu-item--active"
-      to="/7"
-    >
-      Internasjonalt
+    <Item>
+      <NavLink to="/7" activeClassName="navds-accordion-menu-item--active">
+        Internasjonalt
+      </NavLink>
     </Item>
     <Collapsable title="Meld fra om endringer lang tittel på første nivå">
-      <Item
-        component={NavLink}
-        activeClassName="navds-accordion-menu-item--active"
-        to="/8"
-      >
-        Subtitle 6
+      <Item>
+        <NavLink to="/8" activeClassName="navds-accordion-menu-item--active">
+          Subtitle 6
+        </NavLink>
       </Item>
     </Collapsable>
-    <Item
-      component={NavLink}
-      activeClassName="navds-accordion-menu-item--active"
-      to="/9"
-    >
-      Nivå 1
+    <Item>
+      <NavLink to="/9" activeClassName="navds-accordion-menu-item--active">
+        Nivå 1
+      </NavLink>
     </Item>
   </AccordionMenu>
 );
