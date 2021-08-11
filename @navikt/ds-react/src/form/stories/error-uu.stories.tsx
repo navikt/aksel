00ccebf82644088ed4a7e40f67c2717acaa4bbe6
Ken <26967723+KenAJoh@users.noreply.github.com>
2021-08-11 13:37:12 +0200

    Demo for uu-testing av fieldset (#1165)

---
+++ @navikt/ds-react/src/form/stories/error-uu.stories.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Fieldset } from "../index";
+import { Meta } from "@storybook/react/types-6-0";
+import { Button, Checkbox, CheckboxGroup } from "../../index";
+export default {
+  title: "ds-react/form/uu-demo",
+  component: Fieldset,
+} as Meta;
+
+export const Demo1 = () => {
+  const [error, setError] = useState("");
+  const [fieldsetError, setFieldsetError] = useState("");
+  const [propagation, setPropagation] = useState(true);
+  return (
+    <div>
+      <h1>Nested-erros + aria-describedby</h1>
+      <Button
+        style={{ marginBottom: "2rem" }}
+        onClick={() => {
+          !!fieldsetError
+            ? setFieldsetError("")
+            : setFieldsetError("Fieldset error message");
+        }}
+      >
+        Toggle fieldset-error
+      </Button>
+      <Button
+        style={{ marginBottom: "2rem" }}
+        onClick={() => {
+          !!error ? setError("") : setError("Textfield error message");
+        }}
+      >
+        Toggle textfield-error
+      </Button>
+      <Button
+        style={{ marginBottom: "2rem" }}
+        onClick={() => setPropagation(!propagation)}
+      >
+        Toggle errorPropagation
+      </Button>
+      <Fieldset legend="Mollit eiusmod" error={fieldsetError}>
+        <CheckboxGroup
+          errorPropagation={propagation}
+          legend="checkboxgroup"
+          hideLegend
+        >
+          <Checkbox>Checkbox nr 1</Checkbox>
+          <Checkbox error={error}>Checkbox nr 2</Checkbox>
+          <Checkbox>Checkbox nr 3</Checkbox>
+        </CheckboxGroup>
+      </Fieldset>
+    </div>
+  );
+};
