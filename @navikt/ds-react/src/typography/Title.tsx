e85a5038f685036cf42d13f73251ff486a719531
Andreas Nordahl <andnorda@gmail.com>
2021-08-11 12:00:20 +0200

    title without overridable component

--- @navikt/ds-react/src/typography/Title.tsx
+++ @navikt/ds-react/src/typography/Title.tsx
@@ -1,43 +1,39 @@
 import React, { forwardRef } from "react";
 import cl from "classnames";
-import { OverridableComponent } from "../util";
 
 export interface TitleProps extends React.HTMLAttributes<HTMLHeadingElement> {
-  props: {
-    /**
-     * Heading level
-     */
-    level: 1 | 2 | 3 | 4 | 5 | "1" | "2" | "3" | "4" | "5";
-    /**
-     * Sizes from largest "2xl" to smallest "s"
-     */
-    size: "2xl" | "xl" | "l" | "m" | "s";
-    /**
-     * Heading to render
-     */
-    children: React.ReactNode;
-    /**
-     * Custom styling on element
-     */
-    className?: string;
-    /**
-     * Adds margins to typo
-     */
-    spacing?: boolean;
-  } & React.HTMLAttributes<HTMLParagraphElement>;
-  defaultComponent: "h1";
+  /**
+   * Heading level
+   */
+  level: 1 | 2 | 3 | 4 | 5 | "1" | "2" | "3" | "4" | "5";
+  /**
+   * Sizes from largest "2xl" to smallest "s"
+   */
+  size: "2xl" | "xl" | "l" | "m" | "s";
+  /**
+   * Heading to render
+   */
+  children: React.ReactNode;
+  /**
+   * Custom styling on element
+   */
+  className?: string;
+  /**
+   * Adds margins to typo
+   */
+  spacing?: boolean;
 }
 
-const Title: OverridableComponent<TitleProps> = forwardRef(
-  ({ level, size, spacing, className, children, component, ...rest }, ref) => {
-    let HeadingTag = component ?? (`h${level}` as React.ElementType);
+const Title = forwardRef<HTMLHeadElement, TitleProps>(
+  ({ level, size, spacing = false, className, children, ...rest }, ref) => {
+    const HeadingTag = `h${level}` as React.ElementType;
 
     return (
       <HeadingTag
         {...rest}
         ref={ref}
         className={cl(className, "navds-title", `navds-title--${size}`, {
-          "navds-typo--spacing": !!spacing,
+          "navds-typo--spacing": spacing,
         })}
       >
         {children}
