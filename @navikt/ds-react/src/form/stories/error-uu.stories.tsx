595fe600cc025b7dc1a25ecf4f1ba5954e85958b
Ken <ken.aleksander@gmail.com>
2021-08-13 13:22:59 +0200

    fikset story

--- @navikt/ds-react/src/form/stories/error-uu.stories.tsx
+++ @navikt/ds-react/src/form/stories/error-uu.stories.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Fieldset } from "../index";
 import { Meta } from "@storybook/react/types-6-0";
 import { Button, Checkbox, CheckboxGroup, TextField } from "../../index";
-import { SSRProvider } from "../../util";
 export default {
   title: "ds-react/form/uu-demo",
   component: Fieldset,
@@ -14,53 +13,51 @@ export const Demo1 = () => {
   const [groupError, setGroupError] = useState(undefined);
   const [propagation, setPropagation] = useState(true);
   return (
-    <SSRProvider>
-      <div>
-        <h1>Nested-erros + aria-describedby</h1>
-        <Button
-          style={{ marginBottom: "2rem" }}
-          onClick={() => {
-            !!fieldsetError
-              ? setFieldsetError("")
-              : setFieldsetError("Fieldset error message");
-          }}
+    <div>
+      <h1>Nested-erros + aria-describedby</h1>
+      <Button
+        style={{ marginBottom: "2rem" }}
+        onClick={() => {
+          !!fieldsetError
+            ? setFieldsetError("")
+            : setFieldsetError("Fieldset error message");
+        }}
+      >
+        Toggle fieldset-error
+      </Button>
+      <Button
+        style={{ marginBottom: "2rem" }}
+        onClick={() => setGroupError("CheckboxGroup error message")}
+      >
+        Toggle group-error
+      </Button>
+      <Button
+        style={{ marginBottom: "2rem" }}
+        onClick={() => {
+          !!error ? setError("") : setError("Checkbox error message");
+        }}
+      >
+        Toggle checkbox-error
+      </Button>
+      <Button
+        style={{ marginBottom: "2rem" }}
+        onClick={() => setPropagation(!propagation)}
+      >
+        Toggle errorPropagation
+      </Button>
+      <Fieldset legend="Mollit eiusmod" error={fieldsetError}>
+        <TextField label="testlabel"></TextField>
+        <CheckboxGroup
+          errorPropagation={propagation}
+          error={groupError}
+          legend="checkboxgroup"
+          hideLegend
         >
-          Toggle fieldset-error
-        </Button>
-        <Button
-          style={{ marginBottom: "2rem" }}
-          onClick={() => setGroupError("CheckboxGroup error message")}
-        >
-          Toggle group-error
-        </Button>
-        <Button
-          style={{ marginBottom: "2rem" }}
-          onClick={() => {
-            !!error ? setError("") : setError("Checkbox error message");
-          }}
-        >
-          Toggle checkbox-error
-        </Button>
-        <Button
-          style={{ marginBottom: "2rem" }}
-          onClick={() => setPropagation(!propagation)}
-        >
-          Toggle errorPropagation
-        </Button>
-        <Fieldset legend="Mollit eiusmod" error={fieldsetError}>
-          <TextField label="testlabel"></TextField>
-          <CheckboxGroup
-            errorPropagation={propagation}
-            error={groupError}
-            legend="checkboxgroup"
-            hideLegend
-          >
-            <Checkbox>Checkbox nr 1</Checkbox>
-            <Checkbox error={error}>Checkbox nr 2</Checkbox>
-            <Checkbox>Checkbox nr 3</Checkbox>
-          </CheckboxGroup>
-        </Fieldset>
-      </div>
-    </SSRProvider>
+          <Checkbox>Checkbox nr 1</Checkbox>
+          <Checkbox error={error}>Checkbox nr 2</Checkbox>
+          <Checkbox>Checkbox nr 3</Checkbox>
+        </CheckboxGroup>
+      </Fieldset>
+    </div>
   );
 };
