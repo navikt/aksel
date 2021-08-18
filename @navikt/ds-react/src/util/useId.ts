e2149a0f8b72f9faea18a0dac8f22efcb229d9db
Ken <ken.aleksander@gmail.com>
2021-08-18 14:12:35 +0200

    lint fix

--- @navikt/ds-react/src/util/useId.ts
+++ @navikt/ds-react/src/util/useId.ts
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import ShortUuid from "short-uuid";
 
 const canUseDOM = (): boolean => {
