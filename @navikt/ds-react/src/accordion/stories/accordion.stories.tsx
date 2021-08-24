13377393ffd768822b20edca918c41a93d059b74
Ken <ken.aleksander@gmail.com>
2021-08-24 15:57:38 +0200

    oppdatert story

--- @navikt/ds-react/src/accordion/stories/accordion.stories.tsx
+++ @navikt/ds-react/src/accordion/stories/accordion.stories.tsx
@@ -12,6 +12,8 @@ export const All = () => {
   return (
     <>
       <div>
+        <h1>Accordion</h1>
+        <h2>Controlled</h2>
         <Accordion open={open}>
           <Accordion.Header onClick={(e) => setOpen(!open)}>
             Accordion header text
@@ -33,6 +35,7 @@ export const All = () => {
             proident commodo adipisicing eiusmod id.
           </Accordion.Content>
         </Accordion>
+        <h2>Un-controlled</h2>
         <Accordion>
           <Accordion.Header>Accordion header text</Accordion.Header>
           <Accordion.Content>
@@ -52,26 +55,6 @@ export const All = () => {
             proident commodo adipisicing eiusmod id.
           </Accordion.Content>
         </Accordion>
-        {/* <Accordion open={open}>
-        <Accordion.Header onClick={(e) => setOpen(!open)}>
-          Accordion header text
-        </Accordion.Header>
-        <Accordion.Content>
-          Magna aliquip aliquip fugiat nostrud nostrud velit pariatur veniam
-          officia laboris voluptate officia pariatur.Lorem est ex anim velit
-          occaecat nisi qui nostrud sit consectetur consectetur officia nostrud
-          ullamco. Est ex duis proident nostrud elit qui laborum anim minim eu
-          eiusmod. Veniam in nostrud sunt tempor velit incididunt sint ex dolor
-          qui velit id eu. Deserunt magna sunt velit in. Est exercitation id
-          cillum qui do. Minim adipisicing nostrud commodo proident occaecat
-          aliquip nulla anim proident reprehenderit. Magna ipsum officia veniam
-          cupidatat duis veniam dolore reprehenderit mollit velit.Ut consequat
-          commodo minim occaecat id pariatur. Nisi enim tempor laborum commodo.
-          Tempor sit quis nostrud eu cupidatat sunt commodo reprehenderit irure
-          deserunt eiusmod ipsum. Exercitation quis commodo cillum eiusmod
-          eiusmod. Do laborum qui proident commodo adipisicing eiusmod id.
-        </Accordion.Content>
-      </Accordion> */}
       </div>
     </>
   );
