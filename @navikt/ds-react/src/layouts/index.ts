7ea6c9c0f1f7c5f0a4a1983250e15fe33aa76b7f
Ken <26967723+KenAJoh@users.noreply.github.com>
2021-08-25 10:24:03 +0200

    fjernet utdaterte komponenter (#1186)

--- @navikt/ds-react/src/layouts/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ProductPageLayout } from "./product-page/ProductPageLayout";
-export { default as ProductPagePanel } from "./product-page/ProductPagePanel";
-export { default as ProductPageSection } from "./product-page/ProductPageSection";
