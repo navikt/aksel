1cddd9feba187644620af264e256a597e9a18432
Ken <26967723+KenAJoh@users.noreply.github.com>
2021-08-27 11:04:46 +0200

    Oppdatert Loader (#1202)
    Co-authored-by: Andreas Nordahl <andnorda@gmail.com>

--- @navikt/ds-react/src/loader/stories/loader.stories.tsx
+++ @navikt/ds-react/src/loader/stories/loader.stories.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
-import { Loader } from "../index";
-import { Button, Panel, Title } from "../../index";
+import React from "react";
+import { Loader } from "..";
+import { Button } from "../..";
 import { Meta } from "@storybook/react/types-6-0";
-import "./demo.css";
 export default {
   title: "ds-react/loader",
   component: Loader,
@@ -40,90 +39,31 @@ export const All = () => {
         <Loader size="xlarge" variant="neutral" transparent />
         <Loader size="xlarge" variant="interaction" transparent />
         <Loader size="xlarge" variant="inverted" transparent />
-        <h2>Brukt i knapper</h2>
       </div>
+      <h2>Brukt i knapper</h2>
       <div>
         <Button>
-          <span>Laster...</span>
+          Laster...
           <Loader />
-        </Button>{" "}
+        </Button>
         <Button size="small">
-          <span>Laster...</span>
+          Laster...
           <Loader />
         </Button>
         <h2>Variants</h2>
         <Button variant="secondary">
-          <span>Laster...</span>
+          Laster...
           <Loader />
-        </Button>{" "}
+        </Button>
         <Button variant="action">
-          <span>Laster...</span>
+          Laster...
           <Loader />
-        </Button>{" "}
+        </Button>
         <Button variant="danger">
-          <span>Laster...</span>
+          Laster...
           <Loader />
         </Button>
       </div>
-    </div>
-  );
-};
-
-export const LoaderDemo = () => {
-  const [loading, setLoading] = useState(false);
-  const [longText, setLongText] = useState(false);
-
-  const handleAuto = () => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  };
-
-  const text = longText
-    ? "Laster inn nytt innhold fra databasen. Hvis dette tar lengre enn 10 sekunder kan man prøve å laste siden på nytt. Om dette ikke fungerer kan det hende tjenesten har nedetid nå, sjekk ut denne linken for å se status"
-    : "Laster inn innholdet...";
-
-  return (
-    <div className="demo__wrapper">
-      <Title level="1" size="large">
-        Test/Demo for loading state. Ikke bruk som referanse i implmentasjon
-      </Title>
-      <div>
-        <Button onClick={() => setLoading(!loading)}>
-          Toggle Loading state
-        </Button>
-        <Button onClick={() => setLongText(!longText)}>
-          Toggle Text-lengde
-        </Button>
-        <Button onClick={() => handleAuto()}>
-          Automatisk endring (3 sekunder)
-        </Button>
-      </div>
-
-      <Panel
-        border
-        className="demo__panel"
-        aria-live="polite"
-        aria-busy={loading}
-      >
-        {loading ? (
-          <>
-            <Loader size="xlarge" title={text} />
-            <span>{text}</span>
-          </>
-        ) : (
-          <div>
-            <h2>DummyTittel</h2>
-            <p>
-              Dolor nostrud sint incididunt consectetur adipisicing sit. Laborum
-              enim consequat non labore velit ex. Esse amet irure cillum amet
-              dolor ad consequat. Est sit id tempor dolore officia proident
-              consequat sunt cillum.
-            </p>
-          </div>
-        )}
-      </Panel>
     </div>
   );
 };
