0269447ec6373a5ef0786353b9238565e4bafb04
Ken <ken.aleksander@gmail.com>
2021-08-30 10:26:20 +0200

    oppdatert tag med sizing

--- @navikt/ds-react/src/tag/Tag.tsx
+++ @navikt/ds-react/src/tag/Tag.tsx
@@ -1,33 +1,42 @@
 import React, { forwardRef, HTMLAttributes } from "react";
 import cl from "classnames";
+import { Detail } from "../";
 
 export interface TagProps extends HTMLAttributes<HTMLSpanElement> {
   /**
-   * Component content
+   * Tag
    */
   children: React.ReactNode;
   /**
    * Changes background-color and border-color
+   *
    */
   variant: "warning" | "error" | "info" | "success";
+  /**
+   * Changes sizing of tag
+   * @default "medium"
+   */
+  size?: "medium" | "small";
 }
 
 const Tag = forwardRef<HTMLSpanElement, TagProps>(
-  ({ children, className, variant, ...rest }, ref) => {
+  ({ className, variant, size = "medium", ...rest }, ref) => {
     return (
       <span
         ref={ref}
         className={cl(
           "navds-tag",
           className,
-          "navds-detail",
-          "navds-detail--s",
-          `navds-tag--${variant}`
+          `navds-tag--${variant}`,
+          `navds-tag--${size}`,
+          {
+            "navds-body-short": size === "medium",
+            "navds-detail": size === "small",
+            "navds-detail--small": size === "small",
+          }
         )}
         {...rest}
-      >
-        {children}
-      </span>
+      />
     );
   }
 );
