9baf81c57160fd709fb07aae569b2977118d6d28
Ken <ken.aleksander@gmail.com>
2021-09-13 14:51:59 +0200

    oppdatert token index

--- @navikt/ds-tokens/src/index.js
+++ @navikt/ds-tokens/src/index.js
@@ -7,38 +7,6 @@ const colors = specifyColors.colors;
 
 const getFontSize = (size) => `${size / baseFontSize}rem`;
 
-// https://github.com/hihayk/scale/blob/69b766bba2db046d3e8cb4026ae32a32c897f9ff/src/utils.js#L44
-const mixColors = (color, step, amount, mixColor) => {
-  const saturation = Math.round(Color(color).hsl().color[1]);
-  // Setting directly to hex returns different result...
-  return Color(
-    Color(color)
-      .saturate(((step + 1) / 5) * (saturation / 100))
-      .mix(Color(mixColor), ((amount / 100) * (step + 1)) / 5)
-      .string()
-  ).hex();
-};
-
-const lightColor = (color, step) => mixColors(color, step, 100, "white");
-const darkColor = (color, step) => mixColors(color, step, 85, "black");
-
-const baseColors = {
-  blue: "#0067c5",
-  deepblue: "#005B82",
-  lightblue: "#66CBEC",
-  orange: "#FF9100",
-  green: "#06893A",
-  red: "#BA3A26",
-  purple: "#634689",
-  limegreen: "#a2ad00",
-};
-
-const white = "#ffffff";
-const darkgray = "#262626";
-const lightgray = "#F1F1F1";
-
-const gray = (n) => Color(lightgray).mix(Color(darkgray), n).hex();
-
 const globalColors = Object.entries(colors)
   .filter(([key, _]) => key.startsWith("global"))
   .reduce((colors, [key, value]) => ({ ...colors, [key]: value }), {});
@@ -53,6 +21,7 @@ const getColorRef = (color) => {
   return color;
 };
 
+/* TODO: Bruke spacing fra Specify, bruke semantiske farger fra figma i komponenter */
 module.exports = {
   navds: {
     /* NEW COLORS */
@@ -66,100 +35,24 @@ module.exports = {
         },
       };
     }, {}),
-    /* OLD COLORS */
-    color: {
-      white: { value: white },
-      darkgray: { value: darkgray },
-      gray: {
-        90: { value: gray(1) },
-        80: { value: gray(0.8) },
-        60: { value: gray(0.6667) },
-        40: { value: gray(0.4) },
-        20: { value: gray(0.196) },
-        10: { value: gray(0) },
-      },
-      ...Object.entries(baseColors).reduce(
-        (colors, [name, color]) => ({
-          ...colors,
-          [name]: {
-            90: { value: darkColor(color, 3) },
-            80: { value: darkColor(color, 2) },
-            70: { value: darkColor(color, 1) },
-            60: { value: darkColor(color, 0) },
-            50: { value: color },
-            40: { value: lightColor(color, 0) },
-            30: { value: lightColor(color, 1) },
-            20: { value: lightColor(color, 2) },
-            10: { value: lightColor(color, 3) },
-          },
+    spacing: Array(24)
+      .fill(0)
+      .reduce(
+        (spacing, _, index) => ({
+          ...spacing,
+          [index + 1]: { value: `${(index + 1) / 4}rem` },
         }),
         {}
       ),
-      disabled: { value: "{navds.color.gray.40.value}" },
-      action: {
-        default: { value: "{navds.color.blue.50.value}" },
-        hover: { value: "{navds.color.blue.60.value}" },
-        active: { value: "{navds.color.deepblue.50.value}" },
-      },
-      danger: {
-        default: { value: "{navds.color.red.50.value}" },
-        hover: { value: "{navds.color.red.60.value}" },
-        active: { value: "{navds.color.red.70.value}" },
-      },
-      error: {
-        border: { value: "{navds.color.red.50.value}" },
-        background: { value: "{navds.color.red.10.value}" },
-      },
-      warning: {
-        border: { value: "{navds.color.orange.60.value}" },
-        background: { value: "{navds.color.orange.10.value}" },
-      },
-      info: {
-        border: { value: "{navds.color.lightblue.70.value}" },
-        background: { value: "{navds.color.lightblue.10.value}" },
-      },
-      success: {
-        border: { value: "{navds.color.green.50.value}" },
-        background: { value: "{navds.color.green.10.value}" },
-      },
-      border: { value: "{navds.color.gray.40.value}" },
-      background: { value: "{navds.color.white.value}" },
-      text: {
-        primary: { value: "{navds.color.gray.90.value}" },
-        inverse: { value: "{navds.color.white.value}" },
-        disabled: { value: "{navds.color.gray.60.value}" },
-        link: { value: "{navds.color.blue.50.value}" },
-        error: { value: "{navds.color.red.50.value}" },
-      },
-      hover: { value: "{navds.color.gray.40.value}" },
-      tag: {
-        error: {
-          border: { value: "{navds.color.red.50.value}" },
-          background: { value: "{navds.color.red.10.value}" },
-        },
-        warning: {
-          border: { value: "{navds.color.orange.60.value}" },
-          background: { value: "{navds.color.orange.10.value}" },
-        },
-        info: {
-          border: { value: "{navds.color.lightblue.70.value}" },
-          background: { value: "{navds.color.lightblue.10.value}" },
-        },
-        success: {
-          border: { value: "{navds.color.green.50.value}" },
-          background: { value: "{navds.color.green.10.value}" },
-        },
-      },
-      modal: {
-        overlay: {
-          value: "rgba(38, 38, 38, 0.7)",
-        },
-      },
+    shadow: {
+      focus: { value: "0 0 0 3px" },
+    },
+    depth: {
+      modal: { value: 2000 },
+      popover: { value: 1000 },
+      focus: { value: 10 },
     },
 
-    /* NEW TOKENS */
-
-    /* OLD TOKENS */
     font: {
       family: { value: '"Source Sans Pro", Arial, sans-serif' },
       line: {
@@ -184,35 +77,6 @@ module.exports = {
       weight: {
         bold: { value: "600" },
         regular: { value: "400" },
-      },
-    },
-    shadow: {
-      focus: { value: "0 0 0 3px" },
-    },
-    depth: {
-      modal: { value: 2000 },
-      popover: { value: 1000 },
-      focus: { value: 10 },
-    },
-    spacing: Array(24)
-      .fill(0)
-      .reduce(
-        (spacing, _, index) => ({
-          ...spacing,
-          [index + 1]: { value: `${(index + 1) / 4}rem` },
-        }),
-        {}
-      ),
-    checkmark: {
-      image: {
-        white: {
-          value:
-            "url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMyAxMCI+ICAgIDxnPiAgICA8cGF0aCBmaWxsPSIjRkZGRkZGIiBkPSJNNCwxMGMtMC40LDAtMC44LTAuMS0xLjEtMC40TDAuNCw3LjFDMC4xLDYuOCwwLDYuNCwwLDZzMC4yLTAuOCwwLjUtMS4xQzEsNC40LDIsNC40LDIuNSw0LjlMNCw2LjRsNi40LTYgICAgQzEwLjgsMC4xLDExLjEsMCwxMS41LDBjMC40LDAsMC44LDAuMiwxLDAuNWMwLjYsMC42LDAuNSwxLjYtMC4xLDIuMXYwTDUsOS42QzQuNyw5LjksNC40LDEwLDQsMTB6IE0xMS44LDEuOUwxMS44LDEuOSAgICBDMTEuOCwxLjksMTEuOCwxLjksMTEuOCwxLjl6IE0xMS4yLDEuMUMxMS4yLDEuMSwxMS4yLDEuMSwxMS4yLDEuMUwxMS4yLDEuMXoiLz4gICAgPC9nPjwvc3ZnPg==)",
-        },
-        blue: {
-          value:
-            "url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMyAxMCI+ICAgIDxnPiAgICA8cGF0aCBmaWxsPSIjMDA2N0M1IiBkPSJNNCwxMGMtMC40LDAtMC44LTAuMS0xLjEtMC40TDAuNCw3LjFDMC4xLDYuOCwwLDYuNCwwLDZzMC4yLTAuOCwwLjUtMS4xQzEsNC40LDIsNC40LDIuNSw0LjlMNCw2LjRsNi40LTYgICAgQzEwLjgsMC4xLDExLjEsMCwxMS41LDBjMC40LDAsMC44LDAuMiwxLDAuNWMwLjYsMC42LDAuNSwxLjYtMC4xLDIuMXYwTDUsOS42QzQuNyw5LjksNC40LDEwLDQsMTB6IE0xMS44LDEuOUwxMS44LDEuOSAgICBDMTEuOCwxLjksMTEuOCwxLjksMTEuOCwxLjl6IE0xMS4yLDEuMUMxMS4yLDEuMSwxMS4yLDEuMSwxMS4yLDEuMUwxMS4yLDEuMXoiLz4gICAgPC9nPjwvc3ZnPg==)",
-        },
       },
     },
   },
