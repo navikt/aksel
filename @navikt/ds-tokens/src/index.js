8ea57264eb722d10d3e595feaf55a794c5127e40
Ken <ken.aleksander@gmail.com>
2021-09-16 14:18:17 +0200

    oppdatert tokens config

--- @navikt/ds-tokens/src/index.js
+++ @navikt/ds-tokens/src/index.js
@@ -1,16 +1,20 @@
-const Color = require("color");
+const colorNpm = require("color");
+/* const specifyColors = require("./colors");
+const specifySpacing = require("./spacing");
+
+const colors = specifyColors.colors;
+const spacing = specifySpacing.sizes; */
 
 const baseFontSize = 16;
-
 const getFontSize = (size) => `${size / baseFontSize}rem`;
-// https://github.com/hihayk/scale/blob/69b766bba2db046d3e8cb4026ae32a32c897f9ff/src/utils.js#L44
+
 const mixColors = (color, step, amount, mixColor) => {
-  const saturation = Math.round(Color(color).hsl().color[1]);
+  const saturation = Math.round(colorNpm(color).hsl().color[1]);
   // Setting directly to hex returns different result...
-  return Color(
-    Color(color)
+  return colorNpm(
+    colorNpm(color)
       .saturate(((step + 1) / 5) * (saturation / 100))
-      .mix(Color(mixColor), ((amount / 100) * (step + 1)) / 5)
+      .mix(colorNpm(mixColor), ((amount / 100) * (step + 1)) / 5)
       .string()
   ).hex();
 };
@@ -33,10 +37,44 @@ const white = "#ffffff";
 const darkgray = "#262626";
 const lightgray = "#F1F1F1";
 
-const gray = (n) => Color(lightgray).mix(Color(darkgray), n).hex();
+const gray = (n) => colorNpm(lightgray).mix(colorNpm(darkgray), n).hex();
 
+/* const globalColors = Object.entries(colors)
+  .filter(([key, _]) => key.startsWith("global"))
+  .reduce((colors, [key, value]) => ({ ...colors, [key]: value }), {});
+
+const getColorRef = (color) => {
+  for (const c in globalColors) {
+    if (colorNpm(globalColors[c]).string() === colorNpm(color).string()) {
+      return `{navds.${c.replace("global", "globalColor")}.value}`;
+    }
+  }
+  return color;
+}; */
+
+/* TODO: Bruke spacing fra Specify, bruke semantiske farger fra figma i komponenter */
 module.exports = {
   navds: {
+    /* NEW COLORS */
+    /* ...Object.entries(colors).reduce((colors, [name, color]) => {
+      const newColor = name.startsWith("global") ? color : getColorRef(color);
+
+      return {
+        ...colors,
+        [name.replace("global", "globalColor")]: {
+          value: newColor,
+        },
+      };
+    }, {}), */
+    /* NEW SPACING */
+    /* ...Object.entries(spacing).reduce((spacing, [name, size]) => {
+      return {
+        ...spacing,
+        [name]: {
+          value: `${Number(size.replace("px", "")) / baseFontSize}rem`,
+        },
+      };
+    }, {}), */
     color: {
       white: { value: white },
       darkgray: { value: darkgray },
@@ -126,15 +164,24 @@ module.exports = {
         },
       },
     },
-    border: {
-      default: {
-        value: "1px solid {navds.color.border.value}",
-      },
-      radius: { value: "4px" },
+    spacing: Array(24)
+      .fill(0)
+      .reduce(
+        (spacing, _, index) => ({
+          ...spacing,
+          [index + 1]: { value: `${(index + 1) / 4}rem` },
+        }),
+        {}
+      ),
+    shadow: {
+      focus: { value: "0 0 0 3px" },
     },
-    panel: {
-      hover: { value: "1px solid {navds.color.blue.50.value}" },
+    depth: {
+      modal: { value: 2000 },
+      popover: { value: 1000 },
+      focus: { value: 10 },
     },
+
     font: {
       family: { value: '"Source Sans Pro", Arial, sans-serif' },
       line: {
@@ -159,7 +206,6 @@ module.exports = {
           l: { value: getFontSize(28) },
           m: { value: getFontSize(24) },
           s: { value: getFontSize(20) },
-          xs: { value: getFontSize(18) },
         },
         xl: { value: getFontSize(20) },
         l: { value: getFontSize(18) },
@@ -169,69 +215,6 @@ module.exports = {
       weight: {
         bold: { value: "600" },
         regular: { value: "400" },
-      },
-    },
-    shadow: {
-      focus: { value: "0 0 0 3px {navds.color.blue.80.value}" },
-      "focus-on-dark": { value: "0 0 0 3px {navds.color.orange.40.value}" },
-      hover: { value: "{navds.color.hover.value} 0 2px 1px 0" },
-    },
-    text: {
-      focus: { value: "{navds.color.blue.80.value}" },
-      shadow: { value: "0 0 0 2px {navds.color.blue.80.value}" },
-    },
-    contentContainer: {
-      maxWidth: { value: "79.5rem" },
-      padding: {
-        small: { value: "{navds.spacing.4.value}" },
-        mediumAndLarger: { value: "{navds.spacing.6.value}" },
-      },
-    },
-    grid: {
-      gutter: {
-        small: { value: "{navds.spacing.4.value}" },
-        mediumAndLarger: { value: "{navds.spacing.6.value}" },
-      },
-    },
-    layout: {
-      background: {
-        white: { value: white },
-        gray: { value: "{navds.color.gray.10.value}" },
-      },
-      padding: {
-        small: { value: "{navds.spacing.4.value}" },
-        mediumAndLarger: { value: "{navds.spacing.10.value}" },
-      },
-    },
-    "z-index": {
-      modal: { value: 2000 },
-      popover: { value: 1000 },
-      focus: { value: 10 },
-    },
-    sidebar: {
-      sticky: {
-        offset: { value: "0" },
-      },
-    },
-    spacing: Array(24)
-      .fill(0)
-      .reduce(
-        (spacing, _, index) => ({
-          ...spacing,
-          [index + 1]: { value: `${(index + 1) / 4}rem` },
-        }),
-        {}
-      ),
-    checkmark: {
-      image: {
-        white: {
-          value:
-            "url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMyAxMCI+ICAgIDxnPiAgICA8cGF0aCBmaWxsPSIjRkZGRkZGIiBkPSJNNCwxMGMtMC40LDAtMC44LTAuMS0xLjEtMC40TDAuNCw3LjFDMC4xLDYuOCwwLDYuNCwwLDZzMC4yLTAuOCwwLjUtMS4xQzEsNC40LDIsNC40LDIuNSw0LjlMNCw2LjRsNi40LTYgICAgQzEwLjgsMC4xLDExLjEsMCwxMS41LDBjMC40LDAsMC44LDAuMiwxLDAuNWMwLjYsMC42LDAuNSwxLjYtMC4xLDIuMXYwTDUsOS42QzQuNyw5LjksNC40LDEwLDQsMTB6IE0xMS44LDEuOUwxMS44LDEuOSAgICBDMTEuOCwxLjksMTEuOCwxLjksMTEuOCwxLjl6IE0xMS4yLDEuMUMxMS4yLDEuMSwxMS4yLDEuMSwxMS4yLDEuMUwxMS4yLDEuMXoiLz4gICAgPC9nPjwvc3ZnPg==)",
-        },
-        blue: {
-          value:
-            "url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMyAxMCI+ICAgIDxnPiAgICA8cGF0aCBmaWxsPSIjMDA2N0M1IiBkPSJNNCwxMGMtMC40LDAtMC44LTAuMS0xLjEtMC40TDAuNCw3LjFDMC4xLDYuOCwwLDYuNCwwLDZzMC4yLTAuOCwwLjUtMS4xQzEsNC40LDIsNC40LDIuNSw0LjlMNCw2LjRsNi40LTYgICAgQzEwLjgsMC4xLDExLjEsMCwxMS41LDBjMC40LDAsMC44LDAuMiwxLDAuNWMwLjYsMC42LDAuNSwxLjYtMC4xLDIuMXYwTDUsOS42QzQuNyw5LjksNC40LDEwLDQsMTB6IE0xMS44LDEuOUwxMS44LDEuOSAgICBDMTEuOCwxLjksMTEuOCwxLjksMTEuOCwxLjl6IE0xMS4yLDEuMUMxMS4yLDEuMSwxMS4yLDEuMSwxMS4yLDEuMUwxMS4yLDEuMXoiLz4gICAgPC9nPjwvc3ZnPg==)",
-        },
       },
     },
   },
