58c3351d2fa6a61b07de6acde9c9f8dad6d220b1
Andreas Nordahl <andnorda@gmail.com>
2021-09-28 12:24:02 +0200

    [feature] Internal header in separate package (#1268)
    * add internal css package
    
    * ds-react-internal package and move internal header
    
    * remove placeholder style!
    
    * move mdx story
    
    * oppdatert typo-styling
    
    * oppdatert lerna config
    
    * lurte inn oppdatering av scripts
    
    Co-authored-by: Ken <ken.aleksander@gmail.com>

---
+++ @navikt/internal/react/src/header/index.ts
@@ -0,0 +1,6 @@
+export { default as Header } from "./Header";
+export * from "./Header";
+export { default as HeaderTitle } from "./HeaderTitle";
+export * from "./HeaderTitle";
+export { default as HeaderUser } from "./HeaderUser";
+export * from "./HeaderUser";
