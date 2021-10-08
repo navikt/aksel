da3426b36a3195eab3cc45108f9d172b88eb3993
Andreas Nordahl <andnorda@gmail.com>
2021-10-08 09:34:47 +0200

    remove unused header button

--- @navikt/internal/react/src/header/Header.tsx
+++ @navikt/internal/react/src/header/Header.tsx
@@ -1,7 +1,6 @@
 import React, { forwardRef, HTMLAttributes } from "react";
 import HeaderTitle, { HeaderTitleType } from "./HeaderTitle";
 import HeaderUser, { HeaderUserType } from "./HeaderUser";
-import HeaderButton, { HeaderButtonType } from "./HeaderButton";
 import HeaderDropdown, { HeaderDropdownType } from "./HeaderDropdown";
 import cl from "classnames";
 
@@ -18,17 +17,15 @@ interface HeaderComponent
   > {
   Title: HeaderTitleType;
   User: HeaderUserType;
-  Button: HeaderButtonType;
   Dropdown: HeaderDropdownType;
 }
 
 const Header = forwardRef(({ className, ...rest }, ref) => (
-  <header ref={ref} className={cl("navdsi-header", className)} {...rest} />
+  <header {...rest} ref={ref} className={cl("navdsi-header", className)} />
 )) as HeaderComponent;
 
 Header.Title = HeaderTitle;
 Header.User = HeaderUser;
-Header.Button = HeaderButton;
 Header.Dropdown = HeaderDropdown;
 
 export default Header;
