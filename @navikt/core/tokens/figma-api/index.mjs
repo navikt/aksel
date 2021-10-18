ed162062d654f63aeeefffd5957cd383b8cf046a
Ken <ken.aleksander@gmail.com>
2021-10-18 10:31:03 +0200

    oppdatert index

--- @navikt/core/tokens/figma-api/index.mjs
+++ @navikt/core/tokens/figma-api/index.mjs
@@ -1,26 +1,19 @@
 import { writeFile, readFile } from "fs";
 import { getColors } from "./colors.mjs";
-import { createNewPr } from "./create-pr.mjs";
 
 const main = async () => {
   if (!process.env.FIGMA_TOKEN) {
     throw new Error("Need to add FIGMA_TOKEN in .env");
   }
-  let createPr = false;
 
   const colors = await getColors();
 
   const oldColors = await readFile("./src/colors.json", "utf-8", (e, data) => {
     if (e) {
-      createPr = true;
       return {};
     }
     return data;
   });
-
-  if (oldColors !== JSON.stringify(colors)) {
-    createPr = true;
-  }
 
   await writeFile(
     "./src/colors.json",
@@ -33,8 +26,6 @@ const main = async () => {
       console.log("Oppdaterte colors.json");
     }
   );
-
-  createPr && createNewPr(JSON.stringify(colors, null, 2));
 };
 
 try {
