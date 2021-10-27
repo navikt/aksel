7020259da92b92bcf9570214032d873ec4059523
Ken <ken.aleksander@gmail.com>
2021-10-27 21:50:02 +0200

    oppdatert ikon-nedlastning

--- @navikt/core/icons/figma-api/write-meta.ts
+++ @navikt/core/icons/figma-api/write-meta.ts
@@ -4,12 +4,11 @@ import { ComponentT } from ".";
 
 const generateMetadata = (publishedIconNodes: ComponentT[]) => {
   return publishedIconNodes
-    .map(({ name, description, created_at, updated_at, containing_frame }) => {
+    .map(({ name, description, created_at, containing_frame }) => {
       return {
         name: startCase(name).replace(/\s/g, ""),
         description,
         created_at,
-        updated_at,
         // strip emojis
         pageName: containing_frame.pageName.replace(
           /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
