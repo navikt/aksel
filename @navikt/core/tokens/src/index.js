20da2f2596dbc8d2ca8cd6cb85f53e9038924e4c
Ken <ken.aleksander@gmail.com>
2021-11-12 16:40:55 +0100

    fjernet all color-text tokens

--- @navikt/core/tokens/src/index.js
+++ @navikt/core/tokens/src/index.js
@@ -96,12 +96,6 @@ module.exports = {
         border: { value: "{navds.color.green.50.value}" },
         background: { value: "{navds.color.green.10.value}" },
       },
-      text: {
-        primary: { value: "{navds.color.gray.90.value}" },
-        inverse: { value: "{navds.color.white.value}" },
-        disabled: { value: "{navds.color.gray.60.value}" },
-        error: { value: "{navds.color.red.50.value}" },
-      },
       hover: { value: "{navds.color.gray.40.value}" },
       tag: {
         error: {
