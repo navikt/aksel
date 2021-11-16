0678780972041564508a1ac8a6601c71fca50ea9
Ken <ken.aleksander@gmail.com>
2021-11-16 15:20:33 +0100

    satt opp storybook

---
+++ @navikt/navno/react/src/product-card/stories/product-card.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ProductCard } from "..";
+
+export default {
+  title: "ds-react-navno/product-card",
+  component: ProductCard,
+};
+
+export const All = () => {
+  return <ProductCard>tekst</ProductCard>;
+};
