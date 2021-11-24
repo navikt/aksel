791500370a32c6cb1dd0b468c5df4cda44a1eb18
bdahle <bdahle@hey.com>
2021-11-24 12:25:32 +0100

    copy story from old file

--- @navikt/core/react/src/step-indicator/stories/step-indicator.stories.tsx
+++ @navikt/core/react/src/step-indicator/stories/step-indicator.stories.tsx
@@ -10,7 +10,71 @@ export const All = () => {
   return (
     <>
       <h1>StepIndicator</h1>
-      <StepIndicator></StepIndicator>
+      <StepIndicator
+        steg={[
+          { label: "Dette steget først", index: 0 },
+          { label: "Og så dette steget", aktiv: true, index: 1 },
+          { label: "Deretter må du gjøre dette", index: 2 },
+          { label: "Konklusjonen", disabled: true, index: 3 },
+        ]}
+        onChange={() => {}}
+      />
     </>
   );
 };
+
+// import React from "react";
+// import StepIndicator from "../StepIndicator";
+// import { Meta } from "@storybook/react/types-6-0";
+
+// export default {
+//   title: "nav-frontend/StepIndicator",
+//   component: StepIndicator,
+// } as Meta;
+
+// export const All = () => {
+//   return (
+//     <div style={{ display: "grid", gridAutoRows: "8rem", rowGap: "2rem" }}>
+//       <StepIndicator
+//         steg={[
+//           { label: "Dette steget først", index: 0 },
+//           { label: "Og så dette steget", aktiv: true, index: 1 },
+//           { label: "Deretter må du gjøre dette", index: 2 },
+//           { label: "Konklusjonen", disabled: true, index: 3 },
+//         ]}
+//         onChange={() => {}}
+//       />
+//       <StepIndicator
+//         visLabel
+//         steg={[
+//           { label: "Dette steget først", index: 0 },
+//           { label: "Og så dette steget", aktiv: true, index: 1 },
+//           { label: "Deretter må du gjøre dette", index: 2 },
+//           { label: "Konklusjonen", disabled: true, index: 3 },
+//         ]}
+//         onChange={() => {}}
+//       />
+//       <StepIndicator
+//         kompakt
+//         steg={[
+//           { label: "Dette steget først", index: 0 },
+//           { label: "Og så dette steget", aktiv: true, index: 1 },
+//           { label: "Deretter må du gjøre dette", index: 2 },
+//           { label: "Konklusjonen", disabled: true, index: 3 },
+//         ]}
+//         onChange={() => {}}
+//       />
+//       <StepIndicator
+//         kompakt
+//         visLabel
+//         steg={[
+//           { label: "Dette steget først", index: 0 },
+//           { label: "Og så dette steget", aktiv: true, index: 1 },
+//           { label: "Deretter må du gjøre dette", index: 2 },
+//           { label: "Konklusjonen", disabled: true, index: 3 },
+//         ]}
+//         onChange={() => {}}
+//       />
+//     </div>
+//   );
+// };
