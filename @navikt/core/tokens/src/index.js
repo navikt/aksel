f0ce29a134ffb7c2a69a4f7fe5ea1c72833b3841
Andreas Nordahl <andnorda@gmail.com>
2021-12-06 15:45:03 +0100

    fix old token refs

--- @navikt/core/tokens/src/index.js
+++ @navikt/core/tokens/src/index.js
@@ -108,9 +108,9 @@ module.exports = {
     },
     shadow: {
       focus: { value: "0 0 0 3px {navds.semantic.color.focus.value}" },
-      "focus-on-dark": { value: "0 0 0 3px {navds.color.orange.40.value}" },
+      "focus-on-dark": { value: "0 0 0 3px {navds.color.orange.400.value}" },
       popover: {
-        value: "0 2px 4px 0 {navds.global.color.gray.20.value}",
+        value: "0 2px 4px 0 {navds.global.color.gray.200.value}",
       },
       card: {
         value:
