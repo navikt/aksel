89f69250108bf2be03025ce8b2a02437c42dd493
Andreas Nordahl <andnorda@gmail.com>
2021-12-06 16:21:22 +0100

    remove old color tokens

--- @navikt/core/tokens/src/index.js
+++ @navikt/core/tokens/src/index.js
@@ -1,75 +1,13 @@
-const Color = require("color");
 const newColors = require("./colors.json");
 const newSpacing = require("./spacing.json");
 
 const baseFontSize = 16;
-
 const getFontSize = (size) => `${size / baseFontSize}rem`;
-// https://github.com/hihayk/scale/blob/69b766bba2db046d3e8cb4026ae32a32c897f9ff/src/utils.js#L44
-const mixColors = (color, step, amount, mixColor) => {
-  const saturation = Math.round(Color(color).hsl().color[1]);
-  // Setting directly to hex returns different result...
-  return Color(
-    Color(color)
-      .saturate(((step + 1) / 5) * (saturation / 100))
-      .mix(Color(mixColor), ((amount / 100) * (step + 1)) / 5)
-      .string()
-  ).hex();
-};
-
-const lightColor = (color, step) => mixColors(color, step, 100, "white");
-const darkColor = (color, step) => mixColors(color, step, 85, "black");
-
-const baseColors = {
-  blue: "#0067c5",
-  deepblue: "#005B82",
-  lightblue: "#66CBEC",
-  orange: "#FF9100",
-  green: "#06893A",
-  red: "#BA3A26",
-  purple: "#634689",
-  limegreen: "#a2ad00",
-};
-
-const white = "#ffffff";
-const darkgray = "#262626";
-const lightgray = "#F1F1F1";
-
-const gray = (n) => Color(lightgray).mix(Color(darkgray), n).hex();
 
 module.exports = {
   navds: {
     ...(newSpacing?.navds ?? {}),
     ...(newColors?.navds ?? {}),
-    color: {
-      white: { value: white },
-      darkgray: { value: darkgray },
-      gray: {
-        90: { value: gray(1) },
-        80: { value: gray(0.8) },
-        60: { value: gray(0.6667) },
-        40: { value: gray(0.4) },
-        20: { value: gray(0.196) },
-        10: { value: gray(0) },
-      },
-      ...Object.entries(baseColors).reduce(
-        (colors, [name, color]) => ({
-          ...colors,
-          [name]: {
-            90: { value: darkColor(color, 3) },
-            80: { value: darkColor(color, 2) },
-            70: { value: darkColor(color, 1) },
-            60: { value: darkColor(color, 0) },
-            50: { value: color },
-            40: { value: lightColor(color, 0) },
-            30: { value: lightColor(color, 1) },
-            20: { value: lightColor(color, 2) },
-            10: { value: lightColor(color, 3) },
-          },
-        }),
-        {}
-      ),
-    },
     font: {
       family: { value: '"Source Sans Pro", Arial, sans-serif' },
       line: {
