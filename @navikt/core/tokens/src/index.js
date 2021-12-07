1292cb6cabe90c1c1496d117dc4ddc9fe8cce81b
Ken <ken.aleksander@gmail.com>
2021-12-07 14:36:53 +0100

    la til focus inverted

--- @navikt/core/tokens/src/index.js
+++ @navikt/core/tokens/src/index.js
@@ -41,8 +41,8 @@ module.exports = {
     },
     shadow: {
       focus: { value: "0 0 0 3px {navds.semantic.color.focus.@.value}" },
-      "focus-on-dark": {
-        value: "0 0 0 3px {navds.global.color.orange.400.value}",
+      "focus-inverted": {
+        value: "0 0 0 3px {navds.semantic.color.focus.inverted.value}",
       },
       popover: {
         value: "0 2px 4px 0 {navds.global.color.gray.200.value}",
