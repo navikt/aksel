d82701a00f1e5a82e8fed725aff3a4110683397b
Andreas Nordahl <andnorda@gmail.com>
2021-12-14 09:39:02 +0100

    a11y table story for morten

--- @navikt/core/react/src/table/stories/table.stories.tsx
+++ @navikt/core/react/src/table/stories/table.stories.tsx
@@ -143,16 +143,22 @@ const SelectionTable = ({ size }) => {
 
   return (
     <Table size={size}>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>Selected</Table.HeaderCell>
-          <Table.HeaderCell>ID</Table.HeaderCell>
-          <Table.HeaderCell>Fornavn</Table.HeaderCell>
-          <Table.HeaderCell>Etternavn</Table.HeaderCell>
-          <Table.HeaderCell>Rolle</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
       <Table.Body>
+        <Table.Row>
+          <Table.DataCell>
+            <Checkbox
+              size={size}
+              checked={selectedRows.includes("all")}
+              onChange={() => toggleSelectedRow("all")}
+            >
+              Select all
+            </Checkbox>
+          </Table.DataCell>
+          <Table.HeaderCell scope="col">Name</Table.HeaderCell>
+          <Table.HeaderCell scope="col">Age</Table.HeaderCell>
+          <Table.HeaderCell scope="col">Country</Table.HeaderCell>
+          <Table.HeaderCell scope="col">Points</Table.HeaderCell>
+        </Table.Row>
         <Table.Row selected={selectedRows.includes("1")}>
           <Table.DataCell>
             <Checkbox
@@ -160,14 +166,15 @@ const SelectionTable = ({ size }) => {
               hideLabel
               checked={selectedRows.includes("1")}
               onChange={() => toggleSelectedRow("1")}
-            >
-              Selected
-            </Checkbox>
+              aria-labelledby="x_r1"
+            ></Checkbox>
           </Table.DataCell>
-          <Table.HeaderCell>1</Table.HeaderCell>
-          <Table.DataCell>Jean-Luc</Table.DataCell>
-          <Table.DataCell>Picard</Table.DataCell>
-          <Table.DataCell>Kaptein</Table.DataCell>
+          <Table.HeaderCell scope="row">
+            <span id="x_r1">Donald Smith</span>
+          </Table.HeaderCell>
+          <Table.DataCell>32</Table.DataCell>
+          <Table.DataCell>USA</Table.DataCell>
+          <Table.DataCell>38</Table.DataCell>
         </Table.Row>
         <Table.Row selected={selectedRows.includes("2")}>
           <Table.DataCell>
@@ -176,14 +183,15 @@ const SelectionTable = ({ size }) => {
               hideLabel
               checked={selectedRows.includes("2")}
               onChange={() => toggleSelectedRow("2")}
-            >
-              Selected
-            </Checkbox>
+              aria-labelledby="x_r2"
+            ></Checkbox>
           </Table.DataCell>
-          <Table.HeaderCell>2</Table.HeaderCell>
-          <Table.DataCell>William</Table.DataCell>
-          <Table.DataCell>Riker</Table.DataCell>
-          <Table.DataCell>Kommandør</Table.DataCell>
+          <Table.HeaderCell scope="row">
+            <span id="x_r2">Preben Aalborg</span>
+          </Table.HeaderCell>
+          <Table.DataCell>44</Table.DataCell>
+          <Table.DataCell>Denmark</Table.DataCell>
+          <Table.DataCell>11</Table.DataCell>
         </Table.Row>
         <Table.Row selected={selectedRows.includes("3")}>
           <Table.DataCell>
@@ -192,14 +200,15 @@ const SelectionTable = ({ size }) => {
               hideLabel
               checked={selectedRows.includes("3")}
               onChange={() => toggleSelectedRow("3")}
-            >
-              Selected
-            </Checkbox>
+              aria-labelledby="x_r3"
+            ></Checkbox>
           </Table.DataCell>
-          <Table.HeaderCell>3</Table.HeaderCell>
-          <Table.DataCell>Geordi</Table.DataCell>
-          <Table.DataCell>La Forge</Table.DataCell>
-          <Table.DataCell>Sjefsingeniør</Table.DataCell>
+          <Table.HeaderCell scope="row">
+            <span id="x_r3">Rudolph Bachenmeier</span>
+          </Table.HeaderCell>
+          <Table.DataCell>32</Table.DataCell>
+          <Table.DataCell>Germany</Table.DataCell>
+          <Table.DataCell>70</Table.DataCell>
         </Table.Row>
       </Table.Body>
     </Table>
