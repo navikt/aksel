4c54047d6b0abac4862baad8ebfedf13c09dd057
Ken <ken.aleksander@gmail.com>
2021-12-16 16:43:14 +0100

    fikset storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,10 +4,7 @@ module.exports = {
     return config;
   },
   devServer: { stats: "errors-only" },
-  stories: [
-    "../packages/**/*.stories.@(js|jsx|ts|tsx|mdx)",
-    "../@navikt/**/*.stories.@(js|jsx|ts|tsx|mdx)",
-  ],
+  stories: ["../@navikt/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
   addons: [
     "@storybook/addon-docs",
     "@storybook/addon-a11y",
