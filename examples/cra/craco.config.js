12d81f8553b2c5a051d08de38ca41ec7b0517aac
Ken <ken.aleksander@gmail.com>
2021-12-16 15:46:49 +0100

    [ci skip] Fjernet ubrukte eksempler

--- examples/cra/craco.config.js
+++
@@ -1,9 +0,0 @@
-const { ESLINT_MODES } = require("@craco/craco");
-const CracoLessPlugin = require("craco-less");
-
-module.exports = {
-  plugins: [{ plugin: CracoLessPlugin }],
-  eslint: {
-    mode: ESLINT_MODES.file,
-  },
-};
