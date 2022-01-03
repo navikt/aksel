6d5e7dd0f3599cafb0bb2d67ad0ff1fb5bd09c6b
Ken <26967723+KenAJoh@users.noreply.github.com>
2022-01-03 09:33:38 +0100

    Opprydding i Repo, flytting av gammel kode til nytt repo (#1372)
    Co-authored-by: Andreas Nordahl <andnorda@gmail.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,3 @@
-import "nav-frontend-core";
 import "@navikt/ds-css/index.css";
 import "@navikt/ds-css-internal/index.css";
 
--- packages/nav-frontend-grid/src/grid.tsx
+++
@@ -1,3 +0,0 @@
-export { default as Container } from "./container";
-export { default as Row } from "./row";
-export { default as Column } from "./column";
