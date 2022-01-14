004dfb90f1359fed61f34fdc5db529e45dc89d9d
Ken <ken.aleksander@gmail.com>
2022-01-14 09:22:31 +0100

    fikset console errors i Switch

--- @navikt/core/react/src/form/Switch.tsx
+++ @navikt/core/react/src/form/Switch.tsx
@@ -21,8 +21,8 @@ const SelectedIcon = () => (
     aria-label="Deaktiver valg"
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M4.01386 8L10.25 2L11 2.75L4.01386 9.5L1 6.5L1.75 5.75L4.01386 8Z"
       fill="currentColor"
       stroke="currentColor"
