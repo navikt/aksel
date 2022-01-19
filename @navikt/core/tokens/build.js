1aa805c33690fb01590da677efd8215fc0596d17
Ken <ken.aleksander@gmail.com>
2022-01-19 15:14:38 +0100

    json fil med formaterte tokens

--- @navikt/core/tokens/build.js
+++ @navikt/core/tokens/build.js
@@ -8,7 +8,38 @@ StyleDictionary.registerTransform({
     kebabCase([options.prefix].concat(prop.path).join(" ")),
 });
 
+StyleDictionary.registerFormat({
+  name: "referenceFormat",
+  formatter: ({ dictionary, platform, file, options }) => {
+    const newTokens = dictionary.allTokens
+      .map((token, i) => {
+        let value = JSON.stringify(token.value);
+        // new option added to decide whether or not to output references
+        if (options.outputReferences) {
+          console.log(dictionary.getReferences(token.original.value));
+          // the `dictionary` object now has `usesReference()` and
+          // `getReferences()` methods. `usesReference()` will return true if
+          // the value has a reference in it. `getReferences()` will return
+          // an array of references to the whole tokens so that you can access
+          // their names or any other attributes.
+          if (dictionary.usesReference(token.original.value)) {
+            const refs = dictionary.getReferences(token.original.value);
+            refs.forEach((ref) => {
+              value = value.replace(ref.value, `var(${ref.path.join("-")})`);
+            });
+          }
+        }
+
+        return `"${kebabCase(token.name).replace("navds-", "")}": ${value}${
+          i === dictionary.allTokens.length - 1 ? "" : ","
+        }`;
+      })
+      .join(`\n\t`);
+    return `{\n\t${newTokens}\n}`;
+  },
+});
+
 const StyleDictionaryExtended = StyleDictionary.extend(
-  __dirname + "/config.json"
+  __dirname + "/config.js"
 );
 StyleDictionaryExtended.buildAllPlatforms();
---
+++ @navikt/core/tokens/config.js
@@ -0,0 +1,65 @@
+module.exports = {
+  source: ["src/index.js", "src/**/*.json"],
+  platforms: {
+    scss: {
+      transformGroup: "scss",
+      buildPath: "dist/",
+      files: [
+        {
+          destination: "tokens.scss",
+          format: "scss/variables",
+        },
+      ],
+    },
+    less: {
+      transformGroup: "less",
+      buildPath: "dist/",
+      files: [
+        {
+          destination: "tokens.less",
+          format: "less/variables",
+        },
+      ],
+    },
+    css: {
+      transformGroup: "css",
+      buildPath: "dist/",
+      files: [
+        {
+          destination: "tokens.css",
+          format: "css/variables",
+          options: {
+            outputReferences: true,
+          },
+        },
+      ],
+    },
+    js: {
+      transformGroup: "js",
+      buildPath: "dist/",
+      files: [
+        {
+          destination: "tokens.js",
+          format: "javascript/es6",
+        },
+        {
+          destination: "tokens-cjs.js",
+          format: "javascript/module-flat",
+        },
+      ],
+    },
+    ts: {
+      transformGroup: "js",
+      buildPath: "tailwind/src/",
+      files: [
+        {
+          destination: "tokens.json",
+          format: "referenceFormat",
+          options: {
+            outputReferences: true,
+          },
+        },
+      ],
+    },
+  },
+};
