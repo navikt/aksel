d822991b66beba4e874b091a73d30ca177385e5a
Ken <ken.aleksander@gmail.com>
2022-01-21 13:58:07 +0100

    ferdig tailwind config

--- @navikt/core/tokens/tailwind/src/index.ts
+++ @navikt/core/tokens/tailwind/src/index.ts
@@ -1,4 +1,20 @@
-import Colors from "./colors";
+import { writeFileSync } from "fs";
 import * as tokens from "./tokens.json";
+import Reducer from "./reducer";
 
-console.log(Colors(tokens));
+const config = {
+  theme: {
+    colors: Reducer(tokens, ["semantic-color", "global-color"]),
+    spacing: Reducer(tokens, ["spacing"]),
+    zIndex: Reducer(tokens, ["z-index"]),
+    boxShadow: Reducer(tokens, ["shadow"]),
+    fontWeight: Reducer(tokens, ["font-weight"]),
+    fontSize: Reducer(tokens, ["font-size"]),
+    lineHeight: Reducer(tokens, ["font-line-height"]),
+    fontFamily: Reducer(tokens, ["font-family"]),
+  },
+};
+
+const outputString = `module.exports = ${JSON.stringify(config, null, 2)};`;
+
+writeFileSync("./tailwind/tailwind.config.js", outputString);
