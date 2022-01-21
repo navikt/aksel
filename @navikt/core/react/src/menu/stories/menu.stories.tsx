7226b57e159dcf9c977ad6988f0fcc6628bef052
Ken <ken.aleksander@gmail.com>
2022-01-21 14:38:51 +0100

    oppdatert stories, backgorund-color på menu

--- @navikt/core/react/src/menu/stories/menu.stories.tsx
+++ @navikt/core/react/src/menu/stories/menu.stories.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import cl from "classnames";
 import { Menu } from "../index";
 import { NavLink, HashRouter as Router } from "react-router-dom";
-import { Button } from "../..";
 import { MenuItemProps } from "../MenuItem";
 
 export default {
@@ -29,54 +28,41 @@ export const All = () => (
       <Menu.Item href="#luctus">Luctus</Menu.Item>
       <Menu.Item href="#justo">Justo</Menu.Item>
       <Menu.Item href="#pariatur">Pariatur</Menu.Item>
-      <Button>Test</Button>
     </Menu>
 
-    <h2>With Drawers</h2>
+    <h2>With Collapse</h2>
     <Menu>
       <Menu.Item href="#leo">Leo</Menu.Item>
-      <Menu.Drawer title="Proin">
+      <Menu.Collapse title="Proin">
         <Menu.Item href="#nulla" active>
           Nulla
         </Menu.Item>
         <Menu.Item href="#luctus">Luctus</Menu.Item>
-      </Menu.Drawer>
-      <Menu.Drawer title="Accumsan">
+      </Menu.Collapse>
+      <Menu.Collapse title="Accumsan">
         <Menu.Item href="#justo" aria-current="page">
           Justo
         </Menu.Item>
         <Menu.Item href="#pariatur" active aria-current="page">
           Pariatur
         </Menu.Item>
-        <Menu.Drawer title="Proin">
+        <Menu.Collapse title="Proin">
           <Menu.Item href="#nulla" active>
             Nulla
           </Menu.Item>
-          <Menu.Drawer title="Proin">
+          <Menu.Collapse title="Proin">
             <Menu.Item href="#nulla" active>
               Nulla
             </Menu.Item>
             <Menu.Item href="#luctus">Luctus</Menu.Item>
-          </Menu.Drawer>
+          </Menu.Collapse>
           <Menu.Item href="#luctus">Luctus</Menu.Item>
-        </Menu.Drawer>
-      </Menu.Drawer>
+        </Menu.Collapse>
+      </Menu.Collapse>
       <Menu.Item href="#justo" active>
         Justo
       </Menu.Item>
       <Menu.Item href="#pariatur">Pariatur</Menu.Item>
-    </Menu>
-    <h2>With links</h2>
-    <Menu>
-      <Menu.Link href="#leo">Leo</Menu.Link>
-
-      <Menu.Link href="#justo">Justo</Menu.Link>
-      <Menu.Link href="#pariatur">Pariatur</Menu.Link>
-      <Menu.Link href="#nulla">Nulla</Menu.Link>
-      <Menu.Link href="#luctus">Luctus</Menu.Link>
-      <Menu.Link href="#justo">Justo</Menu.Link>
-      <Menu.Link href="#pariatur">Pariatur</Menu.Link>
-      <Button>Test</Button>
     </Menu>
   </div>
 );
@@ -109,29 +95,29 @@ export const ReactRouter = () => {
 
   return (
     <Menu>
-      <Menu.Drawer title="Nivå 1 - 3 rems høy">
+      <Menu.Collapse title="Nivå 1 - 3 rems høy">
         <MenuItemLink to="/1">Nivå 2 innrykk 2 rems</MenuItemLink>
         <MenuItemLink to="/2">Subtitle 3</MenuItemLink>
-      </Menu.Drawer>
-      <Menu.Drawer title="Skjema og søknad">
-        <Menu.Drawer title="Satser">
+      </Menu.Collapse>
+      <Menu.Collapse title="Skjema og søknad">
+        <Menu.Collapse title="Satser">
           <MenuItemLink to="/3">
             Nivå 3 - 3 rems innrykk Har du rett til dagpenger når du mottar
             annen økonomisk støtte fra NAV?
           </MenuItemLink>
-        </Menu.Drawer>
+        </Menu.Collapse>
         <MenuItemLink to="/4">Subtitle 2</MenuItemLink>
         <MenuItemLink to="/5">Subtitle 3</MenuItemLink>
-      </Menu.Drawer>
-      <Menu.Drawer title="Sakbehandlingstider">
-        <Menu.Drawer title="Subtitle 4 Lang tittel på andre nivå lorem ipsum">
+      </Menu.Collapse>
+      <Menu.Collapse title="Sakbehandlingstider">
+        <Menu.Collapse title="Subtitle 4 Lang tittel på andre nivå lorem ipsum">
           <MenuItemLink to="/6">Subtitle 5</MenuItemLink>
-        </Menu.Drawer>
-      </Menu.Drawer>
+        </Menu.Collapse>
+      </Menu.Collapse>
       <MenuItemLink to="/7">Internasjonalt</MenuItemLink>
-      <Menu.Drawer title="Meld fra om endringer lang tittel på første nivå">
+      <Menu.Collapse title="Meld fra om endringer lang tittel på første nivå">
         <MenuItemLink to="/8">Subtitle 6</MenuItemLink>
-      </Menu.Drawer>
+      </Menu.Collapse>
       <MenuItemLink to="/9">Nivå 1</MenuItemLink>
     </Menu>
   );
