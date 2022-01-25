265322c5b1399f5361cda0777a66c1ce1dcbf9d5
Bjørn Harbo Dahle <bdahle@hey.com>
2022-01-25 13:56:49 +0100

    implementert ThemedPageHeader, uferdig

--- @navikt/navno/react/src/themed-page-header/ThemedPageHeader.tsx
+++ @navikt/navno/react/src/themed-page-header/ThemedPageHeader.tsx
@@ -1,6 +1,36 @@
 import React from "react";
 
-const ThemedPageHeader = () => {
-  return <div>Test</div>;
+import { Heading, BodyLong } from "@navikt/ds-react";
+
+type PageTypes = "situation" | "product" | "guide";
+interface ThemedPageHeaderProps {
+  title: string;
+  subTitle: string;
+  illustration: any; //change type
+  pageType: PageTypes;
+  modifiedTime: string;
+}
+const ThemedPageHeader = ({
+  title,
+  subTitle,
+  illustration,
+  pageType,
+  modifiedTime,
+}: ThemedPageHeaderProps) => {
+  return (
+    <header className={`themed-page-header ${pageType}`}>
+      {illustration}
+      <div className="themed-page-header__text">
+        <Heading size="2xlarge">{title}</Heading>
+        <BodyLong size="small" className="themed-page-header__label">
+          {subTitle.toUpperCase()}
+          <span aria-hidden="true" className={"page-modified-info divider"}>
+            |
+          </span>
+          <span className={"page-modified-info"}>{modifiedTime}</span>
+        </BodyLong>
+      </div>
+    </header>
+  );
 };
 export default ThemedPageHeader;
--- @navikt/navno/react/src/themed-page-header/stories/themed-page-header.stories.tsx
+++ @navikt/navno/react/src/themed-page-header/stories/themed-page-header.stories.tsx
@@ -1,10 +1,35 @@
 import React from "react";
 import { ThemedPageHeader } from "..";
+import { SocialAid, Money } from "@navikt/ds-icons";
 
 export default {
   title: "ds-react-navno/themed-page-header",
 };
 
-export const CopyLinkStory = () => {
-  return <ThemedPageHeader />;
+export const ThemedPageHeaderStory = () => {
+  return (
+    <>
+      <h1>Themed Page Header</h1>
+      <h2>Situation</h2>
+      <ThemedPageHeader
+        title="Er helt eller delvis alene med barn"
+        subTitle="DETTE KAN DU HA RETT TIL"
+        illustration={
+          <SocialAid className="themed-page-header__illustration" /> //change
+        }
+        pageType="situation"
+        modifiedTime="Oppdatert 15. desember 2021"
+      />
+      <h2>Product</h2>
+      <ThemedPageHeader
+        title="Arbeidsavklaringspenger (AAP)"
+        subTitle="PENGESTØTTE OG OPPFØLGING"
+        illustration={
+          <Money className="themed-page-header__illustration" /> //change
+        }
+        pageType="product"
+        modifiedTime="Oppdatert 20. januar 2022"
+      />
+    </>
+  );
 };
