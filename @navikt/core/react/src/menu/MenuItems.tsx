79b2fe56d5ce53b983d942b7f31e486301657e91
Andreas Nordahl <andnorda@gmail.com>
2022-01-25 15:13:13 +0100

    tiny refactor

--- @navikt/core/react/src/menu/MenuItems.tsx
+++ @navikt/core/react/src/menu/MenuItems.tsx
@@ -8,16 +8,14 @@ const MenuItems = ({
 }: {
   children: React.ReactNode;
   className?: string;
-}) => {
-  return (
-    <ul className={cl("navds-menu__list", className)} {...rest}>
-      {React.Children.toArray(children).map((child, i) => (
-        <li key={i} className="navds-menu__list-item">
-          {child}
-        </li>
-      ))}
-    </ul>
-  );
-};
+}) => (
+  <ul {...rest} className={cl("navds-menu__list", className)}>
+    {React.Children.toArray(children).map((child, i) => (
+      <li key={i} className="navds-menu__list-item">
+        {child}
+      </li>
+    ))}
+  </ul>
+);
 
 export default MenuItems;
