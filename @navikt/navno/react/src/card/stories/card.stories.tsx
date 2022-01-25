8f0cefe4fba2af21a9e9e32908818601bb759816
Terje Karlsen <terjeofnorway@gmail.com>
2022-01-25 15:33:48 +0100

    improves documentation for card usage

--- @navikt/navno/react/src/card/stories/card.stories.tsx
+++ @navikt/navno/react/src/card/stories/card.stories.tsx
@@ -1,5 +1,8 @@
+import { BodyLong, Heading } from "@navikt/ds-react";
 import React from "react";
 import * as Cards from "../generated-cards";
+
+import allCards from "../cards.json";
 
 export default {
   title: "ds-react-navno/Cards",
@@ -20,13 +23,62 @@ export default {
 };
 
 const Template = ({ card, customText, ...args }) => {
-  const CardName = Cards[card];
+  const CardComponent = Cards[card];
 
   return (
     <>
-      <CardName {...args} size="large" customText={customText} />
-      <CardName {...args} size="mini" customText={customText} />
-      <CardName {...args} size="micro" customText={customText} />
+      <Heading size="2xlarge">Cards</Heading>
+      <BodyLong spacing>
+        Below are the three available card sizes that you may set using{" "}
+        <i>size</i> prop. Use the Controls to view a particular card and its
+        illustration.
+      </BodyLong>
+      <Heading size="xlarge" spacing>
+        Usage
+      </Heading>
+      <BodyLong
+        spacing
+      >{`import { ${card} } from '@navikt/ds-navno'`}</BodyLong>
+      <Heading size="xlarge" spacing>
+        Large
+      </Heading>
+      <CardComponent {...args} size="large" customText={customText} />
+      <Heading size="xlarge" spacing>
+        Mini
+      </Heading>
+      <CardComponent {...args} size="mini" customText={customText} />
+      <Heading size="xlarge" spacing>
+        Micro
+      </Heading>
+      <CardComponent {...args} size="micro" customText={customText} />
+      <Heading size="xlarge" spacing>
+        All cards
+      </Heading>
+      <BodyLong spacing>
+        Below is a list of all cards available at the moment. If you are missing
+        a card or you believe that a title is wrong, please contact us on{" "}
+        <i>#team-personbruker</i> on Slack.
+      </BodyLong>
+      <Heading size="large" spacing>
+        Product cards
+      </Heading>
+      <ul>
+        {allCards
+          .filter(({ type }) => type === "product")
+          .map((card) => (
+            <li>{card.componentName}Card</li>
+          ))}
+      </ul>
+      <Heading size="large" spacing>
+        Situation cards
+      </Heading>
+      <ul>
+        {allCards
+          .filter(({ type }) => type === "situation")
+          .map((card) => (
+            <li>{card.componentName}Card</li>
+          ))}
+      </ul>
     </>
   );
 };
