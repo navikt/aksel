983711f007202baa1fd7f6f8744d247ed2219183
Bjørn Harbo Dahle <bdahle@hey.com>
2022-01-27 09:40:55 +0100

    skjule illustrasjon på mobil

--- @navikt/navno/react/src/themed-page-header/ThemedPageHeader.tsx
+++ @navikt/navno/react/src/themed-page-header/ThemedPageHeader.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Heading, BodyLong } from "@navikt/ds-react";
+import { Heading, BodyShort } from "@navikt/ds-react";
 import Animation from "../animation/Animation";
 
 type PageTypes = "situation" | "product" | "guide";
@@ -28,13 +28,17 @@ const ThemedPageHeader = ({
       />
       <div className="themed-page-header__text">
         <Heading size="2xlarge">{title}</Heading>
-        <BodyLong size="small" className="themed-page-header__label">
-          {subTitle.toUpperCase()}
-          <span aria-hidden="true" className={"page-modified-info divider"}>
+        <div className="themed-page-header__tagline-wrapper">
+          <BodyShort size="small" className="themed-page-header__tagline-label">
+            {subTitle.toUpperCase()}
+          </BodyShort>
+          <span aria-hidden="true" className={"page-modified-info__divider"}>
             |
           </span>
-          <span className={"page-modified-info"}>{modifiedTime}</span>
-        </BodyLong>
+          <BodyShort className="themed-page-header__modified-label">
+            <span className={"page-modified-info"}>{modifiedTime}</span>
+          </BodyShort>
+        </div>
       </div>
     </header>
   );
