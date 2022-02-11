a73dd98ea77c24bee56212d41cd6408d44b9e210
Ken <ken.aleksander@gmail.com>
2022-02-11 12:58:21 +0100

    bedre story

--- @navikt/core/react/src/form/search-field/stories/search-field.stories.tsx
+++ @navikt/core/react/src/form/search-field/stories/search-field.stories.tsx
@@ -13,27 +13,27 @@ export const All = () => {
     <div style={{ maxWidth: 300 }}>
       <h1>SearchField</h1>
       <div>
-        <SearchField label="Mollit eiusmod" onSearch={console.log}>
+        <SearchField label="Søk alle sider om X og Y" onSearch={console.log}>
           <SearchField.Button />
         </SearchField>
         <br />
-        <SearchField label="Mollit eiusmod" onSearch={console.log}>
+        <SearchField label="Søk alle sider om X og Y" onSearch={console.log}>
           <SearchField.Button variant="primary" />
         </SearchField>
       </div>
 
       <h2>SearchField small</h2>
       <SearchField
-        label="Mollit eiusmod"
-        description="Ea cupidatat eu sunt commodo"
+        label="Søk alle sider om X og Y"
+        description="Beskrivelse av søket"
         size="small"
       >
         <SearchField.Button />
       </SearchField>
       <br />
       <SearchField
-        label="Mollit eiusmod"
-        description="Ea cupidatat eu sunt commodo"
+        label="Søk alle sider om X og Y"
+        description="Beskrivelse av søket"
         size="small"
         hideLabel
       >
@@ -41,21 +41,21 @@ export const All = () => {
       </SearchField>
 
       <h2>Med knappe-tekst</h2>
-      <SearchField label="Mollit eiusmod" hideLabel={false}>
+      <SearchField label="Søk alle sider om X og Y" hideLabel={false}>
         <SearchField.Button>Søk</SearchField.Button>
       </SearchField>
-      <SearchField label="Mollit eiusmod" hideLabel={false}>
+      <SearchField label="Søk alle sider om X og Y" hideLabel={false}>
         <SearchField.Button variant="primary">Søk</SearchField.Button>
       </SearchField>
       <h2>Hidelabel false</h2>
-      <SearchField label="Mollit eiusmod" hideLabel={false}>
+      <SearchField label="Søk alle sider om X og Y" hideLabel={false}>
         <SearchField.Button />
       </SearchField>
       <h2>Controlled state </h2>
       <SearchField
         value={value}
-        label="Mollit eiusmod"
-        description="Ea cupidatat eu sunt commodo"
+        label="Søk alle sider om X og Y"
+        description="Beskrivelse av søket"
         onChange={(e) => setValue(e)}
         onClear={() => setValue("")}
       >
@@ -64,8 +64,8 @@ export const All = () => {
       <h2>No clear button</h2>
       <SearchField
         hideLabel
-        label="Mollit eiusmod"
-        description="Ea cupidatat eu sunt commodo"
+        label="Søk alle sider om X og Y"
+        description="Beskrivelse av søket"
         clearButton={false}
       >
         <SearchField.Button />
