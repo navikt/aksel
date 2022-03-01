ad191179ab82d623b78cef5dde54a8e660250c45
Andreas Nordahl <andnorda@gmail.com>
2022-03-01 09:23:54 +0100

    [fix] Table sort (#1445)
    * story baseline
    
    * pagination first take
    
    * sort first take
    
    * fix sort logic
    
    * sort styling
    
    * fix widths
    
    * scope row
    
    * steps
    
    * pagination styles
    
    * sortable birth year
    
    * simplify comparator
    
    * async table story
    
    * HeaderCell allows sorting
    
    * remove redundant story. fix some styling
    
    * fix chromatic
    
    * refactor column header
    
    * overflow auto
    
    * refactor pagination
    
    * fix pagination steps test
    
    * separate pagination comp
    
    * xsmall button in table
    
    * fikset type-error i ds-react-internal
    
    * Update @navikt/core/css/button.css
    
    Co-authored-by: Ken <26967723+KenAJoh@users.noreply.github.com>
    
    * Update @navikt/core/css/pagination.css
    
    Co-authored-by: Ken <26967723+KenAJoh@users.noreply.github.com>
    
    * Update @navikt/core/css/pagination.css
    
    Co-authored-by: Ken <26967723+KenAJoh@users.noreply.github.com>
    
    * Update @navikt/core/css/table.css
    
    Co-authored-by: Ken <26967723+KenAJoh@users.noreply.github.com>
    
    * Update @navikt/core/react/src/pagination/Pagination.tsx
    
    Co-authored-by: Ken <26967723+KenAJoh@users.noreply.github.com>
    
    * Update @navikt/core/react/src/pagination/Pagination.tsx
    
    Co-authored-by: Ken <26967723+KenAJoh@users.noreply.github.com>
    
    * Update @navikt/core/react/src/table/ColumnHeader.tsx
    
    Co-authored-by: Ken <26967723+KenAJoh@users.noreply.github.com>
    
    * move storybook static conf
    
    * sort arrow fix
    
    * new, adjustable step logic
    
    * fix ellipsis key bug
    
    * pagination styling
    
    * some a11y things
    
    * lint
    
    * some sizing
    
    * next prev focus style
    
    * add pagination component and styles
    
    * pagination css custom props
    
    * 1px #fff linje mellom bg og shadow
    
    * fix page logic on story
    
    * user defined sort change logic
    
    * fix table padding
    
    * fix focus bug
    
    * fix tilbake title bug
    
    * digihot table story
    
    * no active style, selected blue
    
    * yarn.lock
    
    * remove duplicate pagination stories
    
    * fix storybook
    
    * bump
    
    * table cell alignment
    
    * Update @navikt/core/react/src/table/Table.tsx
    
    Co-authored-by: Ken <26967723+KenAJoh@users.noreply.github.com>
    
    * Update @navikt/core/react/src/table/stories/table-async.stories.tsx
    
    Co-authored-by: Ken <26967723+KenAJoh@users.noreply.github.com>
    
    * Update @navikt/core/css/table.css
    
    Co-authored-by: Ken <26967723+KenAJoh@users.noreply.github.com>
    
    Co-authored-by: Ken <ken.aleksander@gmail.com>
    Co-authored-by: Ken <26967723+KenAJoh@users.noreply.github.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,10 @@
 import "@navikt/ds-css/index.css";
 import "@navikt/ds-css-internal/index.css";
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
+initialize();
+
+export const decorators = [mswDecorator];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
