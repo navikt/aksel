78fdb2051dc019e347b6b5d264bf82d98cf04bf6
Ken <ken.aleksander@gmail.com>
2022-03-16 11:19:51 +0100

    fikset config

--- @navikt/core/icons/template.js
+++ @navikt/core/icons/template.js
@@ -35,7 +35,7 @@ function defaultTemplate(
   ${imports}
   ${interfaces}
   const ${componentName} = React.forwardRef((${props}) => {
-    let titleId = useId(_titleId);
+    let titleId: string | undefined = useId(_titleId);
     titleId = title ? titleId : undefined;
     return ${jsx};
   });
