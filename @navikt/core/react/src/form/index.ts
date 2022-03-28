2fd24229c0abd1706cb407cf0a6a2036f380329f
Andreas Nordahl <andnorda@gmail.com>
2022-03-28 17:27:59 +0200

    purge suggest field

--- @navikt/core/react/src/form/index.ts
+++ @navikt/core/react/src/form/index.ts
@@ -11,7 +11,6 @@ export {
 } from "./Fieldset";
 export { Radio, RadioGroup, RadioGroupProps, RadioProps } from "./radio";
 export { Search, SearchProps, SearchClearEvent } from "./search";
-export { SuggestField, SuggestFieldProps } from "./suggest-field";
 export { default as Select, SelectProps } from "./Select";
 export { default as Switch, SwitchProps } from "./Switch";
 export { ErrorSummary, ErrorSummaryProps } from "./error-summary";
--- @navikt/core/react/src/form/suggest-field/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as SuggestField, SuggestFieldProps } from "./SuggestField";
