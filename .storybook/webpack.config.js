686e3202bb8ca1a4b76e4b3453255faf3d696dd3
Ken <ken.aleksander@gmail.com>
2022-04-01 11:57:39 +0200

    undo storybook endring

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,13 +1,13 @@
 const path = require("path");
 
 module.exports = (baseConfig) => {
-  /* baseConfig.config.module.rules.push({
+  baseConfig.config.module.rules.push({
     test: /\.css$/,
     use: ["style-loader", "css-loader"],
     include: [
       path.resolve(__dirname, "../node_modules/@navikt/core/css/index.css"),
     ],
-  }); */
+  });
   baseConfig.config.resolve.alias = {
     ...baseConfig.config.resolve.alias,
     "./normalize.css": path.resolve(
