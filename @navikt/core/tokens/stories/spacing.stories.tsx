49c5e8f284f5d259efcf112aac95d97b278d79f6
Ken <26967723+KenAJoh@users.noreply.github.com>
2022-04-04 16:03:55 +0200

    Tokens stories (#1484)
    * tokens stories
    
    * Update @navikt/core/tokens/stories/spacing.stories.tsx
    
    Co-authored-by: Andreas Nordahl <andnorda@gmail.com>
    
    Co-authored-by: Andreas Nordahl <andnorda@gmail.com>

---
+++ @navikt/core/tokens/stories/spacing.stories.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Meta } from "@storybook/react/types-6-0";
+import * as tokens from "../dist/tokens.js";
+
+export default {
+  title: "ds-tokens/Spacing",
+} as Meta;
+
+export const Spacing = () => {
+  const allSpacing: { [key: string]: string } = Object.entries(tokens).reduce(
+    (old, [key, val]) =>
+      key.startsWith("NavdsSpacing") ? { ...old, [key]: val } : { ...old },
+    {}
+  );
+
+  const sortObject = Object.fromEntries(
+    Object.entries(allSpacing).sort(
+      (a, b) =>
+        Number(a[0].replace("NavdsSpacing", "")) -
+        Number(b[0].replace("NavdsSpacing", ""))
+    )
+  );
+
+  return (
+    <div className="colgap">
+      {Object.entries(sortObject).map(([key, val]) => (
+        <div>
+          {key}
+          <div
+            key={key}
+            style={{
+              width: val,
+              height: 40,
+              backgroundColor: "thistle",
+              display: "flex",
+              alignItems: "center",
+            }}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
