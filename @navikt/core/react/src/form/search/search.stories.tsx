5714394fa5673051ef5a9e55b63b380bf362e4c9
Ken <ken.aleksander@gmail.com>
2022-04-05 12:00:48 +0200

    rename story

--- @navikt/core/react/src/form/search/search.stories.tsx
+++ @navikt/core/react/src/form/search/search.stories.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 
 import { Search } from "../index";
 export default {
-  title: "ds-react/form/search",
+  title: "ds-react/Form/Search",
   component: Search,
   argTypes: {
     clearButton: {
