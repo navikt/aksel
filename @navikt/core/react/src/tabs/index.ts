ee356ff94095ab6d893d481199601a736aec2bb1
Ken <ken.aleksander@gmail.com>
2022-04-27 10:47:02 +0200

    fikset tabs export

--- @navikt/core/react/src/tabs/index.ts
+++ @navikt/core/react/src/tabs/index.ts
@@ -1,4 +1,4 @@
 export { TabProps } from "./Tab";
-export { ListProps } from "./TabList";
-export { PanelProps } from "./TabPanel";
+export { TabListProps } from "./TabList";
+export { TabPanelProps } from "./TabPanel";
 export { default as Tabs, TabsProps } from "./Tabs";
