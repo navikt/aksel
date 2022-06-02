da2d1012a120b09e70a7955b2974ded8e7bc95d2
Ken <ken.aleksander@gmail.com>
2022-06-02 15:03:54 +0200

    fjernet msw

---
+++ .storybook/test-runner.js
@@ -0,0 +1,15 @@
+const { injectAxe, checkA11y } = require("axe-playwright");
+
+module.exports = {
+  async preRender(page, context) {
+    await injectAxe(page);
+  },
+  async postRender(page, context) {
+    await checkA11y(page, "#root", {
+      detailedReport: true,
+      detailedReportOptions: {
+        html: true,
+      },
+    });
+  },
+};
