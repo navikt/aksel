a009536b79e7b8e90e4688695c6dc97b626dd5b0
Ken <ken.aleksander@gmail.com>
2022-06-13 15:52:55 +0200

    linkpanel stories

---
+++ @navikt/core/react/src/link-panel/link-panel.stories.tsx
@@ -0,0 +1,57 @@
+import { Meta } from "@storybook/react/types-6-0";
+import React from "react";
+import { LinkPanel } from "..";
+
+export default {
+  title: "ds-react/LinkPanel",
+  component: LinkPanel,
+  argTypes: {
+    border: {
+      control: {
+        type: "boolean",
+      },
+    },
+  },
+} as Meta;
+
+export const Default = (props: any) => {
+  return (
+    <LinkPanel href="#" border={props?.border}>
+      <LinkPanel.Title>
+        Consectetur eu duis aliqua eu irure fugiat fugiat eu.
+      </LinkPanel.Title>
+      {props.description && (
+        <LinkPanel.Description>
+          Aliqua id aliquip Lorem esse
+        </LinkPanel.Description>
+      )}
+    </LinkPanel>
+  );
+};
+
+Default.args = {
+  description: false,
+};
+
+export const Description = () => {
+  return (
+    <LinkPanel href="#">
+      <LinkPanel.Title>
+        Consectetur eu duis aliqua eu irure fugiat fugiat eu.
+      </LinkPanel.Title>
+      <LinkPanel.Description>
+        Aliqua id aliquip Lorem esse
+      </LinkPanel.Description>
+    </LinkPanel>
+  );
+};
+
+export const NoBorder = () => {
+  return (
+    <LinkPanel href="#" border={false}>
+      <LinkPanel.Title>
+        Consectetur eu duis aliqua eu irure fugiat fugiat eu.
+      </LinkPanel.Title>
+    </LinkPanel>
+  );
+};
