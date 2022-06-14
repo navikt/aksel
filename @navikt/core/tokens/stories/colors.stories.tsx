25dbcce053a3ea2b2931319eb4935ddc43197455
Ken <26967723+KenAJoh@users.noreply.github.com>
2022-06-14 13:21:50 +0200

    Flere storybook oppdateringer (#1561)

--- @navikt/core/tokens/stories/colors.stories.tsx
+++ @navikt/core/tokens/stories/colors.stories.tsx
@@ -6,27 +6,72 @@ export default {
   title: "ds-tokens/Colors",
 } as Meta;
 
-export const Global = () => {
-  const globalColors: { [key: string]: string } = Object.entries(tokens).reduce(
+const globalColor = (
+  color:
+    | "gray"
+    | "blue"
+    | "red"
+    | "orange"
+    | "limegreen"
+    | "deepblue"
+    | "green"
+    | "lightblue"
+    | "purple"
+): { [key: string]: string } => {
+  return Object.entries(tokens).reduce(
     (old, [key, val]) =>
-      key.includes("GlobalColor") ? { ...old, [key]: val } : { ...old },
+      key.includes("GlobalColor") && key.toLowerCase().includes(`color${color}`)
+        ? { ...old, [key]: val }
+        : { ...old },
     {}
-  );
-
-  return (
-    <div className="colgap">
-      {Object.entries(globalColors).map(([key, val]) => (
-        <div key={key}>
-          <span>{key}</span>
-          <div style={{ background: val, width: 600, height: 40 }} />
-        </div>
-      ))}
-    </div>
   );
 };
 
+const RenderGlobal = ({ color }) => (
+  <div>
+    {Object.entries(globalColor(color))
+      .sort((a, b) => (a[0].endsWith("50") ? -1 : 1))
+      .map(([key, val]) => (
+        <div
+          style={{
+            background: val,
+            width: "50rem",
+            height: "5rem",
+            position: "relative",
+          }}
+        >
+          <div
+            style={{
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              background: "rgba(39,39,39,0.4)",
+              width: "fit-content",
+              color: "white",
+              padding: 2,
+              fontSize: 14,
+            }}
+          >
+            {`${key}: ${val}`}
+          </div>
+        </div>
+      ))}
+  </div>
+);
+export const GlobalGray = () => <RenderGlobal color="gray" />;
+export const GlobalBlue = () => <RenderGlobal color="blue" />;
+export const GlobalDeepBlue = () => <RenderGlobal color="deepblue" />;
+export const GlobalLightBlue = () => <RenderGlobal color="lightblue" />;
+export const GlobalRed = () => <RenderGlobal color="red" />;
+export const GlobalOrange = () => <RenderGlobal color="orange" />;
+export const GlobalGreen = () => <RenderGlobal color="green" />;
+export const GlobalLimeGreen = () => <RenderGlobal color="limegreen" />;
+export const GlobalPurple = () => <RenderGlobal color="purple" />;
+
 export const Semantic = () => {
-  const globalColors: { [key: string]: string } = Object.entries(tokens).reduce(
+  const semanticColors: { [key: string]: string } = Object.entries(
+    tokens
+  ).reduce(
     (old, [key, val]) =>
       key.includes("SemanticColor") ? { ...old, [key]: val } : { ...old },
     {}
@@ -34,10 +79,33 @@ export const Semantic = () => {
 
   return (
     <div className="colgap">
-      {Object.entries(globalColors).map(([key, val]) => (
-        <div key={key}>
-          <span>{key}</span>
-          <div style={{ background: val, width: 600, height: 40 }} />
+      <p>
+        Semantiske tokens vil oppdateres på sikt. Vi anbefaler å holde seg til
+        globale tokens for nå.
+      </p>
+      {Object.entries(semanticColors).map(([key, val]) => (
+        <div
+          style={{
+            background: val,
+            width: "50rem",
+            height: "5rem",
+            position: "relative",
+          }}
+        >
+          <div
+            style={{
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              background: "rgba(39,39,39,0.4)",
+              width: "fit-content",
+              color: "white",
+              padding: 2,
+              fontSize: 14,
+            }}
+          >
+            {`${key}: ${val}`}
+          </div>
         </div>
       ))}
     </div>
