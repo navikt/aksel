99498b118599ffb28e9293fe79ec5f9c97c310f6
Ken <ken.aleksander@gmail.com>
2022-06-20 20:45:56 +0200

    første test

---
+++ @navikt/codemod/transforms/__testfixtures__/v1-chat/test.input.js
@@ -0,0 +1,3 @@
+export const Page = () => {
+  return <div>123</div>;
+};
---
+++ @navikt/codemod/transforms/__tests__/v1-chat.ts
@@ -0,0 +1,8 @@
+jest.autoMockOff();
+const defineTest = require("jscodeshift/dist/testUtils").defineTest;
+
+const fixtures = ["test"];
+
+for (const fixture of fixtures) {
+  defineTest(__dirname, "v1-chat", null, `v1-chat/${fixture}`);
+}
