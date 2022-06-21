2e3b86071616bad2b41634ab40b830618d4e95e5
Ken <ken.aleksander@gmail.com>
2022-06-21 10:42:17 +0200

    codemod build-config

--- @navikt/codemod/jest.config.js
+++ @navikt/codemod/jest.config.js
@@ -1,7 +1,7 @@
 const customJestConfig = {
   testMatch: ["**/__tests__/**/*.js"],
   verbose: true,
-  rootDir: "./transforms",
+  rootDir: "./dist",
 };
 
 // createJestConfig is exported in this way to ensure that next/jest can load the Next.js config which is async
