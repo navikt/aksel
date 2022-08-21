301902afbf9102fea3416755524fb1bc23ba415e
Ken <ken.aleksander@gmail.com>
2022-08-21 08:38:12 +0200

    panel setup

--- @navikt/core/react/src/datepicker/Panel.tsx
+++ @navikt/core/react/src/datepicker/Panel.tsx
@@ -11,10 +11,67 @@ export const DatePickerPanel = forwardRef<HTMLDivElement, DatePickerPanelProps>(
     <div
       {...rest}
       ref={ref}
-      className={cl("navds-datepicker-panel", className, {
-        "navds-datepicker-panel--open": open,
+      className={cl("navds-datepicker__panel", className, {
+        "navds-datepicker__panel--open": open,
       })}
-    ></div>
+    >
+      <table className="navds-datepicker__table">
+        <thead>
+          <tr>
+            <td className="navds-datepicker__td">Ma</td>
+            <td className="navds-datepicker__td">Ti</td>
+            <td className="navds-datepicker__td">On</td>
+            <td className="navds-datepicker__td">To</td>
+            <td className="navds-datepicker__td">Fr</td>
+            <td className="navds-datepicker__td">Lø</td>
+            <td className="navds-datepicker__td">Sø</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {[...new Array(7)].map((_, y) => (
+              <td className="navds-datepicker__td">
+                <button className="navds-datepicker__td-button">{y + 1}</button>
+              </td>
+            ))}
+          </tr>
+          <tr>
+            {[...new Array(7)].map((_, y) => (
+              <td className="navds-datepicker__td">
+                <button className="navds-datepicker__td-button">{y + 8}</button>
+              </td>
+            ))}
+          </tr>
+          <tr>
+            {[...new Array(7)].map((_, y) => (
+              <td className="navds-datepicker__td">
+                <button className="navds-datepicker__td-button">
+                  {y + 15}
+                </button>
+              </td>
+            ))}
+          </tr>
+          <tr>
+            {[...new Array(7)].map((_, y) => (
+              <td className="navds-datepicker__td">
+                <button className="navds-datepicker__td-button">
+                  {y + 22}
+                </button>
+              </td>
+            ))}
+          </tr>
+          <tr>
+            {[...new Array(3)].map((_, y) => (
+              <td className="navds-datepicker__td">
+                <button className="navds-datepicker__td-button">
+                  {y + 29}
+                </button>
+              </td>
+            ))}
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 );
 
