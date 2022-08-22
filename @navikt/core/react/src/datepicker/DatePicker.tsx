58e2045c1bdf239b324e30173a0b59449549ceed
Ken <ken.aleksander@gmail.com>
2022-08-22 07:45:13 +0200

    dot-notation for input

--- @navikt/core/react/src/datepicker/DatePicker.tsx
+++ @navikt/core/react/src/datepicker/DatePicker.tsx
@@ -1,127 +1,23 @@
-import { Calender } from "@navikt/ds-icons";
-import cl from "clsx";
-import React, { forwardRef, InputHTMLAttributes, useState } from "react";
-import { Button } from "../button";
-import { FormFieldProps, useFormField } from "../form/useFormField";
-import { BodyShort, ErrorMessage, Label } from "../typography";
-import { omit } from "../util";
-import DatePickerPanel from "./Panel";
+import React, { forwardRef } from "react";
+import DatePickerInput, { DatePickerInputType } from "./DatePickerInput";
 
-export interface DatePickerProps
-  extends FormFieldProps,
-    Omit<InputHTMLAttributes<HTMLInputElement>, "size" | "onChange"> {
-  /**
-   * Input label
-   */
-  label: React.ReactNode;
-  /**
-   * Shows label and description for screenreaders-only
-   * @default false
-   */
-  hideLabel?: boolean;
-  /**
-   * Changes padding and font-sizes
-   * @default medium
-   */
-  size?: "medium" | "small";
+export interface DatePickerProps extends React.HTMLAttributes<HTMLDivElement> {
+  children?: React.ReactNode;
 }
 
-export const DatePicker = forwardRef<HTMLInputElement, DatePickerProps>(
-  (props, ref) => {
-    const [open, setOpen] = useState(true);
+interface DatePickerComponent
+  extends React.ForwardRefExoticComponent<DatePickerProps> {
+  Input: DatePickerInputType;
+}
 
-    const {
-      inputProps,
-      size = "medium",
-      inputDescriptionId,
-      errorId,
-      showErrorMsg,
-      hasError,
-    } = useFormField(props, "datepicker");
+export const DatePicker = forwardRef<HTMLDivElement, DatePickerProps>(
+  ({ children }, ref) => {
+    /* const [open, setOpen] = useState(true); */
 
-    const {
-      className,
-      hideLabel = false,
-      label,
-      description,
-      value,
-      defaultValue,
-      ...rest
-    } = props;
-
-    return (
-      <div
-        className={cl(
-          className,
-          "navds-form-field",
-          `navds-form-field--${size}`,
-          "navds-datepicker",
-          {
-            "navds-datepicker--error": hasError,
-            "navds-datepicker--disabled": !!inputProps.disabled,
-          }
-        )}
-      >
-        <Label
-          htmlFor={inputProps.id}
-          size={size}
-          className={cl("navds-form-field__label", {
-            "navds-sr-only": hideLabel,
-          })}
-        >
-          {label}
-        </Label>
-        {!!description && (
-          <BodyShort
-            as="div"
-            className={cl("navds-form-field__description", {
-              "navds-sr-only": hideLabel,
-            })}
-            id={inputDescriptionId}
-            size={size}
-          >
-            {description}
-          </BodyShort>
-        )}
-        <div className="navds-datepicker__wrapper">
-          <div className="navds-datepicker__wrapper-inner">
-            <input
-              ref={ref}
-              {...omit(rest, ["error", "errorId", "size"])}
-              {...inputProps}
-              /* onChange={(e) => handleChange(e.target.value)} */
-              className={cl(
-                className,
-                "navds-datepicker__input",
-                "navds-text-field__input",
-                "navds-body-short",
-                `navds-body-${size}`
-              )}
-            />
-            <Button
-              variant="tertiary"
-              type="button"
-              size="small"
-              /* onClick={(e) => handleClear({ trigger: "Click", event: e })} */
-              className="navds-datepicker__input-button"
-              icon={<Calender title="åpne datovelger" />}
-            />
-          </div>
-        </div>
-        <DatePickerPanel open={open} />
-        <div
-          className="navds-form-field__error"
-          id={errorId}
-          aria-relevant="additions removals"
-          aria-live="polite"
-        >
-          {showErrorMsg && (
-            <ErrorMessage size={size}>{props.error}</ErrorMessage>
-          )}
-        </div>
-      </div>
-    );
+    return <div>{children}</div>;
   }
-);
+) as DatePickerComponent;
+
+DatePicker.Input = DatePickerInput;
 
 export default DatePicker;
