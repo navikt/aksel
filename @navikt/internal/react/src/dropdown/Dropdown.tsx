3f18280557a6dc7c4a8b9a4e136e69ede10b78ef
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-08-22 14:48:40 +0200

    [feature] Event ved lukking av dropdown (#1620)

--- @navikt/internal/react/src/dropdown/Dropdown.tsx
+++ @navikt/internal/react/src/dropdown/Dropdown.tsx
@@ -2,8 +2,11 @@ import React, { createContext, useState } from "react";
 import Toggle, { ToggleType } from "./Toggle";
 import Menu, { MenuType } from "./Menu";
 
+type DropdownElementChosenHandler = (element: React.MouseEvent) => void;
+
 export interface DropdownProps {
   children: React.ReactNode;
+  onSelect?: DropdownElementChosenHandler;
 }
 
 export interface DropdownType extends React.FC<DropdownProps> {
@@ -16,11 +19,12 @@ export interface DropdownContextType {
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
   readonly anchorEl: Element | null;
   setAnchorEl: React.Dispatch<React.SetStateAction<Element | null>>;
+  onSelect: DropdownElementChosenHandler;
 }
 
 export const DropdownContext = createContext<DropdownContextType | null>(null);
 
-export const Dropdown = (({ children }) => {
+export const Dropdown = (({ children, onSelect }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [anchorEl, setAnchorEl] = useState<Element | null>(null);
 
@@ -31,6 +35,10 @@ export const Dropdown = (({ children }) => {
         setIsOpen,
         anchorEl,
         setAnchorEl,
+        onSelect: (event) => {
+          onSelect?.(event);
+          setIsOpen(false);
+        },
       }}
     >
       {children}
