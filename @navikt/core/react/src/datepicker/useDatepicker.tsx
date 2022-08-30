23de48e94ece155744b68dbfd60c1e725f4fc196
Ken <ken.aleksander@gmail.com>
2022-08-30 14:34:01 +0200

    fjernet console.logs

--- @navikt/core/react/src/datepicker/useDatepicker.tsx
+++ @navikt/core/react/src/datepicker/useDatepicker.tsx
@@ -52,7 +52,6 @@ export const useDatepicker = (
   } = opt;
 
   const locale = getLocaleFromString(_locale);
-  console.log(locale);
   // Initialize states
   const [month, setMonth] = useState(defaultSelected ?? today);
   const [selectedDay, setSelectedDay] = useState(defaultSelected);
