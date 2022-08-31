0d9ec3ea9d47f5c9da812f5642b758aef0aaf867
Kai Erik Schieren <kai.schieren@soprasteria.com>
2022-08-31 10:44:01 +0200

    mode single tests

--- @navikt/core/react/src/datepicker/utils/__tests__/handle-mode.test.ts
+++ @navikt/core/react/src/datepicker/utils/__tests__/handle-mode.test.ts
@@ -1,11 +1,32 @@
 import { getInitialSelected } from "../handle-mode";
+import { isWeekend } from "date-fns";
 
 describe("Returns initial selected state", () => {
-  test("Should return date (Aug 17 2022)", () => {
+  test("Date should be valid (Date)", () => {
     const disabled = [];
     const disableWeekends = false;
     expect(
       getInitialSelected("single", disabled, disableWeekends)
     ).toBeInstanceOf(Date);
+  });
+  test("Date should be disabled (undefined)", () => {
+    const disabled = [new Date()];
+    const disableWeekends = false;
+    expect(
+      getInitialSelected("single", disabled, disableWeekends)
+    ).toBeUndefined();
+  });
+  test("Date should be disabled if today is weekend (Date | undefined)", () => {
+    const disabled = [];
+    const disableWeekends = true;
+    if (isWeekend(new Date())) {
+      expect(
+        getInitialSelected("single", disabled, disableWeekends)
+      ).toBeUndefined();
+    } else {
+      expect(
+        getInitialSelected("single", disabled, disableWeekends)
+      ).toBeInstanceOf(Date);
+    }
   });
 });
