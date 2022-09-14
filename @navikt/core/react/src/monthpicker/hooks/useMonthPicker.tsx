2949557196b7d489a5a4134103233ee6fa8d539b
Kai Erik Schieren <kai.schieren@soprasteria.com>
2022-09-14 09:53:08 +0200

    useMonthPicker initial states

--- @navikt/core/react/src/monthpicker/hooks/useMonthPicker.tsx
+++ @navikt/core/react/src/monthpicker/hooks/useMonthPicker.tsx
@@ -1,8 +1,19 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import { getLocaleFromString } from "../../datepicker/utils/locale";
 import { MonthPickerDefaultProps } from "../MonthPicker";
 import { MonthPickerInputProps } from "../MonthPickerInput";
+import { getDefaultSelected } from "../utils/get-initial-month";
 
 export interface UseMonthPickerOptions
-  extends Pick<MonthPickerDefaultProps, "locale" | "fromDate" | "toDate"> {
+  extends Pick<
+    MonthPickerDefaultProps,
+    | "locale"
+    | "fromDate"
+    | "toDate"
+    | "defaultSelected"
+    | "disabled"
+    | "dropdownCaption"
+  > {
   /** Make the selection required. */
   required?: boolean;
   /**
@@ -14,21 +25,6 @@ export interface UseMonthPickerOptions
 
 interface UseMonthPickerValue {
   /**
-   * Use: <MonthPicker {...monthpickerProps}/>
-   */
-  monthpickerProps: MonthPickerDefaultProps;
-  /**
-   * Use: <MonthPicker.Input {...inputProps}/>
-   */
-  inputProps: Pick<
-    MonthPickerInputProps,
-    "onChange" | "onFocus" | "onBlur" | "value" | "wrapperRef"
-  >;
-  /**
-   * Resets all states
-   */
-  reset: () => void;
-  /**
    * Selected month callback
    */
   selectedMonth?: Date;
@@ -37,3 +33,43 @@ interface UseMonthPickerValue {
    */
   setSelected: (date?: Date) => void;
 }
+
+export const useDatepicker = (
+  opt: UseMonthPickerOptions = {}
+): UseMonthPickerValue => {
+  const {
+    locale: _locale = "nb",
+    required,
+    defaultSelected,
+    fromDate = new Date(),
+    toDate,
+    openOnFocus = true,
+    disabled = [],
+    dropdownCaption = false,
+  } = opt;
+
+  const initialMonth = getDefaultSelected(
+    disabled,
+    dropdownCaption,
+    fromDate,
+    defaultSelected,
+    toDate
+  );
+
+  const locale = getLocaleFromString(_locale);
+
+  const inputRef = useRef<HTMLDivElement>(null);
+  const monthpickerRef = useRef<HTMLDivElement>(null);
+
+  // Initialize states
+  const [selectedMonth, setSelectedMonth] = useState(initialMonth);
+  const [year, setYear] = useState(selectedMonth);
+  const [open, setOpen] = useState(false);
+
+  const setSelected = (date: Date | undefined) => {
+    date && setSelectedMonth(date);
+    date && setYear(date);
+  };
+
+  return { setSelected, selectedMonth };
+};
