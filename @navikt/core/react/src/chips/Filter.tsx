255146737d6cf6a7c647dd558c83024556ef6c65
Ken <ken.aleksander@gmail.com>
2022-10-16 13:47:46 +0200

    :art: Chips API

--- @navikt/core/react/src/chips/Filter.tsx
+++ @navikt/core/react/src/chips/Filter.tsx
@@ -1,29 +1,37 @@
 import { SuccessStroke } from "@navikt/ds-icons";
 import cl from "clsx";
-import React, { forwardRef, useState } from "react";
+import React, { forwardRef } from "react";
 import { OverridableComponent } from "..";
 
 export interface FilterChipsProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: string;
+  /**
+   *
+   */
+  onSelect?: () => void;
+  /**
+   *
+   */
+  selected?: boolean;
 }
 
 export const FilterChips: OverridableComponent<
   FilterChipsProps,
   HTMLButtonElement
 > = forwardRef(
-  ({ className, children, as: Component = "button", ...rest }, ref) => {
-    const [active, setActive] = useState(false);
-
+  (
+    { className, children, selected, as: Component = "button", ...rest },
+    ref
+  ) => {
     return (
       <Component
         {...rest}
         ref={ref}
         className={cl("navds-chips__chip navds-chips__filter", className)}
-        onClick={() => setActive((x) => !x)}
-        aria-pressed={active}
+        aria-pressed={selected}
       >
-        {active && <SuccessStroke aria-hidden />}
+        {selected && <SuccessStroke aria-hidden />}
         {children}
       </Component>
     );
--- @navikt/core/react/src/chips/chips.stories.tsx
+++ @navikt/core/react/src/chips/chips.stories.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { List, Root, TabsTrigger } from "@radix-ui/react-tabs";
+import React, { useState } from "react";
 import { Chips } from ".";
 
 export default {
@@ -8,10 +9,23 @@ export default {
 };
 
 export const Filter = () => {
+  const [selected, setSelected] = useState<number[]>([]);
   return (
     <Chips>
       {new Array(4).fill(1).map((_, y) => (
-        <Chips.Filter key={y}>Filter</Chips.Filter>
+        <Chips.Filter
+          selected={selected.includes(y)}
+          onClick={() =>
+            setSelected(
+              selected.includes(y)
+                ? selected.filter((x) => x !== y)
+                : [...selected, y]
+            )
+          }
+          key={y}
+        >
+          Filter
+        </Chips.Filter>
       ))}
     </Chips>
   );
@@ -28,6 +42,7 @@ export const Input = () => {
 };
 
 export const Small = () => {
+  const [selected, setSelected] = useState<number[]>([]);
   return (
     <div className="colgap">
       <Chips size="small">
@@ -37,9 +52,42 @@ export const Small = () => {
       </Chips>
       <Chips size="small">
         {new Array(4).fill(1).map((_, y) => (
-          <Chips.Filter key={y}>Filter</Chips.Filter>
+          <Chips.Filter
+            selected={selected.includes(y)}
+            onClick={() =>
+              setSelected(
+                selected.includes(y)
+                  ? selected.filter((x) => x !== y)
+                  : [...selected, y]
+              )
+            }
+            key={y}
+          >
+            Filter
+          </Chips.Filter>
         ))}
       </Chips>
     </div>
+  );
+};
+
+export const AsTabs = () => {
+  const [selected, setSelected] = useState("chip1");
+  return (
+    <Root onValueChange={setSelected}>
+      <List asChild>
+        <Chips>
+          <TabsTrigger asChild value="chip1">
+            <Chips.Filter selected={selected === "chip1"}>Chip1</Chips.Filter>
+          </TabsTrigger>
+          <TabsTrigger asChild value="chip2">
+            <Chips.Filter selected={selected === "chip2"}>Chip2</Chips.Filter>
+          </TabsTrigger>
+          <TabsTrigger asChild value="chip3">
+            <Chips.Filter selected={selected === "chip3"}>Chip3</Chips.Filter>
+          </TabsTrigger>
+        </Chips>
+      </List>
+    </Root>
   );
 };
