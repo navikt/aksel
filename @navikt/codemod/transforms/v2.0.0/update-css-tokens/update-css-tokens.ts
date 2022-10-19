9cbdd50e9cb11e6d718d6d14fb32343c5454817d
Ken <ken.aleksander@gmail.com>
2022-10-19 17:48:46 +0200

    js-transform

--- @navikt/codemod/transforms/v2.0.0/update-css-tokens/update-css-tokens.ts
+++ @navikt/codemod/transforms/v2.0.0/update-css-tokens/update-css-tokens.ts
@@ -4,9 +4,12 @@ export const tokens = [
   ["--navds-global-color-green-100", "--a-green-100"],
   ["--navds-global-color-blue-100", "--a-blue-100"],
   ["--navds-global-color-blue-200", "--a-blue-200"],
+  ["--navds-global-color-blue-400", "--a-blue-400"],
   ["--navds-global-color-green-100", "--a-green-100"],
   ["--navds-global-color-green-500", "--a-green-500"],
   ["--navds-semantic-color-link", "--a-link"],
+  ["--navds-global-color-gray-100", "--a-gray-100"],
+  ["--navds-global-color-gray-400", "--a-gray-400"],
   ["--navds-global-color-gray-600", "--a-gray-600"],
 ];
 
