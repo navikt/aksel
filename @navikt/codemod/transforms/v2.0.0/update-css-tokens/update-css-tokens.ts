81b479eb11dd2b899df844c2991bc76112b34805
Ken <26967723+KenAJoh@users.noreply.github.com>
2022-10-21 10:36:23 +0200

    :sparkles: Placeholder-kode for migrering av tokens til v2 (#1682)

---
+++ @navikt/codemod/transforms/v2.0.0/update-css-tokens/update-css-tokens.ts
@@ -0,0 +1,29 @@
+// TODO: Hent utenfra
+export const tokens = [
+  ["--navds-global-color-red-100", "--a-red-100"],
+  ["--navds-global-color-green-100", "--a-green-100"],
+  ["--navds-global-color-blue-100", "--a-blue-100"],
+  ["--navds-global-color-blue-200", "--a-blue-200"],
+  ["--navds-global-color-blue-400", "--a-blue-400"],
+  ["--navds-global-color-green-100", "--a-green-100"],
+  ["--navds-global-color-green-500", "--a-green-500"],
+  ["--navds-semantic-color-link", "--a-link"],
+  ["--navds-global-color-gray-100", "--a-gray-100"],
+  ["--navds-global-color-gray-400", "--a-gray-400"],
+  ["--navds-global-color-gray-600", "--a-gray-600"],
+];
+
+/**
+ * @param {import('jscodeshift').FileInfo} file
+ * @param {import('jscodeshift').API} api
+ */
+export default function transformer(file, api, options) {
+  let src = file.source;
+
+  tokens.forEach((tok) => {
+    const rgx = new RegExp("(" + tok[0] + ")", "gm");
+    src = src.replace(rgx, tok[1]);
+  });
+
+  return src;
+}
