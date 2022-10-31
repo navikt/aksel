3f18f4148a3dddc94a34fae32b3731894de4ae9d
Ken <ken.aleksander@gmail.com>
2022-10-31 12:36:53 +0100

    :sparkles: Tags-eksempler

--- aksel.nav.no/website/pages/eksempler/tag/error.tsx
+++ aksel.nav.no/website/pages/eksempler/tag/error.tsx
@@ -2,11 +2,17 @@ import { Tag } from "@navikt/ds-react";
 import { withDsExample } from "components/website-modules/examples/withDsExample";
 
 const Example = () => {
-  return <Tag variant="error">Error</Tag>;
+  return (
+    <div className="flex gap-2">
+      <Tag variant="error">Error</Tag>
+      <Tag variant="error-filled">Error</Tag>
+    </div>
+  );
 };
 
 export default withDsExample(Example);
 
 export const args = {
-  index: 3,
+  index: 4,
+  desc: "Beskriver noe som er farlig, tjenestebrudd, irreversibelt, etc.",
 };
