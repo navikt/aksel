b39d471694e40d599f5cadb4ce96a5fc86acd6af
Ken <ken.aleksander@gmail.com>
2022-11-22 13:20:28 +0100

    :sparkles: SSO

--- aksel.nav.no/website/sanity/sanity.config.js
+++ aksel.nav.no/website/sanity/sanity.config.js
@@ -1,26 +1,77 @@
-import { defineConfig } from "sanity";
+import { defineConfig, createAuthStore } from "sanity";
 import { deskTool } from "sanity/desk";
+import schemas from "./schema";
+
+const sharedConfig = {
+  projectId: "hnbe3yhs",
+  apiVersion: "2021-10-21",
+  plugins: [deskTool()],
+  schema: schemas,
+  auth: createAuthStore({
+    redirectOnSingle: false,
+    mode: "replace",
+    projectId: "hnbe3yhs",
+    providers: [
+      {
+        name: "saml",
+        title: "NAV SSO",
+        url: "https://api.sanity.io/v2021-10-01/auth/saml/login/f3270b37",
+        logo: "/images/navlogo.svg",
+      },
+      {
+        name: "github",
+        title: "GitHub",
+        url: "https://api.sanity.io/v1/auth/login/github",
+      },
+      {
+        name: "google",
+        title: "Google",
+        url: "https://api.sanity.io/v1/auth/login/google",
+      },
+    ],
+  }),
+};
 
 export const workspaceConfig = defineConfig([
   {
+    // /admin defaults to first in list
     name: "default",
-    title: "Prod",
-    projectId: "hnbe3yhs",
+    title: "Live",
     dataset: "production",
-    apiVersion: "2021-10-21",
-    // the base path is required whenever more than one workspace is defined and is used for route matching
     basePath: "/admin/prod",
-    plugins: [deskTool()],
-    schema: { types: [] },
+    ...sharedConfig,
   },
   {
     name: "dev",
     title: "Dev",
-    projectId: "hnbe3yhs",
     dataset: "development",
-    apiVersion: "2021-10-21",
     basePath: "/admin/dev",
-    plugins: [deskTool()],
-    schema: { types: [] },
+    ...sharedConfig,
   },
 ]);
+
+/*
+interface WorkspaceOptions {
+  name: string
+  title: string
+  projectId: string
+  dataset: string
+  plugins?: PluginOptions[]
+  schema?: SchemaPluginOptions
+  document?: DocumentPluginOptions
+  tools?: Tool[] | ComposableOption<Tool[], ConfigContext>
+  form?: SanityFormConfig
+  basePath?: string
+  subtitle?: string
+  logo?: React.ComponentType
+  icon?: React.ComponentType
+  navbar?: {
+    components?: {
+      ToolMenu: React.ComponentType<ToolMenuProps>
+    }
+  }
+  theme?: StudioTheme
+}
+
+
+*/
