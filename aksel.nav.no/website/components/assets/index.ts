b4f8ec0e0e061924bf7ed07278889f390777d93a
Ken <ken.aleksander@gmail.com>
2022-12-03 17:54:39 +0100

    :art: Logo

--- aksel.nav.no/website/components/assets/index.ts
+++ aksel.nav.no/website/components/assets/index.ts
@@ -3,3 +3,4 @@ export { default as NavLogoWhite } from "./NavLogoWhite";
 export { default as NAVLogoDark } from "./NavLogoDark";
 
 export * from "./DsFrontpageIllustration";
+export * from "./Logo";
