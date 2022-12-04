8086de8dd0edce78709f97ff23381982cbc92352
Ken <ken.aleksander@gmail.com>
2022-12-04 22:03:51 +0100

    :art: Mer robust preview redirect

--- aksel.nav.no/website/pages/api/preview.ts
+++ aksel.nav.no/website/pages/api/preview.ts
@@ -9,10 +9,17 @@ function redirectToPreview(res, Location) {
   res.end();
 }
 
+const landingPages = [
+  "komponenter",
+  "grunnleggende",
+  "god-praksis",
+  "produktbloggen",
+];
+
 export default async function preview(req, res) {
   const { slug } = req.query;
 
-  if (!slug) {
+  if (!slug && slug !== ":slug*") {
     return redirectToPreview(res, "/");
   }
 
@@ -20,20 +27,37 @@ export default async function preview(req, res) {
     return res.status(401).json({ message: "Invalid preview-token" });
   }
 
+  if (slug === ":slug*") {
+    return redirectToPreview(res, `/`);
+  }
+
+  const isLanding = landingPages.find((x) => x === slug);
+
+  if (isLanding) {
+    return redirectToPreview(res, `/${isLanding}`);
+  }
+
   // Check if the article with the given `slug` exists
-  const article = await previewClient.fetch(
-    "*[slug.current == $slug][0].slug.current",
+  const { article, godpraksis } = await previewClient.fetch(
+    `{"article": *[slug.current == $slug][0].slug.current,
+    "godpraksis": *[slug.current == $gp && _type == "aksel_tema"][0].slug.current}`,
     {
       slug,
+      gp: slug.replace("god-praksis/", ""),
     }
   );
 
   // If the slug doesn't exist prevent preview mode from being enabled
-  if (!article) {
+  if (!article && !godpraksis) {
     return res.status(401).json({ message: "Invalid slug" });
+  }
+
+  if (article) {
+    return redirectToPreview(res, `/${article}`);
+  } else if (godpraksis) {
+    return redirectToPreview(res, `/god-praksis/${godpraksis}`);
   }
 
   // Redirect to the path from the fetched article
   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  return redirectToPreview(res, `/${article}`);
 }
