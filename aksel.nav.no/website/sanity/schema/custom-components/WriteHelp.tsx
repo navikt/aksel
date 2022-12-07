6373ade3fda40e02af24e5681df4ea9118c60c86
Kai Erik Schieren <kai.schieren@soprasteria.com>
2022-12-07 15:28:26 +0100

    :construction: :sparkles: fetch relevant skrivehjelp data

--- aksel.nav.no/website/sanity/schema/custom-components/WriteHelp.tsx
+++ aksel.nav.no/website/sanity/schema/custom-components/WriteHelp.tsx
@@ -1,4 +1,21 @@
+import { useClient } from "sanity";
+import useSWR from "swr";
+
 export const WriteHelp = (props) => {
+  const client = useClient({ apiVersion: "2021-06-07" });
+  const docType = props.schemaType?.options?.docType;
+  const { data, error } = useSWR(`*[_id == "skrivehjelp"][0]`, (query) =>
+    client.fetch(query)
+  );
+
+  if (error) {
+    return <div>Kan ikke hente skrivehjelp...</div>;
+  }
+
+  const content = data?.[`${docType}_writeHelp`];
+
+  console.log(content);
+
   return (
     <div className="flex shrink-0 items-center justify-between">
       hello world
