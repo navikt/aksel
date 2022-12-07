1fbdb08c9ab546f680dad0d47a5c2bdee329a7d6
Kai Erik Schieren <kai.schieren@soprasteria.com>
2022-12-07 13:01:31 +0100

    :construction: skrivehjelp document types

--- aksel.nav.no/website/sanity/schema/documents/presets/skrivehjelp.ts
+++ aksel.nav.no/website/sanity/schema/documents/presets/skrivehjelp.ts
@@ -1,9 +1,23 @@
 import { defineField } from "sanity";
 
-export const skrivehjelp = defineField({
-  title: "Skrivehjelp",
-  name: "writeHelp",
-  type: "string",
-  group: "skrivehjelp",
-  description: "Hjelpen er her!",
-});
+type DocumentType =
+  | "aksel_blogg"
+  | "aksel_artikkel"
+  | "aksel_prinsipp"
+  | "komponent_artikkel"
+  | "ds_artikkel"
+  | "testDoc";
+
+export const skrivehjelp = (documentType: DocumentType) => {
+  return defineField({
+    title: "Skrivehjelp",
+    name: "writeHelp",
+    type: "string",
+    group: "skrivehjelp",
+    description: "Hjelpen er her!",
+    options: {
+      //@ts-ignore
+      docType: documentType,
+    },
+  });
+};
