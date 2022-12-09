d3f5a174dc70b18cc03496c7d928d9144933330e
Ken <ken.aleksander@gmail.com>
2022-12-09 22:22:26 +0100

    :art: animasjon forside

---
+++ aksel.nav.no/website/pages/demo.tsx
@@ -0,0 +1,58 @@
+const Demo = () => {
+  return (
+    <div className="aksel-cube-wrapper grid h-full w-full place-items-center">
+      <svg
+        width="2514"
+        height="2509"
+        viewBox="0 0 2514 2509"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="aksel-cube"
+      >
+        <path
+          d="M316.642 2037.17L630.754 2351.29L317.113 2351.29L3.00021 2037.17L316.642 2037.17Z"
+          stroke="rgba(255,255,255,1)"
+          strokeWidth="4.5"
+          strokeLinejoin="round"
+          className="aksel-cube__1"
+        />
+        <path
+          d="M630.359 2038.04L944.471 1723.93L630.359 1409.81L316.248 1723.93L630.359 2038.04Z"
+          stroke="rgba(255,255,255,1)"
+          strokeWidth="4.5"
+          strokeLinejoin="round"
+          className="aksel-cube__2 "
+        />
+        <path
+          d="M1257.25 1724.79L1571.36 1410.68H1257.72L943.61 1724.79H1257.25Z"
+          stroke="rgba(255,255,255,1)"
+          strokeWidth="4.5"
+          strokeLinejoin="round"
+          className="aksel-cube__3 "
+        />
+        <path
+          d="M1570.5 1411.54L1884.61 1097.43L1570.5 783.318L1256.39 1097.43L1570.5 1411.54Z"
+          stroke="rgba(255,255,255,1)"
+          strokeWidth="4.5"
+          strokeLinejoin="round"
+          className="aksel-cube__4"
+        />
+        <path
+          d="M1569.64 784.181L1883.75 470.069H1570.11L1255.99 784.181H1569.64Z"
+          stroke="rgba(255,255,255,1)"
+          strokeWidth="4.5"
+          strokeLinejoin="round"
+          className="aksel-cube__5 "
+        />
+        <path
+          d="M2197 156.82L2511.11 470.932L2197.47 470.933L1883.36 156.82L2197 156.82Z"
+          stroke="rgba(255,255,255,1)"
+          strokeWidth="4.5"
+          strokeLinejoin="round"
+          className="aksel-cube__6 "
+        />
+      </svg>
+    </div>
+  );
+};
+export default Demo;
--- aksel.nav.no/website/pages/index.tsx
+++ aksel.nav.no/website/pages/index.tsx
@@ -93,6 +93,60 @@ const GetStarted = () => {
   );
 };
 
+const AkselCube = () => (
+  <div className="absolute -top-[19.5%] -left-[41%] right-0 z-0 scale-125 ">
+    <svg
+      viewBox="0 0 2514 2509"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="aksel-cube z-0"
+    >
+      <path
+        d="M316.642 2037.17L630.754 2351.29L317.113 2351.29L3.00021 2037.17L316.642 2037.17Z"
+        stroke="rgba(255,255,255,1)"
+        strokeWidth="4.5"
+        strokeLinejoin="round"
+        className="aksel-cube__1"
+      />
+      <path
+        d="M630.359 2038.04L944.471 1723.93L630.359 1409.81L316.248 1723.93L630.359 2038.04Z"
+        stroke="rgba(255,255,255,1)"
+        strokeWidth="4.5"
+        strokeLinejoin="round"
+        className="aksel-cube__2 "
+      />
+      <path
+        d="M1257.25 1724.79L1571.36 1410.68H1257.72L943.61 1724.79H1257.25Z"
+        stroke="rgba(255,255,255,1)"
+        strokeWidth="4.5"
+        strokeLinejoin="round"
+        className="aksel-cube__3 "
+      />
+      <path
+        d="M1570.5 1411.54L1884.61 1097.43L1570.5 783.318L1256.39 1097.43L1570.5 1411.54Z"
+        stroke="rgba(255,255,255,1)"
+        strokeWidth="4.5"
+        strokeLinejoin="round"
+        className="aksel-cube__4"
+      />
+      <path
+        d="M1569.64 784.181L1883.75 470.069H1570.11L1255.99 784.181H1569.64Z"
+        stroke="rgba(255,255,255,1)"
+        strokeWidth="4.5"
+        strokeLinejoin="round"
+        className="aksel-cube__5 "
+      />
+      <path
+        d="M2197 156.82L2511.11 470.932L2197.47 470.933L1883.36 156.82L2197 156.82Z"
+        stroke="rgba(255,255,255,1)"
+        strokeWidth="4.5"
+        strokeLinejoin="round"
+        className="aksel-cube__6 "
+      />
+    </svg>
+  </div>
+);
+
 const WithPreview = lazy(() => import("../components/WithPreview"));
 
 const Forside = ({ tekster, temaer, bloggs }: PageProps): JSX.Element => {
@@ -126,10 +180,11 @@ const Forside = ({ tekster, temaer, bloggs }: PageProps): JSX.Element => {
         />
       </Head>
 
-      <div className="bg-[#DCCAF3]">
+      <div className="aksel-cube-wrapper relative bg-[#DCCAF3]">
         <Header variant="transparent" />
-        <main tabIndex={-1} id="hovedinnhold" className="focus:outline-none">
-          <div className="min-h-screen-header pb-8">
+
+        <main tabIndex={-1} id="hovedinnhold" className=" focus:outline-none">
+          <div className="min-h-screen-header z-20 pb-8">
             <div className="centered-layout xs:mt-36 xs:mb-32 mb-16 mt-20 grid max-w-screen-sm place-items-center text-center">
               <Heading
                 level="1"
@@ -150,6 +205,7 @@ const Forside = ({ tekster, temaer, bloggs }: PageProps): JSX.Element => {
                 style={{ height: "140%" }}
               />
             </div>
+            <AkselCube />
           </div>
           <div className="bg-surface-subtle min-h-96 relative">
             <div className="centered-layout grid max-w-screen-2xl">
