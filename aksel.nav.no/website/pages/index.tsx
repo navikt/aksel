3d0e59441684840c8b7f71c48244298ef016867a
Ken <ken.aleksander@gmail.com>
2022-12-10 15:55:34 +0100

    :lipstick: Fungerende animasjon

--- aksel.nav.no/website/pages/index.tsx
+++ aksel.nav.no/website/pages/index.tsx
@@ -93,57 +93,52 @@ const GetStarted = () => {
   );
 };
 
+const Rect = ({ ...props }) => (
+  <svg
+    width="28.125rem"
+    height="28.125rem"
+    viewBox="0 0 450 450"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden
+    {...props}
+  >
+    <path
+      d="M2.55469 447.272H446.775L446.774 3.05239H2.55424L2.55469 447.272Z"
+      stroke="currentColor"
+      strokeWidth="4.5"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+const Skewed = ({ ...props }) => (
+  <svg
+    width="14.1875rem"
+    height="42rem"
+    viewBox="0 0 227 672"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden
+    {...props}
+  >
+    <path
+      d="M224.332 224.831L224.333 669.053L2.55446 447.275L2.55401 3.05265L224.332 224.831Z"
+      stroke="currentColor"
+      strokeWidth="4.5"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
 const AkselCube = () => (
-  <div className="absolute -top-[19.5%] -left-[41%] right-0 z-0 scale-125 ">
-    <svg
-      viewBox="0 0 2514 2509"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className="aksel-cube z-0"
-    >
-      <path
-        d="M316.642 2037.17L630.754 2351.29L317.113 2351.29L3.00021 2037.17L316.642 2037.17Z"
-        stroke="rgba(255,255,255,1)"
-        strokeWidth="4.5"
-        strokeLinejoin="round"
-        className="aksel-cube__1"
-      />
-      <path
-        d="M630.359 2038.04L944.471 1723.93L630.359 1409.81L316.248 1723.93L630.359 2038.04Z"
-        stroke="rgba(255,255,255,1)"
-        strokeWidth="4.5"
-        strokeLinejoin="round"
-        className="aksel-cube__2 "
-      />
-      <path
-        d="M1257.25 1724.79L1571.36 1410.68H1257.72L943.61 1724.79H1257.25Z"
-        stroke="rgba(255,255,255,1)"
-        strokeWidth="4.5"
-        strokeLinejoin="round"
-        className="aksel-cube__3 "
-      />
-      <path
-        d="M1570.5 1411.54L1884.61 1097.43L1570.5 783.318L1256.39 1097.43L1570.5 1411.54Z"
-        stroke="rgba(255,255,255,1)"
-        strokeWidth="4.5"
-        strokeLinejoin="round"
-        className="aksel-cube__4"
-      />
-      <path
-        d="M1569.64 784.181L1883.75 470.069H1570.11L1255.99 784.181H1569.64Z"
-        stroke="rgba(255,255,255,1)"
-        strokeWidth="4.5"
-        strokeLinejoin="round"
-        className="aksel-cube__5 "
-      />
-      <path
-        d="M2197 156.82L2511.11 470.932L2197.47 470.933L1883.36 156.82L2197 156.82Z"
-        stroke="rgba(255,255,255,1)"
-        strokeWidth="4.5"
-        strokeLinejoin="round"
-        className="aksel-cube__6 "
-      />
-    </svg>
+  <div className="aksel-cube">
+    <Skewed className="aksel-cube-1 " />
+    <Rect className="aksel-cube-2 " />
+    <Skewed className="aksel-cube-3 " />
+    <Rect className="aksel-cube-4 " />
+    <Skewed className="aksel-cube-5 " />
+    <Skewed className="aksel-cube-6 " />
   </div>
 );
 
@@ -180,11 +175,12 @@ const Forside = ({ tekster, temaer, bloggs }: PageProps): JSX.Element => {
         />
       </Head>
 
-      <div className="aksel-cube-wrapper relative bg-[#DCCAF3]">
+      <div className="aksel-cube-wrapper relative max-w-[100vw] overflow-hidden bg-[#DCCAF3]">
         <Header variant="transparent" />
+        <AkselCube />
 
         <main tabIndex={-1} id="hovedinnhold" className=" focus:outline-none">
-          <div className="min-h-screen-header z-20 pb-8">
+          <div className="z-20 pb-8">
             <div className="centered-layout xs:mt-36 xs:mb-32 mb-16 mt-20 grid max-w-screen-sm place-items-center text-center">
               <Heading
                 level="1"
@@ -205,7 +201,6 @@ const Forside = ({ tekster, temaer, bloggs }: PageProps): JSX.Element => {
                 style={{ height: "140%" }}
               />
             </div>
-            <AkselCube />
           </div>
           <div className="bg-surface-subtle min-h-96 relative">
             <div className="centered-layout grid max-w-screen-2xl">
