4c3a2fa3e8ffdcccb4bf42192ef629deed65618c
Ken <ken.aleksander@gmail.com>
2022-12-12 10:28:25 +0100

    :art: Oppdatert aksel-lenker

--- aksel.nav.no/website/components/website-modules/BreadCrumbs.tsx
+++ aksel.nav.no/website/components/website-modules/BreadCrumbs.tsx
@@ -1,6 +1,6 @@
-import { AkselArrow } from "components/website-modules/AkselArrow";
 import NextLink from "next/link";
 import { useRouter } from "next/router";
+import cl from "classnames";
 
 export const BreadCrumbs = ({
   href,
@@ -20,7 +20,26 @@ export const BreadCrumbs = ({
   return (
     <NextLink href={href ?? `/god-praksis/${router.query.tema}`} passHref>
       <a className="text-deepblue-500 hover:text-deepblue-800 group mb-1 flex w-fit items-center justify-start focus:underline focus:outline-none">
-        <AkselArrow />
+        <svg
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className={cl(
+            "transition-width text-deepblue-500 group-hover:text-deepblue-800 mr-2 h-[20px] w-[10px] group-hover:w-[16px]"
+          )}
+          aria-hidden
+        >
+          <path
+            d="M8 17.5L2.12132 11.6213C0.949744 10.4497 0.949746 8.55025 2.12132 7.37868L8 1.5"
+            stroke="currentColor"
+            strokeWidth="1.5"
+          />
+          <path
+            d="M16 9.72266L8 9.72266"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="hidden group-hover:block"
+          />
+        </svg>
 
         {text ??
           (router.query.tema as string).replace(/(^\w|\s\w)/g, (m) =>
