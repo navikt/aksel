b57cbf3fd18e42f29fda7b23346165d62021b672
Ken <ken.aleksander@gmail.com>
2022-12-13 15:06:43 +0100

    :art: Bedre hamburgermeny

--- aksel.nav.no/website/components/layout/header/Hamburger.tsx
+++ aksel.nav.no/website/components/layout/header/Hamburger.tsx
@@ -1,11 +1,11 @@
 import { Close, Hamburger as HamburgerIcon } from "@navikt/ds-icons";
-/* import { Popover } from "@navikt/ds-react"; */
+import { Button } from "@navikt/ds-react";
 import cl from "classnames";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import ReactModal from "react-modal";
-import { logNav /* Search */ } from "../..";
+import { logNav } from "../..";
 
 const LinkElement = ({ name, href }) => {
   const { asPath } = useRouter();
@@ -14,12 +14,11 @@ const LinkElement = ({ name, href }) => {
       <Link href={href} passHref>
         <a
           className={cl(
-            "flex h-full w-full items-center rounded px-2 focus:outline-none",
+            "hover:bg-surface-action-subtle-hover focus-visible:shadow-focus-inset relative flex h-full w-full items-center rounded px-2 focus:outline-none",
             {
-              "bg-surface-action-selected text-text-on-action hover:bg-surface-action-selected-hover focus:shadow-focus-gap-inset":
+              "before:bg-surface-action-selected pl-4 font-semibold before:absolute before:left-0 before:h-6 before:w-1 before:rounded-full":
                 asPath.startsWith(href),
-              "hover:bg-surface-action-subtle-hover focus-visible:shadow-focus-inset":
-                !asPath.startsWith(href),
+              "": !asPath.startsWith(href),
             }
           )}
           onClick={(e) =>
@@ -38,7 +37,6 @@ const LinkElement = ({ name, href }) => {
 };
 
 export const Hamburger = () => {
-  const [buttonRef, setButtonRef] = useState(null);
   const [open, setOpen] = useState(false);
 
   useEffect(() => {
@@ -48,55 +46,35 @@ export const Hamburger = () => {
   return (
     <>
       <div className={cl("z-[1050] mr-0 flex h-full justify-center")}>
-        <button
-          ref={setButtonRef}
+        <Button
+          variant="tertiary"
           aria-haspopup="false"
           onClick={() => setOpen(!open)}
           aria-expanded={open}
-          className={cl(
-            "min-w-header grid place-items-center rounded focus:outline-none",
-            {
-              /* "focus-visible:shadow-focus-gap hover:bg-surface-action-selected-hover bg-surface-action-selected text-text-on-action":
-                open, */
-              "focus-visible:shadow-focus hover:bg-surface-action-subtle-hover":
-                !open,
-            }
-          )}
-        >
-          {/* {open ? (
-            <Close
-              className="pointer-events-none text-2xl"
-              aria-label="Lukk meny"
-            />
-          ) : (
+          className="aksel-tertiary-button"
+          icon={
             <HamburgerIcon
               className="pointer-events-none text-2xl"
               aria-label="Åpne meny"
             />
-          )} */}
-          <HamburgerIcon
-            className="pointer-events-none text-2xl"
-            aria-label="Åpne meny"
-          />
-        </button>
+          }
+        />
         <ReactModal
           isOpen={open}
           onRequestClose={() => setOpen(false)}
           aria={{ modal: true }}
           overlayClassName="header-modal__overlay"
           contentLabel="Meny"
-          className="bg-surface-default max-w-screen-xs relative inset-0 block w-[90%] rounded p-6 md:hidden"
+          className="bg-surface-default max-w-screen-xs relative inset-0 block w-[90%] rounded py-14 px-11 md:hidden"
         >
           <nav aria-label="meny">
             <ul>
-              <button
+              <Button
+                variant="tertiary"
                 onClick={() => setOpen(false)}
-                className="hover:bg-surface-action-subtle-hover focus-visible:shadow-focus-inset mb-4 flex h-11 w-full items-center justify-center rounded px-2 focus:outline-none"
-              >
-                <span className="flex shrink-0 items-center justify-center gap-1">
-                  Lukk meny <Close aria-hidden />
-                </span>
-              </button>
+                className="aksel-tertiary-button absolute top-2 right-2"
+                icon={<Close title="lukk meny" className="h-5 w-5" />}
+              />
               <LinkElement name="God praksis" href="/god-praksis" />
               <LinkElement name="Grunnleggende" href="/grunnleggende" />
               <LinkElement name="Komponenter" href="/komponenter" />
@@ -104,24 +82,6 @@ export const Hamburger = () => {
             </ul>
           </nav>
         </ReactModal>
-        {/* <Popover
-          onClose={() => setOpen(false)}
-          anchorEl={buttonRef}
-          open={open}
-          arrow={false}
-          placement="bottom-start"
-          offset={6}
-          className="animate-fadeIn bg-surface-default ring-border-subtle shadow-large z-[1100] w-80 max-w-full rounded border-none ring-1"
-        >
-          <nav aria-label="meny">
-            <ul>
-              <LinkElement name="God praksis" href="/god-praksis" />
-              <LinkElement name="Grunnleggende" href="/grunnleggende" />
-              <LinkElement name="Komponenter" href="/komponenter" />
-              <LinkElement name="Blogg" href="/produktbloggen" />
-            </ul>
-          </nav>
-        </Popover> */}
       </div>
     </>
   );
