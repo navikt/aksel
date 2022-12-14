8ed6b1f52eb9d12d881be9b592a156dba93d7bba
Kai Erik Schieren <kai.schieren@soprasteria.com>
2022-12-14 07:26:39 +0100

    :art: add page skeleton

--- aksel.nav.no/website/pages/god-praksis/artikler/index.tsx
+++ aksel.nav.no/website/pages/god-praksis/artikler/index.tsx
@@ -1,4 +1,26 @@
+import Footer from "components/layout/footer/Footer";
+import { Header } from "components/layout/header/Header";
+import Head from "next/head";
+
 const Artikler = () => {
-  return <div>a</div>;
+  return (
+    <>
+      <Head>
+        <title>Artikler - Aksel</title>
+        <meta property="og:title" content="Artikler - Aksel" />
+      </Head>
+      <div className="bg-surface-subtle">
+        <Header variant="subtle" />
+        <main
+          tabIndex={-1}
+          id="hovedinnhold"
+          className="min-h-[80vh] focus:outline-none"
+        >
+          <div>Innhold</div>
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
 };
 export default Artikler;
