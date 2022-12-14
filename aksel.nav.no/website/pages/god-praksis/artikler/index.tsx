9c75b7a33c34d3c0d26627931208aa02d59a4284
Kai Erik Schieren <kai.schieren@soprasteria.com>
2022-12-14 11:07:40 +0100

    :art: add cube

--- aksel.nav.no/website/pages/god-praksis/artikler/index.tsx
+++ aksel.nav.no/website/pages/god-praksis/artikler/index.tsx
@@ -1,6 +1,7 @@
 import { Heading } from "@navikt/ds-react";
 import Footer from "components/layout/footer/Footer";
 import { Header } from "components/layout/header/Header";
+import { AkselCubeStatic } from "components/website-modules/cube";
 import Head from "next/head";
 
 const Artikler = () => {
@@ -18,6 +19,7 @@ const Artikler = () => {
           className="min-h-[80vh] focus:outline-none"
         >
           <div className="centered-layout mb-40 grid max-w-screen-2xl">
+            <AkselCubeStatic className="text-deepblue-300 opacity-5 " />
             <Heading
               level="1"
               size="xlarge"
