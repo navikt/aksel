9593fdab7b6fda6fe7eecc2c993b0b9ae0a740e6
Kai Erik Schieren <kai.schieren@soprasteria.com>
2022-12-23 15:36:46 +0100

    :art: calculate and update new average time

--- aksel.nav.no/website/pages/api/log-time.ts
+++ aksel.nav.no/website/pages/api/log-time.ts
@@ -1,4 +1,9 @@
+import { noCdnClient } from "@/sanity-client";
 import { NextApiRequest, NextApiResponse } from "next";
+
+const token = process.env.SANITY_WRITE_KEY;
+
+const client = noCdnClient(token);
 
 export default async function logTime(
   req: NextApiRequest,
@@ -7,12 +12,25 @@ export default async function logTime(
   const { id, current, views, time } = req.query;
 
   if (!(id && current && views && time)) {
-    return res
-      .status(400)
-      .json({
-        message: "Missing required parameter(s). id, current, length or views",
-      });
+    return res.status(400).json({
+      message: "Missing required parameter(s). id, current, length or views",
+    });
   }
+
+  const newAverage = Math.round(
+    (Number(time) + Number(current)) / Number(views)
+  );
+
+  // If page is new, create metrics object
+  await client
+    .patch(id as string)
+    .setIfMissing({ "metrics.avgTime": 0 })
+    .set({ "metrics.avgTime": newAverage })
+    .commit()
+    .catch((err) => {
+      console.error("Error:", err);
+      return res.status(500).json({ message: "Error updating page" });
+    });
 
   return res.status(200).json({ message: `Page with id: ${id} updated.` });
 }
