20d6d5c5fb4db6512b19ba105191d00c7f2a6877
Kai Erik Schieren <kai.schieren@soprasteria.com>
2022-12-30 10:45:59 +0100

    :art: fetch and display data

--- aksel.nav.no/website/sanity/schema/custom-components/Metrics.tsx
+++ aksel.nav.no/website/sanity/schema/custom-components/Metrics.tsx
@@ -1,6 +1,32 @@
-export const Metrics = (props) => {
+import { Heading, Stack } from "@sanity/ui";
+import { useFormValue } from "sanity";
+
+export const Metrics = () => {
+  const totalViews = useFormValue(["metrics", "pageviews", "summary"]);
+  const weeks: any = useFormValue(["metrics", "pageviews", "weeks"]);
+  const avgScrollLength = useFormValue(["metrics", "avgScrollLength"]);
+  const avgTime = useFormValue(["metrics", "avgTime"]);
+  const inactiveCount = useFormValue(["metrics", "inactiveCount"]);
+
   return (
-    <div className="flex shrink-0 items-center justify-between">METRICS</div>
+    <Stack>
+      <Heading as="h2" size={1}>
+        Metrikker
+      </Heading>
+      <p>
+        <>Totale sidevisninger: {totalViews}</>
+      </p>
+      <p>Totale uker: {weeks.length}</p>
+      <p>
+        <>Gjennomsnitt scrolllengde: {avgScrollLength}%</>
+      </p>
+      <p>
+        <>Gjennomsnitt tid på siden: {avgTime} sekunder</>
+      </p>
+      <p>
+        <>Totale inaktive: {inactiveCount}</>
+      </p>
+    </Stack>
   );
 };
 
