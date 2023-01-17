8f6503898999389187e758e37bfd0c64cb11f0e9
Ken <ken.aleksander@gmail.com>
2023-01-17 07:19:40 +0100

    :memo: Demo

---
+++ aksel.nav.no/website/pages/eksempler/chat/fliptoptekst.tsx
@@ -0,0 +1,32 @@
+import { Chat } from "@navikt/ds-react";
+import { withDsExample } from "components/website-modules/examples/withDsExample";
+
+const Example = () => {
+  return (
+    <div className="grid gap-10">
+      <Chat avatar="EVA" name="EVA" timestamp="01.01.21 14:00">
+        <Chat.Bubble>Hei! Mitt navn er Eva.</Chat.Bubble>
+        <Chat.Bubble>Hva kan jeg hjelpe deg med?</Chat.Bubble>
+      </Chat>
+      <Chat
+        avatar="NO"
+        name="Ola Normann"
+        timestamp="01.01.21 14:00"
+        position="right"
+        flipToptext
+      >
+        <Chat.Bubble>Hei Eva.</Chat.Bubble>
+        <Chat.Bubble>
+          Hvor sjekker jeg statusen på foreldrepengersøknaden min?
+        </Chat.Bubble>
+      </Chat>
+    </div>
+  );
+};
+
+export default withDsExample(Example);
+
+export const args = {
+  index: 3,
+  desc: "Noen ganger ønsker man mer fleksibilitet for plassering av navn + tid i chatboblen.",
+};
