e26c3e46b29feef69eac386801ab4978bd818453
Ken <ken.aleksander@gmail.com>
2023-01-18 16:12:18 +0100

    :art: Fiktivt eksempel tidslinje

--- aksel.nav.no/website/pages/eksempler/timeline-intern/timeline.tsx
+++ aksel.nav.no/website/pages/eksempler/timeline-intern/timeline.tsx
@@ -1,4 +1,17 @@
-import { SuccessFilled } from "@navikt/ds-icons";
+import {
+  Coronavirus,
+  DialogReport,
+  Edit,
+  Email,
+  EmailOpened,
+  Hospital,
+  Money,
+  Office1,
+  Office2,
+  PeopleDialogOutline,
+  SuccessFilled,
+  Vacation,
+} from "@navikt/ds-icons";
 import { Timeline } from "@navikt/ds-react-internal";
 import { withDsExample } from "components/website-modules/examples/withDsExample";
 
@@ -7,43 +20,42 @@ const row1 = [
     id: "1",
     start: new Date("Jan 1 2022"),
     end: new Date("Jan 31 2022"),
-    status: "success",
-    icon: <SuccessFilled aria-hidden />,
+    status: "warning",
+    icon: <Edit aria-hidden />,
     statusLabel: "Sykemeldt",
   },
   {
     id: "2",
     start: new Date("Feb 1 2022"),
     end: new Date("Mar 15 2022"),
-    status: "danger",
-    onSelectPeriod: () => console.log("PERIOD SELECTED!"),
-    statusLabel: "Sykemeldt",
+    status: "success",
+    statusLabel: "Utbetaling",
+    icon: <Money aria-hidden />,
   },
   {
     id: "3",
-    start: new Date("May 2 2022"),
-    end: new Date("May 25 2022"),
-    status: "success",
-    icon: <SuccessFilled aria-hidden />,
-    statusLabel: "Sykemeldt",
+    start: new Date("Mar 1 2022"),
+    end: new Date("Mar 31 2022"),
+    status: "neutral",
+    onSelectPeriod: () => console.log("SELECTED!"),
+    icon: <Vacation aria-hidden />,
+    label: "test",
+    statusLabel: "Ferie",
   },
   {
     id: "4",
-    start: new Date("Mar 1 2022"),
-    end: new Date("Mar 31 2022"),
+    start: new Date("May 2 2022"),
+    end: new Date("May 25 2022"),
     status: "success",
-    onSelectPeriod: () => console.log("PERIOD SELECTED!"),
-    icon: <SuccessFilled aria-hidden />,
-    label: "test",
-    statusLabel: "Sykemeldt",
+    statusLabel: "Utbetaling",
+    icon: <Money aria-hidden />,
   },
   {
     id: "5",
     start: new Date("Jul 1 2022"),
     end: new Date("Jul 31 2022"),
     status: "warning",
-    icon: <SuccessFilled aria-hidden />,
-    onSelectPeriod: () => console.log("PERIOD SELECTED!"),
+    icon: <Edit aria-hidden />,
     statusLabel: "Sykemeldt",
   },
   {
@@ -51,29 +63,29 @@ const row1 = [
     start: new Date("Aug 1 2022"),
     end: new Date("Aug 30 2022"),
     status: "warning",
-    icon: <SuccessFilled aria-hidden />,
-    statusLabel: "Sykemeldt",
+    icon: <Coronavirus aria-hidden />,
+    statusLabel: "Stønad korona",
   },
 ];
 
 const row2 = [
   {
     id: "7",
-    start: new Date("May 13 2022"),
-    end: new Date("May 25 2022"),
-    status: "warning",
-    onSelectPeriod: () => console.log("PERIOD SELECTED!"),
-    icon: <SuccessFilled aria-hidden />,
-    /* children: <DummyLabel />, */
-  },
-  {
-    id: "8",
     start: new Date("Feb 1 2022"),
     end: new Date("May 2 2022"),
     status: "neutral",
     onSelectPeriod: () => console.log("PERIOD SELECTED!"),
-    icon: <SuccessFilled aria-hidden />,
-    /* children: <DummyLabel />, */
+    icon: <Email aria-hidden />,
+    statusLabel: "Sendte bruker melding, med svarfrist",
+  },
+  {
+    id: "8",
+    start: new Date("May 13 2022"),
+    end: new Date("May 30 2022"),
+    status: "warning",
+    onSelectPeriod: () => console.log("PERIOD SELECTED!"),
+    icon: <DialogReport aria-hidden />,
+    statusLabel: "Bruker started samtale etter utgått svarfrist",
   },
 ];
 
@@ -81,7 +93,7 @@ const Example = () => {
   return (
     <div className="min-w-[800px] overflow-x-auto">
       <Timeline>
-        <Timeline.Row label="Row 1" icon={<SuccessFilled aria-hidden />}>
+        <Timeline.Row label="Person" icon={<Office1 aria-hidden />}>
           {row1.map((p: any, i) => {
             return (
               <Timeline.Period
@@ -94,7 +106,10 @@ const Example = () => {
             );
           })}
         </Timeline.Row>
-        <Timeline.Row label="Row 2" icon={<SuccessFilled aria-hidden />}>
+        <Timeline.Row
+          label="Dialoger"
+          icon={<PeopleDialogOutline aria-hidden />}
+        >
           {row2.map((p: any, i) => {
             return (
               <Timeline.Period
