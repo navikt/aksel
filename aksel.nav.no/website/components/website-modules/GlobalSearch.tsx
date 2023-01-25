4bf0506362fe86d63d930ba14bf9be3d5f0b2b83
Ken <ken.aleksander@gmail.com>
2023-01-25 15:50:37 +0100

    :lipstick: Stylet search-hit

--- aksel.nav.no/website/components/website-modules/GlobalSearch.tsx
+++ aksel.nav.no/website/components/website-modules/GlobalSearch.tsx
@@ -38,6 +38,13 @@ type SearchHit = {
 
 type GroupedHits = { [key: string]: SearchHit[] };
 
+/**
+ *
+ * TODO:
+ * - Suggestionbox med logging av querystring
+ * - Logge index for valgt søk med aplitude, eg 20/26
+ *
+ */
 export const GlobalSearch = () => {
   const [results, setResults] = useState<SearchHit[]>([]);
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -82,7 +89,7 @@ export const GlobalSearch = () => {
           onClear={() => setQuery("")}
         />
       </div>
-      <div className="mt-8">
+      <div className="mt-8 max-w-3xl">
         {results && query && (
           <>
             <Heading level="2" size="small">
@@ -108,10 +115,10 @@ function Group({ groups, query }: { groups: GroupedHits; query: string }) {
         .sort((a, b) => options[a[0]].index - options[b[0]].index)
         .map(([key, val]) => {
           return (
-            <div key={key}>
-              <div className="bg-border-alt-3 mt-4 rounded p-2">
+            <div key={key} className="mt-8 first-of-type:mt-4">
+              <div className="bg-bg-subtle  mt-4 rounded p-2">
                 <Label
-                  className="text-text-on-alt-3"
+                  className="text-text-default"
                   as="h2"
                 >{`${options[key].display} (${val.length})`}</Label>
               </div>
@@ -128,7 +135,7 @@ function Group({ groups, query }: { groups: GroupedHits; query: string }) {
 }
 
 function Hit({ hit, query }: { hit: SearchHit; query: string }) {
-  const hightlight = hit.matches[0].indices
+  const hightlightDesc = hit.matches[0].indices
     .map((y) => hit.matches[0].value.slice(y[0], y[1] + 1))
     .filter((x) => x.includes(query));
 
@@ -142,39 +149,56 @@ function Hit({ hit, query }: { hit: SearchHit; query: string }) {
     const clampBefore = Math.max(idx - 20, 0) === 0;
     const clampAfter = Math.min(idx + 20, value.length) === value.length;
     const slice = value.slice(
-      Math.max(idx - 20, 0),
-      Math.min(idx + 20, value.length)
+      Math.max(idx - 50, 0),
+      Math.min(idx + 50, value.length)
     );
     let str = "";
     !clampBefore && (str += "...");
     str += slice;
     !clampAfter && (str += "...");
 
-    const parts = str.split(new RegExp(`(${query})`, "gi"));
-    return (
-      <span>
-        {parts.map((part, i) => (
-          <span
-            key={i}
-            className={cl({
-              "bg-lightblue-400 font-semibold":
-                part.toLowerCase() === query.toLowerCase(),
-            })}
-          >
-            {part}
-          </span>
-        ))}
-      </span>
-    );
-    /* return str.split(query).map(x => <span key={x}></span>); */
+    return highlightStr(str, query);
   };
 
   return (
-    <div>
-      <NextLink href={hit.item.slug} passHref>
-        <Link className="mt-6">{hit.item.heading}</Link>
-      </NextLink>
-      {hightlight.length > 0 && <div>{getHightlight(query)}</div>}
+    <div className="border-b-border-divider border-b py-1">
+      <Heading level="3" size="small">
+        <NextLink href={hit.item.slug} passHref>
+          <a className="focus-visible:shadow-focus hover:bg-surface-hover inline-block w-full rounded py-6 px-4 focus:outline-none">
+            {highlightStr(hit.item.heading, query)}
+            {/* TODO: aria-hidden vs after-element med inset-0? Høre med uu */}
+            <span className="font-regular text-text-subtle text-lg" aria-hidden>
+              {hightlightDesc.length > 0 ? (
+                <div>{getHightlight(query)}</div>
+              ) : (
+                <div>{hit.item?.ingress ?? hit.item?.intro}</div>
+              )}
+            </span>
+          </a>
+        </NextLink>
+      </Heading>
     </div>
+  );
+}
+
+function splitStr(str: string, query: string) {
+  return str.split(new RegExp(`(${query})`, "gi"));
+}
+
+function highlightStr(str: string, query: string) {
+  return (
+    <span>
+      {splitStr(str, query).map((part, i) => (
+        <span
+          key={i}
+          className={cl({
+            "text-text-default bg-teal-300/30":
+              part.toLowerCase() === query.toLowerCase(),
+          })}
+        >
+          {part}
+        </span>
+      ))}
+    </span>
   );
 }
