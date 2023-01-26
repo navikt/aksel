2ac73a781288cf2b5a774575b85174bd4cbc1619
Ken <ken.aleksander@gmail.com>
2023-01-26 09:17:32 +0100

    :memo: todos

--- aksel.nav.no/website/components/website-modules/GlobalSearch.tsx
+++ aksel.nav.no/website/components/website-modules/GlobalSearch.tsx
@@ -52,7 +52,7 @@ type GroupedHits = { [key: string]: SearchHit[] };
  * - - Kan vi unngå lazyloading hvis Api sender med SVG i result-body?
  * - Wrappe søket i en fullside-modal.
  * - Åpne søk via en knapp i Header?
- * - - Usikker på hvilken komponent som skal "eie" søket, men tror header
+ * - - Usikker på hvilken komponent som skal "eie" søket, men tror Header
  *
  * Logging
  * - SuggestionBox med logging av querystring ved klikk
