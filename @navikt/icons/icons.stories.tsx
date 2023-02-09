78bcd6e9e4b0a4606ee0b37b43ba319cac53a5a5
Ken <ken.aleksander@gmail.com>
2023-02-09 09:27:04 +0100

    :art: Fikset ikonstory

--- @navikt/icons/icons.stories.tsx
+++ @navikt/icons/icons.stories.tsx
@@ -3,25 +3,27 @@ import React from "react";
 import * as Icons from "./src";
 export default {
   title: "ds-icons/Ikoner",
-  argTypes: {
-    Icon: {
-      defaultValue: "System",
-      control: {
-        type: "select",
-        options: Object.entries(Icons).map(([Name, _]) => Name),
-      },
-    },
-    color: { control: "color" },
-  },
 };
 
 export const Default = {
   render: ({ Icon, color, size, ...args }) => {
     const Iconname = Icons[Icon];
+
     return <Iconname style={{ fontSize: size, color }} {...args} />;
   },
 
   args: {
     size: "24px",
+    Icon: "System",
+    title: "Systemikon",
+    argTypes: {
+      Icon: {
+        control: {
+          type: "select",
+          options: Object.entries(Icons).map(([Name, _]) => Name),
+        },
+      },
+      color: { control: "color" },
+    },
   },
 };
