510080a66aec127e41d80c100c90a4a55993c652
Ken <ken.aleksander@gmail.com>
2023-02-10 12:39:12 +0100

    :art: Warning ved feil css

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -41,31 +41,20 @@ export const globalTypes = {
 };
 
 export const withTheme = (StoryFn, context) => {
-  if (
+  const foundCss =
     context.parameters.fileName.startsWith("./@navikt") &&
-    document.querySelector('[data-vite-dev-id$="dist/tw.css"]')
-  ) {
-    return (
-      <div
-        data-theme={
-          context.parameters.theme || context.globals.theme || "light"
-        }
-        lang="no"
-        id="root"
-      >
-        <div>
-          OBS! Ser ut som stylesheet-fra aksel.nav.no er lastet. Refresh siden!
-        </div>
-        <StoryFn />
-      </div>
-    );
-  }
+    document.querySelector('[data-vite-dev-id$="dist/tw.css"]');
   return (
     <div
       data-theme={context.parameters.theme || context.globals.theme || "light"}
       lang="no"
       id="root"
     >
+      {foundCss && (
+        <div className="css-warning">
+          OBS! Ser ut som CSS fra aksel.nav.no er lastet. Refresh siden!
+        </div>
+      )}
       <StoryFn />
     </div>
   );
