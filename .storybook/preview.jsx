29a4d51191eb57fcf49f560be733b57744bd70a9
Ken <ken.aleksander@gmail.com>
2023-02-10 11:28:00 +0100

    :art: Css-sjekk

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -40,14 +40,35 @@ export const globalTypes = {
   },
 };
 
-export const withTheme = (StoryFn, context) => (
-  <div
-    data-theme={context.parameters.theme || context.globals.theme || "light"}
-    lang="no"
-    id="root"
-  >
-    <StoryFn />
-  </div>
-);
+export const withTheme = (StoryFn, context) => {
+  if (
+    context.parameters.fileName.startsWith("./@navikt") &&
+    document.querySelector('[data-vite-dev-id$="dist/tw.css"]')
+  ) {
+    return (
+      <div
+        data-theme={
+          context.parameters.theme || context.globals.theme || "light"
+        }
+        lang="no"
+        id="root"
+      >
+        <div>
+          OBS! Ser ut som stylesheet-fra aksel.nav.no er lastet. Refresh siden!
+        </div>
+        <StoryFn />
+      </div>
+    );
+  }
+  return (
+    <div
+      data-theme={context.parameters.theme || context.globals.theme || "light"}
+      lang="no"
+      id="root"
+    >
+      <StoryFn />
+    </div>
+  );
+};
 
 export const decorators = [withTheme];
