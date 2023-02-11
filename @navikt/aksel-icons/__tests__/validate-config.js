a18b3be464810b7c3e921a64686a4e34f2c1ba15
Ken <ken.aleksander@gmail.com>
2023-02-11 14:37:53 +0100

    :art: Tests for config

--- @navikt/aksel-icons/__tests__/validate-config.js
+++ @navikt/aksel-icons/__tests__/validate-config.js
@@ -1,5 +1,32 @@
-describe("Icons config correctly setup", () => {
-  it("Svg has matching yml-config", () => {
-    expect({ test: 123 }).toEqual({ test: 124 });
+const fastglob = require("fast-glob");
+const path = require("path");
+
+const basePath = path.resolve(__dirname, "../icons");
+
+const svgList = fastglob
+  .sync("*.svg", { cwd: basePath })
+  .map((fileN) => path.basename(fileN));
+
+const ymlList = fastglob
+  .sync("*.yml", { cwd: basePath })
+  .map((fileN) => path.basename(fileN));
+
+describe(`Each SVG-file has a matching YML file`, () => {
+  it(`must have a corresponding YML file`, () => {
+    expect(
+      ymlList
+        .map((yml) => yml.replace(".yml", ".svg"))
+        .filter((file) => !svgList.includes(file))
+    ).toStrictEqual([]);
+  });
+});
+
+describe(`Each YML-file has a matching SVG file`, () => {
+  it(`must have a corresponding SVG file`, () => {
+    expect(
+      svgList
+        .map((svg) => svg.replace(".svg", ".yml"))
+        .filter((file) => !ymlList.includes(file))
+    ).toStrictEqual([]);
   });
 });
