cb4ecb3e8358855819c78230a283612ee047fb78
Ken <ken.aleksander@gmail.com>
2023-02-11 15:33:13 +0100

    :art: Config tests

---
+++ @navikt/aksel-icons/__tests__/validate-config.js
@@ -0,0 +1,33 @@
+const fastglob = require("fast-glob");
+const path = require("path");
+const jsYaml = require("js-yaml");
+const fs = require("fs");
+
+const basePath = path.resolve(__dirname, "../icons");
+
+const ymlList = fastglob
+  .sync("*.yml", { cwd: basePath })
+  .map((fileN) => path.basename(fileN));
+
+describe(`Each icons YML-config is valid`, () => {
+  ymlList.forEach((file) => {
+    it(`${file} has valid YML-config`, () => {
+      const ymlData = jsYaml.load(fs.readFileSync(`${basePath}/${file}`), {
+        schema: jsYaml.JSON_SCHEMA,
+      });
+      expect(ymlData.name).toEqual(file.replace(".yml", ""));
+      expect(ymlData.category).toBeTruthy();
+      expect(ymlData.description).toBeTruthy();
+      expect(ymlData.keywords).toBeTruthy();
+      expect(ymlData.keywords.length).toBeGreaterThan(0);
+      expect(ymlData.date_added).toBeTruthy();
+      expect(isDate(ymlData.date_added)).toBeTruthy();
+      expect(ymlData.date_modified).toBeTruthy();
+      expect(isDate(ymlData.date_modified)).toBeTruthy();
+    });
+  });
+});
+
+function isDate(dateStr) {
+  return !isNaN(new Date(dateStr).getDate());
+}
