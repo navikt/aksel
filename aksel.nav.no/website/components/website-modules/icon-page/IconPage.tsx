0be8fbfe305e9ecc554fa1cea5bdbd264e5bf298
Ken <ken.aleksander@gmail.com>
2023-02-13 09:39:46 +0100

    :truck: Refaktorert top-lenker

--- aksel.nav.no/website/components/website-modules/icon-page/IconPage.tsx
+++ aksel.nav.no/website/components/website-modules/icon-page/IconPage.tsx
@@ -1,16 +1,14 @@
-import { Heading, Link as DsLink, Search } from "@navikt/ds-react";
-import Link from "next/link";
-import NextLink from "next/link";
-import meta from "@navikt/ds-icons/meta.json";
-import cl from "classnames";
 import * as Icons from "@navikt/ds-icons";
-import { useMemo, useState } from "react";
+import meta from "@navikt/ds-icons/meta.json";
+import { Heading, Search } from "@navikt/ds-react";
+import cl from "classnames";
+import Footer from "components/layout/footer/Footer";
 import { Header } from "components/layout/header/Header";
 import { AkselCubeStatic } from "components/website-modules/cube";
-import Footer from "components/layout/footer/Footer";
-import { DownloadIcon, PackageIcon } from "@sanity/icons";
-import { FigmaIcon, GithubIcon } from "components/assets";
+import Link from "next/link";
+import { useMemo, useState } from "react";
 import { IconSidebar } from "./IconSidebar";
+import { TitleLinks } from "./TitleLinks";
 
 const categorizeIcons = (icons) => {
   const categories = [];
@@ -53,7 +51,7 @@ export const IconPage = ({ name }: { name: string }) => {
         id="hovedinnhold"
         className=" min-h-[80vh] focus:outline-none"
       >
-        <div className="centered-layout mb-40 grid  max-w-screen-lg pt-20">
+        <div className="centered-layout mb-40 grid max-w-screen-lg pt-20">
           <div className="mx-auto w-full max-w-screen-md">
             <h1 className="from-deepblue-800 via-deepblue-400 my-0 w-fit bg-gradient-to-tr to-violet-500 bg-clip-text text-7xl font-bold text-transparent">
               Aksel icons
@@ -66,44 +64,7 @@ export const IconPage = ({ name }: { name: string }) => {
           <div className="z-10 mt-16 mb-8 rounded-2xl bg-gradient-to-br from-violet-300/30 to-teal-400/30 p-[2px]">
             <div className="bg-surface-default shadow-medium  h-full w-full rounded-[14px]">
               <div className="border-b-border-subtle grid items-center border-b">
-                <div className="flex w-full items-center gap-3 px-8 pt-6 pb-2">
-                  <ul className="flex w-full flex-wrap justify-between gap-3">
-                    <li className="flex items-center gap-2">
-                      <NextLink
-                        href="https://www.figma.com/community/file/1167474127194981809"
-                        passHref
-                      >
-                        <DsLink className="text-text-default no-underline hover:underline">
-                          <FigmaIcon />{" "}
-                          <span className="ml-1">Figma community</span>
-                        </DsLink>
-                      </NextLink>
-                    </li>
-                    <li className="flex items-center gap-2">
-                      <DownloadIcon className="text-2xl" /> Last ned SVG-pakke
-                    </li>
-                    <li className="flex items-center gap-2">
-                      <NextLink
-                        href="https://www.npmjs.com/package/@navikt/ds-icons"
-                        passHref
-                      >
-                        <DsLink className="text-text-default no-underline hover:underline">
-                          <PackageIcon className="text-2xl" /> Installer med NPM
-                        </DsLink>
-                      </NextLink>
-                    </li>
-                    <li className="flex items-center gap-2">
-                      <NextLink
-                        href="https://github.com/navikt/Designsystemet/tree/master/%40navikt/icons"
-                        passHref
-                      >
-                        <DsLink className="text-text-default no-underline hover:underline">
-                          <GithubIcon /> Github
-                        </DsLink>
-                      </NextLink>
-                    </li>
-                  </ul>
-                </div>
+                <TitleLinks />
                 <form
                   onSubmit={(e) => e.preventDefault()}
                   className="w-full py-2 px-4"
