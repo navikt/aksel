600f7e9136d178652cc34d9230edda6e06b6ae7d
Kai Erik Schieren <kai.schieren@soprasteria.com>
2023-02-14 10:52:57 +0100

    :wheelchair: add aria labels

--- @navikt/core/react/src/list/List.tsx
+++ @navikt/core/react/src/list/List.tsx
@@ -1,6 +1,7 @@
 import cl from "clsx";
 import React, { forwardRef } from "react";
 import { BodyShort, Heading } from "../typography";
+import { useId } from "../util/useId";
 import { ListItem, ListItemType } from "./ListItem";
 
 interface ListProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -26,16 +27,27 @@ interface ListComponent extends React.ForwardRefExoticComponent<ListProps> {
 
 export const List = forwardRef<HTMLDivElement, ListProps>(
   ({ children, className, as = "ul", title, description, ...rest }, ref) => {
+    const headingId = useId();
+    const descriptionId = useId();
     const ListTag = as;
     return (
       <div {...rest} ref={ref} className={cl("navds-list", className)}>
         {title && (
-          <Heading size="small" level="3">
+          <Heading id={`tittel-${headingId}`} size="small" level="3">
             {title}
           </Heading>
         )}
-        {description && <BodyShort>{description}</BodyShort>}
-        <ListTag>{children}</ListTag>
+        {description && (
+          <BodyShort id={`description-${descriptionId}`}>
+            {description}
+          </BodyShort>
+        )}
+        <ListTag
+          aria-labelledby={title && `tittel-${headingId}`}
+          aria-describedby={description && `description-${descriptionId}`}
+        >
+          {children}
+        </ListTag>
       </div>
     );
   }
