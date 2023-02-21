8acaf9c85fe48a48943c0f3860d5598aa1274dc1
Vegard Haugstvedt <it.vegard@gmail.com>
2023-02-21 11:15:34 +0100

    New design for checked options in combobox

--- @navikt/core/react/src/form/combobox/Combobox.tsx
+++ @navikt/core/react/src/form/combobox/Combobox.tsx
@@ -383,6 +383,23 @@ export const Combobox = forwardRef<HTMLInputElement, ComboboxProps>(
                   aria-selected={selectedOptions.includes(o)}
                 >
                   <BodyShort size="medium">{o}</BodyShort>
+                  {selectedOptions.includes(o) && (
+                    <svg
+                      width="16"
+                      height="13"
+                      viewBox="0 0 16 13"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        fill="#005B82"
+                        d="M14.2014 0L16 1.89047L4.77943 13L0 8.39552L1.79361 6.5L4.77418 9.3019L14.2014 0Z"
+                      />
+                    </svg>
+                  )}
                 </li>
               ))}
             </ul>
