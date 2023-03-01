9ca9b4a8fb81fceae1070111e05c08ce1c3da65b
Ken <ken.aleksander@gmail.com>
2023-03-01 20:58:08 +0100

    :art: Columns komponent og styling

--- @navikt/core/react/src/columns/Columns.tsx
+++ @navikt/core/react/src/columns/Columns.tsx
@@ -1,16 +1,143 @@
 import React, { forwardRef, HTMLAttributes } from "react";
 import cl from "clsx";
 
+type SpacingT =
+  | "1"
+  | "2"
+  | "3"
+  | "4"
+  | "5"
+  | "6"
+  | "7"
+  | "8"
+  | "9"
+  | "10"
+  | "11"
+  | "12"
+  | "14"
+  | "16"
+  | "18"
+  | "20"
+  | "24"
+  | "32";
+
 export interface ColumnsProps extends HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
+  /**
+   * Number of columns to display. Can be a number, a string with a unit or tokens for spesific breakpoints.
+   * @example
+   * columns={{ sm: 1, md: 2, lg: 4, xl: 4}}
+   * columns={4}
+   */
+  columns?:
+    | number
+    | string
+    | {
+        xs?: number | string;
+        sm?: number | string;
+        md?: number | string;
+        lg?: number | string;
+        xl?: number | string;
+      };
+  /** Spacing between columns. Can be a number, a string with a unit or tokens for spesific breakpoints.
+   * @example
+   * gap="6"
+   * gap={{ sm: "2", md: "2", lg: "6", xl: "6"}}
+   */
+  gap?:
+    | SpacingT
+    | {
+        xs?: SpacingT;
+        sm?: SpacingT;
+        md?: SpacingT;
+        lg?: SpacingT;
+        xl?: SpacingT;
+      };
 }
 
 export const Columns = forwardRef<HTMLDivElement, ColumnsProps>(
-  ({ className, ...rest }, ref) => {
+  ({ className, columns, gap, ...rest }, ref) => {
+    const styles = {
+      ...columnsProps(columns),
+      ...gapProps(gap),
+    } as React.CSSProperties;
+
     return (
-      <div {...rest} ref={ref} className={cl("navds-columns", className)} />
+      <div
+        {...rest}
+        ref={ref}
+        className={cl("navds-columns", className)}
+        style={styles}
+      />
     );
   }
 );
+
+function gapProps(
+  gap?:
+    | SpacingT
+    | {
+        xs?: SpacingT;
+        sm?: SpacingT;
+        md?: SpacingT;
+        lg?: SpacingT;
+        xl?: SpacingT;
+      }
+) {
+  if (!gap) {
+    return {};
+  }
+
+  if (typeof gap === "string") {
+    return {
+      "--ac-columns-gap-sm": `var(--navds-spacing-${gap})`,
+    };
+  }
+
+  return Object.fromEntries(
+    Object.entries(gap).map(([breakpoint, token]) => [
+      `--ac-columns-gap-${breakpoint}`,
+      `var(--a-spacing-${token})`,
+    ])
+  );
+}
+
+function columnsProps(
+  columns?:
+    | number
+    | string
+    | {
+        xs?: number | string;
+        sm?: number | string;
+        md?: number | string;
+        lg?: number | string;
+        xl?: number | string;
+      }
+) {
+  if (!columns) {
+    return {};
+  }
+
+  if (typeof columns === "string" || typeof columns === "number") {
+    return {
+      [`--ac-columns-grid-template-columns-xs`]: formatColumns(columns),
+    };
+  }
+
+  return Object.fromEntries(
+    Object.entries(columns).map(([breakpoint, value]) => [
+      `--ac-columns-grid-template-columns-${breakpoint}`,
+      formatColumns(value),
+    ])
+  );
+}
+
+const formatColumns = (columns: number | string) => {
+  if (typeof columns === "number") {
+    return `repeat(${columns}, minmax(0, 1fr))`;
+  }
+
+  return columns;
+};
 
 export default Columns;
--- @navikt/core/react/src/columns/columns.stories.tsx
+++ @navikt/core/react/src/columns/columns.stories.tsx
@@ -4,11 +4,62 @@ import { Columns } from ".";
 export default {
   title: "ds-react/Columns",
   component: Columns,
+  parameters: {
+    layout: "fullscreen",
+  },
 };
 
 export const Default = {
   render: () => (
     <Columns>
+      <Placeholder text="1" />
+      <Placeholder text="2" />
+      <Placeholder text="3" />
+      <Placeholder text="4" />
+    </Columns>
+  ),
+};
+
+export const Gap = {
+  render: () => (
+    <Columns gap={{ sm: "6" }}>
+      <Placeholder text="1" />
+      <Placeholder text="2" />
+      <Placeholder text="3" />
+      <Placeholder text="4" />
+    </Columns>
+  ),
+};
+
+export const Width = {
+  render: () => (
+    <Columns gap={{ sm: "6" }} columns={{ sm: 1, lg: "2fr 1fr 2fr 1fr" }}>
+      <Placeholder text="1" />
+      <Placeholder text="2" />
+      <Placeholder text="3" />
+      <Placeholder text="4" />
+    </Columns>
+  ),
+};
+
+export const DynamicWidth = {
+  render: () => (
+    <Columns
+      gap={{ sm: "6", lg: "8" }}
+      columns={{ sm: "1fr 3fr", lg: "2fr 2fr" }}
+    >
+      <Placeholder text="1" />
+      <Placeholder text="2" />
+    </Columns>
+  ),
+};
+
+export const CardsStacking = {
+  render: () => (
+    <Columns
+      gap={{ xs: "4", sm: "6", lg: "8" }}
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+    >
       <Placeholder text="1" />
       <Placeholder text="2" />
       <Placeholder text="3" />
@@ -24,6 +75,7 @@ function Placeholder({ text }) {
         background: "var(--a-deepblue-900)",
         height: "20rem",
         width: "auto",
+        color: "white",
       }}
     >
       {text}
