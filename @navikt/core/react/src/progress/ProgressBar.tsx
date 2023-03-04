0d2d0e0e3c248ca835f061df210f5b4e3e5ff472
Ken <ken.aleksander@gmail.com>
2023-03-04 14:04:15 +0100

    :art: Varianter

--- @navikt/core/react/src/progress/ProgressBar.tsx
+++ @navikt/core/react/src/progress/ProgressBar.tsx
@@ -1,34 +1,96 @@
-import React, { forwardRef, HTMLAttributes } from "react";
+import { ErrorColored, SuccessColored } from "@navikt/ds-icons";
 import cl from "clsx";
+import React, { forwardRef, HTMLAttributes } from "react";
+import { BodyShort, Label } from "../typography";
 
-export interface ProgressBarProps extends HTMLAttributes<HTMLProgressElement> {
+export interface ProgressBarProps extends HTMLAttributes<HTMLDivElement> {
   min: number | string;
   max: number | string;
-  value: number | string;
+  value: number | string | null;
+  label: string;
+  /**
+   * @default false
+   */
+  hideLabel?: boolean;
+
+  /**
+   * @default "active"
+   */
+  status?: "active" | "finished" | "error";
+  /**
+   *
+   */
+  helperText?: string;
+  /**
+   *
+   */
+  variant?: "neutral" | "steps";
 }
 
-export const ProgressBar = forwardRef<HTMLProgressElement, ProgressBarProps>(
-  ({ className, min, max, current, ...rest }, ref) => {
-    const widthStyle = {
-      "--ac-progressbar-width": `${(Number(current) / Number(max)) * 100}%`,
-    } as React.CSSProperties;
+export const ProgressBar = forwardRef<HTMLDivElement, ProgressBarProps>(
+  (
+    {
+      className,
+      min,
+      max,
+      value,
+      label,
+      hideLabel,
+      status = "active",
+      variant = "neutral",
+      helperText,
+      ...rest
+    },
+    ref
+  ) => {
+    const current = Number(value) / Number(max);
+    const isFinished = status === "finished";
 
-    console.log(widthStyle);
+    const inlineStyle = !isFinished
+      ? { transform: `scaleX(${clamp(current)})` }
+      : {};
 
     return (
-      <progress
-        {...rest}
-        ref={ref}
-        className={cl("navds-progressbar", className)}
-        aria-valuemin={Number(min)}
-        aria-valuemax={Number(max)}
-        aria-valuenow={Number(current)}
-        max={max}
-      >
-        <span className="navds-progressbar__indicator" style={widthStyle} />
-      </progress>
+      <div className="navds-progressbar">
+        <div className="navds-progressbar__status">
+          <Label as="span">{label}</Label>
+          {status === "finished" && <SuccessColored aria-hidden />}
+          {status === "error" && <ErrorColored aria-hidden />}
+        </div>
+        <div
+          {...rest}
+          ref={ref}
+          className={cl(
+            "navds-progressbar__bar",
+            className,
+            `navds-progressbar__bar--${status}`,
+            `navds-progressbar__bar--${variant}`,
+            { "navds-progressbar__bar--indeterminate": value === null }
+          )}
+          aria-valuemin={Number(min)}
+          aria-valuemax={Number(max)}
+          aria-valuenow={Number(value)}
+          aria-busy={status !== "finished"}
+          role="progressbar"
+          aria-label={label}
+        >
+          <span
+            className="navds-progressbar__bar-indicator"
+            style={inlineStyle}
+          />
+        </div>
+        {helperText && (
+          <BodyShort as="div" className="navds-progressbar__label">
+            {helperText}
+          </BodyShort>
+        )}
+      </div>
     );
   }
 );
+
+function clamp(value: number) {
+  return Math.min(Math.max(value, 0), 1);
+}
 
 export default ProgressBar;
--- @navikt/core/react/src/progress/progressbar.stories.tsx
+++ @navikt/core/react/src/progress/progressbar.stories.tsx
@@ -1,18 +1,165 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ProgressBar } from ".";
 
 export default {
   title: "ds-react/ProgressBar",
   component: ProgressBar,
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          padding: "2rem",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+  parameters: {
+    layout: "fullscreen",
+  },
 };
 
 export const Default = {
   render: () => (
-    <div className="colgap" style={{ minWidth: 200 }}>
-      <ProgressBar min="0" max="100" current="30" />
-      <ProgressBar min="0" max="200" current="30" />
-      <ProgressBar min="0" max="100" current="0" />
-      <ProgressBar min="0" max="100" current="100" />
+    <div className="rowgap">
+      <div className="colgap" style={{ minWidth: 300, gap: "2rem" }}>
+        <ProgressBar min="0" max="10" value="3" label="Label" />
+        <ProgressBar
+          min="0"
+          max="10"
+          value="3"
+          label="Label"
+          helperText="Steg 3 av 10"
+        />
+        <ProgressBar min="0" max="20" value="8" label="Label" />
+        <ProgressBar min="0" max="10" value="0" label="Label" />
+        <ProgressBar min="0" max="10" value="10" label="Label" />
+        <ProgressBar min="0" max="10" value="20" label="Label" />
+        <ProgressBar min="0" max="10" value="3" label="Label" hideLabel />
+        <ProgressBar
+          min="0"
+          max="10"
+          value="10"
+          status="finished"
+          label="Label"
+          hideLabel
+        />
+        <ProgressBar
+          min="0"
+          max="10"
+          value="4"
+          status="error"
+          label="Label"
+          hideLabel
+        />
+        <ProgressBar min="0" max="10" value={null} label="Label" hideLabel />
+      </div>
+      <div className="colgap" style={{ minWidth: 300, gap: "2rem" }}>
+        <ProgressBar variant="steps" min="0" max="10" value="3" label="Label" />
+        <ProgressBar
+          variant="steps"
+          min="0"
+          max="10"
+          value="3"
+          label="Label"
+          helperText="Steg 3 av 10"
+        />
+        <ProgressBar variant="steps" min="0" max="20" value="8" label="Label" />
+        <ProgressBar variant="steps" min="0" max="10" value="0" label="Label" />
+        <ProgressBar
+          variant="steps"
+          min="0"
+          max="10"
+          value="10"
+          label="Label"
+        />
+        <ProgressBar
+          variant="steps"
+          min="0"
+          max="10"
+          value="20"
+          label="Label"
+        />
+        <ProgressBar
+          variant="steps"
+          min="0"
+          max="10"
+          value="3"
+          label="Label"
+          hideLabel
+        />
+        <ProgressBar
+          variant="steps"
+          min="0"
+          max="10"
+          value="10"
+          status="finished"
+          label="Label"
+          hideLabel
+        />
+        <ProgressBar
+          variant="steps"
+          min="0"
+          max="10"
+          value="4"
+          status="error"
+          label="Label"
+          hideLabel
+        />
+        <ProgressBar
+          variant="steps"
+          min="0"
+          max="10"
+          value={null}
+          label="Label"
+          hideLabel
+        />
+      </div>
     </div>
   ),
+};
+
+export const Demo = () => {
+  const size = 240;
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const interval = setInterval(() => {
+        setProgress((currentProgress) => {
+          const advancement = Math.random() * 16;
+          if (currentProgress + advancement < size) {
+            return currentProgress + advancement;
+          } else {
+            clearInterval(interval);
+            return size;
+          }
+        });
+      }, 200);
+    }, 2000);
+  }, []);
+
+  const running = progress > 0;
+
+  let helperText = running
+    ? `${progress.toFixed(1)}mB of ${size}mB`
+    : "Laster data...";
+  if (progress >= size) {
+    helperText = "Done";
+  }
+
+  return (
+    <ProgressBar
+      value={running ? progress : null}
+      max={size}
+      min={0}
+      status={progress === size ? "finished" : "active"}
+      label="Export data"
+      helperText={helperText}
+    />
+  );
 };
