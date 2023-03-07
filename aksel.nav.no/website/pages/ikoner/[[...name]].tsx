9c2aae44b8f558b2b0995897b2eb016cce317b97
Ken <ken.aleksander@gmail.com>
2023-03-07 11:07:44 +0100

    :fire: Fjernet statisk generering av alle ikonsider

--- aksel.nav.no/website/pages/ikoner/[[...name]].tsx
+++ aksel.nav.no/website/pages/ikoner/[[...name]].tsx
@@ -1,28 +1,25 @@
-import meta from "@navikt/aksel-icons/metadata";
-import { GetStaticPaths, GetStaticProps } from "next/types";
-
 import { IconPage } from "components/website-modules/icon-page/IconPage";
 
-const Page = (props: { name: string }) => <IconPage {...props} />;
+import meta from "@navikt/aksel-icons/metadata";
+
+const Page = (props: { name: string }) => {
+  return <IconPage name={props.name} />;
+};
 
 export default Page;
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  return {
-    paths: [
-      { params: { name: [] } },
-      ...Object.keys(meta).map((x) => ({
-        params: { name: [x] },
-      })),
-    ],
-    fallback: false,
-  };
-};
+export async function getServerSideProps(context) {
+  if (
+    (context?.query?.name?.[0] && !meta[context.query.name[0]]) ||
+    context?.query?.name?.length > 1
+  ) {
+    return {
+      props: {},
+      notFound: true,
+    };
+  }
 
-export const getStaticProps: GetStaticProps = async ({ params: { name } }) => {
   return {
-    props: {
-      name: name ? (typeof name === "string" ? name : name.join("")) : "",
-    },
+    props: { name: context?.query?.name?.[0] ?? "" },
   };
-};
+}
