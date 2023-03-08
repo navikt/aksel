ed636955c27a011b009fd98b93b98237633afb56
Ken <ken.aleksander@gmail.com>
2023-03-08 09:28:10 +0100

    :lipstick: Bedre spacing mellom ikonkategorier

--- aksel.nav.no/website/components/website-modules/icon-page/IconPage.tsx
+++ aksel.nav.no/website/components/website-modules/icon-page/IconPage.tsx
@@ -173,7 +173,7 @@ export const IconPage = ({ name }: { name: string }) => {
                 <div className="flex">
                   <div
                     className={cl(
-                      "animate-fadeIn transition-width grid w-full place-content-start gap-9 gap-y-12 px-6 py-8",
+                      "animate-fadeIn transition-width grid w-full place-content-start gap-8 px-6 py-8",
                       {
                         "border-r-border-subtle border-r": !!name,
                         "basis-2/3": name,
