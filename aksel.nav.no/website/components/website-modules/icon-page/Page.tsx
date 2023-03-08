70d89690bda2b6b5e1ba70c9660b73ee4ee22a3d
Ken <ken.aleksander@gmail.com>
2023-03-08 20:03:00 +0100

    :lipstick: Mobiltilpasset ikon-modal

--- aksel.nav.no/website/components/website-modules/icon-page/Page.tsx
+++ aksel.nav.no/website/components/website-modules/icon-page/Page.tsx
@@ -252,7 +252,7 @@ export const IconPage = ({ name }: { name: string }) => {
                       closeButton={false}
                       aria-modal
                       aria-label={`${name} ikon`}
-                      className="bg-surface-default focus-visible:shadow-focus z-modal absolute block h-full overflow-y-auto rounded py-6 px-6 focus:outline-none"
+                      className="bg-surface-default focus-visible:shadow-focus z-modal absolute block h-full overflow-y-auto rounded py-6 px-2 focus:outline-none sm:px-6"
                     >
                       {name && <IconSidebar name={name} focusRef={focusRef} />}
                     </Modal>
