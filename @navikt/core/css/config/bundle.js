98d15c881c47cb70b034cb0eab8e29dde6c5328c
Ken <ken.aleksander@gmail.com>
2023-04-19 14:14:17 +0200

    :art: Synced bundle-script with script-branch

--- @navikt/core/css/config/bundle.js
+++ @navikt/core/css/config/bundle.js
@@ -9,13 +9,16 @@ const cssImports = require("postcss-import");
 const cssnano = require("cssnano");
 const version = require("../package.json").version;
 
+/**
+ * TODO:
+ * - Lowercase everything
+ * - Bruk config for basline rekkefølge
+ */
 run();
 
 async function run() {
-  if (!fs.existsSync(path.resolve(__dirname, "../dist/module"))) {
-    fs.mkdirSync(path.resolve(__dirname, "../dist/module"), {
-      recursive: true,
-    });
+  if (!fs.existsSync(path.resolve(__dirname, "../dist"))) {
+    fs.mkdirSync(path.resolve(__dirname, "../dist"));
   }
 
   if (!fs.existsSync(path.resolve(__dirname, `../dist/version/${version}`))) {
@@ -37,7 +40,7 @@ async function run() {
  */
 async function bundleMonolith() {
   const indexSrc = path.resolve(__dirname, "../index.css");
-  const indexDist = path.resolve(__dirname, "../dist/module/index.css");
+  const indexDist = path.resolve(__dirname, "../dist/index.css");
 
   return fs.readFile(indexSrc, (_, css) => {
     postcss([cssImports, combineSelectors])
@@ -53,7 +56,7 @@ async function bundleMonolith() {
  */
 async function bundleComponents() {
   const indexSrc = path.resolve(__dirname, "../index.css");
-  const indexDist = path.resolve(__dirname, "../dist/module/Components.css");
+  const indexDist = path.resolve(__dirname, "../dist/Components.css");
 
   return fs.readFile(indexSrc, (_, css) => {
     /* Remove @charset, baseline */
@@ -61,9 +64,8 @@ async function bundleComponents() {
     postcss([cssImports, combineSelectors])
       .process(cssString, { from: indexSrc, to: indexDist })
       .then((result) => {
-        fs.writeFileSync(indexDist, result.css, () => true);
         fs.writeFileSync(
-          indexDist.replace("module", `version/${version}`),
+          indexDist.replace("dist", `dist/version/${version}`),
           result.css,
           () => true
         );
@@ -78,14 +80,14 @@ async function bundleComponents() {
  */
 async function bundleFragments() {
   const files = fastglob
-    .sync("*.css", { cwd: "." })
+    .sync("*.css", { cwd: ".", ignore: "**/*.min.css" })
     .map((fileN) => path.basename(fileN))
     .filter((x) => x !== "index.css")
     .map((x) => ({
       input: path.resolve(__dirname, `../${x}`),
       output: path.resolve(
         __dirname,
-        `../dist/module/${startCase(camelCase(x.replace("css", ""))).replace(
+        `../dist/${startCase(camelCase(x.replace("css", ""))).replace(
           / /g,
           ""
         )}.css`
@@ -93,21 +95,21 @@ async function bundleFragments() {
     }));
 
   /* Bundle forms together for easier cascading */
-  files.push({ input: "form/index.css", output: "dist/module/Forms.css" });
+  files.push({ input: "form/index.css", output: "dist/Forms.css" });
 
   /* Distribute a autprefixed and minified version of the complete stylesheet */
-  files.push({ input: "index.css", output: "dist/module/index.css" });
+  files.push({ input: "index.css", output: "dist/index.css" });
 
-  files.push({ input: "baseline/reset.css", output: "dist/module/reset.css" });
-  files.push({ input: "baseline/fonts.css", output: "dist/module/fonts.css" });
-  files.push({ input: "baseline/print.css", output: "dist/module/print.css" });
+  files.push({ input: "baseline/reset.css", output: "dist/reset.css" });
+  files.push({ input: "baseline/fonts.css", output: "dist/fonts.css" });
+  files.push({ input: "baseline/print.css", output: "dist/print.css" });
   files.push({
     input: "baseline/baseline.css",
-    output: "dist/module/baseline.css",
+    output: "dist/baseline.css",
   });
   files.push({
     input: "baseline/tokens.css",
-    output: "dist/module/tokens.css",
+    output: "dist/tokens.css",
   });
 
   for (let file of files) {
@@ -117,7 +119,7 @@ async function bundleFragments() {
       .then((result) => {
         fs.writeFileSync(file.output, result.css, () => true);
         fs.writeFileSync(
-          file.output.replace("module", `version/${version}`),
+          file.output.replace("dist", `dist/version/${version}`),
           result.css,
           () => true
         );
@@ -132,10 +134,12 @@ async function bundleFragments() {
  * - cssnano: css-minification
  */
 async function bundleMinified() {
-  const files = fastglob.sync("**/*.css", { cwd: "./dist" }).map((x) => ({
-    input: `dist/${x}`,
-    output: `dist/${x}`.replace("css", "min.css"),
-  }));
+  const files = fastglob
+    .sync("**/*.css", { cwd: "./dist", ignore: "**/*.min.css" })
+    .map((x) => ({
+      input: `dist/${x}`,
+      output: `dist/${x}`.replace("css", "min.css"),
+    }));
 
   for (let file of files) {
     const css = fs.readFileSync(file.input, { encoding: "utf-8" });
