5cb4f8039bf3730f1885baa737252c6e6184bc34
Ken <ken.aleksander@gmail.com>
2023-04-24 13:44:22 +0200

    :art: Added test-config

--- @navikt/aksel/help.ts
+++ @navikt/aksel/help.ts
@@ -5,11 +5,11 @@ console.log(`
 
 📝 Documentation
    - ${chalk.blueBright(
-     "https://github.com/navikt/aksel/blob/main/%40navikt/aksel-client/README.md"
+     "https://github.com/navikt/aksel/blob/main/%40navikt/aksel/README.md"
    )}
 
 💻 Commands:
-   - ${chalk.cyan(`npx @navikt/aksel-client ${chalk.green("css-imports")}`)}
+   - ${chalk.cyan(`npx @navikt/aksel ${chalk.green("css-imports")}`)}
    ✔︎ Helps with adding CSS imports for all Aksel-components
    ✔︎ Supports Static and CDN-imports
    ✔︎ Handles cascading, talwind and @layer rules
