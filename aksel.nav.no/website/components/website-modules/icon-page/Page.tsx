1c7734fb09e1328fc93951c3f9463b35470bb6f1
Ken <ken.aleksander@gmail.com>
2023-04-27 12:26:35 +0200

    :memo: Bedre dokumentasjon textarea

--- aksel.nav.no/website/components/website-modules/icon-page/Page.tsx
+++ aksel.nav.no/website/components/website-modules/icon-page/Page.tsx
@@ -105,7 +105,7 @@ export const IconPage = ({ name }: { name: string }) => {
           id="hovedinnhold"
           className="min-h-[80vh] focus:outline-none"
         >
-          <div className="mx-auto mb-40 grid w-full max-w-screen-2xl px-4 pt-20 sm:px-6">
+          <div className="mx-auto grid w-full max-w-screen-2xl px-4 pb-40 pt-20 sm:px-6">
             <div>
               <div>
                 <h1 className="text-deepblue-700 my-0 w-fit text-5xl font-bold">
