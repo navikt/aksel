ca8cc23fa82a61b57bf6c46f5dc95b22df1554ed
Ken <ken.aleksander@gmail.com>
2023-05-09 10:39:33 +0200

    :art: Oppdatert icon used for Stepper

--- @navikt/core/react/src/stepper/Step.tsx
+++ @navikt/core/react/src/stepper/Step.tsx
@@ -1,4 +1,3 @@
-import { CheckmarkIcon } from "@navikt/aksel-icons";
 import cl from "clsx";
 import React, { forwardRef, useContext } from "react";
 import { Label, OverridableComponent } from "..";
@@ -26,6 +25,25 @@ export interface StepperStepProps
    */
   interactive?: boolean;
 }
+
+const CompletedIcon = () => (
+  <svg
+    width="14"
+    height="10"
+    viewBox="0 0 14 10"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    focusable={false}
+    role="img"
+    aria-hidden
+    aria-label="Fullført"
+  >
+    <path
+      d="M4.93563 6.41478L11.3755 0.404669C11.9796 -0.160351 12.9294 -0.130672 13.4959 0.47478C14.0624 1.08027 14.0299 2.03007 13.4249 2.59621L5.92151 9.59934C5.64138 9.85904 5.27598 10 4.90064 10C4.5069 10 4.12756 9.84621 3.83953 9.56111L1.33953 7.06111C0.75401 6.47558 0.75401 5.52542 1.33953 4.93989C1.92506 4.35437 2.87522 4.35437 3.46075 4.93989L4.93563 6.41478Z"
+      fill="currentColor"
+    />
+  </svg>
+);
 
 export interface StepperStepType
   extends OverridableComponent<StepperStepProps, HTMLAnchorElement> {}
@@ -75,7 +93,7 @@ export const StepComponent: OverridableComponent<
       >
         {completed ? (
           <span className="navds-stepper__circle navds-stepper__circle--success">
-            <CheckmarkIcon aria-hidden />
+            <CompletedIcon />
           </span>
         ) : (
           <Label className="navds-stepper__circle" as="span" aria-hidden="true">
