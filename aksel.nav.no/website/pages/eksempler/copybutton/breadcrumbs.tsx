f06260f50d6737e400e49db4a53024a97955d6b2
Ken <ken.aleksander@gmail.com>
2023-05-12 15:44:18 +0200

    :art: Aksel-eksempler for CopyButton

---
+++ aksel.nav.no/website/pages/eksempler/copybutton/breadcrumbs.tsx
@@ -0,0 +1,28 @@
+import { CopyButton } from "@navikt/ds-react";
+import { withDsExample } from "components/website-modules/examples/withDsExample";
+
+const Example = () => {
+  return (
+    <div className="flex items-center gap-1">
+      <span>Flere statsborgerskap: Norge, Danmark, Finland</span>
+      <span>/</span>
+      <span className="flex items-center gap-1">
+        17029645183 <CopyButton size="small" clipboardText="17029645183" />
+      </span>
+      <span>/</span>
+      <span>Gift</span>
+    </div>
+  );
+};
+
+export default withDsExample(Example);
+
+/* Storybook story */
+export const Demo = {
+  render: Example,
+};
+
+export const args = {
+  index: 9,
+  desc: "CopyButton kan eksempelvis inlines i breadcrumbs for kopi av fødselsnummer.",
+};
---
+++ aksel.nav.no/website/pages/eksempler/copybutton/info.tsx
@@ -0,0 +1,41 @@
+import { CopyButton } from "@navikt/ds-react";
+import { withDsExample } from "components/website-modules/examples/withDsExample";
+
+const Example = () => {
+  return (
+    <div>
+      <Hr />
+      <Row text="Addresse 1:"> Osloveien 99, 0111 Oslo</Row>
+      <Row text="Addresse 2:"> Bergenveien 99, 2233 Bergen</Row>
+      <Row text="Telefon:">4040404040</Row>
+      <Row text="E-mail:">nav@naversen.no</Row>
+    </div>
+  );
+};
+
+export default withDsExample(Example);
+
+/* Storybook story */
+export const Demo = {
+  render: Example,
+};
+
+export const args = {
+  index: 10,
+  desc: "Ved utlisting av mye relevant innhold, kan CopyButton brukes for å enklere kopiere informasjonen.",
+};
+
+const Hr = () => (
+  <span className="border-t-1 border-t-border-divider my-1 w-full" />
+);
+
+const Row = ({ children, text }: any) => (
+  <>
+    <div className="grid grid-cols-[1fr_4fr_auto] items-center gap-2">
+      <span>{text}</span>
+      <span>{children}</span>
+      <CopyButton size="small" clipboardText={children} />
+    </div>
+    <Hr />
+  </>
+);
