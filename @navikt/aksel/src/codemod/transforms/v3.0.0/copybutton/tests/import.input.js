d18eb01f1b2fae0153b78cb4e5f15e2ce71ab614
Ken <ken.aleksander@gmail.com>
2023-05-15 10:13:22 +0200

    :art: Setup tests

---
+++ @navikt/aksel/src/codemod/transforms/v3.0.0/copybutton/tests/import.input.js
@@ -0,0 +1,7 @@
+import { Button } from "@navikt/ds-react";
+import { CopyToClipboard, Dropdown } from "@navikt/ds-react-internal";
+
+/* eslint-disable react/jsx-no-undef */
+export const Page = () => {
+  return <DsCpyButton></DsCpyButton>;
+};
---
+++ @navikt/aksel/src/codemod/transforms/v3.0.0/copybutton/tests/import.output.js
@@ -0,0 +1,7 @@
+import { Button, CopyButton } from "@navikt/ds-react";
+import { Dropdown } from "@navikt/ds-react-internal";
+
+/* eslint-disable react/jsx-no-undef */
+export const Page = () => {
+  return <CopyButton />;
+};
