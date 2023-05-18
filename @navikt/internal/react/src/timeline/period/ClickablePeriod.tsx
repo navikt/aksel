fbdaf040fa6401f04d69d0ef1ca9190eb2d6e225
Ken <ken.aleksander@gmail.com>
2023-05-18 12:29:21 +0200

    :art: OnHover popover in Timeline

--- @navikt/internal/react/src/timeline/period/ClickablePeriod.tsx
+++ @navikt/internal/react/src/timeline/period/ClickablePeriod.tsx
@@ -1,4 +1,17 @@
-import { mergeRefs, Popover } from "@navikt/ds-react";
+import {
+  autoUpdate,
+  arrow as flArrow,
+  flip,
+  offset,
+  safePolygon,
+  shift,
+  useDismiss,
+  useFloating,
+  useFocus,
+  useHover,
+  useInteractions,
+} from "@floating-ui/react";
+import { Popover, mergeRefs } from "@navikt/ds-react";
 import cl from "clsx";
 import React, { useMemo, useRef, useState } from "react";
 import { usePeriodContext } from "../hooks/usePeriodContext";
@@ -30,16 +43,60 @@ const ClickablePeriod = React.memo(
     restProps,
     periodRef,
   }: TimelineClickablePeriodProps) => {
-    const buttonRef = useRef<HTMLButtonElement | null>(null);
     const [selected, setSelected] = useState(false);
     const { index } = useRowContext();
     const { firstFocus } = usePeriodContext();
     const { initiate, addFocusable } = useTimelineContext();
+    const arrowRef = useRef<HTMLDivElement | null>(null);
+    const [clicked, setClicked] = useState(false);
+
+    const {
+      x,
+      y,
+      strategy,
+      context,
+      placement,
+      middlewareData: { arrow: { x: arrowX, y: arrowY } = {} },
+      refs,
+      floatingStyles,
+    } = useFloating({
+      placement: "top",
+      open: selected,
+      onOpenChange: (open) => {
+        setSelected(open);
+        setClicked(false);
+      },
+      middleware: [
+        offset(10),
+        shift(),
+        flip({ padding: 5, fallbackPlacements: ["bottom", "top"] }),
+        flArrow({ element: arrowRef, padding: 5 }),
+      ],
+      whileElementsMounted: autoUpdate,
+    });
+
+    const { getFloatingProps, getReferenceProps } = useInteractions([
+      useHover(context, {
+        handleClose: safePolygon(),
+        restMs: 25,
+        delay: { open: 1000 },
+        enabled: !clicked,
+      }),
+      useFocus(context),
+      useDismiss(context),
+    ]);
 
     const mergedRef = useMemo(
-      () => mergeRefs([buttonRef, periodRef]),
-      [periodRef]
+      () => mergeRefs([refs.setReference, periodRef]),
+      [periodRef, refs.setReference]
     );
+
+    const staticSide = {
+      top: "bottom",
+      right: "left",
+      bottom: "top",
+      left: "right",
+    }[placement.split("-")[0]];
 
     return (
       <>
@@ -50,10 +107,6 @@ const ClickablePeriod = React.memo(
             firstFocus && addFocusable(r, index);
             mergedRef(r);
           }}
-          onClick={(e) => {
-            children && setSelected((x) => !x);
-            onSelectPeriod?.(e);
-          }}
           aria-label={ariaLabel(start, end, status, statusLabel)}
           className={cl(
             "navdsi-timeline__period--clickable",
@@ -63,18 +116,59 @@ const ClickablePeriod = React.memo(
               "navdsi-timeline__period--selected": isActive,
             }
           )}
-          style={{
-            width: `${width}%`,
-            [direction]: `${left}%`,
-          }}
           aria-expanded={children ? selected : undefined}
-          onFocus={() => {
-            initiate(index);
-          }}
+          {...getReferenceProps({
+            onFocus: () => {
+              initiate(index);
+            },
+            style: {
+              width: `${width}%`,
+              [direction]: `${left}%`,
+            },
+            onClick: (e) => {
+              children && setClicked(true);
+              children && setSelected((x) => !x);
+              onSelectPeriod?.(
+                e as React.MouseEvent<HTMLButtonElement, MouseEvent>
+              );
+            },
+          })}
         >
           <span className="navdsi-timeline__period--inner">{icon}</span>
         </button>
         {children && (
+          <div
+            className={cl("navds-popover", {
+              "navds-popover--hidden": !selected,
+            })}
+            data-placement={placement}
+            aria-hidden={selected}
+            ref={refs.setFloating}
+            {...getFloatingProps({
+              tabIndex: -1,
+              style: {
+                position: strategy,
+                top: y ?? 0,
+                left: x ?? 0,
+              },
+            })}
+            style={floatingStyles}
+          >
+            <Popover.Content>{children}</Popover.Content>
+            <div
+              ref={(node) => {
+                arrowRef.current = node;
+              }}
+              style={{
+                ...(arrowX != null ? { left: arrowX } : {}),
+                ...(arrowY != null ? { top: arrowY } : {}),
+                ...(staticSide ? { [staticSide]: "-0.5rem" } : {}),
+              }}
+              className="navds-popover__arrow"
+            />
+          </div>
+        )}
+        {/* {children && (
           <Popover
             open={selected}
             onClose={() => setSelected(false)}
@@ -83,7 +177,7 @@ const ClickablePeriod = React.memo(
           >
             <Popover.Content>{children}</Popover.Content>
           </Popover>
-        )}
+        )} */}
       </>
     );
   }
