5549c67128fe5fbcc3b2b523c63714cde714d581
Ken <ken.aleksander@gmail.com>
2023-05-22 15:57:59 +0200

    :art: Updated plugin imports

--- @navikt/aksel-icons/plugin/src/plugin/plugin.ts
+++ @navikt/aksel-icons/plugin/src/plugin/plugin.ts
@@ -1,26 +1,25 @@
 /* eslint-disable no-undef */
-// @ts-ignore
+
 figma.showUI(__html__, { width: 480, height: 600 });
-// @ts-ignore
+
 figma.ui.onmessage = (msg) => {
   if (msg.type === "create-icon") {
     const nodes = [];
-    // @ts-ignore
+
     const icon = figma.createNodeFromSvg(msg.svg);
     icon.name = msg.name;
     icon.resize(msg.size, msg.size);
     nodes.push(icon);
 
-    // @ts-ignore
     figma.currentPage.appendChild(icon);
-    // @ts-ignore
+
     figma.viewport.scrollAndZoomIntoView(nodes);
-    // @ts-ignore
+
     figma.ui.postMessage({
       type: "create-icon",
-      message: `Created ${msg.count} icon}`,
+      message: `Created ${msg.name} icon}`,
     });
   }
-  // @ts-ignore
+
   figma.closePlugin();
 };
