ecf737a849f966f5a84c463e89095266b0a7a908
Ken <ken.aleksander@gmail.com>
2023-05-30 10:35:57 +0200

    :art: Codemod boilerplate

---
+++ @navikt/aksel/src/codemod/transforms/v4.0.0/internal-css/tests/internal-css.test.ts
@@ -0,0 +1,12 @@
+import { check } from "../../../../utils/check";
+
+const migration = "copybutton";
+const fixtures = ["complete"];
+
+for (const fixture of fixtures) {
+  check(__dirname, {
+    fixture,
+    migration,
+    extension: "js",
+  });
+}
