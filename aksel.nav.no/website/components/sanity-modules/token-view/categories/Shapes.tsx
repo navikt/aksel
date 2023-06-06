1abf52684c2f2d37e1fcca2b54c250f917cbe790
Ken <ken.aleksander@gmail.com>
2023-06-06 23:07:58 +0200

    :art: Border-radius tokens

--- aksel.nav.no/website/components/sanity-modules/token-view/categories/Shapes.tsx
+++ aksel.nav.no/website/components/sanity-modules/token-view/categories/Shapes.tsx
@@ -1,37 +1,44 @@
+import { FilesIcon } from "@navikt/aksel-icons";
+import { BodyShort, CopyButton } from "@navikt/ds-react";
 import docs from "@navikt/ds-tokens/docs.json";
-import { Frame } from "../Frame";
+
+import { Grid } from "../Grid";
+import { sanitizeName } from "../utilities";
 
 export const ShapesView = ({ cat }: { cat: string }) => {
   const shapes = docs[cat];
 
   return (
-    <Frame
-      tokens={shapes}
-      styles="borderRadius"
-      element={({ token }: { token: string; name?: string }) => {
+    <Grid>
+      {shapes.map((x) => {
         return (
-          <div
-            className="min-h-16 flex h-full w-full items-center justify-center gap-4 rounded-md px-4 text-5xl font-semibold"
-            aria-hidden
-            style={{
-              background: `var(--a-surface-default)`,
-            }}
-          >
+          <div key={x.name} id={x.name} className="flex w-fit items-center">
             <div
-              className="bg-surface-alt-3-strong h-8 w-8 rounded-md"
               style={{
-                borderRadius: `${token}`,
+                boxShadow: `inset 0 2px 4px 0 rgba(0,0,0,0.06)`,
+                borderRadius: `${x.value}`,
               }}
-            />
-            <div
-              className="bg-surface-alt-3-strong h-8 w-20 rounded-md"
-              style={{
-                borderRadius: `${token}`,
-              }}
-            />
+              className="bg-surface-alt-3-strong group mr-3 grid h-12 w-12 place-content-center rounded-lg"
+            >
+              <CopyButton
+                variant="neutral"
+                copyText={x.name}
+                className="focus-visible:shadow-focus-gap  rounded-lg opacity-0 duration-0 focus-visible:opacity-100 group-hover:opacity-100 group-hover:transition-opacity"
+                icon={<FilesIcon title={x.name} fontSize="1.5rem" />}
+                style={{
+                  borderRadius: `${x.value}`,
+                  color: "var(--a-text-on-action)",
+                }}
+              />
+            </div>
+
+            <BodyShort as="dl" size="small">
+              <dt>{sanitizeName(x.name.replace("border-radius-", ""))}</dt>
+              <dd className="text-text-subtle ">{x.value}</dd>
+            </BodyShort>
           </div>
         );
-      }}
-    />
+      })}
+    </Grid>
   );
 };
