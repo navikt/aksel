63d40a968630f082b5813730b6e73fa92d67a2c4
Ken <ken.aleksander@gmail.com>
2023-06-07 20:15:46 +0200

    :bug: Fikset inlinecode fontweight

--- aksel.nav.no/website/components/website-modules/InlineCode.tsx
+++ aksel.nav.no/website/components/website-modules/InlineCode.tsx
@@ -1,5 +1,5 @@
 export const InlineCode = (props: React.HTMLAttributes<HTMLElement>) => (
-  <code className="text-deepblue-800 font-mono text-sm font-medium leading-normal before:contents">
+  <code className="text-deepblue-800 font-mono text-sm font-semibold leading-normal before:contents">
     <span aria-hidden>`</span>
     <span>{props.children}</span>
     <span aria-hidden>`</span>
