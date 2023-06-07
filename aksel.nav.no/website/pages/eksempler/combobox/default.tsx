9b4f528efacb8b00182b08aaf6e4dd7259c4eaa8
Vegard Haugstvedt <it.vegard@gmail.com>
2023-06-07 15:16:52 +0200

    Missing label

--- aksel.nav.no/website/pages/eksempler/combobox/default.tsx
+++ aksel.nav.no/website/pages/eksempler/combobox/default.tsx
@@ -22,6 +22,7 @@ export const Example = () => {
   return (
     <div>
       <Combobox
+        label="Komboboks"
         options={initialOptions}
         selectedOptions={initialSelectedOptions}
       />
