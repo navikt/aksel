536bd8c24e8015031d80cf8b210e28b09365c52b
Ken <ken.aleksander@gmail.com>
2023-06-07 21:51:22 +0200

    :fire: Fjernet tabs

--- aksel.nav.no/website/components/sanity-modules/token-view/categories/Global.tsx
+++ aksel.nav.no/website/components/sanity-modules/token-view/categories/Global.tsx
@@ -1,5 +1,5 @@
 import { FilesIcon } from "@navikt/aksel-icons";
-import { BodyShort, CopyButton, Tabs } from "@navikt/ds-react";
+import { BodyShort, CopyButton } from "@navikt/ds-react";
 import docs from "@navikt/ds-tokens/docs.json";
 import { sanitizeName, getColorString } from "../utilities";
 import color from "tinycolor2";
@@ -7,54 +7,40 @@ import { Grid } from "../Grid";
 
 export const GlobalView = ({ cat }: { cat: string }) => {
   const colors: { name: string; value: string }[] = docs[cat];
-
   return (
-    <Tabs size="small" defaultValue="tokens">
-      <Tabs.List>
-        <Tabs.Tab value="tokens" label="Tokens" />
-        <Tabs.Tab value="css" label="CSS" />
-        <Tabs.Tab value="scss" label="SCSS" />
-        <Tabs.Tab value="js" label="JS" />
-      </Tabs.List>
-      <Tabs.Panel value="css">TODO</Tabs.Panel>
-      <Tabs.Panel value="scss">TODO</Tabs.Panel>
-      <Tabs.Panel value="js">TODO</Tabs.Panel>
-      <Tabs.Panel value="tokens">
-        <Grid>
-          {colors.map((x) => {
-            return (
-              <div key={x.name} id={x.name} className="flex w-fit items-center">
-                <div
-                  style={{
-                    background: `var(${x.name})`,
-                    boxShadow: `inset 0 2px 4px 0 rgba(0,0,0,0.06)`,
-                  }}
-                  className="group mr-3 grid h-12 w-12 place-content-center rounded-lg"
-                >
-                  <CopyButton
-                    variant="neutral"
-                    copyText={x.name}
-                    className="focus-visible:shadow-focus-gap rounded-lg opacity-0 duration-0 focus-visible:opacity-100 group-hover:opacity-100 group-hover:transition-opacity"
-                    icon={<FilesIcon title={x.name} fontSize="1.5rem" />}
-                    style={{
-                      color: color
-                        .mostReadable(x.value, ["#fff", "#262626"])
-                        .toHexString(),
-                    }}
-                  />
-                </div>
+    <Grid>
+      {colors.map((x) => {
+        return (
+          <div key={x.name} id={x.name} className="flex w-fit items-center">
+            <div
+              style={{
+                background: `var(${x.name})`,
+                boxShadow: `inset 0 2px 4px 0 rgba(0,0,0,0.06)`,
+              }}
+              className="group mr-3 grid h-12 w-12 place-content-center rounded-lg"
+            >
+              <CopyButton
+                variant="neutral"
+                copyText={x.name}
+                className="focus-visible:shadow-focus-gap rounded-lg opacity-0 duration-0 focus-visible:opacity-100 group-hover:opacity-100 group-hover:transition-opacity"
+                icon={<FilesIcon title={x.name} fontSize="1.5rem" />}
+                style={{
+                  color: color
+                    .mostReadable(x.value, ["#fff", "#262626"])
+                    .toHexString(),
+                }}
+              />
+            </div>
 
-                <BodyShort as="dl">
-                  <dt>{sanitizeName(x.name)}</dt>
-                  <dd className="text-text-subtle text-medium">
-                    {getColorString(x.value)}
-                  </dd>
-                </BodyShort>
-              </div>
-            );
-          })}
-        </Grid>
-      </Tabs.Panel>
-    </Tabs>
+            <BodyShort as="dl">
+              <dt>{sanitizeName(x.name)}</dt>
+              <dd className="text-text-subtle text-medium">
+                {getColorString(x.value)}
+              </dd>
+            </BodyShort>
+          </div>
+        );
+      })}
+    </Grid>
   );
 };
