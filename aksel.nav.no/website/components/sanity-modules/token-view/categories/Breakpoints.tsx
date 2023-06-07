2ad1dc35c87d41c743b9a22dd197d8a82a4cf63b
Ken <ken.aleksander@gmail.com>
2023-06-07 18:45:16 +0200

    :art: Migrert token-visning til tabell

--- aksel.nav.no/website/components/sanity-modules/token-view/categories/Breakpoints.tsx
+++ aksel.nav.no/website/components/sanity-modules/token-view/categories/Breakpoints.tsx
@@ -1,5 +1,3 @@
-import docs from "@navikt/ds-tokens/docs.json";
-import { CopyButton } from "@navikt/ds-react";
 import {
   LaptopIcon,
   MobileIcon,
@@ -7,78 +5,58 @@ import {
   MonitorIcon,
   TabletIcon,
 } from "@navikt/aksel-icons";
+import docs from "@navikt/ds-tokens/docs.json";
+import { AkselTable, AkselTableRow } from "components/website-modules/Table";
 
 export const BreakpointsView = ({ cat }: { cat: string }) => {
   const breakpoints = docs[cat];
 
   return (
-    <table className="mb-7 w-full border-separate border-spacing-0 rounded border border-gray-200">
-      <thead>
-        <tr className="rounded-t text-left">
-          <th className="font-regular rounded-tl bg-gray-50 p-2">Enhet</th>
-          <th className="font-regular bg-gray-50 p-2">Verdi</th>
-          <th className="font-regular hidden bg-gray-50 p-2 sm:table-cell">
-            Media queries
-          </th>
-          <th className="font-regular bg-gray-50 p-2">Bruk</th>
-          <th className="font-regular hidden rounded-tr bg-gray-50 p-2 sm:table-cell">
-            <span className="sr-only">Kopi</span>
-          </th>
-        </tr>
-      </thead>
-      <tbody className="">
-        {breakpoints.map((x) => {
-          const Svg = getSvg(x.name);
-
-          return (
-            <tr
-              key={x.value}
-              className="peer border-b border-t border-gray-200 text-base last-of-type:rounded-b"
-            >
-              <td
-                className="border-t border-gray-200 px-2 py-1 text-5xl"
-                aria-hidden
-              >
-                {Svg}
-              </td>
-              <td className="border-t border-gray-200 px-2 py-1">{x.value}</td>
-              <td className="hidden border-t border-gray-200 px-2 py-1 sm:table-cell">
-                {x.description}
-              </td>
-              <td className="border-t border-gray-200 px-2 py-1">
-                {Number(x.value.replace("px", "")) % 2 === 0
+    <AkselTable
+      withCopy
+      th={[
+        { text: "Enhet" },
+        { text: "Verdi" },
+        { text: "Media queries", hideOnSm: true },
+        { text: "Bruk" },
+      ]}
+    >
+      {breakpoints.map((x) => (
+        <AkselTableRow
+          key={x.name}
+          tr={[
+            { text: getSvg(x.name) },
+            { text: x.value },
+            { text: x.description, hideOnSm: true },
+            {
+              text:
+                Number(x.value.replace("px", "")) % 2 === 0
                   ? "Mobile first"
-                  : "Desktop first"}
-              </td>
-              <td className="hidden border-t border-gray-200 px-2 py-1 pr-4 sm:table-cell">
-                <CopyButton
-                  copyText={x.name}
-                  size="small"
-                  className="ml-auto"
-                />
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+                  : "Desktop first",
+            },
+          ]}
+          copyText={x.name}
+        />
+      ))}
+    </AkselTable>
   );
 };
 
 function getSvg(token: string) {
   switch (true) {
     case token.includes("xs"):
-      return <MobileSmallIcon title="Mobile small" />;
+      return <MobileSmallIcon title="Mobile small" fontSize="3rem" />;
+
     case token.includes("sm"):
-      return <MobileIcon title="Mobile regular" />;
+      return <MobileIcon title="Mobile regular" fontSize="3rem" />;
 
     case token.includes("md"):
-      return <TabletIcon title="Tablet" />;
+      return <TabletIcon title="Tablet" fontSize="3rem" />;
 
     case token.includes("lg"):
-      return <LaptopIcon title="Laptop" />;
+      return <LaptopIcon title="Laptop" fontSize="3rem" />;
     case token.includes("xl"):
-      return <MonitorIcon title="Dekstop" />;
+      return <MonitorIcon title="Dekstop" fontSize="3rem" />;
 
     default:
       return null;
--- aksel.nav.no/website/components/sanity-modules/token-view/categories/Spacing.tsx
+++ aksel.nav.no/website/components/sanity-modules/token-view/categories/Spacing.tsx
@@ -1,48 +1,41 @@
 import docs from "@navikt/ds-tokens/docs.json";
-import { CopyButton } from "@navikt/ds-react";
+import { AkselTable, AkselTableRow } from "components/website-modules/Table";
 
 export const SpacingView = ({ cat }: { cat: string }) => {
   const spacings = docs[cat];
 
   return (
-    <table className="mb-7 w-full border-separate border-spacing-0 rounded border border-gray-200">
-      <thead>
-        <tr className="rounded-t text-left">
-          <th className="font-regular rounded-tl bg-gray-50 p-2">Token</th>
-          <th className="font-regular bg-gray-50 p-2">Rem</th>
-          <th className="font-regular bg-gray-50 p-2">px</th>
-          <th className="font-regular hidden rounded-tr bg-gray-50 p-2 sm:table-cell">
-            <span className="sr-only">Kopi</span>
-          </th>
-        </tr>
-      </thead>
-      <tbody className="">
-        {spacings.map((x) => (
-          <tr
-            key={x.value}
-            className="peer border-b border-t border-gray-200 text-base last-of-type:rounded-b"
-          >
-            <td className="border-t border-gray-200 px-2 py-1">
-              {x.name.replace("--a-spacing-", "")}
-            </td>
-            <td className="border-t border-gray-200 px-2 py-1">
-              {x.value.replace("rem", "")}
-            </td>
-            <td className="border-t border-gray-200 px-2 py-1">
-              {Number(x.value.replace("rem", "") * 16)}
-            </td>
-            <td className="hidden items-center justify-between border-t border-gray-200 px-2 py-1 pr-4 sm:flex">
-              <div
-                className="bg-surface-alt-3-strong mr-auto h-8 rounded-md"
-                style={{
-                  width: x.value,
-                }}
-              />
-              <CopyButton copyText={x.name} size="small" />
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <AkselTable
+      withCopy
+      th={[
+        { text: "Token" },
+        { text: "rem" },
+        { text: "px" },
+        { text: "demo", sronly: true, hideOnSm: true },
+      ]}
+    >
+      {spacings.map((x) => (
+        <AkselTableRow
+          key={x.name}
+          tr={[
+            { text: x.name.replace("--a-spacing-", "") },
+            { text: x.value.replace("rem", "") },
+            { text: Number(x.value.replace("rem", "") * 16) },
+            {
+              hideOnSm: true,
+              text: (
+                <div
+                  className="bg-surface-alt-3-strong mr-auto h-8 rounded-md"
+                  style={{
+                    width: x.value,
+                  }}
+                />
+              ),
+            },
+          ]}
+          copyText={x.name}
+        />
+      ))}
+    </AkselTable>
   );
 };
