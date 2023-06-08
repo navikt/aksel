db8c38a094cd183db54aebecb62d5b223920a040
Ken <26967723+KenAJoh@users.noreply.github.com>
2023-06-08 14:44:08 +0200

    [Ny komponent] Skeleton (#1821)

---
+++ .changeset/wise-icons-guess.md
@@ -0,0 +1,8 @@
+---
+"@navikt/ds-css": minor
+"@navikt/ds-react": minor
+---
+
+:tada: Ny komponent Skeleton!
+
+- varianter: text, circle, rounded og rectangle
---
+++ @navikt/core/react/src/skeleton/Skeleton.tsx
@@ -0,0 +1,48 @@
+import React, { forwardRef } from "react";
+import cl from "clsx";
+
+export interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {
+  children?: React.ReactNode;
+  /**
+   * @default text
+   */
+  variant?: "circle" | "rectangle" | "rounded" | "text";
+  /**
+   * When not inferring height from children, you must specify height
+   */
+  height?: number | string;
+  /**
+   * When not infering width from children, you must specify width
+   */
+  width?: number | string;
+}
+
+export const Skeleton = forwardRef<HTMLDivElement, SkeletonProps>(
+  (
+    { className, children, height, width, style, variant = "text", ...rest },
+    ref
+  ) => {
+    return (
+      <div
+        {...rest}
+        ref={ref}
+        className={cl(
+          "navds-skeleton",
+          className,
+          `navds-skeleton--${variant}`,
+          {
+            "navds-skeleton--has-children": Boolean(children),
+            "navds-skeleton--no-height": !height,
+            "navds-skeleton--no-width": !width,
+          }
+        )}
+        style={{ ...style, width, height }}
+        aria-hidden
+      >
+        {children}
+      </div>
+    );
+  }
+);
+
+export default Skeleton;
