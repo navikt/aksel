61908119dce6c0289ee674559d03766fc77a9dab
Ken <ken.aleksander@gmail.com>
2023-06-08 11:51:54 +0200

    :art: Token global copybutton

---
+++ aksel.nav.no/website/components/sanity-modules/token-view/Copy.tsx
@@ -0,0 +1,38 @@
+import { ClipboardIcon } from "@navikt/aksel-icons";
+import { useEffect, useRef, useState } from "react";
+
+export const Copy = ({ text, copyText }) => {
+  const [active, setActive] = useState(false);
+  const timeoutRef = useRef<number>();
+
+  useEffect(() => {
+    return () => {
+      timeoutRef.current && clearTimeout(timeoutRef.current);
+    };
+  }, []);
+
+  const handleClick = () => {
+    timeoutRef.current && clearTimeout(timeoutRef.current);
+
+    navigator.clipboard.writeText(copyText);
+    setActive(true);
+
+    timeoutRef.current = window.setTimeout(() => {
+      setActive(false);
+    }, 2000);
+  };
+
+  return (
+    <button
+      type="button"
+      aria-live="polite"
+      className="py-05 bg-surface-neutral-subtle focus-visible:shadow-focus min-h-8 text-medium hover:bg-surface-neutral-subtle-hover group rounded px-1 leading-none focus:outline-none"
+      onClick={handleClick}
+    >
+      <span className="inline-flex flex-row-reverse items-center gap-1">
+        <ClipboardIcon title={active ? "kopiert" : "kopier css-variabel"} />
+        <span aria-live="polite">{text}</span>
+      </span>
+    </button>
+  );
+};
--- aksel.nav.no/website/components/sanity-modules/token-view/categories/Global.tsx
+++ aksel.nav.no/website/components/sanity-modules/token-view/categories/Global.tsx
@@ -1,9 +1,7 @@
-import { FilesIcon } from "@navikt/aksel-icons";
-import { BodyShort, CopyButton } from "@navikt/ds-react";
 import docs from "@navikt/ds-tokens/docs.json";
-import { sanitizeName, getColorString } from "../utilities";
-import color from "tinycolor2";
 import { Grid } from "../Grid";
+import { getColorString, sanitizeName } from "../utilities";
+import { Copy } from "../Copy";
 
 export const GlobalView = ({ cat }: { cat: string }) => {
   const colors: { name: string; value: string }[] = docs[cat];
@@ -17,27 +15,16 @@ export const GlobalView = ({ cat }: { cat: string }) => {
                 background: `var(${x.name})`,
                 boxShadow: `inset 0 2px 4px 0 rgba(0,0,0,0.06)`,
               }}
-              className="group mr-3 grid h-12 w-12 place-content-center rounded-lg"
-            >
-              <CopyButton
-                variant="neutral"
-                copyText={x.name}
-                className="focus-visible:shadow-focus-gap rounded-lg opacity-0 duration-0 focus-visible:opacity-100 group-hover:opacity-100 group-hover:transition-opacity"
-                icon={<FilesIcon title={x.name} fontSize="1.5rem" />}
-                style={{
-                  color: color
-                    .mostReadable(x.value, ["#fff", "#262626"])
-                    .toHexString(),
-                }}
-              />
-            </div>
-
-            <BodyShort as="dl">
-              <dt>{sanitizeName(x.name)}</dt>
+              className="mr-3 grid h-16 w-16 rounded-lg"
+            />
+            <dl>
+              <dt className="inline-flex items-center gap-2">
+                <Copy text={sanitizeName(x.name)} copyText={x.name} />
+              </dt>
               <dd className="text-text-subtle text-medium">
                 {getColorString(x.value)}
               </dd>
-            </BodyShort>
+            </dl>
           </div>
         );
       })}
