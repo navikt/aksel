b9ce9cd6810344d3650bd999b64f88a8ef2a57f5
Halvor Haugan <halvor.haugan@nav.no>
2023-06-13 13:20:31 +0200

    small variant in sb

--- @navikt/core/react/src/chat/chat.stories.tsx
+++ @navikt/core/react/src/chat/chat.stories.tsx
@@ -55,6 +55,26 @@ export const Default = {
   },
 };
 
+export const Small = () => {
+  return (
+    <Chat
+      avatar="ON"
+      name="Ola Normann"
+      timestamp="01.01.21 14:00"
+      size="small"
+    >
+      <Chat.Bubble>
+        Aute minim nisi sunt mollit duis sunt nulla minim non proident.
+      </Chat.Bubble>
+      <Chat.Bubble>Tempor fugiat amet eu sint in in ullamco.</Chat.Bubble>
+      <Chat.Bubble>
+        Adipisicing laborum est eu laborum est sit in commodo enim sint laboris
+        labore nisi ut.
+      </Chat.Bubble>
+    </Chat>
+  );
+};
+
 export const Colors = () => {
   return (
     <Chat
