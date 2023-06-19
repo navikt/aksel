b68f1b3565f7d5ab2c75c8a0007cd38fe3dd0cbd
Ken <ken.aleksander@gmail.com>
2023-06-19 10:21:00 +0200

    :art: Updated skeleton

--- aksel.nav.no/website/components/layout/header/search/Error.tsx
+++ aksel.nav.no/website/components/layout/header/search/Error.tsx
@@ -1,5 +1,5 @@
 import { searchOptions } from "@/types";
-import { Skeleton as DsSkeleton } from "@navikt/ds-react";
+import { Skeleton } from "@navikt/ds-react";
 import { useContext } from "react";
 import { SearchResultContext } from "./providers";
 
@@ -8,33 +8,29 @@ export const Error = () => {
 
   if (error) {
     return (
-      <p aria-live="assertive">
+      <p aria-live="assertive" className="mx-auto w-fit px-6 py-24">
         Noe gikk galt! Last siden på nytt eller ta kontakt med Aksel.
       </p>
     );
   }
+
   if (!isValidating) {
     return null;
   }
 
   return (
-    <>
-      <div className="mt-16 hidden md:block">
-        <div className="grid gap-4">
-          {Object.keys(searchOptions).map((_, xi) => (
-            <span key={xi} className="flex items-center gap-4">
-              <DsSkeleton height="2rem" width="2rem" variant="rectangle" />
-              <DsSkeleton height="2rem" width="80%" variant="text" />
-            </span>
+    <div className="grid w-full gap-2 p-1">
+      <Skeleton height="3rem" variant="rounded" />
+
+      <div className="flex flex-wrap gap-2 px-4 pb-4 md:px-10">
+        {Object.keys(searchOptions)
+          .slice(0, Object.keys(searchOptions).length - 1)
+          .map((x) => (
+            <Skeleton variant="rounded" height="1.75rem">
+              {searchOptions[x].display}
+            </Skeleton>
           ))}
-        </div>
       </div>
-      <div className="w-full">
-        <div className="grid gap-2">
-          <DsSkeleton width="10rem" variant="text" />
-          <DsSkeleton height="3rem" variant="rounded" />
-        </div>
-      </div>
-    </>
+    </div>
   );
 };
