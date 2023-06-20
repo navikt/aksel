139743192bdf0a821c2972a9d8b33378a39d8281
Ken <ken.aleksander@gmail.com>
2023-06-20 13:16:10 +0200

    :memo: Jsdoc

--- @navikt/core/react/src/layout/stack/HStack.tsx
+++ @navikt/core/react/src/layout/stack/HStack.tsx
@@ -4,6 +4,25 @@ import { StackProps, Stack } from "./Stack";
 
 export type HStackProps = Omit<StackProps, "direction">;
 
+/**
+ * Layout-primitive for horizontal flexbox
+ *
+ * @see [📝 Documentation](https://aksel.nav.no/komponenter/core/Stack)
+ * @see 🏷️ {@link HStackProps}
+ *
+ * @example
+ * <HStack gap="8">
+ *  <MyComponent />
+ *  <MyComponent />
+ * </HStack>
+ *
+ * @example
+ * // Responsive gap
+ * <HStack gap={{xs: "2", md: "6"}}>
+ *  <MyComponent />
+ *  <MyComponent />
+ * </HStack>
+ */
 export const HStack: OverridableComponent<HStackProps, HTMLDivElement> =
   forwardRef((props, ref) => {
     return <Stack {...props} ref={ref} direction="row" />;
--- @navikt/core/react/src/layout/stack/VStack.tsx
+++ @navikt/core/react/src/layout/stack/VStack.tsx
@@ -4,6 +4,25 @@ import { StackProps, Stack } from "./Stack";
 
 export type VStackProps = Omit<StackProps, "direction" | "wrap">;
 
+/**
+ * Layout-primitive for vetical flexbox
+ *
+ * @see [📝 Documentation](https://aksel.nav.no/komponenter/core/Stack)
+ * @see 🏷️ {@link VStackProps}
+ *
+ * @example
+ * <VStack gap="8">
+ *  <MyComponent />
+ *  <MyComponent />
+ * </VStack>
+ *
+ * @example
+ * // Responsive gap
+ * <VStack gap={{xs: "2", md: "6"}}>
+ *  <MyComponent />
+ *  <MyComponent />
+ * </VStack>
+ */
 export const VStack: OverridableComponent<VStackProps, HTMLDivElement> =
   forwardRef((props, ref) => {
     return <Stack {...props} ref={ref} direction="column" wrap={false} />;
