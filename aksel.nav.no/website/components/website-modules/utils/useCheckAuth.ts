cf73724c13303638c13c2543c961e70fab435617
Ken <ken.aleksander@gmail.com>
2023-07-03 13:21:07 +0200

    :art: Fungerende auth mot sanity

--- aksel.nav.no/website/components/website-modules/utils/useCheckAuth.ts
+++ aksel.nav.no/website/components/website-modules/utils/useCheckAuth.ts
@@ -1,17 +1,33 @@
 import { clientConfig } from "@/sanity/config";
-import { useState, useEffect } from "react";
-import { useCurrentUser } from "sanity";
+import { useEffect, useState } from "react";
+let globalUser = null;
 
 export const useCheckAuth = () => {
-  /* const [user, setUser] = useState<boolean>(true); */
-  /* const user = useCurrentUser(); */
-  /* useEffect(() => {
-    const handleAuth = async () => {
-      user =
-      _checkAuth(clientConfig.projectId, null).then(setUser);
-    };
-    handleAuth();
-  }, []); */
+  const [user, setUser] = useState(true);
 
-  return true;
+  useEffect(() => {
+    if (globalUser !== null) {
+      return;
+    }
+    const fetchUser = async () => {
+      const response = await fetch(
+        `https://${clientConfig.projectId}.api.sanity.io/v1/users/me`,
+        {
+          credentials: "include",
+        }
+      ).then((x) => x.text());
+      try {
+        setUser(!!JSON.parse(response)?.id);
+        globalUser = !!JSON.parse(response)?.id;
+        return;
+      } catch (e) {
+        console.log("Failed fetching user from sanity");
+      }
+      setUser(false);
+      globalUser = false;
+    };
+    fetchUser();
+  }, []);
+
+  return user;
 };
