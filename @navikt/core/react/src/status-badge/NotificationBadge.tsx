359e3fba19d2a8f26447948bedec25a09a8a6980
Ken <ken.aleksander@gmail.com>
2023-07-04 13:27:35 +0200

    :art: refactored badge to match figma

--- @navikt/core/react/src/status-badge/NotificationBadge.tsx
+++ @navikt/core/react/src/status-badge/NotificationBadge.tsx
@@ -1,30 +1,11 @@
-import React, { forwardRef } from "react";
+import React, { HTMLAttributes, forwardRef } from "react";
 import cl from "clsx";
 
-export interface NotificationBadgeProps {
+export interface NotificationBadgeProps
+  extends HTMLAttributes<HTMLSpanElement> {
   children?: React.ReactNode;
   className?: string;
-  /**
-   * Changes visuals of status
-   * @default neutral
-   */
-  variant?:
-    | "warning"
-    | "error"
-    | "info"
-    | "success"
-    | "neutral"
-    | "alt1"
-    | "alt2"
-    | "alt3";
-  /**
-   * @default "marker"
-   */
-  shape?: "marker" | "count";
-  /**
-   *
-   */
-  position?: "top-left" | "top-right" | "bottom-left" | "bottom-right";
+  absolute?: boolean;
   /**
    *
    * @default false
@@ -35,37 +16,22 @@ export interface NotificationBadgeProps {
 export const NotificationBadge = forwardRef<
   HTMLSpanElement,
   NotificationBadgeProps
->(
-  (
-    {
-      className,
-      children,
-      variant = "neutral",
-      shape = "marker",
-      position,
-      pulse = false,
-      ...rest
-    },
-    ref
-  ) => {
-    return (
+>(({ className, children, absolute = false, pulse = false, ...rest }, ref) => {
+  return (
+    <span className="navds-notificationbadge__wrapper">
       <span
         {...rest}
         ref={ref}
-        className={cl(
-          "navds-notificationbadge",
-          className,
-          `navds-notificationbadge--${variant}`,
-          `navds-notificationbadge--${shape}`,
-          {
-            [`navds-notificationbadge--${position}`]: !!position,
-            "navds-notificationbadge--pulse": pulse,
-          }
-        )}
-        children={shape === "count" ? children : null}
-      />
-    );
-  }
-);
+        className={cl("navds-notificationbadge", className, {
+          "navds-notificationbadge--pulse": pulse,
+          "navds-notificationbadge--count": !!children,
+          "navds-notificationbadge--marker": !children,
+        })}
+      >
+        {children}
+      </span>
+    </span>
+  );
+});
 
 export default NotificationBadge;
--- @navikt/core/react/src/status-badge/notification-badge.stories.tsx
+++ @navikt/core/react/src/status-badge/notification-badge.stories.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { NotificationBadge } from "./NotificationBadge";
+import { BellIcon } from "@navikt/aksel-icons";
+import { Button } from "../button";
 
 export default {
   title: "ds-react/NotificationBadge",
@@ -10,129 +12,27 @@ export const Default = {
   render: () => {
     return (
       <div className="colgap" style={{ gap: "2rem" }}>
-        <Box label="positions">
-          <NotificationBadge shape="count" position="bottom-left">
-            99
-          </NotificationBadge>
-          <NotificationBadge shape="count" position="top-left">
-            99
-          </NotificationBadge>
-          <NotificationBadge shape="count" position="bottom-right">
-            99
-          </NotificationBadge>
-          <NotificationBadge shape="count" position="top-right">
-            99
-          </NotificationBadge>
+        <Box>
+          <NotificationBadge>42</NotificationBadge>
         </Box>
         <Box label="marker">
-          <NotificationBadge shape="marker" position="bottom-left" />
-          <NotificationBadge shape="marker" position="top-left" />
-          <NotificationBadge shape="marker" position="bottom-right" />
-          <NotificationBadge shape="marker" position="top-right" />
-        </Box>
-        <Box label="variants">
-          <NotificationBadge variant="info" position="top-right" />
-          <NotificationBadge variant="success" position="top-left" />
-          <NotificationBadge variant="error" position="bottom-right" />
-          <NotificationBadge variant="warning" position="bottom-left" />
-        </Box>
-        <Box label="variants">
-          <NotificationBadge variant="alt1" position="top-left" />
-          <NotificationBadge variant="alt2" position="bottom-right" />
-          <NotificationBadge variant="alt3" position="top-right" />
-        </Box>
-        <Box label="count+variant">
-          <NotificationBadge shape="count" variant="info" position="top-right">
-            99
-          </NotificationBadge>
-          <NotificationBadge
-            shape="count"
-            variant="success"
-            position="top-left"
-          >
-            99
-          </NotificationBadge>
-          <NotificationBadge
-            shape="count"
-            variant="error"
-            position="bottom-right"
-          >
-            99
-          </NotificationBadge>
-          <NotificationBadge
-            shape="count"
-            variant="warning"
-            position="bottom-left"
-          >
-            99
-          </NotificationBadge>
+          <NotificationBadge />
         </Box>
         <Box label="pulse">
-          <NotificationBadge
-            pulse
-            shape="count"
-            variant="info"
-            position="top-right"
-          >
-            99
-          </NotificationBadge>
-          <NotificationBadge
-            pulse
-            shape="count"
-            variant="success"
-            position="top-left"
-          >
-            99
-          </NotificationBadge>
-          <NotificationBadge
-            pulse
-            shape="count"
-            variant="error"
-            position="bottom-right"
-          >
-            99
-          </NotificationBadge>
-          <NotificationBadge
-            pulse
-            shape="count"
-            variant="warning"
-            position="bottom-left"
-          >
-            99
-          </NotificationBadge>
+          <NotificationBadge pulse>42</NotificationBadge>
         </Box>
-        <Box>
-          <NotificationBadge
-            pulse
-            shape="count"
-            variant="alt3"
-            position="top-left"
-          >
-            99
-          </NotificationBadge>
-          <NotificationBadge
-            pulse
-            shape="count"
-            variant="alt1"
-            position="top-right"
-          >
-            99
-          </NotificationBadge>
-          <NotificationBadge
-            pulse
-            shape="count"
-            variant="alt2"
-            position="bottom-left"
-          >
-            99
-          </NotificationBadge>
-        </Box>
-        <Box label="pulse">
-          <NotificationBadge pulse variant="info" position="top-right" />
-          <NotificationBadge pulse variant="success" position="top-left" />
-          <NotificationBadge pulse variant="error" position="bottom-right" />
-          <NotificationBadge pulse variant="warning" position="bottom-left" />
-        </Box>
+        <div style={{ position: "relative", width: "fit-content" }}>
+          <Button
+            variant="tertiary"
+            size="small"
+            icon={
+              <>
+                <BellIcon />
+                <NotificationBadge pulse />
+              </>
+            }
+          ></Button>
+        </div>
       </div>
     );
   },
@@ -144,8 +44,8 @@ const Box = ({ ...props }) => (
       position: "relative",
       width: "6rem",
       borderRadius: 4,
-      height: "6rem",
-      background: "var(--a-gray-900)",
+      height: "2rem",
+      background: "var(--a-surface-alt-1-moderate)",
       color: "white",
       fontSize: 14,
       display: "grid",
