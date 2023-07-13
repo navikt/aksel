211f3f8822df7ec3b0164baabb63678f8f783a0b
Lars Hansen <lars.eirik.korsgaard.hansen@nav.no>
2023-07-13 16:57:59 +0200

    add controlled example

---
+++ aksel.nav.no/website/pages/eksempler/combobox/multi-select-controlled.tsx
@@ -0,0 +1,64 @@
+import { UNSAFE_Combobox } from "@navikt/ds-react";
+import { withDsExample } from "components/website-modules/examples/withDsExample";
+import { useMemo, useState } from "react";
+
+const initialOptions = [
+  "Norge",
+  "Sverige",
+  "Danmark",
+  "Finland",
+  "Island",
+  "Storbritannia",
+  "Tyskland",
+  "Frankrike",
+  "Spania",
+  "Portugal",
+  "Italia",
+  "Hellas",
+  "Kroatia",
+  "Tyrkia",
+];
+
+const initialSelectedOptions = ["Norge"];
+
+export const Example = () => {
+  const [value, setValue] = useState("");
+  const [selectedOptions, setSelectedOptions] = useState(
+    initialSelectedOptions
+  );
+  const filteredOptions = useMemo(
+    () =>
+      initialOptions.filter((initialOptions) => initialOptions.includes(value)),
+    [value]
+  );
+
+  const onToggleSelected = (option, isSelected) => {
+    if (isSelected) {
+      setSelectedOptions([...selectedOptions, option]);
+    } else {
+      setSelectedOptions(selectedOptions.filter((o) => o !== option));
+    }
+  };
+
+  return (
+    <div>
+      <UNSAFE_Combobox
+        label="Hvilke land har du besøkt de siste 6 ukene? Velg opptil flere."
+        filteredOptions={filteredOptions}
+        isMultiSelect
+        onChange={(event) => setValue(event.target.value)}
+        onToggleSelected={onToggleSelected}
+        selectedOptions={selectedOptions}
+        options={initialOptions}
+        value={value}
+      />
+    </div>
+  );
+};
+
+export default withDsExample(Example, "static");
+
+export const args = {
+  index: 1,
+  desc: "Du kan Ved Multi Select kan brukeren velge flere valg fra nedtrekkslisten.",
+};
