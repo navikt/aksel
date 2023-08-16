88256a2bc3d3c81a3de0fa4413be13220f3079db
Ken <ken.aleksander@gmail.com>
2023-08-16 13:33:15 +0200

    :fire: Fjernet gammel story

--- @navikt/core/react/src/modal/modal.stories.tsx
+++ @navikt/core/react/src/modal/modal.stories.tsx
@@ -165,18 +165,3 @@ export const Medium = () => (
     <Modal.Body>Lorem ipsum dolor sit amet.</Modal.Body>
   </Modal>
 );
-
-export const Large = () => (
-  <Modal open width="small">
-    <Modal.Header />
-    <Modal.Body>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </Modal.Body>
-  </Modal>
-);
