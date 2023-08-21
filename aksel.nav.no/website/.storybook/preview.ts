d0548fbcea127373b752b416f14f36f6b7afb2da
Ken <ken.aleksander@gmail.com>
2023-08-21 12:43:05 +0200

    :memo: Fjernet kommentarer

--- aksel.nav.no/website/.storybook/preview.ts
+++ aksel.nav.no/website/.storybook/preview.ts
@@ -1,6 +1,5 @@
 import type { Preview } from "@storybook/react";
 
-/* TODO: update import to your tailwind styles file. If you're using Angular, inject this through your angular.json config instead */
 import "../dist/tw.css";
 
 const preview: Preview = {
