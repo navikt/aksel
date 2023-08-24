df82e3216b455c6546b886848fd005c8da09d744
Ken <ken.aleksander@gmail.com>
2023-08-24 12:03:43 +0200

    :label: Oppdatert ts-typer

--- @navikt/aksel-icons/icons.stories.tsx
+++ @navikt/aksel-icons/icons.stories.tsx
@@ -1,46 +1,52 @@
-import React from "react";
-import { Meta } from "@storybook/react";
 import * as AkselIcons from "@navikt/aksel-icons";
+import { StarFillIcon } from "@navikt/aksel-icons";
+import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
 
-export default {
+const meta = {
   title: "aksel-icons/Icons",
-} as Meta;
+} satisfies Meta<typeof StarFillIcon>;
 
-export const Icons = () => {
-  return (
-    <div className="colgap">
-      {Object.entries(AkselIcons).map(([key, Value]) => (
-        <div key={key} className="icon-section">
-          <span>{key}</span>
-          <span className="icon-group">
-            <span>
-              <Value fontSize="3rem" aria-hidden title={key} />
-            </span>
-            <span>
-              <Value fontSize="3rem" aria-hidden title={key + " Inverted"} />
-            </span>
-          </span>
-        </div>
-      ))}
-      <style>
-        {`
-        .icon-section{
-          display: grid;
-          grid-template-columns: repeat(2, 1fr);
-          gap: var(--a-spacing-4);
-          align-items: center;
-        }
-        .icon-group {
-          display: flex;
-          gap: var(--a-spacing-4)
-        }
+export default meta;
+type Story = StoryObj<typeof meta>;
 
-        .icon-group > span:last-of-type {
-          background: var(--a-surface-inverted);
-          color: var(--a-icon-on-inverted);
-        }
-        `}
-      </style>
-    </div>
-  );
+export const Icons: Story = {
+  render: () => {
+    return (
+      <div className="colgap">
+        {Object.entries(AkselIcons).map(([key, Value]) => (
+          <div key={key} className="icon-section">
+            <span>{key}</span>
+            <span className="icon-group">
+              <span>
+                <Value fontSize="3rem" aria-hidden title={key} />
+              </span>
+              <span>
+                <Value fontSize="3rem" aria-hidden title={key + " Inverted"} />
+              </span>
+            </span>
+          </div>
+        ))}
+        <style>
+          {`
+          .icon-section{
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            gap: var(--a-spacing-4);
+            align-items: center;
+          }
+          .icon-group {
+            display: flex;
+            gap: var(--a-spacing-4)
+          }
+
+          .icon-group > span:last-of-type {
+            background: var(--a-surface-inverted);
+            color: var(--a-icon-on-inverted);
+          }
+          `}
+        </style>
+      </div>
+    );
+  },
 };
