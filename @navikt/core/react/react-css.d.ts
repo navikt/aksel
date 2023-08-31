bed689dd5d645d5809a4172995b92cfc5d1dbbbf
Halvor Haugan <halvor.haugan@nav.no>
2023-08-31 12:48:50 +0200

    ny illustrasjon, justering av d.ts-fil

--- @navikt/core/react/react-css.d.ts
+++ @navikt/core/react/react-css.d.ts
@@ -1,9 +1,1 @@
-// eslint-disable-next-line react/no-typos
-import "react";
-
-declare module "react" {
-  interface CSSProperties {
-    [key: `--ac-${string}`]: string | undefined;
-    [key: `--__ac-${string}`]: string | undefined;
-  }
-}
+/// <reference path="./src/react-css.d.ts" />
