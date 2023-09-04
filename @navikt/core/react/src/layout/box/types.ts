c686bca72305415070799d201778de4da3e6e936
Julian Nymark <julian.nymark@nav.no>
2023-09-04 13:45:25 +0200

    :construction: test with module identifier import

--- @navikt/core/react/src/layout/box/types.ts
+++ @navikt/core/react/src/layout/box/types.ts
@@ -1,18 +1,18 @@
-// import bgColors from "../../../../tokens/src/colors-bg.json";
-// import surfaceColors from "../../../../tokens/src/colors-surface.json";
-// import borderColors from "../../../../tokens/src/colors-border.json";
-// import borderRadii from "../../../../tokens/src/border.json";
-// import shadows from "../../../../tokens/src/shadow.json";
+import bgColors from "@navikt/ds-tokens/src/colors-bg.json";
+import surfaceColors from "@navikt/ds-tokens/src/colors-surface.json";
+import borderColors from "@navikt/ds-tokens/src/colors-border.json";
+import borderRadii from "@navikt/ds-tokens/src/border.json";
+import shadows from "@navikt/ds-tokens/src/shadow.json";
 
-// type BackgroundColors = keyof typeof bgColors.a | keyof typeof surfaceColors.a;
-// type BorderColors = keyof typeof borderColors.a;
-// type BorderRadii = keyof typeof borderRadii.a.border.radius;
-// type Shadows = keyof typeof shadows.a.shadow;
+type BackgroundColors = keyof typeof bgColors.a | keyof typeof surfaceColors.a;
+type BorderColors = keyof typeof borderColors.a;
+type BorderRadii = keyof typeof borderRadii.a.border.radius;
+type Shadows = keyof typeof shadows.a.shadow;
 
-type BackgroundColors = any;
-type BorderColors = any;
-type BorderRadii = any;
-type Shadows = any;
+// type BackgroundColors = any;
+// type BorderColors = any;
+// type BorderRadii = any;
+// type Shadows = any;
 
 export type BackgroundSpecifier = BackgroundColors;
 
