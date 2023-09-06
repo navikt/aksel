6833d9653959ae9f6ada2615afc0316789d8098a
Julian Nymark <julian.nymark@nav.no>
2023-09-06 14:10:23 +0200

    :fire: remove style-dicitonary format (now unused), fix shadow.json refs

--- @navikt/core/tokens/config/config.js
+++ @navikt/core/tokens/config/config.js
@@ -43,10 +43,6 @@ module.exports = {
           destination: "tokens.d.ts",
           format: "typescript/es6-declarations",
         },
-        {
-          destination: "tokens-module.d.ts",
-          format: "typescript/module-declarations",
-        },
       ],
     },
     js: {
