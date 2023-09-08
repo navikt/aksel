1b0e276c45f5364ebd638513d88baab472f27cd5
Ken <ken.aleksander@gmail.com>
2023-09-08 10:34:41 +0200

    :art: Skrever seg bort fra sanity-ui

--- aksel.nav.no/website/sanity/schema/custom-components/TemaView.tsx
+++ aksel.nav.no/website/sanity/schema/custom-components/TemaView.tsx
@@ -1,4 +1,5 @@
-import { Card, Heading, Stack } from "@sanity/ui";
+import { Heading, VStack } from "@navikt/ds-react";
+
 import { useEffect, useState } from "react";
 import {
   getPublishedId,
@@ -9,7 +10,7 @@ import {
   useSchema,
 } from "sanity";
 
-export function TemaView(props) {
+export function TemaView() {
   const client = useClient({ apiVersion: "2021-06-07" });
   const title = useFormValue([`title`]);
   const schema = useSchema();
@@ -38,28 +39,26 @@ export function TemaView(props) {
   const notFound = artikler.filter((x) => !sider.includes(x._id));
 
   return (
-    <Stack space={3}>
-      <Heading size={1}>
-        {`Artikler ikke i seksjoner (${notFound.length})`}
+    <VStack gap="1">
+      <Heading size="xsmall" level="2" spacing>
+        {`Artikler uten seksjoner (${notFound.length})`}
       </Heading>
       {notFound.map((x) => (
-        <Card flex={1}>
-          <IntentButton
-            key={x._id}
-            intent="edit"
-            mode="ghost"
-            padding={1}
-            radius={0}
-            params={{
-              type: "aksel_artikkel",
-              id: getPublishedId(x._id),
-            }}
-            style={{ width: "100%" }}
-          >
-            <Preview layout="default" schemaType={type} value={x} key={x._id} />
-          </IntentButton>
-        </Card>
+        <IntentButton
+          key={x._id}
+          intent="edit"
+          mode="ghost"
+          padding={1}
+          radius={0}
+          params={{
+            type: "aksel_artikkel",
+            id: getPublishedId(x._id),
+          }}
+          style={{ width: "100%" }}
+        >
+          <Preview layout="default" schemaType={type} value={x} key={x._id} />
+        </IntentButton>
       ))}
-    </Stack>
+    </VStack>
   );
 }
