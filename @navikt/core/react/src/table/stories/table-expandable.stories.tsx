db77f84ae7b7a3b68c6a32b511da82a5d7be40db
Halvor Haugan <halvor.haugan@nav.no>
2023-09-08 16:59:24 +0200

    Unngå eslint-disable react-hooks/rules-of-hooks

--- @navikt/core/react/src/table/stories/table-expandable.stories.tsx
+++ @navikt/core/react/src/table/stories/table-expandable.stories.tsx
@@ -265,62 +265,58 @@ export const ExpandableOpen = () => {
   );
 };
 
-export const ClickableRow = {
-  render: () => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [isRowOpen1, setIsRowOpen1] = useState(false);
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [isRowOpen2, setIsRowOpen2] = useState(false);
+export const ClickableRow = () => {
+  const [isRowOpen1, setIsRowOpen1] = useState(false);
+  const [isRowOpen2, setIsRowOpen2] = useState(false);
 
-    return (
-      <>
-        <Table zebraStripes>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Navn</Table.HeaderCell>
-              <Table.HeaderCell>Info</Table.HeaderCell>
-              <Table.HeaderCell aria-hidden />
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            <Table.ExpandableRow
-              content={<div>placeholder row 1</div>}
-              togglePlacement="right"
-              onOpenChange={setIsRowOpen1}
-              data-testid="row1"
-              open={isRowOpen1}
-              expandOnRowClick
-            >
-              <Table.DataCell>Ola</Table.DataCell>
-              <Table.DataCell>
-                <Button
-                  size="xsmall"
-                  onClick={() => {
-                    alert("Mer info");
-                  }}
-                >
-                  Mer info
-                </Button>
-              </Table.DataCell>
-            </Table.ExpandableRow>
-            <Table.ExpandableRow
-              content={<div>placeholder row 2</div>}
-              togglePlacement="right"
-              onOpenChange={setIsRowOpen2}
-              data-testid="row2"
-              open={isRowOpen2}
-              expandOnRowClick
-            >
-              <Table.DataCell>Hans</Table.DataCell>
-              <Table.DataCell>
-                <Checkbox hideLabel size="small">
-                  Sett
-                </Checkbox>
-              </Table.DataCell>
-            </Table.ExpandableRow>
-          </Table.Body>
-        </Table>
-      </>
-    );
-  },
+  return (
+    <>
+      <Table zebraStripes>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Navn</Table.HeaderCell>
+            <Table.HeaderCell>Info</Table.HeaderCell>
+            <Table.HeaderCell aria-hidden />
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <Table.ExpandableRow
+            content={<div>placeholder row 1</div>}
+            togglePlacement="right"
+            onOpenChange={setIsRowOpen1}
+            data-testid="row1"
+            open={isRowOpen1}
+            expandOnRowClick
+          >
+            <Table.DataCell>Ola</Table.DataCell>
+            <Table.DataCell>
+              <Button
+                size="xsmall"
+                onClick={() => {
+                  alert("Mer info");
+                }}
+              >
+                Mer info
+              </Button>
+            </Table.DataCell>
+          </Table.ExpandableRow>
+          <Table.ExpandableRow
+            content={<div>placeholder row 2</div>}
+            togglePlacement="right"
+            onOpenChange={setIsRowOpen2}
+            data-testid="row2"
+            open={isRowOpen2}
+            expandOnRowClick
+          >
+            <Table.DataCell>Hans</Table.DataCell>
+            <Table.DataCell>
+              <Checkbox hideLabel size="small">
+                Sett
+              </Checkbox>
+            </Table.DataCell>
+          </Table.ExpandableRow>
+        </Table.Body>
+      </Table>
+    </>
+  );
 };
