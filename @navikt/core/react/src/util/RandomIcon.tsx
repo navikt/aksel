3cddb50f8e62683219b19ab47c6bdfcbd2e63f92
Ken <ken.aleksander@gmail.com>
2023-09-10 22:30:13 +0200

    :art: Allowcomputed i randomicons

--- @navikt/core/react/src/util/RandomIcon.tsx
+++ @navikt/core/react/src/util/RandomIcon.tsx
@@ -1,6 +1,7 @@
 import * as AllIcons from "@navikt/aksel-icons";
 import React from "react";
 
+/* eslint import/namespace: ['error', { allowComputed: true }] */
 const icons = Object.keys(AllIcons).map((key) => AllIcons[key]);
 
 export const RandomIcon = () => {
