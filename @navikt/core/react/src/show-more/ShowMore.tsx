77811b92f0df0067889d5ae52628f6464508c961
Halvor Haugan <halvor.haugan@nav.no>
2023-09-20 14:13:32 +0200

    Justeringer

--- @navikt/core/react/src/show-more/ShowMore.tsx
+++ @navikt/core/react/src/show-more/ShowMore.tsx
@@ -1,16 +1,17 @@
 import React, { forwardRef, useState } from "react";
 import cl from "clsx";
 import { ChevronDownIcon, ChevronUpIcon } from "@navikt/aksel-icons";
-import { Button, OverridableComponent } from "..";
+import { Button, Heading, HeadingProps, useId } from "..";
 
 interface ShowMoreBaseProps
-  extends Omit<React.HTMLAttributes<HTMLDivElement>, "onClick"> {
-  children: React.ReactNode;
+  extends Omit<React.HTMLAttributes<HTMLElement>, "onClick"> {
   /**
-   * Opens component if 'true', collapses if 'false'.
-   * Using this prop removes automatic control of open-state.
+   * Override what element to render the wrapper as.
+   * @default aside
    */
-  open?: boolean;
+  as?: "aside" | "section";
+  /** Content */
+  children: React.ReactNode;
   /**
    * Changes button size
    * @default medium
@@ -22,20 +23,34 @@ interface ShowMoreBaseProps
    */
   variant?: "inline" | "default" | "subtle" | "info";
   /**
-   * Custom collapsed height.
-   * @default 13.5rem
+   * Custom height of content when collapsed.
+   * @default 11.5rem if heading is used, otherwise 13.5rem
    */
   collapsedHeight?: `${number}${string}` | number;
-  /** Click handler for the show more/less button */
-  onClick?: React.MouseEventHandler<HTMLButtonElement>;
   /** Label for the content */
   "aria-label"?: string;
   /** ID of an element that labels the content */
   "aria-labelledby"?: string;
+  /**
+   * Heading text. Will always be available to screen readers, and will be
+   * used as accessible label unless `aria-label` or `aria-labelledby` is used.
+   */
+  heading?: string;
+  /**
+   * Heading size
+   * @default medium
+   */
+  headingSize?: HeadingProps["size"];
+  /** Heading level */
+  headingLevel?: HeadingProps["level"];
 }
 
 export type ShowMoreProps = ShowMoreBaseProps &
-  ({ "aria-label": string } | { "aria-labelledby": string });
+  (
+    | { "aria-label": string }
+    | { "aria-labelledby": string }
+    | { heading: string }
+  );
 
 /**
  * A component for partially hiding less important content.
@@ -48,70 +63,85 @@ export type ShowMoreProps = ShowMoreBaseProps &
  *   Toads have dry, leathery skin, short legs, and large bumps covering the parotoid glands.
  * </ShowMore>
  */
-export const ShowMore: OverridableComponent<ShowMoreProps, HTMLDivElement> =
-  forwardRef(
-    (
-      {
-        as: Component = "div",
-        children,
-        open,
-        size = "medium",
-        variant = "inline",
-        collapsedHeight = "13.5rem",
-        className,
-        onClick,
-        "aria-label": ariaLabel,
-        ...rest
-      },
-      ref
-    ) => {
-      const [internalOpen, setInternalOpen] = useState(false);
+export const ShowMore = forwardRef<HTMLElement, ShowMoreProps>(
+  (
+    {
+      as: Component = "aside",
+      children,
+      size = "medium",
+      variant = "inline",
+      collapsedHeight,
+      className,
+      "aria-labelledby": ariaLabelledby,
+      heading,
+      headingSize = "medium",
+      headingLevel,
+      ...rest
+    },
+    ref
+  ) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const ariaLabelId = useId();
 
-      const isOpen = open ?? internalOpen;
-      const ChevronIcon = isOpen ? ChevronUpIcon : ChevronDownIcon;
+    const ChevronIcon = isOpen ? ChevronUpIcon : ChevronDownIcon;
 
-      return (
-        <Component
-          ref={ref}
-          className={cl(
-            "navds-show-more",
-            `navds-show-more--${variant}`,
-            className,
-            { "navds-show-more--closed": !isOpen }
-          )}
-          {...rest}
+    return (
+      <Component
+        ref={ref}
+        className={cl(
+          "navds-show-more",
+          `navds-show-more--${variant}`,
+          className,
+          {
+            "navds-show-more--closed": !isOpen,
+            "navds-show-more--has-heading": heading,
+          }
+        )}
+        aria-labelledby={
+          !ariaLabelledby && !rest["aria-label"] && heading
+            ? ariaLabelId
+            : ariaLabelledby
+        }
+        {...rest}
+      >
+        {heading && (
+          <Heading size={headingSize} level={headingLevel} id={ariaLabelId}>
+            {heading}
+          </Heading>
+        )}
+
+        <div className="navds-show-more__button-section">
+          <div className="navds-show-more__button-wrapper">
+            <Button
+              type="button"
+              variant="secondary-neutral"
+              className="navds-show-more__button"
+              icon={<ChevronIcon aria-hidden />}
+              iconPosition="right"
+              size={size}
+              onClick={() => setIsOpen(!isOpen)}
+              aria-expanded={isOpen}
+            >
+              {isOpen ? "Vis mindre" : "Vis mer"}
+            </Button>
+          </div>
+        </div>
+
+        <div
+          className="navds-show-more__content"
+          style={
+            isOpen
+              ? {}
+              : { height: collapsedHeight ?? (heading ? "11.5rem" : "13.5rem") }
+          }
+          // @ts-expect-error https://github.com/DefinitelyTyped/DefinitelyTyped/pull/60822
+          inert={isOpen ? undefined : ""}
         >
-          <div className="navds-show-more__button-section">
-            <div className="navds-show-more__button-wrapper">
-              <Button
-                type="button"
-                variant="secondary-neutral"
-                size={size}
-                className="navds-show-more__button"
-                onClick={(e) => {
-                  if (open === undefined) setInternalOpen((isOpen) => !isOpen);
-                  onClick?.(e);
-                }}
-                aria-expanded={isOpen}
-                aria-label={ariaLabel}
-                icon={<ChevronIcon aria-hidden />}
-                iconPosition="right"
-              >
-                {isOpen ? "Vis mindre" : "Vis mer"}
-              </Button>
-            </div>
-          </div>
-
-          <div
-            className="navds-show-more__content"
-            style={isOpen ? {} : { height: collapsedHeight }}
-            aria-hidden={!isOpen}
-          >
-            {children}
-          </div>
-        </Component>
-      );
-    }
-  );
+          {children}
+        </div>
+      </Component>
+    );
+  }
+);
 
 export default ShowMore;
