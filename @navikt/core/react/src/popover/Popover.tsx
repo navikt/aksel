3d04aef902f1a4a42b7adeaf279ab0130a1c9e51
Vegard Haugstvedt <it.vegard@gmail.com>
2023-09-22 12:43:11 +0200

    Seems like getFloatingProps can take "tabIndex: undefined" as an option, reducing the amount of code we need to change.

--- @navikt/core/react/src/popover/Popover.tsx
+++ @navikt/core/react/src/popover/Popover.tsx
@@ -203,16 +203,6 @@ export const Popover = forwardRef<HTMLDivElement, PopoverProps>(
       left: "right",
     }[flPlacement.split("-")[0]];
 
-    const floatingProps = getFloatingProps({
-      ref: floatingRef,
-      style: {
-        position: strategy,
-        top: y ?? 0,
-        left: x ?? 0,
-      },
-    });
-    delete floatingProps.tabIndex;
-
     return (
       <div
         className={cl("navds-popover", className, {
@@ -220,7 +210,15 @@ export const Popover = forwardRef<HTMLDivElement, PopoverProps>(
         })}
         data-placement={flPlacement}
         aria-hidden={!open || !anchorEl}
-        {...floatingProps}
+        {...getFloatingProps({
+          ref: floatingRef,
+          style: {
+            position: strategy,
+            top: y ?? 0,
+            left: x ?? 0,
+          },
+          tabIndex: undefined,
+        })}
         {...rest}
       >
         {children}
