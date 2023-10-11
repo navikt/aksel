d9a1fc7768befe5b53cc945da86c15400a791af0
Ken <ken.aleksander@gmail.com>
2023-10-11 13:01:14 +0200

    :loud_sound: Added hotjar

--- aksel.nav.no/website/pages/_app.tsx
+++ aksel.nav.no/website/pages/_app.tsx
@@ -1,11 +1,12 @@
-import { AppProps } from "next/app";
 import { BaseSEO } from "components/website-modules/seo/BaseSEO";
 import {
   initAmplitude,
   usePageView,
 } from "components/website-modules/utils/amplitude";
 import { useScrollToHashOnPageLoad } from "components/website-modules/utils/util";
+import { AppProps } from "next/app";
 import { useEffect } from "react";
+import { hotjar } from "react-hotjar";
 import { IdContext } from "../components/website-modules/utils/contexts/id-context";
 import "../styles/index.css";
 
@@ -18,12 +19,13 @@ function App({ Component, pageProps, router }: AppProps) {
   useEffect(() => {
     window.location.host === "design.nav.no" &&
       window.location.replace(`http://aksel.nav.no`);
+
+    hotjar.initialize(148751, 6);
   }, []);
 
   return (
     <>
       <BaseSEO path={router.asPath} />
-
       <IdContext.Provider value={{ id: pageProps?.id ?? pageProps?.page?._id }}>
         <Component {...pageProps} />
       </IdContext.Provider>
