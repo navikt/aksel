8896008c0f79dd50db745009bfb97d3cf31c90df
Ken <ken.aleksander@gmail.com>
2023-10-11 11:43:22 +0200

    :art: Bedre håndtering av error

--- aksel.nav.no/website/scripts/update-props.tsx
+++ aksel.nav.no/website/scripts/update-props.tsx
@@ -32,7 +32,7 @@ async function updateProps() {
 
   const remoteProps = await noCdnClient(token).fetch(`*[_type == "ds_props"]`);
 
-  const deletedIds: string[] = [];
+  let deletedIds: string[] = [];
   for (const prop of remoteProps) {
     if (!props.find(({ _id }) => _id === prop._id)) {
       transactionClient.delete(prop._id);
@@ -40,37 +40,36 @@ async function updateProps() {
     }
   }
 
-  if (deletedIds.length > 0) {
-    console.log("\n");
-    console.log(
-      `Found ${deletedIds.length} prop definitions no longer documented.
-This could be caused by moving file-location of prop-definition, a namechange or simply not existing anymore.
-
-How to fix:
-- Go to links provided under and try to manually delete document.
-- You will then be prompted to update referenced document before deleting.
-- After updating reference(s) and deleting document(s) there is no need to run the script again.`
-    );
-    console.log(
-      JSON.stringify(
-        deletedIds.map(
-          (x) =>
-            `https://aksel.nav.no/admin/prod/desk/admin;propsDesignsystemet;${x}`
-        ),
-        null,
-        2
-      )
-    );
-  }
-
   await transactionClient
     .commit()
     .then(() => console.log("Successfully deleted unused prop-documents"))
-    .catch(() =>
-      console.error(
-        "Failed deleting still referenced document(s). See previous warning for fix."
-      )
-    );
+    .catch((e) => {
+      /**
+       * Errormessage includes all ids that failed.
+       */
+      deletedIds = deletedIds.filter((id) => e.message.includes(id));
+
+      console.log("\n");
+      console.log(
+        `Found ${deletedIds.length} prop definitions no longer documented.
+    This could be caused by moving file-location of prop-definition, a namechange or simply not existing anymore.
+
+    How to fix:
+    - Go to links provided under and try to manually delete document.
+    - You will then be prompted to update referenced document before deleting.
+    - After updating reference(s) and deleting document(s) there is no need to run the script again.`
+      );
+      console.log(
+        JSON.stringify(
+          deletedIds.map(
+            (x) =>
+              `https://aksel.nav.no/admin/prod/desk/admin;propsDesignsystemet;${x}`
+          ),
+          null,
+          2
+        )
+      );
+    });
 }
 
 function propList() {
