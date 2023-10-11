35902f945365f8224221d80ff184f3cee4698d33
Ken <ken.aleksander@gmail.com>
2023-10-11 21:36:10 +0200

    :art: Modal deps

--- @navikt/core/react/src/modal/index.ts
+++ @navikt/core/react/src/modal/index.ts
@@ -1,4 +1,5 @@
-export { default as Modal, type ModalProps } from "./Modal";
-export { type ModalHeaderProps } from "./ModalHeader";
+export { default as Modal } from "./Modal";
 export { type ModalBodyProps } from "./ModalBody";
 export { type ModalFooterProps } from "./ModalFooter";
+export { type ModalHeaderProps } from "./ModalHeader";
+export { type ModalProps } from "./types";
