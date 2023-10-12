07b88ed74a9b8743cbfaccce5ab7f827e199601b
Ken <ken.aleksander@gmail.com>
2023-10-12 13:56:40 +0200

    :fire: Fjernet composeEventHandlers

--- @navikt/core/react/src/util/index.ts
+++ @navikt/core/react/src/util/index.ts
@@ -1,21 +1,7 @@
 export * from "./OverridableComponent";
 export { default as debounce } from "./debounce";
 export { default as mergeRefs } from "./mergeRefs";
+export * from "./omit";
 export * from "./useClientLayoutEffect";
 export * from "./useEventListener";
 export * from "./useId";
-export * from "./omit";
-
-/* https://github.com/radix-ui/primitives/blob/main/packages/core/primitive/src/primitive.tsx */
-export const composeEventHandlers = <E>(
-  originalEventHandler?: (event: E) => void,
-  ourEventHandler?: (event: E) => void
-) => {
-  return function handleEvent(event: E) {
-    originalEventHandler?.(event);
-
-    if (!(event as unknown as Event).defaultPrevented) {
-      return ourEventHandler?.(event);
-    }
-  };
-};
