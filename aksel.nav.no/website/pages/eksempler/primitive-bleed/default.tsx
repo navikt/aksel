6b3932b172e058eccdda65aea20305acf58fe0f8
Ken <26967723+KenAJoh@users.noreply.github.com>
2023-10-18 12:50:12 +0200

    [aksel.nav.no] :memo: Oppdatert eksempler for Bleed og show/hide (#2385)
    * :memo: Oppdatert eksempler for Bleed og show/hide
    
    * :recycle: Refactor wrapper rundt bleed-eksempler

--- aksel.nav.no/website/pages/eksempler/primitive-bleed/default.tsx
+++ aksel.nav.no/website/pages/eksempler/primitive-bleed/default.tsx
@@ -1,48 +1,27 @@
-import { Bleed, BodyLong, Box, VStack } from "@navikt/ds-react";
+import { Bleed, Box, HStack } from "@navikt/ds-react";
 import { withDsExample } from "components/website-modules/examples/withDsExample";
 
 const Example = () => {
   return (
-    <VStack gap="2">
-      <Box background="surface-alt-1-subtle" padding="5">
-        <Box background="surface-alt-2-subtle" padding="5">
-          <Bleed marginInline="10 0">
-            <Box padding="3" className="p" background="surface-success-subtle">
-              <BodyLong>marginInline=&quot;10 0&quot;</BodyLong>
-            </Box>
-          </Bleed>
+    <DemoWrapper>
+      <Bleed marginInline="10" asChild>
+        <Box padding="3" className="p" background="surface-alt-3-subtle">
+          <HStack justify="center">marginInline</HStack>
         </Box>
-      </Box>
-      <Box background="surface-alt-1-subtle" padding="5">
-        <Box background="surface-alt-2-subtle" padding="5">
-          <Bleed marginInline="0 10">
-            <Box padding="3" className="p" background="surface-success-subtle">
-              <BodyLong>marginInline=&quot;0 10&quot;</BodyLong>
-            </Box>
-          </Bleed>
-        </Box>
-      </Box>
-      <Box background="surface-alt-1-subtle" padding="5">
-        <Box background="surface-alt-2-subtle" padding="5">
-          <Bleed marginBlock="10 0">
-            <Box padding="3" className="p" background="surface-success-subtle">
-              <BodyLong>marginBlock=&quot;10 0&quot;</BodyLong>
-            </Box>
-          </Bleed>
-        </Box>
-      </Box>
-      <Box background="surface-alt-1-subtle" padding="5">
-        <Box background="surface-alt-2-subtle" padding="5">
-          <Bleed marginBlock="0 10">
-            <Box padding="3" className="p" background="surface-success-subtle">
-              <BodyLong>marginBlock=&quot;0 10&quot;</BodyLong>
-            </Box>
-          </Bleed>
-        </Box>
-      </Box>
-    </VStack>
+      </Bleed>
+    </DemoWrapper>
   );
 };
+
+function DemoWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <Box background="surface-alt-3" padding="5" borderRadius="large">
+      <Box background="surface-subtle" padding="5" borderRadius="medium">
+        {children}
+      </Box>
+    </Box>
+  );
+}
 
 export default withDsExample(Example, {
   showBreakpoints: true,
---
+++ aksel.nav.no/website/pages/eksempler/primitive-bleed/responsive.tsx
@@ -0,0 +1,45 @@
+import { Bleed, BodyLong, Box, HStack } from "@navikt/ds-react";
+import { withDsExample } from "components/website-modules/examples/withDsExample";
+
+const Example = () => {
+  return (
+    <DemoWrapper>
+      <Bleed marginInline={{ xs: "8", md: "12", lg: "16" }} asChild>
+        <Box padding="3" className="p" background="surface-alt-3-subtle">
+          <HStack justify="center">
+            <BodyLong>xs: 8, md: 12, lg: 16</BodyLong>
+          </HStack>
+        </Box>
+      </Bleed>
+    </DemoWrapper>
+  );
+};
+
+function DemoWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <Box
+      background="surface-alt-3"
+      paddingBlock="4"
+      paddingInline={{ xs: "4", md: "8", lg: "12" }}
+      borderRadius="large"
+    >
+      <Box background="surface-subtle" padding="4" borderRadius="medium">
+        {children}
+      </Box>
+    </Box>
+  );
+}
+
+export default withDsExample(Example, {
+  showBreakpoints: true,
+});
+
+/* Storybook story */
+export const Demo = {
+  render: Example,
+};
+
+export const args = {
+  index: 1,
+  desc: "Margin block/inline er begge reponsive, som lar deg endre negativ margin dynamiskt basert på brekkpunkter.",
+};
