d34c72459ee311d78af372e0139630b5284e3ee8
Ken <ken.aleksander@gmail.com>
2023-10-18 18:51:14 +0200

    :fire: No need to lazyload hotjar

--- aksel.nav.no/website/pages/_app.tsx
+++ aksel.nav.no/website/pages/_app.tsx
@@ -3,22 +3,19 @@ import useAmplitudeInit from "components/website-modules/utils/tracking/useAmpli
 import { useScrollToHashOnPageLoad } from "components/website-modules/utils/util";
 import { AppProps } from "next/app";
 import { useEffect } from "react";
+import { hotjar } from "react-hotjar";
 import { IdContext } from "../components/website-modules/utils/contexts/id-context";
 import "../styles/index.css";
 
 function App({ Component, pageProps, router }: AppProps) {
   useScrollToHashOnPageLoad();
-  useAmplitudeInit();
 
+  useAmplitudeInit();
   useEffect(() => {
     window.location.host === "design.nav.no" &&
       window.location.replace(`http://aksel.nav.no`);
 
-    const initHotjar = async () => {
-      const { hotjar } = await import("react-hotjar");
-      hotjar.initialize(148751, 6);
-    };
-    initHotjar();
+    hotjar.initialize(148751, 6);
   }, []);
 
   return (
