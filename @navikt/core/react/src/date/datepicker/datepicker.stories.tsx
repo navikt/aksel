ad2e438dab33e150b024815e431ac7717c2ccdec
Ken <ken.aleksander@gmail.com>
2023-10-19 19:26:44 +0200

    :bug: Datepicker-popover vises ikke ritkig i Modal

--- @navikt/core/react/src/date/datepicker/datepicker.stories.tsx
+++ @navikt/core/react/src/date/datepicker/datepicker.stories.tsx
@@ -3,7 +3,7 @@ import { Meta, StoryObj } from "@storybook/react";
 import isSameDay from "date-fns/isSameDay";
 import React, { useId, useState } from "react";
 import { useDatepicker, useRangeDatepicker } from "..";
-import { BodyLong, Button, HGrid, Modal, VStack } from "../..";
+import { BodyLong, Button, HGrid, HStack, Modal, VStack } from "../..";
 
 import DatePicker, { DatePickerProps } from "./DatePicker";
 
@@ -442,17 +442,32 @@ export const ModalDemo = () => {
   });
 
   return (
-    <Modal open header={{ heading: "Modal-demo" }}>
-      <Modal.Body>
+    <Modal open header={{ heading: "Modal-demo" }} width="1024px">
+      <Modal.Body style={{ position: "relative" }}>
         <BodyLong>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores nisi
           incidunt ipsum cupiditate nostrum nesciunt, corrupti nihil at atque
           animi ab aut. Quam iusto harum eligendi magnam nulla repudiandae
           molestias.
         </BodyLong>
-        <DatePicker {...datepickerProps} dropdownCaption>
-          <DatePicker.Input {...inputProps} label="Velg dato" />
-        </DatePicker>
+
+        <HGrid gap="6" columns={{ xs: 1, lg: 2 }}>
+          <HStack justify="center" align="start">
+            <DatePicker.Standalone
+              fromDate={new Date("Aug 23 2019")}
+              toDate={new Date("Feb 23 2024")}
+            />
+          </HStack>
+          <DatePicker {...datepickerProps} dropdownCaption>
+            <DatePicker.Input {...inputProps} label="Velg dato" />
+          </DatePicker>
+          <HStack justify="center">
+            <DatePicker.Standalone
+              fromDate={new Date("Aug 23 2019")}
+              toDate={new Date("Feb 23 2024")}
+            />
+          </HStack>
+        </HGrid>
       </Modal.Body>
     </Modal>
   );
