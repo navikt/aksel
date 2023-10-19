7e4a32181b04907e9233eb8c272b2f9d52e322b0
Ken <ken.aleksander@gmail.com>
2023-10-19 19:45:03 +0200

    :memo: Modal + datepicker demo

--- aksel.nav.no/website/pages/eksempler/datepicker/modal.tsx
+++ aksel.nav.no/website/pages/eksempler/datepicker/modal.tsx
@@ -1,35 +1,27 @@
-import { DatePicker, useDatepicker, Button, Modal } from "@navikt/ds-react";
+import { DatePicker, Modal } from "@navikt/ds-react";
 import { withDsExample } from "components/website-modules/examples/withDsExample";
-import { useRef } from "react";
 import format from "date-fns/format";
 import nbLocale from "date-fns/locale/nb";
+import { useState } from "react";
 
 const Example = () => {
-  const ref = useRef<HTMLDialogElement>(null);
-
-  const { datepickerProps, inputProps, selectedDay } = useDatepicker({
-    fromDate: new Date("Aug 23 2019"),
-    onDateChange: console.log,
-  });
+  const [date, setDate] = useState(null);
 
   return (
-    <>
-      <Button onClick={() => ref.current?.showModal()}>Åpne modal</Button>
-
-      <Modal ref={ref} header={{ heading: "Heading" }}>
-        <Modal.Body>
-          <div className="min-h-96 min-w-96 max-w-full">
-            <DatePicker {...datepickerProps}>
-              <DatePicker.Input {...inputProps} label="Velg dato" />
-            </DatePicker>
-            <div className="pt-4">
-              {selectedDay &&
-                format(selectedDay, "dd.MM.yyyy", { locale: nbLocale })}
-            </div>
+    <Modal open header={{ heading: "Heading" }}>
+      <Modal.Body>
+        <div className="min-h-96 min-w-96 max-w-full">
+          <DatePicker.Standalone
+            onSelect={setDate}
+            fromDate={new Date("Aug 23 2019")}
+            fixedWeeks
+          />
+          <div className="pt-4">
+            {date && format(date, "dd.MM.yyyy", { locale: nbLocale })}
           </div>
-        </Modal.Body>
-      </Modal>
-    </>
+        </div>
+      </Modal.Body>
+    </Modal>
   );
 };
 
