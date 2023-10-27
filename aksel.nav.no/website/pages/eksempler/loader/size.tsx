7f0498f250b00126247b9cf1b6e661e99f0b1827
Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>
2023-10-27 12:52:17 +0200

    :bug: Oppdater eksemplene på Loader (#2431)

--- aksel.nav.no/website/pages/eksempler/loader/size.tsx
+++ aksel.nav.no/website/pages/eksempler/loader/size.tsx
@@ -1,17 +1,17 @@
-import { Loader } from "@navikt/ds-react";
+import { HStack, Loader } from "@navikt/ds-react";
 import { withDsExample } from "components/website-modules/examples/withDsExample";
 
 const Example = () => {
   return (
-    <div className="grid place-items-center gap-4">
-      <Loader size="3xlarge" title="venter..." />
-      <Loader size="2xlarge" title="venter..." />
-      <Loader size="xlarge" title="venter..." />
-      <Loader size="large" title="venter..." />
-      <Loader size="medium" title="venter..." />
-      <Loader size="small" title="venter..." />
-      <Loader size="xsmall" title="venter..." />
-    </div>
+    <HStack gap="18">
+      <Loader size="3xlarge" title="Venter..." />
+      <Loader size="2xlarge" title="Venter..." />
+      <Loader size="xlarge" title="Venter..." />
+      <Loader size="large" title="Venter..." />
+      <Loader size="medium" title="Venter..." />
+      <Loader size="small" title="Venter..." />
+      <Loader size="xsmall" title="Venter..." />
+    </HStack>
   );
 };
 
