abc5417af2b062903980bf36e1da2a447ac9218c
Ken <ken.aleksander@gmail.com>
2023-10-30 12:03:02 +0100

    :art: Showmore brukes nå bare når det er over 10 tokens i listen

--- aksel.nav.no/website/components/sanity-modules/token-table/TokenTable.tsx
+++ aksel.nav.no/website/components/sanity-modules/token-table/TokenTable.tsx
@@ -15,53 +15,69 @@ const TokenTable = ({ node }: { node: TokenTableT }) => {
     return null;
   }
 
-  return (
-    <div className="mb-7">
+  const Wrapper = ({ children }) => {
+    if (Object.entries(tokens).length < 10) {
+      return <>{children}</>;
+    }
+    return (
       <Bleed marginInline="4" marginBlock="4" asChild>
-        <ShowMore as="section" aria-label="Proptabell" scrollBackOnCollapse>
-          <AkselTable th={[{ text: "Token" }, { text: "Fallback" }]}>
-            {Object.entries(tokens).map(([key, val]) => (
-              <AkselTableRow key={key} tr={[{ text: key }, { text: val }]} />
-            ))}
-          </AkselTable>
-          <div className="border-border-subtle bg-surface-default relative -mt-9 w-full rounded-b-lg border border-t-0 p-2 pr-14">
-            <CopyButton
-              copyText={Object.entries(tokens).reduce(
-                (prev, cur) =>
-                  prev +
-                  `${cur[0]}: ${
-                    cur[1].startsWith("--") ? `var(${cur[1]})` : `${cur[1]}`
-                  };\n`,
-                ""
-              )}
-              className="absolute right-2 top-2 z-10"
-            />
-            <Label size="small" as="span" spacing>
-              Hva er dette?
-            </Label>
-            <BodyLong size="small">
-              Komponent-tokens gir deg muligheten til å sette opp theming eller
-              justere styling uten å måtte overskrive css-klasser. Les gjennom{" "}
-              <NextLink
-                href="/grunnleggende/styling/design-tokens#hec62d38bc813"
-                passHref
-                legacyBehavior
-              >
-                <Link>guiden vår</Link>
-              </NextLink>{" "}
-              eller utforsk alle{" "}
-              <NextLink
-                href="/grunnleggende/styling/design-tokens"
-                passHref
-                legacyBehavior
-              >
-                <Link>design tokens</Link>
-              </NextLink>
-              .
-            </BodyLong>
-          </div>
+        <ShowMore
+          as="section"
+          aria-label="Proptabell"
+          scrollBackOnCollapse
+          collapsedHeight="20rem"
+        >
+          {children}
         </ShowMore>
       </Bleed>
+    );
+  };
+
+  return (
+    <div className="mb-7">
+      <Wrapper>
+        <AkselTable th={[{ text: "Token" }, { text: "Fallback" }]}>
+          {Object.entries(tokens).map(([key, val]) => (
+            <AkselTableRow key={key} tr={[{ text: key }, { text: val }]} />
+          ))}
+        </AkselTable>
+        <div className="border-border-subtle bg-surface-default relative -mt-9 w-full rounded-b-lg border border-t-0 p-2 pr-14">
+          <CopyButton
+            copyText={Object.entries(tokens).reduce(
+              (prev, cur) =>
+                prev +
+                `${cur[0]}: ${
+                  cur[1].startsWith("--") ? `var(${cur[1]})` : `${cur[1]}`
+                };\n`,
+              ""
+            )}
+            className="absolute right-2 top-2 z-10"
+          />
+          <Label size="small" as="span" spacing>
+            Hva er dette?
+          </Label>
+          <BodyLong size="small">
+            Komponent-tokens gir deg muligheten til å sette opp theming eller
+            justere styling uten å måtte overskrive css-klasser. Les gjennom{" "}
+            <NextLink
+              href="/grunnleggende/styling/design-tokens#hec62d38bc813"
+              passHref
+              legacyBehavior
+            >
+              <Link>guiden vår</Link>
+            </NextLink>{" "}
+            eller utforsk alle{" "}
+            <NextLink
+              href="/grunnleggende/styling/design-tokens"
+              passHref
+              legacyBehavior
+            >
+              <Link>design tokens</Link>
+            </NextLink>
+            .
+          </BodyLong>
+        </div>
+      </Wrapper>
     </div>
   );
 };
