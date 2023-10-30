72eea12202511f8536475b9ebda2bb1470fa0566
Julian Nymark <julian.nymark@nav.no>
2023-10-30 11:37:40 +0100

    :sparkles: + :fire: + :recycle:
    - remove route for /sandbox, in public/ means it gets served regardless?
    - for each code example:
       - use jscodeshift to remove imports
       - also transpile to ESNext
    - bump jscodeshift to 0.15.1 (for all uses)
    - the sandbox button now sends you to /sandbox/index.html

--- aksel.nav.no/website/components/sanity-modules/code-examples/Sandbox.tsx
+++ aksel.nav.no/website/components/sanity-modules/code-examples/Sandbox.tsx
@@ -2,16 +2,28 @@ import { PencilIcon } from "@navikt/aksel-icons";
 import { Button } from "@navikt/ds-react";
 import { compressToEncodedURIComponent } from "lz-string";
 import ts from "typescript";
+import * as jscodeshift from "jscodeshift";
+
+const j = jscodeshift.withParser("tsx");
 
 const convertTSXToJSX = (code: string) => {
   return ts.transpile(code, {
     jsx: ts.JsxEmit.Preserve,
-    target: ts.ScriptTarget.ES5,
+    target: ts.ScriptTarget.ESNext,
   });
 };
 
 const removeImportLines = (code: string) => {
-  return code.replace(/import.*?\n/g, "");
+  // using jscodeshift to remove imports
+  const ast = j(code);
+
+  const removeImport = (path: any) => {
+    j(path).remove();
+  };
+
+  ast.find(j.ImportDeclaration).forEach((path: any) => removeImport(path));
+
+  return ast.toSource();
 };
 
 const processCode = (code: string) => {
@@ -37,7 +49,7 @@ export const Sandbox = ({ code }: { code: string }) => {
   );
   return (
     <form
-      action={`/sandbox?code=${compressedCode}`}
+      action={`/sandbox/index.html?code=${compressedCode}`}
       method="POST"
       target="_blank"
       className="h-8"
