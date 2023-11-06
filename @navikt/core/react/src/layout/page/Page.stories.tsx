c7967a25b210180c28f5fde7112ee79d503cf15f
Ken <ken.aleksander@gmail.com>
2023-11-06 11:59:00 +0100

    :art: Page-stories

--- @navikt/core/react/src/layout/page/Page.stories.tsx
+++ @navikt/core/react/src/layout/page/Page.stories.tsx
@@ -1,8 +1,8 @@
 import bgColors from "@navikt/ds-tokens/src/colors-bg.json";
 import type { Meta, StoryFn } from "@storybook/react";
-import React from "react";
-import { Bleed } from "../bleed";
+import React, { useEffect } from "react";
 import { Box } from "../box";
+import { VStack } from "../stack";
 import Page from "./Page";
 import { widths } from "./parts/PageBlock";
 
@@ -71,24 +71,111 @@ export const BelowFold: StoryFn = ({ width, gutters, contentPadding }) => (
   </Page>
 );
 
+export const Background: StoryFn = () => (
+  <VStack gap="4">
+    <Page background="bg-default">default</Page>
+    <Page background="bg-subtle">subtle</Page>
+  </VStack>
+);
+
+export const ContentPadding: StoryFn = ({ width = "laptop-xl" }) => (
+  <Page footer={<Footer width={width} gutters />} contentPadding>
+    <Header width={width} gutters />
+    <Page.Block width={width} gutters as="main">
+      <Box background="surface-alt-3-subtle" style={{ height: "80vh" }}>
+        Main
+      </Box>
+    </Page.Block>
+  </Page>
+);
+
+export const Gutters: StoryFn = ({ width = "laptop" }) => (
+  <Page footer={<Footer width={width} gutters />}>
+    <Header width={width} gutters />
+    <Content width={width} gutters />
+  </Page>
+);
+
+Gutters.parameters = {
+  chromatic: {
+    modes: {
+      mobile_portrait: {
+        viewport: {
+          width: 400,
+          height: 850,
+        },
+      },
+      desktop: {
+        viewport: {
+          width: 1280,
+          height: 960,
+        },
+      },
+    },
+  },
+};
+
+const MILJO_URL = "https://www.nav.no/dekoratoren";
+
+export const WithDecorator: StoryFn = ({
+  width = "laptop",
+  gutters = true,
+}) => {
+  return (
+    <Page
+      contentPadding
+      footerPosition="belowFold"
+      footer={<div id="decorator-footer"></div>}
+    >
+      <div id="decorator-header"></div>
+      <Content width={width} gutters={gutters} />
+      <div
+        id="decorator-env"
+        data-src={`${MILJO_URL}/env?context=privatperson`}
+      ></div>
+    </Page>
+  );
+};
+
+WithDecorator.decorators = [
+  (Story) => {
+    useEffect(() => {
+      const script = document.createElement("script");
+      script.src = `${MILJO_URL}/client.js`;
+      script.async = true;
+      document.body.appendChild(script);
+
+      const styles = document.createElement("link");
+      styles.href = `${MILJO_URL}/css/client.css`;
+      styles.rel = "stylesheet";
+      document.head.appendChild(styles);
+
+      return () => {
+        document.body.removeChild(script);
+        document.head.removeChild(styles);
+      };
+    }, []);
+
+    return <Story />;
+  },
+];
+
 function Header({ width, gutters }) {
   return (
-    <Box as="header" background="surface-alt-3-subtle" style={{ height: 64 }}>
-      <Page.Block width={width} gutters={gutters}>
+    <Page.Block as="header" width={width} gutters={gutters}>
+      <Box background="surface-alt-3-subtle" style={{ height: 64 }}>
         Header
-      </Page.Block>
-    </Box>
+      </Box>
+    </Page.Block>
   );
 }
 
 function Content({ width, gutters }) {
   return (
     <Page.Block width={width} gutters={gutters} as="main">
-      <Bleed marginInline="full full" asChild>
-        <Box background="surface-alt-3-subtle" style={{ height: 300 }}>
-          Main
-        </Box>
-      </Bleed>
+      <Box background="surface-alt-3-subtle" style={{ height: 300 }}>
+        Main
+      </Box>
     </Page.Block>
   );
 }
