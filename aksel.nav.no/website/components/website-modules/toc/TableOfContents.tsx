a6aa73cb67f4a35def996e4e225afa78e328e5a9
Ken <ken.aleksander@gmail.com>
2023-11-11 18:46:44 +0100

    :art: Mindre refactor av TOC

--- aksel.nav.no/website/components/website-modules/toc/TableOfContents.tsx
+++ aksel.nav.no/website/components/website-modules/toc/TableOfContents.tsx
@@ -19,77 +19,71 @@ function TableOfContents({ toc }: TableOfContentsProps) {
       <Label as="h2" className="text-deepblue-800 px-2 pb-1">
         Innhold på siden
       </Label>
-
-      <ul>
-        {toc.map((lvl2) => (
-          <li key={lvl2.id} className="toc-lvl2 group">
-            <>
-              <a
-                href={`#${lvl2.id}`}
-                className={cl(
-                  "hover:bg-surface-hover focus-visible:shadow-focus rounded-medium flex w-fit items-center justify-between px-2 py-1 outline-none transition-[background-color,color] duration-150 ease-out",
-                  {
-                    "bg-surface-hover text-deepblue-800 shadow-xsmall":
-                      lvl2.id === activeId && !activeSubId,
-                  }
-                )}
-              >
-                <BodyShort
-                  size="small"
-                  as="span"
-                  truncate
-                  className="whitespace-break-spaces"
-                  weight={
-                    lvl2.id === activeId && !activeSubId
-                      ? "semibold"
-                      : "regular"
-                  }
-                >
-                  {lvl2.title}
-                </BodyShort>
-              </a>
-              {lvl2.children.length > 0 && (
-                <ul>
-                  {lvl2.children.map((lvl3) => (
-                    <li key={lvl3.id}>
-                      <a
-                        href={`#${lvl3.id}`}
-                        className={cl(
-                          "hover:bg-surface-hover focus-visible:shadow-focus rounded-medium flex w-fit items-center justify-between px-2 py-1 outline-none transition-[background-color,color] duration-150 ease-out group-[.toc-lvl2]:ml-2",
-                          {
-                            "bg-surface-hover text-deepblue-800 shadow-xsmall":
-                              lvl3.id === activeSubId,
-                          }
-                        )}
-                      >
-                        <BodyShort
-                          size="small"
-                          as="span"
-                          truncate
-                          className="whitespace-break-spaces"
-                          weight={
-                            lvl3.id === activeSubId ? "semibold" : "regular"
-                          }
-                        >
-                          {lvl3.title}
-                        </BodyShort>
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </>
-          </li>
-        ))}
-      </ul>
+      <UlList toc={toc} activeId={activeId} activeSubId={activeSubId} />
     </aside>
   );
 }
 
-/*
+function UlList({
+  activeId,
+  activeSubId,
+  toc,
+  nested,
+}: {
+  activeId: string | null;
+  activeSubId: string | null;
+  toc: TableOfContentsT | TableOfContentsT[number]["children"];
 
-.TOC_Link__7eZBF.TOC_active__n1Lqs, .TOC_Link__7eZBF:hover {
-    background-color: var(--surface-active);
+  nested?: boolean;
+}) {
+  const id = nested ? activeSubId : activeId;
+  const isActive = (_id: string) => {
+    if (nested) {
+      return _id === id;
+    }
+    return _id === id && !activeSubId;
+  };
+
+  return (
+    <ul>
+      {toc.map((lvl2) => (
+        <li key={lvl2.id}>
+          <>
+            <a
+              href={`#${lvl2.id}`}
+              className={cl(
+                "hover:bg-surface-hover focus-visible:shadow-focus rounded-medium flex w-fit items-center justify-between px-2 py-1 outline-none transition-[background-color,color] duration-150 ease-out",
+                {
+                  "bg-surface-hover text-deepblue-800 shadow-xsmall": isActive(
+                    lvl2.id
+                  ),
+                  "ml-2": nested,
+                }
+              )}
+            >
+              <BodyShort
+                size="small"
+                as="span"
+                truncate
+                className="whitespace-break-spaces"
+                weight={isActive(lvl2.id) ? "semibold" : "regular"}
+              >
+                {lvl2.title}
+              </BodyShort>
+            </a>
+            {!nested && lvl2.children.length > 0 && (
+              <UlList
+                toc={lvl2.children}
+                activeId={activeId}
+                activeSubId={activeSubId}
+                nested
+              />
+            )}
+          </>
+        </li>
+      ))}
+    </ul>
+  );
 }
-*/
+
 export default TableOfContents;
