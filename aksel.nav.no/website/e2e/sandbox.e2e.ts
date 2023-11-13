e48d641d273e1bcfeca57e2ebb85ee92ab848283
Julian Nymark <julian.nymark@nav.no>
2023-11-13 11:16:51 +0100

    :bug: sandbox tests now for templates and examples

--- aksel.nav.no/website/e2e/sandbox.e2e.ts
+++ aksel.nav.no/website/e2e/sandbox.e2e.ts
@@ -3,27 +3,49 @@ import { getDirectories } from "../scripts/update-examples/parts/get-directories
 import { parseCodeFiles } from "../scripts/update-examples/parts/parse-code-files";
 
 test.describe("sandbox examples (all)", () => {
-  const examples = getDirectories("eksempler");
-  const templates = getDirectories("templates");
+  test.describe("eksempler", () => {
+    const subdirs = getDirectories("eksempler");
 
-  const folders = [...examples, ...templates];
+    for (const subdir of subdirs) {
+      const files = parseCodeFiles(subdir.path, "eksempler");
+      for (const file of files) {
+        if (!file.sandboxEnabled) {
+          continue;
+        }
 
-  for (const folder of folders) {
-    const files = parseCodeFiles(folder.path, "eksempler");
-    for (const file of files) {
-      if (!file.sandboxEnabled) {
-        continue;
+        const url = `/sandbox/preview/index.html?code=${file.sandboxBase64}`;
+
+        test(`check ${subdir.path} - ${file.navn}`, async ({ page }) => {
+          await page.goto(`http://localhost:3000${url}`);
+          await page.waitForLoadState("domcontentloaded");
+          const count = await page.locator("#sandbox-wrapper").count();
+
+          expect(count).toBeGreaterThan(0);
+        });
       }
-
-      const url = `/sandbox/preview/index.html?code=${file.sandboxBase64}`;
-
-      test(`check ${folder.path} - ${file.navn}`, async ({ page }) => {
-        await page.goto(`http://localhost:3000${url}`);
-        await page.waitForLoadState("domcontentloaded");
-        const count = await page.locator("#sandbox-wrapper").count();
-
-        expect(count).toBeGreaterThan(0);
-      });
     }
-  }
+  });
+
+  test.describe("templates", () => {
+    const subdirs = getDirectories("templates");
+
+    for (const subdir of subdirs) {
+      const files = parseCodeFiles(subdir.path, "templates");
+      for (const file of files) {
+        if (!file.sandboxEnabled) {
+          continue;
+        }
+
+        const url = `/sandbox/preview/index.html?code=${file.sandboxBase64}`;
+
+        test(`check ${subdir.path} - ${file.navn}`, async ({ page }) => {
+          await page.goto(`http://localhost:3000${url}`);
+          await page.waitForLoadState("domcontentloaded");
+          const count = await page.locator("#sandbox-wrapper").count();
+
+          expect(count).toBeGreaterThan(0);
+        });
+      }
+    }
+  });
 });
