5962cd8607ddae42d98cc95b06ef82ee3723e593
Halvor Haugan <halvor.haugan@nav.no>
2023-11-16 09:57:38 +0100

    fix

--- aksel.nav.no/website/pages/eksempler/textarea/resizable.tsx
+++ aksel.nav.no/website/pages/eksempler/textarea/resizable.tsx
@@ -1,5 +1,5 @@
+import { withDsExample } from "@/web/examples/withDsExample";
 import { Textarea } from "@navikt/ds-react";
-import { withDsExample } from "components/website-modules/examples/withDsExample";
 
 const Example = () => {
   return <Textarea label="Har du noen tilbakemeldinger?" resize />;
