dd856a84e6e601826444749e3a15064d72d2141c
Vegard Haugstvedt <it.vegard@gmail.com>
2023-11-23 15:34:11 +0100

    Operate directly on the DOM nodes when moving virtual focus, instead of relying on index state.
    The index state became stale/non-synced when replacing elements in the list.

--- @navikt/core/react/src/form/combobox/FilteredOptions/useVirtualFocus.ts
+++ @navikt/core/react/src/form/combobox/FilteredOptions/useVirtualFocus.ts
@@ -1,11 +1,10 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { useState } from "react";
 
 export type VirtualFocusType = {
   activeElement: HTMLElement | undefined;
   getElementById: (id: string) => HTMLElement | undefined;
   isFocusOnTheTop: boolean;
   isFocusOnTheBottom: boolean;
-  setIndex: Dispatch<SetStateAction<number>>;
   moveFocusUp: () => void;
   moveFocusDown: () => void;
   moveFocusToElement: (id: string) => void;
@@ -16,57 +15,85 @@ export type VirtualFocusType = {
 const useVirtualFocus = (
   containerRef: HTMLElement | null
 ): VirtualFocusType => {
-  const [index, setIndex] = useState(-1);
-
-  const listOfAllChildren: Array<HTMLElement> = containerRef?.children
-    ? Array.prototype.slice.call(containerRef?.children)
-    : [];
-  const elementsAbleToReceiveFocus = listOfAllChildren.filter(
-    (child) => child.getAttribute("data-no-focus") !== "true"
+  const [activeElement, setActiveElement] = useState<HTMLElement | undefined>(
+    undefined
   );
 
-  const activeElement = elementsAbleToReceiveFocus[index];
-  const getElementById = (id: string) =>
-    listOfAllChildren.find((element) => element.id === id);
-  const isFocusOnTheTop = index === 0;
-  const isFocusOnTheBottom = index === elementsAbleToReceiveFocus.length - 1;
-
-  const scrollToOption = (newIndex: number) => {
-    const indexOfElementToScrollTo = Math.min(
-      Math.max(newIndex, 0),
-      containerRef?.children.length || 0
+  const getListOfAllChildren = (): Array<HTMLElement> =>
+    containerRef?.children
+      ? Array.prototype.slice.call(containerRef?.children)
+      : [];
+  const getElementsAbleToReceiveFocus = () =>
+    getListOfAllChildren().filter(
+      (child) => child.getAttribute("data-no-focus") !== "true"
     );
-    if (containerRef?.children[indexOfElementToScrollTo]) {
-      const child = containerRef.children[indexOfElementToScrollTo];
-      const { top, bottom } = child.getBoundingClientRect();
+
+  const getElementById = (id: string) =>
+    getListOfAllChildren().find((element) => element.id === id);
+  const isFocusOnTheTop = activeElement
+    ? getElementsAbleToReceiveFocus().indexOf(activeElement) === 0
+    : false;
+  const isFocusOnTheBottom = activeElement
+    ? getElementsAbleToReceiveFocus().indexOf(activeElement) ===
+      getElementsAbleToReceiveFocus().length - 1
+    : false;
+
+  const scrollToOption = (_element?: HTMLElement) => {
+    if (containerRef && _element) {
+      const { top, bottom } = _element.getBoundingClientRect();
       const parentRect = containerRef.getBoundingClientRect();
       if (top < parentRect.top || bottom > parentRect.bottom) {
-        child.scrollIntoView({ block: "nearest" });
+        _element.scrollIntoView({ block: "nearest" });
       }
     }
   };
 
-  const _moveFocusAndScrollTo = (_index: number) => {
-    setIndex(_index);
-    scrollToOption(_index);
+  const _moveFocusAndScrollTo = (_element?: HTMLElement) => {
+    setActiveElement(_element);
+    scrollToOption(_element);
   };
-  const moveFocusUp = () => _moveFocusAndScrollTo(Math.max(index - 1, -1));
-  const moveFocusDown = () =>
-    _moveFocusAndScrollTo(
-      Math.min(index + 1, elementsAbleToReceiveFocus.length - 1)
-    );
-  const moveFocusToTop = () => _moveFocusAndScrollTo(-1);
+
+  const moveFocusUp = () => {
+    if (!activeElement) {
+      return;
+    }
+    const _currentIndex =
+      getElementsAbleToReceiveFocus().indexOf(activeElement);
+    const elementAbove = getElementsAbleToReceiveFocus()[_currentIndex - 1];
+    if (_currentIndex === 0) {
+      setActiveElement(undefined);
+    } else {
+      _moveFocusAndScrollTo(elementAbove);
+    }
+  };
+
+  const moveFocusDown = () => {
+    if (!activeElement) {
+      _moveFocusAndScrollTo(getElementsAbleToReceiveFocus()[0]);
+      return;
+    }
+    const _currentIndex =
+      getElementsAbleToReceiveFocus().indexOf(activeElement);
+    if (_currentIndex === getElementsAbleToReceiveFocus().length - 1) {
+      return;
+    } else {
+      _moveFocusAndScrollTo(getElementsAbleToReceiveFocus()[_currentIndex + 1]);
+    }
+  };
+
+  const moveFocusToTop = () => _moveFocusAndScrollTo(undefined);
   const moveFocusToBottom = () =>
-    _moveFocusAndScrollTo(elementsAbleToReceiveFocus.length - 1);
-  const moveFocusToElement = (id: string) => {
-    const thisElement = elementsAbleToReceiveFocus.find(
-      (_element) => _element.getAttribute("id") === id
+    _moveFocusAndScrollTo(
+      getElementsAbleToReceiveFocus()[
+        getElementsAbleToReceiveFocus().length - 1
+      ]
     );
-    const indexOfElement = thisElement
-      ? elementsAbleToReceiveFocus.indexOf(thisElement)
-      : -1;
-    if (indexOfElement >= 0) {
-      setIndex(indexOfElement);
+  const moveFocusToElement = (id: string) => {
+    const _element = getElementsAbleToReceiveFocus().find(
+      (_focusableElement) => _focusableElement.getAttribute("id") === id
+    );
+    if (_element) {
+      setActiveElement(_element);
     }
   };
 
@@ -75,7 +102,6 @@ const useVirtualFocus = (
     getElementById,
     isFocusOnTheTop,
     isFocusOnTheBottom,
-    setIndex,
     moveFocusUp,
     moveFocusDown,
     moveFocusToElement,
