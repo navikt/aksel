138c4523eea3a057173016573825cbd9286538d9
Julian Nymark <julian.nymark@nav.no>
2023-11-28 15:53:05 +0100

    Templates for 500 error pages (#2500)
    * :sparkles: add "enkel" template for 500
    
    * :sparkles: add more templates for 500 error pages
    
    * :art: shuffle some templates related to 500 errors
    
    * :memo: Oppdatert lenke
    
    * Update aksel.nav.no/website/pages/templates/500/med-lang-en.tsx
    
    * Update aksel.nav.no/website/pages/templates/500/uten-dekorator.tsx
    
    * PR review comments
    
    * Update aksel.nav.no/website/pages/templates/500/med-feil-id.tsx
    
    * Update aksel.nav.no/website/pages/templates/500/med-cta.tsx
    
    * Update aksel.nav.no/website/pages/templates/500/komplett.tsx
    
    * Update aksel.nav.no/website/pages/templates/500/enkel.tsx
    
    * :memo: Oppdatert formulering på engelsk contact-us
    
    * :memo: la til punktum
    
    * :fire: Fjernet HGRID fra template
    
    * :memo: Oppdatert lenketekster
    
    * :memo: Oppdatert tekster for enkel-variant
    
    * :memo: Oppdatert desc-tekster
    
    * :art: Oppdatert layout for 500-sider
    
    * :art: La til data-attr
    
    * :art: Oppdatert lokal bruk av 500-mal
    
    * :bug: Fikset 500-maler
    
    * :test_tube: Tester ikke templates med playwright
    
    * :bug: synket engelsk-tekst på 500-maler
    
    * Update aksel.nav.no/website/pages/500.tsx
    
    Co-authored-by: Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>
    
    * Update aksel.nav.no/website/pages/_error.js
    
    Co-authored-by: Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>
    
    * :fire: Fjernet duplikat comment
    
    * :art: oppdatert indeksering
    
    ---------
    
    Co-authored-by: Ken <ken.aleksander@gmail.com>
    Co-authored-by: Ken <26967723+KenAJoh@users.noreply.github.com>
    Co-authored-by: Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>

---
+++ aksel.nav.no/website/pages/templates/500/med-feil-id.tsx
@@ -0,0 +1,111 @@
+import {
+  BodyShort,
+  Box,
+  Heading,
+  Link,
+  List,
+  Page,
+  VStack,
+} from "@navikt/ds-react";
+import { useEffect } from "react";
+
+function Example() {
+  useDekorator();
+
+  return (
+    <Page data-aksel-template="500-v1" footer={<Footer />}>
+      <Header />
+      <Page.Block as="main" width="xl" gutters>
+        <Box paddingBlock="20 16">
+          <VStack gap="12" align="start">
+            <div>
+              <BodyShort textColor="subtle" size="small">
+                Statuskode 500
+              </BodyShort>
+              <Heading level="1" size="large" spacing>
+                Beklager, noe gikk galt.
+              </Heading>
+              {/* Tekster bør tilpasses den aktuelle 500-feilen. Teksten under er for en generisk 500-feil. */}
+              <BodyShort spacing>
+                En teknisk feil på våre servere gjør at siden er utilgjengelig.
+                Dette skyldes ikke noe du gjorde.
+              </BodyShort>
+              <BodyShort>Du kan prøve å</BodyShort>
+              <List>
+                <List.Item>
+                  vente noen minutter og laste siden på nytt
+                </List.Item>
+                <List.Item>gå tilbake til forrige side</List.Item>
+              </List>
+              <BodyShort>
+                Hvis problemet vedvarer, kan du{" "}
+                {/* https://nav.no/kontaktoss for eksterne flater */}
+                <Link href="#" target="_blank">
+                  kontakte oss (åpnes i ny fane)
+                </Link>
+                .
+              </BodyShort>
+            </div>
+
+            <BodyShort size="small" textColor="subtle">
+              Feil-id: 12345678-9123-4567-8912-345678912345
+            </BodyShort>
+          </VStack>
+        </Box>
+      </Page.Block>
+      <Env />
+    </Page>
+  );
+}
+
+function Header() {
+  return <div id="decorator-header" />;
+}
+
+function Footer() {
+  return <div id="decorator-footer" />;
+}
+
+const MILJO_URL = "https://www.nav.no/dekoratoren";
+
+function Env() {
+  return (
+    <div
+      id="decorator-env"
+      data-src={`${MILJO_URL}/env?context=privatperson`}
+    />
+  );
+}
+
+/**
+ * OBS: Dette er ikke anbefalt metode for å laste dekoratør!
+ * Se `nav-dekoratoren`-dokumentasjon for riktig implementasjon
+ * @see https://github.com/navikt/nav-dekoratoren
+ */
+function useDekorator() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = `${MILJO_URL}/client.js`;
+    script.async = true;
+    document.body.appendChild(script);
+
+    const styles = document.createElement("link");
+    styles.href = `${MILJO_URL}/css/client.css`;
+    styles.rel = "stylesheet";
+    document.head.appendChild(styles);
+
+    return () => {
+      document.body.removeChild(script);
+      document.head.removeChild(styles);
+    };
+  }, []);
+}
+
+// EXAMPLES DO NOT INCLUDE CONTENT BELOW THIS LINE
+export default Example;
+
+export const args = {
+  index: 4,
+  title: "Med feil-id",
+  desc: "Ved å legge til en feil-id kan man enklere feilsøke mot logger hvis man får skjermbilde av feilmelding fra bruker.",
+};
--- aksel.nav.no/website/pages/templates/500/uten-dekorator.tsx
+++ aksel.nav.no/website/pages/templates/500/uten-dekorator.tsx
@@ -1,40 +1,32 @@
 import Footer from "@/layout/footer/Footer";
 import Header from "@/layout/header/Header";
-import { AmplitudeEvents, amplitude } from "@/logging";
 import {
   BodyShort,
   Box,
+  Button,
   Heading,
   Link,
   List,
   Page,
   VStack,
 } from "@navikt/ds-react";
-import { useEffect } from "react";
 
-function ErrorPage({ statusCode }) {
-  useEffect(() => {
-    amplitude.track(AmplitudeEvents.error, {
-      side: window.location.pathname,
-    });
-  }, []);
-
+function Example() {
   return (
-    <Page data-aksel-template="500-v1" footer={<Footer />} id="vk-notFoundId">
+    <Page data-aksel-template="500-v1" footer={<Footer />}>
       <Header />
       <Page.Block as="main" width="xl" gutters>
         <Box paddingBlock="20 8">
           <VStack gap="16">
             <VStack gap="12" align="start">
               <div>
-                {statusCode && (
-                  <BodyShort textColor="subtle" size="small">
-                    Statuskode {statusCode}
-                  </BodyShort>
-                )}
+                <BodyShort textColor="subtle" size="small">
+                  Statuskode 500
+                </BodyShort>
                 <Heading level="1" size="large" spacing>
                   Beklager, noe gikk galt.
                 </Heading>
+                {/* Tekster bør tilpasses den aktuelle 500-feilen. Teksten under er for en generisk 500-feil. */}
                 <BodyShort spacing>
                   En teknisk feil på våre servere gjør at siden er
                   utilgjengelig. Dette skyldes ikke noe du gjorde.
@@ -48,15 +40,19 @@ function ErrorPage({ statusCode }) {
                 </List>
                 <BodyShort>
                   Hvis problemet vedvarer, kan du{" "}
-                  <Link
-                    href="https://github.com/navikt/aksel/issues/new?assignees=&labels=bug+%F0%9F%90%9B&projects=&template=bug-report.md&title=[Aksel.nav.no%20-%20500]"
-                    target="_blank"
-                  >
+                  {/* https://nav.no/kontaktoss for eksterne flater */}
+                  <Link href="#" target="_blank">
                     kontakte oss (åpnes i ny fane)
                   </Link>
                   .
                 </BodyShort>
               </div>
+
+              <BodyShort size="small" textColor="subtle">
+                Feil-id: 12345678-9123-4567-8912-345678912345
+              </BodyShort>
+
+              <Button>Gå til Min side</Button>
             </VStack>
             <div>
               <Heading level="1" size="large" spacing>
@@ -67,10 +63,8 @@ function ErrorPage({ statusCode }) {
                 refresh this page or try again in a few minutes.{" "}
               </BodyShort>
               <BodyShort>
-                <Link
-                  target="_blank"
-                  href="https://github.com/navikt/aksel/issues/new?assignees=&labels=bug+%F0%9F%90%9B&projects=&template=bug-report.md&title=[Aksel.nav.no%20-%20500]"
-                >
+                {/* https://www.nav.no/kontaktoss/en for eksterne flater */}
+                <Link target="_blank" href="#">
                   Contact us (opens in new tab)
                 </Link>{" "}
                 if the problem persists.
@@ -82,4 +76,13 @@ function ErrorPage({ statusCode }) {
     </Page>
   );
 }
-export default ErrorPage;
+
+// EXAMPLES DO NOT INCLUDE CONTENT BELOW THIS LINE
+export default Example;
+
+export const args = {
+  index: 6,
+  title: "Uten Dekoratør",
+  desc: "Malen fungerer ved bruk av andre sidemaler, også da uten dekoratøren.",
+  sandbox: false,
+};
