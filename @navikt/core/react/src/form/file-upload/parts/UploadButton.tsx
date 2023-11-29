7610e7e3a8ded593a44cae696088beb5afca28ba
Ken <ken.aleksander@gmail.com>
2023-11-29 19:55:01 +0100

    :recycle: refactor types/forwardref in UploadButton

--- @navikt/core/react/src/form/file-upload/parts/UploadButton.tsx
+++ @navikt/core/react/src/form/file-upload/parts/UploadButton.tsx
@@ -1,32 +1,42 @@
-import React, { useContext } from "react";
+import cl from "clsx";
+import React, { forwardRef, useContext } from "react";
 import { UploadIcon } from "@navikt/aksel-icons";
 import { Button } from "../../../button";
 import { FileUploadContext } from "../context";
 import { getButtonText } from "../utils/i18n";
 
-const UploadButton = () => {
-  const context = useContext(FileUploadContext);
+export interface FileUploadButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: React.ReactNode;
+}
 
-  if (context === null) {
-    console.error(
-      "You are using <FileUpload.Button> or <FileUpload.Zone> outside of a <FileUpload>"
+const UploadButton = forwardRef<HTMLButtonElement, FileUploadButtonProps>(
+  ({ className, children, ...rest }: FileUploadButtonProps, ref) => {
+    const context = useContext(FileUploadContext);
+
+    if (context === null) {
+      console.error(
+        "You are using <FileUpload.Button> or <FileUpload.Zone> outside of a <FileUpload>"
+      );
+      return null;
+    }
+
+    return (
+      <Button
+        {...rest}
+        ref={ref}
+        className={cl("navds-fileupload__content__button", className)}
+        variant="secondary"
+        onClick={context.onButtonClick}
+        tabIndex={-1}
+        icon={
+          <UploadIcon fontSize="1.5rem" focusable={false} aria-hidden={true} />
+        }
+      >
+        {children ?? getButtonText(context.locale)}
+      </Button>
     );
-    return null;
   }
-
-  return (
-    <Button
-      variant="secondary"
-      className="navds-fileupload__content__button"
-      onClick={context.onButtonClick}
-      tabIndex={-1}
-      icon={
-        <UploadIcon fontSize="1.5rem" focusable={false} aria-hidden={true} />
-      }
-    >
-      {getButtonText(context.locale)}
-    </Button>
-  );
-};
+);
 
 export default UploadButton;
