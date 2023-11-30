dcfaf94d3edf79b5ca73822b0ba5d24e163cc0da
Halvor Haugan <halvor.haugan@nav.no>
2023-11-30 18:09:33 +0100

    [aksel.nav.no] Justering av eksempler på InternalHeader

--- aksel.nav.no/website/pages/eksempler/header/app-switcher.tsx
+++ aksel.nav.no/website/pages/eksempler/header/app-switcher.tsx
@@ -1,45 +1,47 @@
-import { withDsExample } from "@/web/examples/withDsExample";
 import { ExternalLinkIcon, MenuGridIcon } from "@navikt/aksel-icons";
 import { Dropdown, InternalHeader, Spacer } from "@navikt/ds-react";
+import { withDsExample } from "@/web/examples/withDsExample";
 
 const Example = () => {
   return (
-    <InternalHeader>
-      <InternalHeader.Title as="h1">Sykepenger</InternalHeader.Title>
-      <Spacer />
-      <Dropdown defaultOpen>
-        <InternalHeader.Button as={Dropdown.Toggle}>
-          <MenuGridIcon
-            style={{ fontSize: "1.5rem" }}
-            title="Systemer og oppslagsverk"
-          />
-        </InternalHeader.Button>
+    <div style={{ minHeight: "14rem" }}>
+      <InternalHeader>
+        <InternalHeader.Title as="h1">Sykepenger</InternalHeader.Title>
+        <Spacer />
+        <Dropdown defaultOpen>
+          <InternalHeader.Button as={Dropdown.Toggle}>
+            <MenuGridIcon
+              style={{ fontSize: "1.5rem" }}
+              title="Systemer og oppslagsverk"
+            />
+          </InternalHeader.Button>
 
-        <Dropdown.Menu>
-          <Dropdown.Menu.GroupedList>
-            <Dropdown.Menu.GroupedList.Heading>
-              Systemer og oppslagsverk
-            </Dropdown.Menu.GroupedList.Heading>
-            <Dropdown.Menu.GroupedList.Item as="a" target="_blank" href="#">
-              A.Inntekt <ExternalLinkIcon aria-hidden />
-            </Dropdown.Menu.GroupedList.Item>
-            <Dropdown.Menu.GroupedList.Item as="a" target="_blank" href="#">
-              Aa-registeret <ExternalLinkIcon aria-hidden />
-            </Dropdown.Menu.GroupedList.Item>
-            <Dropdown.Menu.GroupedList.Item as="a" target="_blank" href="#">
-              Gosys <ExternalLinkIcon aria-hidden />
-            </Dropdown.Menu.GroupedList.Item>
-            <Dropdown.Menu.GroupedList.Item as="a" target="_blank" href="#">
-              Modia personoversikt <ExternalLinkIcon aria-hidden />
-            </Dropdown.Menu.GroupedList.Item>
-            <Dropdown.Menu.GroupedList.Item as="a" target="_blank" href="#">
-              Oppdrag <ExternalLinkIcon aria-hidden />
-            </Dropdown.Menu.GroupedList.Item>
-          </Dropdown.Menu.GroupedList>
-        </Dropdown.Menu>
-      </Dropdown>
-      <InternalHeader.User name="Ola Normann" />
-    </InternalHeader>
+          <Dropdown.Menu>
+            <Dropdown.Menu.GroupedList>
+              <Dropdown.Menu.GroupedList.Heading>
+                Systemer og oppslagsverk
+              </Dropdown.Menu.GroupedList.Heading>
+              <Dropdown.Menu.GroupedList.Item as="a" target="_blank" href="#">
+                A.Inntekt <ExternalLinkIcon aria-hidden />
+              </Dropdown.Menu.GroupedList.Item>
+              <Dropdown.Menu.GroupedList.Item as="a" target="_blank" href="#">
+                Aa-registeret <ExternalLinkIcon aria-hidden />
+              </Dropdown.Menu.GroupedList.Item>
+              <Dropdown.Menu.GroupedList.Item as="a" target="_blank" href="#">
+                Gosys <ExternalLinkIcon aria-hidden />
+              </Dropdown.Menu.GroupedList.Item>
+              <Dropdown.Menu.GroupedList.Item as="a" target="_blank" href="#">
+                Modia personoversikt <ExternalLinkIcon aria-hidden />
+              </Dropdown.Menu.GroupedList.Item>
+              <Dropdown.Menu.GroupedList.Item as="a" target="_blank" href="#">
+                Oppdrag <ExternalLinkIcon aria-hidden />
+              </Dropdown.Menu.GroupedList.Item>
+            </Dropdown.Menu.GroupedList>
+          </Dropdown.Menu>
+        </Dropdown>
+        <InternalHeader.User name="Ola Normann" />
+      </InternalHeader>
+    </div>
   );
 };
 
