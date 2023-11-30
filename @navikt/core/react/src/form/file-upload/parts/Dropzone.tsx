beeba6ac7659e98007a6527ed27661c1ea398d04
torkjels <3830142+torkjels@users.noreply.github.com>
2023-11-30 10:22:43 +0100

    Fjern logikk for minWidth ettersom vi uansett har width: 100%

--- @navikt/core/react/src/form/file-upload/parts/Dropzone.tsx
+++ @navikt/core/react/src/form/file-upload/parts/Dropzone.tsx
@@ -4,10 +4,9 @@ import React, {
   useContext,
   useMemo,
   useRef,
-  useState,
 } from "react";
 import { BodyShort } from "../../../typography";
-import { mergeRefs, useClientLayoutEffect } from "../../../util";
+import { mergeRefs } from "../../../util";
 import { FileUploadContext } from "../context";
 import { getDragAndLetGoText, getOrText } from "../utils/i18n";
 import UploadButton from "./UploadButton";
@@ -22,15 +21,6 @@ const Dropzone = forwardRef<HTMLDivElement, FileUploadDropzoneProps>(
     const wrapperRef = useRef<HTMLDivElement | null>(null);
     const mergedRef = useMemo(() => mergeRefs([wrapperRef, ref]), [ref]);
 
-    const [minWidth, setMinWidth] = useState<number>();
-
-    useClientLayoutEffect(() => {
-      const height = wrapperRef?.current?.getBoundingClientRect()?.height;
-      if (height) {
-        setMinWidth(height * 2);
-      }
-    }, []);
-
     if (context === null) {
       console.error("<FileUpload.Zone> has to be used within a <FileUpload>.");
       return null;
@@ -41,7 +31,6 @@ const Dropzone = forwardRef<HTMLDivElement, FileUploadDropzoneProps>(
         {...rest}
         ref={mergedRef}
         className={cl("navds-fileupload__content__zone", className)}
-        style={{ minWidth }}
       >
         <BodyShort as="span">{getDragAndLetGoText(context.locale)}</BodyShort>
         <BodyShort as="span">{getOrText(context.locale)}</BodyShort>
