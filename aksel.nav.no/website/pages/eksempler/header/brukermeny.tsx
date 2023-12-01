1f16bfc86652522be25bdfc2f09bdbcc52a91393
Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>
2023-12-01 09:50:27 +0100

    [aksel.nav.no] Justering av eksempler på InternalHeader (#2531)

--- aksel.nav.no/website/pages/eksempler/header/brukermeny.tsx
+++ aksel.nav.no/website/pages/eksempler/header/brukermeny.tsx
@@ -1,4 +1,3 @@
-import { withDsExample } from "@/web/examples/withDsExample";
 import {
   BodyShort,
   Detail,
@@ -6,32 +5,35 @@ import {
   InternalHeader,
   Spacer,
 } from "@navikt/ds-react";
+import { withDsExample } from "@/web/examples/withDsExample";
 
 const Example = () => {
   return (
-    <InternalHeader>
-      <InternalHeader.Title as="h1">Sykepenger</InternalHeader.Title>
-      <Spacer />
-      <Dropdown defaultOpen>
-        <InternalHeader.UserButton
-          as={Dropdown.Toggle}
-          name="Ola N."
-          description="Enhet: Skien"
-        />
-        <Dropdown.Menu>
-          <dl>
-            <BodyShort as="dt" size="small">
-              Ola Normann
-            </BodyShort>
-            <Detail as="dd">D123456</Detail>
-          </dl>
-          <Dropdown.Menu.Divider />
-          <Dropdown.Menu.List>
-            <Dropdown.Menu.List.Item>Logg ut</Dropdown.Menu.List.Item>
-          </Dropdown.Menu.List>
-        </Dropdown.Menu>
-      </Dropdown>
-    </InternalHeader>
+    <div style={{ minHeight: "8rem" }}>
+      <InternalHeader>
+        <InternalHeader.Title as="h1">Sykepenger</InternalHeader.Title>
+        <Spacer />
+        <Dropdown defaultOpen>
+          <InternalHeader.UserButton
+            as={Dropdown.Toggle}
+            name="Ola N."
+            description="Enhet: Skien"
+          />
+          <Dropdown.Menu>
+            <dl>
+              <BodyShort as="dt" size="small">
+                Ola Normann
+              </BodyShort>
+              <Detail as="dd">D123456</Detail>
+            </dl>
+            <Dropdown.Menu.Divider />
+            <Dropdown.Menu.List>
+              <Dropdown.Menu.List.Item>Logg ut</Dropdown.Menu.List.Item>
+            </Dropdown.Menu.List>
+          </Dropdown.Menu>
+        </Dropdown>
+      </InternalHeader>
+    </div>
   );
 };
 
