53288798d524cdb9874bea8af357d9db466955fe
torkjels <3830142+torkjels@users.noreply.github.com>
2023-12-06 11:07:33 +0100

    Reduser duplisering av kode i FileIcon

--- @navikt/core/react/src/form/file-upload/item/ItemIcon.tsx
+++ @navikt/core/react/src/form/file-upload/item/ItemIcon.tsx
@@ -11,8 +11,6 @@ import {
 import { ItemContext } from "./item-context";
 import { Loader } from "../../../loader";
 
-const ICON_CLASS_NAME = "navds-fileitem__icon"
-
 const ItemIcon = () => {
   const context = useContext(ItemContext)
 
@@ -21,11 +19,28 @@ const ItemIcon = () => {
     return null
   }
 
-  const { file, isLoading } = context
+  const { isLoading } = context
 
   if (isLoading) {
     return <div><Loader size="large" /></div>
   }
+
+  return (
+    <div className="navds-fileitem__icon">
+      <Icon />
+    </div>
+  )
+}
+
+const Icon = () => {
+  const context = useContext(ItemContext)
+
+  if (context == null) {
+    console.error("<ItemIcon> has to be used within a <Item>")
+    return null
+  }
+
+  const { file } = context
 
   const extension = file.name.substring(file.name.lastIndexOf(".") + 1)
 
@@ -41,35 +56,21 @@ const ItemIcon = () => {
     case "png":
     case "gif":
     case "webp":
-      return (<div className={ICON_CLASS_NAME}>
-        <FileImageIcon fontSize={iconProps.fontSize} focusable={iconProps.focusable} aria-hidden={iconProps.ariaHidden} />
-      </div>)
+      return <FileImageIcon {...iconProps} />
     case "pdf":
-      return (<div className={ICON_CLASS_NAME}>
-        <FilePdfIcon fontSize={iconProps.fontSize} focusable={iconProps.focusable} aria-hidden={iconProps.ariaHidden} />
-      </div>)
+      return <FilePdfIcon {...iconProps} />
     case "txt":
-      return (<div className={ICON_CLASS_NAME}>
-        <FileTextIcon fontSize={iconProps.fontSize} focusable={iconProps.focusable} aria-hidden={iconProps.ariaHidden} />
-      </div>)
+      return <FileTextIcon {...iconProps} />
     case "csv":
-      return (<div className={ICON_CLASS_NAME}>
-        <FileCsvIcon fontSize={iconProps.fontSize} focusable={iconProps.focusable} aria-hidden={iconProps.ariaHidden} />
-      </div>)
+      return <FileCsvIcon {...iconProps} />
     case "xls":
     case "xlsx":
-      return (<div className={ICON_CLASS_NAME}>
-        <FileExcelIcon fontSize={iconProps.fontSize} focusable={iconProps.focusable} aria-hidden={iconProps.ariaHidden} />
-      </div>)
+      return <FileExcelIcon {...iconProps} />
     case "doc":
     case "docx":
-      return (<div className={ICON_CLASS_NAME}>
-        <FileWordIcon fontSize={iconProps.fontSize} focusable={iconProps.focusable} aria-hidden={iconProps.ariaHidden} />
-      </div>)
+      return <FileWordIcon {...iconProps} />
     default:
-      return (<div className={ICON_CLASS_NAME}>
-        <FileIcon fontSize={iconProps.fontSize} focusable={iconProps.focusable} aria-hidden={iconProps.ariaHidden} />
-      </div>)
+      return <FileIcon {...iconProps} />
   }
 }
 
