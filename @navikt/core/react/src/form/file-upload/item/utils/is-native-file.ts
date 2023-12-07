4fb021ef2c60d98eb0c578ceb6e530bc2ea59a65
torkjels <3830142+torkjels@users.noreply.github.com>
2023-12-07 10:31:34 +0100

    isNativeFile funker nå også i Node

--- @navikt/core/react/src/form/file-upload/item/utils/is-native-file.ts
+++ @navikt/core/react/src/form/file-upload/item/utils/is-native-file.ts
@@ -1,4 +1,12 @@
 import { FileItem } from "../types";
 
-export const isNativeFile = (fileItem: FileItem): fileItem is File =>
-  fileItem instanceof File;
+export const isNativeFile = (fileItem: FileItem): fileItem is File => {
+  /**
+   * Checking for File does not work in Node (for instance on the
+   * server side in Next.js). File is only defined in the browser.
+   * A File is a specialized form of a Blob, so we check if the
+   * file is a Blob, since Blob is defined in both the browser
+   * and in Node.
+   */
+  return fileItem instanceof Blob;
+};
