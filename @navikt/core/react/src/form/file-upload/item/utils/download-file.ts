46bfe92ab80026dfa15ece5d170ff9d27770d5a9
torkjels <3830142+torkjels@users.noreply.github.com>
2023-12-15 16:24:15 +0100

    Fjern unødvendig base64-til-blob-konvertering

--- @navikt/core/react/src/form/file-upload/item/utils/download-file.ts
+++ @navikt/core/react/src/form/file-upload/item/utils/download-file.ts
@@ -3,26 +3,10 @@ import { isNativeFile } from "./file-type-checker";
 
 export const downloadFile = (file: File | FileBase64): void => {
   const a = document.createElement("a");
-  const url = URL.createObjectURL(
-    isNativeFile(file) ? file : base64toBlob(file.base64)
-  );
+  const url = isNativeFile(file) ? URL.createObjectURL(file) : file.base64;
   a.href = url;
   a.download = file.name;
   a.click();
 
   URL.revokeObjectURL(url);
 };
-
-function base64toBlob(base64: string): Blob {
-  const parts = base64.split(";base64,");
-  const contentType = parts[0].split(":")[1];
-  const raw = window.atob(parts[1]);
-  const rawLength = raw.length;
-  const array = new Uint8Array(new ArrayBuffer(rawLength));
-
-  for (let i = 0; i < rawLength; i++) {
-    array[i] = raw.charCodeAt(i);
-  }
-
-  return new Blob([array], { type: contentType });
-}
