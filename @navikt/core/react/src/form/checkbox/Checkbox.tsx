00b1aa5eb9fe8145617df0950d149c1212ee25b1
Ken <ken.aleksander@gmail.com>
2023-12-17 17:46:59 +0100

    :zap: Forenklet checkbox checkmark-scaling

--- @navikt/core/react/src/form/checkbox/Checkbox.tsx
+++ @navikt/core/react/src/form/checkbox/Checkbox.tsx
@@ -54,8 +54,8 @@ export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
           <span className="navds-checkbox__icon">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="calc(0.8125*1rem)"
-              height="calc(0.625*1rem)"
+              width="0.8125rem"
+              height="0.625rem"
               viewBox="0 0 13 10"
               fill="none"
               focusable={false}
