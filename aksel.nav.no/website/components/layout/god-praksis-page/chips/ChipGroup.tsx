adc36c97f03448e575117ce1a7f9524827c8afc4
Ken <ken.aleksander@gmail.com>
2023-12-18 14:31:00 +0100

    :bug: List of articles were set in wrong chipnav

--- aksel.nav.no/website/components/layout/god-praksis-page/chips/ChipGroup.tsx
+++ aksel.nav.no/website/components/layout/god-praksis-page/chips/ChipGroup.tsx
@@ -9,30 +9,25 @@ type ChipsGroupProps = { data: ChipsData; showTema: boolean };
 function ChipsGroup(props: ChipsGroupProps) {
   const { innholdstypeQuery, undertemaQuery } = useGpQuery();
 
-  const undertemaList = useMemo(
-    () =>
-      undertemaQuery
-        ? props.data.filter((entry) => {
-            return entry["undertema-title"] === undertemaQuery;
-          })
-        : props.data,
-    [props.data, undertemaQuery]
-  );
-
-  const innholdstypeList = useMemo(
-    () =>
-      innholdstypeQuery
+  const list = useMemo(() => {
+    return {
+      innholdstype: innholdstypeQuery
         ? props.data.filter((entry) => {
             return entry["innholdstype-title"] === innholdstypeQuery;
           })
         : props.data,
-    [props.data, innholdstypeQuery]
-  );
+      undertema: undertemaQuery
+        ? props.data.filter((entry) => {
+            return entry["undertema-title"] === undertemaQuery;
+          })
+        : props.data,
+    };
+  }, [props.data, innholdstypeQuery, undertemaQuery]);
 
   return (
     <VStack gap="4">
-      {props.showTema && <ChipNav type="undertema" data={undertemaList} />}
-      <ChipNav type="innholdstype" data={innholdstypeList} />
+      {props.showTema && <ChipNav type="undertema" data={list.innholdstype} />}
+      <ChipNav type="innholdstype" data={list.undertema} />
     </VStack>
   );
 }
