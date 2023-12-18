29bf985da9e4dcfa380bbb78c7fcbf371de4f7c3
Halvor Haugan <halvor.haugan@nav.no>
2023-12-18 12:10:15 +0100

    :memo: Modal: Oppdatert JSDoc

---
+++ .changeset/wise-comics-smoke.md
@@ -0,0 +1,5 @@
+---
+"@navikt/ds-react": patch
+---
+
+:memo: Modal: Oppdatert JSDoc
--- @navikt/core/react/src/modal/types.ts
+++ @navikt/core/react/src/modal/types.ts
@@ -34,18 +34,22 @@ export interface ModalProps
    */
   onClose?: React.ReactEventHandler<HTMLDialogElement>;
   /**
-   * Called when the user wants to close the modal (clicked the close button or pressed Esc).
+   * Called when the user tries to close the modal by one of the built-in ways.
+   * @warning Will not always be called when pressing Esc. Refer to the JSDoc for `onCancel` for more info.
    * @returns Whether to close the modal
+   * @deprecated The modal should be implemented in such a way that closing it doesn't cause data loss.
+   *  Use `onClose` if you just need to know when the modal has closed.
    */
   onBeforeClose?: () => boolean | void;
   /**
-   * Called when the user presses the Esc key, unless `onBeforeClose()` returns `false`.
+   * *Sometimes** called when the user presses the Esc key, unless you return `false` in `onBeforeClose()`.
+   * @warning *Some browsers does not always trigger this event. Chrome only triggers it if you have
+   *  interacted with the modal, and will not trigger it a second time if you press Esc twice in a row.
    */
   onCancel?: React.ReactEventHandler<HTMLDialogElement>;
   /**
    * Whether to close when clicking on the backdrop.
-   *
-   * **WARNING:** Users may click outside by accident. Don't use if closing can cause data loss, or the modal contains important info.
+   * @warning Users may click outside by accident. Don't use if closing can cause data loss, or the modal contains important info.
    * @default false
    */
   closeOnBackdropClick?: boolean;
