248d9777ab2e282f60c7d64ad98e82068ee19c9b
Ken <ken.aleksander@gmail.com>
2023-12-18 13:54:19 +0100

    :truck: Moved chips to chipsgroup

--- aksel.nav.no/website/components/layout/god-praksis-page/GodPraksisPage.tsx
+++ aksel.nav.no/website/components/layout/god-praksis-page/GodPraksisPage.tsx
@@ -1,11 +1,10 @@
 import dynamic from "next/dynamic";
-import React, { useEffect } from "react";
 import { Box, Heading, Page, VStack } from "@navikt/ds-react";
 import Footer from "@/layout/footer/Footer";
 import { GpEntryPageProps } from "@/layout/god-praksis-page/types";
 import useInitialState from "@/layout/god-praksis-page/useInitialState";
 import Header from "@/layout/header/Header";
-import ChipNav from "./chips/ChipNav";
+import ChipsGroup from "./chips/ChipGroup";
 import Hero from "./hero/Hero";
 
 const ArticleList = dynamic(() => import("./articles/ArticleList"), {
@@ -14,34 +13,6 @@ const ArticleList = dynamic(() => import("./articles/ArticleList"), {
 
 function GodPraksisPage(props: GpEntryPageProps) {
   const { initialData } = useInitialState(props.initialArticles);
-
-  const [undertemaSelection, setUndertemaSelection] = React.useState<string>();
-  const [innholdstypeSelection, setInnholdstypeSelection] =
-    React.useState<string>();
-
-  const [filteredUndertema, setFilteredUndertema] = React.useState([]);
-  const [filteredInnholdstype, setFilteredInnholdstype] = React.useState([]);
-
-  useEffect(() => {
-    if (undertemaSelection) {
-      setFilteredUndertema(
-        props.chipsData.filter((entry) => {
-          return entry["undertema-title"] === undertemaSelection;
-        })
-      );
-    } else {
-      setFilteredUndertema(props.chipsData);
-    }
-    if (innholdstypeSelection) {
-      setFilteredInnholdstype(
-        props.chipsData.filter((entry) => {
-          return entry["innholdstype-title"] === innholdstypeSelection;
-        })
-      );
-    } else {
-      setFilteredInnholdstype(props.chipsData);
-    }
-  }, [props.chipsData, undertemaSelection, innholdstypeSelection]);
 
   return (
     /* TODO: Add surface-subtle to page-component props */
@@ -56,20 +27,7 @@ function GodPraksisPage(props: GpEntryPageProps) {
           <VStack gap="10">
             <VStack gap="6">
               <Hero tema={props.tema} heroNav={props.heroNav} />
-              <VStack gap="4">
-                {props.tema && (
-                  <ChipNav
-                    type="undertema"
-                    data={filteredInnholdstype}
-                    setSelection={setUndertemaSelection}
-                  />
-                )}
-                <ChipNav
-                  type="innholdstype"
-                  data={filteredUndertema}
-                  setSelection={setInnholdstypeSelection}
-                />
-              </VStack>
+              <ChipsGroup data={props.chipsData} showTema={!!props.tema} />
             </VStack>
             <div>
               {initialData?.length > 0 && (
--- aksel.nav.no/website/components/layout/god-praksis-page/chips/ChipGroup.tsx
+++ aksel.nav.no/website/components/layout/god-praksis-page/chips/ChipGroup.tsx
@@ -1,7 +1,56 @@
-/* type ChipsGroupProps = {}; */
+import React, { useEffect } from "react";
+import { VStack } from "@navikt/ds-react";
+import { ChipsData } from "@/layout/god-praksis-page/types";
+import ChipNav from "./ChipNav";
 
-function ChipsGroup() {
-  return <div>avbc</div>;
+type ChipsGroupProps = { data: ChipsData; showTema: boolean };
+
+function ChipsGroup(props: ChipsGroupProps) {
+  console.log(props.data);
+  const [undertemaSelection, setUndertemaSelection] = React.useState<string>();
+  const [innholdstypeSelection, setInnholdstypeSelection] =
+    React.useState<string>();
+
+  const [filteredUndertema, setFilteredUndertema] = React.useState([]);
+  const [filteredInnholdstype, setFilteredInnholdstype] = React.useState([]);
+
+  useEffect(() => {
+    if (undertemaSelection) {
+      setFilteredUndertema(
+        props.data.filter((entry) => {
+          return entry["undertema-title"] === undertemaSelection;
+        })
+      );
+    } else {
+      setFilteredUndertema(props.data);
+    }
+    if (innholdstypeSelection) {
+      setFilteredInnholdstype(
+        props.data.filter((entry) => {
+          return entry["innholdstype-title"] === innholdstypeSelection;
+        })
+      );
+    } else {
+      setFilteredInnholdstype(props.data);
+    }
+  }, [props.data, undertemaSelection, innholdstypeSelection]);
+
+  return (
+    <VStack gap="4">
+      {props.showTema && (
+        <ChipNav
+          type="undertema"
+          data={filteredInnholdstype}
+          setSelection={setUndertemaSelection}
+        />
+      )}
+      <ChipNav
+        type="innholdstype"
+        data={filteredUndertema}
+        setSelection={setInnholdstypeSelection}
+      />
+    </VStack>
+  );
 }
 
 export default ChipsGroup;
