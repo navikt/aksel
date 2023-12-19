5161b3333ca9da03b2be013e77b820755d7541ee
Halvor Haugan <halvor.haugan@nav.no>
2023-12-19 15:12:19 +0100

    See `onCancel` for more info.

--- @navikt/core/react/src/modal/types.ts
+++ @navikt/core/react/src/modal/types.ts
@@ -36,7 +36,7 @@ export interface ModalProps
   /**
    * Called when the user tries to close the modal by one of the built-in methods.
    * Used if you want to ask the user for confirmation before closing.
-   * @warning Will not always be called when pressing Esc. Refer to the JSDoc for `onCancel` for more info.
+   * @warning Will not always be called when pressing Esc. See `onCancel` for more info.
    * @returns Whether to close the modal
    */
   onBeforeClose?: () => boolean | void;
