17234e60d1a71a3b7c301081df7b63a5fc927560
Ken <ken.aleksander@gmail.com>
2024-01-04 11:22:56 +0100

    :art: Kopiert over scrollbutton kode

--- @navikt/core/react/src/util/hooks/tabs/TabsList.tsx
+++ @navikt/core/react/src/util/hooks/tabs/TabsList.tsx
@@ -1,16 +1,22 @@
 import cl from "clsx";
-import React, { forwardRef } from "react";
+import React, { forwardRef, useMemo, useRef } from "react";
+import mergeRefs from "../../mergeRefs";
+import ScrollButtons from "./ScrollButtons";
 import { TabListProps } from "./types";
 import { useTabList } from "./use-tabs";
 
 export const TabList = forwardRef<HTMLDivElement, TabListProps>(
   ({ className, children, ...rest }, ref) => {
     const ctx = useTabList(rest);
+    const listRef = useRef<HTMLDivElement>(null);
+    const mergedRef = useMemo(() => mergeRefs([listRef, ref]), [ref]);
+
     return (
       <div className="navds-tabs__tablist-wrapper">
+        <ScrollButtons listRef={listRef} />
         <div
           {...ctx}
-          ref={ref}
+          ref={mergedRef}
           className={cl("navds-tabs__tablist", className)}
         >
           {children}
