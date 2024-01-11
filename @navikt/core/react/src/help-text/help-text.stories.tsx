6aeeade527d9baaecd87cc5a38a5cffced162078
Halvor Haugan <halvor.haugan@nav.no>
2024-01-11 15:27:32 +0100

    Fikset story for HelpText

--- @navikt/core/react/src/help-text/help-text.stories.tsx
+++ @navikt/core/react/src/help-text/help-text.stories.tsx
@@ -1,63 +1,57 @@
+import { Meta, StoryFn, StoryObj } from "@storybook/react";
 import React, { useEffect, useRef } from "react";
 import { BodyLong, Heading, HelpText } from "..";
 
-export default {
+const meta: Meta<typeof HelpText> = {
   title: "ds-react/HelpText",
   component: HelpText,
   parameters: {
     chromatic: { delay: 300 },
   },
-  argTypes: {
-    placement: {
-      control: {
-        type: "radio",
-        options: [
-          "top",
-          "bottom",
-          "right",
-          "left",
-          "top-start",
-          "top-end",
-          "bottom-start",
-          "bottom-end",
-          "right-start",
-          "right-end",
-          "left-start",
-          "left-end",
-        ],
-      },
-    },
-    strategy: {
-      control: {
-        type: "radio",
-        options: ["fixed", "absolute"],
-      },
-    },
-  },
 };
+export default meta;
 
-export const Default = {
-  render: (props: any) => {
-    return (
-      <HelpText title="show tooltip" strategy="fixed" {...props}>
-        Id ullamco excepteur elit fugiat labore.
-      </HelpText>
-    );
-  },
+export const Default: StoryObj<typeof HelpText> = {
+  render: (props) => (
+    <HelpText {...props}>Id ullamco excepteur elit fugiat labore.</HelpText>
+  ),
 
   args: {
-    title: "show tooltip",
+    title: "Show tooltip",
+  },
+  argTypes: {
+    placement: {
+      control: { type: "radio" },
+      options: [
+        "top",
+        "bottom",
+        "right",
+        "left",
+        "top-start",
+        "top-end",
+        "bottom-start",
+        "bottom-end",
+        "right-start",
+        "right-end",
+        "left-start",
+        "left-end",
+      ],
+    },
+    strategy: {
+      control: { type: "radio" },
+      options: ["fixed", "absolute"],
+    },
   },
 };
 
-export const Open = () => {
+export const Open: StoryFn = () => {
   const ref = useRef<HTMLButtonElement | null>(null);
   useEffect(() => {
     ref.current && ref.current.click();
   }, []);
 
   return (
-    <HelpText ref={ref} title="show tooltip" strategy="fixed">
+    <HelpText ref={ref} title="Show tooltip" strategy="fixed">
       Incididunt laborum eiusmod ullamco id aliquip officia ex irure aliqua
       laboris id ea do nisi. Ex esse ad duis culpa non aliquip exercitation eu
       culpa cupidatat nisi. Deserunt voluptate consectetur cillum elit qui ad
@@ -66,35 +60,31 @@ export const Open = () => {
   );
 };
 
-export const WrapperClassName = {
-  render: () => {
-    return (
-      <div>
-        <BodyLong spacing>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
-          perspiciatis vero voluptatum, asperiores cumque, deserunt accusantium
-          ullam ipsa accusamus officia cupiditate quae unde esse culpa, ratione
-          ab quam ea quas?
-        </BodyLong>
+export const WrapperClassName: StoryFn = () => (
+  <div>
+    <BodyLong spacing>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+      perspiciatis vero voluptatum, asperiores cumque, deserunt accusantium
+      ullam ipsa accusamus officia cupiditate quae unde esse culpa, ratione ab
+      quam ea quas?
+    </BodyLong>
 
-        <Heading level="2" size="medium">
-          67 år og 1 måneder øklasdjkl askdak døkasøk daøkdkølasøkld asølkdøka
-          <HelpText wrapperClassName="testClass">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
-            perspiciatis vero voluptatum, asperiores cumque, deserunt
-            accusantium ullam ipsa accusamus officia cupiditate quae unde esse
-            culpa, ratione ab quam ea quas?
-          </HelpText>
-          <style>{`.testClass {display: inline-block; margin-left: 0.5rem; vertical-align: middle;}`}</style>
-        </Heading>
+    <Heading level="2" size="medium">
+      67 år og 1 måneder øklasdjkl askdak døkasøk daøkdkølasøkld asølkdøka
+      <HelpText wrapperClassName="testClass">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+        perspiciatis vero voluptatum, asperiores cumque, deserunt accusantium
+        ullam ipsa accusamus officia cupiditate quae unde esse culpa, ratione ab
+        quam ea quas?
+      </HelpText>
+      <style>{`.testClass {display: inline-block; margin-left: 0.5rem; vertical-align: middle;}`}</style>
+    </Heading>
 
-        <BodyLong spacing>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
-          perspiciatis vero voluptatum, asperiores cumque, deserunt accusantium
-          ullam ipsa accusamus officia cupiditate quae unde esse culpa, ratione
-          ab quam ea quas?
-        </BodyLong>
-      </div>
-    );
-  },
-};
+    <BodyLong spacing>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+      perspiciatis vero voluptatum, asperiores cumque, deserunt accusantium
+      ullam ipsa accusamus officia cupiditate quae unde esse culpa, ratione ab
+      quam ea quas?
+    </BodyLong>
+  </div>
+);
