7a44a613a4a283b28ac7a2ffaaf971b5dea7e2a4
Ken <ken.aleksander@gmail.com>
2024-01-11 21:10:38 +0100

    :fire: Fjernet iconPosition-prop fra tag.tsx

--- @navikt/core/react/src/tag/Tag.tsx
+++ @navikt/core/react/src/tag/Tag.tsx
@@ -40,11 +40,6 @@ export interface TagProps extends HTMLAttributes<HTMLSpanElement> {
    * Tag Icon
    */
   icon?: React.ReactNode;
-  /**
-   * Icon position in Tag
-   * @default "left"
-   */
-  iconPosition?: "left" | "right";
 }
 
 /**
@@ -59,18 +54,7 @@ export interface TagProps extends HTMLAttributes<HTMLSpanElement> {
  * ```
  */
 export const Tag = forwardRef<HTMLSpanElement, TagProps>(
-  (
-    {
-      children,
-      className,
-      variant,
-      size = "medium",
-      icon,
-      iconPosition = "left",
-      ...rest
-    },
-    ref,
-  ) => (
+  ({ children, className, variant, size = "medium", icon, ...rest }, ref) => (
     <BodyShort
       {...rest}
       ref={ref}
@@ -83,13 +67,8 @@ export const Tag = forwardRef<HTMLSpanElement, TagProps>(
         `navds-tag--${size}`,
       )}
     >
-      {icon && iconPosition === "left" && (
-        <span className="navds-tag__icon--left">{icon}</span>
-      )}
+      {icon && <span className="navds-tag__icon--left">{icon}</span>}
       {children}
-      {icon && iconPosition === "right" && (
-        <span className="navds-tag__icon--right">{icon}</span>
-      )}
     </BodyShort>
   ),
 );
