9773585b8e85feb8c3079aed4a73cc3e7971c0d0
Ken <ken.aleksander@gmail.com>
2024-01-12 20:49:00 +0100

    :truck: Flyttet resterende hooks

--- @navikt/core/react/src/util/hooks/index.ts
+++ @navikt/core/react/src/util/hooks/index.ts
@@ -2,3 +2,5 @@ export { useCallbackRef } from "./useCallbackRef";
 export { useClientLayoutEffect } from "./useClientLayoutEffect";
 export { useControllableState } from "./useControllableState";
 export { useMergeRefs } from "./useMergeRefs";
+export { useId } from "./useId";
+export { useMedia } from "./useMedia";
--- @navikt/core/react/src/util/index.ts
+++ @navikt/core/react/src/util/index.ts
@@ -3,4 +3,4 @@ export { default as debounce } from "./debounce";
 export * from "./omit";
 export { useClientLayoutEffect } from "./hooks/useClientLayoutEffect";
 export { useEventListener } from "./hooks/useEventListener";
-export * from "./useId";
+export { useId } from "./hooks/useId";
