3e5fe845d45ac3868ef3dac1ece4eecd7fae7868
Ken <ken.aleksander@gmail.com>
2024-01-13 20:16:32 +0100

    :wheelchair: Scrollbutton er nå

--- @navikt/core/react/src/tabs/parts/tablist/ScrollButtons.tsx
+++ @navikt/core/react/src/tabs/parts/tablist/ScrollButtons.tsx
@@ -10,12 +10,12 @@ interface ScrollButtonProps {
 
 function ScrollButton({ hidden, onClick, dir }: ScrollButtonProps) {
   return (
-    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
     <div
       className={cl("navds-tabs__scroll-button", {
         "navds-tabs__scroll-button--hidden": hidden,
       })}
       onClick={onClick}
+      aria-hidden
     >
       {dir === "left" ? (
         <ChevronLeftIcon title="scroll tilbake" />
