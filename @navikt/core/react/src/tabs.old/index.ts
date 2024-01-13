cbe001173221f28a5c1c341a35637353e3f945ef
Ken <ken.aleksander@gmail.com>
2024-01-13 23:39:53 +0100

    :fire: Fjernet radix-tabs import

--- @navikt/core/react/src/tabs.old/index.ts
+++
@@ -1,4 +0,0 @@
-export { type TabProps } from "./Tab";
-export { type TabListProps } from "./TabList";
-export { type TabPanelProps } from "./TabPanel";
-export { default as Tabs, type TabsProps } from "./Tabs";
