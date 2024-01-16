d76b0600482a7a43260f86b5ff4522f845f5aa65
Ken <ken.aleksander@gmail.com>
2024-01-16 18:18:57 +0100

    :bug: Fikset maxWidth extraction fra tokens -> tailwind config

--- @navikt/core/tailwind/src/getMaxWidth.ts
+++ @navikt/core/tailwind/src/getMaxWidth.ts
@@ -1,12 +1,10 @@
 import Reducer from "./reducer";
 
 /**
- * @note This is made just for the case `--a-text-width-max` at the moment
+ * @note This is hardcoded just for the case `--a-text-width-max` at the moment
  */
 export const getMaxWidth = (tokens: { [key: string]: string | number }) => {
-  const widths = Reducer(tokens, ["text-width"]);
+  const width = Reducer(tokens, ["text-width-max"]);
 
-  return Object.keys(widths).reduce((cur, key) => {
-    return Object.assign(cur, { [`--a-text-width-${key}`]: widths[key] });
-  }, {});
+  return { text: Object.values(width)[0] };
 };
