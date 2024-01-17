de0356e6ef1a0eeddc69a9a256be9d45b995bffc
Julian Nymark <julian.nymark@nav.no>
2024-01-17 13:18:29 +0100

    :construction: wip on schema

--- aksel.nav.no/website/sanity/schema/documents/articleViews.tsx
+++ aksel.nav.no/website/sanity/schema/documents/articleViews.tsx
@@ -1,40 +1,41 @@
 import { defineField, defineType } from "sanity";
 
 export const ArticleViews = defineType({
-  title: "ArticleViews",
-  name: "aksel_article_views",
+  title: "Artikkel Visninger",
+  name: "article_views",
   type: "document",
+  readOnly: ({ currentUser }) =>
+    !currentUser.roles.find((x) => x.name === "developer"),
   fields: [
     defineField({
       type: "number",
       name: "views",
       description: "total lifetime views",
-      readOnly: true,
     }),
     defineField({
       type: "number",
       name: "views_week",
       description: "views in the last week",
-      readOnly: true,
     }),
     defineField({
       type: "number",
       name: "views_month",
       description: "views in the last month",
-      readOnly: true,
     }),
     defineField({
       type: "number",
       name: "views_year",
       description: "views in the last year",
-      readOnly: true,
     }),
     defineField({
       title: "articleRef",
       name: "article_ref",
       type: "reference",
       to: { type: "aksel_artikkel" },
-      readOnly: true,
+    }),
+    defineField({
+      type: "string",
+      name: "url",
     }),
   ],
 });
