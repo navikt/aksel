13ec4e07e2918e7f933765b83f0457604cf6641e
Ken <ken.aleksander@gmail.com>
2024-01-18 19:07:44 +0100

    :recycle: maxWidth er nå lettere å forstå

--- @navikt/core/tailwind/src/getMaxWidth.ts
+++ @navikt/core/tailwind/src/getMaxWidth.ts
@@ -1,10 +1,6 @@
-import Reducer from "./reducer";
-
 /**
  * @note This is hardcoded just for the case `--a-text-width-max` at the moment
  */
 export const getMaxWidth = (tokens: { [key: string]: string | number }) => {
-  const width = Reducer(tokens, ["text-width-max"]);
-
-  return { text: Object.values(width)[0] };
+  return { text: tokens["text-width-max"] };
 };
