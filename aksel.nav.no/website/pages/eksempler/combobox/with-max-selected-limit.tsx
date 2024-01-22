a934a7bea4ffff160d34672b92ff62db9b91d6b4
Vegard Haugstvedt <it.vegard@gmail.com>
2024-01-22 11:51:45 +0100

    Legg til Aksel-eksempel

---
+++ aksel.nav.no/website/pages/eksempler/combobox/with-max-selected-limit.tsx
@@ -0,0 +1,49 @@
+import { UNSAFE_Combobox } from "@navikt/ds-react";
+import { withDsExample } from "@/web/examples/withDsExample";
+import { useState } from "react";
+
+const Example = () => {
+  const [selectedOptions, setSelectedOptions] = useState([
+    options[0],
+    options[1],
+  ]);
+  return (
+    <div>
+      <UNSAFE_Combobox
+        label="Hva er de kuleste transportmidlene? (velg opp til 3)"
+        options={options}
+        isMultiSelect
+        maxSelected={{ limit: 3 }}
+        selectedOptions={["car", "bus", "train"]}
+        onToggleSelected={(option, isSelected) =>
+          isSelected
+            ? setSelectedOptions([...selectedOptions, option])
+            : setSelectedOptions(selectedOptions.filter((o) => o !== option))
+        }
+      />
+    </div>
+  );
+};
+
+const options = [
+  "car",
+  "bus",
+  "train",
+  "skateboard",
+  "bicycle",
+  "motorcycle",
+  "boat",
+  "airplane",
+  "helicopter",
+  "truck",
+  "van",
+  "scooter",
+];
+
+// EXAMPLES DO NOT INCLUDE CONTENT BELOW THIS LINE
+export default withDsExample(Example, { variant: "static" });
+
+export const args = {
+  index: 1,
+  desc: "Ved å sette en grense for maks antall valgte vil brukeren få opp en beskjed om at hen ikke kan velge flere når grensen er nådd. Resterende valg vil også bli inaktive.",
+};
