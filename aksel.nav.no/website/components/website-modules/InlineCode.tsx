46e3dd0097ab9d51b71a4715495e3ff29ec59077
Ken <26967723+KenAJoh@users.noreply.github.com>
2024-01-23 15:26:31 +0100

    Update aksel.nav.no/website/components/website-modules/InlineCode.tsx

--- aksel.nav.no/website/components/website-modules/InlineCode.tsx
+++ aksel.nav.no/website/components/website-modules/InlineCode.tsx
@@ -1,7 +1,7 @@
 const InlineCode = (
   props: React.HTMLAttributes<HTMLElement> & { noAmps?: boolean },
 ) => (
-  <code className="rounded rounded-sm bg-bg-subtle px-1 py-05 font-mono text-sm font-semibold">
+  <code className="rounded-sm bg-surface-neutral-subtle px-1 py-05 font-mono text-sm font-semibold">
     {!props?.noAmps && (
       <span aria-hidden className="inline-code-amp">
         `
