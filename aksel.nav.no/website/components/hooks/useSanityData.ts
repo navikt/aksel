4c5cdb22e90d287a9a2c4e09e722e8a883484a94
Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>
2024-01-23 13:24:59 +0100

    [aksel.nav.no] Vis advarsel på gamle gp-artikler (#2650)

---
+++ aksel.nav.no/website/components/hooks/useSanityData.ts
@@ -0,0 +1,16 @@
+import { createContext, useContext } from "react";
+
+export const SanityDataContext = createContext<{
+  id?: string;
+  validUser: boolean;
+}>(null);
+
+export function useSanityData() {
+  const ctx = useContext(SanityDataContext);
+  if (!ctx) {
+    console.error(
+      "useSanityData has to be used inside SanityDataContext.provider",
+    );
+  }
+  return ctx;
+}
