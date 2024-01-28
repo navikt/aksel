dee48dd954e55e4c71fd94d19aa7761a75d00ca4
Ken <ken.aleksander@gmail.com>
2024-01-28 17:48:38 +0100

    :bug: Import feilet

--- @navikt/core/react/src/form/file-upload/index.ts
+++ @navikt/core/react/src/form/file-upload/index.ts
@@ -1,7 +1,5 @@
 export { default as FileUpload } from "./FileUpload";
-export {
-  type DropzoneProps,
-  type OnFileSelectProps,
-} from "./parts/dropzone/dropzone.types";
+export { type OnFileSelectProps } from "./FileUpload.types";
+export { type DropzoneProps } from "./parts/dropzone/dropzone.types";
 export { type FileItemProps } from "./parts/item/Item";
 export { type FileItem, type FileMetadata } from "./parts/item/Item.types";
