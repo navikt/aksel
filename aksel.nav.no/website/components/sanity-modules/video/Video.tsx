826f0e15312b0758ba1f2d97641e54047e8914ea
Halvor Haugan <halvor.haugan@nav.no>
2024-02-05 08:53:01 +0100

    ?

--- aksel.nav.no/website/components/sanity-modules/video/Video.tsx
+++ aksel.nav.no/website/components/sanity-modules/video/Video.tsx
@@ -28,7 +28,7 @@ const Video = ({ node }: VideoProps) => {
         aria-describedby={node.transkripsjon ? transcriptId : undefined}
         poster="/images/og/video-poster.png"
         crossOrigin={
-          globalThis?.location.port === "3000" ? "anonymous" : undefined
+          globalThis?.location?.port === "3000" ? "anonymous" : undefined
         } // Needed for the <track>
       >
         <source src={node.webm.url} type={`video/${node.webm.extension}`} />
