967b8aa1135eeab8753cff39fab3d42d6797282f
Ken <ken.aleksander@gmail.com>
2024-02-06 12:51:04 +0100

    :memo: WithAsChild JSDOC

--- @navikt/core/react/src/util/types/WithAsChild.ts
+++ @navikt/core/react/src/util/types/WithAsChild.ts
@@ -2,14 +2,36 @@ export type WithAsChild =
   | {
       children: React.ReactElement | false | null;
       /**
-       * Renders the children as a child of the component. Merges the props of the component with the props of the child.
+       * Renders the component and its child as a single element,
+       * merging the props of the component with the props of the child.
+       *
+       * @example
+       * ```tsx
+       * <Component asChild data-prop>
+       *   <ChildComponent data-child/>
+       * </Component>
+       *
+       * // Renders
+       * <MergedComponent data-prop data-child />
+       * ```
        */
       asChild: true;
     }
   | {
       children: React.ReactNode;
       /**
-       * Renders the children as a child of the component. Merges the props of the component with the props of the child.
+       * Renders the component and its child as a single element,
+       * merging the props of the component with the props of the child.
+       *
+       * @example
+       * ```tsx
+       * <Component asChild data-prop>
+       *   <ChildComponent data-child/>
+       * </Component>
+       *
+       * // Renders
+       * <MergedComponent data-prop data-child />
+       * ```
        */
       asChild?: false;
     };
