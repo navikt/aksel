d1aed96311f6eecb07c636a75b0fc30e27cacbf7
Ken <ken.aleksander@gmail.com>
2024-02-06 12:32:53 +0100

    :recycle: Mer robust sjekk for conditional elements i WithAsChild

--- @navikt/core/react/src/util/types/WithAsChild.ts
+++ @navikt/core/react/src/util/types/WithAsChild.ts
@@ -1,6 +1,6 @@
 export type WithAsChild =
   | {
-      children: React.ReactElement;
+      children: React.ReactElement | false | null;
       /**
        * Renders the children as a child of the component. Merges the props of the component with the props of the child.
        */
