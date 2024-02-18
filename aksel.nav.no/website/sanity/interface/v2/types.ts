6d12d8898f9b71f5081ae52c25b5933b69fbf700
Ken <ken.aleksander@gmail.com>
2024-02-18 14:42:39 +0100

    :fire: fjernet gammelt preview-api

--- aksel.nav.no/website/sanity/interface/v2/types.ts
+++ aksel.nav.no/website/sanity/interface/v2/types.ts
@@ -18,14 +18,4 @@ export type PreviewProps = {
    * Default/fallback-data if preview fails and for initial load
    */
   props: any;
-  /**
-   * Array of functions we want to run on live data when fetched
-   * @example Supports getting data from nested objects like `page.content`
-   * @example
-   * Here we run `generateSidebar` function on `sidebar` key every time the data is reloaded
-   * resolvers={[
-   *   { key: "sidebar", dataKeys: ["page.content", "page.intro"], cb: (v) => generateSidebar(v[0], "komponenter") },
-   * ]}
-   */
-  resolvers?: ResolverT;
 };
