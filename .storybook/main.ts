6343144f339a9c920c8b7a350f40c7b960398517
Ken <26967723+KenAJoh@users.noreply.github.com>
2024-02-20 16:37:15 +0100

    Update .storybook/main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -48,12 +48,6 @@ export default {
 
   async viteFinal(config, { configType }) {
     return mergeConfig(config, {
-      onwarn(warning, warn) {
-        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
-          return;
-        }
-        warn(warning);
-      },
       plugins:
         configType === "PRODUCTION"
           ? [
