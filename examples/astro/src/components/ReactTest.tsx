b857bf900b0b24113f45958b88d80f168eaf8bc3
Ken <ken.aleksander@gmail.com>
2024-02-21 11:27:34 +0100

    :art: Oppdatert astro-eksempel

--- examples/astro/src/components/ReactTest.tsx
+++ examples/astro/src/components/ReactTest.tsx
@@ -1,9 +1,11 @@
 import { Button, DatePicker } from "@navikt/ds-react";
+import { Alert } from "@navikt/ds-react/Alert";
 
 export const ReactTest = () => {
   return (
     <div>
       ReactTest123 <Button onClick={console.log}>Button</Button>
+      <Alert variant="info">abc</Alert>
       <DatePicker.Standalone
         dropdownCaption
         fromDate={new Date("1 Oct 2020")}
