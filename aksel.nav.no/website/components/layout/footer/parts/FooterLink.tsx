abe0b406ab7182e0ad3cd1b0975706950f2fa362
Ken <ken.aleksander@gmail.com>
2024-02-27 19:45:17 +0100

    :label: Typer footerlink

--- aksel.nav.no/website/components/layout/footer/parts/FooterLink.tsx
+++ aksel.nav.no/website/components/layout/footer/parts/FooterLink.tsx
@@ -7,9 +7,10 @@ function FooterLink({ children, href }) {
       <Link
         className="flex w-fit items-center gap-1 text-text-on-inverted underline hover:no-underline focus:bg-blue-200 focus:text-text-default focus:shadow-focus focus:shadow-blue-200"
         href={href}
-        onClick={(e) =>
-          amplitudeLogNavigation("footer", e.currentTarget.getAttribute("href"))
-        }
+        onClick={(e) => {
+          const target = e.currentTarget.getAttribute("href");
+          target && amplitudeLogNavigation("footer", target);
+        }}
       >
         {children}
       </Link>
