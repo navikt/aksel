c80d9b733b47d1e2994cfa01dfc48355b2feec25
Halvor Haugan <halvor.haugan@nav.no>
2024-04-02 13:04:31 +0200

    forwardRef på Header

--- @navikt/core/react/src/form-summary/FormSummaryHeader.tsx
+++ @navikt/core/react/src/form-summary/FormSummaryHeader.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { Box } from "../layout/box";
 import { HStack } from "../layout/stack";
 
@@ -6,22 +6,24 @@ interface FormSummaryHeaderProps {
   children: React.ReactNode;
 }
 
-export default function FormSummaryHeader({
-  children,
-}: FormSummaryHeaderProps) {
-  return (
-    <Box
-      as="header"
-      background="surface-subtle"
-      borderRadius="large large 0 0"
-      borderWidth="0 0 1 0"
-      borderColor="border-subtle"
-      paddingBlock="4"
-      paddingInline="6"
-    >
-      <HStack justify="space-between" gap="2" wrap={false}>
-        {children}
-      </HStack>
-    </Box>
-  );
-}
+export const FormSummaryHeader = forwardRef<
+  HTMLDivElement,
+  FormSummaryHeaderProps
+>(({ children }, ref) => (
+  <Box
+    ref={ref}
+    as="header"
+    background="surface-subtle"
+    borderRadius="large large 0 0"
+    borderWidth="0 0 1 0"
+    borderColor="border-subtle"
+    paddingBlock="4"
+    paddingInline="6"
+  >
+    <HStack justify="space-between" gap="2" wrap={false}>
+      {children}
+    </HStack>
+  </Box>
+));
+
+export default FormSummaryHeader;
