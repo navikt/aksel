00df5743bce8824fb49028620cb65fb94b940d82
Ken <ken.aleksander@gmail.com>
2024-04-04 15:19:44 +0200

    :dizzy: Animert farger

--- aksel.nav.no/website/components/website-modules/menu/Menu.stories.tsx
+++ aksel.nav.no/website/components/website-modules/menu/Menu.stories.tsx
@@ -1,4 +1,8 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import { Meta, StoryObj } from "@storybook/react";
+import { useState } from "react";
 import { Menu, MenuHeading, MenuListItem } from "./Menu";
 
 const meta = {
@@ -12,23 +16,30 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const MenuDemo: Story = {
-  render: (props) => <Menu {...props} />,
+  render: () => {
+    const [selected, setSelected] = useState(0);
+    return (
+      <Menu>
+        <>
+          <MenuHeading>Primitives</MenuHeading>
+          <ul onClick={() => setSelected((x) => (x === 0 ? 1 : 0))}>
+            <MenuListItem href="#">Bleed</MenuListItem>
+            <MenuListItem href="#">Box</MenuListItem>
+            <MenuListItem href="#" selected={selected === 1}>
+              HGrid
+            </MenuListItem>
+            <MenuListItem href="#">Hide</MenuListItem>
+            <MenuListItem href="#" selected={selected === 0}>
+              HStack
+            </MenuListItem>
+            <MenuListItem href="#">Show</MenuListItem>
+            <MenuListItem href="#">VStack</MenuListItem>
+          </ul>
+        </>
+      </Menu>
+    );
+  },
   args: {
-    children: (
-      <>
-        <MenuHeading>Primitives</MenuHeading>
-        <ul>
-          <MenuListItem href="#">Bleed</MenuListItem>
-          <MenuListItem href="#">Box</MenuListItem>
-          <MenuListItem href="#">HGrid</MenuListItem>
-          <MenuListItem href="#">Hide</MenuListItem>
-          <MenuListItem href="#" selected>
-            HStack
-          </MenuListItem>
-          <MenuListItem href="#">Show</MenuListItem>
-          <MenuListItem href="#">VStack</MenuListItem>
-        </ul>
-      </>
-    ),
+    children: null,
   },
 };
--- aksel.nav.no/website/components/website-modules/menu/Menu.tsx
+++ aksel.nav.no/website/components/website-modules/menu/Menu.tsx
@@ -41,20 +41,23 @@ export function MenuListItem({ children, href, selected }: MenuListItemProps) {
           amplitudeLogNavigation("meny", e.currentTarget.getAttribute("href"))
         }
         className={cl(
-          "ml-2 flex py-05 group-last:last-of-type:pb-0 group-first-of-type:pt-0",
-          "before:absolute before:-left-px before:top-05 before:h-6 before:rounded-r-sm group-first-of-type:before:top-0",
+          "ml-2 flex py-05 focus:outline-none *:focus-visible:shadow-focus group-last:last-of-type:pb-0 group-first-of-type:pt-0",
+          "before:absolute before:-left-px before:top-05 before:h-6 before:rounded-r-sm before:transition-all group-first-of-type:before:top-0",
           {
-            "text-text-subtle before:w-0 before:bg-gray-400 before:transition-[width] before:duration-100 before:ease-[cubic-bezier(0.3,0,0.15,1)] hover:text-text-default hover:before:w-1":
+            "text-text-subtle before:w-0 before:bg-gray-400  before:duration-100 before:ease-[cubic-bezier(0.3,0,0.15,1)] hover:text-text-default hover:before:w-1":
               !selected,
             "text-deepblue-700 before:w-1 before:bg-deepblue-700": selected,
           },
         )}
       >
         <span
-          className={cl("rounded px-2 py-05", {
-            "bg-surface-selected": selected,
-            "": !selected,
-          })}
+          className={cl(
+            "w-full rounded px-2 py-05 transition-colors duration-200 ease-[cubic-bezier(0.3,0,0.15,1)]",
+            {
+              "bg-surface-selected": selected,
+              "bg-transparent": !selected,
+            },
+          )}
         >
           {children}
         </span>
