c4eea53e0211436fe687d890939f350da91ba106
Ken <ken.aleksander@gmail.com>
2024-04-10 10:06:52 +0200

    :memo: Demo for fill-prop på aksel.nav.no

---
+++ aksel.nav.no/website/pages/eksempler/togglegroup/fill.tsx
@@ -0,0 +1,24 @@
+import { ToggleGroup } from "@navikt/ds-react";
+import { withDsExample } from "@/web/examples/withDsExample";
+
+const Example = () => {
+  return (
+    <ToggleGroup defaultValue="lest" fill>
+      <ToggleGroup.Item value="ulest">Ulest</ToggleGroup.Item>
+      <ToggleGroup.Item value="lest">Lest</ToggleGroup.Item>
+      <ToggleGroup.Item value="sendt">Sendt</ToggleGroup.Item>
+    </ToggleGroup>
+  );
+};
+
+// EXAMPLES DO NOT INCLUDE CONTENT BELOW THIS LINE
+export default withDsExample(Example);
+
+/* Storybook story */
+export const Demo = {
+  render: Example,
+};
+
+export const args = {
+  index: 3,
+};
