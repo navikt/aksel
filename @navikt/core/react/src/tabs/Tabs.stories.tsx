fbd489419dcad49461b16085eaf097b1566d1852
Ken <ken.aleksander@gmail.com>
2024-04-10 12:07:58 +0200

    :recycle: Omskrevet tabs-stories for ny syntax

--- @navikt/core/react/src/tabs/Tabs.stories.tsx
+++ @navikt/core/react/src/tabs/Tabs.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import React, { useState } from "react";
 import { DishwasherIcon, FreezerIcon, MugIcon } from "@navikt/aksel-icons";
 import { Tabs } from ".";
@@ -26,39 +26,39 @@ export default {
   },
 } satisfies Meta<typeof Tabs>;
 
-const Panel = () => {
-  return (
-    <>
-      <Tabs.Panel
-        value="test1"
-        className="panel"
-        style={{ background: "var(--a-gray-50)", height: 100 }}
-      >
-        Innholdspanel for Skap-tab
-      </Tabs.Panel>
-      <Tabs.Panel
-        value="test2"
-        className="panel"
-        style={{
-          background: "var(--a-green-50)",
-          height: 100,
-        }}
-      >
-        Innholdspanel for Oppvaskmaskin-tab
-      </Tabs.Panel>
-      <Tabs.Panel
-        value="test3"
-        className="panel"
-        style={{ background: "var(--a-red-50)", height: 100 }}
-      >
-        Innholdspanel for Fryser-tab
-      </Tabs.Panel>
-      <style>{`.panel[data-state="active"] { display:grid; place-content: center;}`}</style>
-    </>
-  );
-};
+type Story = StoryObj<typeof Tabs>;
 
-export const Default = {
+const Panel = () => (
+  <>
+    <Tabs.Panel
+      value="test1"
+      className="panel"
+      style={{ background: "var(--a-gray-50)", height: 100 }}
+    >
+      Innholdspanel for Skap-tab
+    </Tabs.Panel>
+    <Tabs.Panel
+      value="test2"
+      className="panel"
+      style={{
+        background: "var(--a-green-50)",
+        height: 100,
+      }}
+    >
+      Innholdspanel for Oppvaskmaskin-tab
+    </Tabs.Panel>
+    <Tabs.Panel
+      value="test3"
+      className="panel"
+      style={{ background: "var(--a-red-50)", height: 100 }}
+    >
+      Innholdspanel for Fryser-tab
+    </Tabs.Panel>
+    <style>{`.panel[data-state="active"] { display:grid; place-content: center;}`}</style>
+  </>
+);
+
+export const Default: Story = {
   render: (props) => {
     return (
       <Tabs
@@ -92,26 +92,24 @@ export const Default = {
   },
 };
 
-export const Small = () => {
-  return (
-    <Tabs defaultValue="test2" size="small">
-      <Tabs.List>
-        <Tabs.Tab value="test1" icon={<MugIcon aria-hidden />} label="Skap" />
-        <Tabs.Tab
-          value="test2"
-          label="Oppvaskmaskin"
-          icon={<DishwasherIcon aria-hidden />}
-        />
-        <Tabs.Tab
-          value="test3"
-          icon={<FreezerIcon aria-hidden />}
-          label="Fryser"
-        />
-      </Tabs.List>
-      <Panel />
-    </Tabs>
-  );
-};
+export const Small = () => (
+  <Tabs defaultValue="test2" size="small">
+    <Tabs.List>
+      <Tabs.Tab value="test1" icon={<MugIcon aria-hidden />} label="Skap" />
+      <Tabs.Tab
+        value="test2"
+        label="Oppvaskmaskin"
+        icon={<DishwasherIcon aria-hidden />}
+      />
+      <Tabs.Tab
+        value="test3"
+        icon={<FreezerIcon aria-hidden />}
+        label="Fryser"
+      />
+    </Tabs.List>
+    <Panel />
+  </Tabs>
+);
 
 export const Controlled = () => {
   const [activeValue, setActiveValue] = useState("test1");
@@ -135,77 +133,9 @@ export const Controlled = () => {
   );
 };
 
-export const IconPosition = () => {
-  return (
-    <div className="colgap">
-      <Tabs defaultValue="test2" size="small">
-        <Tabs.List>
-          <Tabs.Tab value="test1" icon={<MugIcon aria-hidden />} label="Skap" />
-          <Tabs.Tab
-            value="test2"
-            label="Oppvaskmaskin"
-            icon={<DishwasherIcon aria-hidden />}
-          />
-          <Tabs.Tab
-            value="test3"
-            icon={<FreezerIcon aria-hidden />}
-            label="Fryser"
-          />
-        </Tabs.List>
-        <Panel />
-      </Tabs>
-      <Tabs defaultValue="test2" size="small" iconPosition="top">
-        <Tabs.List style={{ margin: "0 auto" }}>
-          <Tabs.Tab value="test1" icon={<MugIcon aria-hidden />} label="Skap" />
-          <Tabs.Tab
-            value="test2"
-            label="Oppvaskmaskin"
-            icon={<DishwasherIcon aria-hidden />}
-          />
-          <Tabs.Tab
-            value="test3"
-            icon={<FreezerIcon aria-hidden />}
-            label="Fryser"
-          />
-        </Tabs.List>
-        <Panel />
-      </Tabs>
-    </div>
-  );
-};
-
-export const Icon = () => {
-  return (
-    <div className="colgap">
-      <Tabs defaultValue="test2">
-        <Tabs.List style={{ margin: "0 auto" }}>
-          <Tabs.Tab value="test1" icon={<MugIcon title="Skap" />} />
-          <Tabs.Tab
-            value="test2"
-            icon={<DishwasherIcon title="Oppvaskmaskin" />}
-          />
-          <Tabs.Tab value="test3" icon={<FreezerIcon title="Fryser" />} />
-        </Tabs.List>
-        <Panel />
-      </Tabs>
-      <Tabs defaultValue="test2" size="small" iconPosition="top">
-        <Tabs.List style={{ margin: "0 auto" }}>
-          <Tabs.Tab value="test1" icon={<MugIcon title="Skap" />} />
-          <Tabs.Tab
-            value="test2"
-            icon={<DishwasherIcon title="Oppvaskmaskin" />}
-          />
-          <Tabs.Tab value="test3" icon={<FreezerIcon title="Fryser" />} />
-        </Tabs.List>
-        <Panel />
-      </Tabs>
-    </div>
-  );
-};
-
-export const Overflow = () => {
-  return (
-    <Tabs defaultValue="test2" style={{ maxWidth: 300 }}>
+export const IconPosition = () => (
+  <VStack gap="4">
+    <Tabs defaultValue="test2" size="small">
       <Tabs.List>
         <Tabs.Tab value="test1" icon={<MugIcon aria-hidden />} label="Skap" />
         <Tabs.Tab
@@ -221,18 +151,13 @@ export const Overflow = () => {
       </Tabs.List>
       <Panel />
     </Tabs>
-  );
-};
-
-export const Fill = {
-  render: () => (
-    <Tabs defaultValue="test2" fill>
-      <Tabs.List>
+    <Tabs defaultValue="test2" size="small" iconPosition="top">
+      <Tabs.List style={{ margin: "0 auto" }}>
         <Tabs.Tab value="test1" icon={<MugIcon aria-hidden />} label="Skap" />
         <Tabs.Tab
           value="test2"
-          icon={<DishwasherIcon aria-hidden />}
           label="Oppvaskmaskin"
+          icon={<DishwasherIcon aria-hidden />}
         />
         <Tabs.Tab
           value="test3"
@@ -242,59 +167,116 @@ export const Fill = {
       </Tabs.List>
       <Panel />
     </Tabs>
-  ),
-  parameters: {
-    layout: "fullscreen",
-  },
-};
+  </VStack>
+);
 
-export const CustomIds = {
-  render: () => (
+export const Icon = () => (
+  <VStack gap="4">
     <Tabs defaultValue="test2">
-      <Tabs.List>
-        <Tabs.Tab
-          value="test1"
-          label="Skap"
-          id="custom-tabid-1"
-          aria-controls="custom-tabpanelid-1"
-        />
+      <Tabs.List style={{ margin: "0 auto" }}>
+        <Tabs.Tab value="test1" icon={<MugIcon title="Skap" />} />
         <Tabs.Tab
           value="test2"
-          label="Oppvaskmaskin"
-          id="custom-tabid-2"
-          aria-controls="custom-tabpanelid-2"
+          icon={<DishwasherIcon title="Oppvaskmaskin" />}
         />
+        <Tabs.Tab value="test3" icon={<FreezerIcon title="Fryser" />} />
       </Tabs.List>
-      <Tabs.Panel
-        value="test1"
-        className="panel"
-        id="custom-tabpanelid-1"
-        aria-labelledby="custom-tabid-1"
-      >
-        Innholdspanel for Skap-tab
-      </Tabs.Panel>
-      <Tabs.Panel
-        value="test2"
-        className="panel"
-        id="custom-tabpanelid-2"
-        aria-labelledby="custom-tabid-2"
-      >
-        Innholdspanel for Oppvaskmaskin-tab
-      </Tabs.Panel>
+      <Panel />
     </Tabs>
-  ),
-  parameters: {
-    layout: "fullscreen",
-  },
+    <Tabs defaultValue="test2" size="small" iconPosition="top">
+      <Tabs.List style={{ margin: "0 auto" }}>
+        <Tabs.Tab value="test1" icon={<MugIcon title="Skap" />} />
+        <Tabs.Tab
+          value="test2"
+          icon={<DishwasherIcon title="Oppvaskmaskin" />}
+        />
+        <Tabs.Tab value="test3" icon={<FreezerIcon title="Fryser" />} />
+      </Tabs.List>
+      <Panel />
+    </Tabs>
+  </VStack>
+);
+
+export const Overflow = () => (
+  <Tabs defaultValue="test2" style={{ maxWidth: 300 }}>
+    <Tabs.List>
+      <Tabs.Tab value="test1" icon={<MugIcon aria-hidden />} label="Skap" />
+      <Tabs.Tab
+        value="test2"
+        label="Oppvaskmaskin"
+        icon={<DishwasherIcon aria-hidden />}
+      />
+      <Tabs.Tab
+        value="test3"
+        icon={<FreezerIcon aria-hidden />}
+        label="Fryser"
+      />
+    </Tabs.List>
+    <Panel />
+  </Tabs>
+);
+
+export const Fill = () => (
+  <Tabs defaultValue="test2" fill>
+    <Tabs.List>
+      <Tabs.Tab value="test1" icon={<MugIcon aria-hidden />} label="Skap" />
+      <Tabs.Tab
+        value="test2"
+        icon={<DishwasherIcon aria-hidden />}
+        label="Oppvaskmaskin"
+      />
+      <Tabs.Tab
+        value="test3"
+        icon={<FreezerIcon aria-hidden />}
+        label="Fryser"
+      />
+    </Tabs.List>
+    <Panel />
+  </Tabs>
+);
+
+Fill.parameters = {
+  layout: "fullscreen",
 };
+
+export const CustomIds = () => (
+  <Tabs defaultValue="test2">
+    <Tabs.List>
+      <Tabs.Tab
+        value="test1"
+        label="Skap"
+        id="custom-tabid-1"
+        aria-controls="custom-tabpanelid-1"
+      />
+      <Tabs.Tab
+        value="test2"
+        label="Oppvaskmaskin"
+        id="custom-tabid-2"
+        aria-controls="custom-tabpanelid-2"
+      />
+    </Tabs.List>
+    <Tabs.Panel
+      value="test1"
+      className="panel"
+      id="custom-tabpanelid-1"
+      aria-labelledby="custom-tabid-1"
+    >
+      Innholdspanel for Skap-tab
+    </Tabs.Panel>
+    <Tabs.Panel
+      value="test2"
+      className="panel"
+      id="custom-tabpanelid-2"
+      aria-labelledby="custom-tabid-2"
+    >
+      Innholdspanel for Oppvaskmaskin-tab
+    </Tabs.Panel>
+  </Tabs>
+);
 
 export const Chromatic = {
   render: () => (
     <VStack gap="6" align="center">
-      <div>
-        <h2>Default</h2>
-        <Default.render />
-      </div>
       <div>
         <h2>Small</h2>
         <Small />
@@ -318,7 +300,7 @@ export const Chromatic = {
       <div>
         <h2>Fill</h2>
         <div style={{ minWidth: 600 }}>
-          <Fill.render />
+          <Fill />
         </div>
       </div>
     </VStack>
