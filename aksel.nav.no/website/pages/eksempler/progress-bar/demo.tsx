4d240b8941cc82e975e3a851f1ccf71a1234609d
Lars Hansen <lars.eirik.korsgaard.hansen@nav.no>
2024-04-12 11:45:46 +0200

    remove demo

--- aksel.nav.no/website/pages/eksempler/progress-bar/demo.tsx
+++ aksel.nav.no/website/pages/eksempler/progress-bar/demo.tsx
@@ -1,24 +1,24 @@
-import { ProgressBar } from "@navikt/ds-react";
-import { withDsExample } from "@/web/examples/withDsExample";
+// import { ProgressBar } from "@navikt/ds-react";
+// import { withDsExample } from "@/web/examples/withDsExample";
 
-const Example = () => {
-  return (
-    <div className="flex flex-wrap gap-2">
-      <ProgressBar size="small" />
-      <ProgressBar />
-      <ProgressBar size="large" />
-    </div>
-  );
-};
+// const Example = () => {
+//   return (
+//     <div className="flex flex-wrap gap-2">
+//       <ProgressBar size="small" />
+//       <ProgressBar />
+//       <ProgressBar size="large" />
+//     </div>
+//   );
+// };
 
-// EXAMPLES DO NOT INCLUDE CONTENT BELOW THIS LINE
-export default withDsExample(Example);
+// // EXAMPLES DO NOT INCLUDE CONTENT BELOW THIS LINE
+// export default withDsExample(Example);
 
-/* Storybook story */
-export const Demo = {
-  render: Example,
-};
+// /* Storybook story */
+// export const Demo = {
+//   render: Example,
+// };
 
-export const args = {
-  index: 0,
-};
+// export const args = {
+//   index: 0,
+// };
