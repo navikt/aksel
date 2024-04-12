406977b5fb8a7d58d6f5b75b68683b1dabddc62f
Lars Hansen <lars.eirik.korsgaard.hansen@nav.no>
2024-04-12 10:56:26 +0200

    Add overcomplicated loading simulator

--- @navikt/core/react/src/progress-bar/ProgressBar.stories.tsx
+++ @navikt/core/react/src/progress-bar/ProgressBar.stories.tsx
@@ -15,10 +15,38 @@ type Story = StoryObj<typeof ProgressBar>;
 
 export const Default: Story = {
   render: () => {
+    const [value, setValue] = React.useState(0);
+    React.useEffect(() => {
+      const withRandomIntervals = (callback: () => void) => {
+        const interval = Math.random() * 800 + 100; // Random time between 0.1-0.8 seconds
+        return setTimeout(() => {
+          callback();
+          withRandomIntervals(callback); // Recursively call setRandomInterval
+        }, interval);
+      };
+
+      const intervalId = withRandomIntervals(() => {
+        setValue(
+          (oldValue) =>
+            oldValue >= 100 ? 100 : oldValue + Math.random() * 35 + 5, // Increase value 5-40 of 100
+        );
+      });
+
+      const timeoutId = setTimeout(
+        () => clearInterval(intervalId),
+        Math.random() * 4000 + 1000, // Random time between 1-5 seconds
+      );
+
+      return () => {
+        clearInterval(intervalId);
+        clearTimeout(timeoutId);
+      };
+    }, []);
+
     return (
-      <div>
+      <div style={{ width: "400px" }}>
         <p id="progress-bar-label">Loading</p>
-        <ProgressBar size="medium" value={0} labelId="progress-bar-label" />
+        <ProgressBar size="medium" value={value} labelId="progress-bar-label" />
       </div>
     );
   },
