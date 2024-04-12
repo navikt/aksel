13a8fa25b22c44bdc7d8b36d17744ee4bf129235
Ken <ken.aleksander@gmail.com>
2024-04-12 11:35:51 +0200

    :zap: Bruker sanity useCurrentUser for å vise tilganger til bruker på profilside

--- aksel.nav.no/website/sanity/schema/custom-components/EditorPage.tsx
+++ aksel.nav.no/website/sanity/schema/custom-components/EditorPage.tsx
@@ -1,28 +1,13 @@
 import React from "react";
-import { useClient, useCurrentUser } from "sanity";
-import useSWR from "swr";
-import { Heading, Label, Link, Loader } from "@navikt/ds-react";
-import { SANITY_API_VERSION } from "@/sanity/config";
+import { useCurrentUser } from "sanity";
+import { Heading, Label, Link } from "@navikt/ds-react";
 
-export const EditorPage = () => {
+export const EditorPage = (...rest) => {
+  console.log(rest);
   const user = useCurrentUser();
 
-  const client = useClient({ apiVersion: SANITY_API_VERSION });
-  const { data, error } = useSWR(
-    `*[count((contributors[]->{email, alt_email})[@.email == "${user?.email}" || @.alt_email == "${user?.email}"]) > 0]`,
-    (query) => client.fetch(query),
-  );
-
-  if (error || !user) {
-    return <div>Feilet lasting av bruker...</div>;
-  }
-
-  if (!data) {
-    return (
-      <div className="mx-auto mt-24">
-        <Loader size="xlarge" variant="neutral" />
-      </div>
-    );
+  if (!user) {
+    return null;
   }
 
   return (
