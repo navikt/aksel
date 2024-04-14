29821be45d2e72d287f119240382bf4eafb3066e
Ken <ken.aleksander@gmail.com>
2024-04-14 07:59:06 +0200

    :art: Artikler med contributors får automatisk editor lagt til ved creation

--- aksel.nav.no/website/sanity/schema/documents/presets/editors.ts
+++ aksel.nav.no/website/sanity/schema/documents/presets/editors.ts
@@ -1,4 +1,5 @@
 import { defineField } from "sanity";
+import { SANITY_API_VERSION } from "@/sanity/config";
 
 export const editorField = defineField({
   title: "Bidragsytere",
@@ -8,4 +9,28 @@ export const editorField = defineField({
   of: [{ type: "reference", to: [{ type: "editor" }] }],
   group: "settings",
   validation: (Rule) => Rule.required(),
+  initialValue: async (_, { currentUser, getClient }) => {
+    const client = getClient({ apiVersion: SANITY_API_VERSION });
+    let profile = await client.fetch(
+      `*[_type == "editor" && (lower($mail) == lower(email) || lower($mail) == lower(alt_email))][0]`,
+      { mail: currentUser?.email },
+    );
+
+    /**
+     * If user forgot to create a profile, we create one for them
+     */
+    if (!profile) {
+      if (!currentUser?.email.endsWith("@nav.no")) {
+        return [];
+      }
+      profile = await client.createIfNotExists({
+        _type: "editor",
+        _id: `auto-editor-${currentUser?.id}`,
+        email: currentUser?.email,
+        title: currentUser?.name,
+      });
+    }
+
+    return [{ _ref: profile._id, _type: "reference" }];
+  },
 });
