c2968a0e6d2e3811d935b5ac771efd7dd77bbf1a
Ken <26967723+KenAJoh@users.noreply.github.com>
2024-04-15 11:16:27 +0200

    :art: Artikler med contributors får automatisk editor (#2849)
    * :art: Artikler med contributors får automatisk editor lagt til ved creation
    
    * :lock: Lager bare profil automatisk for SSO-inlogging

--- aksel.nav.no/website/sanity/schema/documents/presets/editors.ts
+++ aksel.nav.no/website/sanity/schema/documents/presets/editors.ts
@@ -1,4 +1,5 @@
 import { defineField } from "sanity";
+import { SANITY_API_VERSION } from "@/sanity/config";
 
 export const editorField = defineField({
   title: "Bidragsytere",
@@ -8,4 +9,32 @@ export const editorField = defineField({
   of: [{ type: "reference", to: [{ type: "editor" }] }],
   group: "settings",
   validation: (Rule) => Rule.required(),
+  initialValue: async (_, { currentUser, getClient }) => {
+    const client = getClient({ apiVersion: SANITY_API_VERSION });
+    let profile = await client.fetch(
+      `*[_type == "editor" && (lower($mail) == lower(email) || lower($mail) == lower(alt_email))][0]`,
+      { mail: currentUser?.email },
+    );
+
+    /**
+     * If user forgot to create a profile, we create one for them
+     */
+    if (!profile) {
+      /* Make sure to only create profile for SSO-logins */
+      if (
+        !currentUser?.email.endsWith("@nav.no") ||
+        !currentUser.provider?.includes("saml")
+      ) {
+        return [];
+      }
+      profile = await client.createIfNotExists({
+        _type: "editor",
+        _id: `auto-editor.${currentUser.id}`,
+        email: currentUser.email,
+        title: currentUser.name,
+      });
+    }
+
+    return [{ _ref: profile._id, _type: "reference" }];
+  },
 });
