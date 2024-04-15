3a73a029f6272f76e10cb6baefbaa2040feaef6b
Julian Nymark <julian.nymark@nav.no>
2024-04-15 09:53:42 +0200

    :sparkles: new FormSummary component (#2802)
    * :construction: wip on Summary
    
    * :sparkles: new HR component
    
    * :necktie: fancy utility function to inject components into children
    
    This is to be used in 2 places where we iterate over children and add <hr /> components between them all.
    
    It also wraps the children in a <Box>, which is used here and not really reflected in the name of the function :thinking:
    
    * :art: improved version of Summary component
    
    * Remove (some) magic
    
    * Fix children type in injectHRBetween
    
    * css
    
    * header gap and bg
    
    * responsive story + example with long texts
    
    * :truck: Summary -> FormSummary
    
    * :art: update stories
    
    * :art: update story + tweak CSS for EditButton
    
    * :memo: add JSDoc
    
    * export FormSummary in core/react/src/index.ts
    
    * FormSummaryValue bruker BodyLong
    
    * :memo: update JSDoc
    
    * :memo: add demo
    
    * :memo: update README.md for eksempler
    
    * forwardRef på Answers
    
    * forwardRef på Header
    
    * forwardRef på Label og Value
    
    * forwardRef på Heading og Edit
    
    * Answer export
    
    * spread props
    
    * :fire: remove HR component
    
    * :memo: update JSDoc
    
    * :art: export more FormSummary
    
    * :fire: duplicate dependency of _mapping (same as main)
    
    * :memo: update JSDoc for FormSummary
    
    * changeset
    
    * :bookmark: changeset
    
    * :fire: duplicate changeset
    
    * Rename EditButton -> Edit
    
    * oops
    
    * Link er vel valgfritt
    
    * :bug: add correct CSS dependency for FormSummary
    
    * :lipstick: FormSummary
    
    * :lipstick: FormSummary
    
    * className navds-form-summary
    
    * className navds-form-summary__answers
    
    * className navds-form-summary__header
    
    * Update @navikt/core/css/form/form-summary.css
    
    Co-authored-by: Ken <26967723+KenAJoh@users.noreply.github.com>
    
    * Update @navikt/core/react/src/form-summary/FormSummaryLabel.tsx
    
    Co-authored-by: Ken <26967723+KenAJoh@users.noreply.github.com>
    
    * Update @navikt/core/react/src/form-summary/FormSummaryValue.tsx
    
    Co-authored-by: Ken <26967723+KenAJoh@users.noreply.github.com>
    
    * Header wrapper på små skjermer
    
    * Rekkefølgen på selectors samme som DOM
    
    * :art: structure consistency (ref, then ...rest, then className, then other things)
    
    - also convert a props -> {not_the_rest, ...rest}
    
    * update mappings
    
    * Flytt border slik at du kan ha flere dd etter hverandre
    
    * :art: update stories
    
    * :art: update stories (more empty values)
    
    * :truck: story ComplexData -> RealisticUsage
    
    * Mindre padding på små skjermer
    
    * :art: update stories
    
    * Answer forwardRef
    
    * :art: update stories
    
    * Eksempel på linjeskift i value i story
    
    * :art: update story
    
    * :art: update story
    
    * :truck: FormSummary -> Form/FormSummary
    
    * :art: update Demo and story
    
    * :label: make href required for FormSummaryEdit
    
    * :label: + :construction: typing woes
    
    * :lipstick: tweak CSS to make it look just right
    
    * :label: correct typing using Exclude > Omit
    
    * :art: use div over section without label
    
    * :lipstick: FormSummary, last-child padding removal
    
    * Fjernet kommentar
    
    * typo i readme
    
    * :truck: FormSummary.Edit -> FormSummary.EditLink
    
    * :label: make href for EditLink optional
    
    * :truck: rename FormSummaryEdit -> FormSummaryEditLink
    
    * FormSummaryEdit -> FormSummaryEditLink
    
    * :sparkles: add example to FormSummary
    
    * :art: update examples
    
    * :art: update examples
    
    * :lipstick: FormSummary
    
    * :art: types + jsdoc tweaks (FormSummary)
    
    ---------
    
    Co-authored-by: Halvor Haugan <halvor.haugan@nav.no>
    Co-authored-by: Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>
    Co-authored-by: Ken <26967723+KenAJoh@users.noreply.github.com>

---
+++ @navikt/core/react/src/form/form-summary/FormSummaryHeading.tsx
@@ -0,0 +1,25 @@
+import React, { forwardRef } from "react";
+import { Heading, HeadingProps } from "../../typography";
+import { OverridableComponent } from "../../util/types";
+
+export interface FormSummaryHeadingProps
+  extends React.HTMLAttributes<HTMLHeadingElement> {
+  /**
+   * Heading text.
+   */
+  children: React.ReactNode;
+  /**
+   * The heading level.
+   * @default "3"
+   */
+  level?: Exclude<HeadingProps["level"], "1">;
+}
+
+export const FormSummaryHeading: OverridableComponent<
+  FormSummaryHeadingProps,
+  HTMLHeadingElement
+> = forwardRef(({ level = "3", ...rest }, ref) => (
+  <Heading ref={ref} {...rest} size="medium" level={level} />
+));
+
+export default FormSummaryHeading;
---
+++ aksel.nav.no/website/pages/eksempler/formsummary/demo.tsx
@@ -0,0 +1,99 @@
+import { FormSummary, Link } from "@navikt/ds-react";
+import { withDsExample } from "@/web/examples/withDsExample";
+
+const Example = () => {
+  return (
+    <FormSummary>
+      <FormSummary.Header>
+        <FormSummary.Heading>Personalia</FormSummary.Heading>
+        <FormSummary.EditLink href="#" />
+      </FormSummary.Header>
+
+      <FormSummary.Answers>
+        <FormSummary.Answer>
+          <FormSummary.Label>Navn</FormSummary.Label>
+          <FormSummary.Value>Ola Nordmann</FormSummary.Value>
+        </FormSummary.Answer>
+
+        <FormSummary.Answer>
+          <FormSummary.Label>Adresse</FormSummary.Label>
+          <FormSummary.Value>
+            Gate 123
+            <br />
+            1234 Sted
+          </FormSummary.Value>
+        </FormSummary.Answer>
+
+        <FormSummary.Answer>
+          <FormSummary.Label>Fødselsnummer</FormSummary.Label>
+          <FormSummary.Value>12345678910</FormSummary.Value>
+        </FormSummary.Answer>
+
+        <FormSummary.Answer>
+          <FormSummary.Label>Barn nr. 1</FormSummary.Label>
+          <FormSummary.Value>
+            <FormSummary.Answers>
+              <FormSummary.Answer>
+                <FormSummary.Label>Navn</FormSummary.Label>
+                <FormSummary.Value>Kari Nordmann</FormSummary.Value>
+              </FormSummary.Answer>
+              <FormSummary.Answer>
+                <FormSummary.Label>Kjønn</FormSummary.Label>
+                <FormSummary.Value>Jente</FormSummary.Value>
+              </FormSummary.Answer>
+              <FormSummary.Answer>
+                <FormSummary.Label>Alder</FormSummary.Label>
+                <FormSummary.Value>6</FormSummary.Value>
+              </FormSummary.Answer>
+            </FormSummary.Answers>
+          </FormSummary.Value>
+        </FormSummary.Answer>
+
+        <FormSummary.Answer>
+          <FormSummary.Label>Barn nr. 2</FormSummary.Label>
+          <FormSummary.Value>
+            <FormSummary.Answers>
+              <FormSummary.Answer>
+                <FormSummary.Label>Navn</FormSummary.Label>
+                <FormSummary.Value>Per Nordmann</FormSummary.Value>
+              </FormSummary.Answer>
+              <FormSummary.Answer>
+                <FormSummary.Label>Kjønn</FormSummary.Label>
+                <FormSummary.Value>Gutt</FormSummary.Value>
+              </FormSummary.Answer>
+              <FormSummary.Answer>
+                <FormSummary.Label>Alder</FormSummary.Label>
+                <FormSummary.Value>7</FormSummary.Value>
+              </FormSummary.Answer>
+            </FormSummary.Answers>
+          </FormSummary.Value>
+        </FormSummary.Answer>
+
+        <FormSummary.Answer>
+          <FormSummary.Label>Kontaktinformasjon</FormSummary.Label>
+          <FormSummary.Value>
+            <Link>Fyll ut kontaktinformasjon</Link>
+          </FormSummary.Value>
+        </FormSummary.Answer>
+
+        <FormSummary.Answer>
+          <FormSummary.Label>Hvordan vil du bli varslet?</FormSummary.Label>
+          <FormSummary.Value>E-post</FormSummary.Value>
+          <FormSummary.Value>SMS</FormSummary.Value>
+        </FormSummary.Answer>
+      </FormSummary.Answers>
+    </FormSummary>
+  );
+};
+
+// EXAMPLES DO NOT INCLUDE CONTENT BELOW THIS LINE
+export default withDsExample(Example, { variant: "static" });
+
+/* Storybook story */
+export const Demo = {
+  render: Example,
+};
+
+export const args = {
+  index: 0,
+};
