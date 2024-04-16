987118fd4769133fce84b6376c103402327b9768
Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>
2024-04-16 10:34:27 +0200

    FormSummary.Heading: Fjern overridable, level oblig. og css-justeringer på <sm (#2857)

--- @navikt/core/react/src/form/form-summary/FormSummaryHeading.tsx
+++ @navikt/core/react/src/form/form-summary/FormSummaryHeading.tsx
@@ -1,6 +1,5 @@
 import React, { forwardRef } from "react";
 import { Heading, HeadingProps } from "../../typography";
-import { OverridableComponent } from "../../util/types";
 
 export interface FormSummaryHeadingProps
   extends React.HTMLAttributes<HTMLHeadingElement> {
@@ -10,16 +9,15 @@ export interface FormSummaryHeadingProps
   children: React.ReactNode;
   /**
    * The heading level.
-   * @default "3"
    */
-  level?: Exclude<HeadingProps["level"], "1">;
+  level: Exclude<HeadingProps["level"], "1">;
 }
 
-export const FormSummaryHeading: OverridableComponent<
-  FormSummaryHeadingProps,
-  HTMLHeadingElement
-> = forwardRef(({ level = "3", ...rest }, ref) => (
-  <Heading ref={ref} {...rest} size="medium" level={level} />
+export const FormSummaryHeading = forwardRef<
+  HTMLHeadingElement,
+  FormSummaryHeadingProps
+>((props: FormSummaryHeadingProps, ref) => (
+  <Heading ref={ref} {...props} size="medium" />
 ));
 
 export default FormSummaryHeading;
