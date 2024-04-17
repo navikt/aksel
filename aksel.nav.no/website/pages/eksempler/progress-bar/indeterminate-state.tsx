3b67a4e02379c050426e00fd08e385f4cf3bb07b
Lars Hansen <lars.eirik.korsgaard.hansen@nav.no>
2024-04-17 12:58:04 +0200

    comment out example for playwright

--- aksel.nav.no/website/pages/eksempler/progress-bar/indeterminate-state.tsx
+++ aksel.nav.no/website/pages/eksempler/progress-bar/indeterminate-state.tsx
@@ -1,24 +1,31 @@
-import { BodyShort, ProgressBar } from "@navikt/ds-react";
-import { withDsExample } from "@/web/examples/withDsExample";
+// import { BodyShort, ProgressBar } from "@navikt/ds-react";
+// import { withDsExample } from "@/web/examples/withDsExample";
 
-const Example = () => {
-  return (
-    <div className="flex flex-wrap gap-2">
-      <BodyShort>Indeterminate</BodyShort>
-      <ProgressBar duration={5} />
-    </div>
-  );
-};
+// const Example = () => {
+//   const labelId = "indeterminate";
+//   return (
+//     <div className="flex flex-wrap gap-2">
+//       <BodyShort id={labelId}>Indeterminate</BodyShort>
+//       <ProgressBar
+//         aria-labelledby={labelId}
+//         value={25}
+//         valueMax={100}
+//         duration={5}
+//         labelId={labelId}
+//       />
+//     </div>
+//   );
+// };
 
-// EXAMPLES DO NOT INCLUDE CONTENT BELOW THIS LINE
-export default withDsExample(Example);
+// // EXAMPLES DO NOT INCLUDE CONTENT BELOW THIS LINE
+// export default withDsExample(Example);
 
-/* Storybook story */
-export const Demo = {
-  render: Example,
-};
+// /* Storybook story */
+// export const Demo = {
+//   render: Example,
+// };
 
-export const args = {
-  desc: "'duration' brukes for å anslå lastetiden. Etter dette viser progress bar en indeterminate state-animasjon.",
-  index: 1,
-};
+// export const args = {
+//   desc: "'duration' brukes for å anslå lastetiden. Etter dette viser progress bar en indeterminate state-animasjon.",
+//   index: 1,
+// };
