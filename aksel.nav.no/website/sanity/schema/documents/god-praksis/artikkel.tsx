f0a3d3de3d1007507751bf103ba8ceffff1dab9f
Ken <ken.aleksander@gmail.com>
2024-04-18 08:16:18 +0200

    :coffin: Fjernet aksel_tema og gammelt feedback-dokument

--- aksel.nav.no/website/sanity/schema/documents/god-praksis/artikkel.tsx
+++ aksel.nav.no/website/sanity/schema/documents/god-praksis/artikkel.tsx
@@ -16,84 +16,83 @@ import { titleField } from "../presets/title-field";
 
 const prefix = "god-praksis/artikler/";
 
-export const godPraksisArtikkel = () =>
-  defineType({
-    title: "God praksis artikkel",
-    name: "aksel_artikkel",
-    type: "document",
-    groups: SanityTabGroups,
-    ...artikkelPreview("God praksis"),
-    fields: [
-      oppdateringsvarsel,
-      ...hiddenFields,
-      defineField({
-        name: "innholdstype",
-        title: "Innholdstype",
-        type: "reference",
-        to: [{ type: "gp.innholdstype" }],
-        components: {
-          field: InnholdstypeHighlight,
-        },
-        options: {
-          disableNew: true,
-        },
-        group: "innhold",
-        validation: (Rule) => Rule.required(),
-      }),
-      defineField({
-        name: "undertema",
-        title: "Undertema",
-        type: "array",
-        group: "innhold",
-        validation: (Rule) => Rule.required(),
-        components: {
-          field: UndertemaHighlight,
-        },
-        of: [
-          defineArrayMember({
-            title: "Undertema",
-            type: "reference",
-            to: [{ type: "gp.tema.undertema" }],
-            options: {
-              disableNew: true,
-              filter: async ({ getClient, parent }) => {
-                const undertema = (
-                  parent as {
-                    _key: string;
-                    _ref?: string;
-                    _type: "reference";
-                  }[]
-                )
-                  .filter((x) => !!x._ref)
-                  .map((x) => x._ref);
+export const GodPraksisArtikkel = defineType({
+  title: "God praksis artikkel",
+  name: "aksel_artikkel",
+  type: "document",
+  groups: SanityTabGroups,
+  ...artikkelPreview("God praksis"),
+  fields: [
+    oppdateringsvarsel,
+    ...hiddenFields,
+    defineField({
+      name: "innholdstype",
+      title: "Innholdstype",
+      type: "reference",
+      to: [{ type: "gp.innholdstype" }],
+      components: {
+        field: InnholdstypeHighlight,
+      },
+      options: {
+        disableNew: true,
+      },
+      group: "innhold",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "undertema",
+      title: "Undertema",
+      type: "array",
+      group: "innhold",
+      validation: (Rule) => Rule.required(),
+      components: {
+        field: UndertemaHighlight,
+      },
+      of: [
+        defineArrayMember({
+          title: "Undertema",
+          type: "reference",
+          to: [{ type: "gp.tema.undertema" }],
+          options: {
+            disableNew: true,
+            filter: async ({ getClient, parent }) => {
+              const undertema = (
+                parent as {
+                  _key: string;
+                  _ref?: string;
+                  _type: "reference";
+                }[]
+              )
+                .filter((x) => !!x._ref)
+                .map((x) => x._ref);
 
-                const client = getClient({ apiVersion: SANITY_API_VERSION });
-                const temaIds = await client.fetch("*[_id in $ids].tema._ref", {
-                  ids: undertema,
-                });
-                return {
-                  filter: "!(tema._ref in $temaIds)",
-                  params: { temaIds },
-                };
-              },
+              const client = getClient({ apiVersion: SANITY_API_VERSION });
+              const temaIds = await client.fetch("*[_id in $ids].tema._ref", {
+                ids: undertema,
+              });
+              return {
+                filter: "!(tema._ref in $temaIds)",
+                params: { temaIds },
+              };
             },
-          }),
-        ],
-      }),
-      titleField,
-      editorField,
-      sanitySlug(prefix, 3),
-      ingressField,
-      defineField({
-        title: "Innhold",
-        description:
-          "Innholdet i artikkelen er riktekst. Tips: klikk på ikon i høyre hjørne for å skrive i fullskjerm.",
-        name: "content",
-        type: "riktekst_standard",
-        group: "innhold",
-      }),
-      relevanteArtiklerField,
-      BaseSEOPreset,
-      skrivehjelp,
-    ],
-  });
+          },
+        }),
+      ],
+    }),
+    titleField,
+    editorField,
+    sanitySlug(prefix, 3),
+    ingressField,
+    defineField({
+      title: "Innhold",
+      description:
+        "Innholdet i artikkelen er riktekst. Tips: klikk på ikon i høyre hjørne for å skrive i fullskjerm.",
+      name: "content",
+      type: "riktekst_standard",
+      group: "innhold",
+    }),
+    relevanteArtiklerField,
+    BaseSEOPreset,
+    skrivehjelp,
+  ],
+});
