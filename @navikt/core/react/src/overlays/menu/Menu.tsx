f21fe8b442327b1ae2f9fa71b03f129b8ed7696a
Ken <ken.aleksander@gmail.com>
2024-04-27 15:51:47 +0200

    feat: Menu checkbox

--- @navikt/core/react/src/overlays/menu/Menu.tsx
+++ @navikt/core/react/src/overlays/menu/Menu.tsx
@@ -582,10 +582,44 @@ export const MenuRadio = ({ children }: MenuRadioProps) => {
 /**
  * Checkbox
  */
-interface MenuCheckboxProps {
-  children: React.ReactNode;
+
+type CheckedState = boolean | "indeterminate";
+
+interface MenuCheckboxProps extends MenuItemProps {
+  checked?: CheckedState;
+  onCheckedChange?: (checked: boolean) => void;
 }
 
-export const MenuCheckbox = ({ children }: MenuCheckboxProps) => {
-  return <div>{children}</div>;
-};
+export const MenuCheckbox = forwardRef(
+  (
+    { checked = false, onSelect, onCheckedChange, ...rest }: MenuCheckboxProps,
+    ref,
+  ) => {
+    return (
+      <MenuItem
+        role="menuitemcheckbox"
+        aria-checked={isIndeterminate(checked) ? "mixed" : checked}
+        {...rest}
+        ref={ref}
+        data-state={getCheckedState(checked)}
+        onSelect={composeEventHandlers<any>(
+          onSelect,
+          () => onCheckedChange?.(isIndeterminate(checked) ? true : !checked),
+          { checkForDefaultPrevented: false },
+        )}
+      />
+    );
+  },
+);
+
+function isIndeterminate(checked?: CheckedState): checked is "indeterminate" {
+  return checked === "indeterminate";
+}
+
+function getCheckedState(checked: CheckedState) {
+  return isIndeterminate(checked)
+    ? "indeterminate"
+    : checked
+      ? "checked"
+      : "unchecked";
+}
