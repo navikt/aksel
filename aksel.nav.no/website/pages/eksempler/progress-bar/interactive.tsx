3f65eee9e947c6e6423d581c5c0da7adf0cea486
Lars Eirik Korsgaard Hansen <71271458+larseirikhansen@users.noreply.github.com>
2024-04-29 10:53:02 +0200

    Progressbar demos (#2885)

---
+++ aksel.nav.no/website/pages/eksempler/progress-bar/interactive.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { BodyShort, Button, ProgressBar } from "@navikt/ds-react";
+import { withDsExample } from "@/web/examples/withDsExample";
+
+const starWarsQuotes = [
+  "May the force be with you",
+  "I find your lack of faith disturbing",
+  "It's a trap",
+  "Do or do not, there is no try",
+  "I am your father",
+  "The force will be with you, always",
+  "I've got a bad feeling about this",
+  "I love you.",
+  "I know.",
+  "I am a Jedi, like my father before me",
+  "Never tell me the odds",
+  "Search your feelings, you know it to be true",
+];
+
+const Example = () => {
+  const [value, setValue] = React.useState(3);
+  return (
+    <div>
+      <div>
+        <ProgressBar value={value} valueMax={12} aria-label="interactive" />
+
+        <BodyShort style={{ margin: "3rem 0", textAlign: "center" }}>
+          {starWarsQuotes[value]}
+        </BodyShort>
+      </div>
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <Button
+          onClick={() =>
+            setValue((oldValue) => (oldValue === 0 ? 0 : oldValue - 1))
+          }
+        >
+          Forrige sitat
+        </Button>
+        <Button
+          onClick={() =>
+            setValue((oldValue) => (oldValue === 12 ? 12 : oldValue + 1))
+          }
+        >
+          Neste sitat
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+// EXAMPLES DO NOT INCLUDE CONTENT BELOW THIS LINE
+export default withDsExample(Example);
+
+/* Storybook story */
+export const Demo = {
+  render: Example,
+};
+
+export const args = {
+  index: 0,
+};
---
+++ aksel.nav.no/website/pages/eksempler/progress-bar/sizes.tsx
@@ -0,0 +1,46 @@
+import { ProgressBar, VStack } from "@navikt/ds-react";
+import { withDsExample } from "@/web/examples/withDsExample";
+
+const Example = () => {
+  return (
+    <VStack gap="4">
+      <p id="progress-bar-label-small">Fremdrift i søknaden (liten versjon)</p>
+      <ProgressBar
+        value={1}
+        valueMax={12}
+        size="small"
+        aria-labelledby="progress-bar-label-small"
+      />
+
+      <p id="progress-bar-label-medium">
+        Fremdrift i søknaden (medium versjon)
+      </p>
+      <ProgressBar
+        value={6}
+        valueMax={12}
+        size="medium"
+        aria-labelledby="progress-bar-label-medium"
+      />
+
+      <p id="progress-bar-label-large">Fremdrift i søknaden (stor versjon)</p>
+      <ProgressBar
+        value={11}
+        valueMax={12}
+        size="large"
+        aria-labelledby="progress-bar-label-large"
+      />
+    </VStack>
+  );
+};
+
+// EXAMPLES DO NOT INCLUDE CONTENT BELOW THIS LINE
+export default withDsExample(Example);
+
+/* Storybook story */
+export const Demo = {
+  render: Example,
+};
+
+export const args = {
+  index: 2,
+};
