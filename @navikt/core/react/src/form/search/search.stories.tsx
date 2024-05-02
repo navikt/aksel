aa82657d3613ecd28a9929fc9b15bd15eec87a94
Julian Nymark <julian.nymark@nav.no>
2024-05-03 01:04:42 +0200

    :construction: WIP on some dropdown prop API

--- @navikt/core/react/src/form/search/search.stories.tsx
+++ @navikt/core/react/src/form/search/search.stories.tsx
@@ -75,6 +75,42 @@ export const Small = () => (
   </div>
 );
 
+export const Dropdown = (props) => {
+  const [state, setState] = useState("");
+  return (
+    <div className="colgap">
+      <Search
+        value={props.controlled ? state : undefined}
+        onChange={props.controlled ? setState : undefined}
+        label="Søk"
+        size={props.size}
+        clearButton={props.clearButton}
+        variant={props.variant}
+        hideLabel={props.hideLabel}
+        error={props.error}
+        onSearchClick={console.log}
+        dropdown={(searchTerm) => {
+          if (!searchTerm) return [];
+          return [
+            "apple",
+            "banana",
+            "break fluid",
+            "bandana",
+            "banjo",
+            "grape",
+            "melon",
+            "pineapple",
+            "juice",
+            "cat",
+            "dog",
+            "xylophone",
+          ].filter((term) => term.startsWith(searchTerm));
+        }}
+      />
+    </div>
+  );
+};
+
 export const Variants = () => (
   <div className="rowgap">
     <div className="colgap">
