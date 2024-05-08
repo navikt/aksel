9406f4a77a4f7935e3c048549ec150320e9b2c48
Ken <ken.aleksander@gmail.com>
2024-05-08 10:02:20 +0200

    refactor: Truncate title

--- aksel.nav.no/website/components/website-modules/search/parts/Hit.tsx
+++ aksel.nav.no/website/components/website-modules/search/parts/Hit.tsx
@@ -44,7 +44,7 @@ export const Hit = forwardRef<
             href={href}
             onClick={() => logSuccess(index, `/${hit.item.slug}`)}
             className={cl(
-              "group scroll-my-32 text-xl font-semibold underline hover:decoration-[3px] focus:outline-none",
+              "group scroll-my-32 truncate text-xl font-semibold underline hover:decoration-[3px] focus:outline-none",
               "after:absolute after:inset-0 after:rounded-lg after:ring-inset focus-visible:after:ring-[3px] focus-visible:after:ring-border-focus",
             )}
           >
