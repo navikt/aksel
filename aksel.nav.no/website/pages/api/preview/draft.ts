57a6a18dbe7cc495303858be2e4239e6b7a6308a
Ken <26967723+KenAJoh@users.noreply.github.com>
2024-05-15 09:37:13 +0200

    feat: API-routes for entering draftmode in nextjs (#2922)

---
+++ aksel.nav.no/website/pages/api/preview/draft.ts
@@ -0,0 +1,44 @@
+import { validatePreviewUrl } from "@sanity/preview-url-secret";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { createClient } from "next-sanity";
+import {
+  SANITY_API_VERSION,
+  SANITY_DATASET,
+  SANITY_PROJECT_ID,
+} from "@/sanity/config";
+
+const token = process.env.SANITY_PREVIEW_TOKEN;
+
+if (!token) {
+  throw new Error(
+    "A secret is provided but there is no `SANITY_PREVIEW_TOKEN` environment variable setup.",
+  );
+}
+
+const client = createClient({
+  projectId: SANITY_PROJECT_ID,
+  dataset: SANITY_DATASET,
+  apiVersion: SANITY_API_VERSION,
+  useCdn: false,
+  token,
+});
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse<string | void>,
+) {
+  if (!req.url) {
+    throw new Error("Missing url");
+  }
+  const { isValid, redirectTo = "/" } = await validatePreviewUrl(
+    client,
+    req.url,
+  );
+  if (!isValid) {
+    return res.status(401).send("Invalid secret");
+  }
+  // Enable Draft Mode by setting the cookies
+  res.setDraftMode({ enable: true });
+  res.writeHead(307, { Location: redirectTo });
+  res.end();
+}
