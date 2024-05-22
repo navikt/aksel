70dc5f18d11f3869b4c8fa6f6b13d18db43377b7
Ken <26967723+KenAJoh@users.noreply.github.com>
2024-05-22 14:57:32 +0200

    refactor: Optimized omit (#2936)

--- @navikt/core/react/src/util/omit.ts
+++ @navikt/core/react/src/util/omit.ts
@@ -2,13 +2,9 @@ export function omit<T extends object, K extends keyof T>(
   obj: T,
   props: K[],
 ): Omit<T, K> {
-  return Object.entries(obj)
-    .filter(([key]) => !props.includes(key as K))
-    .reduce(
-      (prevObj, [key, value]) => ({
-        ...prevObj,
-        [key]: value,
-      }),
-      {} as Omit<T, K>,
-    );
+  const filteredEntries = Object.entries(obj).filter(
+    ([key]) => !props.includes(key as K),
+  );
+
+  return Object.fromEntries(filteredEntries) as Omit<T, K>;
 }
