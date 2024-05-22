e38f288fcde0c93767f501f441514a27666bcc8e
Halvor Haugan <halvor.haugan@nav.no>
2024-05-22 10:05:55 +0200

    Include templates in ds storybook

--- aksel.nav.no/website/pages/templates/soknad-introside-pass-av-barn/soknad-introside-pass-av-barn.tsx
+++ aksel.nav.no/website/pages/templates/soknad-introside-pass-av-barn/soknad-introside-pass-av-barn.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { ArrowRightIcon } from "@navikt/aksel-icons";
 import {
   Accordion,
@@ -323,6 +323,7 @@ export default Example;
 /* Storybook story */
 export const Demo = {
   render: Example,
+  parameters: { layout: "fullscreen" },
 };
 
 export const args = {
