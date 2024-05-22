2e233e6a3f0dd85606fcef9dddbd920a08f86430
Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>
2024-05-22 15:48:43 +0200

    :recycle: Refactor form intro template (#2946)

--- aksel.nav.no/website/pages/templates/soknad-introside-foreldrepenger/soknad-introside-foreldrepenger.tsx
+++ aksel.nav.no/website/pages/templates/soknad-introside-foreldrepenger/soknad-introside-foreldrepenger.tsx
@@ -25,8 +25,8 @@ function Example() {
   return (
     <Page footer={<Footer />}>
       <Header />
-      <VStack as="main" gap="8">
-        <Page.Block width="text" gutters>
+      <Page.Block width="text" gutters>
+        <VStack as="main" gap="8">
           <Bleed
             marginInline={{ lg: "24" }}
             data-aksel-template="form-intropage-v1"
@@ -48,118 +48,112 @@ function Example() {
               </VStack>
             </Stack>
           </Bleed>
-        </Page.Block>
-        <Page.Block width="text" gutters>
-          <VStack gap="8">
-            <GuidePanel poster>
-              <Heading level="2" size="medium" spacing>
-                Hei, Darth Vader!
-              </Heading>
-              <BodyLong spacing>
-                Jeg er her for å veilede deg gjennom søknaden. Svarene dine
-                lagres i underveis, slik at du trygt kan gå tilbake og endre
-                dem.
-              </BodyLong>
-              <BodyLong spacing>
-                Foreldrepenger skal erstatte inntekten din når du skal være
-                hjemme med barnet i forbindelse med fødsel eller adopsjon.
-              </BodyLong>
-              <BodyLong>
-                <Link href="#">
-                  Les mer om foreldrepenger, hvem som kan søke og hva du kan få
-                  på nav.no.
-                </Link>
-              </BodyLong>
-            </GuidePanel>
-            <RadioGroup legend="Hvilket barn gjelder søknaden din?">
-              <Radio value="luke">Luke født 19 BBY</Radio>
-              <Radio value="leia">Leia født 19 BBY</Radio>
-              <Radio value="other" description="Det vil opprettes en ny sak">
-                Et annet barn
-              </Radio>
-            </RadioGroup>
-            <div>
-              <Heading level="2" size="large" spacing>
-                Før du søker
-              </Heading>
-              <BodyLong>
-                Vi lagrer søknaden din i 24 timer. Hvis du ikke fortsetter innen
-                den tid, blir søknaden slettet.
-              </BodyLong>
-            </div>
-            <div>
-              <Accordion>
-                <Accordion.Item>
-                  <Accordion.Header>
-                    Informasjon vi henter om deg
-                  </Accordion.Header>
-                  <Accordion.Content>
-                    <BodyLong>
-                      Her skal det så informasjon om hvor vi vil hente
-                      opplysninger om søkeren og hva slags opplysninger vi
-                      henter.
-                    </BodyLong>
-                  </Accordion.Content>
-                </Accordion.Item>
-                <Accordion.Item>
-                  <Accordion.Header>
-                    Hvordan vi behandler personopplysninger
-                  </Accordion.Header>
-                  <Accordion.Content>
-                    <BodyLong>
-                      Her skal det stå informasjon om hvordan vi behandler
-                      personopplysningene til søkeren.
-                    </BodyLong>
-                  </Accordion.Content>
-                </Accordion.Item>
-                <Accordion.Item>
-                  <Accordion.Header>Automatisk saksbehandling</Accordion.Header>
-                  <Accordion.Content>
-                    <BodyLong>
-                      Her skal det stå informasjon om hva automatisk behandling
-                      er, hva det betyr for søkeren og informasjon om søkerens
-                      rettigheter ved automatisk avslag.
-                    </BodyLong>
-                  </Accordion.Content>
-                </Accordion.Item>
-                <Accordion.Item>
-                  <Accordion.Header>Vi lagrer svar underveis</Accordion.Header>
-                  <Accordion.Content>
-                    <BodyLong>
-                      Her skal det stå informasjon om hvordan denne søknaden
-                      mellomlagrer informasjonen til søkeren og hvor lenge
-                      informasjonen lagres. Vi skal informere om mellomlagring
-                      ved både automatisk lagring og ved samtykke til lagring
-                      med lagre-knapp.
-                    </BodyLong>
-                  </Accordion.Content>
-                </Accordion.Item>
-              </Accordion>
-            </div>
-            <div>
-              <BodyLong>
-                Det er viktig at du gir oss riktige opplysninger slik at vi kan
-                behandle saken din.{" "}
-                <Link href="#gi-riktige-opplysninger">
-                  Les mer om viktigheten av å gi riktige opplysninger.
-                </Link>
-              </BodyLong>
-              <Box paddingBlock="4 8">
-                <Checkbox>
-                  Jeg vil svare så godt jeg kan på spørsmålene i søknaden.
-                </Checkbox>
-              </Box>
-              <Button
-                variant="primary"
-                icon={<ArrowRightIcon aria-hidden />}
-                iconPosition="right"
-              >
-                Start søknad
-              </Button>
-            </div>
-          </VStack>
-        </Page.Block>
-      </VStack>
+          <GuidePanel poster>
+            <Heading level="2" size="medium" spacing>
+              Hei, Darth Vader!
+            </Heading>
+            <BodyLong spacing>
+              Jeg er her for å veilede deg gjennom søknaden. Svarene dine lagres
+              i underveis, slik at du trygt kan gå tilbake og endre dem.
+            </BodyLong>
+            <BodyLong spacing>
+              Foreldrepenger skal erstatte inntekten din når du skal være hjemme
+              med barnet i forbindelse med fødsel eller adopsjon.
+            </BodyLong>
+            <BodyLong>
+              <Link href="#">
+                Les mer om foreldrepenger, hvem som kan søke og hva du kan få på
+                nav.no.
+              </Link>
+            </BodyLong>
+          </GuidePanel>
+          <RadioGroup legend="Hvilket barn gjelder søknaden din?">
+            <Radio value="luke">Luke født 19 BBY</Radio>
+            <Radio value="leia">Leia født 19 BBY</Radio>
+            <Radio value="other" description="Det vil opprettes en ny sak">
+              Et annet barn
+            </Radio>
+          </RadioGroup>
+          <div>
+            <Heading level="2" size="large" spacing>
+              Før du søker
+            </Heading>
+            <BodyLong>
+              Vi lagrer søknaden din i 24 timer. Hvis du ikke fortsetter innen
+              den tid, blir søknaden slettet.
+            </BodyLong>
+          </div>
+          <div>
+            <Accordion>
+              <Accordion.Item>
+                <Accordion.Header>
+                  Informasjon vi henter om deg
+                </Accordion.Header>
+                <Accordion.Content>
+                  <BodyLong>
+                    Her skal det så informasjon om hvor vi vil hente
+                    opplysninger om søkeren og hva slags opplysninger vi henter.
+                  </BodyLong>
+                </Accordion.Content>
+              </Accordion.Item>
+              <Accordion.Item>
+                <Accordion.Header>
+                  Hvordan vi behandler personopplysninger
+                </Accordion.Header>
+                <Accordion.Content>
+                  <BodyLong>
+                    Her skal det stå informasjon om hvordan vi behandler
+                    personopplysningene til søkeren.
+                  </BodyLong>
+                </Accordion.Content>
+              </Accordion.Item>
+              <Accordion.Item>
+                <Accordion.Header>Automatisk saksbehandling</Accordion.Header>
+                <Accordion.Content>
+                  <BodyLong>
+                    Her skal det stå informasjon om hva automatisk behandling
+                    er, hva det betyr for søkeren og informasjon om søkerens
+                    rettigheter ved automatisk avslag.
+                  </BodyLong>
+                </Accordion.Content>
+              </Accordion.Item>
+              <Accordion.Item>
+                <Accordion.Header>Vi lagrer svar underveis</Accordion.Header>
+                <Accordion.Content>
+                  <BodyLong>
+                    Her skal det stå informasjon om hvordan denne søknaden
+                    mellomlagrer informasjonen til søkeren og hvor lenge
+                    informasjonen lagres. Vi skal informere om mellomlagring ved
+                    både automatisk lagring og ved samtykke til lagring med
+                    lagre-knapp.
+                  </BodyLong>
+                </Accordion.Content>
+              </Accordion.Item>
+            </Accordion>
+          </div>
+          <div>
+            <BodyLong>
+              Det er viktig at du gir oss riktige opplysninger slik at vi kan
+              behandle saken din.{" "}
+              <Link href="#gi-riktige-opplysninger">
+                Les mer om viktigheten av å gi riktige opplysninger.
+              </Link>
+            </BodyLong>
+            <Box paddingBlock="4 8">
+              <Checkbox>
+                Jeg vil svare så godt jeg kan på spørsmålene i søknaden.
+              </Checkbox>
+            </Box>
+            <Button
+              variant="primary"
+              icon={<ArrowRightIcon aria-hidden />}
+              iconPosition="right"
+            >
+              Start søknad
+            </Button>
+          </div>
+        </VStack>
+      </Page.Block>
       <Env />
     </Page>
   );
