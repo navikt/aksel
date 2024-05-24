f48178de22ff4be8c9e9e4586c4a9be7d5d95428
Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>
2024-05-24 11:12:51 +0200

    Adjust docs for Chat and some primitives (#2948)

--- aksel.nav.no/website/pages/eksempler/chat/toptextPosition.tsx
+++ aksel.nav.no/website/pages/eksempler/chat/toptextPosition.tsx
@@ -5,8 +5,9 @@ const Example = () => {
   return (
     <VStack gap="10">
       <Chat avatar="EVA" name="EVA" timestamp="01.01.21 14:00">
-        <Chat.Bubble>Hei! Mitt navn er Eva.</Chat.Bubble>
-        <Chat.Bubble>Hva kan jeg hjelpe deg med?</Chat.Bubble>
+        <Chat.Bubble toptextPosition="right">
+          Hei! Mitt navn er Eva. Hva kan jeg hjelpe deg med?
+        </Chat.Bubble>
       </Chat>
       <Chat
         avatar="ON"
