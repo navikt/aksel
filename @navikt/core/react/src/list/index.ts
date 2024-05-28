11a2ca1085681f81fe7ba92f9c7b1533b4839755
Halvor Haugan <halvor.haugan@nav.no>
2024-05-28 13:59:36 +0200

    List: Adjusted examples and JSDOC

--- @navikt/core/react/src/list/index.ts
+++ @navikt/core/react/src/list/index.ts
@@ -1,4 +1,4 @@
 "use client";
 export { default as List } from "./List";
-export type { ListProps } from "./types";
-export { default as ListItem, type ListItemProps } from "./ListItem";
+export type { ListProps, ListItemProps } from "./types";
+export { default as ListItem } from "./ListItem";
--- aksel.nav.no/website/pages/eksempler/list/icons.tsx
+++ aksel.nav.no/website/pages/eksempler/list/icons.tsx
@@ -10,23 +10,20 @@ import { withDsExample } from "@/web/examples/withDsExample";
 const Example = () => {
   return (
     <List>
-      <List.Item
-        title="Kritisk"
-        icon={<ExclamationmarkTriangleIcon aria-hidden />}
-      >
-        dette er problemer som må løses så fort som mulig. De hindrer bruk av
-        siden og kan i verste fall være farlig for bruker.
+      <List.Item icon={<ExclamationmarkTriangleIcon aria-hidden />}>
+        Kritisk: Dette er problemer som må løses så fort som mulig. De hindrer
+        bruk av siden og kan i verste fall være farlig for bruker.
       </List.Item>
-      <List.Item title="Høy" icon={<ChevronUpDoubleCircleIcon aria-hidden />}>
-        brukere er forhindret i å forstå innholdet eller utføre kritiske
+      <List.Item icon={<ChevronUpDoubleCircleIcon aria-hidden />}>
+        Høy: Brukere er forhindret i å forstå innholdet eller utføre kritiske
         oppgaver, og det finnes ingen alternative løsninger.
       </List.Item>
-      <List.Item title="Medium" icon={<ChevronUpCircleIcon aria-hidden />}>
-        vanskelige, tidkrevende eller frustrerende for brukere å få tilgang til
-        innhold eller funksjonalitet.
+      <List.Item icon={<ChevronUpCircleIcon aria-hidden />}>
+        Medium: Vanskelige, tidkrevende eller frustrerende for brukere å få
+        tilgang til innhold eller funksjonalitet.
       </List.Item>
-      <List.Item title="Lav" icon={<ChevronDownCircleIcon aria-hidden />}>
-        brukeren få tilgang til alt innhold og funksjonalitet, men
+      <List.Item icon={<ChevronDownCircleIcon aria-hidden />}>
+        Lav: Brukeren få tilgang til alt innhold og funksjonalitet, men
         brukeropplevelsen er dårlig.
       </List.Item>
     </List>
@@ -42,6 +39,6 @@ export const Demo = {
 };
 
 export const args = {
-  index: 3,
-  desc: "Husk å sette aria-hidden på ikonet.",
+  index: 5,
+  desc: "Husk å sette aria-hidden på dekorative ikoner.",
 };
