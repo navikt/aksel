6f220de9c8f3ed097faf05871b6f9a498cb94f4d
Ken <26967723+KenAJoh@users.noreply.github.com>
2024-06-07 11:50:27 +0200

    refactor: Fixed noAssignInExpressions lint-errors (#2983)
    * refactor: Fixed noAssignInExpressions lint-errors
    
    * Update @navikt/core/react/src/util/i18n/get.ts
    
    Co-authored-by: Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>
    
    * refactor: added dialog-polyfill to biome ignorelist
    
    ---------
    
    Co-authored-by: Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>

--- @navikt/core/react/src/modal/dialog-polyfill.ts
+++ @navikt/core/react/src/modal/dialog-polyfill.ts
@@ -631,7 +631,6 @@ dialogPolyfill.DialogManager = function () {
     this.mo_ = new MutationObserver(function (records) {
       var removed = [];
       records.forEach(function (rec) {
-        // biome-ignore lint/suspicious/noImplicitAnyLet: Reduntant to type c in this scenario
         for (var i = 0, c; (c = rec.removedNodes[i]); ++i) {
           if (!(c instanceof Element)) {
             continue;
@@ -679,7 +678,6 @@ dialogPolyfill.DialogManager.prototype.unblockDocument = function () {
 dialogPolyfill.DialogManager.prototype.updateStacking = function () {
   var zIndex = this.zIndexHigh_;
 
-  // biome-ignore lint/suspicious/noImplicitAnyLet: Reduntant to type dpi in this scenario
   for (var i = 0, dpi; (dpi = this.pendingDialogStack[i]); ++i) {
     dpi.updateZIndex(--zIndex, --zIndex);
     if (i === 0) {
@@ -705,7 +703,6 @@ dialogPolyfill.DialogManager.prototype.containedByTopDialog_ = function (
   candidate,
 ) {
   while ((candidate = findNearestDialog(candidate))) {
-    // biome-ignore lint/suspicious/noImplicitAnyLet: Reduntant to type dpi in this scenario
     for (var i = 0, dpi; (dpi = this.pendingDialogStack[i]); ++i) {
       if (dpi.dialog === candidate) {
         return i === 0; // only valid if top-most
