d114d336ad406cc6f85594337fcd0dda09edf38b
Lars Hansen <lars.eirik.korsgaard.hansen@nav.no>
2024-06-10 11:03:18 +0200

    Guidepanel

--- @navikt/core/react/src/guide-panel/guidepanel.stories.tsx
+++ @navikt/core/react/src/guide-panel/guidepanel.stories.tsx
@@ -1,9 +1,11 @@
-import { Meta } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import React from "react";
 import { InformationIcon } from "@navikt/aksel-icons";
 import { VStack } from "../layout/stack";
 import { BodyLong } from "../typography";
 import GuidePanel from "./GuidePanel";
+
+type Story = StoryObj<typeof GuidePanel>;
 
 export default {
   title: "ds-react/GuidePanel",
@@ -15,29 +17,30 @@ adipisicing proident. Tempor ipsum ea cupidatat qui esse do veniam
 cupidatat. Excepteur irure reprehenderit esse tempor nisi duis qui ea
 enim id.`;
 
-export const Default = {
-  render: (props: { poster: boolean; colorOverride: boolean }) => {
-    const style = props.colorOverride
-      ? {
-          "--ac-guide-panel-illustration-bg": "var(--a-purple-200)",
-          "--ac-guide-panel-border": "var(--a-purple-400)",
-        }
-      : {};
+export const Default: StoryObj<{ poster?: boolean; colorOverride?: boolean }> =
+  {
+    render: (props) => {
+      const style = props.colorOverride
+        ? {
+            "--ac-guide-panel-illustration-bg": "var(--a-purple-200)",
+            "--ac-guide-panel-border": "var(--a-purple-400)",
+          }
+        : {};
 
-    return (
-      <GuidePanel style={style} poster={props?.poster}>
-        {panelText}
-      </GuidePanel>
-    );
-  },
+      return (
+        <GuidePanel style={style} poster={props?.poster}>
+          {panelText}
+        </GuidePanel>
+      );
+    },
 
-  args: {
-    poster: false,
-    colorOverride: false,
-  },
-};
+    args: {
+      poster: false,
+      colorOverride: false,
+    },
+  };
 
-export const PosterVariants = {
+export const PosterVariants: Story = {
   render: () => (
     <VStack gap="6" align="start">
       <GuidePanel>
@@ -58,33 +61,69 @@ export const PosterVariants = {
   },
 };
 
-export const ColorOverride = () => (
-  <GuidePanel
-    style={{
-      "--ac-guide-panel-illustration-bg": "var(--a-purple-200)",
-      "--ac-guide-panel-border": "var(--a-purple-400)",
-    }}
-  >
-    {panelText}
-  </GuidePanel>
-);
+export const ColorOverride: Story = {
+  render: () => (
+    <GuidePanel
+      style={{
+        "--ac-guide-panel-illustration-bg": "var(--a-purple-200)",
+        "--ac-guide-panel-border": "var(--a-purple-400)",
+      }}
+    >
+      {panelText}
+    </GuidePanel>
+  ),
+};
 
-export const Content = () => (
-  <GuidePanel>
-    <BodyLong spacing>
-      Duis et ex ad magna nostrud ut officia nulla cillum commodo sint irure
-      elit nulla. Ad proident nulla ex sunt exercitation sunt Lorem non laboris
-      ea ex cillum nulla consequat. Enim pariatur eiusmod quis est fugiat
-      officia nostrud dolore occaecat nisi.
-    </BodyLong>
-    <BodyLong>
-      Do esse magna nulla amet excepteur. Tempor laboris ipsum magna velit
-      dolore nulla id ex mollit. Deserunt ut esse laboris pariatur tempor
-      laborum veniam enim. Nisi deserunt officia minim enim.
-    </BodyLong>
-  </GuidePanel>
-);
+export const Content: Story = {
+  render: () => (
+    <GuidePanel>
+      <BodyLong spacing>
+        Duis et ex ad magna nostrud ut officia nulla cillum commodo sint irure
+        elit nulla. Ad proident nulla ex sunt exercitation sunt Lorem non
+        laboris ea ex cillum nulla consequat. Enim pariatur eiusmod quis est
+        fugiat officia nostrud dolore occaecat nisi.
+      </BodyLong>
+      <BodyLong>
+        Do esse magna nulla amet excepteur. Tempor laboris ipsum magna velit
+        dolore nulla id ex mollit. Deserunt ut esse laboris pariatur tempor
+        laborum veniam enim. Nisi deserunt officia minim enim.
+      </BodyLong>
+    </GuidePanel>
+  ),
+};
 
-export const CustomIllustration = () => (
-  <GuidePanel illustration={<InformationIcon />}>{panelText}</GuidePanel>
-);
+export const CustomIllustration: Story = {
+  render: () => (
+    <GuidePanel illustration={<InformationIcon />}>{panelText}</GuidePanel>
+  ),
+};
+
+export const Chromatic: Story = {
+  render: (props, context) => (
+    <div>
+      <div>
+        <h2>Default</h2>
+        {Default.render?.(Default.args || {}, context)}
+      </div>
+      <div>
+        <h2>PosterVariants</h2>
+        {PosterVariants.render?.(props, context)}
+      </div>
+      <div>
+        <h2>ColorOverride</h2>
+        {ColorOverride.render?.(props, context)}
+      </div>
+      <div>
+        <h2>Content</h2>
+        {Content.render?.(props, context)}
+      </div>
+      <div>
+        <h2>CustomIllustration</h2>
+        {CustomIllustration.render?.(props, context)}
+      </div>
+    </div>
+  ),
+  parameters: {
+    chromatic: { disable: false },
+  },
+};
