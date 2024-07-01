856093ce659ecc48e879244918c0b6c69afe6e38
Ken <ken.aleksander@gmail.com>
2024-07-01 15:41:08 +0200

    feat: MenuTrigger & MenuPortal

--- @navikt/core/react/src/overlays/dropdown-menu/DropdownMenu.tsx
+++ @navikt/core/react/src/overlays/dropdown-menu/DropdownMenu.tsx
@@ -1,7 +1,11 @@
-import React, { useRef } from "react";
+import React, { forwardRef, useRef } from "react";
+import { Slot } from "../../slot/Slot";
 import { useId } from "../../util";
+import { composeEventHandlers } from "../../util/composeEventHandlers";
 import { createContext } from "../../util/create-context";
+import { useMergeRefs } from "../../util/hooks";
 import { useControllableState } from "../../util/hooks/useControllableState";
+import { AsChildProps } from "../../util/types";
 import { Menu } from "../floating-menu/Menu";
 
 /* -------------------------------------------------------------------------- */
@@ -16,9 +20,10 @@ type DropdownMenuContextValue = {
   onOpenToggle: () => void;
 };
 
-const [DropdownMenuProvider] = createContext<DropdownMenuContextValue>({
-  name: "DropdownMenuContext",
-});
+const [DropdownMenuProvider, useDropdownMenuContext] =
+  createContext<DropdownMenuContextValue>({
+    name: "DropdownMenuContext",
+  });
 
 interface DropdownMenuProps {
   children?: React.ReactNode;
@@ -55,3 +60,87 @@ export const DropdownMenu = ({
     </DropdownMenuProvider>
   );
 };
+
+/* -------------------------------------------------------------------------- */
+/*                             DropdownMenuTrigger                            */
+/* -------------------------------------------------------------------------- */
+type DropdownMenuTriggerProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+  AsChildProps;
+
+export const DropdownMenuTrigger = forwardRef<
+  HTMLButtonElement,
+  DropdownMenuTriggerProps
+>(
+  (
+    {
+      disabled = false,
+      asChild,
+      onPointerDown,
+      onKeyDown,
+      ...rest
+    }: DropdownMenuTriggerProps,
+    forwardedRef,
+  ) => {
+    const context = useDropdownMenuContext();
+
+    const Comp = asChild ? Slot : "button";
+
+    const mergedRefs = useMergeRefs(forwardedRef, context.triggerRef);
+
+    return (
+      <Menu.Anchor asChild>
+        <Comp
+          type="button"
+          id={context.triggerId}
+          aria-haspopup="menu"
+          aria-expanded={context.open}
+          aria-controls={context.open ? context.contentId : undefined}
+          data-state={context.open ? "open" : "closed"}
+          data-disabled={disabled ? "" : undefined}
+          disabled={disabled}
+          {...rest}
+          ref={mergedRefs}
+          onPointerDown={composeEventHandlers(onPointerDown, (event) => {
+            // only call handler if it's the left button (mousedown gets triggered by all mouse buttons)
+            // but not when the control key is pressed (avoiding MacOS right click)
+            if (!disabled && event.button === 0 && event.ctrlKey === false) {
+              context.onOpenToggle();
+              // prevent trigger focusing when opening
+              // this allows the content to be given focus without competition
+              if (!context.open) event.preventDefault();
+            }
+          })}
+          onKeyDown={composeEventHandlers(onKeyDown, (event) => {
+            if (disabled) return;
+            if (["Enter", " "].includes(event.key)) {
+              context.onOpenToggle();
+            }
+            if (event.key === "ArrowDown") {
+              context.onOpenChange(true);
+            }
+            // prevent keydown from scrolling window / first focused item to execute
+            if (["Enter", " ", "ArrowDown"].includes(event.key)) {
+              event.preventDefault();
+            }
+          })}
+        />
+      </Menu.Anchor>
+    );
+  },
+);
+
+/* -------------------------------------------------------------------------- */
+/*                             DropdownMenuPortal                             */
+/* -------------------------------------------------------------------------- */
+type PortalProps = React.ComponentPropsWithoutRef<typeof Menu.Portal>;
+type MenuPortalElement = React.ElementRef<typeof Menu.Portal>;
+
+type MenuPortalProps = PortalProps & {
+  children: React.ReactElement;
+};
+
+export const MenuPortal = forwardRef<MenuPortalElement, MenuPortalProps>(
+  (props: MenuPortalProps, ref) => {
+    return <Menu.Portal ref={ref} {...props} />;
+  },
+);
