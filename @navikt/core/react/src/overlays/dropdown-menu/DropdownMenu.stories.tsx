d6699181405273e0239f1b46962e9a26eefe15d7
Ken <ken.aleksander@gmail.com>
2024-07-05 10:03:27 +0200

    testing: Un-nested variants

--- @navikt/core/react/src/overlays/dropdown-menu/DropdownMenu.stories.tsx
+++ @navikt/core/react/src/overlays/dropdown-menu/DropdownMenu.stories.tsx
@@ -1,6 +1,5 @@
 import { Meta, StoryObj } from "@storybook/react";
 import React, { useState } from "react";
-import { Button } from "../../button";
 import { VStack } from "../../layout/stack";
 import { DropdownMenu } from "./DropdownMenu";
 
@@ -185,37 +184,37 @@ export const RadioGroups: Story = {
   decorators: [DemoDecorator],
 };
 
-export const Demo: Story = {
+export const SeparatorWithGroupedItems: Story = {
   render: () => {
     return (
-      <DropdownMenu open>
+      <DropdownMenu>
         <DropdownMenu.Trigger>
-          <Button>Dropdown</Button>
+          <button>Open dropdown</button>
         </DropdownMenu.Trigger>
         <DropdownMenu.Content>
-          <DropdownMenu.Group label="Lenker">
+          <DropdownMenu.Group label="Group 1">
             <DropdownMenu.Item onSelect={() => console.log("Item 1 clicked")}>
               Item 1
             </DropdownMenu.Item>
-            <DropdownMenu.Item
-              onSelect={() => console.log("Item 2 clicked")}
-              shortcut="⌘+S"
-            >
+            <DropdownMenu.Item onSelect={() => console.log("Item 2 clicked")}>
               Item 2
+            </DropdownMenu.Item>
+            <DropdownMenu.Item onSelect={() => console.log("Item 3 clicked")}>
+              Item 3
             </DropdownMenu.Item>
           </DropdownMenu.Group>
           <DropdownMenu.Separator />
-          <DropdownMenu.CheckboxItem checked>
-            Checkbox
-          </DropdownMenu.CheckboxItem>
-          <DropdownMenu.CheckboxItem shortcut="⌘+S">
-            Checkbox
-          </DropdownMenu.CheckboxItem>
-          <DropdownMenu.Separator />
-          <DropdownMenu.RadioGroup value="2">
-            <DropdownMenu.RadioItem value="1">Radio 1</DropdownMenu.RadioItem>
-            <DropdownMenu.RadioItem value="2">Radio 2</DropdownMenu.RadioItem>
-          </DropdownMenu.RadioGroup>
+          <DropdownMenu.Group label="Group 2">
+            <DropdownMenu.Item onSelect={() => console.log("Item 4 clicked")}>
+              Item 4
+            </DropdownMenu.Item>
+            <DropdownMenu.Item onSelect={() => console.log("Item 5 clicked")}>
+              Item 5
+            </DropdownMenu.Item>
+            <DropdownMenu.Item onSelect={() => console.log("Item 6 clicked")}>
+              Item 6
+            </DropdownMenu.Item>
+          </DropdownMenu.Group>
         </DropdownMenu.Content>
       </DropdownMenu>
     );
@@ -223,105 +222,28 @@ export const Demo: Story = {
   decorators: [DemoDecorator],
 };
 
-export const MarkupDemo = () => {
-  return (
-    <div className="root">
-      <style>{`ul, li {list-style:none; margin:0;padding:0}
-      .root {
-      display:flex; gap: 2rem;}`}</style>
-      <section aria-label="List markup test">
-        <ul role="menu" aria-orientation="vertical" id="r5" tabIndex={0}>
-          <li>
-            <ul role="group" aria-labelledby="r6">
-              <li id="r6" aria-hidden="true">
-                Gruppe 1
-              </li>
-              <li role="presentation">
-                <a href="#123" role="menuitem" tabIndex={0}>
-                  item 1 i gruppe 1
-                </a>
-              </li>
-              <li role="presentation">
-                <a href="#123" role="menuitem" tabIndex={0}>
-                  item 2 i gruppe 1
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <ul role="group" aria-labelledby="r10">
-              <li id="r10" aria-hidden="true">
-                Gruppe 2
-              </li>
-              <li role="presentation">
-                <a href="#123" role="menuitem" tabIndex={0}>
-                  item 3 i gruppe 2
-                </a>
-              </li>
-              <li role="presentation">
-                <a href="#123" role="menuitem" tabIndex={0}>
-                  item 4 i gruppe 2
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </section>
-      <section aria-label="div-markup test">
-        <div role="menu" aria-orientation="vertical" id="r5" tabIndex={0}>
-          <div role="group" aria-labelledby="r6">
-            <div id="r6" aria-hidden="true">
-              Gruppe 1
-            </div>
-            <div role="menuitem" tabIndex={0}>
-              item 1 i gruppe 1
-            </div>
-            <div role="menuitem" tabIndex={0}>
-              item 2 i gruppe 1
-            </div>
-          </div>
-          <div role="group" aria-labelledby="r11">
-            <div id="r11" aria-hidden="true">
-              Gruppe 2
-            </div>
-            <div role="menuitem" tabIndex={0}>
-              item 3 i gruppe 2
-            </div>
-            <div role="menuitem" tabIndex={0}>
-              item 4 i gruppe 2
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
-  );
-};
-
-export const MarkupDemo2 = () => {
-  return (
-    <div className="root">
-      <style>{`ul, li {list-style:none; margin:0;padding:0}
-      .root {
-      display:flex; gap: 2rem;}`}</style>
-      <section aria-label="Link versus button test">
-        <div role="menu" aria-orientation="vertical" tabIndex={0}>
-          <a
-            href="#123"
-            role="menuitem"
-            tabIndex={0}
-            onClick={() => alert("klikket på lenke")}
-          >
-            Dette er en lenke med menuitem
-          </a>
-          <button
-            role="menuitem"
-            tabIndex={0}
-            onClick={() => alert("klikket på knapp")}
-          >
-            Dette er en knapp med menuitem
-          </button>
-        </div>
-      </section>
-    </div>
-  );
+export const SeparatorWithItems: Story = {
+  render: () => {
+    return (
+      <DropdownMenu>
+        <DropdownMenu.Trigger>
+          <button>Open dropdown</button>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Item onSelect={() => console.log("Item 1 clicked")}>
+            Item 1
+          </DropdownMenu.Item>
+          <DropdownMenu.Separator />
+          <DropdownMenu.Item onSelect={() => console.log("Item 2 clicked")}>
+            Item 2
+          </DropdownMenu.Item>
+          <DropdownMenu.Separator />
+          <DropdownMenu.Item onSelect={() => console.log("Item 3 clicked")}>
+            Item 3
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu>
+    );
+  },
+  decorators: [DemoDecorator],
 };
