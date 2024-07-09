975913c03c717635cb1ff5f4eb765b463d080aef
Ken <ken.aleksander@gmail.com>
2024-07-09 14:07:01 +0200

    feat: Added support for markdown-links in intro-list

--- aksel.nav.no/website/components/sanity-modules/intro-seksjon/IntroSeksjon.tsx
+++ aksel.nav.no/website/components/sanity-modules/intro-seksjon/IntroSeksjon.tsx
@@ -2,6 +2,7 @@ import ErrorBoundary from "@/error-boundary";
 import { SanityBlockContent } from "@/sanity-block";
 import { AkselGrunnleggendeDocT, AkselKomponentDocT } from "@/types";
 import { List, ListItem } from "@/web/List";
+import { markdownLink } from "./markdown-link";
 
 type IntroProps = {
   node: AkselKomponentDocT["intro"] | AkselGrunnleggendeDocT["intro"];
@@ -24,7 +25,7 @@ const Intro = ({ node, internal }: IntroProps) => {
           {internal && <ListItem>Bruk på interne flater</ListItem>}
           {node.brukes_til.map((x) => (
             <ListItem icon key={x}>
-              {x}
+              {markdownLink(x)}
             </ListItem>
           ))}
         </List>
@@ -32,7 +33,7 @@ const Intro = ({ node, internal }: IntroProps) => {
           <List title="Uegnet til:">
             {node.brukes_ikke_til.map((x) => (
               <ListItem icon key={x}>
-                {x}
+                {markdownLink(x)}
               </ListItem>
             ))}
           </List>
---
+++ aksel.nav.no/website/components/sanity-modules/intro-seksjon/markdown-link.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "@navikt/ds-react";
+
+function markdownLink(x: string) {
+  const regex = /\[([^\]]+)\]\((https?:\/\/[^\s)]+)\)/g;
+  let lastIndex = 0;
+  const elements: React.ReactNode[] = [];
+
+  x.replace(regex, (match, text, url, index) => {
+    if (index > lastIndex) {
+      elements.push(x.slice(lastIndex, index));
+    }
+    elements.push(
+      <Link href={url} key={index}>
+        {text}
+      </Link>,
+    );
+
+    lastIndex = index + match.length;
+    return match;
+  });
+
+  if (lastIndex < x.length) {
+    elements.push(x.slice(lastIndex));
+  }
+
+  return elements;
+}
+
+export { markdownLink };
