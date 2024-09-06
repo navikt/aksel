b23c83e0c24b043e3da00e22163ffe5322973597
Julian Nymark <julian.nymark@nav.no>
2024-09-06 17:20:04 +0200

    :construction: translations in Provider

--- @navikt/core/react/src/provider/Provider.tsx
+++ @navikt/core/react/src/provider/Provider.tsx
@@ -1,10 +1,16 @@
 import React, { createContext, useContext } from "react";
+import { TranslationDictionary } from "../../cjs/util/i18n/i18n.types";
 
 export interface ProviderContextType {
   /**
    * Global root-element to attach portals to (Tooltip)
    */
   rootElement?: HTMLElement;
+  /**
+   * Translation object (from language .json files)
+   * Merged with the default language translations object (officially provided translations).
+   */
+  translations?: TranslationDictionary | TranslationDictionary[];
 }
 
 export const ProviderContext = createContext<ProviderContextType | undefined>(
@@ -14,6 +20,7 @@ export const ProviderContext = createContext<ProviderContextType | undefined>(
 export interface ProviderProps {
   children?: React.ReactNode;
   rootElement?: HTMLElement;
+  translations?: TranslationDictionary | TranslationDictionary[];
 }
 
 export const useProvider = () => useContext(ProviderContext);
