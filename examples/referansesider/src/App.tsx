dd03bd87056e7fa4e90042b3894c8d8b809fd086
Julian Nymark <julian.nymark@nav.no>
2024-09-16 15:34:16 +0200

    :construction: [referansesider] add example for variant in styled-components

--- examples/referansesider/src/App.tsx
+++ examples/referansesider/src/App.tsx
@@ -3,17 +3,19 @@ import "@navikt/ds-tokens";
 import * as tokens from "@navikt/ds-tokens/dist/tokens";
 import "./App.css";
 
-const StyledButton = styled(variantProps)`
-  background-color: var(--a-surface-success);
-  color: ${tokens.APurple500};
-  border: solid 2px salmon;
+const StyledButton = styled.button<{ variant?: "purple" }>`
+  background-color: ${(props) =>
+    props.variant === "purple" ? tokens.APurple500 : tokens.AGrayalpha400};
+  color: ${(props) =>
+    props.variant === "purple" ? tokens.APurple800 : tokens.AWhite};
+  border: solid 2px ${tokens.ADeepblue900};
 `;
 
 function App() {
   return (
     <>
       <button>normal button</button>
-      <StyledButton className="my-best-class">styled button</StyledButton>
+      <StyledButton variant="purple">purple variant</StyledButton>
     </>
   );
 }
