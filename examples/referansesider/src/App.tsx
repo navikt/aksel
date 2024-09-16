3c6459305ee658729f332fd24285994db104738b
Julian Nymark <julian.nymark@nav.no>
2024-09-16 12:45:37 +0200

    :construction: [referansesider] add aksel dependencies

--- examples/referansesider/src/App.tsx
+++ examples/referansesider/src/App.tsx
@@ -1,33 +1,12 @@
-import { useState } from "react";
+import "@navikt/ds-css";
+import { Button } from "@navikt/ds-react";
 import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((_count) => _count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Dette er referanseside 1!</h1>
+      <Button variant="primary">Send inn søknad</Button>
     </>
   );
 }
