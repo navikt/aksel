12f6ae9134c1eee7a6cb157d9d63b64d143be94f
Julian Nymark <julian.nymark@nav.no>
2024-09-16 13:13:42 +0200

    :construction: [referansesider] use styled-components, try to import our tokens

--- examples/referansesider/src/App.tsx
+++ examples/referansesider/src/App.tsx
@@ -1,13 +1,23 @@
+import styled from "styled-components";
 import "@navikt/ds-css";
-import { Button } from "@navikt/ds-react";
+import { Button, VStack } from "@navikt/ds-react";
+import "@navikt/ds-tokens";
+// TODO: why does this not work?
+// import { ASurfaceSuccess } from "@navikt/ds-tokens";
 import "./App.css";
+
+const StyledButton = styled(Button)`
+  color: orange;
+  background-color: var(--a-surface-success);
+  border: solid 2px salmon;
+`;
 
 function App() {
   return (
-    <>
-      <h1>Dette er referanseside 1!</h1>
-      <Button variant="primary">Send inn søknad</Button>
-    </>
+    <VStack gap="10">
+      <Button>normal Aksel button! (unstyled)</Button>
+      <StyledButton variant="secondary">styled button</StyledButton>
+    </VStack>
   );
 }
 
