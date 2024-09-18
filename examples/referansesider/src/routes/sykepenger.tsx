03495f8343387a95c80fc45ece3924ff395fff11
Julian Nymark <julian.nymark@nav.no>
2024-09-18 10:40:42 +0200

    :lipstick: [referansesider] adjust sykepenger header

--- examples/referansesider/src/routes/sykepenger.tsx
+++ examples/referansesider/src/routes/sykepenger.tsx
@@ -7,11 +7,12 @@ import { Page } from "../components/Page";
 const Detail = styled.span`
   color: ${tokens.AGray500};
   font-size: 20px;
+  font-weight: 300;
   font-variant-caps: all-small-caps;
 `;
 
 const Header = styled.h1`
-  color: ${tokens.ARed600};
+  color: #99185e;
   font-size: 48px;
   font-weight: ${tokens.AFontWeightBold};
   margin-bottom: 1rem;
@@ -23,15 +24,19 @@ const InfoSummary = styled.span`
 
 const FancyHeader = () => {
   return (
-    <>
+    <div className="mt-16 relative">
+      <SykepengerIkon
+        className="-translate-x-32 translate-y-1"
+        colorStroke="stroke-[#99185e]"
+        colorFill="fill-[#99185e]"
+      />
       <Detail>PENGESTØTTE — FOR ARBEIDSGIVERE</Detail>
       <Header>Sykepenger</Header>
       <InfoSummary>
         Erstatter inntekten din når du ikke kan jobbe på grunn av sykdom eller
         skade.
       </InfoSummary>
-      <SykepengerIkon className="-translate-x-32 translate-y-1" />
-    </>
+    </div>
   );
 };
 
