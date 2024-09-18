8c27da3726aa78a956009296968af0104beae894
Ken <ken.aleksander@gmail.com>
2024-09-18 10:07:56 +0200

    refactor: removed unused types

--- @navikt/core/tokens/darkside/util.ts
+++ @navikt/core/tokens/darkside/util.ts
@@ -3,15 +3,6 @@ import _ from "lodash";
 export const colorThemeModes = ["light", "dark"] as const;
 export type ColorThemeMode = (typeof colorThemeModes)[number];
 
-export const TokenTypes = {
-  GLOBAL_COLOR: "global-color",
-  COLOR: "color",
-  GLOBAL_SPACING: "global-spacing",
-  GLOBAL_RADIUS: "global-radius",
-} as const;
-
-export type TokenType = (typeof TokenTypes)[keyof typeof TokenTypes];
-
 export const globalColorRoles = [
   "neutral",
   "accent",
@@ -50,11 +41,6 @@ export type GlobalColorScale = (typeof globalColorScales)[number];
 
 export type GlobalColorVariable = Record<
   GlobalColorScale,
-  { value: string; type: "global-color"; comment?: string; group: string }
->;
-
-export type GlobalColorVariableWith000 = Record<
-  GlobalColorScale | "000",
   { value: string; type: "global-color"; comment?: string; group: string }
 >;
 
