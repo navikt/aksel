104427d7ef79255108378c4f68b54f84f2b5c95b
Julian Nymark <julian.nymark@nav.no>
2024-09-18 13:57:10 +0200

    :art: [referansesider] more work on Sykepenger

--- examples/referansesider/src/routes/sykepenger.tsx
+++ examples/referansesider/src/routes/sykepenger.tsx
@@ -1,6 +1,8 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { ReactNode, createFileRoute } from "@tanstack/react-router";
 import styled from "styled-components";
+import { twMerge } from "tailwind-merge";
 import * as tokens from "@navikt/ds-tokens/dist/tokens";
+import ArrowDownRightIcon from "../assets/ArrowDownRightIcon";
 import SykepengerIkon from "../assets/SykepengerIkon";
 import { Page } from "../components/Page";
 
@@ -40,11 +42,69 @@ const FancyHeader = () => {
   );
 };
 
+const Link = styled.a`
+  color: ${tokens.ABlue600};
+  text-decoration: underline;
+  &:hover {
+    text-decoration: none;
+  }
+`;
+
+const PlainText = ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className: string;
+}) => {
+  return <p className={twMerge(className, `mb-9`)}>{children}</p>;
+};
+
+const PinkList = () => {
+  return (
+    <div>
+      <h2 id="heading-page-navigation-menu-:R8mm:">Innhold på siden</h2>
+      <ul aria-labelledby="heading-page-navigation-menu-:R8mm:">
+        <li>
+          <Link className="flex flex-row items-center" href="#hvem">
+            <ArrowDownRightIcon
+              colorStroke="stroke-[#0056b4]"
+              colorFill="fill-[#0056b4]"
+            />
+            <span>Hvem kan få?</span>
+          </Link>
+        </li>
+        <li>
+          <a href="#hva">
+            <span>Hva kan du få?</span>
+          </a>
+        </li>
+        <li>
+          <a href="#sok">
+            <span>Søke, ettersende eller klage</span>
+          </a>
+        </li>
+        <li>
+          <a href="#har">
+            <span>Når du har sykepenger</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
 const Component = () => {
   return (
     <>
       <Page>
         <FancyHeader />
+        <PlainText className="my-6">
+          Det finnes også informasjon om sykepenger til{" "}
+          <Link href="#">arbeidsgivere</Link> og{" "}
+          <Link href="#">leger og tannleger eller andre behandlere</Link>.
+        </PlainText>
+        <PinkList />
       </Page>
     </>
   );
