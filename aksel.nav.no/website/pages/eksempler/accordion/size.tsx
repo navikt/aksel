e023ace28d28d7d0f83e4f411b15159bcd46e036
Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>
2024-09-18 16:06:33 +0200

    Adjust examples of Accordion (#3159)

--- aksel.nav.no/website/pages/eksempler/accordion/size.tsx
+++ aksel.nav.no/website/pages/eksempler/accordion/size.tsx
@@ -1,12 +1,15 @@
-import { Accordion, Link } from "@navikt/ds-react";
+import { Accordion, Heading, Link } from "@navikt/ds-react";
 import { withDsExample } from "@/web/examples/withDsExample";
 
 const Example = () => {
   return (
     <>
+      <Heading size="small" spacing textColor="subtle">
+        size=large
+      </Heading>
       <Accordion size="large" headingSize="large">
         <Accordion.Item>
-          <Accordion.Header>Til deg som er mellom 62 og 67 år</Accordion.Header>
+          <Accordion.Header>Til deg mellom 62 og 67 år</Accordion.Header>
           <Accordion.Content>
             Hvis du er mellom 62 og 67 år når du søker, må du som hovedregel ha
             hatt en pensjonsgivende inntekt som tilsvarer x G, året før du fikk
@@ -15,9 +18,13 @@ const Example = () => {
         </Accordion.Item>
       </Accordion>
       <br />
+
+      <Heading size="small" spacing textColor="subtle">
+        size=medium
+      </Heading>
       <Accordion size="medium" headingSize="medium">
         <Accordion.Item>
-          <Accordion.Header>Til deg som er mellom 62 og 67 år</Accordion.Header>
+          <Accordion.Header>Til deg mellom 62 og 67 år</Accordion.Header>
           <Accordion.Content>
             Hvis du er mellom 62 og 67 år når du søker, må du som hovedregel ha
             hatt en pensjonsgivende inntekt som tilsvarer x G, året før du fikk
@@ -26,9 +33,13 @@ const Example = () => {
         </Accordion.Item>
       </Accordion>
       <br />
+
+      <Heading size="small" spacing textColor="subtle">
+        size=small
+      </Heading>
       <Accordion size="small" headingSize="small">
         <Accordion.Item>
-          <Accordion.Header>Til deg som er mellom 62 og 67 år</Accordion.Header>
+          <Accordion.Header>Til deg mellom 62 og 67 år</Accordion.Header>
           <Accordion.Content>
             Hvis du er mellom 62 og 67 år når du søker, må du som hovedregel ha
             hatt en pensjonsgivende inntekt som tilsvarer x G, året før du fikk
@@ -37,9 +48,13 @@ const Example = () => {
         </Accordion.Item>
       </Accordion>
       <br />
+
+      <Heading size="small" spacing textColor="subtle">
+        size=small headingSize=xsmall
+      </Heading>
       <Accordion size="small" headingSize="xsmall">
         <Accordion.Item>
-          <Accordion.Header>Til deg som er mellom 62 og 67 år</Accordion.Header>
+          <Accordion.Header>Til deg mellom 62 og 67 år</Accordion.Header>
           <Accordion.Content>
             Hvis du er mellom 62 og 67 år når du søker, må du som hovedregel ha
             hatt en pensjonsgivende inntekt som tilsvarer x G, året før du fikk
