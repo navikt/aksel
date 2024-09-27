4065cdbb76cf6bc53d8f20d8fdb012ad6d88c9a7
Ken <ken.aleksander@gmail.com>
2024-09-27 12:54:59 +0200

    feat: Implemented figma plugin

---
+++ @navikt/core/tokens/darkside/figma/plugin/plugin-util.ts
@@ -0,0 +1,131 @@
+import { FigmaToken, FigmaTokenConfig } from "../figma-config.types";
+
+export type ScopedFigmaTokenConfig = Omit<
+  FigmaTokenConfig,
+  "version" | "timestamp"
+>;
+
+export type ResolvedFigmaCollection = Record<
+  keyof ScopedFigmaTokenConfig,
+  {
+    collection: VariableCollection;
+    variables: Variable[];
+    config: ScopedFigmaTokenConfig[keyof ScopedFigmaTokenConfig];
+  }
+>;
+
+export type ResolvedFigmaCollectionValue =
+  ResolvedFigmaCollection[keyof ResolvedFigmaCollection];
+
+export async function createOrFindVariable(
+  token: FigmaToken,
+  { collection, variables, config }: ResolvedFigmaCollectionValue,
+) {
+  let existingVariable = variables.find(
+    (variable) => variable.name === token.name,
+  );
+
+  if (!existingVariable) {
+    console.info("Creating new variable: ", token.name);
+    existingVariable = figma.variables.createVariable(
+      token.name,
+      collection,
+      token.figmaType,
+    );
+
+    /**
+     * We have to make sure new variables are added to config so that
+     * semantic colors can reference them correctly.
+     */
+    variables.push(existingVariable);
+  }
+
+  existingVariable.hiddenFromPublishing = config.hideFromPublishing;
+  existingVariable.setVariableCodeSyntax("WEB", token.code.web);
+  existingVariable.scopes = token.scopes;
+  existingVariable.description = token.comment ?? "";
+
+  return existingVariable;
+}
+
+export const SEMANTIC_FIGMA_MODES = ["lightmode", "darkmode"] as const;
+
+export async function resolveAliasId(
+  token: FigmaToken,
+  mode: (typeof SEMANTIC_FIGMA_MODES)[number],
+  configuration: ResolvedFigmaCollection,
+) {
+  const alias = token.alias;
+
+  if (!alias) {
+    return null;
+  }
+
+  const configToReference =
+    mode === "lightmode"
+      ? configuration["globalLight"]
+      : configuration["globalDark"];
+
+  const aliasToken = configToReference.config.tokens.find(
+    (t) => t.name === alias,
+  );
+
+  if (!aliasToken) {
+    return null;
+  }
+
+  const existingVariable = configToReference.variables.find(
+    (variable) => variable.name === aliasToken.name,
+  );
+
+  if (!existingVariable) {
+    return null;
+  }
+
+  return existingVariable.id;
+}
+
+export async function createOrFindCollection(
+  config: ResolvedFigmaCollectionValue["config"],
+) {
+  const existingCollections =
+    await figma.variables.getLocalVariableCollectionsAsync();
+
+  let existingCollection = existingCollections.find(
+    (collection) => collection.name === config.name,
+  );
+
+  if (!existingCollection) {
+    existingCollection = figma.variables.createVariableCollection(config.name);
+  }
+
+  existingCollection.hiddenFromPublishing;
+
+  console.info("Creating new collection: ", config.name);
+  return existingCollection;
+}
+
+export function getExistingVariables(collection: VariableCollection) {
+  const variables: Variable[] = [];
+
+  for (const id of collection.variableIds) {
+    const variable = figma.variables.getVariableById(id);
+    variable && variables.push(variable);
+  }
+  return variables;
+}
+
+export async function reset() {
+  const localCollections =
+    await figma.variables.getLocalVariableCollectionsAsync();
+
+  localCollections.forEach((collection) => {
+    console.info("Resetting: ", collection.name);
+    collection.remove();
+  });
+}
+
+export function getLastNumber(name: string): number {
+  const matches = name.match(/\d+/g);
+  return matches ? parseInt(matches[matches.length - 1], 10) : 0;
+}
---
+++ @navikt/core/tokens/darkside/figma/plugin/plugin.ts
@@ -0,0 +1,172 @@
+import _config from "../../../figma-config.json";
+import type { FigmaTokenConfig } from "../figma-config.types";
+import {
+  type ResolvedFigmaCollection,
+  type ResolvedFigmaCollectionValue,
+  SEMANTIC_FIGMA_MODES,
+  ScopedFigmaTokenConfig,
+  createOrFindCollection,
+  createOrFindVariable,
+  getExistingVariables,
+  getLastNumber,
+  reset,
+  resolveAliasId,
+} from "./plugin-util";
+
+const globalConfig = _config as FigmaTokenConfig;
+
+/**
+ * TODO:
+ * - Add remote fetch of config from CDN i production after figma config PR is merged
+ */
+const main = async () => {
+  await reset();
+
+  const collections = await buildCollections();
+
+  await updateGlobalColorCollection(collections.globalLight);
+  await updateGlobalColorCollection(collections.globalDark);
+  await updateGlobalScalingCollection(collections.radius);
+  await updateGlobalScalingCollection(collections.spacing);
+  await updateSemanticColorCollection(collections);
+};
+
+async function buildCollections() {
+  const collections = {} as ResolvedFigmaCollection;
+
+  const scopedConfig: ScopedFigmaTokenConfig = {
+    globalLight: globalConfig.globalLight,
+    globalDark: globalConfig.globalDark,
+    semanticColors: globalConfig.semanticColors,
+    radius: globalConfig.radius,
+    spacing: globalConfig.spacing,
+  };
+
+  for (const [key, config] of Object.entries(scopedConfig)) {
+    const collection = await createOrFindCollection(config);
+    const variables = getExistingVariables(collection);
+
+    collections[key as keyof ScopedFigmaTokenConfig] = {
+      collection,
+      variables,
+      config,
+    };
+  }
+
+  return collections;
+}
+
+async function updateGlobalColorCollection(
+  configuration: ResolvedFigmaCollectionValue,
+) {
+  const { collection, config } = configuration;
+
+  /**
+   * "000"-tokens ends up last in list, so we want to sort them manually by scale.
+   */
+  const sortedTokensByScale = config.tokens.sort(
+    (a, b) => getLastNumber(a.name) - getLastNumber(b.name),
+  );
+
+  for (const token of sortedTokensByScale) {
+    const variable = await createOrFindVariable(token, configuration);
+
+    if (typeof token.value !== "string") {
+      throw new Error(`Token value is not a string: ${token}`);
+    }
+
+    variable.setValueForMode(
+      collection.defaultModeId,
+      figma.util.rgba(token.value),
+    );
+  }
+
+  console.info("Updated collection: ", config.name);
+}
+
+async function updateGlobalScalingCollection(
+  configuration: ResolvedFigmaCollectionValue,
+) {
+  const { collection, config } = configuration;
+
+  const sortedTokensByPx = config.tokens.sort((a, b) => {
+    if (typeof a.value === "number" && typeof b.value === "number") {
+      return a.value - b.value;
+    }
+    return 0;
+  });
+
+  for (const token of sortedTokensByPx) {
+    const variable = await createOrFindVariable(token, configuration);
+
+    variable.setValueForMode(collection.defaultModeId, token.value);
+  }
+
+  console.info("Updated collection: ", config.name);
+}
+
+async function updateSemanticColorCollection(
+  configuration: ResolvedFigmaCollection,
+) {
+  const { collection, config } = configuration["semanticColors"];
+
+  const modes: {
+    name: (typeof SEMANTIC_FIGMA_MODES)[number];
+    modeId: string;
+  }[] = SEMANTIC_FIGMA_MODES.map((mode) => {
+    const foundMode = collection.modes.find((m) => m.name === mode);
+
+    if (!foundMode) {
+      return {
+        modeId: collection.addMode(mode),
+        name: mode as (typeof SEMANTIC_FIGMA_MODES)[number],
+      };
+    }
+    return foundMode as {
+      name: (typeof SEMANTIC_FIGMA_MODES)[number];
+      modeId: string;
+    };
+  });
+
+  /**
+   * Removed default "value"-mode if it exists
+   */
+  collection.modes.forEach((mode) => {
+    if (!modes.find((m) => m.name === mode.name)) {
+      collection.removeMode(mode.modeId);
+    }
+  });
+
+  for (const token of config.tokens) {
+    const variable = await createOrFindVariable(
+      token,
+      configuration["semanticColors"],
+    );
+
+    for (const mode of modes) {
+      const aliasId = await resolveAliasId(token, mode.name, configuration);
+
+      if (aliasId) {
+        variable.setValueForMode(mode.modeId, {
+          type: "VARIABLE_ALIAS",
+          id: aliasId,
+        });
+        continue;
+      }
+
+      if (typeof token.value !== "string") {
+        throw new Error(`Token value is not a string: ${token}`);
+      }
+
+      /* Some semantic values will be hardcoded to a custom value */
+      variable.setValueForMode(mode.modeId, figma.util.rgba(token.value));
+    }
+  }
+  console.info("Updated collection: ", config.name);
+}
+
+main().then(() =>
+  figma.closePlugin(
+    `Local variables updated! Last config update: ${globalConfig.timestamp}.`,
+  ),
+);
