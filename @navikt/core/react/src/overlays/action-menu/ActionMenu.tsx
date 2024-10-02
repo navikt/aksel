34067fa22d0ea7bee29411dc30c52216bc143925
Ken <ken.aleksander@gmail.com>
2024-10-02 20:51:32 +0200

    refactor: Simplified SVG with groups

--- @navikt/core/react/src/overlays/action-menu/ActionMenu.tsx
+++ @navikt/core/react/src/overlays/action-menu/ActionMenu.tsx
@@ -637,6 +637,7 @@ type ActionMenuCheckboxItemElement = React.ElementRef<typeof Menu.CheckboxItem>;
 type MenuCheckboxItemProps = React.ComponentPropsWithoutRef<
   typeof Menu.CheckboxItem
 >;
+
 interface ActionMenuCheckboxItemProps
   extends Omit<MenuCheckboxItemProps, "asChild"> {
   children: React.ReactNode;
@@ -687,69 +688,54 @@ const ActionMenuCheckboxItem = forwardRef<
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              className="navds-action-menu__indicator-icon navds-action-menu__indicator-icon--unchecked"
+              className="navds-action-menu__indicator-icon"
               aria-hidden
             >
-              <rect
-                width="24"
-                height="24"
-                rx="4"
-                fill="var(--a-border-default)"
-              />
-              <rect
-                x="1"
-                y="1"
-                width="22"
-                height="22"
-                rx="3"
-                fill="var(--a-surface-default)"
-                strokeWidth="2"
-              />
-            </svg>
-            <svg
-              width="1em"
-              height="1em"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="navds-action-menu__indicator-icon navds-action-menu__indicator-icon--indeterminate"
-              aria-hidden
-            >
-              <rect
-                width="24"
-                height="24"
-                rx="4"
-                fill="var(--a-surface-action-selected)"
-              />
-              <rect
-                x="6"
-                y="10"
-                width="12"
-                height="4"
-                rx="1"
-                fill="var(--a-surface-default)"
-              />
-            </svg>
-
-            <svg
-              width="1em"
-              height="1em"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="navds-action-menu__indicator-icon navds-action-menu__indicator-icon--checked"
-              aria-hidden
-            >
-              <rect
-                width="24"
-                height="24"
-                rx="4"
-                fill="var(--a-surface-action-selected)"
-              />
-              <path
-                d="M10.0352 13.4148L16.4752 7.40467C17.0792 6.83965 18.029 6.86933 18.5955 7.47478C19.162 8.08027 19.1296 9.03007 18.5245 9.59621L11.0211 16.5993C10.741 16.859 10.3756 17 10.0002 17C9.60651 17 9.22717 16.8462 8.93914 16.5611L6.43914 14.0611C5.85362 13.4756 5.85362 12.5254 6.43914 11.9399C7.02467 11.3544 7.97483 11.3544 8.56036 11.9399L10.0352 13.4148Z"
-                fill="var(--a-surface-default)"
-              />
+              <g className="navds-action-menu__indicator-icon--unchecked">
+                <rect
+                  width="24"
+                  height="24"
+                  rx="4"
+                  fill="var(--a-border-default)"
+                />
+                <rect
+                  x="1"
+                  y="1"
+                  width="22"
+                  height="22"
+                  rx="3"
+                  fill="var(--a-surface-default)"
+                  strokeWidth="2"
+                />
+              </g>
+              <g className="navds-action-menu__indicator-icon--indeterminate">
+                <rect
+                  width="24"
+                  height="24"
+                  rx="4"
+                  fill="var(--a-surface-action-selected)"
+                />
+                <rect
+                  x="6"
+                  y="10"
+                  width="12"
+                  height="4"
+                  rx="1"
+                  fill="var(--a-surface-default)"
+                />
+              </g>
+              <g className="navds-action-menu__indicator-icon--checked">
+                <rect
+                  width="24"
+                  height="24"
+                  rx="4"
+                  fill="var(--a-surface-action-selected)"
+                />
+                <path
+                  d="M10.0352 13.4148L16.4752 7.40467C17.0792 6.83965 18.029 6.86933 18.5955 7.47478C19.162 8.08027 19.1296 9.03007 18.5245 9.59621L11.0211 16.5993C10.741 16.859 10.3756 17 10.0002 17C9.60651 17 9.22717 16.8462 8.93914 16.5611L6.43914 14.0611C5.85362 13.4756 5.85362 12.5254 6.43914 11.9399C7.02467 11.3544 7.97483 11.3544 8.56036 11.9399L10.0352 13.4148Z"
+                  fill="var(--a-surface-default)"
+                />
+              </g>
             </svg>
           </Menu.ItemIndicator>
         </Marker>
@@ -838,55 +824,49 @@ const ActionMenuRadioItem = forwardRef<
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              className="navds-action-menu__indicator-icon navds-action-menu__indicator-icon--unchecked"
+              className="navds-action-menu__indicator-icon"
               aria-hidden
             >
-              <rect
-                width="24"
-                height="24"
-                rx="12"
-                fill="var(--a-border-default)"
-              />
-              <rect
-                x="1"
-                y="1"
-                width="22"
-                height="22"
-                rx="11"
-                strokeWidth="2"
-                fill="var(--a-surface-default)"
-              />
-            </svg>
-            <svg
-              width="1em"
-              height="1em"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="navds-action-menu__indicator-icon navds-action-menu__indicator-icon--checked"
-              aria-hidden
-            >
-              <rect
-                x="1"
-                y="1"
-                width="22"
-                height="22"
-                rx="11"
-                fill="var(--a-surface-default)"
-              />
-              <rect
-                x="1"
-                y="1"
-                width="22"
-                height="22"
-                rx="11"
-                stroke="var(--a-surface-action-selected)"
-                strokeWidth="2"
-              />
-              <path
-                d="M20 12C20 16.4178 16.4178 20 12 20C7.58222 20 4 16.4178 4 12C4 7.58222 7.58222 4 12 4C16.4178 4 20 7.58222 20 12Z"
-                fill="var(--a-surface-action-selected)"
-              />
+              <g className="navds-action-menu__indicator-icon--unchecked">
+                <rect
+                  width="24"
+                  height="24"
+                  rx="12"
+                  fill="var(--a-border-default)"
+                />
+                <rect
+                  x="1"
+                  y="1"
+                  width="22"
+                  height="22"
+                  rx="11"
+                  strokeWidth="2"
+                  fill="var(--a-surface-default)"
+                />
+              </g>
+              <g className="navds-action-menu__indicator-icon--checked">
+                <rect
+                  x="1"
+                  y="1"
+                  width="22"
+                  height="22"
+                  rx="11"
+                  fill="var(--a-surface-default)"
+                />
+                <rect
+                  x="1"
+                  y="1"
+                  width="22"
+                  height="22"
+                  rx="11"
+                  stroke="var(--a-surface-action-selected)"
+                  strokeWidth="2"
+                />
+                <path
+                  d="M20 12C20 16.4178 16.4178 20 12 20C7.58222 20 4 16.4178 4 12C4 7.58222 7.58222 4 12 4C16.4178 4 20 7.58222 20 12Z"
+                  fill="var(--a-surface-action-selected)"
+                />
+              </g>
             </svg>
           </Menu.ItemIndicator>
         </Marker>
