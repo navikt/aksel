f925c76d50df30ae787da2668a61d462f185d502
Halvor Haugan <halvor.haugan@nav.no>
2024-10-08 12:57:39 +0200

    Add translations for NN and EN

---
+++ @navikt/core/react/src/util/i18n/locales/en.ts
@@ -0,0 +1,25 @@
+export default {
+  FileUpload: {
+    dropzone: {
+      button: "Select file",
+      buttonMultiple: "Select files",
+      dragAndDrop: "Drag and drop file here",
+      dragAndDropMultiple: "Drag and drop files here",
+      drop: "Drop",
+      or: "or",
+      disabled: "File upload disabled",
+      disabledFilelimit: "You cannot upload more files",
+    },
+    item: {
+      retryButtonTitle: "Try uploading the file again",
+      deleteButtonTitle: "Delete file",
+      uploading: "Uploading…",
+      downloading: "Downloading…",
+    },
+  },
+  FormProgress: {
+    step: "Step {activeStep} of {totalSteps}",
+    showAllSteps: "Show all steps",
+    hideAllSteps: "Hide all steps",
+  },
+};
---
+++ @navikt/core/react/src/util/i18n/locales/nn.ts
@@ -0,0 +1,25 @@
+export default {
+  FileUpload: {
+    dropzone: {
+      button: "Vel fil",
+      buttonMultiple: "Vel filer",
+      dragAndDrop: "Dra og slepp fila her",
+      dragAndDropMultiple: "Dra og slepp filer her",
+      drop: "Slepp",
+      or: "eller",
+      disabled: "Filopplasting er deaktivert",
+      disabledFilelimit: "Du kan ikkje laste opp fleire filer",
+    },
+    item: {
+      retryButtonTitle: "Prøv å laste opp fila på nytt",
+      deleteButtonTitle: "Slett fila",
+      uploading: "Lastar opp…",
+      downloading: "Lastar ned…",
+    },
+  },
+  FormProgress: {
+    step: "Steg {activeStep} av {totalSteps}",
+    showAllSteps: "Vis alle steg",
+    hideAllSteps: "Skjul alle steg",
+  },
+};
