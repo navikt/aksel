dcf5452d2805cbf068a6a771c1196994d48aa1a4
Julian Nymark <julian.nymark@nav.no>
2024-10-08 16:34:15 +0200

    Referansesider: dagoversikten content

--- examples/referansesider/src/routes/dagoversikten.tsx
+++ examples/referansesider/src/routes/dagoversikten.tsx
@@ -1,12 +1,17 @@
 import { createFileRoute } from "@tanstack/react-router";
 import {
   BaggageIcon,
+  BriefcaseIcon,
+  CalendarIcon,
   ChevronLeftCircleIcon,
   ChevronRightCircleIcon,
   FilesIcon,
   PersonIcon,
+  PlateFillIcon,
 } from "@navikt/aksel-icons";
-import { Page } from "../components/Page";
+import { Button } from "../components/Button";
+import { Link } from "../components/Link";
+import { Tag } from "../components/Tag";
 
 const Breadcrumb = () => {
   return (
@@ -72,9 +77,58 @@ const DagoversiktPage = () => {
     <div>
       <Breadcrumb />
       <Timeline />
-      <Page>
-        <div></div>
-      </Page>
+      <div className="flex border border-gray-300">
+        <div className="flex flex-col border border-gray-300">
+          <div className="flex gap-2">
+            <Tag $variant="neutral">Forlengelse</Tag>
+            <Tag $variant="info">Revurdering</Tag>
+          </div>
+          <div className="flex">
+            <CalendarIcon />
+            <span>09.05.19 - 16.05.19</span>
+          </div>
+          <div className="flex">
+            <CalendarIcon />
+            <span>09.05.19</span>
+          </div>
+          <div className="flex">
+            <PlateFillIcon className="text-red-600" />
+            <span>09.05.19 - 16.05.19</span>
+          </div>
+          <div className="flex">
+            <BriefcaseIcon />
+            <span>09.05.19 - 16.05.19</span>
+          </div>
+          <div className="flex justify-between">
+            <span>Månedsbeløp:</span>
+            <span>10 000,00 kr</span>
+          </div>
+          <span className="mt-4">UTBETALINGSINFORMASJON</span>
+          <div className="flex justify-between">
+            <span>Sykepengegrunnlag:</span>
+            <span>240 123,00 kr</span>
+          </div>
+          <div className="flex justify-between">
+            <span>Utbetalingsdager:</span>
+            <span>3 dager</span>
+          </div>
+          <div className="flex justify-between mt-4 font-bold">
+            <span>Beløp for perioden</span>
+            <span>2 769,00 kr</span>
+          </div>
+          <Link>Simulering</Link>
+          <div>
+            <Button variant="primary">Fatt vedtak</Button>
+            <Button variant="secondary">Kan ikke behandles her</Button>
+          </div>
+        </div>
+        <div className="border grow border-gray-300">
+          <div>Vis varsler (3 av 4 er sjekket)</div>
+        </div>
+        <div className="border border-gray-300">
+          <div>Historikk</div>
+        </div>
+      </div>
     </div>
   );
 };
