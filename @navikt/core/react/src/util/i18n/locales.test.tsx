e55705b6633c5885fec6bb253d216a955a1d97ef
Halvor Haugan <halvor.haugan@nav.no>
2024-10-08 14:06:02 +0200

    TS will do some of the testing for us

--- @navikt/core/react/src/util/i18n/locales.test.tsx
+++ @navikt/core/react/src/util/i18n/locales.test.tsx
@@ -5,20 +5,6 @@ import en from "./locales/en";
 import nb from "./locales/nb";
 import nn from "./locales/nn";
 
-function compareKeys(a: TranslationObject, b: TranslationObject) {
-  const aKeys = Object.keys(a);
-  const bKeys = Object.keys(b);
-  expect(aKeys).toEqual(bKeys);
-  aKeys.forEach((key) => {
-    if (typeof a[key] === "object") {
-      expect(b[key]).toBeTypeOf("object");
-      if (typeof b[key] === "object") {
-        compareKeys(a[key], b[key]);
-      }
-    }
-  });
-}
-
 function checkValues(obj: TranslationObject) {
   Object.values(obj).forEach((value) => {
     if (typeof value === "object") {
@@ -31,8 +17,6 @@ function checkValues(obj: TranslationObject) {
 }
 
 describe("Locale", () => {
-  test("NB and NN should have the same keys", () => compareKeys(nb, nn));
-  test("NB and EN should have the same keys", () => compareKeys(nb, en));
   test("NB should have no empty strings", () => checkValues(nb));
   test("NN should have no empty strings", () => checkValues(nn));
   test("EN should have no empty strings", () => checkValues(en));
