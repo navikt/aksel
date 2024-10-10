16bed794820351e17f8531e82f94b5365f92dfb5
Ken <ken.aleksander@gmail.com>
2024-10-10 12:22:04 +0200

    bug: Fixed types for YML-handling

--- @navikt/aksel-icons/config/figma/make-configs.ts
+++ @navikt/aksel-icons/config/figma/make-configs.ts
@@ -4,18 +4,34 @@ import { dump } from "js-yaml";
 import { resolve } from "path";
 import { resolveName } from "./icon-name";
 
+export type IconYml = {
+  name: string;
+  category: string;
+  sub_category: string;
+  keywords: string[];
+  variant: string;
+  updated_at: string;
+  created_at: string;
+};
+
 export const makeConfig = (
   icons: PublishedComponent[],
   dirLocation: string,
 ) => {
-  icons.forEach((icon) => {
+  for (const icon of icons) {
     const name = resolveName(icon).replace(".svg", "");
     const keywords = icon.description
       .split(",")
       .map((x) => x.trim())
       .filter(Boolean);
 
-    const config = {
+    if (!icon.containing_frame?.pageName || !icon.containing_frame?.name) {
+      throw new Error(
+        `Missing containing_frame.pageName or containing_frame.name for icon ${name}`,
+      );
+    }
+
+    const config: IconYml = {
       name,
       category: icon.containing_frame?.pageName,
       sub_category: icon.containing_frame?.name,
@@ -46,5 +62,5 @@ export const makeConfig = (
     writeFileSync(resolve(dirLocation, `${config.name}.yml`), yml, {
       encoding: "utf8",
     });
-  });
+  }
 };
