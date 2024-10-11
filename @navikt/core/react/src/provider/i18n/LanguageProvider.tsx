17ae19c5fe9b646925adf0cb002f33bcc93ce5ad
Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>
2024-10-11 12:01:11 +0200

    Add translations for NN and EN (#3209)

--- @navikt/core/react/src/provider/i18n/LanguageProvider.tsx
+++ @navikt/core/react/src/provider/i18n/LanguageProvider.tsx
@@ -1,12 +1,12 @@
 import React, { createContext, useContext } from "react";
-import { TranslationDictionary } from "../../util/i18n/i18n.types";
+import { PartialTranslations } from "../../util/i18n/i18n.types";
 import nb from "../../util/i18n/locales/nb";
 
 export interface LanguageProviderContextType {
   /**
    * Merged with the default language translations object (officially provided translations).
    */
-  translations: TranslationDictionary | TranslationDictionary[];
+  translations: PartialTranslations | PartialTranslations[];
 }
 
 export const LanguageProviderContext =
@@ -16,7 +16,7 @@ export const LanguageProviderContext =
 
 export interface LanguageProviderProps {
   children?: React.ReactNode;
-  translations?: TranslationDictionary | TranslationDictionary[];
+  translations?: PartialTranslations | PartialTranslations[];
 }
 
 export const useProvider = () => useContext(LanguageProviderContext);
