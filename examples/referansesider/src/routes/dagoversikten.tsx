6d2115cdc9b6f665be57fd221603bdce6dad4146
Julian Nymark <julian.nymark@nav.no>
2024-10-11 13:57:20 +0200

    Referansesider: dagoversikten structure + completed leftside

--- examples/referansesider/src/routes/dagoversikten.tsx
+++ examples/referansesider/src/routes/dagoversikten.tsx
@@ -4,10 +4,12 @@ import {
   BaggageIcon,
   BriefcaseIcon,
   CalendarIcon,
+  ChevronDownIcon,
   ChevronLeftCircleIcon,
   ChevronRightCircleIcon,
   FilesIcon,
   PersonCircleFillIcon,
+  PersonIcon,
   PlateFillIcon,
 } from "@navikt/aksel-icons";
 import * as tokens from "@navikt/ds-tokens/dist/darkside/tokens";
@@ -87,8 +89,93 @@ const Timeline = () => {
           <span className="border-black border px-2 py-1">4 år</span>
         </div>
       </div>
+      <h1>
+        copy paste litt HTML/CSS fra rendered Timelinecomponent (+ tokens)
+      </h1>
     </div>
   );
+};
+
+const ScSmallCaps = styled.span`
+  color: ${tokens.TextSubtle};
+  font-weight: 600;
+  font-variant-caps: all-small-caps;
+`;
+
+const LeftSide = () => {
+  return (
+    <div>
+      <div className="flex gap-2 mb-4">
+        <Tag $variant="neutral">Forlengelse</Tag>
+        <Tag $variant="info">Revurdering</Tag>
+      </div>
+      <div className="flex items-center gap-2">
+        <CalendarIcon />
+        <span>09.05.19 - 16.05.19</span>
+      </div>
+      <div className="flex items-center gap-2">
+        <CalendarIcon />
+        <span>09.05.19</span>
+      </div>
+      <div className="flex items-center gap-2">
+        <PlateFillIcon className="text-red-600" />
+        <span>09.05.19 (10 dager igjen)</span>
+      </div>
+      <div className="flex items-center gap-2">
+        <BriefcaseIcon />
+        <span>Sykepleierhuset AS</span>
+        <ChevronDownIcon />
+      </div>
+      <div className="flex justify-between">
+        <span>Månedsbeløp:</span>
+        <span>10 000,00 kr</span>
+      </div>
+      <ScSmallCaps className="mt-4 text-lg">UTBETALINGSINFORMASJON</ScSmallCaps>
+      <div className="flex justify-between">
+        <span>Sykepengegrunnlag:</span>
+        <span>240 123,00 kr</span>
+      </div>
+      <div className="flex justify-between">
+        <span>Utbetalingsdager:</span>
+        <span>3 dager</span>
+      </div>
+      <div className="flex justify-between mt-4 font-bold">
+        <span>Beløp for perioden</span>
+        <span>2 769,00 kr</span>
+      </div>
+      <div className="flex justify-between">
+        <div className="flex items-center gap-2">
+          <BriefcaseIcon />
+          <span>Sykepleierhuset AS</span>
+        </div>
+        <span>240 123,00 kr</span>
+      </div>
+      <div className="flex justify-between">
+        <div className="flex items-center gap-2">
+          <PersonIcon />
+          <span>Mia Cathrine...</span>
+        </div>
+        <span>-</span>
+      </div>
+      <Link className="my-4">Simulering</Link>
+      <div className="flex gap-4">
+        <Button variant="primary" size="small">
+          Fatt vedtak
+        </Button>
+        <Button variant="secondary" size="small">
+          Kan ikke behandles her
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+const RightSide = () => {
+  return <div>Historikk</div>;
+};
+
+const Center = () => {
+  return <div>center</div>;
 };
 
 const DagoversiktPage = () => {
@@ -97,56 +184,15 @@ const DagoversiktPage = () => {
       <div>
         <Breadcrumb />
         <Timeline />
-        <div className="flex border border-gray-300">
-          <div className="flex flex-col border border-gray-300">
-            <div className="flex gap-2">
-              <Tag $variant="neutral">Forlengelse</Tag>
-              <Tag $variant="info">Revurdering</Tag>
-            </div>
-            <div className="flex">
-              <CalendarIcon />
-              <span>09.05.19 - 16.05.19</span>
-            </div>
-            <div className="flex">
-              <CalendarIcon />
-              <span>09.05.19</span>
-            </div>
-            <div className="flex">
-              <PlateFillIcon className="text-red-600" />
-              <span>09.05.19 - 16.05.19</span>
-            </div>
-            <div className="flex">
-              <BriefcaseIcon />
-              <span>09.05.19 - 16.05.19</span>
-            </div>
-            <div className="flex justify-between">
-              <span>Månedsbeløp:</span>
-              <span>10 000,00 kr</span>
-            </div>
-            <span className="mt-4">UTBETALINGSINFORMASJON</span>
-            <div className="flex justify-between">
-              <span>Sykepengegrunnlag:</span>
-              <span>240 123,00 kr</span>
-            </div>
-            <div className="flex justify-between">
-              <span>Utbetalingsdager:</span>
-              <span>3 dager</span>
-            </div>
-            <div className="flex justify-between mt-4 font-bold">
-              <span>Beløp for perioden</span>
-              <span>2 769,00 kr</span>
-            </div>
-            <Link>Simulering</Link>
-            <div>
-              <Button variant="primary">Fatt vedtak</Button>
-              <Button variant="secondary">Kan ikke behandles her</Button>
-            </div>
+        <div className="flex border-1 border-gray-300">
+          <div className="flex flex-col border border-gray-300 p-4">
+            <LeftSide />
           </div>
           <div className="border grow border-gray-300">
-            <div>Vis varsler (3 av 4 er sjekket)</div>
+            <Center />
           </div>
           <div className="border border-gray-300">
-            <div>Historikk</div>
+            <RightSide />
           </div>
         </div>
       </div>
