34903863e574986210a4642fc5da39f0d761e8e8
Ken <26967723+KenAJoh@users.noreply.github.com>
2024-11-14 09:57:41 +0100

    Update aksel.nav.no/website/components/website-modules/examples/__parts/PageDemoContent.tsx
    Co-authored-by: Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>

--- aksel.nav.no/website/components/website-modules/examples/__parts/PageDemoContent.tsx
+++ aksel.nav.no/website/components/website-modules/examples/__parts/PageDemoContent.tsx
@@ -5,10 +5,8 @@ function Content({ children }: { children: React.ReactNode }) {
     <div className="decorator-content">
       <style>
         {`.decorator-content {
-          width: 100%;
           padding-block: 5rem;
           padding-inline: 0.25rem;
-          height: 100%;
           display: grid;
           place-content: center;
           background: repeating-linear-gradient(
