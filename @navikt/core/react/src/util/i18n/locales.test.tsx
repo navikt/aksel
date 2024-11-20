3067857e6ba75648e4bb83c5be54df31611c2a17
Halvor Haugan <halvor.haugan@nav.no>
2024-11-20 16:58:15 +0100

    naming

--- @navikt/core/react/src/util/i18n/locales.test.tsx
+++ @navikt/core/react/src/util/i18n/locales.test.tsx
@@ -5,41 +5,45 @@ import en from "./locales/en";
 import nb from "./locales/nb";
 import nn from "./locales/nn";
 
-type NestedValue = string | Record<string, string | Record<string, string>>;
+type NestedObject = Record<string, string | Record<string, string>>;
 
-function checkValues(obj: Translations | NestedValue) {
-  Object.entries(obj).forEach(([key, value]) => {
-    if (key === "dateLocale") {
-      return;
-    }
-    if (typeof value === "object") {
-      checkValues(value);
-    } else {
-      expect(value).toBeTypeOf("string");
-      expect(value).not.toBe("");
-    }
-  });
+function checkValues(obj: Translations | NestedObject) {
+  Object.entries(obj).forEach(
+    ([key, value]: [string, string | NestedObject]) => {
+      if (key === "dateLocale") {
+        return;
+      }
+      if (typeof value === "object") {
+        checkValues(value);
+      } else {
+        expect(value).toBeTypeOf("string");
+        expect(value).not.toBe("");
+      }
+    },
+  );
 }
 
-function checkPlaceholders(
-  baseTrans: Translations | NestedValue, // Translations to check against
-  trans: Translations | NestedValue, // Translations to check
+function checkTranslationPlaceholders(
+  baseTranslations: Translations | NestedObject, // Translations to check against
+  translations: Translations | NestedObject, // Translations to check
 ) {
-  Object.entries(baseTrans).forEach(([key, baseVal]: [string, NestedValue]) => {
-    if (key === "dateLocale") {
-      return;
-    }
-    if (typeof baseVal === "object") {
-      checkPlaceholders(baseVal, trans[key]);
-    } else {
-      const correctPlaceholders = baseVal.match(/{[^}]*}/g) || [];
-      const transPlaceholders = trans[key].match(/{[^}]*}/g) || [];
-      expect(
-        transPlaceholders.sort(),
-        `Wrong placeholders in "${trans[key]}" (key=${key})`,
-      ).toEqual(correctPlaceholders.sort());
-    }
-  });
+  Object.entries(baseTranslations).forEach(
+    ([key, baseVal]: [string, string | NestedObject]) => {
+      if (key === "dateLocale") {
+        return;
+      }
+      if (typeof baseVal === "object") {
+        checkTranslationPlaceholders(baseVal, translations[key]);
+      } else {
+        const correctPlaceholders = baseVal.match(/{[^}]*}/g) || [];
+        const transPlaceholders = translations[key].match(/{[^}]*}/g) || [];
+        expect(
+          transPlaceholders.sort(),
+          `Wrong placeholders in "${translations[key]}" (key=${key})`,
+        ).toEqual(correctPlaceholders.sort());
+      }
+    },
+  );
 }
 
 describe("Locale", () => {
@@ -48,7 +52,7 @@ describe("Locale", () => {
   test("EN should have no empty strings", () => checkValues(en));
 
   test("NN should have the same placeholders as NB", () =>
-    checkPlaceholders(nb, nn));
+    checkTranslationPlaceholders(nb, nn));
   test("EN should have the same placeholders as NB", () =>
-    checkPlaceholders(nb, en));
+    checkTranslationPlaceholders(nb, en));
 });
