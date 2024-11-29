9edc31b3db86fe749cd34733d2d5b94d4b866dc2
Ken <ken.aleksander@gmail.com>
2024-11-29 13:40:21 +0100

    :bug: Fixed weeknumber as the button broke old datepicker

--- @navikt/core/react/src/date/datepicker/parts/WeekNumber.tsx
+++ @navikt/core/react/src/date/datepicker/parts/WeekNumber.tsx
@@ -1,7 +1,9 @@
 /* https://github.com/gpbl/react-day-picker/blob/7f78cd5/src/components/WeekNumber/WeekNumber.tsx#L21 */
+import cl from "clsx";
 import React from "react";
-import { useDayPicker } from "react-day-picker";
+import { Button as RDPButton, useDayPicker } from "react-day-picker";
 import { Button } from "../../../button";
+import { UNSAFE_useAkselTheme } from "../../../provider";
 import { labelWeekNumber, labelWeekNumberButton } from "../../utils/labels";
 
 export interface WeekNumberProps {
@@ -16,6 +18,7 @@ export interface WeekNumberProps {
  * https://github.com/gpbl/react-day-picker/tree/main/src/components/WeekNumber
  */
 function WeekNumber(props: WeekNumberProps): JSX.Element {
+  const themeContext = UNSAFE_useAkselTheme(false);
   const { number: weekNumber, dates } = props;
   const {
     onWeekNumberClick,
@@ -32,7 +35,7 @@ function WeekNumber(props: WeekNumberProps): JSX.Element {
   if (!onWeekNumberClick) {
     return (
       <span
-        className={classNames.weeknumber}
+        className={cl(classNames.weeknumber, "navds-date__weeknumber-number")}
         style={styles.weeknumber}
         aria-label={weekLabel}
       >
@@ -50,17 +53,33 @@ function WeekNumber(props: WeekNumberProps): JSX.Element {
     onWeekNumberClick(weekNumber, dates, e);
   };
 
+  if (themeContext) {
+    return (
+      <Button
+        variant="secondary-neutral"
+        name="week-number"
+        aria-label={label}
+        style={styles.weeknumber}
+        className="navds-date__weeknumber"
+        onClick={handleClick}
+        size="small"
+        icon={
+          <span className="navds-date__weeknumber-number">{weekNumber}</span>
+        }
+      />
+    );
+  }
+
   return (
-    <Button
-      variant="secondary-neutral"
+    <RDPButton
       name="week-number"
       aria-label={label}
       style={styles.weeknumber}
-      className="navds-date__weeknumber"
+      className={classNames.weeknumber}
       onClick={handleClick}
     >
       {weekNumber}
-    </Button>
+    </RDPButton>
   );
 }
 
