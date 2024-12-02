e6db99b0db40fa588cd7f08e2e76946358b670ad
Ken <ken.aleksander@gmail.com>
2024-12-02 15:52:54 +0100

    :recycle: Using camelCase on tokens breaks primtives. Now uses kebab-case

--- @navikt/core/react/src/layout/box/Box.stories.tsx
+++ @navikt/core/react/src/layout/box/Box.stories.tsx
@@ -407,7 +407,7 @@ export const BoxNewDarksideLight: Story = {
     <Box.New
       background="accent-moderate"
       shadow="dialog"
-      borderColor="brandOne-strong"
+      borderColor="brand-one-strong"
       borderWidth="2"
       padding="5"
       paddingInline="20"
@@ -423,7 +423,7 @@ export const BoxNewDarksideDark: Story = {
     <Box.New
       background="accent-moderate"
       shadow="dialog"
-      borderColor="brandOne-strong"
+      borderColor="brand-one-strong"
       borderWidth="2"
       padding="5"
       paddingInline="20"
