05f9714a69a13828ca4dd914bcbb9cfc7add784f
Ken <ken.aleksander@gmail.com>
2024-12-03 16:28:22 +0100

    V1 of CSS bundler

--- @navikt/core/css/darkside/bundle.ts
+++ @navikt/core/css/darkside/bundle.ts
@@ -1,11 +1,19 @@
+import browserslist from "browserslist";
 import fs from "fs";
-import { Features, bundle } from "lightningcss";
+import { Features, browserslistToTargets, bundle } from "lightningcss";
 
+console.log(
+  browserslist(">= 0.3% in NO, safari >= 15.4, iOS >= 15.4, not dead"),
+);
 const { code } = bundle({
   filename: `./index.css`,
   minify: false,
   include:
     Features.Nesting | Features.MediaRangeSyntax | Features.HexAlphaColors,
+
+  targets: browserslistToTargets(
+    browserslist(">= 0.3% in NO, safari >= 15.4, iOS >= 15.4, not dead"),
+  ),
 });
 
 fs.writeFileSync("./OUTPUT.css", code.toString());
