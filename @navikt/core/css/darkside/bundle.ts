648a7849e14328fb1eeef2e22cfa3926c1e89ca3
Ken <ken.aleksander@gmail.com>
2024-12-03 16:56:31 +0100

    :recycle: Remove built in variables added by lightningcss

--- @navikt/core/css/darkside/bundle.ts
+++ @navikt/core/css/darkside/bundle.ts
@@ -1,19 +1,34 @@
 import browserslist from "browserslist";
-import fs from "fs";
+
+/* import fs from "fs"; */
 import { Features, browserslistToTargets, bundle } from "lightningcss";
 
-console.log(
-  browserslist(">= 0.4% in NO, safari >= 15.4, iOS >= 15.4, not dead"),
-);
 const { code } = bundle({
   filename: `./index.css`,
   minify: false,
   include:
     Features.Nesting | Features.MediaRangeSyntax | Features.HexAlphaColors,
 
+  drafts: {
+    customMedia: false,
+  },
   targets: browserslistToTargets(
-    browserslist(">= 0.4% in NO, safari >= 15.4, iOS >= 15.4, not dead"),
+    browserslist(">= 0.5% in NO, safari >= 15.4, iOS >= 15.4, not dead"),
   ),
 });
 
-fs.writeFileSync("./OUTPUT.css", code.toString());
+let codeString = code.toString();
+
+/**
+ * LightningCSS adds these tokens to the bundle that we want removed:
+ * --lightningcss-light: initial;
+ * --lightningcss-dark: ;
+ */
+codeString = codeString
+  .split("\n")
+  .filter((line) => !line.includes("--lightningcss-"))
+  .join("\n");
+
+/* fs.writeFileSync("./OUTPUT.css", codeString); */
+
+console.info(codeString);
