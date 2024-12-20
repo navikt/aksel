0313d340fedf4063f9dca9039a285304258e8e9f
Ken <ken.aleksander@gmail.com>
2024-12-20 09:58:53 +0100

    :sparkles: Added new space tokens

--- @navikt/core/tokens/types.ts
+++ @navikt/core/tokens/types.ts
@@ -85,28 +85,12 @@ export type BorderColorWithRoleKeys =
   | `${ColorRoles}-subtleA`
   | `${ColorRoles}-strong`;
 
-export type SpacingKeys =
-  | "0"
-  | "05"
-  | "1"
-  | "1-alt"
-  | "2"
-  | "3"
-  | "4"
-  | "5"
-  | "6"
-  | "7"
-  | "8"
-  | "9"
-  | "10"
-  | "11"
-  | "12"
-  | "14"
-  | "16"
-  | "18"
-  | "20"
-  | "24"
-  | "32";
+export const spacingInPixels = [
+  0, 1, 2, 4, 6, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 56, 64, 72, 80, 96,
+  128,
+] as const;
+
+export type SpacingKeys = (typeof spacingInPixels)[number];
 
 export type ShadowKeys = "dialog";
 
