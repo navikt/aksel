aea8bd1b86ca092dfc09dbaf367fab8908d04f4c
Ken <ken.aleksander@gmail.com>
2025-01-28 19:01:11 +0100

    :test_tube: Added legacy-support for deprecated files

--- @navikt/core/css/config/tests/bundle.test.ts
+++ @navikt/core/css/config/tests/bundle.test.ts
@@ -6,6 +6,7 @@ import { describe, expect, test } from "vitest";
  * Darkside config might have some new files added. We can skip these when checking for equality.
  */
 const newFiles = ["theme"];
+const deprecatedFiles = ["linkpanel"];
 
 describe("Check that old and new (darkside) bundle matches", () => {
   test("Darkside includes the same files as old bundle", () => {
@@ -27,6 +28,9 @@ describe("Check that old and new (darkside) bundle matches", () => {
 
     // Compare file names
     oldFiles.forEach((file) => {
+      if (deprecatedFiles.find((oldFile) => file.includes(oldFile))) {
+        return;
+      }
       expect(darksideFiles).toContain(file);
     });
 
