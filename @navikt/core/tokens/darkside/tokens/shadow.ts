75d2f7a595fac9ad39523e772149e362173e215c
Ken <26967723+KenAJoh@users.noreply.github.com>
2025-02-04 19:01:46 +0100

    [Darkside] Quality assurance P7 (#3530)
    * :lipstick: Stepper text-colors
    
    * :lipstick: Adjusted paddings form-summary
    
    * :lipstick: Alpha-border formsummary
    
    * :bug: Re-added animation to formprogress
    
    * :lipstick: moderate active state for button
    
    * :recycle: Copybutton re-uses button-component
    
    * :lipstick: Updated shadow for lightmode
    
    * :fire: Removed comments CopyButton
    
    * :memo: Added bw-compatibility for CopyButton
    
    * :bug: Added default-size for legacy CopyButton
    
    * :bug: Boolean flipped so that legacy wrapper were used on wrong state
    
    * :bug: Boolean flipped so that legacy wrapper were used on wrong state
    
    * :bug: Now accounts for custom icons in legacy CopyButton

--- @navikt/core/tokens/darkside/tokens/shadow.ts
+++ @navikt/core/tokens/darkside/tokens/shadow.ts
@@ -7,7 +7,7 @@ export function shadowTokenConfig(theme: ColorTheme) {
       dialog: {
         value:
           theme === "light"
-            ? `0px 0px 1px 0px rgba(7 9 13 / 0.02), 0px 2px 5px 0px rgba(7 9 13 / 0.10), 0px 5px 12px 0px rgba(7 9 13 / 0.12)`
+            ? `0px 4px 6px 0px rgba(0, 0, 0, 0.15), 0px 1px 3px -1px rgba(0, 0, 0, 0.15), 0px 1px 1px 0px rgba(0, 0, 0, 0.06)`
             : `0px 0px 1px 0px rgba(7 9 13 / 0.00), 0px 2px 5px 0px rgba(7 9 13 / 0.35), 0px 5px 12px 0px rgba(7 9 13 / 0.50)`,
         type: "shadow",
         comment: "Used for dialog elements like modals and popovers.",
