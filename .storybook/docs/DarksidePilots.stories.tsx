28b56d22e411ebe629bd79a21d6a85f6f9b25c0a
Ken <ken.aleksander@gmail.com>
2025-02-04 15:43:07 +0100

    :memo: React-docs

--- .storybook/docs/DarksidePilots.stories.tsx
+++ .storybook/docs/DarksidePilots.stories.tsx
@@ -276,7 +276,7 @@ export const Setup = () => {
   );
 };
 
-export const Tokens = () => {
+export const ReactPackage = () => {
   return (
     <Box.New paddingBlock="space-48" asChild className="darkside-docs">
       <Page>
@@ -284,85 +284,12 @@ export const Tokens = () => {
           <BodyShort as="time">{`Last update: ${LAST_UPDATED}`}</BodyShort>
 
           <Heading size="xlarge" as="h1" spacing>
-            Setup
-          </Heading>
-        </Page.Block>
-        <Page.Block width="md" gutters>
-          <Heading size="large" as="h2" spacing>
-            Installation
-          </Heading>
-          <BodyLong spacing>
-            Here you have the basics to get started with the new system. We
-            provide more in-depth guides for each part in the sidebar.
-          </BodyLong>
-        </Page.Block>
-        <Page.Block width="md" gutters>
-          <Heading size="large" as="h2" spacing>
-            Design tokens
-          </Heading>
-          <Heading size="medium" as="h3" spacing>
-            Import-paths
-          </Heading>
-          <pre className="docs-pre">
-            <code className="docs-code">{`@navikt/ds-tokens/darkside-css
-@navikt/ds-tokens/darkside-scss
-@navikt/ds-tokens/darkside-less
-@navikt/ds-tokens/darkside-js`}</code>
-          </pre>
-          <BodyLong spacing>
-            The design-tokens are avaliable in multiple formats. You can import
-            them in your project by using the import-paths above. All of these
-            formats are based on CSS-variables to support theming and darkmode.
-          </BodyLong>
-          <BodyLong spacing weight="semibold">
-            Note: If using SCSS, LESS or JS, you will also have to import the
-            CSS-version tokens or they will not work. If this is not possible,
-            you can import the static version of the tokens. You will not get
-            theming or darkmode support with this version built in.
-          </BodyLong>
-          <pre className="docs-pre">
-            <code className="docs-code">{`@navikt/ds-tokens/darkside-scss-static
-@navikt/ds-tokens/darkside-less-static
-@navikt/ds-tokens/darkside-js-static`}</code>
-          </pre>
-        </Page.Block>
-        <Page.Block width="md" gutters>
-          <Heading size="large" as="h2" spacing>
-            @navikt/ds-tailwind
-          </Heading>
-          <Heading size="medium" as="h3" spacing>
-            Import-paths
-          </Heading>
-          <pre className="docs-pre">
-            <code className="docs-code">@navikt/ds-tailwind/darkside</code>
-          </pre>
-          <BodyLong spacing>
-            Replace the old <code>@navikt/ds-tailwind</code>-import with the
-            above in your config, and you now have the new tailwind-config in
-            your project.
-          </BodyLong>
-        </Page.Block>
-        <Page.Block width="md" gutters>
-          <Heading size="large" as="h2" spacing>
-            @navikt/ds-css
-          </Heading>
-          <Heading size="medium" as="h3" spacing>
-            Import-paths
-          </Heading>
-          <pre className="docs-pre">
-            <code className="docs-code">@navikt/ds-css/darkside</code>
-          </pre>
-          <BodyLong spacing>
-            Replace the old <code>@navikt/ds-css</code>-import with the above,
-            and you now have the new CSS-package in your project. Note that for
-            this to work in conjunction with <code>@navikt/ds-react</code>, you
-            will have to wrap your application in a <code>Theme</code>
-            -component.
-          </BodyLong>
-        </Page.Block>
-        <Page.Block width="md" gutters>
-          <Heading size="large" as="h2" spacing>
             @navikt/ds-react
+          </Heading>
+        </Page.Block>
+        <Page.Block width="md" gutters>
+          <Heading size="large" as="h2" spacing>
+            Setup
           </Heading>
           <pre className="docs-pre">
             <code className="docs-code">
@@ -386,6 +313,129 @@ export const Tokens = () => {
             can also change between <code>theme=light</code> and{" "}
             <code>theme=dark</code> when implementing theme-switching interally
             in your application.
+          </BodyLong>
+          <Heading size="large" as="h2" spacing>
+            Changes
+          </Heading>
+          <BodyLong spacing>
+            While most of these changes will not affect your application, we
+            list them here just in case.
+          </BodyLong>
+        </Page.Block>
+        {/*  */}
+        <Page.Block width="md" gutters>
+          <Heading size="medium" as="h3" spacing>
+            Accordion
+          </Heading>
+          <Heading size="small" as="h4" spacing>
+            Properties
+          </Heading>
+
+          <BodyLong spacing>
+            <code>variant</code> and <code>headingSize</code> are deprecated and
+            no longer supported. <code>headingSize=small</code> now defaults to{" "}
+            <code>xsmall</code> Heading and <code>headingSize=medium</code>{" "}
+            defaults <code>small</code> Heading.
+          </BodyLong>
+          <Heading size="small" as="h4" spacing>
+            Other changes
+          </Heading>
+
+          <BodyLong spacing>
+            <code>Accordion.Content</code> now has an extra nested{" "}
+            <code>div</code>, potentially breaking custom css overrides.
+          </BodyLong>
+        </Page.Block>
+        {/*  */}
+        <Page.Block width="md" gutters>
+          <Heading size="medium" as="h3" spacing>
+            Datepicker
+          </Heading>
+
+          <BodyLong spacing>
+            Weeknumber-button uses our own button-component, and no longer a
+            custom button.
+          </BodyLong>
+        </Page.Block>
+        <Page.Block width="md" gutters>
+          <Heading size="medium" as="h3" spacing>
+            GuidePanel
+          </Heading>
+
+          <BodyLong spacing>
+            Component got a complete overhaul. Custom overrides might break.
+          </BodyLong>
+        </Page.Block>
+        <Page.Block width="md" gutters>
+          <Heading size="medium" as="h3" spacing>
+            Popover, HelpText, Tooltip
+          </Heading>
+          <Heading size="small" as="h4" spacing>
+            Properties
+          </Heading>
+
+          <BodyLong spacing>
+            <code>arrow</code> is removed. All our floating dialog-elents now
+            come without arrow. If you have a custom <code>offset</code>, you
+            might have to update it.
+          </BodyLong>
+        </Page.Block>
+
+        <Page.Block width="md" gutters>
+          <Heading size="medium" as="h3" spacing>
+            Pagination
+          </Heading>
+
+          <BodyLong spacing>
+            Button now uses <code>tertiary-neutral</code> and not regular{" "}
+            <code>tertiary</code> variant.
+          </BodyLong>
+        </Page.Block>
+        <Page.Block width="md" gutters>
+          <Heading size="medium" as="h3" spacing>
+            Primitives
+          </Heading>
+
+          <BodyLong spacing>
+            Given that these are closely tied to our tokens, you will most
+            likely have to update parts of them to test the new system
+          </BodyLong>
+          <BodyLong spacing>
+            All primitives using our current <code>spacing</code>-tokens now has
+            the new <code>space</code>-tokens. You can use the old tokens while
+            testing, but the full release will remove them from the proplist
+          </BodyLong>
+
+          <pre className="docs-pre">
+            <code className="docs-code">
+              {`// Before
+<HStack gap="4">
+
+// After:
+<HStack gap="space-16">`}
+            </code>
+          </pre>
+
+          <Heading size="small" as="h4" spacing>
+            Page
+          </Heading>
+          <BodyLong spacing>
+            <code>background</code>-prop is deprecated and cannot be used with
+            the new system. Defaults to <code>bg-default</code>-token.
+          </BodyLong>
+          <Heading size="small" as="h4" spacing>
+            Box
+          </Heading>
+          <BodyLong spacing>
+            Since <code>{`<Box />`}</code> is directly tied to our tokens, we
+            now offer <code>{`<Box.New />`}</code> as an altenative. This
+            component is based on our new tokens and can use the backgrounds,
+            borders, border-radius and shadows from the new system.{" "}
+            <strong>
+              All instances of <code>{`<Box />`}</code> will break when using
+              the new system if the <code>background</code>, <code>shadow</code>{" "}
+              or <code>borderColor</code> properties are used.
+            </strong>
           </BodyLong>
         </Page.Block>
       </Page>
