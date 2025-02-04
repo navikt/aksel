918e29ef1b7eba513a6fbcd5684f868ac20ef4dd
Ken <ken.aleksander@gmail.com>
2025-02-04 20:40:18 +0100

    :test_tube: Setup chromatic-run for complete build

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,8 +3,8 @@ import { Preview } from "@storybook/react";
 import React, { useLayoutEffect } from "react";
 // @ts-expect-error - Temporary
 import darksideCss from "../@navikt/core/css/darkside/index.css?inline";
-// @ts-expect-error - Temporary
-import defaultCss from "../@navikt/core/css/index.css?inline";
+
+/* import defaultCss from "../@navikt/core/css/index.css?inline"; */
 import { Provider } from "../@navikt/core/react/src/provider";
 import { Theme } from "../@navikt/core/react/src/theme";
 import en from "../@navikt/core/react/src/util/i18n/locales/en";
@@ -15,12 +15,14 @@ import "./layout.css";
 const ModeDecorator = ({ children, mode, theme }) => {
   useLayoutEffect(() => {
     const style = document.createElement("style");
-    style.innerHTML = mode === "darkside" ? darksideCss : defaultCss;
+    /* style.innerHTML = mode === "darkside" ? darksideCss : defaultCss; */
+    style.innerHTML = darksideCss;
     document.head.appendChild(style);
 
-    if (mode === "darkside") {
+    /* if (mode === "darkside") {
       document.body.style.setProperty("background", "var(--ax-bg-default)");
-    }
+      } */
+    document.body.style.setProperty("background", "var(--ax-bg-default)");
 
     return () => {
       document.head.removeChild(style);
@@ -28,13 +30,19 @@ const ModeDecorator = ({ children, mode, theme }) => {
     };
   }, [mode]);
 
-  return mode === "darkside" ? (
+  return (
+    <Theme theme={theme || undefined} hasBackground={false}>
+      {children}
+    </Theme>
+  );
+
+  /* return mode === "darkside" ? (
     <Theme theme={theme || undefined} hasBackground={false}>
       {children}
     </Theme>
   ) : (
     children
-  );
+  ); */
 };
 
 const translations = {
