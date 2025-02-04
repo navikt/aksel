c3c345269676ac2e12c7d77d6b82692b49b85876
Vegard Haugstvedt <it.vegard@gmail.com>
2025-02-04 12:55:51 +0100

    Add "Add new activity" modal

--- examples/referansesider/src/routes/aktivitetsplan.tsx
+++ examples/referansesider/src/routes/aktivitetsplan.tsx
@@ -125,7 +125,9 @@ const AktivitetsplanPage = () => {
             </MainCard>
             <HStack align="center" justify="space-between" gap="space-16">
               <HStack gap="space-16">
-                <Button icon={<StarIcon />}>Legg til aktivitet</Button>
+                <Button as={Link} to="/aktivitetsplan/ny" icon={<StarIcon />}>
+                  Legg til aktivitet
+                </Button>
                 <Button variant="secondary">Filtrer</Button>
               </HStack>
               <Select label="Velg periode" hideLabel>
