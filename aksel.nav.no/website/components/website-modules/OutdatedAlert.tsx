510979f8d8726ddc687f052094174114d57c33c8
Julian Nymark <julian.nymark@nav.no>
2025-02-12 14:23:51 +0100

    Add more info on how to update an article

--- aksel.nav.no/website/components/website-modules/OutdatedAlert.tsx
+++ aksel.nav.no/website/components/website-modules/OutdatedAlert.tsx
@@ -1,5 +1,5 @@
 import { HourglassBottomFilledIcon } from "@navikt/aksel-icons";
-import { BodyLong, Button, Heading } from "@navikt/ds-react";
+import { BodyLong, Button, Heading, Link } from "@navikt/ds-react";
 import { useSanityData } from "@/hooks/useSanityData";
 
 export default function OutdatedAlert() {
@@ -20,9 +20,13 @@ export default function OutdatedAlert() {
         </BodyLong>
         {ctx?.validUser && (
           <>
+            <BodyLong className="mb-2 mt-2">
+              <Link href="https://aksel.nav.no/side/skriv-for-aksel#a5b79ddd59da">
+                Hvordan oppdaterer man innhold i Sanity?
+              </Link>
+            </BodyLong>
             <BodyLong className="mb-2">
-              Du ser også dette fordi du kan redigere denne artikkelen. Har du
-              lyst til å kontrollere innholdet nå?
+              Har du lyst til å kontrollere innholdet nå?
             </BodyLong>
             <Button
               as="a"
