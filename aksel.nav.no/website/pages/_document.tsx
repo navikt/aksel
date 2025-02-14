68ab8e12c844ecf4ba42be91d8069b1d933aeed5
Julian Nymark <julian.nymark@nav.no>
2025-02-14 14:05:49 +0100

    :construction: test data-tag, set no auto tracking

--- aksel.nav.no/website/pages/_document.tsx
+++ aksel.nav.no/website/pages/_document.tsx
@@ -1,8 +1,19 @@
 // This is imported to allow the Output File Tracing feature of Next.js to work correctly with the log patcher
 import "next-logger";
 import { Head, Html, Main, NextScript } from "next/document";
+import { useEffect, useState } from "react";
 
 export default function Document() {
+  const [umamiTag, setUmamiTag] = useState<string>();
+
+  useEffect(() => {
+    const isProdUrl = () => window.location.host === "aksel.nav.no";
+    const isPreview = () => !!document.getElementById("exit-preview-id");
+
+    setUmamiTag(
+      isPreview() ? "preview" : isProdUrl() ? "production" : "development",
+    );
+  }, []);
   return (
     <Html lang="no">
       <Head>
@@ -19,6 +30,8 @@ export default function Document() {
           src="https://cdn.nav.no/team-researchops/sporing/sporing.js"
           data-host-url="https://umami.nav.no"
           data-website-id="7b9fb2cd-40f4-4a30-b208-5b4dba026b57"
+          data-auto-track="false"
+          data-tag={umamiTag}
         ></script>
       </Head>
       <body data-theme="light" className="aksel antialiased">
