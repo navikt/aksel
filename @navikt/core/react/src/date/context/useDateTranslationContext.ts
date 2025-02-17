821343b67d702379013ceba5961a5872dca4fd1f
Ken <26967723+KenAJoh@users.noreply.github.com>
2025-02-17 09:46:40 +0100

    [Datepicker] Bumped `react-day-picker` to v9.  (#3525)
    * :memo: Bumped date-fns to v4
    
    * TEMP UPDATES
    
    * TEMP CSS UPDATES
    
    * :sparkles: New Caption component for datepicker
    
    * :recycle: Extract util
    
    * :recycle: Refactor standalone datepicker
    
    * :recycle: Refactor dialog datepicker
    
    * :recycle: Migrated weekday-click
    
    * :recycle: Migrated mobile-view week-click
    
    * :coffin: Removed old code
    
    * :truck: Re-strucutre project
    
    * :bug: Fix imports
    
    * :recycle: New MonthPicker context
    
    * :recycle: Replace use to MonthPickerContext
    
    * :recycle: Migrated standalone monthpicker
    
    * :recycle: Remove react-day-picker connection for utils
    
    * :truck: Re-structure MonthPicker
    
    * :memo: rename context
    
    * :truck: Re-structure DatePicker
    
    * :bug: Fix imports and nb
    
    * :recycle: Parts restructure
    
    * :truck: Move inputcontext
    
    * :truck: Co-locate date-spesific context
    
    * :truck: Co-locate date-hooks
    
    * :bug: Update missing import-updates
    
    * :bug: Use new input-context in modal, popover
    
    * :truck: Moved new utils into date-utils
    
    * :recycle: simplified calendar-range logic
    
    * :bug: Fix test-runs
    
    * :test_tube: Storybook tests for monthpicker
    
    * :test_tube: Storybook tests for monthpicker
    
    * :lipstick: Lock datepicker dialog to top of screen
    
    * :bug: Don't re-open modal
    
    * :bug: Avoid double open-toggle
    
    * :test_tube: Storybook tests for DatePicker
    
    * :recycle: Use local dateRange-handler
    
    * :bug: month-update caused custom-components to re-render
    
    * :bug: casings
    
    * :bug: casings 2
    
    * :bug: casings 3
    
    * :bug: casings 4
    
    * :test_tube: Added tests for legacy dom-structure
    
    * :bug: Resolved re-render causing tests to fail
    
    * :test_tube: More datepicker tests, daypicker now requires us to handle focus
    
    * :memo: Changeset
    
    * Update @navikt/core/react/src/date/datepicker/parts/DatePicker.RDP.tsx
    
    * :bug: Added weekend-className for backwards compat support
    
    * :memo: yarn lock sync
    
    * :bug: Allow defaultMonth-prop in Datepicker

--- @navikt/core/react/src/date/context/useDateTranslationContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "../../util/create-context";
-import { TFunction } from "../../util/i18n/i18n.types";
-
-interface DateTranslationContextProps {
-  translate: TFunction<"DatePicker">;
-}
-
-export const [DateTranslationContextProvider, useDateTranslationContext] =
-  createContext<DateTranslationContextProps>();
