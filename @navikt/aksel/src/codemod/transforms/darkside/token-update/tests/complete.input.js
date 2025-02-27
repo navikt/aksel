75e1a3a773a6dc981067a02965ec9c1e89bb759a
Ken <ken.aleksander@gmail.com>
2025-02-27 20:59:43 +0100

    :bug: Revert mistakes

--- @navikt/aksel/src/codemod/transforms/darkside/token-update/tests/complete.input.js
+++ @navikt/aksel/src/codemod/transforms/darkside/token-update/tests/complete.input.js
@@ -1,8 +1,8 @@
 export const testComponent = () => {
   return (
     <div
-      className="test bg-(--ax-bg-neutral-strong)"
-      style={{ backgroundColor: "var(--ax-bg-neutral-soft)" }}
+      className="test bg-(--a-surface-neutral)"
+      style={{ backgroundColor: "var(--a-surface-subtle)" }}
     >
       Test
     </div>
--- @navikt/aksel/src/codemod/transforms/v2.0.0/update-css-tokens/tests/complete.output.js
+++ @navikt/aksel/src/codemod/transforms/v2.0.0/update-css-tokens/tests/complete.output.js
@@ -1,16 +1,16 @@
 const StyledDiv = styled`
---v2-migration__navds-global-color-red-100: var(--ax-success-200);
+--v2-migration__navds-global-color-red-100: var(--a-green-100);
 --v2-migration__navds-global-color-blue-100: red;
 
 .test{
-  color: var(--ax-accent-200);
+  color: var(--a-blue-100);
 }
 `;
 
 <style>
   {`
   .test{
-    color: var(--ax-accent-200);
+    color: var(--a-blue-100);
   }
   `}
 </style>;
