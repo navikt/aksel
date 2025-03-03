56d3758598ef8e1a343a78c54a26dcebd50d1ed3
Ken <ken.aleksander@gmail.com>
2025-03-03 20:18:14 +0100

    :recycle: Cookie-tracking refactor

--- aksel.nav.no/website/components/website-modules/Umami.tsx
+++ aksel.nav.no/website/components/website-modules/Umami.tsx
@@ -1,15 +1,42 @@
 "use client";
 
+import getConfig from "next/config";
+import Script from "next/script";
+import { useEffect, useState } from "react";
+import { useCookies } from "./CookieProvider";
+
+export const Umami = () => {
+  const trackingId = getConfig().publicRuntimeConfig.UMAMI_TRACKING_ID;
+  const [umamiTag, setUmamiTag] = useState<string | undefined>();
+  const { consent } = useCookies();
+
+  useEffect(() => {
+    setUmamiTag(classifyTraffic());
+  }, []);
+
+  /* We only track with umami if optional cookies are accepted */
+  if (consent !== "accepted") {
+    return null;
+  }
+
+  return (
+    <Script
+      defer
+      src="https://cdn.nav.no/team-researchops/sporing/sporing.js"
+      data-host-url="https://umami.nav.no"
+      data-website-id={trackingId}
+      data-tag={umamiTag}
+    />
+  );
+};
+
 /**
  * Classifies the current traffic as either "organic" or "polluted"
  *
  * Organic traffic: Production traffic from real users on the main site
  * Polluted traffic: Traffic from preview environments, example pages, templates, admin pages, or non-production environments
- *
- * @returns {"organic" | "polluted"} Traffic classification
  */
-
-export const classifyTraffic = () => {
+function classifyTraffic() {
   const isProdUrl = () => window.location.host === "aksel.nav.no";
   const isPreview = () => !!document.getElementById("exit-preview-id");
   const isExample = () => window.location.pathname.startsWith("/eksempler/");
@@ -26,4 +53,4 @@ export const classifyTraffic = () => {
     return "organic";
   }
   return "polluted";
-};
+}
