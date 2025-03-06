25ffe42f66878a6035338ac0f88fbfbfdb066a1c
Ken <ken.aleksander@gmail.com>
2025-03-06 21:55:36 +0100

    :memo: Moved css, less, scss tests

--- @navikt/aksel/src/darkside/transforms/tests/darkside-tokens.test.ts
+++ @navikt/aksel/src/darkside/transforms/tests/darkside-tokens.test.ts
@@ -8,3 +8,36 @@ for (const fixture of ["js-replace-all", "js-replace-some"]) {
     extension: "js",
   });
 }
+
+/* SCSS transforms */
+for (const fixture of ["scss-complete"]) {
+  check(__dirname, {
+    fixture,
+    migration: "darkside-tokens-scss",
+    extension: "scss",
+  });
+}
+
+/* LESS transforms */
+for (const fixture of ["less-complete"]) {
+  check(__dirname, {
+    fixture,
+    migration: "darkside-tokens-less",
+    extension: "less",
+  });
+}
+
+/* CSS transforms */
+for (const fixture of ["css-complete"]) {
+  check(__dirname, {
+    fixture,
+    migration: "darkside-tokens-css",
+    extension: "css",
+  });
+
+  check(__dirname, {
+    fixture,
+    migration: "darkside-tokens-css",
+    extension: "js",
+  });
+}
