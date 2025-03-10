ba07f059eb8bd4b5f7e538a727784da1de7ba8df
Ken <ken.aleksander@gmail.com>
2025-03-10 23:12:53 +0100

    :sparkles: Setup new sidebar

---
+++ aksel.nav.no/website/components/layout/sidebar-v2/Sidebar.tsx
@@ -0,0 +1,5 @@
+function Sidebar() {
+  return <div>123</div>;
+}
+
+export { Sidebar };
--- aksel.nav.no/website/components/layout/templates/WithSidebar.tsx
+++ aksel.nav.no/website/components/layout/templates/WithSidebar.tsx
@@ -2,11 +2,12 @@ import cl from "clsx";
 import Image from "next/legacy/image";
 import NextLink from "next/link";
 import { Box, Detail, Heading, Link } from "@navikt/ds-react";
-import Sidebar from "@/layout/sidebar/Sidebar";
+import SidebarOld from "@/layout/sidebar/Sidebar";
 import { urlFor } from "@/sanity/interface";
 import { SidebarT, TableOfContentsT } from "@/types";
 import { capitalize } from "@/utils";
 import TableOfContents from "@/web/toc/TableOfContents";
+import { Sidebar } from "../sidebar-v2/Sidebar";
 
 export const WithSidebar = ({
   children,
@@ -39,7 +40,8 @@ export const WithSidebar = ({
       className="min-h-screen-header"
     >
       <div className="mx-auto flex w-full max-w-screen-2xl gap-6">
-        <Sidebar kategori={pageType.type} links={sidebar} />
+        {/* <SidebarOld kategori={pageType.type} links={sidebar} /> */}
+        <Sidebar />∏
         <main
           tabIndex={-1}
           id="hovedinnhold"
