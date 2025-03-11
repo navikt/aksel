c14a90235257bbfc71bc2ede0bf186340dfb1158
Ken <ken.aleksander@gmail.com>
2025-03-11 10:07:38 +0100

    :lipstick: Styled dropdown button

--- aksel.nav.no/website/components/layout/sidebar-v2/Sidebar.tsx
+++ aksel.nav.no/website/components/layout/sidebar-v2/Sidebar.tsx
@@ -1,5 +1,5 @@
 import cl from "clsx";
-import React, { useState } from "react";
+import React, { useId, useState } from "react";
 import { ChevronDownIcon } from "@navikt/aksel-icons";
 import { Detail } from "@navikt/ds-react";
 import { SidebarT } from "@/types";
@@ -17,40 +17,51 @@ function Sidebar(props: SidebarProps) {
     <nav
       {...rest}
       aria-label="Sidemeny"
-      className={cl(className, "w-sidebar shrink-0 self-start px-2")}
+      className={cl(className, "relative w-sidebar shrink-0 self-start px-2")}
     >
       <ul className="space-y-3">
         {sidebarData.map((section, index) => {
+          // eslint-disable-next-line react-hooks/rules-of-hooks
+          const id = useId();
           return (
             <React.Fragment key={section.label}>
               <li>
                 <Detail
+                  as="div"
                   weight="semibold"
-                  className="text-ax-text-neutral-subtle py-0.5 pl-2"
+                  className="py-0.5 pl-2 text-ax-text-neutral-subtle"
+                  id={id}
                 >
                   {section.label}
                 </Detail>
+                <ul aria-labelledby={id}>
+                  {section.links.map((link) => {
+                    // eslint-disable-next-line react-hooks/rules-of-hooks
+                    const [open, setOpen] = useState(false);
+                    return (
+                      <li key={link.title}>
+                        <button
+                          onClick={() => setOpen(!open)}
+                          className="flex w-full items-center justify-between self-stretch rounded-medium py-1 pl-2 pr-1 text-medium leading-5 hover:bg-ax-bg-neutral-moderate-hoverA"
+                          aria-expanded={open}
+                        >
+                          {link.title}
+                          <ChevronDownIcon aria-hidden />
+                        </button>
+                        <ul hidden={!open}>
+                          {link.pages.map((page) => {
+                            return <li key={page.heading}>{page.heading}</li>;
+                          })}
+                        </ul>
+                      </li>
+                    );
+                  })}
+                </ul>
               </li>
-              {section.links.map((link) => {
-                const [open, setOpen] = useState(false);
-                return (
-                  <li key={link.title}>
-                    <button onClick={() => setOpen(!open)}>
-                      {link.title}
-                      <ChevronDownIcon aria-hidden />
-                    </button>
-                    <ul hidden={!open}>
-                      {link.pages.map((page) => {
-                        return <li key={page.heading}>{page.heading}</li>;
-                      })}
-                    </ul>
-                  </li>
-                );
-              })}
               {index !== sidebarData.length - 1 && (
                 <li
                   aria-hidden
-                  className="w-ful border-ax-border-neutral-subtle h-px border-t"
+                  className="w-ful h-px border-t border-ax-border-neutral-subtle"
                 />
               )}
             </React.Fragment>
