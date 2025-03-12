9767367ff58805366adbcfe85bbf73a68a706e2f
Ken <ken.aleksander@gmail.com>
2025-03-12 12:51:28 +0100

    :recycle: Revert to legacy sidebar for prod

--- aksel.nav.no/website/components/layout/templates/WithSidebar.tsx
+++ aksel.nav.no/website/components/layout/templates/WithSidebar.tsx
@@ -1,14 +1,12 @@
 import cl from "clsx";
 import Image from "next/legacy/image";
 import NextLink from "next/link";
-import { Box, Detail, Heading, Link, Show } from "@navikt/ds-react";
-
-/* import SidebarOld from "@/layout/sidebar/Sidebar"; */
+import { Box, Detail, Heading, Link } from "@navikt/ds-react";
+import SidebarOld from "@/layout/sidebar/Sidebar";
 import { urlFor } from "@/sanity/interface";
 import { SidebarT, TableOfContentsT } from "@/types";
 import { capitalize } from "@/utils";
 import { TableOfContents } from "@/web/toc/TableOfContents";
-import { Sidebar } from "../sidebar-v2/Sidebar";
 
 export const WithSidebar = ({
   children,
@@ -41,8 +39,9 @@ export const WithSidebar = ({
       className="min-h-screen-header"
     >
       <div className="mx-auto flex w-full max-w-screen-2xl gap-6">
-        {/* <SidebarOld kategori={pageType.type} links={sidebar} /> */}
-        <Show asChild above="md">
+        <SidebarOld kategori={pageType.type} links={sidebar} />
+        {/* New sidebar implementation demo */}
+        {/* <Show asChild above="md">
           <Sidebar
             sidebarData={[
               { label: "Grunnleggende", links: sidebar },
@@ -56,7 +55,7 @@ export const WithSidebar = ({
               },
             ]}
           />
-        </Show>
+        </Show> */}
         <main
           tabIndex={-1}
           id="hovedinnhold"
