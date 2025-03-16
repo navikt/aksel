7dfdc74138cbe0d442b981e863db3f211af75d30
Ken <ken.aleksander@gmail.com>
2025-03-16 01:46:02 +0100

    ✨ Refactor GlobalSearchHitCollection for improved styling and accessibility

--- aksel.nav.no/website/app/_ui/global-search/GlobalSearch.hit.tsx
+++ aksel.nav.no/website/app/_ui/global-search/GlobalSearch.hit.tsx
@@ -1,22 +1,31 @@
 "use client";
 
-import cl from "clsx";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Heading } from "@navikt/ds-react";
-import { Tag } from "@/cms/frontpage-blocks/latest-articles/Tag";
 import { urlFor } from "@/sanity/interface";
 import { StatusTag } from "@/web/StatusTag";
 import { SearchHitT, SearchResultPageTypesT } from "./GlobalSearch.config";
 import styles from "./GlobalSearch.module.css";
+
+/* TODO: Move this to global Aksel-config */
+const doctypeToRole: Record<SearchResultPageTypesT, string> = {
+  ds_artikkel: "brand-blue",
+  komponent_artikkel: "brand-blue",
+  templates_artikkel: "brand-blue",
+  aksel_artikkel: "teal",
+  aksel_blogg: "pink",
+  aksel_prinsipp: "neutral",
+  aksel_standalone: "neutral",
+};
 
 function GlobalSearchHitCollection({
   heading,
   searchHits,
   tag,
 }: {
-  heading?: React.ReactNode;
+  heading: React.ReactNode;
   searchHits: SearchHitT[];
   tag?: Partial<SearchResultPageTypesT>;
 }) {
@@ -26,31 +35,14 @@ function GlobalSearchHitCollection({
 
   return (
     <div>
-      {heading && tag && (
-        <h2
-          className={cl("top-0 z-10 p-2 px-6 md:sticky md:px-10", {
-            "bg-deepblue-50": ["komponent_artikkel", "ds_artikkel"].includes(
-              tag,
-            ),
-            "bg-violet-50": ["aksel_prinsipp", "aksel_standalone"].includes(
-              tag,
-            ),
-            "bg-teal-50": tag === "aksel_artikkel",
-            "bg-pink-50": tag === "aksel_blogg",
-          })}
-        >
-          <Tag hTag="span" type={tag} size="small" inline />
-        </h2>
-      )}
-      {heading && !tag && (
-        <Heading
-          className="top-0 z-10 bg-surface-subtle p-2 px-6 md:sticky md:px-10"
-          size="small"
-          level="2"
-        >
-          {heading}
-        </Heading>
-      )}
+      <Heading
+        className={styles.searchSectionHeading}
+        size="small"
+        level="2"
+        data-color-role={doctypeToRole[tag ?? ""] ?? "neutral"}
+      >
+        {heading}
+      </Heading>
       <ul>
         {searchHits.map((x, xi) => (
           <GlobalSearchLink key={xi} hit={x} tag={tag} />
