8db6965ba754e6d96e0f75b864b75f7b981c2b51
Ken <ken.aleksander@gmail.com>
2025-03-16 20:46:10 +0100

    :fire: removed browser-token

--- aksel.nav.no/website/app/_sanity/live.ts
+++ aksel.nav.no/website/app/_sanity/live.ts
@@ -3,7 +3,6 @@
 import { defineLive } from "next-sanity";
 import { client } from "./client";
 
-// set your viewer token
 const token = process.env.SANITY_READ;
 if (!token) {
   throw new Error("Missing SANITY_READ");
@@ -12,5 +11,4 @@ if (!token) {
 export const { sanityFetch, SanityLive } = defineLive({
   client,
   serverToken: token,
-  browserToken: token,
 });
