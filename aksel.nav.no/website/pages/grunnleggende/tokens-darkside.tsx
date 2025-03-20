24fe155b1914abc32ec60766516c21f34c558fae
Vegard Haugstvedt <it.vegard@gmail.com>
2025-03-20 09:31:05 +0100

    Extract Color token example

--- aksel.nav.no/website/pages/grunnleggende/tokens-darkside.tsx
+++ aksel.nav.no/website/pages/grunnleggende/tokens-darkside.tsx
@@ -6,7 +6,6 @@ import {
   MobileIcon,
   MobileSmallIcon,
   MonitorIcon,
-  PaletteIcon,
   SpaceHorizontalIcon,
   TabletIcon,
 } from "@navikt/aksel-icons";
@@ -43,6 +42,7 @@ import { generateSidebar } from "@/utils";
 // import { TextWithMarkdown } from "@/web/TextWithMarkdown";
 import { SEO } from "@/web/seo/SEO";
 import { TableOfContents } from "@/web/toc/TableOfContents";
+import ColorToken from "../../components/token-docs/token/example/ColorToken";
 import Toolbar from "../../components/token-docs/toolbar/Toolbar";
 
 // import { grunnleggendeKategorier } from "../../sanity/config";
@@ -98,66 +98,6 @@ const ExampleContainer = ({
     {children}
   </Box>
 );
-
-const ColorToken = ({ token }: { token: any }) => {
-  switch (token.category) {
-    case "backgroundColor":
-      return (
-        <Box
-          borderColor="border-subtle"
-          borderRadius="full"
-          borderWidth="1"
-          style={{ backgroundColor: token.value }}
-          width="32px"
-          height="32px"
-        />
-      );
-    case "borderColor":
-      return (
-        <Box
-          borderRadius="full"
-          borderWidth="4"
-          width="32px"
-          height="32px"
-          style={{ borderColor: token.value }}
-        />
-      );
-    case "textColor": {
-      if (token.modifier === "icon") {
-        return (
-          <Box width="32px" height="32px">
-            <VStack as="div" align="center" justify="center" height="100%">
-              <PaletteIcon
-                title="a11y-title"
-                color={token.value}
-                fontSize="1.5rem"
-              />
-            </VStack>
-          </Box>
-        );
-      }
-      return (
-        <Box width="32px" height="32px">
-          <VStack as="div" align="center" justify="center" height="100%">
-            <Heading size="medium" style={{ color: token.value }}>
-              Aa
-            </Heading>
-          </VStack>
-        </Box>
-      );
-    }
-    default:
-      return (
-        <Box
-          borderRadius="medium"
-          borderWidth="1"
-          background={token.value}
-          width="32px"
-          height="32px"
-        />
-      );
-  }
-};
 
 const FontToken = ({ token }: { token: (typeof tokenDocs)[number] }) => {
   let fontStyling = {
