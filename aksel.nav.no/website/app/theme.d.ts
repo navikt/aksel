d77e7e203e522ed3a7fcdef5ca6abe6169314fb6
Ken <26967723+KenAJoh@users.noreply.github.com>
2025-03-28 12:10:47 +0100

    :lipstick: Updated global meta-purple color for darkside (#3703)

--- aksel.nav.no/website/app/theme.d.ts
+++ aksel.nav.no/website/app/theme.d.ts
@@ -1,10 +1,7 @@
 import "react";
 import type { GlobalColorRoles } from "@navikt/ds-tokens/types";
 
-type AkselBrandColors =
-  | "aksel-brand-teal"
-  | "aksel-brand-violet"
-  | "aksel-brand-pink";
+type AkselBrandColors = "aksel-brand-teal" | "aksel-brand-pink";
 
 declare module "react" {
   interface HTMLAttributes {
