cf6833818e40607421be5346142dd04eb91c99f2
Ken <ken.aleksander@gmail.com>
2025-03-31 14:35:16 +0200

    :tada: Added metadata for changelog view support in mønster pages

--- aksel.nav.no/website/app/dev/(designsystemet)/monster-maler/[...slug]/page.tsx
+++ aksel.nav.no/website/app/dev/(designsystemet)/monster-maler/[...slug]/page.tsx
@@ -1,12 +1,18 @@
 import { PortableTextBlock } from "next-sanity";
 import { notFound } from "next/navigation";
+import { Heading } from "@navikt/ds-react";
 import { sanityFetch } from "@/app/_sanity/live";
 import {
   MONSTER_MALER_BY_SLUG_QUERY,
   TOC_BY_SLUG_QUERY,
 } from "@/app/_sanity/queries";
 import { CustomPortableText } from "@/app/_ui/portable-text/CustomPortableText";
+import styles from "@/app/_ui/portable-text/CustomPortableText.module.css";
 import { TableOfContents } from "@/app/_ui/toc/TableOfContents";
+import {
+  WebsiteTable,
+  WebsiteTableRow,
+} from "@/app/_ui/website-table/WebsiteTable";
 import {
   DesignsystemetPageHeader,
   DesignsystemetPageLayout,
@@ -42,6 +48,9 @@ export default async function Page({ params }: Props) {
     notFound();
   }
 
+  const metadata = page.content?.find((x) => x._type === "kode_eksempler")?.dir
+    ?.metadata;
+
   return (
     <DesignsystemetPageLayout layout="with-toc">
       <DesignsystemetPageHeader data={page} />
@@ -56,6 +65,36 @@ export default async function Page({ params }: Props) {
         value={page.content as PortableTextBlock[]}
         data-block-margin="space-28"
       />
+      {metadata?.changelog && (
+        <div>
+          <Heading
+            className={styles.headingElement}
+            tabIndex={-1}
+            id="changelog"
+            level="2"
+            size="large"
+            data-level="2"
+          >
+            Endringer
+          </Heading>
+          <WebsiteTable
+            th={[{ text: "Dato" }, { text: "Versjon" }, { text: "Endringer" }]}
+          >
+            {metadata.changelog
+              .sort((a, b) => (a.version ?? 0) - (b.version ?? 0))
+              .map((log) => (
+                <WebsiteTableRow
+                  key={log.version}
+                  tr={[
+                    { text: log.date },
+                    { text: log.version },
+                    { text: log.description },
+                  ]}
+                />
+              ))}
+          </WebsiteTable>
+        </div>
+      )}
     </DesignsystemetPageLayout>
   );
 }
