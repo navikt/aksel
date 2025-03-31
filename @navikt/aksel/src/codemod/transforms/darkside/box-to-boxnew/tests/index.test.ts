ff306016b3826d5f68d90311d5ab32c0e491af64
Ken <ken.aleksander@gmail.com>
2025-03-31 10:25:34 +0200

    :test_tube: Added more tests

--- @navikt/aksel/src/codemod/transforms/darkside/box-to-boxnew/tests/index.test.ts
+++ @navikt/aksel/src/codemod/transforms/darkside/box-to-boxnew/tests/index.test.ts
@@ -1,7 +1,7 @@
 import { check } from "../../../../utils/check";
 
 const migration = "box-to-boxnew";
-const fixtures = ["box", "import", "import2"];
+const fixtures = ["box", "import", "import2", "idempotent", "idempotent-2"];
 
 for (const fixture of fixtures) {
   check(__dirname, {
