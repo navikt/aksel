97c04c6ae4f15366d6be0e0ea2a86814714bdb03
Julian Nymark <julian.nymark@nav.no>
2025-04-01 12:31:33 +0200

    :construction: query quirks

--- aksel.nav.no/website/app/dev/produktbloggen/[...slug]/page.tsx
+++ aksel.nav.no/website/app/dev/produktbloggen/[...slug]/page.tsx
@@ -1,3 +1,6 @@
+import { notFound } from "next/navigation";
+import { sanityFetch } from "@/app/_sanity/live";
+import { BLOGG_BY_SLUG_QUERY } from "@/app/_sanity/queries";
 import { parseDesignsystemSlug } from "../../(designsystemet)/slug";
 
 type Props = {
@@ -9,6 +12,21 @@ export default async function Page({ params }: Props) {
   const { slug } = await params;
 
   const parsedSlug = parseDesignsystemSlug(slug, "produktbloggen");
+
+  console.log({ parsedSlug });
+
+  const { data: page } = await sanityFetch({
+    query: BLOGG_BY_SLUG_QUERY,
+    params: { slug: parsedSlug },
+    stega: false,
+    perspective: "published",
+  });
+
+  console.log({ page });
+
+  if (!page?._id) {
+    notFound();
+  }
 
   return <div>page for {parsedSlug}</div>;
 }
