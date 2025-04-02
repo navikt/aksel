63c6cf3b611674d0dd384f31c755544ec314a1ab
Ken <ken.aleksander@gmail.com>
2025-04-02 13:57:20 +0200

    :recycle: Use inline head to set local styles instead of css-modules

--- aksel.nav.no/website/app/_ui/theming/Theme.button.tsx
+++ aksel.nav.no/website/app/_ui/theming/Theme.button.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { useTheme } from "next-themes";
+import Head from "next/head";
 import { useMemo } from "react";
 import { MoonIcon, SunIcon } from "@navikt/aksel-icons";
 import { Button } from "@navikt/ds-react";
-import styles from "./Theme.module.css";
 
 function ThemeButton() {
   const { theme, setTheme } = useTheme();
@@ -12,20 +12,38 @@ function ThemeButton() {
   const Icon = useMemo(() => {
     return theme === "light" ? (
       <MoonIcon
-        className={styles.themeIconDark}
+        style={{ display: "var(--website-theme-toggle-dark-display)" }}
         title="Endre til mørkt modus"
       />
     ) : (
-      <SunIcon className={styles.themeIconLight} title="Endre til lyst modus" />
+      <SunIcon
+        style={{ display: "var(--website-theme-toggle-light-display)" }}
+        title="Endre til lyst modus"
+      />
     );
   }, [theme]);
 
   return (
-    <Button
-      variant="tertiary-neutral"
-      icon={Icon}
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-    />
+    <>
+      <Head>
+        <style>{`
+        :root, .light {
+          --website-theme-toggle-light-display: block;
+          --website-theme-toggle-dark-display: none;
+        }
+
+        .dark {
+          --website-theme-toggle-light-display: none;
+          --website-theme-toggle-dark-display: block;
+        }
+      `}</style>
+      </Head>
+      <Button
+        variant="tertiary-neutral"
+        icon={Icon}
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      />
+    </>
   );
 }
 
