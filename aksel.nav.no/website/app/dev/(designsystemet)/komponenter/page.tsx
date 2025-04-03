5f028d86653a860bfc96f5c1bb9d4df0276c56ce
Ken <ken.aleksander@gmail.com>
2025-04-03 13:27:42 +0200

    :lipstick: Align overview heading with regular page heading

--- aksel.nav.no/website/app/dev/(designsystemet)/komponenter/page.tsx
+++ aksel.nav.no/website/app/dev/(designsystemet)/komponenter/page.tsx
@@ -2,8 +2,7 @@ import NextImage from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import type { Image } from "sanity";
-import { ChevronRightIcon } from "@navikt/aksel-icons";
-import { BodyLong, BodyShort, HStack, Heading, Tag } from "@navikt/ds-react";
+import { BodyShort, Heading, Tag } from "@navikt/ds-react";
 import { sanityFetch } from "@/app/_sanity/live";
 import { KOMPONENTOVERSIKT_QUERY } from "@/app/_sanity/queries";
 import { KOMPONENTOVERSIKT_QUERYResult } from "@/app/_sanity/query-types";
@@ -12,7 +11,6 @@ import { getStatusTag } from "@/app/_ui/theming/theme-config";
 import pagestyles from "../_ui/Designsystemet.module.css";
 import { DesignsystemetPageLayout } from "../_ui/DesignsystemetPage";
 import styles from "../_ui/Overview.module.css";
-import { OverviewPageToggle } from "../_ui/OverviewPageToggle";
 
 /* export async function generateMetadata(
   { params }: Props,
@@ -41,23 +39,13 @@ import { OverviewPageToggle } from "../_ui/OverviewPageToggle";
 } */
 
 /* https://nextjs.org/docs/app/api-reference/file-conventions/page#props */
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-}) {
+export default async function Page() {
   const { data: components } = await sanityFetch({
     query: KOMPONENTOVERSIKT_QUERY,
   });
 
   if (!components || components.length === 0) {
     notFound();
-  }
-
-  let { sort = "grid" } = await searchParams;
-
-  if (typeof sort !== "string") {
-    sort = "grid";
   }
 
   const list = sortOverview(components);
@@ -76,82 +64,16 @@ export default async function Page({
           Dette er generiske komponenter som passer til det meste. Både åpne
           sider og interne ekspertsystemer.
         </BodyShort>
-        <HStack marginBlock="space-24 space-48" justify="end">
-          <OverviewPageToggle value={sort} />
-        </HStack>
       </div>
 
-      {sort === "grid" ? (
-        <ul className={styles.overviewGrid}>
-          {list.map((component) => (
-            <li key={component.heading} className={styles.overviewLi}>
-              <OverviewCard page={component} />
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <ul className={styles.overviewList}>
-          {list.map((component) => (
-            <li key={component.heading} className={styles.overviewLi}>
-              <OverviewInlineCard page={component} />
-            </li>
-          ))}
-        </ul>
-      )}
+      <ul className={styles.overviewGrid}>
+        {list.map((component) => (
+          <li key={component.heading} className={styles.overviewLi}>
+            <OverviewCard page={component} />
+          </li>
+        ))}
+      </ul>
     </DesignsystemetPageLayout>
-  );
-}
-
-function OverviewInlineCard({
-  page,
-}: {
-  page: KOMPONENTOVERSIKT_QUERYResult[number];
-}) {
-  const imageUrl = urlForImage(page.status?.bilde as Image)?.url();
-
-  /* TODO: Add fallback to svg or something */
-  if (!imageUrl) {
-    return null;
-  }
-
-  const statusTag = getStatusTag(page.status?.tag);
-  const statusTagWithoutStable = getStatusTag(page.status?.tag, true);
-
-  return (
-    <Link
-      href={`/${page?.slug}`}
-      prefetch={false}
-      data-color-role={statusTag?.colorRole}
-      className={styles.overviewInlineCardLink}
-    >
-      <span className={styles.overviewInlineCardImageWrapper}>
-        <NextImage
-          src={imageUrl}
-          width="200"
-          height="200"
-          layout="fixed"
-          objectFit="contain"
-          alt={page?.heading + " thumbnail"}
-          aria-hidden
-        />
-      </span>
-      <div className={styles.overviewInlineCardContent}>
-        <Heading as="span" size="small" className={styles.overviewCardHeading}>
-          <span>{page?.heading}</span>
-          {statusTagWithoutStable && (
-            <Tag
-              size="small"
-              variant="success"
-              data-color-role={statusTag?.colorRole}
-            >
-              {statusTagWithoutStable.text}
-            </Tag>
-          )}
-        </Heading>
-        {page.description && <BodyLong>{page.description}</BodyLong>}
-      </div>
-      <ChevronRightIcon aria-hidden fontSize="1.5rem" />
-    </Link>
   );
 }
 
@@ -161,11 +83,6 @@ function OverviewCard({
   page: KOMPONENTOVERSIKT_QUERYResult[number];
 }) {
   const imageUrl = urlForImage(page.status?.bilde as Image)?.url();
-
-  /* TODO: Add fallback to svg or something */
-  if (!imageUrl) {
-    return null;
-  }
 
   const statusTag = getStatusTag(page.status?.tag);
   const statusTagWithoutStable = getStatusTag(page.status?.tag, true);
@@ -178,15 +95,18 @@ function OverviewCard({
       className={styles.overviewCardLink}
     >
       <span className={styles.overviewImageWrapper}>
-        <NextImage
-          src={imageUrl}
-          width="200"
-          height="200"
-          layout="fixed"
-          objectFit="contain"
-          alt={page?.heading + " thumbnail"}
-          aria-hidden
-        />
+        {imageUrl ? (
+          <NextImage
+            src={imageUrl}
+            width="200"
+            height="160"
+            layout="fixed"
+            alt={page?.heading + " thumbnail"}
+            aria-hidden
+          />
+        ) : (
+          <FallbackSvg />
+        )}
       </span>
       <Heading as="span" size="small" className={styles.overviewCardHeading}>
         <span>{page?.heading}</span>
@@ -201,6 +121,38 @@ function OverviewCard({
         )}
       </Heading>
     </Link>
+  );
+}
+
+function FallbackSvg() {
+  return (
+    <svg
+      width="200"
+      height="200"
+      viewBox="0 0 200 200"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M100 153C129.271 153 153 129.271 153 100C153 70.7289 129.271 47 100 47C70.7289 47 47 70.7289 47 100C47 129.271 70.7289 153 100 153ZM100 165C135.899 165 165 135.899 165 100C165 64.1015 135.899 35 100 35C64.1015 35 35 64.1015 35 100C35 135.899 64.1015 165 100 165Z"
+        fill="#001630"
+        fillOpacity="0.188235"
+      />
+      <path
+        d="M96.768 67C97.5378 65.6667 99.4622 65.6667 100.232 67L112.789 88.75C113.559 90.0833 112.597 91.75 111.057 91.75H85.9426C84.403 91.75 83.4408 90.0833 84.2106 88.75L96.768 67Z"
+        fill="#5D6573"
+      />
+      <path
+        d="M96 114.5C96 121.956 89.9558 128 82.5 128C75.0442 128 69 121.956 69 114.5C69 107.044 75.0442 101 82.5 101C89.9558 101 96 107.044 96 114.5Z"
+        fill="#5D6573"
+      />
+      <path
+        d="M105 104C105 102.895 105.895 102 107 102H129C130.105 102 131 102.895 131 104V126C131 127.105 130.105 128 129 128H107C105.895 128 105 127.105 105 126V104Z"
+        fill="#5D6573"
+      />
+    </svg>
   );
 }
 
