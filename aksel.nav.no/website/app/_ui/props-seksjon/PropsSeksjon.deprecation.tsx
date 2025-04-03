59bd70e8e7dc0289c64248f1dbd973ec401bee7d
Julian Nymark <julian.nymark@nav.no>
2025-04-03 13:03:49 +0200

    :recycle: use the [], not the () or modules

--- aksel.nav.no/website/app/_ui/props-seksjon/PropsSeksjon.deprecation.tsx
+++ aksel.nav.no/website/app/_ui/props-seksjon/PropsSeksjon.deprecation.tsx
@@ -9,7 +9,7 @@ function PropsSeksjonDeprecation({ text }: { text?: string }) {
   }
 
   return (
-    <li className={`${styles.propsSeksjonLiDeprecated}`}>
+    <li className={`${styles.propsSeksjonLi} text-[--ax-text-danger-subtle]`}>
       <div className={styles.propsSeksjonLiTitle}>Deprecated:</div>
       <Box marginInline="space-8">{text}</Box>
     </li>
