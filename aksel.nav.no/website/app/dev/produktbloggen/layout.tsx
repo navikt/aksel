50428dc6d7d82bff5b5f136123b352847a42d7ab
Julian Nymark <julian.nymark@nav.no>
2025-04-23 16:40:03 +0200

    use <PageBlock> over custom CSS

--- aksel.nav.no/website/app/dev/produktbloggen/layout.tsx
+++ aksel.nav.no/website/app/dev/produktbloggen/layout.tsx
@@ -1,4 +1,6 @@
 import { Page } from "@navikt/ds-react";
+// @ts-expect-error module loading is a bit broken (@ routes for website)
+import { PageBlock } from "@navikt/ds-react/Page";
 import Footer from "@/app/_ui/footer/Footer";
 import { Header } from "@/app/_ui/header/Header";
 import styles from "./_ui/Produktbloggen.module.css";
@@ -12,7 +14,7 @@ export default async function DesignsystemLayout({
     <Page className={styles.websitePage} footer={<Footer />}>
       <Header variant="produktbloggen" />
       <main tabIndex={-1} id="hovedinnhold" className={styles.main}>
-        <div className={styles.pageLayout}>{children}</div>
+        <PageBlock>{children}</PageBlock>
       </main>
     </Page>
   );
