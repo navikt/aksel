d5ca617a3b4f2bd09f8630035f70267f05e93f14
Ken <26967723+KenAJoh@users.noreply.github.com>
2025-05-06 19:48:37 +0200

    [Appdir] Support "top-level"-pages (#3757)
    * :tada: Support top level articles for designsystem-pages
    
    * ♻️ Update slug handling, staticParams and metadata for overview-pages

--- aksel.nav.no/website/app/dev/(designsystemet)/komponenter/[category]/[page]/page.tsx
+++ aksel.nav.no/website/app/dev/(designsystemet)/komponenter/[category]/[page]/page.tsx
@@ -1,101 +1,42 @@
-import { Metadata, ResolvingMetadata } from "next";
-import { PortableTextBlock } from "next-sanity";
-import { notFound } from "next/navigation";
+import { Metadata } from "next";
 import type { Image } from "sanity";
 import { sanityFetch } from "@/app/_sanity/live";
-import {
-  KOMPONENT_BY_SLUG_QUERY,
-  METADATA_BY_SLUG_QUERY,
-  TOC_BY_SLUG_QUERY,
-} from "@/app/_sanity/queries";
+import { METADATA_BY_SLUG_QUERY } from "@/app/_sanity/queries";
 import { urlForOpenGraphImage } from "@/app/_sanity/utils";
-import { CustomPortableText } from "@/app/_ui/portable-text/CustomPortableText";
-import { SystemPanel } from "@/app/_ui/system-panel/SystemPanel";
-import { TableOfContents } from "@/app/_ui/toc/TableOfContents";
-import { DesignsystemetKomponentIntro } from "@/app/dev/(designsystemet)/_ui/Designsystemet.intro";
-import {
-  DesignsystemetPageHeader,
-  DesignsystemetPageLayout,
-} from "@/app/dev/(designsystemet)/_ui/DesignsystemetPage";
-import {
-  getStaticParamsSlugs,
-  parseDesignsystemSlug,
-} from "@/app/dev/(designsystemet)/slug";
+import { getStaticParamsSlugs } from "@/app/dev/(designsystemet)/slug";
+import { KomponenterPage } from "../_ui/KomponenterPage";
 
 type Props = {
   params: Promise<{ category: string; page: string }>;
 };
 
-export async function generateMetadata(
-  { params }: Props,
-  parent: ResolvingMetadata,
-): Promise<Metadata> {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { category, page } = await params;
 
   const { data: pageData } = await sanityFetch({
     query: METADATA_BY_SLUG_QUERY,
-    params: { slug: parseDesignsystemSlug(category, page, "komponenter") },
+    params: { slug: `komponenter/${category}/${page}` },
     stega: false,
   });
-
-  const ogImages = (await parent).openGraph?.images || [];
-  const pageOgImage = urlForOpenGraphImage(pageData?.seo?.image as Image);
-
-  pageOgImage && ogImages.unshift(pageOgImage);
 
   return {
     title: pageData?.heading,
     description: pageData?.seo?.meta,
     openGraph: {
-      images: ogImages,
+      images: urlForOpenGraphImage(pageData?.seo?.image as Image),
     },
   };
 }
 
 export async function generateStaticParams() {
-  return await getStaticParamsSlugs("komponent_artikkel");
+  return await getStaticParamsSlugs({
+    type: "komponent_artikkel",
+    onlyTopLevelPages: false,
+  });
 }
 
 export default async function Page({ params }: Props) {
   const { category, page } = await params;
 
-  const parsedSlug = parseDesignsystemSlug(category, page, "komponenter");
-
-  const [{ data: pageData }, { data: toc = [] }] = await Promise.all([
-    sanityFetch({
-      query: KOMPONENT_BY_SLUG_QUERY,
-      params: { slug: parsedSlug },
-    }),
-    sanityFetch({
-      query: TOC_BY_SLUG_QUERY,
-      params: { slug: parsedSlug },
-    }),
-  ]);
-
-  if (!pageData?._id) {
-    notFound();
-  }
-
-  return (
-    <DesignsystemetPageLayout layout="with-toc">
-      <DesignsystemetPageHeader data={pageData} />
-      <TableOfContents
-        feedback={{
-          name: pageData.heading,
-          text: "Send innspill",
-        }}
-        toc={toc}
-      />
-      <div>
-        {["beta", "new"].includes(pageData.status?.tag ?? "") && (
-          <SystemPanel
-            variant={pageData.status?.tag as "beta" | "new"}
-            unsafeBeta={pageData.status?.unsafe}
-          />
-        )}
-        <DesignsystemetKomponentIntro data={pageData} />
-        <CustomPortableText value={pageData.content as PortableTextBlock[]} />
-      </div>
-    </DesignsystemetPageLayout>
-  );
+  return <KomponenterPage slug={`komponenter/${category}/${page}`} />;
 }
--- aksel.nav.no/website/app/dev/(designsystemet)/monster-maler/[category]/[page]/page.tsx
+++ aksel.nav.no/website/app/dev/(designsystemet)/monster-maler/[category]/[page]/page.tsx
@@ -1,132 +1,42 @@
-import { Metadata, ResolvingMetadata } from "next";
-import { PortableTextBlock } from "next-sanity";
-import { notFound } from "next/navigation";
+import { Metadata } from "next";
 import type { Image } from "sanity";
-import { Heading } from "@navikt/ds-react";
 import { sanityFetch } from "@/app/_sanity/live";
-import {
-  METADATA_BY_SLUG_QUERY,
-  MONSTER_MALER_BY_SLUG_QUERY,
-  TOC_BY_SLUG_QUERY,
-} from "@/app/_sanity/queries";
+import { METADATA_BY_SLUG_QUERY } from "@/app/_sanity/queries";
 import { urlForOpenGraphImage } from "@/app/_sanity/utils";
-import { CustomPortableText } from "@/app/_ui/portable-text/CustomPortableText";
-import styles from "@/app/_ui/portable-text/CustomPortableText.module.css";
-import { TableOfContents } from "@/app/_ui/toc/TableOfContents";
-import {
-  WebsiteTable,
-  WebsiteTableRow,
-} from "@/app/_ui/website-table/WebsiteTable";
-import {
-  DesignsystemetPageHeader,
-  DesignsystemetPageLayout,
-} from "@/app/dev/(designsystemet)/_ui/DesignsystemetPage";
-import {
-  getStaticParamsSlugs,
-  parseDesignsystemSlug,
-} from "@/app/dev/(designsystemet)/slug";
+import { getStaticParamsSlugs } from "@/app/dev/(designsystemet)/slug";
+import { MonsterMalerPage } from "../_ui/MonsterMalerPage";
 
 type Props = {
   params: Promise<{ category: string; page: string }>;
 };
 
-export async function generateMetadata(
-  { params }: Props,
-  parent: ResolvingMetadata,
-): Promise<Metadata> {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { category, page } = await params;
 
   const { data: pageData } = await sanityFetch({
     query: METADATA_BY_SLUG_QUERY,
-    params: { slug: parseDesignsystemSlug(category, page, "monster-maler") },
+    params: { slug: `monster-maler/${category}/${page}` },
     stega: false,
   });
-
-  const ogImages = (await parent).openGraph?.images || [];
-  const pageOgImage = urlForOpenGraphImage(pageData?.seo?.image as Image);
-
-  pageOgImage && ogImages.unshift(pageOgImage);
 
   return {
     title: pageData?.heading,
     description: pageData?.seo?.meta,
     openGraph: {
-      images: ogImages,
+      images: urlForOpenGraphImage(pageData?.seo?.image as Image),
     },
   };
 }
 
 export async function generateStaticParams() {
-  return await getStaticParamsSlugs("templates_artikkel");
+  return await getStaticParamsSlugs({
+    type: "templates_artikkel",
+    onlyTopLevelPages: false,
+  });
 }
 
 export default async function Page({ params }: Props) {
   const { category, page } = await params;
 
-  const parsedSlug = parseDesignsystemSlug(category, page, "monster-maler");
-
-  const [{ data: pageData }, { data: toc = [] }] = await Promise.all([
-    sanityFetch({
-      query: MONSTER_MALER_BY_SLUG_QUERY,
-      params: { slug: parsedSlug },
-    }),
-    sanityFetch({
-      query: TOC_BY_SLUG_QUERY,
-      params: { slug: parsedSlug },
-    }),
-  ]);
-
-  if (!pageData?._id) {
-    notFound();
-  }
-
-  const metadata = pageData.content?.find((x) => x._type === "kode_eksempler")
-    ?.dir?.metadata;
-
-  return (
-    <DesignsystemetPageLayout layout="with-toc">
-      <DesignsystemetPageHeader data={pageData} />
-      <TableOfContents
-        feedback={{
-          name: pageData.heading,
-          text: "Send innspill",
-        }}
-        toc={toc}
-      />
-      <CustomPortableText
-        value={pageData.content as PortableTextBlock[]}
-        data-block-margin="space-28"
-      />
-      {metadata?.changelog && (
-        <div>
-          <Heading
-            className={styles.headingElement}
-            tabIndex={-1}
-            id="changelog"
-            level="2"
-            size="large"
-            data-level="2"
-          >
-            Endringer
-          </Heading>
-          <WebsiteTable
-            th={[{ text: "Dato" }, { text: "Versjon" }, { text: "Endringer" }]}
-          >
-            {metadata.changelog
-              .sort((a, b) => (a.version ?? 0) - (b.version ?? 0))
-              .map((log) => (
-                <WebsiteTableRow
-                  key={log.version}
-                  tr={[
-                    { text: log.date },
-                    { text: log.version },
-                    { text: log.description },
-                  ]}
-                />
-              ))}
-          </WebsiteTable>
-        </div>
-      )}
-    </DesignsystemetPageLayout>
-  );
+  return <MonsterMalerPage slug={`monster-maler/${category}/${page}`} />;
 }
