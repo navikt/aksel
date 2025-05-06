37417ad4c34ab70d70b4667c3bc9a9fb409b5d19
Vegard Haugstvedt <it.vegard@gmail.com>
2025-05-06 14:29:40 +0200

    Fine-tune search weighting. Still needs more adjustments - likely sorting results, as a "similar result" can now appear before a direct result.
    E.g. "avstand" -> Background colors with "standard" in a comment appears before the "Avstand" category tokens.

--- aksel.nav.no/website/app/dev/(designsystemet)/grunnleggende/darkside/design-tokens/_ui/toolbar/SearchField.utils.ts
+++ aksel.nav.no/website/app/dev/(designsystemet)/grunnleggende/darkside/design-tokens/_ui/toolbar/SearchField.utils.ts
@@ -4,17 +4,18 @@ import { TokenForDocumentationT } from "../types";
 
 function tokenIndex(_tokens: TokenForDocumentationT[]) {
   return new Fuse(_tokens, {
-    threshold: 0.3,
+    threshold: 0.25,
     keys: [
       { name: "name", weight: 0.9 },
       { name: "group", weight: 0.3 },
-      { name: "category", weight: 0.3 },
+      { name: "category", weight: 0.7 },
+      { name: "categoryTitle", weight: 0.7 },
       { name: "comment", weight: 0.3 },
-      { name: "type", weight: 0.3 },
-      { name: "role", weight: 0.3 },
-      { name: "modifier", weight: 0.3 },
+      { name: "type", weight: 0.7 },
+      { name: "role", weight: 0.7 },
+      { name: "modifier", weight: 0.7 },
     ],
-    shouldSort: false,
+    //shouldSort: false,
     ignoreLocation: true,
   });
 }
