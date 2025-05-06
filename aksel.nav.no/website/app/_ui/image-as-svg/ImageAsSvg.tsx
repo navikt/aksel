69f07dbdf3a94eeb134fd75f9129610757c53d40
Ken <ken.aleksander@gmail.com>
2025-05-06 19:55:25 +0200

    ✨ Introduce ImageAsThemedSvg for SVG handling in ImageAsSvg.tsx

---
+++ aksel.nav.no/website/app/_ui/image-as-svg/ImageAsSvg.tsx
@@ -0,0 +1,73 @@
+import DOMPurify from "isomorphic-dompurify";
+import styles from "./ImageAsSvg.module.css";
+
+/**
+ * Utility function to fetch and format a image as SVG with recoloring.
+ */
+async function ImageAsThemedSvg({ url, size }: { url?: string; size: number }) {
+  if (!url) {
+    return null;
+  }
+
+  const svgString = await fetch(url)
+    .then((res) => res.text())
+    .catch(() => null);
+
+  if (!svgString) {
+    return null;
+  }
+
+  const formattedSvgString = formatSvgString(svgString);
+
+  const cleanedSvg = DOMPurify.sanitize(formattedSvgString, {
+    USE_PROFILES: { svg: true },
+  });
+
+  return (
+    <div
+      // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+      dangerouslySetInnerHTML={{ __html: cleanedSvg }}
+      aria-hidden
+      style={{ "--website-svg-size": `${size}px` }}
+    />
+  );
+}
+
+const colorReplacements = {
+  "#99F6E4": "var(--ax-bg-moderate-hoverA)",
+  "#AEF4E4": "var(--ax-bg-moderate-hoverA)",
+  "#003453": "var(--ax-text-subtle)",
+  "#262626": "var(--ax-text-subtle)",
+  "#23262A": "var(--ax-text-subtle)",
+  white: "var(--ax-bg-default)",
+  "#417DA0": "var(--ax-bg-strong)",
+  "#D7E6F0": "var(--ax-bg-moderate-hover)",
+  "#C0D6E4": "var(--ax-bg-moderate-pressed)",
+  "#E3EFF7": "var(--ax-bg-moderate)",
+  "#005A92": "var(--ax-bg-strong)",
+};
+
+/**
+ * Formats the SVG string by replacing colors and adding classes.
+ */
+function formatSvgString(svgString: string) {
+  let formattedSvgString = svgString;
+
+  formattedSvgString = formattedSvgString.replace(
+    "<svg",
+    `<svg class="${styles.imageAsModule}" aria-hidden`,
+  );
+
+  Object.keys(colorReplacements).forEach((color) => {
+    const replacement = colorReplacements[color];
+
+    formattedSvgString = formattedSvgString.replace(
+      new RegExp(color, "g"),
+      replacement,
+    );
+  });
+
+  return formattedSvgString;
+}
+
+export { ImageAsThemedSvg };
--- aksel.nav.no/website/app/dev/(designsystemet)/_ui/overview/DesignsystemetOverview.tsx
+++ aksel.nav.no/website/app/dev/(designsystemet)/_ui/overview/DesignsystemetOverview.tsx
@@ -1,4 +1,3 @@
-import NextImage from "next/image";
 import type { Image } from "sanity";
 import {
   Bleed,
@@ -11,6 +10,7 @@ import {
 } from "@navikt/ds-react";
 import { DESIGNSYSTEM_OVERVIEW_BY_CATEGORY_QUERYResult } from "@/app/_sanity/query-types";
 import { urlForImage } from "@/app/_sanity/utils";
+import { ImageAsThemedSvg } from "@/app/_ui/image-as-svg/ImageAsSvg";
 import { getStatusTag } from "@/app/_ui/theming/theme-config";
 import { MarkdownText } from "@/app/_ui/typography/MarkdownText";
 import {
@@ -81,17 +81,7 @@ function DesignsystemetOverviewCard({
       <VStack gap="space-16">
         <Bleed marginInline="space-20" marginBlock="space-16 0">
           <span className={styles.overviewImageWrapper}>
-            {imageUrl ? (
-              <NextImage
-                src={imageUrl}
-                width={200}
-                height={200}
-                alt={page?.heading + " thumbnail"}
-                aria-hidden
-              />
-            ) : (
-              <FallbackSvg />
-            )}
+            <ImageAsThemedSvg url={imageUrl} size={200} />
           </span>
         </Bleed>
 
@@ -114,39 +104,6 @@ function DesignsystemetOverviewCard({
         </LinkCardTitle>
       </VStack>
     </LinkCard>
-  );
-}
-
-function FallbackSvg() {
-  return (
-    <svg
-      width="200"
-      height="200"
-      viewBox="0 0 200 200"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      aria-hidden
-    >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M100 153C129.271 153 153 129.271 153 100C153 70.7289 129.271 47 100 47C70.7289 47 47 70.7289 47 100C47 129.271 70.7289 153 100 153ZM100 165C135.899 165 165 135.899 165 100C165 64.1015 135.899 35 100 35C64.1015 35 35 64.1015 35 100C35 135.899 64.1015 165 100 165Z"
-        fill="#001630"
-        fillOpacity="0.188235"
-      />
-      <path
-        d="M96.768 67C97.5378 65.6667 99.4622 65.6667 100.232 67L112.789 88.75C113.559 90.0833 112.597 91.75 111.057 91.75H85.9426C84.403 91.75 83.4408 90.0833 84.2106 88.75L96.768 67Z"
-        fill="#5D6573"
-      />
-      <path
-        d="M96 114.5C96 121.956 89.9558 128 82.5 128C75.0442 128 69 121.956 69 114.5C69 107.044 75.0442 101 82.5 101C89.9558 101 96 107.044 96 114.5Z"
-        fill="#5D6573"
-      />
-      <path
-        d="M105 104C105 102.895 105.895 102 107 102H129C130.105 102 131 102.895 131 104V126C131 127.105 130.105 128 129 128H107C105.895 128 105 127.105 105 126V104Z"
-        fill="#5D6573"
-      />
-    </svg>
   );
 }
 
