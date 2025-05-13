df8d5d693188e049b4afe2e66768fd29746c9e7d
Vegard Haugstvedt <it.vegard@gmail.com>
2025-05-13 17:26:27 +0200

    Add designsystem front page skeleton

---
+++ aksel.nav.no/website/app/dev/(designsystemet)/designsystemet/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const metadata = {
+  title: "Designsystemet",
+  description:
+    "Designsystemet Aksel er et felles design- og utviklingsrammeverk for NAV. " +
+    "Det inneholder retningslinjer, komponenter og verktøy for å bygge digitale tjenester " +
+    "som er brukervennlige, tilgjengelige og i tråd med NAVs visuelle identitet.",
+};
+
+const Page = async () => {
+  return (
+    <>
+      <main></main>
+    </>
+  );
+};
+
+export default Page;
