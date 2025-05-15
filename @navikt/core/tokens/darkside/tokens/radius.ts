b69ebaaf0152cc204cb19390d25b674d28908d63
Ken <ken.aleksander@gmail.com>
2025-05-15 19:40:01 +0200

    :recycle: Temp support legacy radius toke for darkside system

--- @navikt/core/tokens/darkside/tokens/radius.ts
+++ @navikt/core/tokens/darkside/tokens/radius.ts
@@ -1,4 +1,4 @@
-import { type BorderRadiusKeys } from "../../types";
+import { type BorderRadiusKeys, LegacyBorderRadiusKeys } from "../../types";
 import { type StyleDictionaryToken } from "../tokens.util";
 
 export const radiusTokenConfig = {
@@ -29,6 +29,46 @@ export const radiusTokenConfig = {
       comment: "TODO: Sjur fyller ut",
     },
   },
+  border: {
+    radius: {
+      full: {
+        value: "9999px",
+        type: "global-radius",
+        comment: "TODO: Sjur fyller ut",
+        figmaIgnore: true,
+      },
+      small: {
+        value: "2px",
+        type: "global-radius",
+        comment: "TODO: Sjur fyller ut",
+        figmaIgnore: true,
+      },
+      medium: {
+        value: "4px",
+        type: "global-radius",
+        comment: "TODO: Sjur fyller ut",
+        figmaIgnore: true,
+      },
+      large: {
+        value: "8px",
+        type: "global-radius",
+        comment: "TODO: Sjur fyller ut",
+        figmaIgnore: true,
+      },
+      xlarge: {
+        value: "12px",
+        type: "global-radius",
+        comment: "TODO: Sjur fyller ut",
+        figmaIgnore: true,
+      },
+    },
+  },
 } satisfies {
   radius: Record<BorderRadiusKeys, StyleDictionaryToken<"global-radius">>;
+  border: {
+    radius: Record<
+      LegacyBorderRadiusKeys,
+      StyleDictionaryToken<"global-radius">
+    >;
+  };
 };
