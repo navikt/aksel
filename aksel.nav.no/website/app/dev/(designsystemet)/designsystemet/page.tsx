dc7e314bb4733cc3a6c6e9573e0c6dbbba0a6d1e
Vegard Haugstvedt <it.vegard@gmail.com>
2025-05-20 17:21:18 +0200

    Implement first working version of "layers" component

--- aksel.nav.no/website/app/dev/(designsystemet)/designsystemet/page.tsx
+++ aksel.nav.no/website/app/dev/(designsystemet)/designsystemet/page.tsx
@@ -1,12 +1,34 @@
+import NextLink from "next/link";
 import React from "react";
-import { CodeIcon, PaletteIcon } from "@navikt/aksel-icons";
-import { BodyLong, HStack, Heading, Tag, VStack } from "@navikt/ds-react";
+import {
+  CodeIcon,
+  ComponentIcon,
+  Density2Icon,
+  PaletteIcon,
+  SidebarLeftIcon,
+  TokenIcon,
+} from "@navikt/aksel-icons";
+import {
+  Bleed,
+  BodyLong,
+  BoxNew,
+  HStack,
+  Heading,
+  Link,
+  List,
+  Page,
+  Tag,
+  VStack,
+} from "@navikt/ds-react";
+import { ListItem } from "@navikt/ds-react/List";
+import { PageBlock } from "@navikt/ds-react/Page";
 import {
   LinkCard,
   LinkCardDescription,
   LinkCardIcon,
   LinkCardTitle,
 } from "../../(god-praksis)/_ui/link-card/LinkCard";
+import "./ds-forside.css";
 
 export const metadata = {
   title: "Designsystemet",
@@ -16,44 +38,116 @@ export const metadata = {
     "som er brukervennlige, tilgjengelige og i tråd med NAVs visuelle identitet.",
 };
 
-const Page = async () => {
+const DottedLine = () => (
+  <div className="aksel__ds-frontpage__dotted-line" aria-hidden="true" />
+);
+
+const DesignsystemetPage = async () => {
   return (
-    <VStack as="main" align="center" gap="space-80">
-      <VStack gap="space-48" paddingBlock="space-24">
-        <VStack gap="space-16" width="600px" align="center">
-          <VStack gap="space-8">
-            <Tag variant="success">Ny versjon av Aksel er ute (darkside)</Tag>
-            <Heading level="1" size="xlarge">
-              Aksel designsystemet
-            </Heading>
+    <Page>
+      <PageBlock as="main" width="md" gutters>
+        <VStack align="center" gap="space-80">
+          <VStack gap="space-48" paddingBlock="space-24">
+            <VStack gap="space-16" maxWidth="600px" align="center">
+              <VStack gap="space-8">
+                <Tag variant="success">
+                  Ny versjon av Aksel er ute (darkside)
+                </Tag>
+                <Heading level="1" size="xlarge">
+                  Aksel designsystemet
+                </Heading>
+              </VStack>
+              <BodyLong size="large" as="p" align="center">
+                Aksel er designsystemet til Navs produktutvikling, en samling
+                med designtokens, dokumenterte komponenter, maler og guider. En
+                komplett plattform for å lage førsteklasses grensesnitt
+              </BodyLong>
+            </VStack>
+            <Bleed marginInline="full">
+              <HStack gap="space-24" justify="center">
+                <LinkCard hasArrow={false}>
+                  <LinkCardTitle as="h2">Start med design</LinkCardTitle>
+                  <LinkCardDescription>
+                    Figma-filer og kreative arenaer
+                  </LinkCardDescription>
+                  <LinkCardIcon>
+                    <PaletteIcon fontSize="48" />
+                  </LinkCardIcon>
+                </LinkCard>
+                <LinkCard hasArrow={false}>
+                  <LinkCardTitle as="h2">Start med utvikling</LinkCardTitle>
+                  <LinkCardDescription>
+                    Installasjon og tips
+                  </LinkCardDescription>
+                  <LinkCardIcon>
+                    <CodeIcon fontSize="48" />
+                  </LinkCardIcon>
+                </LinkCard>
+              </HStack>
+            </Bleed>
           </VStack>
-          <BodyLong size="large" as="p" align="center">
-            Aksel er designsystemet til Navs produktutvikling, en samling med
-            designtokens, dokumenterte komponenter, maler og guider. En komplett
-            plattform for å lage førsteklasses grensesnitt
-          </BodyLong>
+          <BoxNew
+            background="brand-blue-soft"
+            borderColor="brand-blue-subtle"
+            borderWidth="1"
+            borderRadius="xlarge"
+            padding="space-40"
+            width="100%"
+          >
+            <VStack gap="space-24">
+              <VStack align="center" gap="space-16">
+                <Heading level="2" size="large">
+                  Et fleksibelt system
+                </Heading>
+                <BodyLong size="large" as="p" align="center">
+                  Aksel designsystemet består av flere lag
+                  <br /> som kan brukes enkeltvis eller som en full pakke.
+                </BodyLong>
+              </VStack>
+              <List className="aksel-layers-list">
+                <ListItem
+                  icon={<SidebarLeftIcon fontSize="24" aria-hidden="true" />}
+                >
+                  <Link as={NextLink} href="#komponenter">
+                    Mønster og maler
+                    <DottedLine />
+                  </Link>
+                </ListItem>
+                <ListItem
+                  icon={<ComponentIcon fontSize="24" aria-hidden="true" />}
+                >
+                  <div className="link-group">
+                    <Link as={NextLink} href="#komponenter">
+                      Komponenter
+                    </Link>{" "}
+                    og{" "}
+                    <Link as={NextLink} href="#komponenter">
+                      ikoner
+                      <DottedLine />
+                    </Link>
+                  </div>
+                </ListItem>
+                <ListItem
+                  icon={<Density2Icon fontSize="24" aria-hidden="true" />}
+                >
+                  <Link as={NextLink} href="#komponenter">
+                    Layout primitives
+                    <DottedLine />
+                  </Link>
+                </ListItem>
+                <ListItem icon={<TokenIcon fontSize="24" aria-hidden="true" />}>
+                  <Link as={NextLink} href="#komponenter">
+                    Design tokens
+                    <DottedLine />
+                  </Link>
+                </ListItem>
+              </List>
+            </VStack>
+          </BoxNew>
         </VStack>
-        <HStack gap="space-24">
-          <LinkCard>
-            <LinkCardTitle as="h2">Start med design</LinkCardTitle>
-            <LinkCardDescription>
-              Figma-filer og kreative arenaer
-            </LinkCardDescription>
-            <LinkCardIcon>
-              <PaletteIcon fontSize="48" />
-            </LinkCardIcon>
-          </LinkCard>
-          <LinkCard>
-            <LinkCardTitle as="h2">Start med utvikling</LinkCardTitle>
-            <LinkCardDescription>Installasjon og tips</LinkCardDescription>
-            <LinkCardIcon>
-              <CodeIcon fontSize="48" />
-            </LinkCardIcon>
-          </LinkCard>
-        </HStack>
-      </VStack>
-    </VStack>
+      </PageBlock>
+    </Page>
   );
 };
 
-export default Page;
+export default DesignsystemetPage;
