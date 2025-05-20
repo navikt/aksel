a5d157e2180e9c89a0657f0298617d204a746bab
Ken <ken.aleksander@gmail.com>
2025-05-20 12:12:40 +0200

    :recycle: Base-theming Datepicker

--- @navikt/core/react/src/date/datepicker/DatePicker.stories.tsx
+++ @navikt/core/react/src/date/datepicker/DatePicker.stories.tsx
@@ -564,3 +564,9 @@ export const Chromatic: Story = {
     chromatic: { disable: false },
   },
 };
+
+export const ColorRole = () => (
+  <div data-color-role="meta-purple">
+    <UseDatepicker />
+  </div>
+);
