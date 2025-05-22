b66652236a923b7b26894b90c49793914ff6a09f
Ken <ken.aleksander@gmail.com>
2025-05-22 12:46:27 +0200

    :recycle: Base-theming FormProgress and ProgressBar

--- @navikt/core/react/src/progress-bar/ProgressBar.tsx
+++ @navikt/core/react/src/progress-bar/ProgressBar.tsx
@@ -134,7 +134,6 @@ export const ProgressBar = forwardRef<HTMLDivElement, ProgressBarProps>(
         role="progressbar"
         aria-labelledby={ariaLabelledBy}
         aria-label={ariaLabel}
-        data-color-role="info"
         {...rest}
       >
         <div
