859d4c309db10ddf84bc9c1583645e7572f9622f
Ken <ken.aleksander@gmail.com>
2025-05-26 16:20:52 +0200

    :memo: Prepare redirects for designsystemet routing update

--- aksel.nav.no/website/next.config.ts
+++ aksel.nav.no/website/next.config.ts
@@ -113,17 +113,33 @@ const nextConfig: NextConfig = {
         destination: "/api/preview?slug=:slug*",
         permanent: true,
       },
-      /* TODO: Use icon-page redirects after release */
-      /* {
-        source: "/ikoner",
-        destination: "/dev/grunnleggende/ikoner",
-        permanent: false,
-      },
-      {
-        source: "/ikoner/:slug*",
-        destination: "/dev/komponenter/ikoner?iconName=:slug*",
-        permanent: false,
-      }, */
+      /* ikon-page */
+      // {
+      //   source: "/ikoner",
+      //   destination: "/komponenter/ikoner",
+      //   permanent: true,
+      // },
+      // {
+      //   source: "/ikoner/:slug*",
+      //   destination: "/komponenter/ikoner",
+      //   permanent: true,
+      // },
+      /* landing-pages */
+      // {
+      //   source: "/grunnleggende",
+      //   destination: "/designsystemet",
+      //   permanent: true,
+      // },
+      // {
+      //   source: "/komponenter",
+      //   destination: "/designsystemet",
+      //   permanent: true,
+      // },
+      // {
+      //   source: "/monster-maler",
+      //   destination: "/designsystemet",
+      //   permanent: true,
+      // },
     ];
   },
 
