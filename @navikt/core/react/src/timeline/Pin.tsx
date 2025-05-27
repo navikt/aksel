03395ae8a1499b9012acdf220cd8a61d29f35de6
Ken <26967723+KenAJoh@users.noreply.github.com>
2025-05-27 10:01:07 +0200

    :bug: Reverted regression that lead to 'aria-controls' being ignored (#3828)
    * :bug: Reverted regression that lead to 'aria-controls' being ignored when passed to Pin or Period.
    
    * :bug: Add role='dialog' to timeline popover

--- @navikt/core/react/src/timeline/Pin.tsx
+++ @navikt/core/react/src/timeline/Pin.tsx
@@ -11,7 +11,6 @@ import {
   useFocus,
   useHover,
   useInteractions,
-  useRole,
 } from "@floating-ui/react";
 import { format } from "date-fns";
 import React, { forwardRef, useRef, useState } from "react";
@@ -79,13 +78,11 @@ export const Pin = forwardRef<HTMLButtonElement, TimelinePinProps>(
     });
     const focus = useFocus(context);
     const dismiss = useDismiss(context);
-    const role = useRole(context, { role: "dialog" });
 
     const { getFloatingProps, getReferenceProps } = useInteractions([
       hover,
       focus,
       dismiss,
-      role,
     ]);
 
     const mergedRef = useMergeRefs(refs.setReference, ref);
@@ -112,6 +109,7 @@ export const Pin = forwardRef<HTMLButtonElement, TimelinePinProps>(
             })}
             type="button"
             aria-expanded={children ? open : undefined}
+            aria-haspopup={children ? "true" : "false"}
             {...getReferenceProps({
               onKeyDown: (e) => {
                 rest?.onKeyDown?.(e as React.KeyboardEvent<HTMLButtonElement>);
@@ -135,6 +133,7 @@ export const Pin = forwardRef<HTMLButtonElement, TimelinePinProps>(
               className={cn("navds-timeline__popover")}
               data-placement={placement}
               ref={refs.setFloating}
+              role="dialog"
               {...getFloatingProps()}
               style={floatingStyles}
             >
