0d1eb4cfcd34e7e9a7e51e6c7e4b2d55e9ec99db
stainawarjar <210375207+stainawarjar@users.noreply.github.com>
2025-05-28 11:14:40 +0200

    Add rudimentary show more functionality

--- aksel.nav.no/website/app/dev/(designsystemet)/grunnleggende/endringslogg/_ui/LogEntry.tsx
+++ aksel.nav.no/website/app/dev/(designsystemet)/grunnleggende/endringslogg/_ui/LogEntry.tsx
@@ -1,8 +1,11 @@
 import { PortableTextBlock } from "next-sanity";
 import Image from "next/image";
+import { useRef, useState } from "react";
+import { ChevronDownIcon, ChevronUpIcon } from "@navikt/aksel-icons";
 import {
   BodyShort,
   Box,
+  Button,
   HStack,
   Heading,
   Link,
@@ -14,6 +17,8 @@ import { urlForImage } from "@/app/_sanity/utils";
 import styles from "./Changelog.module.css";
 
 export default function LogEntry({ logEntry, index, isLastEntry = false }) {
+  const [expanded, setExpanded] = useState(false);
+  const logEntryContainer = useRef(null);
   return (
     <li key={"log-entry-" + index}>
       <VStack width="48px" height="var(--ax-space-32)" align="center">
@@ -72,11 +77,24 @@ export default function LogEntry({ logEntry, index, isLastEntry = false }) {
           </HStack>
           {/* TODO: [endringslogg] Ensure proper wrapping on mobile */}
           {/* TODO: [endringslogg] Ensure slimmer margins on mobile */}
-          {/* TODO: [endringslogg] Show 'Vis mer'-button when log-entry height>400px(?) */}
+          {/* TODO: [endringslogg] Fix 'Vis mer' when fremhevet */}
+          {/* TODO: [endringslogg] Fix 'Vis mer' when short content wo/expansion */}
           <VStack
+            ref={logEntryContainer}
             marginBlock="space-0 space-32"
             padding={logEntry.fremhevet ? "space-16" : "space-0"}
             className={logEntry.fremhevet ? styles.innholdFremhevet : ""}
+            style={{
+              overflow: "hidden",
+              maxHeight: expanded ? "unset" : "200px",
+              maskImage: expanded
+                ? "unset"
+                : "linear-gradient(var(--ax-bg-default), 90%, transparent)",
+              maskSize: "90%",
+              maskPosition: "",
+              scrollMarginTop:
+                "calc(var(--website-header-height) + var(--ax-space-12) + 48px + var(--ax-space-16))",
+            }}
           >
             <Heading size="large" level="2" spacing>
               <Link
@@ -119,6 +137,39 @@ export default function LogEntry({ logEntry, index, isLastEntry = false }) {
               value={logEntry.innhold as PortableTextBlock[]}
             />
           </VStack>
+          <Button
+            size="small"
+            variant="secondary-neutral"
+            icon={expanded ? <ChevronUpIcon /> : <ChevronDownIcon />}
+            style={{
+              position: "relative",
+              alignSelf: "start",
+              backgroundColor: "Background",
+              ...(expanded ? { top: "-20px" } : { top: "-20px" }),
+              ...(logEntry.fremhevet
+                ? {
+                    color: "var(--aksel-brand-pink-1000)",
+                    boxShadow: "var(--aksel-brand-pink-600)",
+                  }
+                : {}),
+            }}
+            onClick={(e) => {
+              e.stopPropagation();
+              setExpanded((prev) => !prev);
+              if (expanded) {
+                setTimeout(() => {
+                  if (logEntryContainer.current) {
+                    logEntryContainer.current.scrollIntoView({
+                      smooth: true,
+                      block: "start",
+                    });
+                  }
+                }, 100);
+              }
+            }}
+          >
+            {expanded ? "Vis mindre" : "Vis mer"}
+          </Button>
         </VStack>
       </HStack>
     </li>
