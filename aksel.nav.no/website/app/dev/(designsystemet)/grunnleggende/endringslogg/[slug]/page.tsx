c621e5db918826b248651bf54b9633b6cbfe607d
stainawarjar <210375207+stainawarjar@users.noreply.github.com>
2025-06-06 10:46:50 +0200

    Add toc to article page

--- aksel.nav.no/website/app/dev/(designsystemet)/grunnleggende/endringslogg/[slug]/page.tsx
+++ aksel.nav.no/website/app/dev/(designsystemet)/grunnleggende/endringslogg/[slug]/page.tsx
@@ -2,13 +2,28 @@ import { format } from "date-fns";
 import { nb } from "date-fns/locale";
 import { PortableTextBlock, defineQuery } from "next-sanity";
 import Image from "next/image";
+import type { Image as SanityImage } from "sanity";
 import { BodyShort, HStack, Heading, Tag, VStack } from "@navikt/ds-react";
 import { CustomPortableText } from "@/app/CustomPortableText";
 import { sanityFetch } from "@/app/_sanity/live";
+import {
+  ENDRINGSLOGG_QUERYResult,
+  TOC_BY_SLUG_QUERYResult,
+} from "@/app/_sanity/query-types";
 import { urlForImage } from "@/app/_sanity/utils";
+import { TableOfContents } from "@/app/_ui/toc/TableOfContents";
 import { DesignsystemetPageLayout } from "../../../_ui/DesignsystemetPage";
-import TokenTableOfContents from "../../darkside/design-tokens/_ui/TokenTableOfContents";
 import styles from "../_ui/Changelog.module.css";
+
+const ENDRINGSLOGG_QUERY = defineQuery(
+  `*[_type == "ds_endringslogg_artikkel" && slug.current == $slug]{heading, "slug": slug.current, endringsdato, endringstype, fremhevet, herobilde, innhold}`,
+);
+const TOC_BY_SLUG_QUERY = defineQuery(
+  `*[_type == "ds_endringslogg_artikkel" && slug.current == $slug][0].innhold[style match 'h2']{
+  "id": _key,
+  "title": pt::text(@)
+}`,
+);
 
 type Props = {
   params: Promise<{ slug: string }>;
@@ -16,15 +31,24 @@ type Props = {
 
 export default async function (props: Props) {
   const { slug } = await props.params;
-  const logEntry = (
-    await sanityFetch({
-      query: defineQuery(
-        `*[_type == "ds_endringslogg_artikkel" && slug.current == $slug]{heading, "slug": slug.current, endringsdato, endringstype, fremhevet, herobilde, innhold}`,
-      ),
-      params: { slug: `${slug}` },
-    })
-  ).data[0];
 
+  const [{ data: logEntries }, { data: toc }]: [
+    { data: ENDRINGSLOGG_QUERYResult },
+    { data: TOC_BY_SLUG_QUERYResult },
+  ] = await Promise.all([
+    sanityFetch<typeof ENDRINGSLOGG_QUERY>({
+      query: ENDRINGSLOGG_QUERY,
+      params: { slug: `${slug}` },
+    }),
+    sanityFetch({
+      query: TOC_BY_SLUG_QUERY,
+      params: { slug },
+    }),
+  ]);
+
+  const logEntry = logEntries[0];
+
+  // const tocByMap = logEntry.innhold.reduce((filtered, block) =>  bloc.style === 'h2' ? filtered.push({id: block._key, title: filtered. block.}))
   return (
     <DesignsystemetPageLayout layout="with-toc">
       <VStack>
@@ -42,16 +66,16 @@ export default async function (props: Props) {
           size="xlarge"
           level="1"
           spacing
-          className={logEntry.fremhevet && styles.headingFremhevet}
+          className={logEntry.fremhevet ? styles.headingFremhevet : ""}
         >
           {logEntry.heading}
         </Heading>
         <HStack gap="space-16" marginBlock="space-0 space-28">
           <BodyShort
             size="small"
-            className={logEntry.fremhevet && styles.dateFremhevet}
+            className={logEntry.fremhevet ? styles.dateFremhevet : ""}
           >
-            {format(new Date(logEntry.endringsdato), "d. MMMM yyy", {
+            {format(new Date(logEntry.endringsdato || ""), "d. MMMM yyy", {
               locale: nb,
             })}
           </BodyShort>
@@ -65,30 +89,29 @@ export default async function (props: Props) {
           <Image
             data-block-margin="space-28"
             className={styles.herobilde}
-            // style={{
-            //   backgroundColor:
-            //     logEntry.fremhevet && logEntry.herobilde?.bakgrunnsfarge
-            //       ? logEntry.herobilde.bakgrunnsfarge
-            //       : "",
-            //   "--herobilde-bg-color-first":
-            //     logEntry.herobilde.bakgrunnsfarge ||
-            //     "var(--aksel-brand-pink-400)",
-            //   "--herobilde-bg-color-last":
-            //     logEntry.herobilde.bakgrunnsfarge ||
-            //     "var(--aksel-brand-pink-700)",
-            //   "--herobilde-bg-degrees": "130deg",
-            // }}
-            alt={logEntry.herobilde.alt}
+            alt={logEntry.herobilde.alt ? logEntry.herobilde.alt : ""}
             loading="lazy"
             decoding="async"
-            src={urlForImage(logEntry.herobilde)?.auto("format").url() || ""}
+            src={
+              urlForImage(logEntry.herobilde as SanityImage)
+                ?.auto("format")
+                .url() || ""
+            }
             width={1200}
             height={630}
           />
         )}
         <CustomPortableText value={logEntry.innhold as PortableTextBlock[]} />
       </VStack>
-      <TokenTableOfContents />
+
+      <TableOfContents
+        feedback={{
+          name: "Endringslogg",
+          text: "Innspill til siden",
+        }}
+        showChangelogLink={true}
+        toc={toc}
+      />
     </DesignsystemetPageLayout>
   );
 }
