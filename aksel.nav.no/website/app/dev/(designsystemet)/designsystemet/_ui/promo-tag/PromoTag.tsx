c4822fe81ed49992d57b8dde04c95f1b4ae3c9af
Vegard Haugstvedt <it.vegard@gmail.com>
2025-06-11 14:21:05 +0200

    Use data-color and slightly tweak padding for PromoTag

--- aksel.nav.no/website/app/dev/(designsystemet)/designsystemet/_ui/promo-tag/PromoTag.tsx
+++ aksel.nav.no/website/app/dev/(designsystemet)/designsystemet/_ui/promo-tag/PromoTag.tsx
@@ -1,18 +1,18 @@
 import Link from "next/link";
-import { ArrowRightIcon } from "@navikt/aksel-icons";
+import { ChevronRightIcon } from "@navikt/aksel-icons";
 import { Detail } from "@navikt/ds-react";
 import "./promo-tag.css";
 
 const PromoTag = () => {
   return (
-    <Link href="#">
-      <Detail as="span" className="promoTag">
+    <Link href="#" data-color="aksel-brand-pink">
+      <Detail as="span" className="promoTag" textColor="default">
         <Detail as="span" textColor="contrast" className="promoTagLabel">
           Nyhet
         </Detail>
         Ny versjon av Aksel er ute (darkside) 🎉
-        <ArrowRightIcon
-          fontSize="16"
+        <ChevronRightIcon
+          fontSize="16px"
           aria-hidden="true"
           className="promoTagArrow"
         />
