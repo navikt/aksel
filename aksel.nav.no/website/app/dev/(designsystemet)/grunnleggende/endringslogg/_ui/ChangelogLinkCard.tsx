fe989f00ac733d0cd1b4c0e5e7481fca516059af
Halvor Haugan <halvor.haugan@nav.no>
2025-06-23 14:35:25 +0200

    ChangelogLinkCard changes

--- aksel.nav.no/website/app/dev/(designsystemet)/grunnleggende/endringslogg/_ui/ChangelogLinkCard.tsx
+++ aksel.nav.no/website/app/dev/(designsystemet)/grunnleggende/endringslogg/_ui/ChangelogLinkCard.tsx
@@ -1,20 +1,14 @@
 import { format } from "date-fns";
 import { nb } from "date-fns/locale";
-import { PortableTextBlock } from "next-sanity";
-import { version } from "react";
-import { BodyShort, BoxNew, Heading, Tag, VStack } from "@navikt/ds-react";
-import { CustomPortableText } from "@/app/CustomPortableText";
-import { ENDRINGSLOGG_WITH_NEIGHBORS_QUERYResult } from "@/app/_sanity/query-types";
+import { Detail, Heading, LinkCard, Tag, VStack } from "@navikt/ds-react";
 import {
-  LinkCard,
   LinkCardAnchor,
+  LinkCardDescription,
+  LinkCardFooter,
   LinkCardTitle,
-} from "@/app/dev/(god-praksis)/_ui/link-card/LinkCard";
+} from "@navikt/ds-react/LinkCard";
+import { ENDRINGSLOGG_WITH_NEIGHBORS_QUERYResult } from "@/app/_sanity/query-types";
 import { capitalize } from "@/utils";
-import styles from "../_ui/Changelog.module.css";
-import { bumpHeadingLevels } from "./utils";
-
-const inertValue = parseInt(version.split(".")[0]) > 18; // Support for inert was added in React 19
 
 const ChangelogLinkCard = ({
   logEntry,
@@ -23,36 +17,31 @@ const ChangelogLinkCard = ({
   logEntry: NonNullable<ENDRINGSLOGG_WITH_NEIGHBORS_QUERYResult>["primary"];
   label: string;
 }) => (
-  <VStack gap="space-16">
+  <VStack gap="space-12">
     <Heading size="small" level="2">
       {label}
     </Heading>
-    <LinkCard hasArrow={true}>
-      <VStack style={{ height: "14rem" }} align="start">
-        <LinkCardTitle as="h2" size="large">
-          <LinkCardAnchor href={`${logEntry.slug}`}>
-            {logEntry.heading}
-          </LinkCardAnchor>
-        </LinkCardTitle>
-        <CustomPortableText
-          className={`${styles.portableTextFirstHeading} ${styles.portableTextInCard}`}
-          inert={inertValue}
-          value={bumpHeadingLevels(logEntry.innhold) as PortableTextBlock[]}
-        />
-        <BoxNew>
-          <BodyShort size="small" spacing>
-            {format(new Date(logEntry.endringsdato || ""), "d. MMMM yyy", {
-              locale: nb,
-            })}
-          </BodyShort>
-        </BoxNew>
-        <BoxNew>
-          <Tag size="xsmall" variant="info-moderate">
-            {capitalize(logEntry.endringstype || "")}
-          </Tag>
-        </BoxNew>
-      </VStack>
+
+    <LinkCard arrow={false}>
+      <LinkCardTitle as="h3">
+        <LinkCardAnchor href={`${logEntry.slug}`}>
+          {logEntry.heading}
+        </LinkCardAnchor>
+      </LinkCardTitle>
+      <LinkCardDescription>
+        <Detail textColor="subtle">
+          {format(new Date(logEntry.endringsdato || ""), "d. MMMM yyy", {
+            locale: nb,
+          })}
+        </Detail>
+      </LinkCardDescription>
+      <LinkCardFooter>
+        <Tag size="xsmall" variant="info-moderate">
+          {capitalize(logEntry.endringstype || "")}
+        </Tag>
+      </LinkCardFooter>
     </LinkCard>
   </VStack>
 );
+
 export default ChangelogLinkCard;
