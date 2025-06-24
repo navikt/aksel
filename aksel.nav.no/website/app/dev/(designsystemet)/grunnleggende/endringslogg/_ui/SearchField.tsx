56f45dc892cf2d32082ef0290407ff66b3800a3f
stainawarjar <210375207+stainawarjar@users.noreply.github.com>
2025-06-24 15:28:53 +0200

    Fix persisting search query in search box after page revisit

--- aksel.nav.no/website/app/dev/(designsystemet)/grunnleggende/endringslogg/_ui/SearchField.tsx
+++ aksel.nav.no/website/app/dev/(designsystemet)/grunnleggende/endringslogg/_ui/SearchField.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useRef } from "react";
 import { Search } from "@navikt/ds-react";
 
 export default function SearchField() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
+  const searchRef = useRef<HTMLInputElement>(null);
 
   function handleSearch(query: string) {
     const params = new URLSearchParams(searchParams?.toString());
@@ -22,9 +24,16 @@ export default function SearchField() {
     event.preventDefault();
   }
 
+  useEffect(() => {
+    if (searchRef.current) {
+      searchRef.current.value = searchParams?.get("fritekst") || "";
+    }
+  }, [searchParams]);
+
   return (
     <form role="search" onSubmit={onSubmit}>
       <Search
+        ref={searchRef}
         label="Søk i endringsloggen"
         defaultValue={searchParams?.get("fritekst") || ""}
         onChange={handleSearch}
