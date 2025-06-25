3ce8b50abd1ec11c8df2a02eac2fd6793701fe58
Vegard Haugstvedt <it.vegard@gmail.com>
2025-06-25 02:09:12 +0200

    Handle promoTag being null

--- aksel.nav.no/website/app/dev/(designsystemet)/designsystemet/_ui/DSLandingPageHeading.tsx
+++ aksel.nav.no/website/app/dev/(designsystemet)/designsystemet/_ui/DSLandingPageHeading.tsx
@@ -2,27 +2,30 @@ import { BodyLong, BoxNew, Heading, VStack } from "@navikt/ds-react";
 import { DS_FRONT_PAGE_QUERYResult } from "@/app/_sanity/query-types";
 import PromoTag from "./promo-tag/PromoTag";
 
-type PromoTag = NonNullable<DS_FRONT_PAGE_QUERYResult>["ds_forside_promo_tag"];
+type PromoTagType =
+  NonNullable<DS_FRONT_PAGE_QUERYResult>["ds_forside_promo_tag"];
 
 type Props = {
   title: NonNullable<DS_FRONT_PAGE_QUERYResult>["ds_forside_title"];
   introText: NonNullable<DS_FRONT_PAGE_QUERYResult>["ds_forside_ingress"];
   promoTag: {
-    label: NonNullable<PromoTag>["label"];
-    text: NonNullable<PromoTag>["text"];
-    link: NonNullable<PromoTag>["link"];
-  };
+    label: NonNullable<PromoTagType>["label"];
+    text: NonNullable<PromoTagType>["text"];
+    link: NonNullable<PromoTagType>["link"];
+  } | null;
 };
 
 const DSLandingPageHeading = ({ title, introText, promoTag }: Props) => (
   <BoxNew maxWidth="text">
     <VStack gap="space-16" maxWidth="600px" align="center">
       <VStack gap="space-8" align="center">
-        <PromoTag
-          label={promoTag.label}
-          text={promoTag.text}
-          link={promoTag.link}
-        />
+        {promoTag && (
+          <PromoTag
+            label={promoTag.label}
+            text={promoTag.text}
+            link={promoTag.link}
+          />
+        )}
         {title && (
           <Heading level="1" size="xlarge" className="aksel__page-title">
             {title}
