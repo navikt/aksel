400c0aac95ff1cd2cd455b9fcab7a84dec5c1cdc
stainawarjar <210375207+stainawarjar@users.noreply.github.com>
2025-06-26 07:06:13 +0200

    Move component definition out of FilterChips

--- aksel.nav.no/website/app/dev/(designsystemet)/grunnleggende/endringslogg/_ui/FilterChips.tsx
+++ aksel.nav.no/website/app/dev/(designsystemet)/grunnleggende/endringslogg/_ui/FilterChips.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { startTransition, useOptimistic } from "react";
-import { Chips, Label, VStack } from "@navikt/ds-react";
-import { capitalize } from "@/utils";
+import { VStack } from "@navikt/ds-react";
+import FilterGroup from "./FilterGroup";
 
 interface Props {
   years: string[];
@@ -18,86 +16,18 @@ export default function FilterChips({
   categories,
   selectedCategory,
 }: Props) {
-  const { push, prefetch } = useRouter();
-
-  const [optYear, expectYear] = useOptimistic(
-    selectedYear,
-    (_, optimisticYear: string | null) => optimisticYear,
-  );
-  const [optCategory, expectCategory] = useOptimistic(
-    selectedCategory,
-    (_, optimisticCategory: string | null) => optimisticCategory,
-  );
-  const pathname = usePathname();
-  const searchParams = useSearchParams()?.toString();
-  function getHref(option: string, type: "category" | "period") {
-    const params = new URLSearchParams(searchParams?.toString());
-    if (type === "period") {
-      params.set("periode", `${option === optYear ? "alle" : option}`);
-    } else if (type === "category") {
-      if (option === optCategory) {
-        params.delete("kategori");
-      } else {
-        params.set("kategori", option);
-      }
-    }
-    return `${pathname}?${params.toString()}`;
-  }
-
-  const FilterGroup = ({
-    type,
-    options,
-    selectedOption,
-    label,
-  }: {
-    type: "category" | "period";
-    options: string[];
-    selectedOption: string | number | null;
-    label: string;
-  }) => (
-    <VStack gap="space-8">
-      <Label as="div">{label}</Label>
-      <Chips>
-        {options.map((option) => {
-          const href = getHref(option, type);
-          return (
-            <Chips.Toggle
-              key={option}
-              selected={selectedOption === option}
-              checkmark={false}
-              variant="neutral"
-              onMouseEnter={() => {
-                prefetch(href);
-              }}
-              onClick={() => {
-                if (typeof option === "number") {
-                  startTransition(() => expectYear(option));
-                } else {
-                  startTransition(() => expectCategory(option));
-                }
-                push(href);
-              }}
-            >
-              {capitalize(option.toString())}
-            </Chips.Toggle>
-          );
-        })}
-      </Chips>
-    </VStack>
-  );
-
   return (
     <VStack gap="space-24">
       <FilterGroup
         type="period"
         options={years}
-        selectedOption={optYear}
+        selectedOption={selectedYear}
         label="År"
       />
       <FilterGroup
         type="category"
         options={categories}
-        selectedOption={optCategory}
+        selectedOption={selectedCategory}
         label="Kategori"
       />
     </VStack>
