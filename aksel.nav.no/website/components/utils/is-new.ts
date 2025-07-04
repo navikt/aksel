ecbf2ffc261ea75fa1207b79c3d1848c4c1a3b50
Ken <ken.aleksander@gmail.com>
2025-07-04 13:54:56 +0200

    :fire: Removed unused util-code

--- aksel.nav.no/website/components/utils/is-new.ts
+++
@@ -1,7 +0,0 @@
-export const isNew = (date: string) => {
-  const date1 = new Date(date);
-  const now = new Date();
-  const timeDiff = Math.abs(now.getTime() - date1.getTime());
-  const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-  return diffDays <= 90;
-};
