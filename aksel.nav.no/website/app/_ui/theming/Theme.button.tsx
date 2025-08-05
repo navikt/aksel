82757cf094eedda0531c379ddbddbaa5b5997bbe
Ken <ken.aleksander@gmail.com>
2025-08-05 09:28:54 +0200

    :recycle: Use tooltip for theme-switcher button

--- aksel.nav.no/website/app/_ui/theming/Theme.button.tsx
+++ aksel.nav.no/website/app/_ui/theming/Theme.button.tsx
@@ -3,7 +3,7 @@
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import { MoonIcon, SunIcon } from "@navikt/aksel-icons";
-import { Button } from "@navikt/ds-react";
+import { Button, Tooltip } from "@navikt/ds-react";
 
 function ThemeButton() {
   const { resolvedTheme, setTheme } = useTheme();
@@ -11,17 +11,43 @@ function ThemeButton() {
   useEffect(() => setIsMounted(true), []);
 
   return (
-    <Button
-      variant="tertiary-neutral"
-      icon={
-        isMounted && resolvedTheme === "dark" ? (
-          <MoonIcon title="Endre til lyst" />
-        ) : (
-          <SunIcon title="Endre til mørkt" />
-        )
-      }
-      onClick={() => setTheme(resolvedTheme === "light" ? "dark" : "light")}
-    />
+    <>
+      <style id="theme-button">{`
+        :root, .light {
+          --website-theme-toggle-light-display: block;
+          --website-theme-toggle-dark-display: none;
+        }
+
+        .dark {
+          --website-theme-toggle-light-display: none;
+          --website-theme-toggle-dark-display: block;
+        }
+      `}</style>
+      <Tooltip
+        content={
+          isMounted && resolvedTheme === "dark"
+            ? "Endre til lyst tema"
+            : "Endre til mørkt tema"
+        }
+      >
+        <Button
+          variant="tertiary-neutral"
+          icon={
+            <>
+              <MoonIcon
+                aria-hidden
+                style={{ display: "var(--website-theme-toggle-dark-display)" }}
+              />
+              <SunIcon
+                aria-hidden
+                style={{ display: "var(--website-theme-toggle-light-display)" }}
+              />
+            </>
+          }
+          onClick={() => setTheme(resolvedTheme === "light" ? "dark" : "light")}
+        />
+      </Tooltip>
+    </>
   );
 }
 
