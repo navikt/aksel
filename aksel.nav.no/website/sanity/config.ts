dc009805935657afe82c1225e443336e0eb07b13
Julian Nymark <julian.nymark@nav.no>
2025-08-12 12:11:16 +0200

    sync clientConfigs

--- aksel.nav.no/website/sanity/config.ts
+++ aksel.nav.no/website/sanity/config.ts
@@ -2,11 +2,7 @@ import { ClientConfig } from "@sanity/client";
 
 export const SANITY_PROJECT_ID = "hnbe3yhs";
 export const SANITY_API_VERSION = "2025-08-10";
-export let SANITY_DATASET = "production";
-
-if (process.env.LOCAL_DATASET_OVERRIDE === "development") {
-  SANITY_DATASET = "development";
-}
+export const SANITY_DATASET = "production";
 
 export const clientConfig: ClientConfig = {
   projectId: SANITY_PROJECT_ID,
