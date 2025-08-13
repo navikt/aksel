d376b7c09327f82e7a5aaef6eb2c6a67bc5c663d
Ken <ken.aleksander@gmail.com>
2025-08-13 10:33:53 +0200

    :fire: Deprecate headingSize-prop in Accordion

--- aksel.nav.no/website/pages/eksempler/accordion/size.tsx
+++ aksel.nav.no/website/pages/eksempler/accordion/size.tsx
@@ -7,7 +7,7 @@ const Example = () => {
       <Heading size="small" spacing textColor="subtle">
         size=large
       </Heading>
-      <Accordion size="large" headingSize="large">
+      <Accordion size="large">
         <Accordion.Item>
           <Accordion.Header>Til deg mellom 62 og 67 år</Accordion.Header>
           <Accordion.Content>
@@ -22,7 +22,7 @@ const Example = () => {
       <Heading size="small" spacing textColor="subtle">
         size=medium
       </Heading>
-      <Accordion size="medium" headingSize="medium">
+      <Accordion size="medium">
         <Accordion.Item>
           <Accordion.Header>Til deg mellom 62 og 67 år</Accordion.Header>
           <Accordion.Content>
@@ -37,7 +37,7 @@ const Example = () => {
       <Heading size="small" spacing textColor="subtle">
         size=small
       </Heading>
-      <Accordion size="small" headingSize="small">
+      <Accordion size="small">
         <Accordion.Item>
           <Accordion.Header>Til deg mellom 62 og 67 år</Accordion.Header>
           <Accordion.Content>
@@ -48,20 +48,6 @@ const Example = () => {
         </Accordion.Item>
       </Accordion>
       <br />
-
-      <Heading size="small" spacing textColor="subtle">
-        size=small headingSize=xsmall
-      </Heading>
-      <Accordion size="small" headingSize="xsmall">
-        <Accordion.Item>
-          <Accordion.Header>Til deg mellom 62 og 67 år</Accordion.Header>
-          <Accordion.Content>
-            Hvis du er mellom 62 og 67 år når du søker, må du som hovedregel ha
-            hatt en pensjonsgivende inntekt som tilsvarer x G, året før du fikk
-            nedsatt arbeidsevnen. Nav kan gjøre <Link href="#">unntak</Link>.
-          </Accordion.Content>
-        </Accordion.Item>
-      </Accordion>
     </>
   );
 };
