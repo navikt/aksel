8da3803cd85f84bc5ad23f94e3825793c5dc7cb1
Julian Nymark <julian.nymark@nav.no>
2025-08-14 13:32:02 +0200

    now support semver range searches

--- aksel.nav.no/website/app/(routes)/(designsystemet)/grunnleggende/endringslogg/page.tsx
+++ aksel.nav.no/website/app/(routes)/(designsystemet)/grunnleggende/endringslogg/page.tsx
@@ -2,7 +2,11 @@ import { format } from "date-fns";
 import { nb } from "date-fns/locale";
 import { defineQuery } from "next-sanity";
 import { Metadata } from "next/types";
-import { validRange as semverValidRange } from "semver";
+import {
+  coerce as semverCoerce,
+  satisfies as semverSatisfies,
+  validRange as semverValidRange,
+} from "semver";
 import { Heading, VStack } from "@navikt/ds-react";
 import { PageProps } from "@/app/(routes)/next-types";
 import { sanityFetch } from "@/app/_sanity/live";
@@ -54,59 +58,71 @@ export default async function Page({ searchParams }: PageProps) {
     ? paramCategory.toString()
     : null;
 
-  /**
-   * Proposed change
-   * - Check if paramTextFilter is a valid semver version range with `validRange`:  console.info("valid", semverValidRange(">=1.0.0 <2.0.0"));
-   * - If it is, assume we want to filter by semver version
-   * - - We probably want to ignore periode and kategori filters when this is true
-   * - If it is not, treat it as a regular text filter
-   * - When true we need to do a few things:
-   * - - Fetch all "code" entries: Only need title and _id
-   * - - Filter the entries by semver version range using `satisfies`: console.info(semverSatisfies("1.0.1", ">=1.0.0 <2.0.0"));
-   * - - Now we can fetch the full entries for the filtered results.
-   * - - @note: Might be just as fast to fetch code all entries, needs to be tested.
-   *
-   */
-  const isSemverSearch = semverValidRange(paramTextFilter.toString().trim());
-  console.warn("isSemver:", isSemverSearch, paramTextFilter.toString().trim());
+  const searchText = paramTextFilter.toString().trim();
+  const textFilter = searchText.split(" ");
 
-  const textFilter = paramTextFilter.toString().trim().split(" ");
+  let logEntries: ENDRINGSLOGG_QUERYResult = [];
 
-  const yearQuery = yearFilter
-    ? " && endringsdato >= $year && endringsdato <= $nextYear"
-    : "";
-  const categoryQuery = categoryFilter ? " && endringstype == $category" : "";
-  const textQuery =
-    // content[].caption refers captions on images in content
-    // "[heading, endringsdato, endringstype, pt::text(content[]), content[].caption]";
-    "[heading, endringsdato, endringstype, content[].children[].text, content[].caption]";
+  const isSemverSearch = textFilter[0] === "semver";
 
-  const sanityObject = {
-    query: defineQuery(
-      `*[_type == "ds_endringslogg_artikkel"${yearQuery}${categoryQuery}${
-        textFilter
-          ? textFilter.reduce(
-              (queryString, _, index) =>
-                `${queryString} && ${textQuery} match $textFilter${index}`,
-              "",
-            )
-          : ""
-      }]{${ENDRINGSLOGG_FIELDS}} | order(endringsdato desc)`,
-    ) as typeof ENDRINGSLOGG_QUERY,
-    params: {
-      year: `${yearFilter}`,
-      nextYear: `${yearFilter && +yearFilter + 1}`,
-      category: `${categoryFilter}`,
-      ...(textFilter
-        ? textFilter?.reduce((acc, text, index) => {
-            acc[`textFilter${index}`] = `*${text}*`;
-            return acc;
-          }, {})
-        : {}),
-    },
-  };
+  if (isSemverSearch) {
+    textFilter.shift();
+    const semverRange = semverValidRange(textFilter.join());
+    console.warn("isSemver:", isSemverSearch, semverRange);
 
-  const { data: logEntries } = await sanityFetch(sanityObject);
+    // OPTIMIZE: we fetch all for now and filter those after sanity request
+    const sanityObject = {
+      query: defineQuery(
+        `*[_type == "ds_endringslogg_artikkel" && endringstype == "kode"]{
+          ${ENDRINGSLOGG_FIELDS}
+        } | order(endringsdato desc)`,
+      ) as typeof ENDRINGSLOGG_QUERY,
+    };
+
+    logEntries = (await sanityFetch(sanityObject)).data;
+    logEntries = logEntries.filter((entry) => {
+      return semverSatisfies(
+        semverCoerce(entry.heading) || "",
+        semverRange || "",
+      );
+    });
+  } else {
+    const yearQuery = yearFilter
+      ? " && endringsdato >= $year && endringsdato <= $nextYear"
+      : "";
+    const categoryQuery = categoryFilter ? " && endringstype == $category" : "";
+    const textQuery =
+      // content[].caption refers captions on images in content
+      // "[heading, endringsdato, endringstype, pt::text(content[]), content[].caption]";
+      "[heading, endringsdato, endringstype, content[].children[].text, content[].caption]";
+
+    const sanityObject = {
+      query: defineQuery(
+        `*[_type == "ds_endringslogg_artikkel"${yearQuery}${categoryQuery}${
+          textFilter
+            ? textFilter.reduce(
+                (queryString, _, index) =>
+                  `${queryString} && ${textQuery} match $textFilter${index}`,
+                "",
+              )
+            : ""
+        }]{${ENDRINGSLOGG_FIELDS}} | order(endringsdato desc)`,
+      ) as typeof ENDRINGSLOGG_QUERY,
+      params: {
+        year: `${yearFilter}`,
+        nextYear: `${yearFilter && +yearFilter + 1}`,
+        category: `${categoryFilter}`,
+        ...(textFilter
+          ? textFilter?.reduce((acc, text, index) => {
+              acc[`textFilter${index}`] = `*${text}*`;
+              return acc;
+            }, {})
+          : {}),
+      },
+    };
+
+    logEntries = (await sanityFetch(sanityObject)).data;
+  }
 
   logEntries.forEach((logEntry) => {
     logEntry.content = bumpHeadingLevels(logEntry.content) || null;
