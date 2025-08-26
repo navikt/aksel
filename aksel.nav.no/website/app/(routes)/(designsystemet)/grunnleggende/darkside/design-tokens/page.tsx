60e5aeba2d329f33f91b975c9fa7aea28dda7280
Ken <ken.aleksander@gmail.com>
2025-08-26 09:59:38 +0200

    :recycle: Design-token toc moved between filter and content resolving team-aksel#843

--- aksel.nav.no/website/app/(routes)/(designsystemet)/grunnleggende/darkside/design-tokens/page.tsx
+++ aksel.nav.no/website/app/(routes)/(designsystemet)/grunnleggende/darkside/design-tokens/page.tsx
@@ -6,6 +6,7 @@ import { DesignsystemetEyebrow } from "../../../_ui/Designsystemet.eyebrow";
 import { DesignsystemetPageLayout } from "../../../_ui/DesignsystemetPage";
 import TokenTableOfContents from "./_ui/TokenTableOfContents";
 import TokensPage from "./_ui/TokensPage";
+import Toolbar from "./_ui/toolbar/Toolbar";
 
 /* Since page relies on searchparams for general navigation, we avoid Suspense by foce-dynamic */
 export const dynamic = "force-dynamic";
@@ -20,44 +21,43 @@ export const metadata: Metadata = {
 const Page = async () => {
   return (
     <DesignsystemetPageLayout layout="with-toc">
-      <VStack gap="space-40">
-        <VStack gap="space-24">
-          <VStack>
-            <DesignsystemetEyebrow type="ds_artikkel" />
-            <Heading level="1" size="xlarge" data-aksel-heading-color>
-              Tokenoversikt
-            </Heading>
-            <BodyLong size="large">
-              Tokens er alle farger, avstander, typografi, osv. som vi bruker i
-              komponenter og layout.
-            </BodyLong>
-          </VStack>
-          <VStack gap="space-16">
-            <HStack gap="space-16" align="center">
-              <Link href="https://github.com/navikt/aksel" variant="neutral">
-                <GithubIcon aria-hidden />
-                GitHub
-              </Link>
-              <Link
-                href="https://www.figma.com/community/file/1214869602572392330"
-                variant="neutral"
-              >
-                <FigmaIcon aria-hidden />
-                Figma-community
-              </Link>
-              <Link
-                href="https://aksel.nav.no/grunnleggende/kode/endringslogg"
-                variant="neutral"
-              >
-                <ClockDashedIcon aria-hidden />
-                Endringslogg
-              </Link>
-            </HStack>
-          </VStack>
+      <VStack gap="space-8">
+        <VStack marginBlock="0 space-16">
+          <DesignsystemetEyebrow type="ds_artikkel" />
+          <Heading level="1" size="xlarge" data-aksel-heading-color>
+            Tokenoversikt
+          </Heading>
+          <BodyLong size="large">
+            Tokens er alle farger, avstander, typografi, osv. som vi bruker i
+            komponenter og layout.
+          </BodyLong>
         </VStack>
-        <TokensPage />
+        <VStack gap="space-16" marginBlock="0 space-16">
+          <HStack gap="space-16" align="center">
+            <Link href="https://github.com/navikt/aksel" variant="neutral">
+              <GithubIcon aria-hidden />
+              GitHub
+            </Link>
+            <Link
+              href="https://www.figma.com/community/file/1214869602572392330"
+              variant="neutral"
+            >
+              <FigmaIcon aria-hidden />
+              Figma-community
+            </Link>
+            <Link
+              href="https://aksel.nav.no/grunnleggende/kode/endringslogg"
+              variant="neutral"
+            >
+              <ClockDashedIcon aria-hidden />
+              Endringslogg
+            </Link>
+          </HStack>
+        </VStack>
+        <Toolbar />
       </VStack>
       <TokenTableOfContents />
+      <TokensPage />
     </DesignsystemetPageLayout>
   );
 };
