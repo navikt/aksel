7380abeef27b555dfc951537ed34affd54f60d64
Ken <ken.aleksander@gmail.com>
2025-09-01 22:52:46 +0200

    :tada: Added animation utils

---
+++ @navikt/core/react/src/infocard/collapsible/useAnimationFinished.ts
@@ -0,0 +1,85 @@
+"use client";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { useAnimationFrame } from "./useAnimationFrame";
+import { useEventCallback } from "./useEventCallback";
+
+/**
+ * Executes a function once all animations have finished on the provided element.
+ * @param elementOrRef - The element to watch for animations.
+ * @param waitForNextTick - Whether to wait for the next tick before checking for animations.
+ */
+export function useAnimationsFinished(
+  elementOrRef: React.RefObject<HTMLElement | null> | HTMLElement | null,
+  waitForNextTick = false,
+) {
+  const frame = useAnimationFrame();
+
+  return useEventCallback(
+    (
+      /**
+       * A function to execute once all animations have finished.
+       */
+      fnToExecute: () => void,
+      /**
+       * An optional [AbortSignal](https://developer.mozilla.org/en-US/docs/Web/API/AbortSignal) that
+       * can be used to abort `fnToExecute` before all the animations have finished.
+       * @default null
+       */
+      signal: AbortSignal | null = null,
+    ) => {
+      frame.cancel();
+
+      if (elementOrRef == null) {
+        return;
+      }
+
+      let element: HTMLElement;
+      if ("current" in elementOrRef) {
+        if (elementOrRef.current == null) {
+          return;
+        }
+
+        element = elementOrRef.current;
+      } else {
+        element = elementOrRef;
+      }
+
+      if (
+        typeof element.getAnimations !== "function" ||
+        globalThis.AKSEL_ANIMATIONS_DISABLED
+      ) {
+        fnToExecute();
+      } else {
+        frame.request(() => {
+          function exec() {
+            if (!element) {
+              return;
+            }
+
+            Promise.allSettled(
+              element.getAnimations().map((anim) => anim.finished),
+            ).then(() => {
+              if (signal?.aborted) {
+                return;
+              }
+              /*
+               * Ensures the state change is committed before the browser paints that frame,
+               * making timing deterministic.
+               */
+              ReactDOM.flushSync(fnToExecute);
+            });
+          }
+
+          /* `open: true` animations need to wait for the next tick to be detected */
+          if (waitForNextTick) {
+            frame.request(exec);
+          } else {
+            exec();
+          }
+        });
+      }
+    },
+  );
+}
---
+++ @navikt/core/react/src/infocard/collapsible/useCollapsibleRoot.ts
@@ -0,0 +1,237 @@
+"use client";
+
+import React, { useMemo } from "react";
+import { useClientLayoutEffect, useId } from "../../util";
+import { useControllableState } from "../../util/hooks/useControllableState";
+import { useAnimationsFinished } from "./useAnimationFinished";
+import { useEventCallback } from "./useEventCallback";
+import { useTransitionStatus } from "./useTransitionStatus";
+
+type AnimationType = "css-transition" | "css-animation" | "none" | null;
+/* TODO: https://github.com/mui/base-ui/blob/master/packages/react/src/utils/useOpenChangeComplete.tsx#L10 */
+
+interface Dimensions {
+  height: number | undefined;
+  width: number | undefined;
+}
+
+interface UseCollapsibleRootProps {
+  /**
+   * Whether the collapsible panel is currently open and controlled.
+   */
+  open?: boolean;
+  /**
+   * Whether the collapsible panel is initially open. Assumes uncontrolled behavior.
+   * @default false
+   */
+  defaultOpen?: boolean;
+  /**
+   * Event handler called when the panel is opened or closed.
+   */
+  onOpenChange: (open: boolean) => void;
+  /**
+   * Whether the component should ignore user interaction.
+   * @default false
+   */
+  disabled: boolean;
+}
+
+function useCollapsibleRoot(
+  input: UseCollapsibleRootProps,
+): useCollapsibleRoot.ReturnValue {
+  const {
+    open: openParam,
+    defaultOpen = false,
+    onOpenChange,
+    disabled,
+  } = input;
+
+  const isControlled = openParam !== undefined;
+
+  const [open, setOpen] = useControllableState({
+    value: openParam,
+    defaultValue: defaultOpen,
+    /* onChange: onOpenChange, */
+  });
+
+  const { mounted, setMounted, transitionStatus } = useTransitionStatus(
+    open,
+    true,
+    true,
+  );
+
+  const [visible, setVisible] = React.useState(open);
+
+  const [{ height, width }, setDimensions] = React.useState<Dimensions>({
+    height: undefined,
+    width: undefined,
+  });
+
+  const defaultPanelId = useId();
+
+  const [panelIdState, setPanelIdState] = React.useState<string | undefined>();
+  const panelId = panelIdState ?? defaultPanelId;
+
+  const [hiddenUntilFound, setHiddenUntilFound] = React.useState(false);
+  const [keepMounted, setKeepMounted] = React.useState(false);
+
+  const abortControllerRef = React.useRef<AbortController | null>(null);
+  const animationTypeRef = React.useRef<AnimationType>(null);
+  const transitionDimensionRef = React.useRef<"width" | "height" | null>(null);
+  const panelRef: React.RefObject<HTMLElement | null> = React.useRef(null);
+
+  const runOnceAnimationsFinish = useAnimationsFinished(panelRef, false);
+
+  const handleTrigger = useEventCallback(() => {
+    const nextOpen = !open;
+
+    const panel = panelRef.current;
+
+    if (animationTypeRef.current === "css-animation" && panel != null) {
+      panel.style.removeProperty("animation-name");
+    }
+
+    if (!hiddenUntilFound && !keepMounted) {
+      if (
+        animationTypeRef.current != null &&
+        animationTypeRef.current !== "css-animation"
+      ) {
+        if (!mounted && nextOpen) {
+          setMounted(true);
+        }
+      }
+
+      if (animationTypeRef.current === "css-animation") {
+        if (!visible && nextOpen) {
+          setVisible(true);
+        }
+        if (!mounted && nextOpen) {
+          setMounted(true);
+        }
+      }
+    }
+
+    setOpen(nextOpen);
+    onOpenChange(nextOpen);
+
+    if (animationTypeRef.current === "none") {
+      if (mounted && !nextOpen) {
+        setMounted(false);
+      }
+    }
+  });
+
+  useClientLayoutEffect(() => {
+    /**
+     * Unmount immediately when closing in controlled mode and keepMounted={false}
+     * and no CSS animations or transitions are applied
+     */
+    if (
+      isControlled &&
+      animationTypeRef.current === "none" &&
+      !keepMounted &&
+      !open
+    ) {
+      setMounted(false);
+    }
+  }, [isControlled, keepMounted, open, openParam, setMounted]);
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  return useMemo(
+    () => ({
+      abortControllerRef,
+      animationTypeRef,
+      disabled,
+      handleTrigger,
+      height,
+      mounted,
+      open,
+      panelId,
+      panelRef,
+      runOnceAnimationsFinish,
+      setDimensions,
+      setHiddenUntilFound,
+      setKeepMounted,
+      setMounted,
+      setOpen,
+      setPanelIdState,
+      setVisible,
+      transitionDimensionRef,
+      transitionStatus,
+      visible,
+      width,
+    }),
+    [
+      abortControllerRef,
+      animationTypeRef,
+      disabled,
+      handleTrigger,
+      height,
+      mounted,
+      open,
+      panelId,
+      panelRef,
+      runOnceAnimationsFinish,
+      setDimensions,
+      setHiddenUntilFound,
+      setKeepMounted,
+      setMounted,
+      setOpen,
+      setVisible,
+      transitionDimensionRef,
+      transitionStatus,
+      visible,
+      width,
+    ],
+  );
+}
+
+export { useCollapsibleRoot };
+
+export namespace useCollapsibleRoot {
+  export interface ReturnValue {
+    abortControllerRef: React.RefObject<AbortController | null>;
+    animationTypeRef: React.RefObject<AnimationType>;
+    /**
+     * Whether the component should ignore user interaction.
+     */
+    disabled: boolean;
+    handleTrigger: () => void;
+    /**
+     * The height of the panel.
+     */
+    height: number | undefined;
+    /**
+     * Whether the collapsible panel is currently mounted.
+     */
+    mounted: boolean;
+    /**
+     * Whether the collapsible panel is currently open.
+     */
+    open: boolean;
+    panelId: React.HTMLAttributes<Element>["id"];
+    panelRef: React.RefObject<HTMLElement | null>;
+    runOnceAnimationsFinish: (
+      fnToExecute: () => void,
+      signal?: AbortSignal | null,
+    ) => void;
+    setDimensions: React.Dispatch<React.SetStateAction<Dimensions>>;
+    setHiddenUntilFound: React.Dispatch<React.SetStateAction<boolean>>;
+    setKeepMounted: React.Dispatch<React.SetStateAction<boolean>>;
+    setMounted: (open: boolean) => void;
+    setOpen: (open: boolean) => void;
+    setPanelIdState: (id: string | undefined) => void;
+    setVisible: React.Dispatch<React.SetStateAction<boolean>>;
+    transitionDimensionRef: React.RefObject<"height" | "width" | null>;
+    transitionStatus: TransitionStatus;
+    /**
+     * The visible state of the panel used to determine the `[hidden]` attribute
+     * only when CSS keyframe animations are used.
+     */
+    visible: boolean;
+    /**
+     * The width of the panel.
+     */
+    width: number | undefined;
+  }
+}
