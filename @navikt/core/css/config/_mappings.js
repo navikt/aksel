f3c111a43f905f2dff194289ad938f076ae5388c
Ken <ken.aleksander@gmail.com>
2025-09-10 20:35:54 +0200

    :memo: Added CSS mappings

--- @navikt/core/css/config/_mappings.js
+++ @navikt/core/css/config/_mappings.js
@@ -145,6 +145,11 @@ const StyleMappings = {
     },
     { component: "Ingress", main: typoCss },
     {
+      component: "InfoCard",
+      main: "info-card.css",
+      dependencies: [typoCss],
+    },
+    {
       component: "InternalHeader",
       main: "internalheader.css",
       dependencies: [typoCss],
