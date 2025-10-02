7f48fc55b8197450db13acaddac7d4318c025135
Ken <ken.aleksander@gmail.com>
2025-10-02 12:52:12 +0200

    :recycle: Clean up use in debounce

--- @navikt/core/react/src/util/debounce.ts
+++ @navikt/core/react/src/util/debounce.ts
@@ -9,10 +9,9 @@ export default function debounce<T extends unknown[]>(
   leading = false,
 ) {
   const timeout = new Timeout();
-
   function debounced(this: any, ...args: T) {
     const later = () => {
-      // Clear internal state (id set to EMPTY) before invoking for consistent isStarted logic
+      // Clear first to mark idle before invoking callback
       timeout.clear();
       func.apply(this, args);
     };
