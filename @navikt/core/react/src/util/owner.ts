bf37dfceb13013a92a40da1b2b82f49a324c9d0c
Ken <ken.aleksander@gmail.com>
2025-10-02 10:07:44 +0200

    :tada: Added browser API utils

---
+++ @navikt/core/react/src/util/owner.ts
@@ -0,0 +1,15 @@
+/**
+ * Returns the owner document of a given element.
+ */
+function ownerDocument(node: Element | null) {
+  return node?.ownerDocument || document;
+}
+
+/**
+ * Returns the owner window of a given element.
+ */
+function ownerWindow(node: Document | Element | null): typeof window {
+  return node?.ownerDocument?.defaultView || window;
+}
+
+export { ownerDocument, ownerWindow };
