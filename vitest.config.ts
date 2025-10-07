13d47ed23ee8e99637e5324705c6db5c6f6547b1
Ken <ken.aleksander@gmail.com>
2025-10-07 16:13:10 +0200

    :tada: Run only local stories, not node_modules

---
+++ vitest.config.ts
@@ -0,0 +1,32 @@
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+import path from "node:path";
+import { defineConfig } from "vitest/config";
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
+export default defineConfig({
+  test: {
+    projects: [
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({
+            configDir: path.join(__dirname, ".storybook"),
+            tags: { exclude: ["skip-test"] },
+          }),
+        ],
+        test: {
+          name: "storybook",
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: "playwright",
+            instances: [{ browser: "chromium" }],
+          },
+          setupFiles: [".storybook/vitest.setup.ts"],
+        },
+      },
+    ],
+  },
+});
