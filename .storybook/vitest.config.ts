4bb2eb3c00d5b9a8e16912c9c094f9c02d7e63be
Ken <ken.aleksander@gmail.com>
2025-10-07 16:19:58 +0200

    :truck: Move test-config to separate dir

--- .storybook/vitest.config.ts
+++ .storybook/vitest.config.ts
@@ -1,5 +1,4 @@
 import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
-import path from "node:path";
 import { defineConfig } from "vitest/config";
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
@@ -12,7 +11,7 @@ export default defineConfig({
           // The plugin will run tests for the stories defined in your Storybook config
           // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
           storybookTest({
-            configDir: path.join(__dirname, ".storybook"),
+            configDir: "./.storybook",
             tags: { exclude: ["skip-test"] },
           }),
         ],
@@ -24,7 +23,7 @@ export default defineConfig({
             provider: "playwright",
             instances: [{ browser: "chromium" }],
           },
-          setupFiles: [".storybook/vitest.setup.ts"],
+          setupFiles: ["./.storybook/vitest.setup.ts"],
         },
       },
     ],
