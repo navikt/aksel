182753c8d2b5333d779ba1d2ac87a6cf652861a9
Ken <26967723+KenAJoh@users.noreply.github.com>
2025-10-08 14:33:31 +0200

    [Vitest] Run storybook tests in CI (#4163)
    * :tada: Run only local stories, not node_modules
    
    * :truck: Move test-config to separate dir
    
    * :tada: Added UI-tests to CI action
    
    * :bug: Fix test-command used in action
    
    * :memo: rename action
    
    * :bug: Add storybook command with CI-flag
    
    * :test_tube: Add forced file paralellism
    
    * :zap: only test play-fn tests
    
    * :fire: Remove a11y annotation
    
    * :fire: Cleanup after fixing performance
    
    * :bug: Allow test-runner widget to run without crash
    
    * Update .storybook/main.ts
    
    Co-authored-by: Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>
    
    * :memo: Name update
    
    ---------
    
    Co-authored-by: Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -86,6 +86,13 @@ export default {
     docs: {
       codePanel: true,
     },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: "todo",
+    },
   },
 
   globalTypes: {
---
+++ vitest.config.ts
@@ -0,0 +1,32 @@
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+import { defineConfig } from "vitest/config";
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
+export default defineConfig({
+  test: {
+    projects: [
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({
+            configDir: "./.storybook",
+            tags: { exclude: ["skip-test"], include: ["play-fn"] },
+            storybookScript: "yarn storybook --ci",
+          }),
+        ],
+        test: {
+          name: "storybook",
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: "playwright",
+            instances: [{ browser: "chromium" }],
+          },
+          setupFiles: [".storybook/vitest.setup.ts"],
+        },
+      },
+    ],
+  },
+});
