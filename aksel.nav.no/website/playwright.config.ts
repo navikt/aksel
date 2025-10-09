4ff8c0fd52c62ced0aaf37b1d4c23d29c4bd2e60
Ken <ken.aleksander@gmail.com>
2025-10-09 20:59:18 +0200

    :zap: Test increasing workers to 2 in CI

--- aksel.nav.no/website/playwright.config.ts
+++ aksel.nav.no/website/playwright.config.ts
@@ -42,7 +42,7 @@ const config: PlaywrightTestConfig = {
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   reporter: process.env.CI ? "blob" : "html",
   use: {
     baseURL: opts.baseURL,
