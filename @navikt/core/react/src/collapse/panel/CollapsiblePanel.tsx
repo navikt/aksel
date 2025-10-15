a90e61513802bc60b464d5f2d7fcf2ad548e3217
Ken <ken.aleksander@gmail.com>
2025-10-15 12:00:52 +0200

    :recycle: Add production warning

--- @navikt/core/react/src/collapse/panel/CollapsiblePanel.tsx
+++ @navikt/core/react/src/collapse/panel/CollapsiblePanel.tsx
@@ -1,11 +1,22 @@
 import React, { forwardRef } from "react";
 import type { AsChildProps } from "../../util/types";
+import { useCollapsibleRootContext } from "../root/CollapsibleRoot.context";
 
 type CollapsiblePanelProps = React.HTMLAttributes<HTMLDivElement> &
   AsChildProps;
 
 const CollapsiblePanel = forwardRef<HTMLDivElement, CollapsiblePanelProps>(
   ({ children, ...rest }: CollapsiblePanelProps, forwardedRef) => {
+    const { hiddenUntilFound, keepMounted } = useCollapsibleRootContext();
+
+    if (process.env.NODE_ENV !== "production") {
+      if (hiddenUntilFound && keepMounted === false) {
+        console.warn(
+          "The `keepMounted={false}` prop on a Collapsible will be ignored when using `hiddenUntilFound` since it requires the Panel to remain mounted even when closed.",
+        );
+      }
+    }
+
     return (
       <div ref={forwardedRef} {...rest}>
         {children}
