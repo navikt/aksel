bcc94bbae52d7f91475bf701a4de9ec9eb2d7c1a
Ken <ken.aleksander@gmail.com>
2025-10-15 11:56:48 +0200

    :sparkles: Set up trigger

--- @navikt/core/react/src/collapse/trigger/CollapsibleTrigger.tsx
+++ @navikt/core/react/src/collapse/trigger/CollapsibleTrigger.tsx
@@ -1,5 +1,8 @@
 import React, { forwardRef } from "react";
+import { Slot } from "../../slot/Slot";
+import { composeEventHandlers } from "../../util/composeEventHandlers";
 import type { AsChildProps } from "../../util/types";
+import { useCollapsibleRootContext } from "../root/CollapsibleRoot.context";
 
 type CollapsibleTriggerProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
   AsChildProps;
@@ -7,13 +10,36 @@ type CollapsibleTriggerProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
 const CollapsibleTrigger = forwardRef<
   HTMLButtonElement,
   CollapsibleTriggerProps
->(({ children, ...rest }: CollapsibleTriggerProps, forwardedRef) => {
-  return (
-    <button ref={forwardedRef} {...rest}>
-      {children}
-    </button>
-  );
-});
+>(
+  (
+    {
+      children,
+      asChild,
+      disabled: disabledProp,
+      onClick,
+      ...rest
+    }: CollapsibleTriggerProps,
+    forwardedRef,
+  ) => {
+    const { open, panelId, disabled, handleTrigger } =
+      useCollapsibleRootContext();
+
+    const Component = asChild ? Slot : "button";
+
+    return (
+      <Component
+        ref={forwardedRef}
+        {...rest}
+        aria-controls={open ? panelId : undefined}
+        aria-expanded={open}
+        disabled={disabledProp ?? disabled}
+        onClick={composeEventHandlers(onClick, handleTrigger)}
+      >
+        {children}
+      </Component>
+    );
+  },
+);
 
 export { CollapsibleTrigger };
 export type { CollapsibleTriggerProps };
