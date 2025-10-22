a7b06b9b80b7bac26bce7e374076a714d4b8015c
Ken <26967723+KenAJoh@users.noreply.github.com>
2025-10-22 10:34:16 +0200

    Update @navikt/core/react/src/inline-message/root/InlineMessage.tsx
    Co-authored-by: Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>

--- @navikt/core/react/src/inline-message/root/InlineMessage.tsx
+++ @navikt/core/react/src/inline-message/root/InlineMessage.tsx
@@ -39,7 +39,7 @@ interface InlineMessageProps extends React.HTMLAttributes<HTMLDivElement> {
  */
 const InlineMessage: OverridableComponent<
   InlineMessageProps,
-  HTMLAnchorElement
+  HTMLDivElement
 > = forwardRef(
   (
     {
