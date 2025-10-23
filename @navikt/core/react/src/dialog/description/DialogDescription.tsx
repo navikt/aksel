ebafce5b9ee1a7506e8775bb8c05293e754f665f
Ken <ken.aleksander@gmail.com>
2025-10-23 19:59:02 +0200

    :bug: Fix imports

--- @navikt/core/react/src/dialog/description/DialogDescription.tsx
+++ @navikt/core/react/src/dialog/description/DialogDescription.tsx
@@ -2,33 +2,34 @@ import React, { forwardRef } from "react";
 import { useRenameCSS } from "../../theme/Theme";
 import { BodyShort } from "../../typography";
 
-type DialogTitleProps = React.HTMLAttributes<HTMLParagraphElement>;
+type DialogDescriptionProps = React.HTMLAttributes<HTMLParagraphElement>;
 
 /**
- * @see 🏷️ {@link DialogTitleProps}
+ * @see 🏷️ {@link DialogDescriptionProps}
  * @example
  * ```jsx
  * ```
  * TODO: Handle size
  */
-const DialogTitle = forwardRef<HTMLParagraphElement, DialogTitleProps>(
-  ({ className, children, ...restProps }, forwardedRef) => {
-    const { cn } = useRenameCSS();
+const DialogDescription = forwardRef<
+  HTMLParagraphElement,
+  DialogDescriptionProps
+>(({ className, children, ...restProps }, forwardedRef) => {
+  const { cn } = useRenameCSS();
 
-    return (
-      <BodyShort
-        {...restProps}
-        ref={forwardedRef}
-        className={cn(className, "navds-dialog__title")}
-        size="medium"
-        data-color="neutral"
-        textColor="subtle"
-      >
-        {children}
-      </BodyShort>
-    );
-  },
-);
+  return (
+    <BodyShort
+      {...restProps}
+      ref={forwardedRef}
+      className={cn(className, "navds-dialog__description")}
+      size="medium"
+      data-color="neutral"
+      textColor="subtle"
+    >
+      {children}
+    </BodyShort>
+  );
+});
 
-export { DialogTitle };
-export type { DialogTitleProps };
+export { DialogDescription };
+export type { DialogDescriptionProps };
