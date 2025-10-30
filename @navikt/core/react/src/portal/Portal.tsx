f660f9d515296c0afe7ec571f498d8842c674d79
Ken <ken.aleksander@gmail.com>
2025-10-30 12:05:47 +0100

    :fire: Revert portal changes

--- @navikt/core/react/src/portal/Portal.tsx
+++ @navikt/core/react/src/portal/Portal.tsx
@@ -1,6 +1,6 @@
 import React, { HTMLAttributes, forwardRef } from "react";
 import ReactDOM from "react-dom";
-import { PortalContextProvider, usePortalNode } from "../dialog/usePortalNode";
+import { useProvider } from "../provider/Provider";
 import { Slot } from "../slot/Slot";
 import { Theme, useThemeInternal } from "../theme/Theme";
 import { AsChildProps } from "../util/types";
@@ -17,10 +17,8 @@ export type PortalProps = PortalBaseProps & AsChildProps;
 export const Portal = forwardRef<HTMLDivElement, PortalProps>(
   ({ rootElement, asChild, ...rest }, ref) => {
     const themeContext = useThemeInternal(false);
-
-    const { portalNode, portalSubtree } = usePortalNode({
-      root: rootElement,
-    });
+    const contextRoot = useProvider()?.rootElement;
+    const root = rootElement ?? contextRoot ?? globalThis?.document?.body;
 
     const Component = asChild ? Slot : "div";
 
@@ -29,29 +27,26 @@ export const Portal = forwardRef<HTMLDivElement, PortalProps>(
      * If a theme is present, we want to make sure that theme cascades to portaled element.
      */
     if (themeContext?.isDarkside) {
-      return (
-        <>
-          {portalSubtree}
-          <PortalContextProvider portalNode={portalNode}>
-            {portalNode &&
-              ReactDOM.createPortal(
-                <Theme
-                  theme={themeContext.theme}
-                  asChild
-                  hasBackground={false}
-                  data-color={themeContext.color}
-                >
-                  <Component ref={ref} {...rest} />
-                </Theme>,
-                portalNode,
-              )}
-          </PortalContextProvider>
-        </>
-      );
+      return root
+        ? ReactDOM.createPortal(
+            <Theme
+              theme={themeContext.theme}
+              asChild
+              hasBackground={false}
+              data-color={themeContext.color}
+            >
+              <Component ref={ref} data-aksel-portal="" {...rest} />
+            </Theme>,
+            root,
+          )
+        : null;
     }
 
-    return portalNode
-      ? ReactDOM.createPortal(<Component ref={ref} {...rest} />, portalNode)
+    return root
+      ? ReactDOM.createPortal(
+          <Component ref={ref} data-aksel-portal="" {...rest} />,
+          root,
+        )
       : null;
   },
 );
