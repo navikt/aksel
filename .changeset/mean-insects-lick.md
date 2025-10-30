61a52aa5dde7c61e7f01a25914baa01f70fca649
Ken <26967723+KenAJoh@users.noreply.github.com>
2025-10-30 12:03:48 +0100

    [Process] New prop `isTruncated` (#4196)
    * :test_tube: set up views for Process line
    
    * :bug: Handle connectorplacement for dot-version
    
    * :memo: Docs for use of connectorPlacement
    
    * :recycle: Implement new connector prop for legacy
    
    * :memo: Changeset
    
    * :memo: Changeset edit
    
    * :bug: Use border-image unset instead of none to help with lightningcss minification
    
    * Update .changeset/mean-insects-lick.md
    
    Co-authored-by: Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>
    
    * Update aksel.nav.no/website/pages/eksempler/process/connector.tsx
    
    Co-authored-by: Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>
    
    * :memo: Add connector-stories to chromatic
    
    * :tada: Update Process-prop from connectorPlacement -> isTruncated
    
    * :memo: Update website docs for new isTruncated prop in Process
    
    * Update aksel.nav.no/website/pages/eksempler/process/truncated.tsx
    
    Co-authored-by: Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>

---
+++ .changeset/mean-insects-lick.md
@@ -0,0 +1,6 @@
+---
+"@navikt/ds-react": minor
+"@navikt/ds-css": minor
+---
+
+Process: Added new prop `isTruncated` to `<Process />`. This allows you to add a connector to the start of first event and end of last event to visualize a "truncated" process.
---
+++ aksel.nav.no/website/pages/eksempler/process/truncated.tsx
@@ -0,0 +1,37 @@
+import { BodyShort, Process } from "@navikt/ds-react";
+import { withDsExample } from "@/web/examples/withDsExample";
+
+const Example = () => {
+  return (
+    <Process isTruncated="both">
+      <Process.Event title="02. Jan. 2020. klokken 10:21">
+        Din utbetalingsplan har blitt oppdatert, les vedtaket for mer
+        informasjon
+      </Process.Event>
+      <Process.Event title="27. Des. 2019. klokken 14:48">
+        <BodyShort>Livsopphold, Husleie er ferdig behandlet</BodyShort>
+        <BodyShort textColor="subtle" size="small">
+          Et brev fra veileder følger med denne hendelsen
+        </BodyShort>
+      </Process.Event>
+      <Process.Event title="21. Des. 2019. klokken 16:52">
+        Du har sendt 10 vedlegg til NAV
+      </Process.Event>
+    </Process>
+  );
+};
+
+// EXAMPLES DO NOT INCLUDE CONTENT BELOW THIS LINE
+export default withDsExample(Example, {
+  variant: "static",
+});
+
+/* Storybook story */
+export const Demo = {
+  render: Example,
+};
+
+export const args = {
+  index: 5,
+  desc: "`isTruncated` legger til en linje før første og/eller etter siste hendelse. Dette brukes for å illustrere at prosessen har flere hendelser før/etter enn det som vises.",
+};
