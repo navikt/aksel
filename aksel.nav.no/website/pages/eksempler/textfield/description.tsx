cbdc631493d5d39821e9f37a9c343c5834967129
Halvor Haugan <halvor.haugan@nav.no>
2025-10-31 12:30:36 +0100

    backticks

--- aksel.nav.no/website/pages/eksempler/textfield/description.tsx
+++ aksel.nav.no/website/pages/eksempler/textfield/description.tsx
@@ -15,5 +15,5 @@ export const Demo = {
 
 export const args = {
   index: 2,
-  desc: "Bruk description i tillegg til label når det er behov for mer utfyllende forklaring.",
+  desc: "Bruk `description` i tillegg til `label` når det er behov for mer utfyllende forklaring.",
 };
