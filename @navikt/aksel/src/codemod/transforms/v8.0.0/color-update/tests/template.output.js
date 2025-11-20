56a3139537350102f2ae1aa77f780a8643297f94
Ken <ken.aleksander@gmail.com>
2025-11-20 16:11:28 +0100

    :tada: Set up template cases

---
+++ @navikt/aksel/src/codemod/transforms/v8.0.0/color-update/tests/template.output.js
@@ -0,0 +1,12 @@
+import { Tag } from "@navikt/ds-react";
+
+const Component = () => {
+  return <div>
+    <Tag data-color="info" variant="outline">Text</Tag>
+    <Tag data-color="info" variant="moderate">Text</Tag>
+    <Tag data-color="info" variant="strong">Text</Tag>
+    <Tag data-color="info" variant="outline">Text</Tag>
+    <Tag data-color="info" variant="moderate">Text</Tag>
+    <Tag data-color="info" variant="strong">Text</Tag>
+  </div>;
+}
