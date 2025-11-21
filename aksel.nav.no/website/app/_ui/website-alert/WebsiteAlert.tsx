ad5bd1d3a59ec8cd80b23bce722d53231872a719
Ken <ken.aleksander@gmail.com>
2025-11-21 12:09:40 +0100

    :recycle: Always use InfoCard in WebsiteAlert

--- aksel.nav.no/website/app/_ui/website-alert/WebsiteAlert.tsx
+++ aksel.nav.no/website/app/_ui/website-alert/WebsiteAlert.tsx
@@ -1,5 +1,10 @@
 import { PortableTextBlock, stegaClean } from "next-sanity";
-import { InformationSquareIcon } from "@navikt/aksel-icons";
+import {
+  CheckmarkCircleIcon,
+  ExclamationmarkTriangleIcon,
+  InformationSquareIcon,
+  XMarkOctagonIcon,
+} from "@navikt/aksel-icons";
 import { Alert } from "@navikt/ds-react";
 import {
   InfoCard,
@@ -7,12 +12,6 @@ import {
   InfoCardHeader,
   InfoCardTitle,
 } from "@navikt/ds-react/InfoCard";
-import {
-  LocalAlert,
-  LocalAlertContent,
-  LocalAlertHeader,
-  LocalAlertTitle,
-} from "@navikt/ds-react/LocalAlert";
 import { CustomPortableText } from "@/app/CustomPortableText";
 import { ExtractPortableComponentProps } from "@/app/_sanity/types";
 
@@ -31,57 +30,37 @@ function WebsiteAlert(props: ExtractPortableComponentProps<"alert">) {
     );
   }
 
-  if (variant === "info") {
-    return (
-      <InfoCard data-color="info" data-block-margin="space-28">
-        <InfoCardHeader icon={<InformationSquareIcon aria-hidden />}>
-          <InfoCardTitle
-            as={stegaClean(heading_level) ?? "h2"}
-            id={`${encodeURIComponent(heading)}-a`}
-            className="scroll-m-20"
-          >
-            {heading}
-          </InfoCardTitle>
-        </InfoCardHeader>
-        <InfoCardContent>
-          <CustomPortableText value={body as PortableTextBlock[]} />
-        </InfoCardContent>
-      </InfoCard>
-    );
-  }
-
-  /**
-   * We remove the `role` prop since none of the "Alerts" are actually
-   * meant to be assertive live regions.
-   */
   return (
-    <LocalAlert
-      status={variant}
+    <InfoCard
+      data-color={variant === "error" ? "danger" : variant}
       data-block-margin="space-28"
-      role={undefined}
-      /**
-       * TODO: Multiple alerts with same status leads to repeated landmark labels in Axe.
-       * - Add more modules in Sanity for displaying positive/negative content: Do/Dont for content etc
-       * - Migrate most of the current Alert-usages to these new modules
-       * - Remove ignore when above is done
-       * @see /god-praksis/artikler/vurder-behovet-for-ki for page where multiple alerts are used.
-       */
-      data-axe-ignore
     >
-      <LocalAlertHeader>
-        <LocalAlertTitle
+      <InfoCardHeader icon={<StatusIcon status={variant} />}>
+        <InfoCardTitle
           as={stegaClean(heading_level) ?? "h2"}
           id={`${encodeURIComponent(heading)}-a`}
           className="scroll-m-20"
         >
           {heading}
-        </LocalAlertTitle>
-      </LocalAlertHeader>
-      <LocalAlertContent>
+        </InfoCardTitle>
+      </InfoCardHeader>
+      <InfoCardContent>
         <CustomPortableText value={body as PortableTextBlock[]} />
-      </LocalAlertContent>
-    </LocalAlert>
+      </InfoCardContent>
+    </InfoCard>
   );
+}
+
+const STATUS_ICONS = {
+  info: InformationSquareIcon,
+  success: CheckmarkCircleIcon,
+  warning: ExclamationmarkTriangleIcon,
+  error: XMarkOctagonIcon,
+} as const;
+
+function StatusIcon({ status }: { status: keyof typeof STATUS_ICONS }) {
+  const Icon = STATUS_ICONS[status];
+  return <Icon aria-hidden />;
 }
 
 export { WebsiteAlert };
