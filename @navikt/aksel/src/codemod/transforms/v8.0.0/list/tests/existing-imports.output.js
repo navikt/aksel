cbcdf9f87f9529aec0d75b4bdf57dba2d9698d4f
Ken <ken.aleksander@gmail.com>
2025-11-24 12:46:47 +0100

    :memo: Handle allready existing imports

---
+++ @navikt/aksel/src/codemod/transforms/v8.0.0/list/tests/existing-imports.output.js
@@ -0,0 +1,30 @@
+import { List, Heading as CustomHeading, BodyShort, Box } from "@navikt/ds-react";
+
+export const MyComponent = () => {
+  return (
+    <div>
+      <Box marginBlock="space-16" asChild>
+        <CustomHeading>Heading</CustomHeading>
+      </Box>
+      <div>
+        <CustomHeading level="2" size="small">
+          Lorem Ipsum Dolor Sit Amet
+        </CustomHeading>
+        <BodyShort>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel
+          risus volutpat.
+        </BodyShort>
+        <Box marginBlock="space-16" asChild>
+          <List>
+            <List.Item>
+              lorem ipsum
+            </List.Item>
+            <List.Item>
+              lorem ipsum
+            </List.Item>
+          </List>
+        </Box>
+      </div>
+    </div>
+  );
+};
