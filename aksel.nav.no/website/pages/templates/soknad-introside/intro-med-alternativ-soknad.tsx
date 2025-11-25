78f780b6fa508def251b607c9274c4446447af8e
Ken <ken.aleksander@gmail.com>
2025-11-25 14:09:10 +0100

    :bug: Re-add react import

--- aksel.nav.no/website/pages/templates/soknad-introside/intro-med-alternativ-soknad.tsx
+++ aksel.nav.no/website/pages/templates/soknad-introside/intro-med-alternativ-soknad.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { ArrowRightIcon } from "@navikt/aksel-icons";
 import {
   Accordion,
--- aksel.nav.no/website/pages/templates/soknad-introside/intro-med-fremhevet-info.tsx
+++ aksel.nav.no/website/pages/templates/soknad-introside/intro-med-fremhevet-info.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { ArrowRightIcon, InformationSquareIcon } from "@navikt/aksel-icons";
 import {
   Accordion,
