cf2b8b13f7cbc103b9654b447a69e48cfd285059
Ken <ken.aleksander@gmail.com>
2025-11-25 10:40:54 +0100

    :tada: Update button variant and colors

---
+++ aksel.nav.no/website/pages/eksempler/button/demo.tsx
@@ -0,0 +1,24 @@
+import { Button, HStack } from "@navikt/ds-react";
+import { withDsExample } from "@/web/examples/withDsExample";
+
+const Example = () => {
+  return (
+    <HStack gap="space-8">
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="tertiary">Tertiary</Button>
+    </HStack>
+  );
+};
+
+// EXAMPLES DO NOT INCLUDE CONTENT BELOW THIS LINE
+export default withDsExample(Example);
+
+/* Storybook story */
+export const Demo = {
+  render: Example,
+};
+
+export const args = {
+  index: 0,
+};
