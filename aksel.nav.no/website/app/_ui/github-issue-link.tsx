734a3d3f9792e41b730ce9c9ba5c165683601abc
Halvor Haugan <halvor.haugan@nav.no>
2025-11-28 11:52:09 +0100

    fix "window is not defined" + stricter type on template

--- aksel.nav.no/website/app/_ui/github-issue-link.tsx
+++ aksel.nav.no/website/app/_ui/github-issue-link.tsx
@@ -4,7 +4,7 @@ import { Link } from "@navikt/ds-react";
 
 interface Props {
   labels: string;
-  template: string;
+  template: "bug-report.md";
   title: string;
   includeUrlInBody?: boolean;
   children: React.ReactNode;
@@ -17,15 +17,14 @@ export default function GitHubIssueLink({
   includeUrlInBody,
   children,
 }: Props) {
-  const url =
+  const url = typeof window === "undefined" ? "" : window.location.href;
+
+  const href =
     "https://github.com/navikt/aksel/issues/new?" +
     `labels=${encodeURIComponent(labels)}` +
     `&template=${template}` +
     `&title=${encodeURIComponent(title)}` +
-    `${
-      includeUrlInBody
-        ? `&body=${encodeURIComponent(`URL: ${window.location.href}`)}`
-        : ""
-    }`;
-  return <Link href={url}>{children}</Link>;
+    `${includeUrlInBody ? `&body=${encodeURIComponent(`URL: ${url}`)}` : ""}`;
+
+  return <Link href={href}>{children}</Link>;
 }
