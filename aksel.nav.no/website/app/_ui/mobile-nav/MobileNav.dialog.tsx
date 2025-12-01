6430d4a312c2968b926c4090fd005d697a972fdc
Ken <ken.aleksander@gmail.com>
2025-12-01 11:26:07 +0100

    :tada: Use dialog for website mobile-menu

--- aksel.nav.no/website/app/_ui/mobile-nav/MobileNav.dialog.tsx
+++ aksel.nav.no/website/app/_ui/mobile-nav/MobileNav.dialog.tsx
@@ -1,40 +1,20 @@
 "use client";
 
-import { XMarkIcon } from "@navikt/aksel-icons";
-import { Button, Heading, Modal } from "@navikt/ds-react";
-import styles from "./MobileNav.module.css";
+import { Dialog } from "@navikt/ds-react";
 import { useMobileNav } from "./MobileNav.provider";
 
 function MobileNavDialog({ children }: { children: React.ReactNode }) {
-  const { open, toggleOpen, focusRef } = useMobileNav();
+  const { open, toggleOpen } = useMobileNav();
 
   return (
-    <Modal
-      open={open}
-      onClose={() => toggleOpen(false)}
-      width="small"
-      closeOnBackdropClick
-      className={styles.mobileNavDialog}
-      aria-labelledby="mobile-nav-menu"
-    >
-      <Modal.Header
-        closeButton={false}
-        className={styles.mobileNavDialogHeader}
-      >
-        <Heading level="1" size="medium" id="mobile-nav-menu">
-          Aksel
-        </Heading>
-        <Button
-          onClick={() => toggleOpen(false)}
-          variant="secondary-neutral"
-          icon={<XMarkIcon title="Lukk" />}
-          ref={(ref) => {
-            focusRef.current = ref;
-          }}
-        />
-      </Modal.Header>
-      {children}
-    </Modal>
+    <Dialog open={open} onOpenChange={() => toggleOpen(false)}>
+      <Dialog.Popup width="small" closeOnOutsideClick position="right">
+        <Dialog.Header>
+          <Dialog.Title>Aksel</Dialog.Title>
+        </Dialog.Header>
+        {children}
+      </Dialog.Popup>
+    </Dialog>
   );
 }
 
