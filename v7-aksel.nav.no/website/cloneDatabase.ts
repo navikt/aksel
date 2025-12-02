d561ce4d23eaa41b00104bd3fccec33153579972
Ken <ken.aleksander@gmail.com>
2025-12-02 12:47:29 +0100

    :tada: Extracted all relevant types to fetch

--- v7-aksel.nav.no/website/cloneDatabase.ts
+++ v7-aksel.nav.no/website/cloneDatabase.ts
@@ -1,13 +1,18 @@
 /**
  * List of document types that should be preserved during a content migration.
  */
-export const preservedTypes = [
-  "komponenter_landingsside",
+const alltypes = [
   "grunnleggende_landingsside",
-  "templates_landingsside",
+  "komponenter_landingsside",
+  "token_kategori",
+  "kode_eksempler_fil",
   "komponent_artikkel",
   "ds_artikkel",
+  "ds_props",
+  "aksel_standalone",
+  "redirect",
   "aksel_ds_forside",
   "aksel_forside",
-  "aksel_standalone",
+  "sanity.fileAsset",
+  "sanity.imageAsset",
 ];
