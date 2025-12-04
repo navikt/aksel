cb6a4156aa972314fd875e0e2e6004ea96a31880
Ken <ken.aleksander@gmail.com>
2025-12-04 16:16:49 +0100

    :bug: Use local import of data instead of fs-read

--- v7-aksel.nav.no/website/app/_sanity/live.ts
+++ v7-aksel.nav.no/website/app/_sanity/live.ts
@@ -1,21 +1,6 @@
-import fs from "fs";
 import { evaluate, parse } from "groq-js";
-import NodeCache from "node-cache";
 import "server-only";
-
-const cache = new NodeCache({ stdTTL: 0 });
-const DATASET_KEY = "sanity-dataset";
-
-function getDataset() {
-  let dataset = cache.get(DATASET_KEY);
-
-  if (!dataset) {
-    dataset = JSON.parse(fs.readFileSync("./scripts/doc-data.json", "utf-8"));
-    cache.set(DATASET_KEY, dataset);
-  }
-
-  return dataset;
-}
+import Data from "../../scripts/doc-data.json";
 
 async function sanityLocalFetch({
   query,
@@ -25,8 +10,8 @@ async function sanityLocalFetch({
   params?: { [key: string]: any };
 }) {
   const tree = parse(query);
-  const dataset = getDataset();
-  const value = await evaluate(tree, { dataset, params });
+
+  const value = await evaluate(tree, { dataset: Data, params });
 
   return { data: await value.get() };
 }
