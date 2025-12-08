a66861ef644cae55eae3412f2b36f113820da86a
Ken <ken.aleksander@gmail.com>
2025-12-08 12:27:09 +0100

    :memo: Update stylelint TODOs

--- @navikt/aksel-stylelint/src/recommended.ts
+++ @navikt/aksel-stylelint/src/recommended.ts
@@ -3,9 +3,11 @@ module.exports = {
   rules: {
     "aksel/design-token-exists": true,
     "aksel/no-internal-tokens": true,
+    /* TODO: Update to check .aksel classes */
     "aksel/no-class-override": [true, { severity: "warning" }],
     "aksel/no-deprecated-classes": true,
     "aksel/design-token-no-global-override": true,
+    /* TODO: Remove this rule in v8 */
     "aksel/design-token-no-component-reference": true,
   },
 };
