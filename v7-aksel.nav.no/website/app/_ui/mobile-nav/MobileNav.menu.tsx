bf0618c38167670335b90eeb7100f9e585b81a55
Halvor Haugan <halvor.haugan@nav.no>
2025-12-08 12:00:00 +0100

    Clean up MobileNav

--- v7-aksel.nav.no/website/app/_ui/mobile-nav/MobileNav.menu.tsx
+++ v7-aksel.nav.no/website/app/_ui/mobile-nav/MobileNav.menu.tsx
@@ -1,85 +1,16 @@
 "use client";
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useState } from "react";
-import { ArrowLeftIcon, ChevronDownIcon } from "@navikt/aksel-icons";
-import { HGrid, VStack } from "@navikt/ds-react";
-import { useMobileNav } from "@/app/_ui/mobile-nav/MobileNav.provider";
+import { HGrid } from "@navikt/ds-react";
 import styles from "./MobileNav.module.css";
 
 function MobileNavMenu({ children }: { children: React.ReactNode }) {
-  const { focusRef } = useMobileNav();
-  const pathName = usePathname();
-  const [open, setOpen] = useState(pathName?.startsWith("/designsystemet"));
-
-  const handleOpenToggle = (toState?: boolean) => {
-    setOpen(toState);
-
-    queueMicrotask(() => {
-      focusRef.current?.focus();
-    });
-  };
-
-  return open ? (
-    <DesignsystemView toggleClose={() => handleOpenToggle(false)}>
-      {children}
-    </DesignsystemView>
-  ) : (
-    <InitialView toggleOpen={() => handleOpenToggle(true)} />
-  );
-}
-
-function InitialView({ toggleOpen }: { toggleOpen: () => void }) {
-  return (
-    <VStack marginInline="space-8" position="relative">
-      <HGrid columns="1fr auto">
-        <a href="/designsystemet" className={styles.mobileNavItem}>
-          Designsystemet
-        </a>
-        <button
-          className={styles.mobileNavItem}
-          onClick={toggleOpen}
-          aria-expanded="false"
-        >
-          <ChevronDownIcon title="Åpne designsystem-meny" fontSize="1.5rem" />
-        </button>
-      </HGrid>
-      <span className={styles.divider} />
-
-      <Link href="/god-praksis" className={styles.mobileNavItem}>
-        God praksis
-      </Link>
-      <span className={styles.divider} />
-      <Link href="/produktbloggen" className={styles.mobileNavItem}>
-        Bloggen
-      </Link>
-    </VStack>
-  );
-}
-
-function DesignsystemView({
-  children,
-  toggleClose,
-}: {
-  children: React.ReactNode;
-  toggleClose: () => void;
-}) {
   return (
     <div>
       <HGrid
-        columns="auto 1fr"
+        columns="auto"
         marginInline="space-8"
         className={styles.mobileNavGroupSticky}
       >
-        <button
-          className={styles.mobileNavItem}
-          onClick={toggleClose}
-          aria-expanded="true"
-        >
-          <ArrowLeftIcon title="Lukk designsystem-meny" fontSize="1.5rem" />
-        </button>
-
         <a
           href="/designsystemet"
           className={`${styles.mobileNavItem} ${styles.mobileNavItemCentered}`}
