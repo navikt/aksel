8c001705873a7051b288195b17d21069cb588127
Halvor Haugan <83693529+HalvorHaugan@users.noreply.github.com>
2025-12-08 15:57:27 +0100

    Apply suggestions from code review

--- @navikt/aksel-stylelint/src/deprecations.ts
+++ @navikt/aksel-stylelint/src/deprecations.ts
@@ -6,11 +6,13 @@ type DeprecatedList = {
 
 export const deprecations: DeprecatedList = [
   {
+    // For testing
     classes: ["aksel-deprecated-example", "aksel-other-deprecated-example"],
     message:
       "Removed in vX.X.X, see documentation [link] for more information.",
   },
   {
+    // For testing
     classes: ["aksel-deprecated-prefix-example"],
     message:
       "Removed in vX.X.X, see documentation [link] for more information.",
