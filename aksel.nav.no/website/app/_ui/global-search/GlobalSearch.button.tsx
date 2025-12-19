b2b12a3129edc225b3837b8d196f025f9386b43d
Ken <ken.aleksander@gmail.com>
2025-12-19 11:55:11 +0100

    :bug: Optional trigger for fallback in global search suspense

--- aksel.nav.no/website/app/_ui/global-search/GlobalSearch.button.tsx
+++ aksel.nav.no/website/app/_ui/global-search/GlobalSearch.button.tsx
@@ -1,37 +1,59 @@
 "use client";
 
+import { type ButtonHTMLAttributes, forwardRef } from "react";
 import { MagnifyingGlassIcon } from "@navikt/aksel-icons";
 import { Bleed, Button, Detail, Dialog, HStack, Show } from "@navikt/ds-react";
 import { Kbd } from "@/app/_ui/kbd/Kbd";
 import styles from "./GlobalSearch.module.css";
 
-function GlobalSearchButton() {
-  return (
-    <Dialog.Trigger>
-      <Button variant="secondary-neutral" aria-keyshortcuts="Control+k">
-        <Bleed asChild marginInline={{ xs: "space-8", md: "space-8 0" }}>
-          <HStack gap="space-6" align="center" as="span">
-            <MagnifyingGlassIcon
-              className={styles.searchButtonIcon}
-              aria-label="Åpne søk"
-              fontSize="1.5rem"
-            />
-            <Show above="md" asChild>
-              <HStack gap="space-8" as="span" aria-hidden>
-                Søk
-                <HStack gap="space-2" asChild>
-                  <Detail as="span">
-                    <Kbd>Ctrl</Kbd>
-                    <Kbd>k</Kbd>
-                  </Detail>
-                </HStack>
-              </HStack>
-            </Show>
-          </HStack>
-        </Bleed>
-      </Button>
-    </Dialog.Trigger>
-  );
+/**
+ * Trigger is optional to allow for use in Suspense fallback
+ */
+function GlobalSearchButton({ trigger = true }: { trigger?: boolean }) {
+  if (trigger) {
+    return (
+      <Dialog.Trigger>
+        <SearchButton />
+      </Dialog.Trigger>
+    );
+  }
+
+  return <SearchButton />;
 }
+
+const SearchButton = forwardRef<
+  HTMLButtonElement,
+  ButtonHTMLAttributes<HTMLButtonElement>
+>((props, forwardedRef) => {
+  return (
+    <Button
+      {...props}
+      ref={forwardedRef}
+      variant="secondary-neutral"
+      aria-keyshortcuts="Control+k"
+    >
+      <Bleed asChild marginInline={{ xs: "space-8", md: "space-8 0" }}>
+        <HStack gap="space-6" align="center" as="span">
+          <MagnifyingGlassIcon
+            className={styles.searchButtonIcon}
+            aria-label="Åpne søk"
+            fontSize="1.5rem"
+          />
+          <Show above="md" asChild>
+            <HStack gap="space-8" as="span" aria-hidden>
+              Søk
+              <HStack gap="space-2" asChild>
+                <Detail as="span">
+                  <Kbd>Ctrl</Kbd>
+                  <Kbd>k</Kbd>
+                </Detail>
+              </HStack>
+            </HStack>
+          </Show>
+        </HStack>
+      </Bleed>
+    </Button>
+  );
+});
 
 export { GlobalSearchButton };
--- aksel.nav.no/website/app/_ui/header/Header.tsx
+++ aksel.nav.no/website/app/_ui/header/Header.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Suspense } from "react";
 import { BodyShort, Box, HStack, Hide, Show, Spacer } from "@navikt/ds-react";
 import { GlobalSearch } from "@/app/_ui/global-search/GlobalSearch";
+import { GlobalSearchButton } from "@/app/_ui/global-search/GlobalSearch.button";
 import { MobileNav } from "@/app/_ui/mobile-nav/MobileNav";
 import { ThemeButton } from "@/app/_ui/theming/Theme.button";
 import AkselLogo from "@/assets/Logo";
@@ -73,7 +74,7 @@ function Header({ variant }: { variant?: "default" | "produktbloggen" }) {
           </Box>
         </Show>
         <HStack align="center" gap="2">
-          <Suspense>
+          <Suspense fallback={<GlobalSearchButton trigger={false} />}>
             <GlobalSearch />
           </Suspense>
 
