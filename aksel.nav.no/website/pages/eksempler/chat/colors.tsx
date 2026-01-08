49e88e8e2059b57d9313bc33feddb2fcc9a6c4f2
Ken <ken.aleksander@gmail.com>
2026-01-08 16:19:13 +0100

    :memo: More example updates for darkside colors

--- aksel.nav.no/website/pages/eksempler/chat/colors.tsx
+++ aksel.nav.no/website/pages/eksempler/chat/colors.tsx
@@ -4,7 +4,12 @@ import { withDsExample } from "@/web/examples/withDsExample";
 const Example = () => {
   return (
     <VStack gap="space-40">
-      <Chat avatar="EVA" name="EVA" timestamp="01.01.21 14:00" variant="info">
+      <Chat
+        avatar="EVA"
+        name="EVA"
+        timestamp="01.01.21 14:00"
+        data-color="info"
+      >
         <Chat.Bubble>Hei! Mitt navn er Eva.</Chat.Bubble>
         <Chat.Bubble>Hva kan jeg hjelpe deg med?</Chat.Bubble>
       </Chat>
@@ -12,7 +17,7 @@ const Example = () => {
         name="Ola Normann"
         timestamp="01.01.21 14:00"
         position="right"
-        variant="subtle"
+        data-color="brand-magenta"
       >
         <Chat.Bubble>Hei Eva.</Chat.Bubble>
         <Chat.Bubble>
