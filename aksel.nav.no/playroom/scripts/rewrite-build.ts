12a578f0ff71c65f031c1c0241a05b8a960ba9e6
Ken <ken.aleksander@gmail.com>
2026-01-09 16:37:14 +0100

    :bug: Resolve playroom preview issue

--- aksel.nav.no/playroom/scripts/rewrite-build.ts
+++ aksel.nav.no/playroom/scripts/rewrite-build.ts
@@ -1,3 +1,4 @@
+import fs from "fast-glob";
 import { readFileSync, writeFileSync } from "node:fs";
 
 const buildDir = "./dist/sandbox";
@@ -10,3 +11,18 @@ let frame = readFileSync(frameFile, "utf-8");
 frame = frame.replaceAll("./", "../");
 
 writeFileSync(`${buildPreviewDir}/frame.html`, frame);
+
+/**
+ * Temp fix until this issue is resolved
+ * https://github.com/seek-oss/playroom/issues/469
+ */
+const inputString = `$/,"/")`;
+const outputString = `/, "/")`;
+
+const jsFiles = fs.sync(`${buildDir}/preview.*.js`, { onlyFiles: true });
+for (const file of jsFiles) {
+  let content = readFileSync(file, "utf-8");
+
+  content = content.replaceAll(inputString, outputString);
+  writeFileSync(file, content);
+}
