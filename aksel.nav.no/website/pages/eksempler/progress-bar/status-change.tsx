75fa10a78146906fa48ce0ebd62c597ffa0b7e23
Ken <ken.aleksander@gmail.com>
2026-01-13 09:28:20 +0100

    :memo: Example id

--- aksel.nav.no/website/pages/eksempler/progress-bar/status-change.tsx
+++ aksel.nav.no/website/pages/eksempler/progress-bar/status-change.tsx
@@ -6,12 +6,12 @@ const Example = () => {
   return (
     <VStack width="300px" gap="space-8">
       <ProgressBar
-        aria-labelledby="indeterminate-working"
+        aria-labelledby="indeterminate-error"
         value={5}
         valueMax={7}
         data-color="danger"
       />
-      <InlineMessage status="error" id="indeterminate-working">
+      <InlineMessage status="error" id="indeterminate-error">
         Noe gikk galt!
       </InlineMessage>
     </VStack>
