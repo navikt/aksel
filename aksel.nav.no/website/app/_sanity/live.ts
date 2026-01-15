0a4639eeee1b56bdc42b08796a06d86f5d74acee
Ken <ken.aleksander@gmail.com>
2026-01-15 12:53:21 +0100

    :fire: Removed perspective stack from sanity

--- aksel.nav.no/website/app/_sanity/live.ts
+++ aksel.nav.no/website/app/_sanity/live.ts
@@ -1,17 +1,10 @@
-import { type DefinedSanityFetchType, defineLive } from "next-sanity/live";
+import { defineLive } from "next-sanity/live";
 import "server-only";
 import { readWithDraftToken } from "@/app/_sanity/token";
 import { client } from "./client";
 
-export const { sanityFetch: _sanityFetch, SanityLive } = defineLive({
+export const { sanityFetch, SanityLive } = defineLive({
   client,
   serverToken: readWithDraftToken,
   browserToken: readWithDraftToken,
 });
-
-/**
- * TODO: Remove before v8 release!!
- * We only unwrap it to override perspecitive for testing
- */
-export const sanityFetch: DefinedSanityFetchType = (args) =>
-  _sanityFetch({ ...args, perspective: ["rMeiw8Pi2"] });
