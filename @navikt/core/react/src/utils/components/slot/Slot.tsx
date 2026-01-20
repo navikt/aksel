493528e81dd7d60fee72bd1ec4c6447315f10877
Halvor Haugan <halvor.haugan@nav.no>
2026-01-20 15:21:52 +0100

    just console error

--- @navikt/core/react/src/utils/components/slot/Slot.tsx
+++ @navikt/core/react/src/utils/components/slot/Slot.tsx
@@ -37,12 +37,9 @@ const Slot = React.forwardRef<HTMLElement, SlotProps>((props, forwardedRef) => {
     throw error;
   }
 
-  const error2 = new Error(
-    "Aksel: Components using 'asChild' expects to recieve a valid React element child.",
-  );
-  error2.name = "SlotError";
-  Error.captureStackTrace?.(error2, Slot);
-  throw error2;
+  console.error("Aksel: Slot component expected a valid React element child.");
+
+  return null;
 });
 
 export { Slot, type SlotProps };
