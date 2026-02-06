86a33d40e0dc45d16f74cc4a320e6feb58dbbb6b
Ken <ken.aleksander@gmail.com>
2026-02-06 12:20:01 +0100

    :tada: Detect user agent from header for search-button icon

--- aksel.nav.no/website/app/_ui/global-search/GlobalSearch.button.tsx
+++ aksel.nav.no/website/app/_ui/global-search/GlobalSearch.button.tsx
@@ -1,61 +1,39 @@
 "use client";
 
-import {
-  type ButtonHTMLAttributes,
-  forwardRef,
-  useEffect,
-  useMemo,
-  useState,
-} from "react";
+import { type ButtonHTMLAttributes, forwardRef } from "react";
 import { MagnifyingGlassIcon } from "@navikt/aksel-icons";
-import {
-  Bleed,
-  Box,
-  Button,
-  Detail,
-  Dialog,
-  HStack,
-  Show,
-} from "@navikt/ds-react";
+import { Bleed, Button, Detail, Dialog, HStack, Show } from "@navikt/ds-react";
 import { Kbd } from "@/app/_ui/kbd/Kbd";
 import styles from "./GlobalSearch.module.css";
 
 /**
  * Trigger is optional to allow for use in Suspense fallback
  */
-function GlobalSearchButton({ trigger = true }: { trigger?: boolean }) {
+function GlobalSearchButton({
+  trigger = true,
+  isMac,
+}: {
+  trigger?: boolean;
+  isMac: boolean;
+}) {
   if (trigger) {
     return (
       <Dialog.Trigger>
-        <SearchButton />
+        <SearchButton isMac={isMac} />
       </Dialog.Trigger>
     );
   }
 
-  return <SearchButton />;
+  return <SearchButton isMac={isMac} />;
 }
 
 const SearchButton = forwardRef<
   HTMLButtonElement,
-  ButtonHTMLAttributes<HTMLButtonElement>
->((props, forwardedRef) => {
-  const [isMac, setIsMac] = useState<boolean | null>(null);
-
-  useEffect(() => {
-    const userAgent = globalThis.navigator.userAgent.toLowerCase();
-    setIsMac(/mac|iphone|ipad|ipod/.test(userAgent));
-  }, []);
-
-  const shortCutkey = useMemo(() => {
-    if (isMac === null) {
-      return "⌘";
-    }
-    return isMac ? "⌘" : "Ctrl";
-  }, [isMac]);
-
+  ButtonHTMLAttributes<HTMLButtonElement> & { isMac: boolean }
+>(({ isMac, ...rest }, forwardedRef) => {
   return (
     <Button
-      {...props}
+      {...rest}
       ref={forwardedRef}
       variant="secondary-neutral"
       aria-keyshortcuts="Control+k"
@@ -72,12 +50,9 @@ const SearchButton = forwardRef<
               Søk
               <HStack gap="space-2" asChild>
                 <Detail as="span">
-                  <Box
-                    className={styles.keyboardShortcut}
-                    data-loaded={isMac !== null}
-                  >
-                    <Kbd>{shortCutkey}</Kbd>
-                  </Box>
+                  <div className={styles.keyboardShortcut}>
+                    <Kbd>{isMac ? "⌘" : "Ctrl"}</Kbd>
+                  </div>
                   <Kbd>k</Kbd>
                 </Detail>
               </HStack>
