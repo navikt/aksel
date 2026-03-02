112cea77bbcb03e4c475c64278483c92fbb54fa6
Ken <26967723+KenAJoh@users.noreply.github.com>
2026-03-02 14:59:43 +0100

    [InlineMessage] :tada: Updated InlineMessage to use BodyLong based on review (#4683)
    * :tada: Updated InlineMessage to use BodyLong based on review
    
    * :bug: Avoid duplicating typo-import

--- @navikt/core/react/src/inline-message/root/InlineMessage.tsx
+++ @navikt/core/react/src/inline-message/root/InlineMessage.tsx
@@ -1,6 +1,6 @@
 import React, { forwardRef } from "react";
 import { useThemeInternal } from "../../theme/Theme";
-import { BodyShort } from "../../typography";
+import { BodyLong } from "../../typography";
 import { type OverridableComponent, useId } from "../../utils-external";
 import { cl } from "../../utils/helpers";
 import { useI18n } from "../../utils/i18n/i18n.hooks";
@@ -64,7 +64,7 @@ export const InlineMessage: OverridableComponent<
     const contentId = useId();
 
     return (
-      <BodyShort
+      <BodyLong
         ref={forwardedRef}
         className={cl("aksel-inline-message", className)}
         data-color={status === "error" ? "danger" : status}
@@ -75,9 +75,9 @@ export const InlineMessage: OverridableComponent<
       >
         <InlineMessageIcon status={status} />
         {status && (
-          <BodyShort id={statusId} aria-hidden visuallyHidden>
+          <BodyLong id={statusId} aria-hidden visuallyHidden>
             {`${translate(status)}: `}
-          </BodyShort>
+          </BodyLong>
         )}
         {/** biome-ignore lint/a11y/useAriaPropsSupportedByRole: Testing shows that this works. */}
         <span
@@ -87,7 +87,7 @@ export const InlineMessage: OverridableComponent<
         >
           {children}
         </span>
-      </BodyShort>
+      </BodyLong>
     );
   },
 );
