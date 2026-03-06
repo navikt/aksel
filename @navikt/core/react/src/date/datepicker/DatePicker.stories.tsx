c999ed7d1bfcaa8f85dda7c76781a098d5c5fc50
Ken <ken.aleksander@gmail.com>
2026-03-06 13:56:18 +0100

    :bug: Even more placeholder space for chromatic demo

--- @navikt/core/react/src/date/datepicker/DatePicker.stories.tsx
+++ @navikt/core/react/src/date/datepicker/DatePicker.stories.tsx
@@ -623,7 +623,7 @@ export const FixedWeeks = () => {
   });
 
   return (
-    <Box minHeight="32rem">
+    <Box minHeight="42rem">
       <div style={{ display: "flex", gap: "1rem" }}>
         <DatePicker {...datepickerProps} dropdownCaption fixedWeeks open>
           <DatePicker.Input {...inputProps} label="Velg dato" />
