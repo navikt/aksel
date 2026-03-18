7b4a47d31b6cedadbe47935acbae605937094ccc
Anniken Granberg <43889032+nixenixe@users.noreply.github.com>
2026-03-18 08:52:29 +0100

    DataTable: dnd native skeleton

--- @navikt/core/react/src/data/drag-and-drop/item/DataDragAndDropItem.tsx
+++ @navikt/core/react/src/data/drag-and-drop/item/DataDragAndDropItem.tsx
@@ -1,10 +1,6 @@
-import { useSortable } from "@dnd-kit/react/sortable";
-import React, { useRef } from "react";
+import React from "react";
 import { HStack } from "../../../primitives/stack";
 import { cl } from "../../../utils/helpers";
-import { useMergeRefs } from "../../../utils/hooks";
-import { DataDragAndDropDragHandler } from "../drag-handler/DataDragAndDropDragHandler";
-import { DataDragAndDropContext } from "../root/DataDragAndDrop.context";
 
 interface DataDragAndDropItemProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
@@ -32,37 +28,21 @@ interface DataDragAndDropItemProps extends React.HTMLAttributes<HTMLDivElement> 
 const DataDragAndDropItem = React.forwardRef<
   HTMLDivElement,
   DataDragAndDropItemProps
->(({ children, id, index, className, ...rest }, forwardedRef) => {
-  const handleRef = useRef<HTMLDivElement>(null);
-  const { ref, isDragging, isDropTarget } = useSortable({
-    id,
-    index,
-    handle: handleRef,
-  });
-  const mergedRef = useMergeRefs(ref, forwardedRef);
-  const context = React.useContext(DataDragAndDropContext);
-  const mouseDragging = isDragging && context?.inputMethod === "mouse";
-  const mouseDropTarget = isDropTarget && context?.inputMethod === "mouse";
-  const keyboardDragging = isDragging && context?.inputMethod === "keyboard";
+>(({ children, className, ...rest }, forwardedRef) => {
+  // const context = useDataDragAndDropContext();
 
   return (
     <HStack gap="space-8" align="center" wrap={false} asChild>
       {/* TODO Should this be a <li>? */}
       <div
-        ref={mergedRef}
+        ref={forwardedRef}
         {...rest}
         className={cl("aksel-data-table__drag-and-drop-item", className)}
-        data-dragging={isDragging}
+        /*data-dragging={isDragging}
         data-mouse-dragging={mouseDragging}
         data-keyboard-dragging={keyboardDragging}
-        data-drop-target={mouseDropTarget}
-        tabIndex={-1}
+        data-drop-target={mouseDropTarget}*/
       >
-        <DataDragAndDropDragHandler
-          handleRef={handleRef}
-          keyboardDragging={keyboardDragging}
-          alt
-        />
         <div>{children}</div>
       </div>
     </HStack>
--- @navikt/core/react/src/data/drag-and-drop/root/DataDragAndDropRoot.tsx
+++ @navikt/core/react/src/data/drag-and-drop/root/DataDragAndDropRoot.tsx
@@ -1,11 +1,8 @@
-import { DragDropProvider, DragOverlay } from "@dnd-kit/react";
-import { isSortable } from "@dnd-kit/react/sortable";
-import React, { forwardRef, isValidElement } from "react";
-import { VStack } from "../../../primitives/stack";
+import React, { forwardRef } from "react";
 import DataDragAndDropItem, {
   DataDragAndDropItemProps,
 } from "../item/DataDragAndDropItem";
-import { DataDragAndDropContext } from "./DataDragAndDrop.context";
+import { DataDragAndDropProvider } from "./DataDragAndDrop.context";
 
 interface DataDragAndDropProps extends React.HTMLAttributes<HTMLDivElement> {
   children: any[];
@@ -29,62 +26,23 @@ interface DataDragAndDropRootComponent extends React.ForwardRefExoticComponent<
   Item: typeof DataDragAndDropItem;
 }
 
+/**
+ * TODO
+ * setItems on root
+ * state : active element
+ * pointer over listener / state, onPointerEnter, onPointerLeave
+ * Overlay - Use floating component
+ * Keyboard navigation
+ * Handle - button, arrows also button
+ * UU - announce on drag start, item moved, drag end
+ */
+
 const DataDragAndDrop = forwardRef<HTMLDivElement, DataDragAndDropProps>(
-  ({ setItems, children, ...rest }, forwardedRef) => {
-    const [inputMethod, setInputMethod] = React.useState<
-      "mouse" | "keyboard" | null
-    >(null);
-
-    const setItemOrder = (initalIndex: number, targetIndex: number) => {
-      setItems((items) => {
-        const newItems = [...items];
-        const [movedItem] = newItems.splice(initalIndex, 1);
-        newItems.splice(targetIndex, 0, movedItem);
-        return newItems;
-      });
-    };
-
+  ({ children }, forwardedRef) => {
     return (
-      <DataDragAndDropContext.Provider value={{ inputMethod }}>
-        <DragDropProvider
-          // TODO Look into overriding default keybinds, might eliminate context need
-          onBeforeDragStart={(event) =>
-            setInputMethod(
-              event.operation.activatorEvent?.type === "pointerdown"
-                ? "mouse"
-                : "keyboard",
-            )
-          }
-          onDragOver={(event) => {
-            if (event.operation.activatorEvent?.type === "pointerdown") {
-              // Prevents items to rearrange while dragging with mouse, but allows keyboard dragging to work as intended
-              event.preventDefault();
-            }
-          }}
-          onDragEnd={(event) => {
-            const { source, target } = event.operation;
-            if (!isSortable(source) || !isSortable(target)) return;
-            setItemOrder(source.initialIndex, target.index);
-          }}
-        >
-          <VStack asChild gap="space-12">
-            <div {...rest} ref={forwardedRef}>
-              {children}
-            </div>
-          </VStack>
-          <DragOverlay dropAnimation={null}>
-            {(source) => {
-              // Overlay not needed and causes glitching when using keyboard
-              if (inputMethod === "keyboard") return null;
-              if (!isSortable(source)) return null;
-              if (isValidElement(children[source.initialIndex])) {
-                return children[source.initialIndex];
-              }
-              return null;
-            }}
-          </DragOverlay>
-        </DragDropProvider>
-      </DataDragAndDropContext.Provider>
+      <DataDragAndDropProvider inputMethod={null}>
+        <div ref={forwardedRef}>{children}</div>
+      </DataDragAndDropProvider>
     );
   },
 ) as DataDragAndDropRootComponent;
