66d614d8121e4aa88ded9697961307671feb34d8
Halvor Haugan <halvor.haugan@nav.no>
2026-03-19 12:05:17 +0100

    Fieldset: Adjust examples

--- aksel.nav.no/website/pages/eksempler/fieldset/agepicker.tsx
+++ aksel.nav.no/website/pages/eksempler/fieldset/agepicker.tsx
@@ -1,18 +1,18 @@
-import { Fieldset, HStack, Select } from "@navikt/ds-react";
+import { BodyShort, Fieldset, HStack, Select } from "@navikt/ds-react";
 import { withDsExample } from "@/web/examples/withDsExample";
 
 const Example = () => {
   return (
     <Fieldset legend="Når vil du ta ut alderspensjon?">
       <HStack gap="space-16">
-        <Select label="Velg år">
+        <Select label={<BodyShort as="span">Velg år</BodyShort>}>
           <option></option>
           <option>62 år</option>
           <option>63 år</option>
           <option>64 år</option>
           <option>...</option>
         </Select>
-        <Select label="Velg måneder">
+        <Select label={<BodyShort as="span">Velg måneder</BodyShort>}>
           <option></option>
           <option>0 mnd. (apr.)</option>
           <option>1 mnd. (mai)</option>
@@ -34,5 +34,5 @@ export const Demo = {
 
 export const args = {
   index: 0,
-  desc: "Bruk av Fieldset her gjør at man unngår å gjenta konteksten i hver Select.",
+  desc: "Bruk av Fieldset her gjør at man unngår å gjenta konteksten i hver Select. (Her har vi justert ned font-vekten på labelen til hvert felt for å skape et tydeligere hierarki.)",
 };
