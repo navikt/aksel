4ca7e847bd25355149cf18b78280ff62c1009b3c
Ken <ken.aleksander@gmail.com>
2026-03-23 10:47:43 +0100

    :tada: Added checkbox-item component

--- @navikt/core/react/src/form/checkbox/Checkbox.tsx
+++ @navikt/core/react/src/form/checkbox/Checkbox.tsx
@@ -3,11 +3,12 @@ import { BodyShort } from "../../typography";
 import { omit, useId } from "../../utils-external";
 import { cl } from "../../utils/helpers";
 import { ReadOnlyIconWithTitle } from "../ReadOnlyIcon";
+import { CheckboxInput } from "./checkbox-input/CheckboxInput";
 import { CheckboxProps } from "./types";
 import useCheckbox from "./useCheckbox";
 
 export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
-  (props, ref) => {
+  (props: CheckboxProps, forwardedRef) => {
     const { inputProps, hasError, size, readOnly, nested } = useCheckbox(props);
     const descriptionId = useId();
 
@@ -25,53 +26,26 @@ export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
         )}
         data-color={hasError ? "danger" : props["data-color"]}
       >
-        <div className="aksel-checkbox__input-wrapper">
-          <input
-            {...omit(props, [
-              "children",
-              "size",
-              "error",
-              "description",
-              "hideLabel",
-              "indeterminate",
-              "errorId",
-              "readOnly",
-            ])}
-            {...omit(inputProps, ["aria-invalid", "aria-describedby"])}
-            aria-describedby={
-              cl(inputProps["aria-describedby"], {
-                [descriptionId]: props.description,
-              }) || undefined
-            }
-            type="checkbox"
-            className="aksel-checkbox__input"
-            ref={(el) => {
-              if (el) {
-                el.indeterminate = props.indeterminate ?? false;
-              }
-
-              if (typeof ref === "function") {
-                ref(el);
-              } else if (ref != null) {
-                ref.current = el;
-              }
-            }}
-          />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 13 10"
-            fill="none"
-            focusable={false}
-            role="img"
-            aria-hidden
-            className="aksel-checkbox__icon"
-          >
-            <path
-              d="M4.03524 6.41478L10.4752 0.404669C11.0792 -0.160351 12.029 -0.130672 12.5955 0.47478C13.162 1.08027 13.1296 2.03007 12.5245 2.59621L5.02111 9.59934C4.74099 9.85904 4.37559 10 4.00025 10C3.60651 10 3.22717 9.84621 2.93914 9.56111L0.439143 7.06111C-0.146381 6.47558 -0.146381 5.52542 0.439143 4.93989C1.02467 4.35437 1.97483 4.35437 2.56036 4.93989L4.03524 6.41478Z"
-              fill="currentColor"
-            />
-          </svg>
-        </div>
+        <CheckboxInput
+          ref={forwardedRef}
+          {...omit(props, [
+            "children",
+            "size",
+            "error",
+            "description",
+            "hideLabel",
+            "indeterminate",
+            "errorId",
+            "readOnly",
+          ])}
+          {...omit(inputProps, ["aria-invalid", "aria-describedby"])}
+          aria-describedby={
+            cl(inputProps["aria-describedby"], {
+              [descriptionId]: props.description,
+            }) || undefined
+          }
+          indeterminate={props.indeterminate ?? false}
+        />
         <BodyShort
           as="label"
           htmlFor={inputProps.id}
