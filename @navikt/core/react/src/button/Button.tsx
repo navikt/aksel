acb37dc77c7f6beec83836940d9d4a1edc441c0b
Halvor Haugan <halvor.haugan@nav.no>
2026-03-26 16:38:21 +0100

    test change

--- @navikt/core/react/src/button/Button.tsx
+++ @navikt/core/react/src/button/Button.tsx
@@ -6,6 +6,7 @@ import type { OverridableComponent } from "../utils-external";
 import { omit } from "../utils-external";
 import { cl, composeEventHandlers } from "../utils/helpers";
 
+// test
 type legacyVariants =
   | "primary-neutral"
   | "secondary-neutral"
