c4f0a108410edf98e24046a6e29eeca7f6ba27c3
Julian Nymark <julian.nymark@nav.no>
2026-04-13 14:21:44 +0200

    fix: send placement as lenkegruppe and actual link text as lenketekst in UmamiLink

--- aksel.nav.no/website/app/_ui/umami/UmamiLink.tsx
+++ aksel.nav.no/website/app/_ui/umami/UmamiLink.tsx
@@ -8,16 +8,20 @@ import { umamiTrack } from "@/app/_ui/umami/Umami.track";
 import styles from "./UmamiLink.module.css";
 
 export const UmamiLink = ({
-  umamiKilde,
+  lenkegruppe,
   ...props
-}: LinkProps & { umamiKilde: string }) => {
+}: LinkProps & { lenkegruppe: string }) => {
   return (
     <Link
       {...props}
       onClick={() =>
         umamiTrack(Events.NAVIGERE, {
-          lenketekst: stegaClean(umamiKilde),
+          lenketekst:
+            typeof props.children === "string"
+              ? stegaClean(props.children)
+              : stegaClean(props.href)!,
           destinasjon: stegaClean(props.href)!,
+          lenkegruppe: stegaClean(lenkegruppe),
         })
       }
       className={styles.umamiLink}
