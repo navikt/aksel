b33bebcb55ad1667bf29230b82adea93175fceb0
Halvor Haugan <halvor.haugan@nav.no>
2026-05-29 09:39:58 +0200

    [website] Add label and desc to FileUpload.Trigger example

--- aksel.nav.no/website/pages/eksempler/file-upload/trigger-button.tsx
+++ aksel.nav.no/website/pages/eksempler/file-upload/trigger-button.tsx
@@ -1,14 +1,30 @@
 import { UploadIcon } from "@navikt/aksel-icons";
-import { Button, FileUpload } from "@navikt/ds-react";
+import { BodyShort, Button, FileUpload, Label, VStack } from "@navikt/ds-react";
 import { withDsExample } from "@/web/examples/withDsExample";
 
 const Example = () => {
   return (
-    <FileUpload.Trigger multiple={false} onSelect={console.info}>
-      <Button variant="secondary" icon={<UploadIcon aria-hidden />}>
-        Last opp filer
-      </Button>
-    </FileUpload.Trigger>
+    <VStack gap="space-8" align="start">
+      <VStack gap="space-2" align="start">
+        <Label id="label-id" as="div">
+          Last opp dokumentasjon
+        </Label>
+        <BodyShort id="desc-id" textColor="subtle">
+          Du kan laste opp Word- og PDF-filer. Maks 3 filer. Maks størrelse 1
+          MB.
+        </BodyShort>
+      </VStack>
+
+      <FileUpload.Trigger multiple={false} onSelect={console.info}>
+        <Button
+          aria-describedby="label-id desc-id"
+          variant="secondary"
+          icon={<UploadIcon aria-hidden />}
+        >
+          Last opp filer
+        </Button>
+      </FileUpload.Trigger>
+    </VStack>
   );
 };
 
@@ -22,4 +38,5 @@ export const Demo = {
 
 export const args = {
   index: 6,
+  desc: "Enkel knapp uten dropzone. Label og ev. beskrivelse kobles opp med `aria-describedby`.",
 };
