b943867b4592f65ff38d5e763645d62de5164ca4
Ken <ken.aleksander@gmail.com>
2026-06-04 19:10:12 +0200

    :memo: Comments

--- mcp-server/src/helpers/fetch.ts
+++ mcp-server/src/helpers/fetch.ts
@@ -1,5 +1,9 @@
 const defaultFetchTimeoutMs = 10000;
 
+/**
+ * A wrapper around the standard `fetch` function that adds a default timeout.
+ * Makes sure that if the fetch takes longer than `timeoutMs`, it will be aborted and throw an error.
+ */
 function fetchWithTimeout(
   input: string | URL | Request,
   init?: RequestInit,
