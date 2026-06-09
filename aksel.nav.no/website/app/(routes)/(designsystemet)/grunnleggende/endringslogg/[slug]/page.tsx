e0609559ee03ebfe0672e3ce9869170f87f78ea3
Ken <ken.aleksander@gmail.com>
2026-06-09 13:26:35 +0200

    :tada: Clean up fremhevet code

--- aksel.nav.no/website/app/(routes)/(designsystemet)/grunnleggende/endringslogg/[slug]/page.tsx
+++ aksel.nav.no/website/app/(routes)/(designsystemet)/grunnleggende/endringslogg/[slug]/page.tsx
@@ -1,10 +1,9 @@
 import { format } from "date-fns";
 import { nb } from "date-fns/locale";
 import { PortableTextBlock } from "next-sanity";
-import Image from "next/image";
 import { notFound } from "next/navigation";
 import { Metadata } from "next/types";
-import { BodyShort, Box, HGrid, HStack, Heading, Tag } from "@navikt/ds-react";
+import { BodyShort, Box, HGrid, HStack, Heading } from "@navikt/ds-react";
 import { CustomPortableText } from "@/app/CustomPortableText";
 import { sanityFetch } from "@/app/_sanity/live";
 import {
@@ -12,11 +11,10 @@ import {
   ENDRINGSLOGG_WITH_NEIGHBORS_QUERY,
   SLUG_BY_TYPE_QUERY,
 } from "@/app/_sanity/queries";
-import { urlForImage, urlForOpenGraphImage } from "@/app/_sanity/utils";
+import { urlForOpenGraphImage } from "@/app/_sanity/utils";
 import { TableOfContents } from "@/app/_ui/toc/TableOfContents";
 import { capitalizeText } from "@/ui-utils/format-text";
 import { DesignsystemetPageLayout } from "../../../_ui/DesignsystemetPage";
-import styles from "../_ui/Changelog.module.css";
 import ChangelogLinkCard from "../_ui/ChangelogLinkCard";
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
@@ -37,7 +35,6 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
       publishedTime: pageData?.endringsdato || undefined,
       images:
         urlForOpenGraphImage(pageData?.seo?.image) ||
-        urlForOpenGraphImage(pageData?.herobilde) ||
         "/images/og/endringslogg/OG-endringslogg.png",
     },
   };
@@ -79,42 +76,23 @@ export default async function (props: Props) {
     }
   });
 
-  const { fremhevet, endringstype, heading, endringsdato, herobilde, content } =
-    logs.primary;
+  const { endringstype, heading, endringsdato, content } = logs.primary;
 
   return (
     <DesignsystemetPageLayout layout="with-toc">
       <div>
-        <BodyShort
-          size="medium"
-          textColor="subtle"
-          data-color={fremhevet ? "aksel-brand-pink" : "brand-blue"}
-        >
+        <BodyShort size="medium" textColor="subtle" data-color="brand-blue">
           {capitalizeText(endringstype || "")}
         </BodyShort>
-        <Heading
-          size="xlarge"
-          level="1"
-          spacing
-          data-color={fremhevet ? "aksel-brand-pink" : "brand-blue"}
-        >
+        <Heading size="xlarge" level="1" spacing data-color="brand-blue">
           {heading}
         </Heading>
         <HStack gap="space-16" marginBlock="space-0 space-28">
-          <BodyShort
-            size="small"
-            textColor="subtle"
-            data-color={fremhevet ? "aksel-brand-pink" : "brand-blue"}
-          >
+          <BodyShort size="small" textColor="subtle" data-color="brand-blue">
             {format(new Date(endringsdato || ""), "d. MMMM yyy", {
               locale: nb,
             })}
           </BodyShort>
-          {fremhevet && (
-            <Tag size="xsmall" variant="strong" data-color="aksel-brand-pink">
-              Fremhevet
-            </Tag>
-          )}
         </HStack>
       </div>
       <TableOfContents
@@ -125,20 +103,9 @@ export default async function (props: Props) {
         toc={toc || []}
       />
       <Box marginBlock="space-0 space-24">
-        {fremhevet && herobilde?.asset && (
-          <Image
-            className={styles.herobilde}
-            alt={herobilde.alt ? herobilde.alt : ""}
-            loading="lazy"
-            decoding="async"
-            src={urlForImage(herobilde)?.auto("format").url() || ""}
-            width={1200}
-            height={630}
-          />
-        )}
         <CustomPortableText
           value={content as PortableTextBlock[]}
-          data-color={fremhevet ? "aksel-brand-pink" : "brand-blue"}
+          data-color="brand-blue"
         />
       </Box>
       <HGrid
