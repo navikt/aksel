5f6a4690b02850ff325009fd42811125457a0c3c
Anniken Granberg <43889032+nixenixe@users.noreply.github.com>
2026-06-22 13:51:11 +0200

    open state

--- @navikt/core/react/src/lookup/Lookup.tsx
+++ @navikt/core/react/src/lookup/Lookup.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useRef, useState } from "react";
+import React, { forwardRef, useRef } from "react";
 import { XMarkIcon } from "@navikt/aksel-icons";
 import { Button } from "../button";
 import { Popover, PopoverProps } from "../popover";
@@ -8,7 +8,7 @@ import { useId } from "../utils-external";
 import { FocusBoundary } from "../utils/components/focus-boundary/FocusBoundary";
 import { FocusGuards } from "../utils/components/focus-guards/FocusGuards";
 import { cl, composeEventHandlers } from "../utils/helpers";
-import { useMergeRefs } from "../utils/hooks";
+import { useControllableState, useMergeRefs } from "../utils/hooks";
 import { useI18n } from "../utils/i18n/i18n.hooks";
 
 export interface LookupProps
@@ -27,6 +27,21 @@ export interface LookupProps
    * Title for the lookup element. Usually the same as `word`.
    */
   heading: string;
+  /**
+   * Controlled open-state.
+   *
+   * Using this removes automatic control of open-state.
+   */
+  open?: boolean;
+  /**
+   * The open state when initially rendered. Use when you do not need to control the open state.
+   * @default false
+   */
+  defaultOpen?: boolean;
+  /**
+   * Callback for current open-state.
+   */
+  onOpenChange?: (open: boolean) => void;
 }
 
 /**
@@ -54,11 +69,19 @@ export const Lookup = forwardRef<HTMLButtonElement, LookupProps>(
       onClick,
       heading,
       id: idProp,
+      open,
+      defaultOpen = false,
+      onOpenChange,
       ...rest
     },
     ref,
   ) => {
-    const [openState, setOpenState] = useState(false);
+    const [_open, _setOpen] = useControllableState({
+      defaultValue: defaultOpen,
+      value: open,
+      onChange: onOpenChange,
+    });
+
     const anchorRef = useRef<HTMLButtonElement>(null);
     const mergedRef = useMergeRefs(anchorRef, ref);
     const contentRef = useRef<HTMLDivElement>(null);
@@ -75,16 +98,14 @@ export const Lookup = forwardRef<HTMLButtonElement, LookupProps>(
           id={triggerId}
           ref={mergedRef}
           className={cl("aksel-lookup-trigger", className)}
-          onClick={composeEventHandlers(onClick, () =>
-            setOpenState((old) => !old),
-          )}
+          onClick={composeEventHandlers(onClick, () => _setOpen((old) => !old))}
           aria-haspopup="dialog"
-          aria-expanded={openState}
-          aria-controls={openState ? popoverContentId : undefined}
+          aria-expanded={_open}
+          aria-controls={_open ? popoverContentId : undefined}
         >
           {word}
         </button>
-        {openState && (
+        {_open && (
           <FocusGuards>
             <FocusBoundary
               loop
@@ -96,9 +117,9 @@ export const Lookup = forwardRef<HTMLButtonElement, LookupProps>(
               <Portal>
                 <Popover
                   anchorEl={anchorRef.current}
-                  open={openState}
+                  open={_open}
                   onClose={() => {
-                    setOpenState(false);
+                    _setOpen(false);
                   }}
                   placement={placement}
                   strategy={strategy}
@@ -128,7 +149,7 @@ export const Lookup = forwardRef<HTMLButtonElement, LookupProps>(
                       variant="tertiary"
                       data-color="neutral"
                       icon={<XMarkIcon title={translate("close")} />}
-                      onClick={() => setOpenState(false)}
+                      onClick={() => _setOpen(false)}
                     />
                   </Popover.Content>
                 </Popover>
