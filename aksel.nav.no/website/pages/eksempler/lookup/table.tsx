5f4b6dbedde65f22bcc97458c2b8da2195150963
Anniken Granberg <43889032+nixenixe@users.noreply.github.com>
2026-08-25 10:20:12 +0200

    added example with table

---
+++ aksel.nav.no/website/pages/eksempler/lookup/table.tsx
@@ -0,0 +1,66 @@
+import { Lookup, Table, VStack } from "@navikt/ds-react";
+import { withDsExample } from "@/web/examples/withDsExample";
+
+const Example = () => {
+  return (
+    <Table>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell scope="col">100% aldersperson</Table.HeaderCell>
+          <Table.HeaderCell scope="col">Kr per år</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {data.map(({ pension, amount, lookup }) => {
+          return (
+            <Table.Row key={pension}>
+              <Table.DataCell>
+                {lookup ? <Lookup word={pension}>{lookup}</Lookup> : pension}
+              </Table.DataCell>
+              <Table.DataCell>{amount}</Table.DataCell>
+            </Table.Row>
+          );
+        })}
+      </Table.Body>
+    </Table>
+  );
+};
+
+const data = [
+  {
+    pension: "Grunnpensjon",
+    amount: "168 788",
+  },
+  {
+    pension: "Tilleggspensjon",
+    amount: "28 680",
+  },
+  {
+    pension: "Inntektspensjon",
+    amount: "40 680",
+    lookup: (
+      <VStack align="center">
+        Pensjonsbeholdning x Uttaksgrad x Andel dagens regler
+        <hr style={{ height: "1px", width: "100%" }} />
+        Delingstall
+      </VStack>
+    ),
+  },
+  {
+    pension: "Garantiillegg",
+    amount: "28 234",
+  },
+];
+
+// EXAMPLES DO NOT INCLUDE CONTENT BELOW THIS LINE
+export default withDsExample(Example);
+
+/* Storybook story */
+export const Demo = {
+  render: Example,
+};
+
+export const args: ExampleArgsT = {
+  index: 2,
+  desc: "Lookup kan brukes i tabeller for å gi mer informasjon om innholdet i cellene.",
+};
