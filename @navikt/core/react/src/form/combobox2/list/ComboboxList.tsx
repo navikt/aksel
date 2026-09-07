20e38e66f248dceab6c1c0b44c72b0207d61a2da
Halvor Haugan <halvor.haugan@nav.no>
2026-09-07 17:25:19 +0200

    feat(Combobox2): uncontrolled, hidden input, stories, extend HTMLAttributes

--- @navikt/core/react/src/form/combobox2/list/ComboboxList.tsx
+++ @navikt/core/react/src/form/combobox2/list/ComboboxList.tsx
@@ -101,7 +101,7 @@ function ComboboxListContent<
             onToggleOption={localOnToggleOption}
             isSelected={selectedOptions.includes(option.value)}
             hasVirtualFocus={virtuallyFocusedOptionValue === option.value}
-            filterString={deferredFilterString}
+            highlightText={deferredFilterString}
           >
             {children as ComboboxListProps<typeof option>["children"]}
           </ComboboxOption>
@@ -114,7 +114,7 @@ function ComboboxListContent<
         onToggleOption={localOnToggleOption}
         isSelected={selectedOptions.includes(optOrGroup.value)}
         hasVirtualFocus={virtuallyFocusedOptionValue === optOrGroup.value}
-        filterString={deferredFilterString} // TODO: Vurder å kunne slå av dette for bedre ytelse
+        highlightText={deferredFilterString} // TODO: Vurder å kunne slå av dette for bedre ytelse
       >
         {children as ComboboxListProps<typeof optOrGroup>["children"]}
       </ComboboxOption>
