50df22046773230f0bd30f57cbdb3b0becf0c7c8
Anniken Granberg <43889032+nixenixe@users.noreply.github.com>
2026-09-15 18:20:05 +0200

    export stuff

--- @navikt/core/react/src/coachmark/index.ts
+++ @navikt/core/react/src/coachmark/index.ts
@@ -1,2 +1,24 @@
 "use client";
-export { default as Coachmark, type CoachmarkProps } from "./Coachmark";
+export {
+  Coachmark,
+  CoachmarkContent,
+  CoachmarkTitle,
+  CoachmarkDescription,
+  CoachmarkFooter,
+  CoachmarkImage,
+  CoachmarkProgress,
+  CoachmarkCloseTrigger,
+  CoachmarkNextTrigger,
+} from "./root/CoachmarkRoot";
+export type {
+  CoachmarkStepType,
+  CoachmarkProps,
+  CoachmarkContentProps,
+  CoachmarkTitleProps,
+  CoachmarkDescriptionProps,
+  CoachmarkFooterProps,
+  CoachmarkImageProps,
+  CoachmarkProgressProps,
+  CoachmarkCloseTriggerProps,
+  CoachmarkNextTriggerProps,
+} from "./root/CoachmarkRoot";
