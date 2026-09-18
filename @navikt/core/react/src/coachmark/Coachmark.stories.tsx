a261edd03ee36267091e0d33ca3d6f3ebee70ef1
Anniken Granberg <43889032+nixenixe@users.noreply.github.com>
2026-09-18 10:38:06 +0200

    new animation

--- @navikt/core/react/src/coachmark/Coachmark.stories.tsx
+++ @navikt/core/react/src/coachmark/Coachmark.stories.tsx
@@ -267,7 +267,7 @@ export const CoachmarkMixed: StoryFn<typeof Coachmark> = () => {
 };
 
 export const CoachmarkDot: StoryFn<typeof Coachmark> = () => {
-  const animations = ["ONE", "TWO", "THREE"];
+  const animations = ["ONE", "TWO", "THREE", "FOUR"];
   /* TODO:
    * - Set data-color only for dot?
    */
