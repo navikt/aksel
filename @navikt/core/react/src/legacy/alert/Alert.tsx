fe626853c8050736b41e4815a5f70c92417e897f
Anniken Granberg <43889032+nixenixe@users.noreply.github.com>
2026-09-18 13:45:57 +0200

    oops

--- @navikt/core/react/src/legacy/alert/Alert.tsx
+++ @navikt/core/react/src/legacy/alert/Alert.tsx
@@ -77,7 +77,7 @@ const IconMap = {
  * [LocalAlert](https://aksel.nav.no/komponenter/core/localalert),
  * [InfoCard](https://aksel.nav.no/komponenter/core/infocard), or
  * [InlineMessage](https://aksel.nav.no/komponenter/core/inlinemessage) instead.
- * Component will be removed in future versions when the usage of this component is phased enough out.
+ * Component will be removed in future version.
  *
  * @see [📝 Documentation](https://aksel.nav.no/komponenter/legacy/alert)
  * @see 🏷️ {@link AlertProps}
