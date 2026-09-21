2a71087fed55437b6653dfdc9f371b59054021d9
Ken <ken.aleksander@gmail.com>
2026-09-21 10:24:42 +0200

    fix: Removed redundant hstacks

--- aksel.nav.no/website/pages/eksempler/badge/count-anchored.tsx
+++ aksel.nav.no/website/pages/eksempler/badge/count-anchored.tsx
@@ -1,24 +1,20 @@
-import { Button, HStack, VStack } from "@navikt/ds-react";
+import { Button, VStack } from "@navikt/ds-react";
 import { Badge } from "@navikt/ds-react/PREVIEW";
 import { withDsExample } from "@/web/examples/withDsExample";
 
 const Example = () => {
   return (
     <VStack gap="space-12">
-      <HStack align="center" gap="space-8">
-        <Badge count={5}>
-          <Button variant="secondary" aria-label="Meldinger, 5 uleste">
-            Meldinger
-          </Button>
-        </Badge>
-      </HStack>
-      <HStack align="center" gap="space-8">
-        <Badge count={120}>
-          <Button variant="secondary" aria-label="Meldinger, 120 uleste">
-            Meldinger
-          </Button>
-        </Badge>
-      </HStack>
+      <Badge count={5}>
+        <Button variant="secondary" aria-label="Meldinger, 5 uleste">
+          Meldinger
+        </Button>
+      </Badge>
+      <Badge count={120}>
+        <Button variant="secondary" aria-label="Meldinger, 120 uleste">
+          Meldinger
+        </Button>
+      </Badge>
     </VStack>
   );
 };
